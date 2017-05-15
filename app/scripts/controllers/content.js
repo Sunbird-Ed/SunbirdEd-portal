943a93a90ea93db1e696425f2f91cf5191ccc3ef
Poonam <pnmsharma623.com>
2017-05-15 18:46:10 +0530

    Issue #1 feat: filters for search content

--- app/scripts/controllers/content.js
+++ app/scripts/controllers/content.js
@@ -8,47 +8,98 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-        .controller('ContentCtrl', function (contentService, $log) {
-            var content = this;
-            content.keyword = '';
+    .controller('ContentCtrl', function(contentService, $log) {
+        var content = this;
+        content.keyword = '';
+        content.filters = {};
+        content.languages = [
+            "Bengali", "English", "Gujarati", "Hindi", "Kannada", "Marathi", "Punjabi", "Tamil", "Telugu"
+        ];
+        content.contentTypes = [
+            "Story", "Worksheet", "Collections", "Game", "Plugin", "Template"
+        ]
+        content.statuses = [
+            "Draft", "Live", "Review", "Flagged", "Flag Draft", "Flag Review"
+        ]
 
-            content.listView = false;
-            content.searchContent = function () {
-                var req = {
-                    "query": content.keyword,
-                    "filters": {
-                    },
-                    "params": {
-                        "cid": "12"
-                    }
+        content.selectedLanguage = '';
+        content.selectedContentType = '';
+        content.selectedStatus = '';
+
+        content.listView = false;
+        content.searchContent = function() {
+
+            var req = {
+                "query": content.keyword,
+                "filters": content.filters,
+                "params": {
+                    "cid": "12"
                 }
+            }
 
-                contentService.search(req).then(function (res) {
+            contentService.search(req).then(function(res) {
                     console.log(res);
                     if (res.responseCode === "OK") {
                         content.data = res.result;
 
                     }
-                }), function (errorMessage) {
+                }),
+                function(errorMessage) {
                     $log.warn(errorMessage);
                 };
-            };
+        };
 
-            content.toggleView = function (isList)
-            {
-                content.listView = isList;
+        content.toggleView = function(isList) {
+            content.listView = isList;
+        }
+        content.loadRating = function() {
+            $('.ui.rating')
+                .rating({
+
+                    maxRating: 5
+
+                }).rating("disable", true);
+
+            $('.popup-button').popup({ inline: true });
+        };
+
+        content.searchContent();
+
+
+        function initSemanticUi() {
+            $('.ui.accordion')
+                .accordion();
+
+            $('.dropdown').dropdown({
+                useLabels: false,
+                forceSelection: false,
+                label: {
+                    duration: 0,
+                },
+                debug: true,
+                performance: true,
+            });
+
+        }
+        window.onload = initSemanticUi;
+        content.applyFilter = function() {
+            if (content.selectedLanguage) {
+                content.filters['language'] = content.selectedLanguage;
+            };
+            if (content.selectedLessonType) {
+                content.filters['contentType'] = content.selectedContentType;
             }
-            content.loadRating = function () {
-                $('.ui.rating')
-                        .rating({
-
-                            maxRating: 5
-
-                        }).rating("disable", true);
-
-                $('.popup-button').popup({inline: true});
-            };
+            if (content.selectedStatus) {
+                content.filters['status'] = content.selectedStatus;
+            }
             content.searchContent();
 
-        });
+        };
+        content.resetFilter = function() {
+            content.filters = {};
+            content.searchContent();
+            $('.dropdown').dropdown('clear');
 
+        }
+
+    });
