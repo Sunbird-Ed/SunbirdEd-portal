0561fc8bfee4fa80b7e72dbdb376d81e07410be5
loganathan <loganathan@netops-Latitude-E6540>
2017-05-15 14:55:17 +0530

    Issue #1 feat: auto-suggest for search content

--- app/scripts/controllers/content.js
+++ app/scripts/controllers/content.js
@@ -11,9 +11,12 @@ angular.module('playerApp')
         .controller('ContentCtrl', function (contentService, $log) {
             var content = this;
             content.keyword = '';
-
             content.listView = false;
-            content.searchContent = function () {
+            content.data = [];
+            content.autosuggest_data = [];
+
+            content.searchContent = function ($event) {
+                content.enableLoader(true);
                 var req = {
                     "query": content.keyword,
                     "filters": {
@@ -22,18 +25,31 @@ angular.module('playerApp')
                         "cid": "12"
                     }
                 }
-
                 contentService.search(req).then(function (res) {
+                    content.enableLoader(false);
                     console.log(res);
                     if (res.responseCode === "OK") {
-                        content.data = res.result;
-
+                        if ($event != undefined && content.keyword != '')
+                        {
+                            content.autosuggest_data = res.result;
+                        } else
+                        {
+                            content.data = res.result;
+                            content.autosuggest_data = [];
+                        }
                     }
                 }), function (errorMessage) {
                     $log.warn(errorMessage);
                 };
             };
-
+            content.enableLoader = function (isEnabled) {
+                if (isEnabled) {
+                    $('#search-input-container').addClass('loading');
+                } else
+                {
+                    $('#search-input-container').removeClass('loading');
+                }
+            }
             content.toggleView = function (isList)
             {
                 content.listView = isList;
@@ -41,14 +57,17 @@ angular.module('playerApp')
             content.loadRating = function () {
                 $('.ui.rating')
                         .rating({
-
                             maxRating: 5
-
                         }).rating("disable", true);
-
-                $('.popup-button').popup({inline: true});
+                $('.popup-button').popup();
             };
             content.searchContent();
+            content.setSearchText = function (text)
+            {
+                content.keyword = text;
+                content.searchContent();
+            }          
 
         });
 
+        
