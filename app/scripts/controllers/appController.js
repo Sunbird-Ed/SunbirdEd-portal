e9e3e46030546764870cb980032d986273b372bc
loganathan <loganathan@netops-Latitude-E6540>
2017-05-16 14:25:08 +0530

    Issue #1 fix: ui issues in content

---
+++ app/scripts/controllers/appController.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name playerApp.controller:AppCtrl
+ * @description
+ * # AppCtrl
+ * Controller of the playerApp
+ */
+angular.module('playerApp')
+    .controller('AppCtrl', function($rootScope, $state) {
+
+        $rootScope.initilizwDropDown = function() {
+            alert('init');
+            $('#userDropdown')
+                .dropdown({
+                    action: 'combo'
+                });
+        };
+       
+    });
