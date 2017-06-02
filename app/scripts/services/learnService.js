d594cf5deb36d59d4a74b1951f0695f6635b6884
Poonam <pnmsharma623.com>
2017-06-02 17:18:42 +0530

    Issue #109 feat: learn home page

--- app/scripts/services/learnService.js
+++ app/scripts/services/learnService.js
@@ -2,45 +2,91 @@
 angular.module('playerApp')
     .service('learnService', function(httpServiceJava, config, $sessionStorage, $q) {
         function enrolledCourses(uid) {
-            var url = config.URL.USER_BASE + config.URL.COURSE.ENROLLED_COURSES + '/' + uid;
-            // return httpServiceJava.get(url, req);
-            console.log('url', url);
-            var deferred = $q.defer();
-            deferred.resolve({
-                'id': '8e27cbf5-e299-43b0-bca7-8347f7e5abcf',
-                'ver': 'v1',
-                'ts': '2017-05-27 06:22:29:972+0530',
-                'params': {
-                    'resmsgid': null,
-                    'msgid': '8e27cbf5-e299-43b0-bca7-8347f7e5abcf',
-                    'err': null,
-                    'status': 'success',
-                    'errmsg': null
-                },
-                'responseCode': 'OK',
-                'result': {
-                    'courses': [{
-                        'dateTime': 1495886160605,
-                        'lastReadContentStatus': 1,
-                        'enrolledDate': '2017-05-27 05:24:29:079+0530',
-                        'addedby': 'e9280b815c0e41972bf754e9409b66d778b8e11bb91844892869a1e828d7d2f2',
-                        'delta': 'delta',
-                        'active': true,
-                        'description': 'course description',
-                        'userId': 'e9280b815c0e41972bf754e9409b66d778b8e11bb91844892869a1e828d7d2f2',
-                        'courseName': 'course name 2',
-                        'grade': null,
-                        'progress': 0,
-                        'id': '642f1dfa59249f23797ba070bdebbce1c27e0d756699dcd24dba669f86fbad88',
-                        'lastReadContentId': 'ek step cont-284',
-                        'tocUrl': null,
-                        'courseId': '0122542310741688321',
-                        'status': 0
-                    }]
-                }
-
-            });
-            return deferred.promise;
+            var url = config.URL.MOCK_API_BASE + config.URL.COURSE.GET_ENROLLED_COURSES + '/' + uid;
+            return httpServiceJava.get(url);
+            // var deferred = $q.defer();
+            // deferred.resolve({
+            //     'id': 'sunbird.user.courses',
+            //     'ver': '1.0',
+            //     'ts': '2017-05-13T10:49:58:600+0530',
+            //     'params': {
+            //         'resmsgid': '7c27cbf5-e299-43b0-bca7-8347f7e5abcf',
+            //         'msgid': null,
+            //         'err': null,
+            //         'status': 'success',
+            //         'errmsg': null
+            //     },
+            //     'responseCode': 'OK',
+            //     'result': {
+            //         'courses': [{
+            //                 'userId': ' user1',
+            //                 'courseId': ' course1',
+            //                 'name': ' course name 1',
+            //                 'description': ' course description 1',
+            //                 'enrolledDate': '2017-05-136 10:49:58:600+0530',
+            //                 'progress': 10,
+            //                 'grade': 'A',
+            //                 'active': ' true',
+            //                 'delta': {},
+            //                 'tocurl': 'CDN URL of the toc',
+            //                 'status': '1'
+            //             },
+            //             {
+            //                 'userId': ' user1',
+            //                 'courseId': ' course2',
+            //                 'name': ' course name 2',
+            //                 'description': ' course description 2',
+            //                 'enrolledDate': '2017-05-136 10:49:58:600+0530',
+            //                 'progress': 10,
+            //                 'grade': 'A',
+            //                 'active': ' true',
+            //                 'delta': {},
+            //                 'tocurl': 'CDN URL of the toc',
+            //                 'status': '1'
+            //             },
+            //             {
+            //                 'userId': ' user1',
+            //                 'courseId': ' course2',
+            //                 'name': ' course name 2',
+            //                 'description': ' course description 2',
+            //                 'enrolledDate': '2017-05-136 10:49:58:600+0530',
+            //                 'progress': 10,
+            //                 'grade': 'A',
+            //                 'active': ' true',
+            //                 'delta': {},
+            //                 'tocurl': 'CDN URL of the toc',
+            //                 'status': '1'
+            //             },
+            //             {
+            //                 'userId': ' user1',
+            //                 'courseId': ' course2',
+            //                 'name': ' course name 2',
+            //                 'description': ' course description 2',
+            //                 'enrolledDate': '2017-05-136 10:49:58:600+0530',
+            //                 'progress': 10,
+            //                 'grade': 'A',
+            //                 'active': ' true',
+            //                 'delta': {},
+            //                 'tocurl': 'CDN URL of the toc',
+            //                 'status': '1'
+            //             },
+            //             {
+            //                 'userId': ' user1',
+            //                 'courseId': ' course2',
+            //                 'name': ' course name 2',
+            //                 'description': ' course description 2',
+            //                 'enrolledDate': '2017-05-136 10:49:58:600+0530',
+            //                 'progress': 10,
+            //                 'grade': 'A',
+            //                 'active': ' true',
+            //                 'delta': {},
+            //                 'tocurl': 'CDN URL of the toc',
+            //                 'status': '1'
+            //             }
+            //         ]
+            //     }
+            // });
+            // return deferred.promise;
         }
         return {
             enrolledCourses: enrolledCourses
---
+++ test/spec/controllers/learnController.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('Controller:LearnCtrl', function() {
+    // load the controller's module
+    beforeEach(module('playerApp'));
+    var enrolledCoursesSuccessResponse = { 'id': 'sunbird.user.courses', 'ver': '1.0', 'ts': '2017-05-13T10:49:58:600+0530', 'params': { 'resmsgid': '7c27cbf5-e299-43b0-bca7-8347f7e5abcf', 'msgid': null, 'err': null, 'status': 'success', 'errmsg': null }, 'responseCode': 'OK', 'result': { 'courses': [{ 'userId': ' user1', 'courseId': ' course1', 'name': ' course name 1', 'description': ' course description 1', 'enrolledDate': '2017-05-136 10:49:58:600+0530', 'progress': 10, 'grade': 'A', 'active': ' true', 'delta': {}, 'tocurl': 'CDN URL of the toc', 'status': '1' }, { 'userId': ' user1', 'courseId': ' course2', 'name': ' course name 2', 'description': ' course description 2', 'enrolledDate': '2017-05-136 10:49:58:600+0530', 'progress': 10, 'grade': 'A', 'active': ' true', 'delta': {}, 'tocurl': 'CDN URL of the toc', 'status': '1' }, { 'userId': ' user1', 'courseId': ' course3', 'name': ' course name 3', 'description': ' course description 3', 'enrolledDate': '2017-05-136 10:49:58:600+0530', 'progress': 10, 'grade': 'A', 'active': ' true', 'delta': {}, 'tocurl': 'CDN URL of the toc', 'status': '1' }] } };
+    var LearnCtrl,
+        scope,
+        learnService,
+        $q,
+        deferred;
+
+    beforeEach(inject(function($controller, $rootScope, _$q_, _learnService_) {
+        scope = $rootScope.$new();
+        $q = _$q_;
+        deferred = $q.defer();
+        learnService = _learnService_;
+        spyOn(learnService, 'enrolledCourses').and.returnValue(deferred.promise);
+        AuthCtrl = $controller('LearnCtrl', {
+            $scope: scope
+        });
+        spyOn(LearnCtrl, 'enrolledCourses').and.callThrough();
+    }));
+    it('should return enrolled courses', (function(done) {
+        deferred.resolve(enrolledCoursesSuccessResponse);
+
+        LearnCtrl.enrolledCourses();
+        learnService.enrolledCourses();
+        scope.$apply();
+        expect(LearnCtrl.enrolledCourses).toHaveBeenCalled();
+        expect(learnService.enrolledCourses).toHaveBeenCalled();
+        done();
+    }));
+});
