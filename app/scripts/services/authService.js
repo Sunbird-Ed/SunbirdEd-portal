6743c29ca5ae6e671e79951fa2fd2ee131373287
Poonam <pnmsharma623.com>
2017-06-02 17:18:42 +0530

    Issue #109 feat: learn home page

--- app/scripts/services/authService.js
+++ app/scripts/services/authService.js
@@ -2,51 +2,54 @@
 angular.module('playerApp')
     .service('authService', function(httpServiceJava, config, $q, $sessionStorage, $rootScope, $state) {
         function login(req) {
-
             var url = config.URL.USER_BASE + config.URL.AUTH.LOGIN;
-            return httpServiceJava.post(url, req);
-            // var deferred = $q.defer();
-            // deferred.resolve({
-            //     id: 'sunbird.login',
-            //     ver: '1.0',
-            //     ts: '2017-05-136 10:49:58:600+0530',
-            //     params: {
-            //         resmsgid: '7 c27cbf5 - e299 - 43 b0 - bca7 - 8347 f7e5abcf',
-            //         msgid: '8e27 cbf5 - e299 - 43 b0 - bca7 - 8347 f7e5abcf',
-            //         err: null,
-            //         status: 'success',
-            //         errmsg: null
-            //     },
-            //     responseCode: 'OK',
-            //     result: {
-            //         'authToken': 'token123',
-            //         'firstName': 'firstname'
-            //     }
-            // });
-            // return deferred.promise;
+            // return httpServiceJava.post(url, req);
+            var deferred = $q.defer();
+            deferred.resolve({
+                'id': null,
+                'ver': 'v1',
+                'ts': '2017-06-02 04:24:38:939+0530',
+                'params': {
+                    'resmsgid': null,
+                    'msgid': null,
+                    'err': null,
+                    'status': 'success',
+                    'errmsg': null
+                },
+                'responseCode': 'OK',
+                'result': {
+                    'response': {
+                        'source': 'android',
+                        'firstName': 'user1',
+                        'token': '4eb30018-d781-3b82-b87a-7fc09cb86b1f',
+                        'userId': 'user1'
+                    }
+                }
+            });
+            return deferred.promise;
         }
 
         function logout(req) {
-            var url = config.URL.BASE + config.URL.AUTH.LOGOUT;
-            return httpServiceJava.post(url, req);
-            // var deferred = $q.defer();
-            // deferred.resolve({
-            //     'id': '7c27cbf5-e299-43b0-bca7-8347f7e5abcf',
-            //     'ver': 'v1',
-            //     'ts': '2017-05-136 10:49:58:600+0530',
-            //     'params': {
-            //         'resmsgid': null,
-            //         'msgid': null,
-            //         'err': 'SUCCESS',
-            //         'status': 'success',
-            //         'errmsg': 'Success'
-            //     },
-            //     'responseCode': 'OK',
-            //     'result': {
-
-            //     }
-            // });
-            // return deferred.promise;
+            var url = config.URL.USER_BASE + config.URL.AUTH.LOGOUT;
+            // return httpServiceJava.post(url, req);
+            var deferred = $q.defer();
+            deferred.resolve({
+                'id': '8e27cbf5-e299-43b0-bca7-8347f7e5abcf',
+                'ver': 'v1',
+                'ts': '2017-06-02 04:24:57:956+0530',
+                'params': {
+                    'resmsgid': null,
+                    'msgid': '8e27cbf5-e299-43b0-bca7-8347f7e5abcf',
+                    'err': null,
+                    'status': 'success',
+                    'errmsg': null
+                },
+                'responseCode': 'OK',
+                'result': {
+                    'response': 'SUCCESS'
+                }
+            });
+            return deferred.promise;
         }
 
         function validUser() {
