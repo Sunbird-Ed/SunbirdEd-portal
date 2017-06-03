cc308d3f995115509edfda4b9e9ed500e9d8ef0b
loganathan <loganathan@netops-Latitude-E6540>
2017-06-03 09:48:08 +0530

    feat: lecture view

--- app/scripts/controllers/courseScheduleController.js
+++ app/scripts/controllers/courseScheduleController.js
@@ -1,9 +1,12 @@
 angular.module('playerApp')
         .controller('courseScheduleCtrl', function (courseService, $timeout, $scope, $sce, $sessionStorage, $stateParams) {
             var toc = this;
+            toc.playList = {};
             $scope.contentPlayer = {
-                isContentPlayerEnabled: false
+                isContentPlayerEnabled: false,
+
             };
+            //$scope.contentPlayer.contentData=};
             toc.getCourseToc = function () {
                 toc.courseId = $stateParams.courseId;
                 courseService.courseHierarchy(toc.courseId).then(function (res) {
@@ -12,7 +15,7 @@ angular.module('playerApp')
             }
             toc.getCourseToc();
             toc.expandMe = function ($event, item) {
-               
+
                 if (item.mimeType == "application/vnd.ekstep.content-collection")
                 {
                     if (!$($event.target).closest("li").find('.toc-list-sub-menu').first().hasClass('active'))
@@ -24,11 +27,33 @@ angular.module('playerApp')
                     }
                 } else
                 {
-                    debugger;
-                    $scope.contentPlayer.isContentPlayerEnabled=true;
-                    $scope.contentPlayer.contentData = item;
+
+                    toc.playPlaylistContent(item.identifier);
+
                 }
             };
+
+            toc.checkAndAddToPlaylist = function (item) {
+                if (item.mimeType != "application/vnd.ekstep.content-collection" && toc.playList[item.identifier] == undefined)
+                {
+                    toc.playList[item.identifier] = item;
+                }
+            }
+
+            toc.playPlaylistContent = function (itemId) {
+                
+                    $scope.contentPlayer.isContentPlayerEnabled =false;
+                    $scope.contentPlayer.contentData =undefined;
+                    $timeout(function () {
+                    var playlistKeysArr = Object.keys(toc.playList);
+                    var curItemIndex = playlistKeysArr.indexOf(itemId);
+                    toc.nextPlaylistItem = ((curItemIndex + 1) < playlistKeysArr.length ? playlistKeysArr[curItemIndex + 1] : '');
+                    toc.prevPlaylistItem = ((curItemIndex - 1) >= 0 ? playlistKeysArr[curItemIndex - 1] : '');
+                    $scope.contentPlayer.isContentPlayerEnabled = true;
+                    $scope.contentPlayer.contentData = toc.playList[itemId];                   
+                }, 0);
+            }
+
             toc.getAllChildrenCount = function (index) {
                 var childCount = toc.getChildNodeCount(toc.courseHierachy.children[index], 0);
                 return childCount;
@@ -37,7 +62,7 @@ angular.module('playerApp')
 
 
                 if (obj.children == undefined || obj.children.length == 0)
-                {                    
+                {
                     return cnt;
                 } else {
                     cnt += obj.children.length;
