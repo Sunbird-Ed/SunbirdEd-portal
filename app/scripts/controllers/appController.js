1d156f28b2c092b497197427277e1e61f09f08bf
loganathan <loganathan@netops-Latitude-E6540>
2017-06-08 11:26:30 +0530

    Issue #1 fix: sidebar menu items

--- app/scripts/controllers/appController.js
+++ app/scripts/controllers/appController.js
@@ -8,22 +8,72 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-    .controller('AppCtrl', function() {
-        $('body').click(function(e) {
-            if ($(e.target).closest('div.dropdown-menu-list').prop('id') == 'search-suggestions') {
-                return false;
-            } else {
-                $('body').find('.dropdown-menu-list').removeClass('visible').addClass('hidden');
-            }
-        });
+        .controller('AppCtrl', function ($scope, $state, $stateParams, $rootScope) {
+            $('body').click(function (e) {
+                if ($(e.target).closest('div.dropdown-menu-list').prop('id') == 'search-suggestions') {
+                    return false;
+                } else {
+                    $('body').find('.dropdown-menu-list').removeClass('visible').addClass('hidden');
+                }
+            });
 
-        // $scope.initilizwDropDown = function() {
-        //     $('#dropdown-menu-list')
-        //         .dropdown({
-        //             action: 'combo'
-        //         });
-        // };
-        // $('.small.modal')
-        //     .modal('show')
-        // ;
-    });
+            // $scope.initilizwDropDown = function() {
+            //     $('#dropdown-menu-list')
+            //         .dropdown({
+            //             action: 'combo'
+            //         });
+            // };
+            // $('.small.modal')
+            //     .modal('show')
+            // ;
+            $scope.$watch(function () {
+                return $state.$current.name
+            }, function (newState, oldState) {
+                console.log(newState);
+                if (newState.toLowerCase() == "course" || newState.toLowerCase() == "toc") {
+                    $scope.courseId = $stateParams.courseId;
+                    $rootScope.sideMenuData = [{
+                            "icon": "",
+                            "name": "COURSE SCHEDULE",
+                            "children": [],
+                            "link": "/toc/" + $scope.courseId + "/no"
+                        },
+                        {
+                            "icon": "",
+                            "name": "LECTURE VIEW",
+                            "children": [],
+                            "link": "/toc/" + $scope.courseId + "/yes"
+                        },
+                        {
+                            "icon": "",
+                            "name": "NOTES",
+                            "children": [],
+                            "link": "/note"
+                        }
+                    ];
+                } else
+                {
+                    $rootScope.sideMenuData = [{
+                            "icon": "large add circle icon",
+                            "name": "Add Course",
+                            "children": [],
+                            "link": "#"
+                        },
+                        {
+                            "icon": "large bookmark icon",
+                            "name": "My Bookmarks",
+                            "children": [],
+                            "link": "#"
+                        },
+                        {
+                            "icon": "large search icon",
+                            "name": "Explore",
+                            "children": [],
+                            "link": "#"
+                        }
+                    ];
+
+                }
+            })
+
+        });
