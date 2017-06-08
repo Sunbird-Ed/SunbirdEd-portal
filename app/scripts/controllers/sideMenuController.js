2a0702f8f6bd849b772c25cdd39bbc9840ee997c
Poonam <pnmsharma623.com>
2017-06-08 12:18:54 +0530

    Issue #184 fix:side menu fix

--- app/scripts/controllers/sideMenuController.js
+++ app/scripts/controllers/sideMenuController.js
@@ -1,29 +1,28 @@
 'use strict';
 
 angular.module('playerApp')
-        .controller('sideMenuCtrl', function ($scope, $rootScope) {
-            var sideMenu = this;
-
-            $rootScope.sideMenuData = [{
-                    "icon": "large add circle icon",
-                    "name": "Add Course",
-                    "children": [],
-                    "link": "#"
-                },
-                {
-                    "icon": "large bookmark icon",
-                    "name": "My Bookmarks",
-                    "children": [],
-                    "link": "#"
-                },
-                {
-                    "icon": "large search icon",
-                    "name": "Explore",
-                    "children": [],
-                    "link": "#"
-                }
-            ];
-           
-
-
+    .controller('sideMenuCtrl', function($scope, $rootScope, $location) {
+        $scope.$on('$locationChangeSuccess', function() {
+            $scope.currentPath = $location.path();
         });
+
+        $rootScope.sideMenuData = [{
+                'icon': 'large add circle icon',
+                'name': 'Add Course',
+                'children': [],
+                'link': '#'
+            },
+            {
+                'icon': 'large bookmark icon',
+                'name': 'My Bookmarks',
+                'children': [],
+                'link': '#'
+            },
+            {
+                'icon': 'large search icon',
+                'name': 'Explore',
+                'children': [],
+                'link': '#'
+            }
+        ];
+    });
