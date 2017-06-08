e1fa9a05dbc663a6e8e9b0e92a618c287b53e536
Poonam <pnmsharma623.com>
2017-06-08 14:26:59 +0530

    Issue #185 fix:learn to course view page reload fix

--- app/scripts/controllers/learnController.js
+++ app/scripts/controllers/learnController.js
@@ -1,35 +1,38 @@
 'use strict';
 
 angular.module('playerApp')
-        .controller('LearnCtrl', function (learnService, $log, $scope, $sessionStorage, $timeout,$location) {
-            var learn = this;
-            var uid = $sessionStorage.userId;
+    .controller('LearnCtrl', function(learnService, $log, $scope, $sessionStorage, $timeout, $location) {
+        var learn = this;
+        var uid = $sessionStorage.userId;
 
-            $scope.$on('ngRepeatFinished', function () {
-                $('.regular').slick({
-                    infinite: true,
-                    slidesToShow: 4,
-                    slidesToScroll: 4
-                });
+        $scope.$on('ngRepeatFinished', function() {
+            $('.regular').not('.slick-initialized').slick({
+                infinite: true,
+                refresh: true,
+                slidesToShow: 4,
+                slidesToScroll: 4
             });
+        });
 
-            function handleFailedResponse(errorResponse) {
-                var error = {};
-                error.isError = true;
-                error.message = '';
-                error.responseCode = errorResponse.responseCode;
-                learn.error = error;
 
-                $timeout(function () {
-                    $scope.error = {};
-                }, 2000);
-            }
-            learn.openCourseView = function (courseId) {
-                courseId='do_11225144311893196816';
-                $location.path('/course/' + courseId);
-            }
-            learn.courses = function () {
-                learnService.enrolledCourses(uid).then(function (successResponse) {
+
+        function handleFailedResponse(errorResponse) {
+            var error = {};
+            error.isError = true;
+            error.message = '';
+            error.responseCode = errorResponse.responseCode;
+            learn.error = error;
+
+            $timeout(function() {
+                $scope.error = {};
+            }, 2000);
+        }
+        learn.openCourseView = function(courseId) {
+            courseId = 'do_11225144311893196816';
+            $location.path('/course/' + courseId);
+        };
+        learn.courses = function() {
+            learnService.enrolledCourses(uid).then(function(successResponse) {
                     if (successResponse && successResponse.responseCode === 'OK') {
                         learn.enrolledCourses = successResponse.result.courses;
                     } else {
@@ -37,10 +40,10 @@ angular.module('playerApp')
                         handleFailedResponse(successResponse);
                     }
                 })
-                        .catch(function (error) {
-                            $log.warn(error);
-                            handleFailedResponse('enrolledCourses', error);
-                        });
-            };
-            learn.courses();
-        });
+                .catch(function(error) {
+                    $log.warn(error);
+                    handleFailedResponse('enrolledCourses', error);
+                });
+        };
+        learn.courses();
+    });
