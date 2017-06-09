c371d77c73a765feb22430452446cd78093abb6f
loganathan <loganathan@netops-Latitude-E6540>
2017-06-09 13:17:08 +0530

    Issue #1 fix: gulp dependency update

--- gulpfile.js
+++ gulpfile.js
@@ -54,6 +54,8 @@ var paths = {
         player.app + '/thirdparty/bower_components/slick-carousel/slick/slick.js',
         player.app + '/thirdparty/bower_components/pdfjs-dist/build/pdf.js',
         player.app + '/thirdparty/bower_components/angular-pdf-viewer/dist/angular-pdf-viewer.min.js',
+        player.app + '/thirdparty/bower_components/angular-translate/angular-translate.js',
+        player.app + '/thirdparty/bower_components/angular-sanitize/angular-sanitize.js',
 
     ],
     karma: 'karma.conf.js',
