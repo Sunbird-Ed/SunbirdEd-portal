1ac3a056b1b9b7df2cc45bd6bcf104c981af605b
Harishkumar Gangula <harishg@ilimi.in>
2017-06-12 15:06:18 +0530

    Issue #1 style: UI Fixes for header menu and reload issue

--- app/server.js
+++ app/server.js
@@ -7,11 +7,11 @@ var request = require('request');
 
 app.use(express.static('./'));
 app.use('/ekContentEditor', express.static('./thirdparty/content-editor'))
-app.get('/', function(req, res) {
-    res.sendFile(__dirname+'/index.html');
-});
 app.get('/ekContentEditor', function (req, res) {
     res.sendFile(__dirname+"/thirdparty/content-editor/index.html");
 })
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname+'/index.html');
+});
 app.listen(7000);
 console.log('app running on port 7000');
