69f343226e8f6d87b7f1f4d1269bb7227d1cdc0c
anuj <anujtech46>
2017-06-13 13:39:06 +0530

    Issue #1 fix: Merge

--- app/scripts/services/courseService.js
+++ app/scripts/services/courseService.js
@@ -8,26 +8,32 @@
  * Service in the playerApp.
  */
 angular.module('playerApp')
-        .service('courseService', function (httpServiceJava, config, $sessionStorage,httpService) {
+    .service('courseService', function(httpServiceJava, config, $sessionStorage, httpService) {
+        function search(req) {
+            var url = config.URL.BASE + config.URL.COURSE.SEARCH;
+            return httpService.post(url, req);
+        }
 
-            function getUserCourseSchedule(req) {
-                var url = config.URL.USER_BASE + config.URL.COURSE.USER_COURSE_SCHEDULE + "/" + $sessionStorage.token;
-                return httpServiceJava.get(url, req);
-            }
-            function courseContentState(req)
-            {
-                var url = config.URL.USER_BASE + config.URL.COURSE.USER_CONTENT_STATE;
-                return httpServiceJava.post(url, req);
-            }
-            function getCourseHierarchy(courseId){
-                var url = config.URL.BASE + config.URL.COURSE.HIERARCHY+"/"+courseId;
-                return httpService.get(url);
-            }
+        function getUserCourseSchedule(req) {
+            var url = config.URL.USER_BASE + config.URL.COURSE.USER_COURSE_SCHEDULE + "/" + $sessionStorage.token;
+            return httpServiceJava.get(url, req);
+        }
 
-            return {
-                courseSchedule: getUserCourseSchedule,
-                courseContentState: courseContentState,
-                courseHierarchy:getCourseHierarchy
-            };
+        function courseContentState(req) {
+            var url = config.URL.USER_BASE + config.URL.COURSE.USER_CONTENT_STATE;
+            return httpServiceJava.post(url, req);
+        }
 
-        });
+        function getCourseHierarchy(courseId) {
+            var url = config.URL.BASE + config.URL.COURSE.HIERARCHY + "/" + courseId;
+            return httpService.get(url);
+        }
+
+        return {
+            courseSchedule: getUserCourseSchedule,
+            courseContentState: courseContentState,
+            courseHierarchy: getCourseHierarchy,
+            search: search
+        };
+
+    });
