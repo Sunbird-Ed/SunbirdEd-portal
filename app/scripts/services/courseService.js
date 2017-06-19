c0843a65c6b8e585c32f84d531fbb11b10809b18
loganathan <loganathan.shanmugam@tarento.com>
2017-06-19 16:57:50 +0530

    Issue #1 fix: enroll course functinality

--- app/scripts/services/courseService.js
+++ app/scripts/services/courseService.js
@@ -8,35 +8,37 @@
  * Service in the playerApp.
  */
 angular.module('playerApp')
-    .service('courseService', function(httpServiceJava, config, $sessionStorage, httpService) {
-        function search(req) {
-            var url = config.URL.BASE + config.URL.COURSE.SEARCH;
-            return httpService.post(url, req);
-        }
+        .service('courseService', function (httpServiceJava, config, $sessionStorage, httpService) {
+            function search(req) {
+                var url = config.URL.BASE + config.URL.COURSE.SEARCH;
+                return httpService.post(url, req);
+            }
 
-        function getUserCourseSchedule(req) {
-            var url = config.URL.USER_BASE + config.URL.COURSE.USER_COURSE_SCHEDULE + "/" + $sessionStorage.token;
-            return httpServiceJava.get(url, req);
-        }
+            function getUserCourseSchedule(req) {
+                var url = config.URL.USER_BASE + config.URL.COURSE.USER_COURSE_SCHEDULE + "/" + $sessionStorage.token;
+                return httpServiceJava.get(url, req);
+            }
 
-        function courseContentState(req) {
-            var url = config.URL.USER_BASE + config.URL.COURSE.USER_CONTENT_STATE;
-            return httpServiceJava.post(url, req);
-        }
+            function courseContentState(req) {
+                var url = config.URL.USER_BASE + config.URL.COURSE.USER_CONTENT_STATE;
+                return httpServiceJava.post(url, req);
+            }
 
-        function getCourseHierarchy(courseId) {
-            var url = config.URL.BASE + config.URL.COURSE.HIERARCHY + "/" + courseId;
-            return httpService.get(url);
-        }
-        function enrollUserToCourse(req){
-            
-        }
+            function getCourseHierarchy(courseId) {
+                var url = config.URL.BASE + config.URL.COURSE.HIERARCHY + "/" + courseId;
+                return httpService.get(url);
+            }
+            function enrollUserToCourse(req) {
+                var url = config.URL.MOCK_API_BASE + config.URL.COURSE.ENROLL_USER_COURSE;
+                return httpServiceJava.post(url, req);
+            }
 
-        return {
-            courseSchedule: getUserCourseSchedule,
-            courseContentState: courseContentState,
-            courseHierarchy: getCourseHierarchy,
-            search: search
-        };
+            return {
+                courseSchedule: getUserCourseSchedule,
+                courseContentState: courseContentState,
+                courseHierarchy: getCourseHierarchy,
+                search: search,
+                enrollUserToCourse:enrollUserToCourse
+            };
 
-    });
+        });
