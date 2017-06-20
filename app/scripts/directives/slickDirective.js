9f09e9783d0eba3ee7a46e24bf55cd08d45536fd
Harishkumar Gangula <harishg@ilimi.in>
2017-06-20 11:22:31 +0530

    Issue #1 refactor: added slick directive

---
+++ app/scripts/directives/slickDirective.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name playerApp.directive:contentPlayer
+ * @description
+ * # contentPlayer
+ */
+angular.module('playerApp').directive('addSlick', function($timeout) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            $timeout(function() {
+                $(element).not('.slick-initialized').slick({
+                    infinite: false,
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                });
+            })
+        }
+    };
+});
