c28027b92d1136b3ddcf4a0d5aaf4d9d55dc556b
Harishkumar Gangula <harishg@ilimi.in>
2017-06-20 11:22:31 +0530

    Issue #1 refactor: added slick directive

--- app/scripts/controllers/userContentController.js
+++ app/scripts/controllers/userContentController.js
@@ -8,78 +8,73 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-        .controller('userContentCtrl', ['$scope', 'contentService', '$rootScope', '$timeout', function ($scope, contentService, $rootScope, $timeout) {
-                $scope.contentItem = undefined;
-                $scope.hideUserContent = false;
+    .controller('userContentCtrl', ['$scope', 'contentService', '$rootScope', '$timeout', function($scope, contentService, $rootScope, $timeout) {
+        $scope.contentItem = undefined;
+        $scope.hideUserContent = false;
 
-                $scope.getPublishedContent = function () {
-                    var req = {
-                        'filters': {
-                            'createdBy': '12345678',
-                            'status': ['live'],
-                            'mimeType': ['application/pdf', 'video/youtube', 'video/mp4', 'application/vnd.ekstep.ecml-archive', 'application/vnd.ekstep.html-archive']
-                        },
-                        'params': {
-                            'cid': '12'
-                        },
-                        'sort_by': {
-                            "lastUpdatedOn": "desc"
-                        }
-                    };
-                    $scope.publishedContent = [];
-                    contentService.search(req).then(function (res) {
-                        if (res.responseCode === 'OK' && res.result.count > 0) {
-                            $scope.publishedContent = res.result.content;
-                        }
-                    },
-                            function (errorMessage) {
+        $scope.getPublishedContent = function() {
+            var req = {
+                'filters': {
+                    'createdBy': '12345678',
+                    'status': ['live'],
+                    'mimeType': ['application/pdf', 'video/youtube', 'video/mp4', 'application/vnd.ekstep.ecml-archive', 'application/vnd.ekstep.html-archive']
+                },
+                'params': {
+                    'cid': '12'
+                },
+                'sort_by': {
+                    "lastUpdatedOn": "desc"
+                }
+            };
+            $scope.publishedContent = [];
+            contentService.search(req).then(function(res) {
+                    if (res.responseCode === 'OK' && res.result.count > 0) {
+                        $scope.publishedContent = res.result.content;
+                    }
+                },
+                function(errorMessage) {
 
-                            });
-                };
-                $scope.getPublishedContent();
-                $scope.getDraftContent = function () {
-                    var req = {
-                        'filters': {
-                            'createdBy': '12345678',
-                            'status': ['draft']
-                        },
-                        'params': {
-                            'cid': '12'
-                        },
-                        'sort_by': {
-                            "lastUpdatedOn": "desc"
-                        }
-                    };
-                    $scope.draftContent = [];
-                    contentService.search(req).then(function (res) {
-                        $scope.content = undefined;
-                        if (res.responseCode === 'OK' && res.result.count > 0) {
-                            $scope.draftContent = res;
-                        }
-                    },
-                            function (errorMessage) {});
-                };
-                $scope.getDraftContent();
-                $scope.loadRating = function () {
-                    $('.ui.rating')
-                            .rating({
-                                maxRating: 5
-                            }).rating('disable', true);
-                    $('.regular').not('.slick-initialized').slick({
-                        infinite: true,
-                        slidesToShow: 4,
-                        slidesToScroll: 4
-                    });
-                };
-
-                $scope.editContent = function (content) {
-                    $scope.hideUserContent = true;
-                    $rootScope.$emit('editContentEnable', content);
-                };
-
-                $rootScope.$on('editContentDisable', function (e, c) {
-                    $scope.hideUserContent = false;
-                    $scope.getDraftContent();
-                    $scope.getPublishedContent();
                 });
-            }]);
+        };
+        $scope.getPublishedContent();
+        $scope.getDraftContent = function() {
+            var req = {
+                'filters': {
+                    'createdBy': '12345678',
+                    'status': ['draft']
+                },
+                'params': {
+                    'cid': '12'
+                },
+                'sort_by': {
+                    "lastUpdatedOn": "desc"
+                }
+            };
+            $scope.draftContent = [];
+            contentService.search(req).then(function(res) {
+                    $scope.content = undefined;
+                    if (res.responseCode === 'OK' && res.result.count > 0) {
+                        $scope.draftContent = res;
+                    }
+                },
+                function(errorMessage) {});
+        };
+        $scope.getDraftContent();
+        $scope.loadRating = function() {
+            $('.ui.rating')
+                .rating({
+                    maxRating: 5
+                }).rating('disable', true);
+        };
+
+        $scope.editContent = function(content) {
+            $scope.hideUserContent = true;
+            $rootScope.$emit('editContentEnable', content);
+        };
+
+        $rootScope.$on('editContentDisable', function(e, c) {
+            $scope.hideUserContent = false;
+            $scope.getDraftContent();
+            $scope.getPublishedContent();
+        });
+    }]);
