7637ef11810461497fdc1fcad67f3497d55df4a0
Poonam <pnmsharma623.com>
2017-06-21 14:53:44 +0530

    Issue #240 fix:controller renaming

---
+++ app/scripts/controllers/searchController.js
@@ -0,0 +1,200 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name playerApp.controller:SearchCtrl
+ * @description
+ * # SearchCtrl
+ * Controller of the playerApp
+ */
+angular.module('playerApp')
+    .controller('SearchCtrl', function(contentService, courseService, $scope, $timeout, $rootScope, $stateParams, $state) {
+        var search = this;
+        search.keyword = '';
+        search.filters = {};
+        $scope.selectedSearchKey = $stateParams.searchKey;
+        $timeout(function() {
+            $('#headerSearchdd').dropdown('set selected', $scope.selectedSearchKey);
+        });
+        $scope.$watch('searchKey', function() {
+            $scope.selectedSearchKey = $rootScope.searchKey;
+            search.keyword = '';
+        });
+        $scope.contentPlayer = {
+            isContentPlayerEnabled: false
+        };
+        $scope.close = function() {
+            $rootScope.searchResult = [];
+        };
+
+        search.languages = [
+            'Bengali', 'English', 'Gujarati', 'Hindi', 'Kannada', 'Marathi', 'Punjabi', 'Tamil', 'Telugu'
+        ];
+        search.contentTypes = [
+            'Story', 'Worksheet', 'Collections', 'Game', 'Plugin', 'Template'
+        ];
+        search.subjects = [
+            'Maths', 'English', 'Hindi', 'Assamese', 'Bengali', 'Gujarati', 'Kannada', 'Malayalam', 'Marathi', 'Nepali', 'Oriya', 'Punjabi', 'Tamil', 'Telugu', 'Urdu'
+        ];
+        search.boards = [
+            'NCERT', 'CBSE', 'ICSE', 'MSCERT'
+        ];
+        search.sortBy = [
+            { field: 'lastUpdatedOn', name: 'Updated On' },
+            { field: 'createdOn', name: 'Created On' }
+        ];
+
+        search.searchSelectionKeys = [{ id: 'Courses', name: 'Courses' }, { id: 'Resources', name: 'Resources' }];
+        search.selectedLanguage = '';
+        search.selectedContentType = '';
+        search.selectedSubject = '';
+        search.selectedBoard = '';
+        search.selectedOrder = '';
+        search.orderBy = {};
+        search.autosuggest_data = { content: [] };
+        search.listView = false;
+        search.sortIcon = true;
+        $scope.contentPlayer = {
+            isContentPlayerEnabled: false
+        };
+        $rootScope.showIFrameContent = false;
+        $rootScope.search = search;
+        search.openCourseView = function(courseId) {
+            var isEnrolledCourse = $rootScope.enrolledCourseIds.some(function(id) {
+                return id === courseId;
+            });
+            var courseType = isEnrolledCourse === true ? 'ENROLLED_COURSE' : 'OTHER_COURSE';
+            var showLectureView = 'no';
+            var params = {
+                courseType: courseType,
+                courseId: courseId,
+                lectureView: showLectureView
+            };
+            $state.go('Toc', params);
+        };
+
+        function handleFailedResponse(errorResponse) {
+            var isSearchError = {};
+            isSearchError.isError = true;
+            isSearchError.message = '';
+            isSearchError.responseCode = errorResponse.responseCode;
+            $rootScope.isSearchError = isSearchError;
+            // $scope.$apply();
+            $timeout(function() {
+                $rootScope.isSearchError = {};
+                isSearchError.isError = false;
+            }, 2000);
+        }
+        search.handleSuccessResponse = function(successResponse, $event) {
+            if (successResponse.result.count > 0) {
+                //if $event is passed then search is to get only autosuggest else to get the content
+                if ($event !== undefined && search.keyword !== '') {
+                    search.autosuggest_data = $scope.selectedSearchKey === 'Courses' ?
+                        successResponse.result.course :
+                        successResponse.result.content;
+                } else {
+                    $rootScope.searchKey = $scope.selectedSearchKey;
+                    search.autosuggest_data = [];
+                    $rootScope.searchResult = $scope.selectedSearchKey === 'Courses' ?
+                        successResponse.result.course :
+                        successResponse.result.content;
+                }
+            } else {
+                $rootScope.searchResult = [];
+
+                successResponse.responseCode = 'RESOURCE_NOT_FOUND';
+                handleFailedResponse(successResponse);
+            }
+        };
+
+        search.searchContent = function($event) {
+            search.enableLoader(true);
+            if (search.selectedOrder != '') {
+                search.orderBy[search.selectedOrder] = search.sortIcon ? 'asc' : 'desc';
+            }
+            var req = {
+                'query': search.keyword,
+                'filters': search.filters,
+                'params': {
+                    'cid': '12'
+                },
+                'limit': 20,
+                sort_by: search.orderBy
+            };
+            // req.limit = 20;
+            $rootScope.searchKeyword = search.keyword;
+            if ($scope.selectedSearchKey === 'Resources') {
+                contentService.search(req).then(function(res) {
+                    search.enableLoader(false);
+                    if (res != null && res.responseCode === 'OK') {
+                        search.handleSuccessResponse(res, $event);
+                    } else {
+                        handleFailedResponse(res);
+                    }
+                }).catch(function(error) {
+                    handleFailedResponse(error);
+                });
+            } else if ($scope.selectedSearchKey === 'Courses') {
+                courseService.search(req).then(function(res) {
+                    search.enableLoader(false);
+                    if (res != null && res.responseCode === 'OK') {
+                        search.handleSuccessResponse(res, $event);
+                    } else {
+                        handleFailedResponse(res);
+                    }
+                }).catch(function(error) {
+                    handleFailedResponse(error);
+                });
+            }
+        };
+
+        search.applyFilter = function() {
+            if (search.selectedLanguage) {
+                search.filters['language'] = search.selectedLanguage;
+            }
+            if (search.selectedContentType) {
+                search.filters['contentType'] = search.selectedContentType;
+            }
+            if (search.selectedSubject) {
+                search.filters['subject'] = search.selectedSubject;
+            }
+            if (search.selectedBoard) {
+                search.filters['board'] = search.selectedBoard;
+            }
+            search.keyword = $rootScope.searchKeyword;
+            search.searchContent();
+        };
+        search.resetFilter = function() {
+            $('.dropdown').dropdown('clear');
+            search.filters = {};
+            search.selectedLanguage = '';
+            search.selectedContentType = '';
+            search.selectedSubject = '';
+            search.selectedBoard = '';
+            search.keyword = $rootScope.searchKeyword;
+            search.searchContent();
+        };
+        //to show/hide in-search loading bar
+        search.enableLoader = function(isEnabled) {
+            if (isEnabled) {
+                $('#search-input-container').addClass('loading');
+                search.autosuggest_data = [];
+            } else {
+                $('#search-input-container').removeClass('loading');
+            }
+        };
+        // to apply star rating and more.. popup once content is ready
+        search.loadRating = function() {
+            $('.ui.rating')
+                .rating({
+                    maxRating: 5
+                }).rating('disable', true);
+            $('.popup-button').popup();
+        };
+        //if any item is selected from autosuggest search then set that as keyword
+        search.setSearchText = function(text) {
+            search.keyword = text;
+            search.searchContent();
+        };
+        $('.search-dropdown').dropdown();
+    });
