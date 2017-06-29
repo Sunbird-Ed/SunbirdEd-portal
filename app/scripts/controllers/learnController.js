52bf71993a808e431373bcd345eecca11282a87c
ARUSHI-PC\Prajwal <prajwal.hj@compassitesinc.com>
2017-06-29 15:12:59 +0530

    Issue #1 fix: reset branch

--- app/scripts/controllers/learnController.js
+++ app/scripts/controllers/learnController.js
@@ -2,97 +2,101 @@
 
 angular.module('playerApp')
 
-.controller('LearnCtrl', function(learnService, $log, $scope, $sessionStorage, $timeout, $state, $rootScope, sessionService, $location, $window, config) {
-    var learn = this;
-    var uid = $rootScope.userId ? $rootScope.userId : $window.localStorage.getItem('userId');
-    $rootScope.searchResult = [];
-    $scope.contentPlayer = {
-        isContentPlayerEnabled: false
-    };
+        .controller('LearnCtrl', function (learnService, $log, $scope, $sessionStorage, $timeout, $state, $rootScope, sessionService, $location, $window, config) {
+            var learn = this;
+            var uid = $rootScope.userId ? $rootScope.userId : $window.localStorage.getItem('userId');
+            $rootScope.searchResult = [];
+            $scope.contentPlayer = {
+                isContentPlayerEnabled: false
+            };
 
-        /**
-     * This function helps to show loader with message.
-     * @param {String} headerMessage
-     * @param {String} loaderMessage
-     */
-    function showLoaderWithMessage(headerMessage, loaderMessage) {
-        var loader = {};
-        loader.showLoader = true;
-        loader.headerMessage = headerMessage;
-        loader.loaderMessage = loaderMessage;
-        return loader;
-    }
+            /**
+             * This function helps to show loader with message.
+             * @param {String} headerMessage
+             * @param {String} loaderMessage
+             */
+            function showLoaderWithMessage(headerMessage, loaderMessage) {
+                var loader = {};
+                loader.showLoader = true;
+                loader.headerMessage = headerMessage;
+                loader.loaderMessage = loaderMessage;
+                return loader;
+            }
 
-    /**
-     * This function called when api failed, and its show failed response for 2 sec.
-     * @param {String} message
-     */
-    function showErrorMessage(isClose, message, messageType) {
-        var error = {};
-        error.showError = true;
-        error.isClose = isClose;
-        error.message = message;
-        error.messageType = messageType;
-        return error;
-    }
+            /**
+             * This function called when api failed, and its show failed response for 2 sec.
+             * @param {String} message
+             */
+            function showErrorMessage(isClose, message, messageType) {
+                var error = {};
+                error.showError = true;
+                error.isClose = isClose;
+                error.message = message;
+                error.messageType = messageType;
+                return error;
+            }
 
-    learn.openCourseView = function(course, courseType) {
-        var showLectureView = 'no';
-        var params = { courseType: courseType, courseId: course.contentId,tocId:course.courseId, lectureView: showLectureView, progress: course.progress, total: course.total };
-        sessionService.setSessionData('COURSE_PARAMS', params);
-        $rootScope.isPlayerOpen = true;
-        $state.go('Toc', params);
-    };
+            learn.openCourseView = function (course, courseType) {
+                var showLectureView = 'no';
+                var params = {courseType: courseType, courseId: course.contentId, tocId: course.courseId, lectureView: showLectureView, progress: course.progress, total: course.total};
+                sessionService.setSessionData('COURSE_PARAMS', params);
+                $rootScope.isPlayerOpen = true;
+                $state.go('Toc', params);
+            };
 
-    learn.courses = function() {
-        
-        var api = 'enrollCourseApi';
-        learn[api] = {};
-        learn[api].loader = showLoaderWithMessage("", config.MESSAGES.COURSE.ENROLLED.START);
+            learn.courses = function () {
 
-        learnService.enrolledCourses(uid).then(function(successResponse) {
-                if (successResponse && successResponse.responseCode === 'OK') {
+                var api = 'enrollCourseApi';
+                learn[api] = {};
+                learn[api].loader = showLoaderWithMessage("", config.MESSAGES.COURSE.ENROLLED.START);
+
+                learnService.enrolledCourses(uid).then(function (successResponse) {
+                    if (successResponse && successResponse.responseCode === 'OK') {
+                        learn[api].loader.showLoader = false;
+                        learn.enrolledCourses = successResponse.result.courses;
+                        $rootScope.enrolledCourseIds = [];
+
+                        var isEnrolled = learn.enrolledCourses.forEach(function (course) {
+                            $rootScope.enrolledCourseIds.push(course.courseId);
+                        });
+                    } else {
+                        learn[api].loader.showLoader = false;
+                        learn[api].error = showErrorMessage(true, config.MESSAGES.HOME.ENROLLED.FAILED, config.MESSAGES.COMMON.ERROR);
+                    }
+                })
+                .catch(function (error) {
                     learn[api].loader.showLoader = false;
-                    learn.enrolledCourses = successResponse.result.courses;
-                    $rootScope.enrolledCourseIds = [];
+                    learn[api].error = showErrorMessage(true, config.MESSAGES.HOME.ENROLLED.FAILED, config.MESSAGES.COMMON.ERROR);
+                });
+            };
 
-                    var isEnrolled = learn.enrolledCourses.forEach(function(course) {
-                        $rootScope.enrolledCourseIds.push(course.courseId);
-                    });
-                } else {
-                learn[api].loader.showLoader = false;
-            }
-        })
-        .catch(function (error) {
-            learn[api].loader.showLoader = false;
+            learn.courses();
+
+            learn.otherSection = function () {
+                var req = {
+                    'request': {
+                        'context': {
+                            'userId': uid
+                        }
+                    }
+                };
+                var api = 'pageApi';
+                learn[api] = {};
+                learn[api].loader = showLoaderWithMessage("", config.MESSAGES.COURSE.PAGE_API.START);
+
+                learnService.otherSections(req).then(function (successResponse) {
+                    if (successResponse && successResponse.responseCode === 'OK' && successResponse.result.response) {
+                        learn[api].loader.showLoader = false;
+                        learn.page = successResponse.result.response.sections;
+                    } else {
+                        learn[api].loader.showLoader = false;
+                        learn[api].error = showErrorMessage(true, config.MESSAGES.HOME.PAGE_API.FAILED, config.MESSAGES.COMMON.ERROR);
+                    }
+                })
+                .catch(function (error) {
+                    learn[api].loader.showLoader = false;
+                    learn[api].error = showErrorMessage(true, config.MESSAGES.HOME.PAGE_API.FAILED, config.MESSAGES.COMMON.ERROR);
+                });
+            };
+            learn.otherSection();
         });
-    };
-    
-    learn.courses();
-    
-    learn.otherSection = function() {
-        var req = {
-            'request': {
-                'context': {
-                    'userId': uid
-                }
-            }
-        };
-        var api = 'pageApi';
-        learn[api] = {};
-        learn[api].loader = showLoaderWithMessage("", config.MESSAGES.COURSE.PAGE_API.START);
-        
-        learnService.otherSections(req).then(function(successResponse) {
-            if (successResponse && successResponse.responseCode === 'OK' && successResponse.result.response) {
-                learn[api].loader.showLoader = false;
-                learn.page = successResponse.result.response.sections;
-            } else {
-                learn[api].loader.showLoader = false;
-            }
-        })
-        .catch(function (error) {
-            learn[api].loader.showLoader = false;
-        });
-    };
-    learn.otherSection();
-});
