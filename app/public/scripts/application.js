a6cfad07f48499a158dfde169752f11a1be0664b
Harishkumar Gangula <harishg@ilimi.in>
2017-06-29 19:15:50 +0530

    Issue #1 feat: updated project structure for SSO

---
+++ app/public/scripts/application.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name playerApp
+ * @description
+ * # playerApp
+ *
+ * Main module of the application.
+ */
+angular.module('loginApp', [])
+        .controller('loginCtrl', ['$scope', function($scope){
+            
+        }])
+
+    
--- app/server.js
+++ app/server.js
@@ -1,12 +1,32 @@
 'use strict';
 
-var express = require('express');
-var app = express();
-var router = express.Router();
-var request = require('request');
-var proxy = require('express-http-proxy');
+const express = require('express'),
+app = express(),
+router = express.Router(),
+request = require('request'),
+proxy = require('express-http-proxy'),
+Keycloak     = require('keycloak-connect'),
+session      = require('express-session'),
+path = require('path');
 
-app.use(express.static('./'));
+// Create a new session store in-memory
+var memoryStore = new session.MemoryStore();
+// Setup keycloak to use the in-memory store
+var keycloak = new Keycloak({store: memoryStore});
+
+app.use(session({
+  secret: 'mySecret',
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore
+}));
+app.use(keycloak.middleware({admin: '/'}));
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'private')));
+
 app.use('/ekContentEditor', express.static('./thirdparty/content-editor'))
 app.get('/ekContentEditor', function (req, res) {
     res.sendFile(__dirname+"/thirdparty/content-editor/index.html");
@@ -57,8 +77,17 @@ app.get('/ekContentEditor', function (req, res) {
         }
     }));
 
-app.get('/*', function(req, res) {
-    res.sendFile(__dirname+'/index.html');
+app.all('/private/*', keycloak.protect(), function(req, res) {
+    res.locals.userId = req.kauth.grant.access_token.content.sub;
+    res.render(__dirname+'/private/index.ejs');
 });
-app.listen(80);
+
+app.all('/', function(req, res) {
+    res.sendFile(__dirname+'/public/index.html');
+});
+app.all('*', function(req, res) {
+    res.redirect('/');
+});
+
+app.listen(8080);
 console.log('app running on port 80');
