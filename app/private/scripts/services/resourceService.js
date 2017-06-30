2243b8bee2f1be7bdbc8fab8b5cfb7894f0320c9
Harishkumar Gangula <harishg@ilimi.in>
2017-06-30 19:19:43 +0530

    Issue #1 fix: refactoring services

--- app/private/scripts/services/resourceService.js
+++ app/private/scripts/services/resourceService.js
@@ -2,7 +2,7 @@
 angular.module('playerApp')
     .service('resourceService', function(javaService, config, $rootScope) {
         this.resources = function(req) {
-            var url = config.URL.BASE_PREFIX + config.URL.LEARNER_PREFIX + config.URL.RESOURCE.GET;
+            var url =  config.URL.LEARNER_PREFIX + config.URL.RESOURCE.GET;
             var headers = {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
