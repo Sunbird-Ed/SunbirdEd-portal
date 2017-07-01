dd80f861c925f975219039fb033dd668569cbc81
Poonam <pnmsharma623.com>
2017-07-01 18:08:10 +0530

    Issue #300 feat:profile header

--- app/private/scripts/controllers/profileController.js
+++ app/private/scripts/controllers/profileController.js
@@ -8,8 +8,29 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-    .controller('ProfileController', function() {
-        
-        var profileCtrl = this;
-        
-});
+    .controller('ProfileController', function($rootScope, profileService) {
+        var profile = this;
+        profile.userId = $rootScope.userId;
+
+        profile.userProfile = function(userProfile) {
+            if (userProfile && userProfile.responseCode === 'OK') {
+                var profileData = userProfile.result.profile;
+                profile.profilePic = profileData.avatar;
+                profile.firstName = profileData.firstName;
+                profile.lastName = profileData.lastName;
+                profile.summary = profileData.summary;
+                profile.preferredLanguage = profileData.language;
+                profile.organizationName = profileData.organisations[0].name;
+                profile.city = profileData.address[0].city;
+            }
+        };
+        profile.getProfile = function() {
+            profileService.getUserProfile(profile.userId)
+                .then(function(successResponse) {
+                    profile.userProfile(successResponse);
+                }).catch(function() {
+
+                });
+        };
+        profile.getProfile();
+    });
