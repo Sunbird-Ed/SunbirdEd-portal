507ab1b35bbdfa131c1d7ed235357d625695bb51
loganathan <loganathan.shanmugam@tarento.com>
2017-07-10 19:51:18 +0530

    Issue #1 feat: Course Toc issue fixes

--- app/private/scripts/services/playerTelemetryUtilsService.js
+++ app/private/scripts/services/playerTelemetryUtilsService.js
@@ -1,18 +1,21 @@
 'use strict';
 angular.module('playerApp')
-        .service('playerTelemetryUtilsService', function ($rootScope,$stateParams) {
-             this.startTelemetry = function (data) {
+        .service('playerTelemetryUtilsService', function ($rootScope, $stateParams) {
+            this.startTelemetry = function (data) {
                 org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:start", data);
             };
             this.updateTelemetry = function (data) {
                 org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:intreact", data);
             };
-            this.endTelemetry=function(){
-                 org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:end");
+            this.endTelemetry = function () {
+                org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:end");
+                org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:sync", {
+                    TelemetryData: TelemetryService._data
+                });
             };
-             this.navigateTelemetry =function (data) {
+            this.navigateTelemetry = function (data) {
                 org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:navigate", data);
-            };  
+            };
             this.init = function (data) {
                 var _instance = {
                     correlationData: [{"id": $stateParams.tocId || data.contentId, "type": "ContentSession"}],
