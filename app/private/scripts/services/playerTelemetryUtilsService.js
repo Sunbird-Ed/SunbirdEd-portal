7c4e8e98fb4fe37a93d44810018ad95f3cf29ad9
loganathan <loganathan.shanmugam@tarento.com>
2017-07-10 11:59:58 +0530

    Issue #1 fix: Merge keycloak branch

--- app/private/scripts/services/playerTelemetryUtilsService.js
+++ app/private/scripts/services/playerTelemetryUtilsService.js
@@ -1,16 +1,22 @@
 'use strict';
 angular.module('playerApp')
-        .service('playerTelemetryUtilsService', function () {
-            this.updateTelemetry = function () {
-
+        .service('playerTelemetryUtilsService', function ($rootScope,$stateParams) {
+             this.startTelemetry = function (data) {
+                org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:start", data);
             };
-            this.init = function (item) {
-                console.log(item);
+            this.updateTelemetry = function (data) {
+                org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:intreact", data);
+            };
+            this.endTelemetry=function(){
+                 org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:end");
+            };
+            this.init = function (data) {
                 var _instance = {
-                    correlationData: [{"id": "ba50f82f3a31fe255b6fa24ecbd00578", "type": "ContentSession"}],
-                    user: {"avatar": "assets/icons/avatar_anonymous.png", "profileImage": "assets/icons/avatar_anonymous.png", "gender": "male", "handle": "Anonymous", "age": 6, "standard": -1, "uid": "9g8h4ndAnonymouscg56ngd"},
+                    correlationData: [{"id": $stateParams.tocId || data.contentId, "type": "ContentSession"}],
+                    user: {"avatar": "assets/icons/avatar_anonymous.png", "profileImage": "assets/icons/avatar_anonymous.png", "gender": "male", "handle": "Anonymous", "age": 6, "standard": -1, "sid": "", "did": "", "uid": $rootScope.userId},
                     gameData: {"id": "org.sunbird.player", "ver": "1.0"}
                 }
                 org.sunbird.portal.eventManager.dispatchEvent('sunbird:telemetry:init', _instance);
             };
+
         });
