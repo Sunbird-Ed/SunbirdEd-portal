f9b37e4bbc2efb3d677f4acd78c9d557e5a8448c
anuj <anujtech46>
2017-07-10 14:04:49 +0530

    Issue #1 fix: Merge pull

--- app/private/scripts/controllers/contentPlayerController.js
+++ app/private/scripts/controllers/contentPlayerController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('playerApp').controller('contentPlayerCtrl', function (noteService, $state, $scope, $sce, contentService, pdfDelegate, $timeout, config) {
+angular.module('playerApp').controller('contentPlayerCtrl', function (playerTelemetryUtilsService, $state, $scope, $sce, contentService, pdfDelegate, $timeout, config) {
     var player = this;
     $scope.isClose = $scope.isclose;
     $scope.isHeader = $scope.isheader;
@@ -16,17 +16,20 @@ angular.module('playerApp').controller('contentPlayerCtrl', function (noteServic
 
     function showPlayer(data) {
         $scope.contentData = data;
+        $scope._instance = {
+            id: $scope.contentData.identifier,
+            ver: $scope.contentData.pkgVersion
+        };
         $scope.showMetaData = $scope.isshowmetaview;
         /**
          * @event 'sunbird:portal:telemetryend' 
          * Listen for this event to get the telemetry OE_END event from renderer
          * Player controller dispatching the event subird 
          */
-        window.addEventListener('renderer:telemetryevent:end',function(event, data){
-            console.info('OE_END event:',event.detail.telemetryData);
-            org.sunbird.portal.eventManager.dispatchEvent('sunbird:portal:telemetryend',event.detail.telemetryData);
+        window.addEventListener('renderer:telemetryevent:end', function (event, data) {
+            console.info('OE_END event:', event.detail.telemetryData);
+            org.sunbird.portal.eventManager.dispatchEvent('sunbird:portal:telemetryend', event.detail.telemetryData);
         });
-
 
         if ($scope.contentData.mimeType === 'application/vnd.ekstep.ecml-archive' || $scope.contentData.mimeType === 'application/vnd.ekstep.html-archive') {
             $scope.showIFrameContent = true;
@@ -39,12 +42,12 @@ angular.module('playerApp').controller('contentPlayerCtrl', function (noteServic
                     configuration.context = config.ekstep_CP_config.context;
                     configuration.context.contentId = $scope.contentData.identifier;
                     // TODO: sid,uid,channel 
-                    configuration.context.sid =  'Sunbird_sid';
-                    configuration.context.uid ='Sunbird_uid';
-                    configuration.context.channel='Sunbird_channel',
-                    configuration.context.dimension = 'Sunbird_dimension',
-                    configuration.context.appid = 'Sunbird_appId',
-                    configuration.config = config.ekstep_CP_config.config;
+                    configuration.context.sid = 'Sunbird_sid';
+                    configuration.context.uid = 'Sunbird_uid';
+                    configuration.context.channel = 'Sunbird_channel',
+                            configuration.context.dimension = 'Sunbird_dimension',
+                            configuration.context.appid = 'Sunbird_appId',
+                            configuration.config = config.ekstep_CP_config.config;
                     configuration.plugins = config.ekstep_CP_config.config.plugins;
                     configuration.repos = config.ekstep_CP_config.config.repos;
                     previewContentIframe.contentWindow.initializePreview(configuration);
@@ -52,8 +55,41 @@ angular.module('playerApp').controller('contentPlayerCtrl', function (noteServic
             }, 1000);
         } else {
             $scope.showIFrameContent = false;
+            var telemetryInitData = {contentId: $scope.contentData.identifier}
+            playerTelemetryUtilsService.init(telemetryInitData);
         }
     }
+
+
+    $scope.initVideoEvents = function (video) {
+
+        video.on('play', function () {
+            if (parseInt(this.currentTime()) == 0) {
+                var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, "data": {"mode": "play"}};
+                playerTelemetryUtilsService.startTelemetry(telemetryData);
+            } else {
+                var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "TOUCH", "data": { subtype: "RESUME"}};
+                playerTelemetryUtilsService.updateTelemetry(telemetryData);
+            }
+        });
+        video.on('pause', function () {
+            var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "TOUCH",  "data": {subtype: "PAUSE"}};
+            playerTelemetryUtilsService.updateTelemetry(telemetryData);
+        });
+
+        video.on('ended', function () {
+            playerTelemetryUtilsService.endTelemetry();
+        });
+        video.on('volumechange', function () {
+            var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "TOUCH",  "data": {subtype: "VOLUME"}};
+            playerTelemetryUtilsService.updateTelemetry(telemetryData);
+        });
+        video.on('fullscreenchange', function () {
+            var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "TOUCH", "data": { subtype: "FULLSCREEN"}};
+            playerTelemetryUtilsService.updateTelemetry(telemetryData);
+        });
+    }
+
     /**
      * This function helps to show loader or any error message at the time of api call.
      * @param {Boolean} showMetaLoader
@@ -101,7 +137,7 @@ angular.module('playerApp').controller('contentPlayerCtrl', function (noteServic
         }
 
         $scope.visibility = false;
-
+        playerTelemetryUtilsService.endTelemetry();
     };
     $scope.tryAgain = function () {
         $scope.errorObject = {};
@@ -109,31 +145,54 @@ angular.module('playerApp').controller('contentPlayerCtrl', function (noteServic
     };
     $scope.zoomIn = function () {
         pdfDelegate.$getByHandle('content-player').zoomIn();
+        var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "ZOOM", subtype: "", "data": {"stageId": $scope.getCurrentPage}};
+        playerTelemetryUtilsService.updateTelemetry(telemetryData);
     };
     $scope.zoomOut = function () {
         pdfDelegate.$getByHandle('content-player').zoomOut();
+        var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "ZOOM", subtype: "", "data": {"stageId": $scope.getCurrentPage}};
+        playerTelemetryUtilsService.updateTelemetry(telemetryData);
     };
     $scope.previous = function () {
         pdfDelegate.$getByHandle('content-player').prev();
         $scope.getCurrentPage = $scope.getCurrentPage > 1 ? $scope.getCurrentPage - 1 : $scope.getCurrentPage;
+        var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "TOUCH", subtype: "", "data": {"stageId": $scope.getCurrentPage}};
+        playerTelemetryUtilsService.updateTelemetry(telemetryData);
+        var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, "stageid": $scope.getCurrentPage + 1, "stageto": $scope.getCurrentPage, "data": {}};
+        playerTelemetryUtilsService.navigateTelemetry(telemetryData);
     };
     $scope.next = function () {
         pdfDelegate.$getByHandle('content-player').next();
         $scope.getCurrentPage = $scope.getCurrentPage < $scope.totalPageNumber ? $scope.getCurrentPage + 1 : $scope.getCurrentPage;
+        var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "TOUCH", subtype: "", "data": {"stageId": $scope.getCurrentPage}};
+        playerTelemetryUtilsService.updateTelemetry(telemetryData);
+        var telemetryNavData = {"id": $scope._instance.id, "ver": $scope._instance.ver, "stageid": $scope.getCurrentPage - 1, "stageto": $scope.getCurrentPage, "data": {}};
+        playerTelemetryUtilsService.navigateTelemetry(telemetryNavData);
+        if ($scope.getCurrentPage == $scope.totalPageNumber) {
+            playerTelemetryUtilsService.endTelemetry();
+        }
     };
     $scope.rotate = function () {
         pdfDelegate.$getByHandle('content-player').rotate();
+        var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "ROTATE", subtype: "", "data": {"stageId": $scope.getCurrentPage}};
+        playerTelemetryUtilsService.updateTelemetry(telemetryData);
     };
     $scope.goToPage = function (pageNumber) {
+        var telemetryNavData = {"id": $scope._instance.id, "ver": $scope._instance.ver, "stageid": $scope.getCurrentPage, "stageto": pageNumber, "data": {}};
         pdfDelegate.$getByHandle('content-player').goToPage(pageNumber);
         $scope.getCurrentPage = pageNumber;
+        var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, type: "TOUCH", subtype: "", "data": {"stageId": $scope.getCurrentPage}};
+        playerTelemetryUtilsService.updateTelemetry(telemetryData);
+        playerTelemetryUtilsService.navigateTelemetry(telemetryNavData);
     };
     $scope.getTotalPage = function () {
         $timeout(function () {
             $scope.totalPageNumber = pdfDelegate.$getByHandle('content-player').getPageCount();
             $scope.getCurrentPage = pdfDelegate.$getByHandle('content-player').getCurrentPage();
+            var telemetryData = {"id": $scope._instance.id, "ver": $scope._instance.ver, "data": {"mode": "play", "stageid": 1}};
+            playerTelemetryUtilsService.startTelemetry(telemetryData);
         }, 2000);
     };
 
-   
+
 });
---
+++ app/private/scripts/utils/util.js
@@ -0,0 +1,44 @@
+/**
+ * @author Manjunath Davanam <manjunathd@ilimi.in>
+ */
+
+
+/**
+ * Namespace for the sunbird portal
+ */
+var content_portal = function() {};
+window.org = {sunbird: {} };
+org.sunbird.portal = new content_portal();
+
+org.sunbird.portal.init = function() {
+    console.info("Sunbrid portal init..");
+    org.sunbird.portal.addUnloadEvent();
+    org.sunbird.portal.syncTelemetry();
+    TelemetryService.registerEvents();
+};
+
+
+/**
+ * To add the any window unload events
+ */
+org.sunbird.portal.addUnloadEvent = function() {
+    
+    window.onbeforeunload = function(e) {
+        console.info("window unload is calling..")
+        e = e || window.event;
+        var y = e.pageY || e.clientY;
+        !y && org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:sync", {
+            TelemetryData: TelemetryService._data
+        });
+         org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:end");
+    };
+};
+
+/**
+ * Registerd for the sunbird telemetry sync event
+ */
+org.sunbird.portal.syncTelemetry = function() {
+    org.sunbird.portal.eventManager.addEventListener("sunbird:telemetry:sync", function(event, data) {
+    	console.info("Telemetry sync in progress..");
+    })
+};
