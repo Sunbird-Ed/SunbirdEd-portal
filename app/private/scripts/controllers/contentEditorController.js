a5677529d8f0bec88bc10263a148e29f706f0a42
anuj <anujtech46>
2017-07-10 14:04:49 +0530

    Issue #1 fix: Merge pull

--- app/private/scripts/controllers/contentEditorController.js
+++ app/private/scripts/controllers/contentEditorController.js
@@ -9,28 +9,35 @@
  */
 angular.module('playerApp')
     .controller('ContentEditorController', function(config, $stateParams, $location, $sce, $state) {
-        
+
         var contentEditor = this;
         contentEditor.contentId = $stateParams.contentId;
-        
+
         contentEditor.openContentEditor = function(contentId) {
-            
+
             window.context = config.ekstep_CE_config.context;
             window.context.content_id = contentId;
             window.config = config.ekstep_CE_config.config;
             var baseURL = $location.protocol() + '://' + $location.host() + ':' + $location.port();
-            contentEditor.ekURL = $sce.trustAsResourceUrl(baseURL+"/ekContentEditor?contentId="+contentId);
+            contentEditor.ekURL = $sce.trustAsResourceUrl(baseURL + "/ekContentEditor?contentId=" + contentId);
         };
-        
+
         contentEditor.closeContentEditor = function() {
             $state.go("WorkSpace.DraftContent");
         };
-        
+
         contentEditor.openEditContentForm = function() {
             var params = {contentId : contentEditor.contentId};
             $state.go("EditContent", params);
         };
-        
+
+        contentEditor.init = function() {
+            window.addEventListener('editor:metadata:edit', function(event, data) {
+                console.info('Sunbird edit metadata is calling');
+                org.sunbird.portal.eventManager.dispatchEvent('sunbird:portal:editmetadata');
+            });
+        };
+        contentEditor.init();
         contentEditor.openContentEditor($stateParams.contentId);
-        
-});
+
+    });
