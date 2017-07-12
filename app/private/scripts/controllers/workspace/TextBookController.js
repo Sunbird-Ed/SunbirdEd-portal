ff9da0536230fa5ed16a43c28496bfce4025562f
Harishkumar Gangula <harishg@ilimi.in>
2017-07-12 17:30:32 +0530

    Issue #1 fix: collection and text book editor fixes

--- app/private/scripts/controllers/workspace/TextBookController.js
+++ app/private/scripts/controllers/workspace/TextBookController.js
@@ -111,7 +111,7 @@ angular.module('playerApp')
 
             requestBody.name = requestBody.name ? requestBody.name : "Default title";
             requestBody.description = requestBody.description ? requestBody.description : "Default description";
-            requestBody.contentType = requestBody.contentType ? requestBody.contentType : "Textbook";
+            requestBody.contentType =  "TextBook";
 
             var requestdata = {
                 "content": requestBody
