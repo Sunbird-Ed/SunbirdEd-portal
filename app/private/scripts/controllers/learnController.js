300eca5813f77765d4dc0585435708eb323d2968
loganathan <loganathan.shanmugam@tarento.com>
2017-07-13 20:18:17 +0530

    Issue #1 fix: bug fixes

--- app/private/scripts/controllers/learnController.js
+++ app/private/scripts/controllers/learnController.js
@@ -36,7 +36,7 @@ angular.module('playerApp').controller('LearnCtrl', function (learnService, $sco
 
             learn.openCourseView = function (course, courseType) {
                 var showLectureView = 'no';
-                var params = {courseType: courseType, courseId: course.courseId || course.identifier, tocId: course.courseId || course.identifier, lectureView: showLectureView, progress: course.progress, total: course.total,courseName:course.courseName};
+                var params = {courseType: courseType, courseId: course.courseId || course.identifier, tocId: course.courseId || course.identifier, lectureView: showLectureView, progress: course.progress, total: course.total,courseName:course.courseName,lastReadContentId:course.lastReadContentId};
                 sessionService.setSessionData('COURSE_PARAMS', params);
                 $rootScope.isPlayerOpen = true;
                 $state.go('Toc', params);
