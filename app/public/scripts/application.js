f44a87dba95f0a75ed790f96e129fa96c558cd3d
anuj <anujtech46>
2017-07-14 20:42:12 +0530

    Issue #1 fix:  Changes gulp file to generate config files

--- app/public/scripts/application.js
+++ app/public/scripts/application.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular.module('loginApp', [])
+angular.module('loginApp', ['loginApp.labels', 'loginApp.errorMessages'])
     .controller('loginCtrl', function(labels, $rootScope, errorMessages) {
         $rootScope.errorMessages = errorMessages;
         $rootScope.labels = labels;
---
+++ app/public/scripts/publicLabels.js
@@ -0,0 +1,2 @@
+angular.module("loginApp.labels", [])
+.constant("labels", {"SignUp":{"name":"Name","userName":"User Name","password":"Password","phoneNumber":"Phone Number","email":"Email","gender":"Gender","dob":"Date Of Birth","language":"Language"},"Button":{"signUp":"SIGN UP","login":"LOGIN"},"MainMenu":{"home":"Home","courses":"Courses","profile":"Profile","resources":"Resources","community":"Community"},"Headers":{"signUp":"Sign Up"},"languages":["Bengali","English","Gujarati","Hindi","Kannada","Marathi","Punjabi","Tamil","Telugu"]});
