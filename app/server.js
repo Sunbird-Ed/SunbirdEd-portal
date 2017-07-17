ff92f24e52e0b33d7afea05ecbd3a52663bf2fc8
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-07-17 12:02:47 +0530

    Issue #000 feat : remove key cloak middleware in service calls

--- app/server.js
+++ app/server.js
@@ -49,21 +49,18 @@ app.get('/collectionEditor', function(req, res) {
 
 
 app.all('/public/service/v1/*', proxy(learnerURL, {
-    https: false,
     proxyReqPathResolver: function(req) {
         let urlParam = req.params["0"];
         return require('url').parse(learnerURL + urlParam).path;
     }
 }))
-app.all('/private/service/v1/learner/*', keycloak.protect(), proxy(learnerURL, {
-    https: false,
+app.all('/private/service/v1/learner/*', proxy(learnerURL, {
     proxyReqPathResolver: function(req) {
         let urlParam = req.params["0"];
         return require('url').parse(learnerURL + urlParam).path;
     }
 }))
-app.all('/private/service/v1/content/*', keycloak.protect(), proxy(contentURL, {
-    https: false,
+app.all('/private/service/v1/content/*', proxy(contentURL, {
     proxyReqPathResolver: function(req) {
         let urlParam = req.params["0"];
         let query = require('url').parse(req.url).query;
