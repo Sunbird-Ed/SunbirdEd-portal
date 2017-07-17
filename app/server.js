42d78563dc5630afd418fd7797fcd21c717344c8
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-07-17 18:33:59 +0530

    Issue #000 feat : https proxy issue - revert the changes in application

--- app/server.js
+++ app/server.js
@@ -54,13 +54,13 @@ app.all('/public/service/v1/*', proxy(learnerURL, {
         return require('url').parse(learnerURL + urlParam).path;
     }
 }))
-app.all('/private/service/v1/learner/*', proxy(learnerURL, {
+app.all('/private/service/v1/learner/*', keycloak.protect(), proxy(learnerURL, {
     proxyReqPathResolver: function(req) {
         let urlParam = req.params["0"];
         return require('url').parse(learnerURL + urlParam).path;
     }
 }))
-app.all('/private/service/v1/content/*', proxy(contentURL, {
+app.all('/private/service/v1/content/*', keycloak.protect(), proxy(contentURL, {
     proxyReqPathResolver: function(req) {
         let urlParam = req.params["0"];
         let query = require('url').parse(req.url).query;
