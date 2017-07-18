30b7c01b3d52c544317bcd5325c612d7b829f915
Harishkumar Gangula <harishg@ilimi.in>
2017-07-18 10:21:43 +0530

    Issue #1 fix: lauching editor in different modes

--- app/private/scripts/controllers/collectionEditorController.js
+++ app/private/scripts/controllers/collectionEditorController.js
@@ -8,12 +8,26 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-    .controller('CollectionEditorController', function(config, $stateParams, $location, $sce, $state, $timeout, $rootScope) {
+    .controller('CollectionEditorController', function(config, $stateParams, $location, $sce, $state, $timeout, $rootScope, contentService) {
 
         var collectionEditor = this;
         collectionEditor.contentId = $stateParams.contentId;
 
         collectionEditor.openCollectionEditor = function(data) {
+
+            $("#collectionEditor").iziModal({
+                title: '',
+                iframe: true,
+                iframeURL: "/thirdparty/bower_components/collection-editor-iframe/index.html",
+                navigateArrows: false,
+                fullscreen: false,
+                openFullscreen: true,
+                closeOnEscape: false,
+                overlayClose: false,
+                onClosed: function() {
+                    $state.go('EditContent', { contentId: collectionEditor.contentId });
+                }
+            });
 
             window.context = {
                 user: {
@@ -43,39 +57,51 @@ angular.module('playerApp')
                 }
             }
 
-            $("#collectionEditor").iziModal({
-                title: '',
-                iframe: true,
-                iframeURL: "/thirdparty/bower_components/collection-editor-iframe/index.html",
-                navigateArrows: false,
-                fullscreen: false,
-                openFullscreen: true,
-                closeOnEscape: false,
-                overlayClose: false,
-                onClosed: function() {
-                    $state.go('EditContent', { contentId: collectionEditor.contentId });
+            var req = { contentId: collectionEditor.contentId };
+            var qs = { fields: 'createdBy,status' }
+
+            contentService.getById(req, qs).then(function(response) {
+                if (response && response.responseCode === 'OK') {
+                    if (response.result.content.createdBy !== $rootScope.userId) {
+                        $state.go('Home');
+                    } else {
+                        collectionEditor.updateModeAndStatus(response.result.content.status);
+                        $timeout(function() {
+                            $('#collectionEditor').iziModal('open');
+                        }, 100);
+                    }
                 }
             });
-
-            $timeout(function() {
-                $('#collectionEditor').iziModal('open');
-            }, 100);
         };
 
         collectionEditor.getTreeNodes = function(type) {
             var editorConfig = [];
             switch (type) {
                 case 'Course':
-                    editorConfig.push({"type":"Course","label":"Course","isRoot":true,"editable":true,"childrenTypes":["CourseUnit","Collection","Story","Game","Worksheet"],"addType":"Editor","iconClass":"fa fa-book"},{"type":"CourseUnit","label":"Course Unit","isRoot":false,"editable":true,"childrenTypes":["CourseUnit","Collection","Story","Game","Worksheet"],"addType":"Editor","iconClass":"fa fa-folder"},{"type":"Collection","label":"Collection","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"},{"type":"Story","label":"Story","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"},{"type":"Worksheet","label":"Worksheet","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"},{"type":"Game","label":"Game","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"});
+                    editorConfig.push({ "type": "Course", "label": "Course", "isRoot": true, "editable": true, "childrenTypes": ["CourseUnit", "Collection", "Story", "Game", "Worksheet"], "addType": "Editor", "iconClass": "fa fa-book" }, { "type": "CourseUnit", "label": "Course Unit", "isRoot": false, "editable": true, "childrenTypes": ["CourseUnit", "Collection", "Story", "Game", "Worksheet"], "addType": "Editor", "iconClass": "fa fa-folder" }, { "type": "Collection", "label": "Collection", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" }, { "type": "Story", "label": "Story", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" }, { "type": "Worksheet", "label": "Worksheet", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" }, { "type": "Game", "label": "Game", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" });
                     return editorConfig;
                 case 'Collection':
-                    editorConfig.push({"type":"Collection","label":"Collection","isRoot":true,"editable":true,"childrenTypes":["Collection","Story","Game","Worksheet"],"addType":"Editor","iconClass":"fa fa-folder"},{"type":"Story","label":"Story","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"},{"type":"Worksheet","label":"Worksheet","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"},{"type":"Game","label":"Game","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"});
+                    editorConfig.push({ "type": "Collection", "label": "Collection", "isRoot": true, "editable": true, "childrenTypes": ["Collection", "Story", "Game", "Worksheet"], "addType": "Editor", "iconClass": "fa fa-folder" }, { "type": "Story", "label": "Story", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" }, { "type": "Worksheet", "label": "Worksheet", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" }, { "type": "Game", "label": "Game", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" });
                     return editorConfig;
                 default:
-                    editorConfig.push({"type":"TextBook","label":"Textbook","isRoot":true,"editable":true,"childrenTypes":["TextBookUnit","Collection","Story","Game","Worksheet"],"addType":"Editor","iconClass":"fa fa-book"},{"type":"TextBookUnit","label":"Textbook Unit","isRoot":false,"editable":true,"childrenTypes":["TextBookUnit","Collection","Story","Game","Worksheet"],"addType":"Editor","iconClass":"fa fa-folder"},{"type":"Collection","label":"Collection","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"},{"type":"Story","label":"Story","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"},{"type":"Worksheet","label":"Worksheet","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"},{"type":"Game","label":"Game","isRoot":false,"editable":false,"childrenTypes":[],"addType":"Browser","iconClass":"fa fa-file"});
+                    editorConfig.push({ "type": "TextBook", "label": "Textbook", "isRoot": true, "editable": true, "childrenTypes": ["TextBookUnit", "Collection", "Story", "Game", "Worksheet"], "addType": "Editor", "iconClass": "fa fa-book" }, { "type": "TextBookUnit", "label": "Textbook Unit", "isRoot": false, "editable": true, "childrenTypes": ["TextBookUnit", "Collection", "Story", "Game", "Worksheet"], "addType": "Editor", "iconClass": "fa fa-folder" }, { "type": "Collection", "label": "Collection", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" }, { "type": "Story", "label": "Story", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" }, { "type": "Worksheet", "label": "Worksheet", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" }, { "type": "Game", "label": "Game", "isRoot": false, "editable": false, "childrenTypes": [], "addType": "Browser", "iconClass": "fa fa-file" });
                     return editorConfig;
             }
         };
 
+        collectionEditor.updateModeAndStatus = function (status) {
+            if (status.toLowerCase() === "draft") {
+                window.config.editorConfig.mode = "Edit";
+                window.config.editorConfig.contentStatus = "draft";
+            }
+            if (status.toLowerCase() === "review") {
+                window.config.editorConfig.mode = "Read";
+                window.config.editorConfig.contentStatus = "draft";
+            }
+            if (status.toLowerCase() === "live") {
+                window.config.editorConfig.mode = "Edit";
+                window.config.editorConfig.contentStatus = "live";
+            }
+        }
         collectionEditor.openCollectionEditor($stateParams);
     });
