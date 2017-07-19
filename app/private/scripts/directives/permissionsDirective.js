ea2df7188dff24e12f56c617f8953ecd489f184c
Rayulu Villa <rayulu@Rayulus-MacBook-Pro.local>
2017-07-20 01:01:19 +0530

    Issue #000 feat : Disable permissions temporarily

--- app/private/scripts/directives/permissionsDirective.js
+++ app/private/scripts/directives/permissionsDirective.js
@@ -14,12 +14,12 @@ angular.module('playerApp').directive('sbPermissions', function (permissionsServ
         link: function (scope, element, attrs) {
          if (attrs.permissionOnly) {
              if(!permissionsService.checkRolesPermissions(attrs.permissionOnly.split(','), true)){
-                element.remove();
+                //element.remove();
              }
          }
          if (attrs.permissionExcept) {
              if(permissionsService.checkRolesPermissions(attrs.permissionExcept.split(','), false)){
-                element.remove();
+                //element.remove();
              }
          }
         }
