ba653b6c2a452d43e432939f70f15f6357d8c286
loganathan <loganathan.shanmugam@tarento.com>
2017-07-19 21:24:35 +0530

    Issue #1 fix: content state issue fixes

--- app/private/scripts/controllers/appController.js
+++ app/private/scripts/controllers/appController.js
@@ -8,82 +8,118 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-    .controller('AppCtrl', function($scope, $state, $stateParams, $rootScope, $translate, userService, $q, config, $location, $timeout, portalTelemetryService, setResourceBundle, errorMessages, labels) {
-        $rootScope.userId = $("#userId").attr("value");
-        $rootScope.language = $rootScope.userLanguage || config.SITE.DEFAULT_LANGUAGE;
-        $rootScope.errorMessages=errorMessages;
-        $rootScope.labels=labels;
-        $rootScope.translationBundle = {};
-        $rootScope.searchKey = '';
-        org.sunbird.portal.init();
-        portalTelemetryService.init()
-        $rootScope.openLink = function(url) {
-            $location.path(url);
-        }
-        $rootScope.loadBundle = function() {
-            var promises = [];
-            promises.push(userService.resourceBundle($rootScope.language, 'label'));
-            promises.push(userService.resourceBundle($rootScope.language, 'error'));
-            $q.all(promises).then(function(results) {
-                results.forEach(function(res) {
-                    if (res && res.responseCode == 'OK' && res.result) {
-                        $rootScope.translationBundle = $rootScope.mergeObjects($rootScope.translationBundle, res.result[$rootScope.language]);
-                        $rootScope.addTranslation($rootScope.language, $rootScope.translationBundle);
-                    }
+        .controller('AppCtrl', function ($scope, $state, $stateParams, $rootScope, $translate, userService, $q, config, $location, $timeout, portalTelemetryService, setResourceBundle, errorMessages, labels, learnService, sessionService) {
+            $rootScope.userId = $("#userId").attr("value");
+            $rootScope.language = $rootScope.userLanguage || config.SITE.DEFAULT_LANGUAGE;
+            $rootScope.errorMessages = errorMessages;
+            $rootScope.labels = labels;
+            $rootScope.translationBundle = {};
+            $rootScope.searchKey = '';
+            $rootScope.enrolledCourseIds = {};
+            org.sunbird.portal.init();
+            portalTelemetryService.init()
+            $rootScope.openLink = function (url) {
+                $location.path(url);
+            }
+            $rootScope.loadBundle = function () {
+                var promises = [];
+                promises.push(userService.resourceBundle($rootScope.language, 'label'));
+                promises.push(userService.resourceBundle($rootScope.language, 'error'));
+                $q.all(promises).then(function (results) {
+                    results.forEach(function (res) {
+                        if (res && res.responseCode == 'OK' && res.result) {
+                            $rootScope.translationBundle = $rootScope.mergeObjects($rootScope.translationBundle, res.result[$rootScope.language]);
+                            $rootScope.addTranslation($rootScope.language, $rootScope.translationBundle);
+                        }
+                    });
                 });
-            });
-        };
-        $rootScope.addTranslation = function(language, translationBundle) {
-            if (setResourceBundle(language, translationBundle)) {
-                $translate.use(language);
-            }
-        };
-        $rootScope.mergeObjects = function(obj1, obj2) {
-            var objMerge = '';
-            if (Object.keys(obj1).length > 0) {
-                objMerge = JSON.stringify(obj1) + JSON.stringify(obj2);
-                objMerge = objMerge.replace(/\}\{/, ',');
-                objMerge = JSON.parse(objMerge);
-            } else {
-                objMerge = obj2;
-            }
-            return objMerge;
-        };
-        $rootScope.loadBundle('label');
-        $('body').click(function(e) {
-            if ($(e.target).closest('div.dropdown-menu-list').prop('id') == 'search-suggestions') {
-                return false;
-            } else {
-                $('body').find('.dropdown-menu-list').removeClass('visible').addClass('hidden');
-            }
-        });
-        $scope.logout = function() {
-            window.document.location.href = '/logout';
-        }
-        //get user profile
-        $scope.userProfile = function(userProfile) {
-            if (userProfile && userProfile.responseCode === 'OK') {
-                var profileData = userProfile.result.response;
-                $rootScope.avatar = profileData.avatar;
-                $rootScope.firstName = profileData.firstName;
-                $rootScope.lastName = profileData.lastName;
-                if(profileData.rootOrg) {
-                    $rootScope.orgLogo = profileData.rootOrg.imgUrl;
+            };
+            $rootScope.addTranslation = function (language, translationBundle) {
+                if (setResourceBundle(language, translationBundle)) {
+                    $translate.use(language);
                 }
-            } else {
-                console.error('fetching profile failed');
-                //error handler
+            };
+            $rootScope.mergeObjects = function (obj1, obj2) {
+                var objMerge = '';
+                if (Object.keys(obj1).length > 0) {
+                    objMerge = JSON.stringify(obj1) + JSON.stringify(obj2);
+                    objMerge = objMerge.replace(/\}\{/, ',');
+                    objMerge = JSON.parse(objMerge);
+                } else {
+                    objMerge = obj2;
+                }
+                return objMerge;
+            };
+            $rootScope.loadBundle('label');
+            $('body').click(function (e) {
+                if ($(e.target).closest('div.dropdown-menu-list').prop('id') == 'search-suggestions') {
+                    return false;
+                } else {
+                    $('body').find('.dropdown-menu-list').removeClass('visible').addClass('hidden');
+                }
+            });
+            $scope.logout = function () {
+                window.document.location.href = '/logout';
             }
-        };
-        $scope.getProfile = function() {
-            userService.getUserProfile($rootScope.userId)
-                .then(function(successResponse) {
-                    $scope.userProfile(successResponse);
-                }).catch(function(error) {
+            //get user profile
+            $scope.userProfile = function (userProfile) {
+                if (userProfile && userProfile.responseCode === 'OK') {
+                    var profileData = userProfile.result.response;
+                    $rootScope.avatar = profileData.avatar;
+                    $rootScope.firstName = profileData.firstName;
+                    $rootScope.lastName = profileData.lastName;
+                    if (profileData.rootOrg) {
+                        $rootScope.orgLogo = profileData.rootOrg.imgUrl;
+                    }
+                } else {
+                    console.error('fetching profile failed');
+                    //error handler
+                }
+            };
+            $scope.getProfile = function () {
+                userService.getUserProfile($rootScope.userId)
+                        .then(function (successResponse) {
+                            $scope.userProfile(successResponse);
+                        }).catch(function (error) {
                     console.error('api fetching profile  failed');
                     //error handler
                 });
-        };
-        $scope.getProfile();
-        //end of get user profile
-    });
+            };
+            $scope.getProfile();
+            //end of get user profile
+            $scope.getMyCourses = function () {
+//                var userCourseInfo = sessionService.getSessionData("ENROLLED_COURSES");
+//                if (userCourseInfo && userCourseInfo['uid'] == $rootScope.userId) {
+//                    $rootScope.enrolledCourses = userCourseInfo['courseArr'];
+//                    $rootScope.enrolledCourseIds = userCourseInfo['courseObj'];
+//                } else
+                {
+                    sessionService.setSessionData("ENROLLED_COURSES", undefined);
+                    learnService.enrolledCourses($rootScope.userId).then(function (successResponse) {
+                        if (successResponse && successResponse.responseCode === 'OK') {
+                            $rootScope.enrolledCourses = successResponse.result.courses;
+                            $rootScope.enrolledCourseIds = $rootScope.arrObjsToObject($rootScope.enrolledCourses, 'courseId');
+                            sessionService.setSessionData("ENROLLED_COURSES", {uid: $rootScope.userId, 'courseArr': $rootScope.enrolledCourses, 'courseObj': $rootScope.enrolledCourseIds});
+                        } else {
+                            $rootScope.enrolledCourses = undefined;
+                            sessionService.setSessionData("ENROLLED_COURSES", undefined);
+                        }
+                    });
+                }
+            }
+            $rootScope.arrObjsToObject = function (array, key) {
+                var objData = {};
+                array.forEach(function (item) {
+                    objData[item[key]] = item;
+                });
+                return objData;
+            };
+            $rootScope.enrolledCourses ? $rootScope.enrolledCourses : $scope.getMyCourses();
+//dont remove this .to load progress bars in cards
+            $rootScope.loadProgress = function () {
+                $timeout(function () {
+                    $('.course-progress').progress();
+                });
+            };
+
+        });
