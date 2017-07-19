d041510f52e2f2fa4eb3a7262feb42f1ba45f50e
loganathan <loganathan.shanmugam@tarento.com>
2017-07-19 21:31:23 +0530

    Issue #1 fix: content state issue fixes

--- app/private/scripts/services/permissionsService.js
+++ app/private/scripts/services/permissionsService.js
@@ -8,7 +8,7 @@
  * Service in the playerApp.
  */
 angular.module('playerApp')
-    .service('permissionsService', function(httpServiceJava, config) {
+    .service('permissionsService', function(httpServiceJava, config, $rootScope) {
         var rolesAndPermissions = [];
         var currentUserRoles = [];
         var currentRoleActions = [];
@@ -41,18 +41,31 @@ angular.module('playerApp')
             //if flag is true than and we check equality if data is string and
             // if data is array check if it is array check role/actions exists in array
             this.checkRolesPermissions = function(data, flag) {
-                if (!this.checkActionsPermissions(data, flag)) {
-                    if (_.isArray(data)) {
-                        if ((_.intersection(data, currentUserRoles).length === 0) && !flag) {
-                            return true
+                if(currentUserRoles.length > 0) {
+                    if (!this.checkActionsPermissions(data, flag)) {
+                        if (_.isArray(data)) {
+                            if ((_.intersection(data, currentUserRoles).length === 0) && !flag) {
+                                return true;
+                            }
+                            return ((_.intersection(data, currentUserRoles).length > 0) && flag)
                         }
-                        return ((_.intersection(data, currentUserRoles).length > 0) && flag)
+                    } else{
+                        return true;
                     }
-                } else{
-                    return true;
+                    return false;
+                } else {
+                    this.getCurrentUserProfile().then(function (res) {
+                        if (res && res.responseCode === 'OK') {
+                            this.setCurrentUserRoles(res.result.response.roles);
+                            this.checkRolesPermissions(data, flag);
+                        } else {
+                            //TODO: allow only public permissions
+                        }
+                    }).catch(function(err) {
+                        
+                    });
                 }
-                return false;
-            }
+            };
 
         //if flag is true than and we check equality if data is string and
         // if data is array check if it is array check role/actions exists in array
@@ -68,5 +81,14 @@ angular.module('playerApp')
 
          this.getPermissionsData = function () {
               return httpServiceJava.get(config.URL.LEARNER_PREFIX+config.URL.ROLES.READ);
+        }
+
+        this.getCurrentUserRoles = function() {
+            return currentUserRoles;
+        }
+        
+        this.getCurrentUserProfile = function() {
+            var url = config.URL.LEARNER_PREFIX + config.URL.USER.GET_PROFILE + '/' + $rootScope.userId;
+            return httpServiceJava.get(url);
         }
     })
