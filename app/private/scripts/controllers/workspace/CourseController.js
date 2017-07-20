1b9b13528e6cfbf6a17eb28503a5f96ec6acefdb
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-07-21 02:21:07 +0530

    Issue #1 fix:  collection editor

--- app/private/scripts/controllers/workspace/CourseController.js
+++ app/private/scripts/controllers/workspace/CourseController.js
@@ -109,7 +109,7 @@ angular.module('playerApp')
             requestBody.mimeType = "application/vnd.ekstep.content-collection";
             requestBody.createdBy = course.userId;
 
-            requestBody.name = requestBody.name ? requestBody.name : "Untitle Course";
+            requestBody.name = requestBody.name ? requestBody.name : "Untitled Course";
             requestBody.description = requestBody.description ? requestBody.description : "Description";
             requestBody.contentType = "Course";
 
