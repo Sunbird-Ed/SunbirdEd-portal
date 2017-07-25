555f617b3f6a0479bf5a3cf5c91977b78dcc9f03
loganathan <loganathan.shanmugam@tarento.com>
2017-07-25 18:06:10 +0530

    Issue #1 fix:enable search option in profile page

--- app/private/scripts/controllers/searchController.js
+++ app/private/scripts/controllers/searchController.js
@@ -3,6 +3,8 @@ angular.module('playerApp')
         .controller('SearchCtrl', function (config, sessionService, searchService, $scope, $timeout, $rootScope, $stateParams, $state, $location) {
             var search = this;
             search.initSearch = function () {
+                $rootScope.search.searchResult={};
+                $rootScope.search.autosuggest_data=[];
                 $timeout(function(){
                 $rootScope.$broadcast('initSearch', {});
                 },500);
