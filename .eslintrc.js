a5ad4467b9a7e687aea31e3d46cca27d58509026
Poonam <poonam.sharma@compassitesinc.com>
2017-07-25 11:25:58 +0530

    Issue #1 fix :es6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "env": {
-        "browser": true
+        "browser": true,
+        "es6": true
     },
     "extends": "eslint:recommended",
     "rules": {
@@ -17,10 +18,12 @@ module.exports = {
             "always"
         ],
         "no-undef": ["off"],
-        "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0, "maxBOF": 0 }],
+        "no-multiple-empty-lines": [2, { "max": 1, "maxEOF": 1 }],
+        // "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0, "maxBOF": 0 }],
         "space-infix-ops": ["error"],
         "padded-blocks": ["error", "never"],
-        // "space-before-function-paren": ["error", { "anonymous": "always", "named": "never", "asyncArrow": "always" }],
-        "object-curly-spacing": ["error", "always"]
+        "object-curly-spacing": ["error", "always"],
+        "max-len": ["error", 80],
+        "ecmaVersion": 0
     }
 };
