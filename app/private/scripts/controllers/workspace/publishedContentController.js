ac1f50a5ad98d27dcdb28a0ddda2a1694615fa8e
Poonam <poonam.sharma@compassitesinc.com>
2017-07-26 19:01:00 +0530

    Issue #1 fix:code refactroing workspace

--- app/private/scripts/controllers/workspace/publishedContentController.js
+++ app/private/scripts/controllers/workspace/publishedContentController.js
@@ -8,55 +8,61 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-    .controller('PublishedContentController', function(contentService, searchService, config, $rootScope, $state, ToasterService) {
-
+    .controller('PublishedContentController', function (contentService,
+        searchService, config, $rootScope, $state, ToasterService) {
         var publishedContent = this;
         publishedContent.userId = $rootScope.userId;
 
         function getPublishedContent() {
-
-            publishedContent.loader = ToasterService.loader("", $rootScope.errorMessages.WORKSPACE.PUBLISHED.START);
-
+            publishedContent.loader = ToasterService
+            .loader('', $rootScope.errorMessages.WORKSPACE.PUBLISHED.START);
             var request = {
                 filters: {
-                    status: ["Live"],
+                    status: ['Live'],
                     createdBy: publishedContent.userId
                 },
 
-                'sort_by': {
-                    "lastUpdatedOn": "desc"
+                sort_by: {
+                    lastUpdatedOn: 'desc'
                 }
             };
-            
-            publishedContent.publishedContentData = [];
-            searchService.search(request).then(function(res) {
-                    if (res && res.responseCode === 'OK') {
-                        publishedContent.loader.showLoader = false;
-                        publishedContent.publishedContentData = res.result.content;
-                        if (res.result.count === 0) {
-                            publishedContent.zeroContentMessage = $rootScope.errorMessages.WORKSPACE.PUBLISHED.NO_CONTENT;
-                        }
-                    } else {
-                        publishedContent.loader.showLoader = false;
-                        ToasterService.error($rootScope.errorMessages.WORKSPACE.PUBLISHED.FAILED);
-                    }
-                })
-                .catch(function(error) {
-                    publishedContent.loader.showLoader = false;
-                    ToasterService.error($rootScope.errorMessages.WORKSPACE.PUBLISHED.FAILED);
-                });
-        };
 
-        publishedContent.initializeData = function() {
+            publishedContent.publishedContentData = [];
+            searchService.search(request).then(function (res) {
+                if (res && res.responseCode === 'OK') {
+                    publishedContent.loader.showLoader = false;
+                    publishedContent.publishedContentData = res.result.content;
+                    if (res.result.count === 0) {
+                        publishedContent.zeroContentMessage
+                        = $rootScope.errorMessages
+                        .WORKSPACE.PUBLISHED.NO_CONTENT;
+                    }
+                } else {
+                    publishedContent.loader.showLoader = false;
+                    ToasterService
+                    .error($rootScope.errorMessages.WORKSPACE.PUBLISHED.FAILED);
+                }
+            })
+                .catch(function () {
+                    publishedContent.loader.showLoader = false;
+                    ToasterService
+                    .error($rootScope.errorMessages.WORKSPACE.PUBLISHED.FAILED);
+                });
+        }
+
+        publishedContent.initializeData = function () {
             getPublishedContent();
         };
 
-        publishedContent.openContentPlayer = function(item) {
-            if (item.mimeType === "application/vnd.ekstep.content-collection") {
-                $state.go("CollectionEditor", { contentId: item.identifier, type: item.contentType, state: "WorkSpace.PublishedContent" });
+        publishedContent.openContentPlayer = function (item) {
+            if (item.mimeType === 'application/vnd.ekstep.content-collection') {
+                $state.go('CollectionEditor', { contentId: item.identifier,
+                    type: item.contentType,
+                    state: 'WorkSpace.PublishedContent' });
             } else {
-                var params = { contentId: item.identifier, backState: $state.current.name };
-                $state.go("PreviewContent", params);
+                var params = { contentId: item.identifier,
+                    backState: $state.current.name };
+                $state.go('PreviewContent', params);
             }
         };
     });
