2ba0aff8419236080bd9e9f677baf4442b0ff540
anuj <anujtech46>
2017-07-26 11:16:38 +0530

    Issue #1 fix: Add izimodal in home page

--- app/private/scripts/controllers/homeController.js
+++ app/private/scripts/controllers/homeController.js
@@ -7,17 +7,9 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-        .controller('HomeController', function ($state, learnService, $rootScope, sessionService, config, $scope) {
+        .controller('HomeController', function ($state, learnService, $rootScope, sessionService, config, $scope, ToasterService) {
             var homeCtrl = this;
             var uid = $rootScope.userId;
-            if ($rootScope.accessDenied) {
-                homeCtrl.roleAccessError = {};
-                homeCtrl.roleAccessError.error = showErrorMessage(true, $rootScope.accessDenied, $rootScope.errorMessages.COMMON.ERROR);
-            }
-
-            $scope.$watch("homeCtrl.roleAccessError.error.showError", function () {
-                $rootScope.accessDenied = null;
-            });
 
             homeCtrl.loadCarousel = function () {
                 $('.ui .progress .course-progress').progress();
@@ -33,51 +25,24 @@ angular.module('playerApp')
                         }).rating('disable', true);
             };
 
-            /**
-             * This function helps to show loader with message.
-             * @param {String} headerMessage
-             * @param {String} loaderMessage
-             */
-            function showLoaderWithMessage(headerMessage, loaderMessage) {
-                var loader = {};
-                loader.showLoader = true;
-                loader.headerMessage = headerMessage;
-                loader.loaderMessage = loaderMessage;
-                return loader;
-            }
-
-            /**
-             * This function called when api failed, and its show failed response for 2 sec.
-             * @param {String} message
-             */
-            function showErrorMessage(isClose, message, messageType) {
-                var error = {};
-                error.showError = true;
-                error.isClose = isClose;
-                error.message = message;
-                error.messageType = messageType;
-                return error;
-            }
-
             homeCtrl.courses = function () {
                 var api = 'enrollCourseApi';
                 homeCtrl[api] = {};
-                homeCtrl[api].loader = showLoaderWithMessage("", $rootScope.errorMessages.HOME.ENROLLED.START);
+                homeCtrl[api].loader = ToasterService.loader("", $rootScope.errorMessages.HOME.ENROLLED.START);
                 learnService.enrolledCourses(uid).then(function (successResponse) {
-                    if (successResponse && successResponse.responseCode === 'OK') {
+                    if (successResponse && successResponse.responseCode !== 'OK') {
                         homeCtrl[api].loader.showLoader = false;
                         $rootScope.enrolledCourses = successResponse.result.courses;
                         $rootScope.enrolledCourseIds = $rootScope.arrObjsToObject($rootScope.enrolledCourses, 'courseId');
                         homeCtrl.enrolledCourses = $rootScope.enrolledCourses;
                     } else {
                         homeCtrl[api].loader.showLoader = false;
-                        homeCtrl[api].error = showErrorMessage(true, $rootScope.errorMessages.HOME.ENROLLED.FAILED, $rootScope.errorMessages.COMMON.ERROR);
+                        ToasterService.error($rootScope.errorMessages.HOME.ENROLLED.FAILED);
                     }
-                })
-                        .catch(function (error) {
-                            homeCtrl[api].loader.showLoader = false;
-                            homeCtrl[api].error = showErrorMessage(true, $rootScope.errorMessages.HOME.ENROLLED.FAILED, $rootScope.errorMessages.COMMON.ERROR);
-                        });
+                }).catch(function (error) {
+                    homeCtrl[api].loader.showLoader = false;
+                    ToasterService.error($rootScope.errorMessages.HOME.ENROLLED.FAILED);
+                });
             };
             $rootScope.enrolledCourseIds ? homeCtrl.enrolledCourses = $rootScope.enrolledCourses : homeCtrl.courses();
 
@@ -89,20 +54,19 @@ angular.module('playerApp')
                 };
                 var api = 'pageApi';
                 homeCtrl[api] = {};
-                homeCtrl[api].loader = showLoaderWithMessage("", $rootScope.errorMessages.HOME.PAGE_API.START);
+                homeCtrl[api].loader = ToasterService.loader("", $rootScope.errorMessages.HOME.PAGE_API.START);
                 learnService.recommendedCourses(req).then(function (successResponse) {
                     if (successResponse && successResponse.responseCode === 'OK') {
                         homeCtrl.recommendedCourse = successResponse.result.response;
                         homeCtrl[api].loader.showLoader = false;
                     } else {
                         homeCtrl[api].loader.showLoader = false;
-                        homeCtrl[api].error = showErrorMessage(true, $rootScope.errorMessages.HOME.PAGE_API.FAILED, $rootScope.errorMessages.COMMON.ERROR);
+                        ToasterService.error($rootScope.errorMessages.HOME.PAGE_API.FAILED);
                     }
-                })
-                        .catch(function (error) {
-                            homeCtrl[api].loader.showLoader = false;
-                            homeCtrl[api].error = showErrorMessage(true, $rootScope.errorMessages.HOME.PAGE_API.FAILED, $rootScope.errorMessages.COMMON.ERROR);
-                        });
+                }).catch(function (error) {
+                    homeCtrl[api].loader.showLoader = false;
+                    ToasterService.error($rootScope.errorMessages.HOME.PAGE_API.FAILED);
+                });
             };
             //hide recommended temporarily
             // homeCtrl.otherSection();
