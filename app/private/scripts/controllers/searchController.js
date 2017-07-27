ac53d5d5d1603f572c4d435df36bc2b7160b49c7
loganathan <loganathan.shanmugam@tarento.com>
2017-07-27 17:09:48 +0530

    Issue #1 fix: code refactor

--- app/private/scripts/controllers/searchController.js
+++ app/private/scripts/controllers/searchController.js
@@ -1,15 +1,15 @@
 'use strict';
 angular.module('playerApp')
-        .controller('SearchCtrl', function (config, sessionService, searchService, $scope, $timeout, $rootScope, $stateParams, $state, $location) {
-            var search = this;
-            search.initSearch = function () {
-                if ($rootScope.search) {
-                    $rootScope.search.searchResult = {};
-                    $rootScope.search.autosuggest_data = [];
-                }
-                $timeout(function () {
-                    $rootScope.$broadcast('initSearch', {});
-                }, 500);
-            };
+  .controller('SearchCtrl', function(config, sessionService, searchService, $scope, $timeout, $rootScope, $stateParams, $state, $location) {
+    var search = this;
+    search.initSearch = function() {
+      if ($rootScope.search) {
+        $rootScope.search.searchResult = {};
+        $rootScope.search.autosuggest_data = [];
+      }
+      $timeout(function() {
+        $rootScope.$broadcast('initSearch', {});
+      }, 500);
+    };
 
-        });
+  });
--- app/private/scripts/services/routeHelperService.js
+++ app/private/scripts/services/routeHelperService.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('playerApp')
-        .service('routeHelperService', function ($rootScope, $stateParams, $timeout) {
-            this.loadRouteConfig = function (stateName) {
+        .service('routeHelperService', function ($rootScope, $stateParams, $timeout, sessionService) {
+            this.loadRouteConfig = function (stateName, $stateParamsData) {
+                $stateParams = $stateParamsData;
                 var searchEnabledStates = ['Home', 'Courses', 'Resources', 'CourseNote', 'ContentNote', 'CourseContentNote', 'Toc', 'Player', 'Search', 'TocPlayer', 'PreviewCollection'];
-                var filterEnabledStates = ['Home', 'Courses', 'Resources','Search'];
-                var searchKey = {Home: 'All', Courses: 'Courses', Resources: 'Resources', CourseNote: 'Courses', ContentNote: 'Resources', CourseContentNote: 'Courses', Toc: 'Courses', Player: 'Resources', Search: $stateParams.type, TocPlayer: 'Courses', PreviewCollection: 'Resources'};
+                var filterEnabledStates = ['Home', 'Courses', 'Resources', 'Search'];
+                var searchKey = {Home: 'All', Courses: 'Courses', Resources: 'Resources', CourseNote: 'Courses', ContentNote: 'Resources', CourseContentNote: 'Courses', Toc: 'Courses', Player: 'Resources', Search: $stateParams?$stateParams.type:'All', TocPlayer: 'Courses', PreviewCollection: 'Resources'};
                 if (searchEnabledStates.indexOf(stateName) >= 0) {
                     $rootScope.isSearchPage = true;
                     $rootScope.searchKey = searchKey[stateName];
@@ -18,6 +19,203 @@ angular.module('playerApp')
                     $rootScope.showFilter = false;
                 }
                 this.clearSearchSettings();
+                this.setBreaCrumbs(stateName);
+            };
+            this.setBreaCrumbs = function (stateName) {
+                switch (stateName) {
+                    case 'Home':
+                        {
+                            $rootScope.breadCrumbsData = null;
+                        }
+                        break;
+                    case 'Courses':
+                        {
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    'name': 'Courses',
+                                    'link': 'learn'
+                                }];
+                        }
+                        break;
+                    case 'Resources':
+                        {
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    'name': 'Resources',
+                                    'link': 'resources'
+                                }];
+                        }
+                        break;
+                    case 'CourseNote':
+                        {
+
+                            var courseParams = sessionService.getSessionData('COURSE_PARAMS');
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Courses',
+                                    link: 'learn'
+                                }, {
+                                    'name': courseParams.courseName,
+                                    'link': '/toc/' + courseParams.tocId + '/' + courseParams.courseId + '/' + courseParams.lectureView
+                                }, {
+                                    name: 'Notes',
+                                    link: ''
+                                }];
+                        }
+                        break;
+                    case 'ContentNote':
+                        {
+                            $rootScope.breadCrumbsData = [{
+                                    'name': 'Resources',
+                                    'link': 'resources'
+                                }, {
+                                    'name': $stateParams.contentName,
+                                    link: 'player/' + $stateParams.contentId + '/' + $stateParams.contentName
+                                }, {
+                                    name: 'Notes',
+                                    link: ''
+                                }];
+
+                        }
+                        break;
+                    case 'CourseContentNote':
+                        {
+                            var courseParams = sessionService.getSessionData('COURSE_PARAMS');
+                            var courseLink = '/toc/' + courseParams.tocId + '/' + courseParams.courseId + '/' + courseParams.lectureView;
+                            var contentLink = courseLink + '/' + courseParams.contentId + '/' + courseParams.contentIndex;
+                            $rootScope.breadCrumbsData = [{
+                                    'name': courseParams.courseName,
+                                    'link': courseLink
+                                }, {
+                                    name: courseParams.contentName,
+                                    link: contentLink
+                                }, {
+                                    name: 'Notes',
+                                    link: ''
+                                }];
+                        }
+                        break;
+                    case 'Toc':
+                        {
+                            var courseParams = sessionService.getSessionData('COURSE_PARAMS');
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    name: 'Courses',
+                                    link: 'learn'
+                                }, {
+                                    'name': courseParams.courseName,
+                                    'link': ''
+                                }];
+                        }
+                        break;
+                    case 'Profile':
+                        {
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    name: 'Profile',
+                                    link: ''
+                                }];
+                        }
+                        break;
+                    case 'Player':
+                        {
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    'name': 'Resources',
+                                    'link': 'resources'
+                                }, {
+                                    'name': $stateParams.contentName,
+                                    link: ''
+                                }];
+                        }
+                        break;
+                    case 'Search':
+                        {
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    'name': 'Search',
+                                    'link': ''
+                                }];
+
+                        }
+                        break;
+                    case 'TocPlayer':
+                        {
+                            var courseParams = sessionService.getSessionData('COURSE_PARAMS');
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    name: 'Courses',
+                                    link: 'learn'
+                                }, {
+                                    'name': courseParams.courseName,
+                                    'link': ''
+                                }];
+                        }
+                        break;
+                    case 'WorkSpace':
+                        {
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    name: 'Profile',
+                                    link: 'profile'
+                                }, {
+                                    name: 'Workspace',
+                                    link: 'workspace/content/create'
+                                }];
+                        }
+                        break;
+                    case 'EditContent':
+                        {
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    name: 'Profile',
+                                    link: 'profile'
+                                }, {
+                                    name: 'Workspace',
+                                    link: 'workspace/content/create'
+                                }, {
+                                    name: 'Edit Content',
+                                    link: ''
+                                }];
+                        }
+                        break;
+                    case 'PreviewCollection':
+                        {
+                            $rootScope.breadCrumbsData = [{
+                                    name: 'Home',
+                                    link: 'home'
+                                }, {
+                                    'name': 'Resources',
+                                    'link': 'resources'
+                                }, {
+                                    'name': $stateParams['name'],
+                                    link: ''
+                                }];
+
+                        }
+                        break;
+                    default:
+                    {
+                        $rootScope.breadCrumbsData = null;
+                    }
+                }
             };
             this.clearSearchSettings = function () {
                 if ($rootScope.search) {
