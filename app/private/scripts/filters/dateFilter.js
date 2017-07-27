1c7e6f14a9af9dadace06a1b18c6a0788d1e3f0d
Poonam <poonam.sharma@compassitesinc.com>
2017-07-27 16:15:08 +0530

    Issue #1 fix :factory and filter eslint

--- app/private/scripts/filters/dateFilter.js
+++ app/private/scripts/filters/dateFilter.js
@@ -9,13 +9,13 @@
  * Filter in the studioApp.
  */
 angular.module('playerApp')
-    .filter('dateFilterWithMDY', function() {
-        return function(date) {
-            return moment(date).format("MMMM Do YYYY");
+    .filter('dateFilterWithMDY', function () {
+        return function (date) {
+            return moment(date).format('MMMM Do YYYY');
         };
     })
-    .filter('dateFilterXTimeAgo', function() {
-        return function(date) {
+    .filter('dateFilterXTimeAgo', function () {
+        return function (date) {
             var local = moment(date).local().format('YYYY-MM-DD HH:mm:ss');
             return moment(local).fromNow();
         };
