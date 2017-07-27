96c4f1e3fa3d0eb7c4afa461041f1793b9a5dd09
anuj <anujtech46>
2017-07-27 14:03:00 +0530

    Issue #1 fix: Apply eslint in search controller

--- app/private/scripts/controllers/searchController.js
+++ app/private/scripts/controllers/searchController.js
@@ -1,15 +1,16 @@
 'use strict';
-angular.module('playerApp')
-        .controller('SearchCtrl', function (config, sessionService, searchService, $scope, $timeout, $rootScope, $stateParams, $state, $location) {
-            var search = this;
-            search.initSearch = function () {
-                if ($rootScope.search) {
-                    $rootScope.search.searchResult = {};
-                    $rootScope.search.autosuggest_data = [];
-                }
-                $timeout(function () {
-                    $rootScope.$broadcast('initSearch', {});
-                }, 500);
-            };
 
-        });
+angular.module('playerApp')
+    .controller('SearchCtrl', function (config, sessionService, searchService,
+        $scope, $timeout, $rootScope) {
+        var search = this;
+        search.initSearch = function () {
+            if ($rootScope.search) {
+                $rootScope.search.searchResult = {};
+                $rootScope.search.autosuggest_data = [];
+            }
+            $timeout(function () {
+                $rootScope.$broadcast('initSearch', {});
+            }, 500);
+        };
+    });
