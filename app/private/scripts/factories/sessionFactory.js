cb2fa7e80a54d01a43a7220d78682760071ede1d
Poonam <poonam.sharma@compassitesinc.com>
2017-07-27 16:15:08 +0530

    Issue #1 fix :factory and filter eslint

--- app/private/scripts/factories/sessionFactory.js
+++ app/private/scripts/factories/sessionFactory.js
@@ -1,17 +1,19 @@
 'use strict';
-angular.module('playerApp').factory('sessionService', ['$window', function ($window) {
-        var sessionData = sessionData || JSON.parse($window.sessionStorage.getItem("sbConfig") || '{}');
-        return {
 
-            getSessionData: function (key) {
-                return  sessionData[key];
+angular.module('playerApp')
+.factory('sessionService', ['$window', function ($window) {
+    var sessionData = sessionData // eslint-disable-line
+    || JSON.parse($window.sessionStorage.getItem('sbConfig') || '{}');
+    return {
 
-            },
-            setSessionData: function (key, value) {
-                sessionData[key] = value;
-                $window.sessionStorage.setItem("sbConfig", JSON.stringify(sessionData));
-
-            }
-
+        getSessionData: function (key) {
+            return sessionData[key];
+        },
+        setSessionData: function (key, value) {
+            sessionData[key] = value;
+            $window.sessionStorage
+            .setItem('sbConfig', JSON.stringify(sessionData));
         }
-    }]);
+
+    };
+}]);
