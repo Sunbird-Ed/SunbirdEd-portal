fc3b8843877b6ecd95ee0ef0c05a3ff6f2ef64be
Poonam <poonam.sharma@compassitesinc.com>
2017-07-27 16:15:08 +0530

    Issue #1 fix :factory and filter eslint

--- app/private/scripts/filters/noteListFilter.js
+++ app/private/scripts/filters/noteListFilter.js
@@ -9,16 +9,16 @@
  * Filter in the playerApp.
  */
 angular.module('playerApp')
-    .filter('noteListFilter', function() {
-        return function(noteList, searchText) {
+    .filter('noteListFilter', function () {
+        return function (noteList, searchText) {
             if (searchText) {
                 var lowerCaseQuery = angular.lowercase(searchText);
-                return noteList.filter(function(list) {
-                    return angular.lowercase(list.title).includes(lowerCaseQuery) || angular.lowercase(list.note).includes(lowerCaseQuery);
+                return noteList.filter(function (list) {
+                    return angular.lowercase(list.title)
+                    .includes(lowerCaseQuery)
+                    || angular.lowercase(list.note).includes(lowerCaseQuery);
                 });
-            } else {
-                return noteList;
             }
-
+            return noteList;
         };
     });
