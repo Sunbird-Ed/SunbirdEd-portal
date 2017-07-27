3c3eda84fd2177eed6798b4ad2cacbc2cb468668
Poonam <poonam.sharma@compassitesinc.com>
2017-07-27 15:41:08 +0530

    Issue #1 fix :directive fixed , search applyScript not done

--- app/private/scripts/directives/permissionsDirective.js
+++ app/private/scripts/directives/permissionsDirective.js
@@ -6,22 +6,24 @@
  * @description
  * # permissionsDirective
  */
-angular.module('playerApp').directive('sbPermissions', function (permissionsService) {
-
+angular.module('playerApp')
+.directive('sbPermissions', function (permissionsService) {
     return {
         restrict: 'A',
         scope: {},
         link: function (scope, element, attrs) {
-         if (attrs.permissionOnly) {
-             if(!permissionsService.checkRolesPermissions(attrs.permissionOnly.split(','), true)){
-                element.remove();
-             }
-         }
-         if (attrs.permissionExcept) {
-             if(permissionsService.checkRolesPermissions(attrs.permissionExcept.split(','), false)){
-                element.remove();
-             }
-         }
+            if (attrs.permissionOnly) {
+                if (!permissionsService.checkRolesPermissions(
+                    attrs.permissionOnly.split(','), true)) {
+                    element.remove();
+                }
+            }
+            if (attrs.permissionExcept) {
+                if (permissionsService.checkRolesPermissions(
+                    attrs.permissionExcept.split(','), false)) {
+                    element.remove();
+                }
+            }
         }
     };
 });
