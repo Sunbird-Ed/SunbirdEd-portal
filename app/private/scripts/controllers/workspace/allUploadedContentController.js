86d9c0c8fd15c00091440596d97178103d4866e2
anuj <anujtech46>
2017-07-28 18:44:48 +0530

    Issue #1 fix: Refactor AllUploadedContentController and html

--- app/private/scripts/controllers/workspace/allUploadedContentController.js
+++ app/private/scripts/controllers/workspace/allUploadedContentController.js
@@ -3,75 +3,63 @@
 /**
  * @ngdoc function
  * @name playerApp.controller:AllUploadedContentController
+ * @author Anuj Gupta
  * @description
- * # CreatecontentCtrl
+ * # AllUploadedContentController
  * Controller of the playerApp
  */
 angular.module('playerApp')
-    .controller('AllUploadedContentController', function (contentService,
-  searchService, config, $rootScope, $state, toasterService) {
-        var allUploadedContent = this;
-        allUploadedContent.userId = $rootScope.userId;
-        allUploadedContent.contentStatus = ['Draft'];
-        allUploadedContent.contentMimeType = [
-            'application/vnd.ekstep.html-archive',
-            'video/youtube',
-            'video/mp4', 'application/pdf'];
+    .controller('AllUploadedContentController', ['contentService', 'searchService', 'config',
+        '$rootScope', '$state', 'toasterService', function (contentService, searchService, config,
+            $rootScope, $state, toasterService) {
+            var allUploadedContent = this;
+            allUploadedContent.userId = $rootScope.userId;
+            allUploadedContent.contentStatus = ['Draft'];
+            allUploadedContent.contentMimeType = ['application/vnd.ekstep.html-archive',
+                'video/youtube', 'video/mp4', 'application/pdf'];
+            allUploadedContent.sortBy = 'desc';
+            allUploadedContent.allUploadedContentData = [];
 
-        function getUploadedContent() {
-            allUploadedContent.loader = toasterService
-			.loader('', $rootScope.errorMessages.WORKSPACE.ALL_UPLOADED.START);
+            allUploadedContent.getAllUploadedContent = function () {
+                allUploadedContent.loader = toasterService.loader('', $rootScope.errorMessages
+                                                            .WORKSPACE.ALL_UPLOADED.START);
 
-            var request = {
-                filters: {
-                    status: allUploadedContent.contentStatus,
-                    createdBy: allUploadedContent.userId,
-                    mimeType: allUploadedContent.contentMimeType
-                },
-                sort_by: {
-                    lastUpdatedOn: 'desc'
-                }
+                var request = {
+                    filters: {
+                        status: allUploadedContent.contentStatus,
+                        createdBy: allUploadedContent.userId,
+                        mimeType: allUploadedContent.contentMimeType
+                    },
+                    sort_by: {
+                        lastUpdatedOn: allUploadedContent.sortBy
+                    }
+                };
+
+                searchService.search(request).then(function (res) {
+                    if (res && res.responseCode === 'OK') {
+                        allUploadedContent.loader.showLoader = false;
+                        allUploadedContent.allUploadedContentData = res.result.content;
+                    } else {
+                        allUploadedContent.loader.showLoader = false;
+                        toasterService.error($rootScope.errorMessages.WORKSPACE.ALL_UPLOADED
+                                                                        .FAILED);
+                    }
+                }).catch(function () {
+                    allUploadedContent.loader.showLoader = false;
+                    toasterService.error($rootScope.errorMessages.WORKSPACE.ALL_UPLOADED.FAILED);
+                });
             };
 
-            allUploadedContent.allUploadedContentData = [];
-            searchService.search(request).then(function (res) {
-                if (res && res.responseCode === 'OK') {
-                    allUploadedContent.loader.showLoader = false;
-                    allUploadedContent.allUploadedContentData
-                    = res.result.content;
-                    if (res.result.count === 0) {
-                        allUploadedContent.zeroContentMessage = $rootScope
-										.errorMessages
-								   	.WORKSPACE.ALL_UPLOADED.NO_CONTENT;
-                    }
+            allUploadedContent.editContent = function (item) {
+                if (item.mimeType === 'application/vnd.ekstep.content-collection') {
+                    $state.go('CollectionEditor', {
+                        contentId: item.identifier,
+                        type: item.contentType,
+                        state: 'WorkSpace.AllUploadedContent'
+                    });
                 } else {
-                    allUploadedContent.loader.showLoader = false;
-                    toasterService.error($rootScope
-									.errorMessages
-									.WORKSPACE.ALL_UPLOADED.FAILED);
+                    var params = { contentId: item.identifier };
+                    $state.go('EditContent', params);
                 }
-            })
-            .catch(function () {
-                allUploadedContent.loader.showLoader = false;
-                toasterService.error($rootScope
-									.errorMessages
-									.WORKSPACE.ALL_UPLOADED.FAILED);
-            });
-        }
-
-        allUploadedContent.initializeData = function () {
-            getUploadedContent();
-        };
-
-        allUploadedContent.openEditForm = function (item) {
-            if (item.mimeType === 'application/vnd.ekstep.content-collection') {
-                $state.go('CollectionEditor', {
-							 contentId: item.identifier,
-								 type: item.contentType,
-                    state: 'WorkSpace.AllUploadedContent' });
-            } else {
-                var params = { contentId: item.identifier };
-                $state.go('EditContent', params);
-            }
-        };
-    });
+            };
+        }]);
