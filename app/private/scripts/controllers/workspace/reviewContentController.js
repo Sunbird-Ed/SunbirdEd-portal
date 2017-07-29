29a7565824de16b185c74d83b60c6e3b5f803656
anuj <anujtech46>
2017-07-29 23:14:25 +0530

    Issue #1 fix: Refactor upForReview, published and review controller and view

--- app/private/scripts/controllers/workspace/reviewContentController.js
+++ app/private/scripts/controllers/workspace/reviewContentController.js
@@ -4,64 +4,60 @@
  * @ngdoc function
  * @name playerApp.controller:ReviewContentController
  * @description
- * # CreatecontentCtrl
+ * @author Anuj Gupta
+ * # ReviewContentController
  * Controller of the playerApp
  */
 angular.module('playerApp')
-    .controller('ReviewContentController', function (contentService,
-         searchService, config, $rootScope, $scope, $state, toasterService) {
-        var reviewContent = this;
-        reviewContent.userId = $rootScope.userId;
-        $scope.contentPlayer = { isContentPlayerEnabled: false };
+    .controller('ReviewContentController', ['contentService', 'searchService', 'config',
+        '$rootScope', '$scope', '$state', 'toasterService', function (contentService, searchService,
+        config, $rootScope, $scope, $state, toasterService) {
+            var reviewContent = this;
+            reviewContent.userId = $rootScope.userId;
+            $scope.contentPlayer = { isContentPlayerEnabled: false };
+            reviewContent.status = ['Review'];
+            reviewContent.sortBy = 'desc';
 
-        function getReviewContent() {
-            reviewContent.loader = toasterService
-            .loader('', $rootScope.errorMessages.WORKSPACE.REVIEW.START);
+            reviewContent.getReviewContent = function () {
+                reviewContent.loader = toasterService.loader('', $rootScope.errorMessages.WORKSPACE
+                                        .REVIEW.START);
+                var request = {
+                    filters: {
+                        status: reviewContent.status,
+                        createdBy: reviewContent.userId
+                    },
+                    sort_by: {
+                        lastUpdatedOn: reviewContent.sortBy
+                    }
+                };
 
-            var request = {
-                filters: {
-                    status: ['Review'],
-                    createdBy: reviewContent.userId
-                },
-                sort_by: {
-                    lastUpdatedOn: 'desc'
+                searchService.search(request).then(function (res) {
+                    if (res && res.responseCode === 'OK') {
+                        reviewContent.loader.showLoader = false;
+                        reviewContent.reviewContentData = res.result.content;
+                    } else {
+                        reviewContent.loader.showLoader = false;
+                        toasterService.error($rootScope.errorMessages.WORKSPACE.REVIEW.FAILED);
+                    }
+                }).catch(function () {
+                    reviewContent.loader.showLoader = false;
+                    toasterService.error($rootScope.errorMessages.WORKSPACE.REVIEW.FAILED);
+                });
+            };
+
+            reviewContent.openContentPlayer = function (item) {
+                if (item.mimeType === 'application/vnd.ekstep.content-collection') {
+                    $state.go('CollectionEditor', {
+                        contentId: item.identifier,
+                        type: item.contentType,
+                        state: 'WorkSpace.ReviewContent'
+                    });
+                } else {
+                    var params = {
+                        contentId: item.identifier,
+                        backState: $state.current.name
+                    };
+                    $state.go('PreviewContent', params);
                 }
             };
-            reviewContent.reviewContentData = [];
-            searchService.search(request).then(function (res) {
-                if (res && res.responseCode === 'OK') {
-                    reviewContent.loader.showLoader = false;
-                    reviewContent.reviewContentData = res.result.content;
-                    if (res.result.count === 0) {
-                        reviewContent.zeroContentMessage
-                        = $rootScope.errorMessages.WORKSPACE.REVIEW.NO_CONTENT;
-                    }
-                } else {
-                    reviewContent.loader.showLoader = false;
-                    toasterService
-                    .error($rootScope.errorMessages.WORKSPACE.REVIEW.FAILED);
-                }
-            })
-                .catch(function () {
-                    reviewContent.loader.showLoader = false;
-                    toasterService
-                    .error($rootScope.errorMessages.WORKSPACE.REVIEW.FAILED);
-                });
-        }
-
-        reviewContent.initializeData = function () {
-            getReviewContent();
-        };
-
-        reviewContent.openContentPlayer = function (item) {
-            if (item.mimeType === 'application/vnd.ekstep.content-collection') {
-                $state.go('CollectionEditor', { contentId: item.identifier,
-                    type: item.contentType,
-                    state: 'WorkSpace.ReviewContent' });
-            } else {
-                var params = { contentId: item.identifier,
-                    backState: $state.current.name };
-                $state.go('PreviewContent', params);
-            }
-        };
-    });
+        }]);
