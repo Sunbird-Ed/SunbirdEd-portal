46b692d0cce854676aa05774851a79145380d457
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-08-02 16:08:57 +0530

    Issue #1 fix: batch initial commit

---
+++ app/private/scripts/services/batchService.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name playerApp.batchService
+ * @description
+ * # batchService
+ * Service in the playerApp.
+ */
+
+angular.module('playerApp')
+    .service('batchService', ['httpServiceJava', 'config', function(httpServiceJava, config){
+        this.create = function (req) {
+            return httpServiceJava.post(config.URL.BATCH.CREATE, req);
+        };
+
+        this.update = function (req) {
+            return httpServiceJava.patch(config.URL.BATCH.UPDATE + '/' + req.batchId, req);
+        };
+
+        this.addUsers = function (req) {
+            return httpServiceJava.post(config.URL.BATCH.ADD_USERS + '/' + req.batchId;, req);
+        };
+
+        this.removeUsers = function (req) {
+            return httpServiceJava.remove(config.URL.BATCH.DELETE + '/' + req.batchId, req);
+        };
+
+        this.getBatchDetails = function (req) {
+            return httpServiceJava.get(config.URL.BATCH.GET_DETAILS + '/' + req.batchId, req);
+        };
+
+        this.getAllBatchs = function (req) {
+            return httpServiceJava.get(config.URL.BATCH.GET_BATCHS, req);
+        };
+    }]);
