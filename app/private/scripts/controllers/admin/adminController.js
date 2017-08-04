10d4c6b5dcc61ae749cb872d3ed06306bed21d76
Poonam <poonam.sharma@compassitesinc.com>
2017-08-04 17:09:51 +0530

    Issue #407 feat:user role

--- app/private/scripts/controllers/admin/adminController.js
+++ app/private/scripts/controllers/admin/adminController.js
@@ -15,16 +15,84 @@ angular.module('playerApp')
         '$state',
         'config',
         '$rootScope',
-        '$scope', function (adminService, $timeout, $state, config, $rootScope, $scope) {
+        '$scope',
+        'contentService',
+        function (adminService, $timeout, $state, config, $rootScope, $scope, contentService) {
             console.log('inside conroller', $scope.users);
 
             var admin = this;
             admin.userRoles = config.USER_ROLES;
             admin.searchedUsers = $scope.users;
             admin.userName = '';
+
+
             admin.exportUsers = function () {
                 alasql('SELECT firstName AS Name, email AS Email ' +
                          'INTO CSV(\'Users.csv\',{headers:true}) FROM ?', [admin.searchedUsers]);
+            };
+
+            admin.openImageBrowser = function () {
+                $('#uploadCSV').click();
+            };
+            admin.validateFile = function (files) {
+                var fd = new FormData();
+                var reader = new FileReader();
+                if (files[0] && files[0].name.match(/.(csv|xlsx)$/i)
+                    && files[0].size < 4000000) {
+                    fd.append('file', files[0]);
+                    reader.onload = function () {
+                        profile.uploadFile();
+                    };
+                    reader.readAsDataURL(files[0]);
+                    admin.fileToUpload = fd;
+                } else {
+                    toasterService.warning('');
+                }
+            };
+            admin.uploadFile = function () {
+                    //  profile.loader = toasterService.loader('', apiMessages.SUCCESS.editingProfile);
+                contentService.uploadMedia(admin.fileToUpload).then(function (res) {
+                    if (res && res.responseCode === 'OK') {
+                        // admin.createNewUsers(res.result.url);
+                    } else {
+                    //     profile.loader.showLoader = false;
+                    //     profile.error = toasterService
+                    // .error(apiMessages.ERROR.update);
+                    }
+                }).catch(function () {
+                    // profile.loader.showLoader = false;
+                    // profile.error = toasterService.error(apiMessages.ERROR.update);
+                });
+            };
+            // admin.createNewUsers = function (file) {
+            //     var newUsersReq = {
+            //         user: file,
+            //         organisationId: '1213', // valid or
+            //         channel: 'sfs', // valid and
+            //         provider: 'dsfs'// vald
+            //     };
+            //     adminService.createUsers.then(function (res) {
+
+            //     });
+            // };
+            admin.showModal = function () {
+                $('#changeUserRoles').modal('show');
+            };
+            admin.modalInit = function (organisations) {
+                admin.userOrgsCopy = angular.copy(organisations);
+                $timeout(function () {
+                    $('#userOrgs').dropdown();
+                }, 1000);
+                $('.roleChckbox').checkbox();
+            };
+
+            admin.modal = function (role, userRoles) {
+                admin.selectedOrgUserRoles = userRoles.includes(role)
+                                            ? admin.selectedOrgUserRoles.pop(role)
+                                            : admin.selectedOrgUserRoles.push(role);
+            };
+            admin.isUserRole = function (role, list) {
+                return list.includes(role);
             };
             admin.deleteUser = function (userId) {
                 var removeReq = {
