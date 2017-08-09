1894c7b22e354670df61d1b70a66e76bc091ac1f
loganathan <loganathan.shanmugam@tarento.com>
2017-08-09 17:50:21 +0530

    Issue #1 feat : concept map in search filters

--- app/private/scripts/directives/conceptPicker.js
+++ app/private/scripts/directives/conceptPicker.js
@@ -17,52 +17,56 @@ angular.module('playerApp').directive('conceptPicker', function () {
             $scope.loadConceptTree = function () {
                 $scope.conceptLoader = toasterService.loader(''
                 , $rootScope.errorMessages.WORKSPACE.CONCEPTS.START);
-                $scope.concepts = [];
-                $scope.getConcept(0, 200, function (err) {
-                    if (err === true) {
-                        toasterService.error($rootScope.errorMessages.WORKSPACE.GET.FAILED);
-                    } else {
-                        var domains = [];
-                        var req = {
-                            filters: {
-                                objectType: ['Dimension', 'Domain']
-                            },
-                            params: {
-                                cid: '12'
-                            }
+                if ($rootScope.concepts) {
+                    $scope.loadDomains(false, $rootScope.concepts);
+                } else {
+                    $rootScope.getConcept(0, 200, $scope.loadDomains);
+                }
+            };
+            $scope.loadDomains = function (err, conceptArr) {
+                if (err && err === true) {
+                    toasterService.error($rootScope.errorMessages.WORKSPACE.GET.FAILED);
+                } else {
+                    $rootScope.concepts = conceptArr;
+                    var domains = [];
+                    var req = {
+                        filters: {
+                            objectType: ['Dimension', 'Domain']
+                        },
+                        params: {
+                            cid: '12'
+                        }
 
-                        };
+                    };
                         /** Get domains and dimensions data* */
-                        searchService.search(req).then(function (resp) {
-                            if (resp.result && _.isArray(resp.result.domains)) {
-                                _.forEach(resp.result.domains, function (value) {
-                                    var domain = {};
-                                    domain.id = value.identifier;
-                                    domain.name = value.name;
-                                    var domainChild = [];
+                    searchService.search(req).then(function (resp) {
+                        if (resp.result && _.isArray(resp.result.domains)) {
+                            _.forEach(resp.result.domains, function (value) {
+                                var domain = {};
+                                domain.id = value.identifier;
+                                domain.name = value.name;
+                                var domainChild = [];
                                     /** Get domain child* */
-                                    _.forEach($scope.getChild(value.identifier
+                                _.forEach($scope.getChild(value.identifier
                                         , resp.result.dimensions),
                                      function (val) {
                                          var dimension = {};
                                          dimension.id = val.id;
                                          dimension.name = val.name;
                                         /** Get dimension child* */
-                                         dimension.nodes = $scope.getChild(val.id, $scope.concepts);
+                                         dimension.nodes = $scope.getChild(val.id, $rootScope.concepts);
                                          domainChild.push(dimension);
                                      });
-                                    domain.nodes = domainChild;
-                                    domains.push(domain);
-                                });
-                                $rootScope.conceptData = domains;
-                                $scope.conceptLoader.showLoader = false;
-                                $scope.initConceptBrowser();
-                            }
-                        });
-                    }
-                });
+                                domain.nodes = domainChild;
+                                domains.push(domain);
+                            });
+                            $rootScope.conceptData = domains;
+                            $scope.conceptLoader.showLoader = false;
+                            $scope.initConceptBrowser();
+                        }
+                    });
+                }
             };
-
             $scope.initConceptBrowser = function () {
                 $scope.selectedConcepts = $scope.selectedConcepts || [];
                 $scope.contentConcepts = _.map($scope.selectedConcepts, 'identifier');
@@ -117,31 +121,6 @@ angular.module('playerApp').directive('conceptPicker', function () {
                 return _.uniqBy(childArray, 'id');
             };
 
-            $scope.getConcept = function (offset, limit, callback) {
-                var req = {
-                    filters: {
-                        objectType: ['Concept']
-                    },
-                    offset: offset,
-                    limit: limit
-                };
-                searchService.search(req).then(function (res) {
-                    if (res.result && res.result && _.isArray(res.result.concepts)) {
-                        _.forEach(res.result.concepts, function (value) {
-                            $scope.concepts.push(value);
-                        });
-                        if ((res.result.count > offset) && res.result.count >= (offset + limit)) {
-                            offset += limit;
-                            $scope.getConcept(offset, limit, callback);
-                        } else {
-                            callback(false);
-                        }
-                    }
-                })
-                        .catch(function (err) {
-                            callback(true);
-                        });
-            };
             if (!$rootScope.conceptData) {
                 $scope.loadConceptTree();
             } else {
