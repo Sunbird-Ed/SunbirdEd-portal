332207026eb6c610690d83bb4dca009c9ad5b8cb
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-08-10 21:48:07 +0530

    Issue #1 fix: Batch status dropdown issue fixed

--- app/private/scripts/directives/batchCardDirective.js
+++ app/private/scripts/directives/batchCardDirective.js
@@ -13,8 +13,7 @@ angular.module('playerApp')
             restrict: 'E',
             scope: {
                 showbatchcard: '=',
-                courseid: '=',
-                status: '='
+                courseid: '='
             },
             link: function (scope, element, attrs, batch) {
                 $('#batchStatusOptions').dropdown('set selected ', batch.status);
