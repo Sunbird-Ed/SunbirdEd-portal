5fa83dd2026aebae4b473cc675a788ba585264bf
loganathan <loganathan.shanmugam@tarento.com>
2017-08-11 12:34:52 +0530

    Issue #1 fix : concept meta fix

--- app/private/scripts/services/playerTelemetryUtilsService.js
+++ app/private/scripts/services/playerTelemetryUtilsService.js
@@ -17,10 +17,15 @@ angular.module('playerApp')
                 org.sunbird.portal.eventManager.dispatchEvent("sunbird:telemetry:navigate", data);
             };
             this.init = function (data) {
-                var _instance = {
-                    correlationData: [{"id": $stateParams.courseId || data.contentId, "type": "course"}],
+                var _instance = {                   
                     user: {"sid": "", "did": "", "uid": $rootScope.userId},
                     gameData: {"id": "org.sunbird.player", "ver": "1.0"}
+                }
+                if($stateParams.courseId){
+                     _instance.correlationData= [{"id": $stateParams.courseId || data.contentId, "type": "course"}];
+                }
+                else{
+                     _instance.correlationData= [{"id": data.contentId, "type": "content"}];
                 }
                 var courseId = $stateParams.courseId || data.contentId;
                 if(_.isUndefined(courseId)){
