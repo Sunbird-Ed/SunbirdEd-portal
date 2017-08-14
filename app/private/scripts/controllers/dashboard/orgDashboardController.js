49ff7bf7c7481df312b95878746c071c7d333cfd
Sourav <sourav_d@techjoomla.com>
2017-08-14 16:34:20 +0530

    Issue #1 feat:Organisation creation admin dashboard

--- app/private/scripts/controllers/dashboard/orgDashboardController.js
+++ app/private/scripts/controllers/dashboard/orgDashboardController.js
@@ -13,7 +13,7 @@ angular.module('playerApp')
     'dashboardService', '$timeout', '$state', '$stateParams', 'toasterService',
     function($rootScope, $scope, dashboardService, $timeout, $state, $stateParams, toasterService) {
       var dashboardData = this;
-      dashboardData.height = 120;
+      dashboardData.height = 110;
       dashboardData.showLoader = true;
       dashboardData.showDataDiv = false;
       dashboardData.datasetPreviousValue = 'creation';
@@ -57,6 +57,10 @@ angular.module('playerApp')
                   if (key === 'org.creation.content[@status=review].count') {
                     dashboardData.series.push(numericData.value + ' IN REVIEW');
                   }
+                  
+                  if (key === 'org.creation.content.count'){
+					dashboardData.series.push(numericData.value + ' CREATED');
+				  }
                 });
 
                 angular.forEach(apiResponse.result.series, function(bucketData, key) {
@@ -83,6 +87,14 @@ angular.module('playerApp')
                       publishedArray.push(bucketValue.value);
                     })
                     dashboardData.data.push(publishedArray);
+                  }
+                  
+                  if (key === 'org.creation.content.created_on.count') {
+                    var createdArray = new Array();
+                    angular.forEach(bucketData.buckets, function(bucketValue, bucketKey) {
+                      createdArray.push(bucketValue.value);
+                    })
+                    dashboardData.data.push(createdArray);
                   }
                 });
 
@@ -131,7 +143,6 @@ angular.module('playerApp')
 
                 dashboardData.colors = dashboardService.getChartColors(dashboardData.datasetPreviousValue);
               }
-              dashboardData.apiResponse = apiResponse;
               dashboardData.showDataDiv = true;
             } else {
               toasterService.error(apiResponse.params.errmsg);
@@ -144,7 +155,6 @@ angular.module('playerApp')
           .finally(function() {
             // Hide loading spinner whether our call succeeded or failed.
             dashboardData.showLoader = false;
-            // dashboardData.showDataDiv = true;
           });
       }
       $('#dropdownMenu').dropdown();
