36f4d511ce6a49d6dc2a57fcc1879f3f628c2d16
loganathan <loganathan.shanmugam@tarento.com>
2017-08-18 18:05:37 +0530

    Issue #1 fix : organization changes

--- app/public/scripts/org.js
+++ app/public/scripts/org.js
@@ -7,7 +7,7 @@ var openModal = function () {
         document.getElementsByTagName('body')[0].appendChild(modal);
         var styleNode = document.createElement('style');
         styleNode.type = "text/css";
-        var cssText=".modal{display:none;position:fixed;z-index:1;padding-top:100px;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:#000;background-color:rgba(0,0,0,.4)}.modal-content{position:relative;background-color:#fefefe;margin:auto;padding:0;border:1px solid #888;width:80%;box-shadow:0 4px 8px 0 rgba(0,0,0,.2),0 6px 20px 0 rgba(0,0,0,.19);-webkit-animation-name:animatetop;-webkit-animation-duration:.4s;animation-name:animatetop;animation-duration:.4sborder-radius:6px;}@-webkit-keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}@keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}.close{color:#333;float:right;font-size:28px;font-weight:700}.close:focus,.close:hover{color:#000;text-decoration:none;cursor:pointer}.modal-footer,.modal-header{padding:2px 16px;background-color:#fff;color:#222;border-radius:7px;;}.modal-body{padding:25px 16px 25px;}table{font-family:arial,sans-serif;border-collapse:collapse;width:100%;}td,th{border:1px solid #dddddd;text-align:left;padding:8px;}";
+        var cssText = ".modal{display:none;position:fixed;z-index:1;padding-top:100px;left:0;top:0;width:100%;height:100%;overflow:auto;background-color:#000;background-color:rgba(0,0,0,.4)}.modal-content{position:relative;background-color:#fefefe;margin:auto;padding:0;border:1px solid #888;width:80%;box-shadow:0 4px 8px 0 rgba(0,0,0,.2),0 6px 20px 0 rgba(0,0,0,.19);-webkit-animation-name:animatetop;-webkit-animation-duration:.4s;animation-name:animatetop;animation-duration:.4sborder-radius:6px;}@-webkit-keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}@keyframes animatetop{from{top:-300px;opacity:0}to{top:0;opacity:1}}.close{color:#333;float:right;font-size:28px;font-weight:700}.close:focus,.close:hover{color:#000;text-decoration:none;cursor:pointer}.modal-footer,.modal-header{padding:2px 16px;background-color:#fff;color:#222;border-radius:7px;;}.modal-body{padding:25px 16px 25px;}table{font-family:arial,sans-serif;border-collapse:collapse;width:100%;}td,th{border:1px solid #dddddd;text-align:left;padding:8px;}";
         // browser detection (based on prototype.js)
         if (!!(window.attachEvent && !window.opera)) {
             styleNode.styleSheet.cssText = cssText;
@@ -15,13 +15,13 @@ var openModal = function () {
             var styleText = document.createTextNode(cssText);
             styleNode.appendChild(styleText);
         }
-        document.getElementsByTagName('head')[0].appendChild(styleNode);       
+        document.getElementsByTagName('head')[0].appendChild(styleNode);
         modal.innerHTML = '<div class="modal-content"> <div class="modal-header"><span class="close" onclick="closeModal()">×</span><h2>Organizations List</h2></div>' +
-                '<div class="modal-body"><table id="orgList" border="1"><thead><tr>' +
+                '<div class="modal-body"><p id="org-error"></p><div id="org-loader" align="center"></div><table id="orgList" border="1"><thead><tr>' +
                 '<th>Name</th><th>Email</th><th>Phone</th></tr></thead><tbody></tbody>' +
                 '</table></div><div class="modal-footer"></div></div>';
         modal.setAttribute("class", "modal");
-        
+
     }
     modal.style.display = "block";
     loadData();
@@ -36,39 +36,54 @@ var openModal = function () {
 };
 
 var emptyTable = function (table) {
-    table.getElementsByTagName('tbody')[0].innerHTML = '<tr><td col-span="3">Loading...</td></tr>';
+    table.getElementsByTagName('tbody')[0].innerHTML = '';
 }
 var loadData = function () {
     var table = document.getElementById('orgList');
-    var error = document.createElement('p');
+    var error = document.getElementById('org-error');
+    var loader = document.getElementById('org-loader');
+    error.style.display = 'none';
+    loader.innerHTML = '<h5>Loading Organizations.....</h5>';
     error.textContent = 'Unable to Load Organizations';
     error.style.color = 'red';
     emptyTable(table);
+    table.style.display = 'none';
+    loader.style.display = 'block';
     var client = new HttpClient();
     client.get('/api/public/service/orgs', function (res) {
         res = JSON.parse(res);
         if (res && res.responseCode == "OK") {
             table.style.display = 'table';
             var data = res.result.response.content;
-            var tblHtml = '';
-            data.forEach(function (item, index) {
-                item.orgName = item.orgName || '';
-                item.contactDetails.email = item.contactDetails.email || '';
-                item.contactDetails.phone = item.contactDetails.phone || '';
-                if (item.orgName != '') {
-                    tblHtml += '<tr>' + '<td>' + item.orgName + '</td><td>' + item.contactDetails.email + '</td><td>' + item.contactDetails.phone + '</td></tr>';
-                }
-            });
-            table.getElementsByTagName('tbody')[0].innerHTML = tblHtml;
+            if (data.length > 0)
+            {
+                var tblHtml = '';
+                data.forEach(function (item, index) {
+                    item.orgName = item.orgName || '';
+                    item.contactDetails.email = item.contactDetails.email || '';
+                    item.contactDetails.phone = item.contactDetails.phone || '';
+                    if (item.orgName != '') {
+                        tblHtml += '<tr>' + '<td>' + item.orgName + '</td><td>' + item.contactDetails.email + '</td><td>' + item.contactDetails.phone + '</td></tr>';
+                    }
+                });
+                table.getElementsByTagName('tbody')[0].innerHTML = tblHtml;
+                loader.style.display = 'none';
+                table.style.display = 'block';
+            } else {
+                loader.innerHTML = '<h5>No Organisations found</h5>';
+                table.style.display = 'none';
+            }
+
         } else {
             table.style.display = 'none';
-            insertAfter(error, table)
+            loader.style.display = 'none';
+            error.style.display = 'block';
         }
     });
 }
 
 var closeModal = function () {
-     var modal = document.getElementById('org-list-model');
+    var modal = document.getElementById('org-list-model');
     modal.style.display = "none";
 }
 
