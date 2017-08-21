1d63fb00c15591f3c28d5e2447aa3ada1c8fa287
anuj <anujtech46>
2017-08-21 11:08:43 +0530

    Issue #1 fix: Merge pull

--- app/private/scripts/controllers/batch/batchUpdateController.js
+++ app/private/scripts/controllers/batch/batchUpdateController.js
@@ -59,6 +59,7 @@ angular.module('playerApp')
                     }
                 })
                 $timeout(function () {
+                     
                     $('#users').dropdown();
                     $('#mentors').dropdown();
                     if(batchUpdate.batchData.enrollmentType == 'open'){
@@ -67,8 +68,9 @@ angular.module('playerApp')
                         $('input:radio[name="enrollmentType"]').filter('[value="invite-only"]').attr('checked', true);
                     }
                     $('.ui.calendar').calendar({refresh: true});
+                    var today = new Date();
                     var startDate = new Date(batchUpdate.batchData.startDate),
-                        currentDate = new Date();
+                        currentDate =  new Date(today.setDate(today.getDate() + 1));
                     if(currentDate < startDate){
                         startDate = currentDate;
                     }
@@ -114,11 +116,16 @@ angular.module('playerApp')
                             });
                             $('.ui.calendar #startDate').val(batchUpdate.batchData.startDate);
                             $('.ui.calendar #endDate').val(batchUpdate.batchData.endDate);
+                            $(".ui.modal.transition.hidden").remove();
                         },
                         onHide: function () {
                             var previousUrl = JSON.parse(window.localStorage.getItem('previousURl'));
+                            if($stateParams.name != previousUrl.name){
                             $state.go(previousUrl.name, previousUrl.params);
-                        }
+                        }else{
+                           $state.go('Toc', {courseId:batchUpdate.batchData.courseId,lectureView:'yes'});
+                       }
+                     }
                     }).modal("show");
                 }, 10);
             };
@@ -162,7 +169,7 @@ angular.module('playerApp')
             batchUpdate.hideUpdateBatchModal = function () {
                 $('#updateBatchModal').modal('hide');
                 $('#updateBatchModal').modal('hide others');
-                $('#updateBatchModal').modal('hide dimmer');
+                $('#updateBatchModal').modal('hide dimmer');               
             };
 
             batchUpdate.updateBatchDetails = function(data){
@@ -188,7 +195,8 @@ angular.module('playerApp')
                     }
                     batchService.update(request).then(function (response) {
                         if (response && response.responseCode === 'OK') {
-                            data.users = $('#users').dropdown('get value').split(",");
+                            if(data.enrollmentType != 'open'){
+                            data.users = $('#userSelList').val().split(",");
                             if(data.users && data.users.length > 0){
                                 var userRequest = {
                                     "request" : {
@@ -205,6 +213,10 @@ angular.module('playerApp')
                                     toasterService.error($rootScope.errorMessages.BATCH.ADD_USERS.FAILED);
                                 });
                             }else{
+                                batchUpdate.hideUpdateBatchModal();
+                            }
+                         }
+                         else{
                                 batchUpdate.hideUpdateBatchModal();
                             }
                         }else{
