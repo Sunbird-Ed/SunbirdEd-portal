00e97954ff11b401a4e107c877dc90d624625184
Harishkumar Gangula <harishg@ilimi.in>
2017-09-04 17:32:36 +0530

    Issue #1 fix: apekx changed to ap

---
+++ app/tenant/ap/js/apekx.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	// Add smooth scrolling to all links
+	$(".navbar .nav-item a.nav-link").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+
+
+	// Add scroll to top
+	$("#scrollTop").on('click', function(event) {
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: 0
+			}, 800);
+	});
+
+	jQuery(window).scroll(function() {
+		if (jQuery(this).scrollTop() > 250) {
+			jQuery('#scrollTop').fadeIn(300);
+		} else {
+			jQuery('#scrollTop').fadeOut(300);
+		}
+	});
+
+});
+
