05634ee01e202ace8166aae60a7f918ca013382a
poonamsharma3 <poonam.sharma@compassitesinc.com>
2017-09-07 16:42:28 +0530

    Issue #1 fix:code refectoring

--- app/private/scripts/directives/admin/searchedUser.js
+++ app/private/scripts/directives/admin/searchedUser.js
@@ -8,7 +8,7 @@
  */
 angular.module('playerApp').directive('userSearch', function () {
     return {
-        templateUrl: 'views/search/userOrgSearch.html',
+        templateUrl: 'views/search/userSearch.html',
         restrict: 'E',
         scope: {
             users: '=',
