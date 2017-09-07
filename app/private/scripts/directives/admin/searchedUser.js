40218f469ec781bebcc91c6eb4d7048fbf42de8e
poonamsharma3 <poonam.sharma@compassitesinc.com>
2017-09-07 16:16:54 +0530

    Issue #1 fix:html files folder changed

--- app/private/scripts/directives/admin/searchedUser.js
+++ app/private/scripts/directives/admin/searchedUser.js
@@ -8,7 +8,7 @@
  */
 angular.module('playerApp').directive('userSearch', function () {
     return {
-        templateUrl: 'views/admin/search.html',
+        templateUrl: 'views/search/userOrgSearch.html',
         restrict: 'E',
         scope: {
             users: '=',
