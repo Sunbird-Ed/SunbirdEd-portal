5229c09030c0932dc912b29c0c8c5ea5cbc0db55
anuj <anujtech46>
2017-09-13 16:39:32 +0530

    Issue #1 fix: Workspace code refactor

--- src/app/private/scripts/controllers/workspace/publishedContentController.js
+++ src/app/private/scripts/controllers/workspace/publishedContentController.js
@@ -9,170 +9,152 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-  .controller('PublishedContentController', ['contentService', 'searchService', 'config',
-      '$rootScope', '$state', 'toasterService', '$scope', 'workSpaceUtilsService', '$timeout',
-      'PaginationService',
-      function (contentService, searchService, config, $rootScope, $state,
-      toasterService, $scope, workSpaceUtilsService, $timeout, PaginationService) {
-          var publishedContent = this;
-          publishedContent.userId = $rootScope.userId;
-          publishedContent.status = ['Live'];
-          publishedContent.sortBy = 'desc';
-          $scope.isSelected = false;
-          publishedContent.selectedContentItem = [];
-          publishedContent.message = $rootScope.errorMessages.WORKSPACE;
-          publishedContent.pageLimit = 9;
-          publishedContent.pager = {};
+.controller('PublishedContentController', ['contentService', 'searchService', 'config',
+    '$rootScope', '$state', 'toasterService', '$scope', 'workSpaceUtilsService', '$timeout',
+    'PaginationService', function (contentService, searchService, config, $rootScope, $state,
+    toasterService, $scope, workSpaceUtilsService, $timeout, PaginationService) {
+        var publishedContent = this;
+        publishedContent.userId = $rootScope.userId;
+        publishedContent.status = ['Live'];
+        publishedContent.sortBy = 'desc';
+        $scope.isSelected = false;
+        publishedContent.selectedContentItem = [];
+        publishedContent.message = $rootScope.errorMessages.WORKSPACE;
+        publishedContent.pageLimit = 9;
+        publishedContent.pager = {};
 
-          function showErrorMessage(isClose, message, messageType, messageText) {
-              var error = {};
-              error.showError = true;
-              error.isClose = isClose;
-              error.message = message;
-              error.messageType = messageType;
-              if (messageText) {
-                  error.messageText = messageText;
-              }
-              return error;
-          }
+        function showErrorMessage(isClose, message, messageType, messageText) {
+            var error = {};
+            error.showError = true;
+            error.isClose = isClose;
+            error.message = message;
+            error.messageType = messageType;
+            if (messageText) {
+                error.messageText = messageText;
+            }
+            return error;
+        }
 
-          publishedContent.getPublishedContent = function (pageNumber) {
-              pageNumber = pageNumber || 1;
-              publishedContent.loader = toasterService.loader('', publishedContent.message
-          .PUBLISHED.START);
-              publishedContent.error = {};
-              var request = {
-                  filters: {
-                      status: publishedContent.status,
-                      createdBy: publishedContent.userId,
-                      objectType: 'Content',
-                      contentType: config.contributeContentType
-                  },
-                  sort_by: {
-                      lastUpdatedOn: publishedContent.sortBy
-                  },
-                  offset: (pageNumber - 1) * publishedContent.pageLimit,
-                  limit: publishedContent.pageLimit
-              };
+        publishedContent.getPublishedContent = function (pageNumber) {
+            pageNumber = pageNumber || 1;
+            publishedContent.loader = toasterService.loader('', publishedContent.message
+        .PUBLISHED.START);
+            publishedContent.error = {};
+            var request = {
+                filters: {
+                    status: publishedContent.status,
+                    createdBy: publishedContent.userId,
+                    objectType: 'Content',
+                    contentType: config.contributeContentType
+                },
+                sort_by: {
+                    lastUpdatedOn: publishedContent.sortBy
+                },
+                offset: (pageNumber - 1) * publishedContent.pageLimit,
+                limit: publishedContent.pageLimit
+            };
 
-              searchService.search(request).then(function (res) {
-                  if (res && res.responseCode === 'OK') {
-                      publishedContent.error.showError = false;
-                      publishedContent.loader.showLoader = false;
-                      publishedContent.pager = PaginationService.GetPager(res.result.count,
-              pageNumber, publishedContent.pageLimit);
-                      publishedContent.publishedContentData = res.result.content || [];
-                      publishedContent.totalCount = res.result.count;
-                      publishedContent.pageNumber = pageNumber;
-                      if (publishedContent.publishedContentData.length === 0) {
-                          publishedContent.error = showErrorMessage(true,
-                $rootScope.errorMessages.WORKSPACE.PUBLISHED.NO_CONTENT,
-                $rootScope.errorMessages.COMMON.NO_RESULTS);
-                      }
-                  } else {
-                      publishedContent.error.showError = false;
-                      publishedContent.loader.showLoader = false;
-                      toasterService.error(publishedContent.message.PUBLISHED.FAILED);
-                  }
-              }).catch(function () {
-                  publishedContent.error.showError = false;
-                  publishedContent.loader.showLoader = false;
-                  toasterService.error(publishedContent.message.PUBLISHED.FAILED);
-              });
-          };
+            searchService.search(request).then(function (res) {
+                if (res && res.responseCode === 'OK') {
+                    publishedContent.error.showError = false;
+                    publishedContent.loader.showLoader = false;
+                    publishedContent.pager = PaginationService.GetPager(res.result.count,
+            pageNumber, publishedContent.pageLimit);
+                    publishedContent.publishedContentData = res.result.content || [];
+                    publishedContent.totalCount = res.result.count;
+                    publishedContent.pageNumber = pageNumber;
+                    if (publishedContent.publishedContentData.length === 0) {
+                        publishedContent.error = showErrorMessage(true,
+            $rootScope.errorMessages.WORKSPACE.PUBLISHED.NO_CONTENT,
+            $rootScope.errorMessages.COMMON.NO_RESULTS);
+                    }
+                } else {
+                    publishedContent.error.showError = false;
+                    publishedContent.loader.showLoader = false;
+                    toasterService.error(publishedContent.message.PUBLISHED.FAILED);
+                }
+            }).catch(function () {
+                publishedContent.error.showError = false;
+                publishedContent.loader.showLoader = false;
+                toasterService.error(publishedContent.message.PUBLISHED.FAILED);
+            });
+        };
 
-          function showErrorMessage(isClose, message, messageType, messageText) {
-              var error = {};
-              error.showError = true;
-              error.isClose = isClose;
-              error.message = message;
-              error.messageType = messageType;
-              if (messageText) {
-                  error.messageText = messageText;
-              }
-              return error;
-          }
-          publishedContent.openContentPlayer = function (item) {
-              if (item.mimeType === 'application/vnd.ekstep.content-collection') {
-                  $state.go('CollectionEditor', {
-                      contentId: item.identifier,
-                      type: item.contentType,
-                      state: 'WorkSpace.PublishedContent'
-                  });
-              } else if (item.mimeType === 'application/vnd.ekstep.ecml-archive') {
-                  var params = {
-                      contentId: item.identifier,
-                      state: 'WorkSpace.PublishedContent'
-                  };
-                  $state.go('ContentEditor', params);
-              } else {
-                  var params = {
-                      contentId: item.identifier,
-                      state: 'WorkSpace.PublishedContent'
-                  };
-                  $state.go('GenericEditor', params);
-              }
-          };
+        function showErrorMessage(isClose, message, messageType, messageText) {
+            var error = {};
+            error.showError = true;
+            error.isClose = isClose;
+            error.message = message;
+            error.messageType = messageType;
+            if (messageText) {
+                error.messageText = messageText;
+            }
+            return error;
+        }
 
-          publishedContent.initializeUIElement = function () {
-              $('#actionDropDown').dropdown();
-          };
+        publishedContent.openContentPlayer = function (item) {
+            workSpaceUtilsService.openContentEditor(item, $state.current.name); 
+        };
 
-          publishedContent.openRemoveContentModel = function (ContentId) {
-              publishedContent.removeContentId = ContentId;
-              publishedContent.showRemoveContentModel = true;
-              $timeout(function () {
-                  $('#removeContentModel').modal({}).modal('show');
-              }, 10);
-          };
+        publishedContent.initializeUIElement = function () {
+            $('#actionDropDown').dropdown();
+        };
 
-          publishedContent.hideRemoveContentModel = function () {
-              $('#removeContentModel').modal('hide');
-              $('#removeContentModel').modal('hide all');
-              $('#removeContentModel').modal('hide other');
-              $('#removeContentModel').modal('hide dimmer');
-              publishedContent.removeContentId = '';
-              publishedContent.showRemoveContentModel = false;
-          };
+        publishedContent.openRemoveContentModel = function (ContentId) {
+            publishedContent.removeContentId = ContentId;
+            publishedContent.showRemoveContentModel = true;
+            $timeout(function () {
+                $('#removeContentModel').modal({}).modal('show');
+            }, 10);
+        };
 
-          publishedContent.deleteContent = function (contentId) {
-              var requestData = [contentId];
-              publishedContent.hideRemoveContentModel();
-              publishedContent.loader = toasterService.loader('', publishedContent.message
-          .RETIRE_CONTENT.START);
-              var request = {
-                  contentIds: requestData
-              };
-              contentService.retire(request).then(function (res) {
-                  if (res && res.responseCode === 'OK') {
-                      publishedContent.loader.showLoader = false;
-                      publishedContent.selectedContentItem = [];
-                      toasterService.success(publishedContent.message.RETIRE_CONTENT.SUCCESS);
-                      publishedContent.publishedContentData = workSpaceUtilsService
-              .removeContentLocal(publishedContent.publishedContentData, requestData);
-                      publishedContent.pager = PaginationService
-              .GetPager(publishedContent.totalCount - requestData.length,
-                publishedContent.pageNumber, publishedContent.pageLimit);
-                      if (publishedContent.publishedContentData.length === 0) {
-                          publishedContent.error = showErrorMessage(true,
-                $rootScope.errorMessages.WORKSPACE.PUBLISHED.NO_CONTENT,
-                $rootScope.errorMessages.COMMON.NO_RESULTS);
-                      }
-                  } else {
-                      publishedContent.loader.showLoader = false;
-                      toasterService.error(publishedContent.message.RETIRE_CONTENT.NOT_DELETE);
-                  }
-              }).catch(function () {
-                  publishedContent.loader.showLoader = false;
-                  toasterService.error(publishedContent.message.RETIRE_CONTENT.NOT_DELETE);
-              });
-          };
+        publishedContent.hideRemoveContentModel = function () {
+            $('#removeContentModel').modal('hide');
+            $('#removeContentModel').modal('hide all');
+            $('#removeContentModel').modal('hide other');
+            $('#removeContentModel').modal('hide dimmer');
+            publishedContent.removeContentId = '';
+            publishedContent.showRemoveContentModel = false;
+        };
 
-          publishedContent.setPage = function (page) {
-              if (page < 1 || page > publishedContent.pager.totalPages) {
-                  return;
-              }
-              publishedContent.getPublishedContent(page);
-          };
-      }
-  ]);
+        publishedContent.deleteContent = function (contentId) {
+            var requestData = [contentId];
+            publishedContent.hideRemoveContentModel();
+            publishedContent.loader = toasterService.loader('', publishedContent.message
+                                                    .RETIRE_CONTENT.START);
+            var request = {
+                contentIds: requestData
+            };
+            contentService.retire(request).then(function (res) {
+                if (res && res.responseCode === 'OK') {
+                    publishedContent.loader.showLoader = false;
+                    publishedContent.selectedContentItem = [];
+                    toasterService.success(publishedContent.message.RETIRE_CONTENT.SUCCESS);
+                    publishedContent.publishedContentData = workSpaceUtilsService
+                            .removeContentLocal(publishedContent.publishedContentData, requestData);
+                    publishedContent.pager = PaginationService.GetPager(publishedContent.totalCount 
+                            - requestData.length,
+                    publishedContent.pageNumber, publishedContent.pageLimit);
+                    if (publishedContent.publishedContentData.length === 0) {
+                        publishedContent.error = showErrorMessage(true,
+                        $rootScope.errorMessages.WORKSPACE.PUBLISHED.NO_CONTENT,
+                        $rootScope.errorMessages.COMMON.NO_RESULTS);
+                    }
+                } else {
+                    publishedContent.loader.showLoader = false;
+                    toasterService.error(publishedContent.message.RETIRE_CONTENT.NOT_DELETE);
+                }
+            }).catch(function () {
+                publishedContent.loader.showLoader = false;
+                toasterService.error(publishedContent.message.RETIRE_CONTENT.NOT_DELETE);
+            });
+        };
+
+        publishedContent.setPage = function (page) {
+            if (page < 1 || page > publishedContent.pager.totalPages) {
+                return;
+            }
+            publishedContent.getPublishedContent(page);
+        };
+    }
+]);
--- src/app/private/scripts/controllers/workspace/reviewContentController.js
+++ src/app/private/scripts/controllers/workspace/reviewContentController.js
@@ -9,95 +9,83 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-  .controller('ReviewContentController', ['contentService', 'searchService', 'config',
-      '$rootScope', '$scope', '$state', 'toasterService', 'PaginationService',
-      function (contentService, searchService, config, $rootScope, $scope,
-      $state, toasterService, PaginationService) {
-          var reviewContent = this;
-          reviewContent.userId = $rootScope.userId;
-          $scope.contentPlayer = { isContentPlayerEnabled: false };
-          reviewContent.status = ['Review'];
-          reviewContent.sortBy = 'desc';
-          reviewContent.pageLimit = 9;
-          reviewContent.pager = {};
+.controller('ReviewContentController', ['contentService', 'searchService', 'config',
+    '$rootScope', '$scope', '$state', 'toasterService', 'PaginationService',
+    'workSpaceUtilsService', function (contentService, searchService, config, $rootScope, $scope, 
+    $state, toasterService, PaginationService, workSpaceUtilsService) {
+        var reviewContent = this;
+        reviewContent.userId = $rootScope.userId;
+        $scope.contentPlayer = { isContentPlayerEnabled: false };
+        reviewContent.status = ['Review'];
+        reviewContent.sortBy = 'desc';
+        reviewContent.pageLimit = 9;
+        reviewContent.pager = {};
 
-          function showErrorMessage(isClose, message, messageType, messageText) {
-              var error = {};
-              error.showError = true;
-              error.isClose = isClose;
-              error.message = message;
-              error.messageType = messageType;
-              if (messageText) {
-                  error.messageText = messageText;
-              }
-              return error;
-          }
+        function showErrorMessage(isClose, message, messageType, messageText) {
+            var error = {};
+            error.showError = true;
+            error.isClose = isClose;
+            error.message = message;
+            error.messageType = messageType;
+            if (messageText) {
+                error.messageText = messageText;
+            }
+            return error;
+        }
 
-          reviewContent.getReviewContent = function (pageNumber) {
-              pageNumber = pageNumber || 1;
-              reviewContent.loader = toasterService.loader('', $rootScope.errorMessages.WORKSPACE
-          .REVIEW.START);
-              reviewContent.error = {};
-              var request = {
-                  filters: {
-                      status: reviewContent.status,
-                      createdBy: reviewContent.userId,
-                      objectType: 'Content',
-                      contentType: config.contributeContentType
-                  },
-                  sort_by: {
-                      lastUpdatedOn: reviewContent.sortBy
-                  },
-                  offset: (pageNumber - 1) * reviewContent.pageLimit,
-                  limit: reviewContent.pageLimit
-              };
+        reviewContent.getReviewContent = function (pageNumber) {
+            pageNumber = pageNumber || 1;
+            reviewContent.loader = toasterService.loader('', $rootScope.errorMessages.WORKSPACE
+        .REVIEW.START);
+            reviewContent.error = {};
+            var request = {
+                filters: {
+                    status: reviewContent.status,
+                    createdBy: reviewContent.userId,
+                    objectType: 'Content',
+                    contentType: config.contributeContentType
+                },
+                sort_by: {
+                    lastUpdatedOn: reviewContent.sortBy
+                },
+                offset: (pageNumber - 1) * reviewContent.pageLimit,
+                limit: reviewContent.pageLimit
+            };
 
-              searchService.search(request).then(function (res) {
-                  if (res && res.responseCode === 'OK') {
-                      reviewContent.loader.showLoader = false;
-                      reviewContent.error.showError = false;
-                      reviewContent.reviewContentData = res.result.content || [];
-                      reviewContent.totalCount = res.result.count;
-                      reviewContent.pager = PaginationService.GetPager(res.result.count,
-              pageNumber, reviewContent.pageLimit);
-                      if (reviewContent.reviewContentData.length === 0) {
-                          reviewContent.error = showErrorMessage(true,
-                $rootScope.errorMessages.WORKSPACE.REVIEW.NO_CONTENT,
-                $rootScope.errorMessages.COMMON.NO_RESULTS);
-                      }
-                  } else {
-                      reviewContent.loader.showLoader = false;
-                      reviewContent.error.showError = false;
-                      toasterService.error($rootScope.errorMessages.WORKSPACE.REVIEW.FAILED);
-                  }
-              }).catch(function () {
-                  reviewContent.loader.showLoader = false;
-                  reviewContent.error.showError = false;
-                  toasterService.error($rootScope.errorMessages.WORKSPACE.REVIEW.FAILED);
-              });
-          };
+            searchService.search(request).then(function (res) {
+                if (res && res.responseCode === 'OK') {
+                    reviewContent.loader.showLoader = false;
+                    reviewContent.error.showError = false;
+                    reviewContent.reviewContentData = res.result.content || [];
+                    reviewContent.totalCount = res.result.count;
+                    reviewContent.pager = PaginationService.GetPager(res.result.count,
+                    pageNumber, reviewContent.pageLimit);
+                    if (reviewContent.reviewContentData.length === 0) {
+                        reviewContent.error = showErrorMessage(true,
+            $rootScope.errorMessages.WORKSPACE.REVIEW.NO_CONTENT,
+            $rootScope.errorMessages.COMMON.NO_RESULTS);
+                    }
+                } else {
+                    reviewContent.loader.showLoader = false;
+                    reviewContent.error.showError = false;
+                    toasterService.error($rootScope.errorMessages.WORKSPACE.REVIEW.FAILED);
+                }
+            }).catch(function () {
+                reviewContent.loader.showLoader = false;
+                reviewContent.error.showError = false;
+                toasterService.error($rootScope.errorMessages.WORKSPACE.REVIEW.FAILED);
+            });
+        };
 
-          reviewContent.openContentPlayer = function (item) {
-              if (item.mimeType === 'application/vnd.ekstep.content-collection') {
-                  $state.go('CollectionEditor', {
-                      contentId: item.identifier,
-                      type: item.contentType,
-                      state: 'WorkSpace.ReviewContent'
-                  });
-              } else {
-                  var params = {
-                      contentId: item.identifier,
-                      backState: $state.current.name
-                  };
-                  $state.go('PreviewContent', params);
-              }
-          };
+        reviewContent.openContentPlayer = function (item) {
+            workSpaceUtilsService.openContentPlayer(item, $state.current.name);
+        };
 
-          reviewContent.setPage = function (page) {
-              if (page < 1 || page > reviewContent.pager.totalPages) {
-                  return;
-              }
-              reviewContent.getReviewContent(page);
-          };
-      }
-  ]);
+        reviewContent.setPage = function (page) {
+            if (page < 1 || page > reviewContent.pager.totalPages) {
+                return;
+            }
+            reviewContent.getReviewContent(page);
+        };
+    }
+]);
