9d8c48e2b7bb12e1550f5855a8d953515e3d6c6f
anuj <anujtech46>
2017-09-13 16:39:32 +0530

    Issue #1 fix: Workspace code refactor

--- src/app/private/scripts/services/workSpaceUtilsService.js
+++ src/app/private/scripts/services/workSpaceUtilsService.js
@@ -10,7 +10,7 @@
  */
 
 angular.module('playerApp')
-    .service('workSpaceUtilsService', [function () {
+    .service('workSpaceUtilsService', ['$state', function ($state) { 
         this.reduceObjectIntoArray = function (items, key) {
             return items.reduce(function (validation, item, index) {
                 validation[index] = item[key];
@@ -29,4 +29,62 @@ angular.module('playerApp')
                 return failedIds.indexOf(contentId) === -1;
             });
         };
+        
+        this.collectionEditor = function (item, state) { 
+            $state.go('CollectionEditor', { 
+                contentId: item.identifier, 
+                type: item.contentType, 
+                state: state 
+            }); 
+        }; 
+ 
+        this.contentEditor = function (item, state) { 
+            var params = { 
+                contentId: item.identifier, 
+                state: state 
+            }; 
+            $state.go('ContentEditor', params); 
+        }; 
+ 
+        this.genericEditor = function (item, state) { 
+            var params = { 
+                contentId: item.identifier, 
+                state: state 
+            }; 
+            $state.go('GenericEditor', params); 
+        }; 
+ 
+        this.previewContent = function (item, state) { 
+            var params = { 
+                contentId: item.identifier, 
+                backState: state 
+            }; 
+            $state.go('PreviewContent', params); 
+        }; 
+ 
+        this.previewCollection = function (item, state) { 
+            $state.go('PreviewCollection', { 
+                Id: item.identifier, 
+                name: item.name, 
+                backState: state 
+            }); 
+        }; 
+ 
+        this.openContentEditor = function (item, state) { 
+            if (item.mimeType === 'application/vnd.ekstep.content-collection') { 
+                this.collectionEditor(item, state); 
+            } else if (item.mimeType === 'application/vnd.ekstep.ecml-archive') { 
+                this.contentEditor(item, state); 
+            } else { 
+                this.genericEditor(item, state); 
+            } 
+        }; 
+ 
+        this.openContentPlayer = function (item, state) { 
+            if (item.mimeType === 'application/vnd.ekstep.content-collection') { 
+                this.collectionEditor(item, state); 
+            } else { 
+                this.previewContent(item, state); 
+            } 
+        }; 
     }]);
