0c019afa937fd9110952c80421efc93de034d9be
Harishkumar Gangula <harishg@ilimi.in>
2017-09-14 12:48:25 +0530

    Issue #000 fix: removed consumer id and pointing urls to staging

--- src/app/helpers/environmentVariablesHelper.js
+++ src/app/helpers/environmentVariablesHelper.js
@@ -2,11 +2,11 @@
 const env = process.env;
 
 module.exports = {
-  LEARNER_URL: env.sunbird_learner_player_url || 'https://dev.open-sunbird.org/api/',
-  CONTENT_URL: env.sunbird_content_player_url || 'https://dev.open-sunbird.org/api/',
-  CONTENT_PROXY_URL: env.sunbird_content_proxy_url || 'https://dev.open-sunbird.org',
+  LEARNER_URL: env.sunbird_learner_player_url || 'https://staging.open-sunbird.org/api/',
+  CONTENT_URL: env.sunbird_content_player_url || 'https://staging.open-sunbird.org/api/',
+  CONTENT_PROXY_URL: env.sunbird_content_proxy_url || 'https://staging.open-sunbird.org',
   PORTAL_REALM: env.sunbird_portal_realm || 'sunbird',
-  PORTAL_AUTH_SERVER_URL: env.sunbird_portal_auth_server_url || 'https://dev.open-sunbird.org/auth',
+  PORTAL_AUTH_SERVER_URL: env.sunbird_portal_auth_server_url || 'https://staging.open-sunbird.org/auth',
   PORTAL_AUTH_SERVER_CLIENT: env.sunbird_portal_auth_server_client || "portal",
   APPID: env.sunbird_appid || 'sunbird.portal',
   DEFAUULT_TENANT: env.sunbird_default_tenant,
@@ -17,7 +17,7 @@ module.exports = {
   PORTAL_MONGODB_PORT: env.sunbird_mongodb_port,
   PORTAL_MONGODB_TTL: env.sunbird_mongodb_ttl,
   PORTAL_TELEMETRY_PACKET_SIZE: env.sunbird_telemetry_packet_size || 20,
-  PORTAL_ECHO_API_URL: env.sunbird_echo_api_url || 'https://dev.open-sunbird.org/api/echo/',
+  PORTAL_ECHO_API_URL: env.sunbird_echo_api_url || 'https://staging.open-sunbird.org/api/echo/',
   PORTAL_AUTOCREATE_TRAMPOLINE_USER: env.sunbird_autocreate_trampoline_user || 'true',
   PORTAL_TRAMPOLINE_CLIENT_ID: env.sunbird_trampoline_client_id || 'trampoline',
   PORTAL_TRAMPOLINE_SECRET: env.sunbird_trampoline_secret,
