2d6a28961ba4cae01ce6036914d3bca621da518c
anuj <anujtech46>
2017-09-20 12:08:43 +0530

    Issue #SB-34 fix: Notes api changes

--- src/app/private/scripts/services/noteService.js
+++ src/app/private/scripts/services/noteService.js
@@ -10,27 +10,27 @@
  */
 
 angular.module('playerApp')
-    .service('noteService', ['httpService', 'config', function (httpService, config) {
+    .service('noteService', ['httpServiceJava', 'config', function (httpServiceJava, config) {
         this.search = function (req) {
-            return httpService.post(config.URL.NOTES.SEARCH, req);
+            return httpServiceJava.post(config.URL.NOTES.SEARCH, req);
         };
 
         this.create = function (req) {
-            return httpService.post(config.URL.NOTES.CREATE, req);
+            return httpServiceJava.post(config.URL.NOTES.CREATE, req);
         };
 
         this.update = function (req) {
             var url = config.URL.NOTES.UPDATE + '/' + req.noteId;
-            return httpService.patch(url, req);
+            return httpServiceJava.patch(url, req);
         };
 
         this.get = function (req) {
             var url = config.URL.NOTES.GET + '/' + req.noteId;
-            return httpService.get(url, req);
+            return httpServiceJava.get(url, req);
         };
 
         this.remove = function (req) {
             var url = config.URL.NOTES.DELETE + '/' + req.noteId;
-            return httpService.remove(url, req);
+            return httpServiceJava.remove(url, req);
         };
     }]);
