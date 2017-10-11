7db54e9807fa2195d944f06f178609e580036d18
anuj <anujtech46>
2017-10-11 13:14:12 +0530

    Issue #SB-243 test: Test cases for content editor controller, error message and content player directive

--- src/test/spec/app/private/scripts/directives/errormessage.js.js
+++ src/test/spec/app/private/scripts/directives/errormessage.js.js
@@ -1,20 +1,130 @@
+/**
+ * name: errorMessage.js
+ * author: Anuj Gupta
+ * Date: 11-10-2017
+ */
+
 'use strict';
 
-xdescribe('Directive: errorMessage.js', function () {
+describe('Directive: errorMessage', function() {
 
-  // load the directive's module
-  beforeEach(module('playerApp'));
+    // load the main module
+    beforeEach(module('playerApp'));
 
-  var element,
-    scope;
+    beforeEach(inject(function($rootScope, $controller) {
+        $controller('AppCtrl', {
+            $rootScope: $rootScope,
+            $scope: $rootScope.$new()
+        });
+    }));
 
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
-  }));
+    var element, compile, templateCache, scope,
+        fileName = 'views/common/errorPage.html';
+    var error = {};
+    error.showError = true;
+    error.isClose = true;
 
-  xit('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<error-message.js></error-message.js>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the errorMessage.js directive');
-  }));
+    beforeEach(inject(function ($rootScope, $templateCache, $compile) {
+        scope = $rootScope.$new();
+        $templateCache.put(fileName, '<div>error page</div>')
+        compile = $compile;
+        templateCache = $templateCache;
+    }));
+
+    it('Show info message', function(done) {
+        scope.visibilty = true;
+        error.messageType = "info";
+        error.message = "Info message";
+        var ele = '<error-message data="{{error}}" visibility=true></error-message>'
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+
+        error.message = '';
+        error.isClose = false;
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+        done();
+    });
+
+    it('Show warning message', function(done) {
+        scope.visibilty = true;
+        error.messageType = "warning";
+        error.message = "Warning message";
+        var ele = '<error-message data="{{error}}" visibility=true></error-message>'
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+
+        error.message = '';
+        error.isClose = false;
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+        done();
+    });
+
+    it('Show error message', function(done) {
+        scope.visibilty = true;
+        error.messageType = "error";
+        error.message = "Error message";
+        var ele = '<error-message data="{{error}}" visibility=true></error-message>'
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+
+        error.message = '';
+        error.isClose = false;
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+        done();
+    });
+
+    it('Show success message', function(done) {
+        scope.visibilty = true;
+        error.messageType = "success";
+        error.message = "Success message";
+        var ele = '<error-message data="{{error}}" visibility=true></error-message>'
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+
+        error.message = '';
+        error.isClose = false;
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+        expect(element.text()).toContain('error page');
+        done();
+    });
+
+    it('Show no-results message', function(done) {
+        scope.visibilty = true;
+        error.messageType = "no-results";
+        error.message = "No result message";
+        var ele = '<error-message data="{{error}}" visibility=true></error-message>'
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+
+        error.message = '';
+        error.isClose = false;
+        scope.error = error;
+        element = compile(ele)(scope);
+        scope.$digest();
+        done();
+    });
+
+    xit("Close error message", function() {
+      var ele = '<error-message data="{{error}}" visibility=true></error-message>'
+      scope.error = error;
+      element = compile(ele)(scope);
+      scope.$digest();
+      console.log(scope);
+      spyOn(scope, 'closeErrorMessage').and.callThrough();
+      scope.closeErrorMessage();
+      expect(scope.visibility).toBe(false);
+    });
 });
