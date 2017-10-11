02b42659480d9749b77942d71cf67ea42365fb00
Harishkumar Gangula <harishg@ilimi.in>
2017-10-11 17:38:15 +0530

    Issue #SB-264 fix: JSDocs setup

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,32 +1,35 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var requireDir = require('require-dir');
+var gulp = require('gulp')
+var requireDir = require('require-dir')
 // Require all tasks.
-requireDir('./gulp', { recurse: true });
+requireDir('./gulp', { recurse: true })
 
 // This task is to create production build which creates dist folder inside src
 // to know see sub tasks the gulp/deploy.js
-gulp.task('default', ['packageNodeModules']);
+gulp.task('default', ['packageNodeModules'])
 
 /* Below tasks for node js code test cases to run
  use test-node to run all the test cases for server side js files
- */	
-gulp.task('test-node', ['test-server-js']);
+ */
+gulp.task('test-node', ['test-server-js'])
 
 /* Below tasks for angular code test cases to run
  use test to run all the test cases for angular js files
  */
-gulp.task('test', ['test-angular-frontend']);
+gulp.task('test', ['test-angular-frontend'])
 
 // Below task is used for generate config
-gulp.task('config', ['build-config']);
+gulp.task('config', ['build-config'])
 
 // Below task is used for generate css
-gulp.task('css', ['build-css']);
+gulp.task('css', ['build-css'])
 
-// Below task is used for generate themes
-gulp.task('themes', ['build-themes']);
+// Below task is used for build themes
+gulp.task('themes', ['build-themes'])
 
 // Below task is used for build theme
-gulp.task('theme', ['build-theme']);
+gulp.task('theme', ['build-theme'])
+
+// Below task to create documentation for js files
+gulp.task('js-docs', ['build-js-docs'])
