f4f806b04c6e2d260d46f89e2f211126b8708b31
poonamsharma3 <poonam.sharma@compassitesinc.com>
2017-10-18 12:11:17 +0530

    Issue #SB-331 fix:documentation pageSection and search service

--- src/app/private/scripts/services/pageSectionService.js
+++ src/app/private/scripts/services/pageSectionService.js
@@ -2,6 +2,22 @@
 
 angular.module('playerApp')
     .service('pageSectionService', ['httpServiceJava', function (httpServiceJava) {
+    /**
+     * @class pageSectionService
+     * @desc Service to manage sections of page.
+     * @memberOf Services
+     */
+    /**
+         * @method getPageData
+         * @desc Get page sections
+         * @memberOf Services.pageSectionService
+         * @param {string}  path - Url path of page
+         * @param {object}  request - Request object
+         * @param {string}  request.source - Source ie. 'Web'
+         * @param {string}  request.name - Page type url ie- 'resource' or  'course'
+         * @returns {Promise} Promise object represents sections of a page
+         * @instance
+         */
         this.getPageData = function (path, req) {
             return httpServiceJava.post(path, req);
         };
--- src/app/private/scripts/services/searchService.js
+++ src/app/private/scripts/services/searchService.js
@@ -3,17 +3,87 @@
 angular.module('playerApp')
     .service('searchService', ['httpService', 'config', '$q', 'httpServiceJava',
         function (httpService, config, $q, httpServiceJava) {
+     /**
+     * @class searchService
+     * @desc Service to manage different type of search.
+     * @memberOf Services
+     */
+            /**
+             * @method contentSearch
+             * @desc Search contents
+             * @memberOf Services.searchService
+             * @param {object}  request - Request object
+             * @param {string}  request.query - Search query
+             * @param {object}  request.filters - Filters object
+             * @param {object[]}  request.filters.Curriculum - Filter results by curriculum
+             * @param {object[]}  request.filters.ContentTypes - Filter results  by Content Types
+             * @param {object[]}  request.filters.Medium - Filter results by medium
+             * @param {object[]}  request.filters.Subjects - Filter results by subjects
+             * @param {object[]}  request.filters.Concepts - Filter results by concepts
+             * @param {object}  request.sort_by - Sort_by object
+             * @param {string}  request.sort_by.modifiedOn - Sort search results by modified date
+             * @param {string}  request.sort_by.createdOn - Sort search results by created date
+             * @param {string}  request.offset - Limit number of results
+             * @param {string}  request.limit - Number of result per page
+             * @returns {Promise} Promise object represents the list of content
+             * @instance
+             */
+
             this.contentSearch = function (req) {
                 return httpService.post(config.URL.CONTENT.SEARCH, req);
             };
+            /**
+             * @method courseSearch
+             * @desc Search courses
+             * @memberOf Services.searchService
+             * @param {object}  request - Request object
+             * @param {string}  request.query - Search query
+             * @param {object}  request.filters - Filters object
+             * @param {object[]}  request.filters.Curriculum - Filter results by curriculum
+             * @param {object[]}  request.filters.Medium - Filter results by medium
+             * @param {object[]}  request.filters.Subjects - Filter results by subjects
+             * @param {object[]}  request.filters.Concepts - Filter results by concepts
+             * @param {object}  request.sort_by - Sort_by object
+             * @param {string}  request.sort_by.modifiedOn - Sort search results by modified date
+             * @param {string}  request.sort_by.createdOn - Sort search results by created date
+             * @param {string}  request.offset - Limit number of results
+             * @param {string}  request.limit - Number of result per page
+             * @returns {Promise} Promise object represents the list of courses
+             * @instance
+             */
 
             this.courseSearch = function (req) {
                 return httpService.post(config.URL.COURSE.SEARCH, req);
             };
+            /**
+             * @method search
+             * @desc Search All possible results of query.
+             * @memberOf Services.searchService
+             * @param {object}  request - Request object
+             * @param {string}  request.query - Search query
+             * @param {object}  request.filters - Filters object
+             * @param {object[]}  request.filters.status - Filter results by status of result ie-live active
+             * @param {object[]}  request.filters.createdBy - Filter results by userId
+             * @param {object[]}  request.filters.objectType - Filter results by objectType
+             * @param {object[]}  request.filters.contentType - Filter results by contentType
+             * @param {object}  request.sort_by - Sort_by object
+             * @param {string}  request.sort_by.lastUpdatedOn - Sort search results by last updated date
+             * @param {string}  request.offset - Limit number of results
+             * @param {string}  request.limit - Number of result per page
+             * @returns {Promise} Promise object represents the list of search results
+             * @instance
+             */
 
             this.search = function (req) {
                 return httpService.post(config.URL.COMPOSITE.SEARCH, req);
             };
+            /**
+             * @method setPublicUserProfile
+             * @desc Set user's public profile to local variable.
+             * @memberOf Services.searchService
+             * @returns {string} Boolean value
+             * @instance
+             */
             this.setPublicUserProfile = function (user) {
                 this.publicUser = { responseCode: 'OK',
                     result: {
@@ -21,6 +91,14 @@ angular.module('playerApp')
                 };
                 return true;
             };
+            /**
+             * @method getPublicUserProfile
+             * @desc Get user's public profile from local variable or api
+             * @memberOf Services.searchService
+             * @param {string}  identifier - Identifier of user
+             * @returns {Promise} Promise object represents details of user available locally or api
+             * @instance
+             */
             this.getPublicUserProfile = function (identifier) {
                 if (this.publicUser) {
                     var deferred = $q.defer();
@@ -29,14 +107,35 @@ angular.module('playerApp')
                 }
                 return httpService.get(config.URL.USER.GET_PROFILE + '/' + identifier);
             };
+            /**
+             * @method getOrgTypes
+             * @desc Get all org types
+             * @memberOf Services.searchService
+             * @returns {Promise} Promise object represents list of orgTypes
+             * @instance
+             */
             this.getOrgTypes = function () {
                 var url = config.URL.ORG_TYPE.GET;
                 return httpServiceJava.get(url);
             };
+            /**
+             * @method setOrgTypes
+             * @desc Set orgTypes api result to local variable
+             * @memberOf Services.searchService
+             * @param {object}  orgTypes - list of OrgTypes.
+             * @instance
+             */
             this.setOrgTypes = function (orgTypes) {
                 this.orgTypes = orgTypes;
             };
 
+            /**
+             * @method getOrgTypeS
+             * @desc Get list of orgTypes
+             * @memberOf Services.searchService
+             * @returns {Promise} Promise object represents list of orgTypes available locally or api
+             * @instance
+             */
             this.getOrgTypeS = function () {
                 if (this.orgTypes) {
                     var deferred = $q.defer();
