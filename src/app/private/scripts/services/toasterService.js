4a150acb118215fea057166aa269370965514955
poonamsharma3 <poonam.sharma@compassitesinc.com>
2017-10-22 23:07:59 +0530

    Issue #SB-331 fix:documentation services

--- src/app/private/scripts/services/toasterService.js
+++ src/app/private/scripts/services/toasterService.js
@@ -2,23 +2,47 @@
 
 angular.module('playerApp')
     .service('toasterService', [function () {
+     /**
+     * @class toasterService
+     * @desc Service to manage izi toaster messages.
+     * @memberOf Services
+     */
         iziToast.settings({
             position: 'topCenter',
             titleSize: '18'
         });
-
+            /**
+             * @method success
+             * @desc Format success message
+             * @memberOf Services.toasterService
+             * @param {string}  message - Success message
+             * @instance
+             */
         this.success = function (message) {
             iziToast.success({
                 title: message
             });
         };
 
+         /**
+             * @method info
+             * @desc Format information message
+             * @memberOf Services.toasterService
+             * @param {string}  message - Info message
+             * @instance
+             */
         this.info = function (message) {
             iziToast.info({
                 title: message
             });
         };
-
+         /**
+             * @method error
+             * @desc Format error message
+             * @memberOf Services.toasterService
+             * @param {string}  message - Error message
+             * @instance
+             */
         this.error = function (message) {
             iziToast.error({
                 progressBar: false,
@@ -26,6 +50,13 @@ angular.module('playerApp')
                 title: message
             });
         };
+        /**
+             * @method warning
+             * @desc Format warning message
+             * @memberOf Services.toasterService
+             * @param {string}  message - Warning message
+             * @instance
+             */
 
         this.warning = function (message) {
             iziToast.warning({
@@ -33,6 +64,14 @@ angular.module('playerApp')
             });
         };
 
+        /**
+             * @method loader
+             * @desc Manage loader
+             * @memberOf Services.toasterService
+             * @param {string}  headerMessage - Loader header message
+             * @param {string}  loaderMessage - Loader body message
+             * @instance
+             */
         this.loader = function (headerMessage, loaderMessage) {
             var loader = {};
             loader.showLoader = true;
