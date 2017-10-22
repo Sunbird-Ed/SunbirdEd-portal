3509861a7badd307a54a4b9af451f7e14c173f1d
poonamsharma3 <poonam.sharma@compassitesinc.com>
2017-10-22 23:07:59 +0530

    Issue #SB-331 fix:documentation services

--- src/app/private/scripts/services/userService.js
+++ src/app/private/scripts/services/userService.js
@@ -6,12 +6,26 @@ angular.module('playerApp')
         'httpService',
         'httpServiceJava',
         function (config, httpService, httpServiceJava) {
+     /**
+     * @class userService
+     * @desc Service to manage user profile.
+     * @memberOf Services
+     */
             this.currentUserProfile = {};
             this.resourceBundle = function (language, type) {
                 var url = config.URL.CONFIG_BASE + config.URL.USER.RESOURCE_BUNDLE + '/'
                     + type + '/' + language;
                 return httpService.get(url);
             };
+            /**
+             * @method getUserProfile
+             * @desc Get user profile
+             * @memberOf Services.userService
+             * @param {string}  uId - User identifier
+             * @param {Object}  fields - Specific fields i.e-last login time etc(optional)
+             * @returns {Promise} Promise object represents user profile
+             * @instance
+             */
 
             this.getUserProfile = function (uId, fields) {
                 var url = config.URL.USER.GET_PROFILE + '/' + uId;
@@ -20,20 +34,46 @@ angular.module('playerApp')
                 }
                 return httpServiceJava.get(url);
             };
-
+            /**
+             * @method updateUserProfile
+             * @desc Get user profile
+             * @memberOf Services.userService
+             * @param {Object}  req - Request fields that a user want to update
+             * @returns {Promise} Promise object represents response message and code.
+             * @instance
+             */
             this.updateUserProfile = function (req, name, email) {
                 var url = config.URL.USER.UPDATE_USER_PROFILE;
                 return httpServiceJava.patch(url, req);
             };
-
+            /**
+             * @method getTenantLogo
+             * @desc Get tenant logo
+             * @memberOf Services.userService
+             * @returns {Promise} Promise object represents tenant logo
+             * @instance
+             */
             this.getTenantLogo = function () {
                 return httpService.get(config.URL.USER.TENANT_LOGO);
             };
+            /**
+             * @method setCurrentUserProfile
+             * @desc Set current user profile to local variable
+             * @memberOf Services.userService
+             * @param {Object}  req - User profile
+             * @instance
+             */
 
             this.setCurrentUserProfile = function (userProfile) {
                 this.currentUserProfile = userProfile;
             };
-
+            /**
+             * @method getCurrentUserProfile
+             * @desc Get current user profile from local variable
+             * @memberOf Services.userService
+             * @returns {Object} Promise object represents current user profile
+             * @instance
+             */
             this.getCurrentUserProfile = function () {
                 return this.currentUserProfile;
             };
