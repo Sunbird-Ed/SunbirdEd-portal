33a5eaface2e40b7600dff51ab8c45c9939e2b84
poonamsharma3 <poonam.sharma@compassitesinc.com>
2017-10-24 07:59:53 +0530

    Issue #SB-331 fix:documentation services

--- src/app/private/scripts/services/paginationService.js
+++ src/app/private/scripts/services/paginationService.js
@@ -2,48 +2,49 @@
 
 angular.module('playerApp')
     .service('PaginationService', [function () {
-    // service definition
-        var service = {};
+    /**
+    * @class PaginationService
+     * @desc Service to manages pagination.
+     * @memberOf Services
+     */
 
-    // service implementation
-        function GetPager(totalItems, currentPage, pageSize) {
-        // default to first page
+     /**
+             * @method GetPager
+             * @desc Manage pagination
+             * @memberOf Services.PaginationService
+             * @param {number}  totalItems - Total count of result
+             * @param {number}  currentPage - Current page
+             * @param {number}  pageSize - Number of items per page
+             * @returns {Object} response - Pagination response object contains -totalPage,startPage,
+             * endPage,pageIndex
+             * @instance
+             */
+        this.GetPager = function (totalItems, currentPage, pageSize) {
             currentPage = currentPage || 1;
 
-        // default page size is 10
             pageSize = pageSize || 10;
 
-        // calculate total pages
             var totalPages = Math.ceil(totalItems / pageSize);
 
             var startPage;
             var endPage;
             if (totalPages <= 5) {
-                // less than 5 total pages so show all
                 startPage = 1;
                 endPage = totalPages;
+            } else if (currentPage <= 1) {
+                startPage = 1;
+                endPage = 5;
+            } else if (currentPage + 4 >= totalPages) {
+                startPage = totalPages - 4;
+                endPage = totalPages;
             } else {
-                // more than 5 total pages so calculate start and end pages
-                if (currentPage <= 1) {
-                    startPage = 1;
-                    endPage = 5;
-                } else if (currentPage + 4 >= totalPages) {
-                    startPage = totalPages - 4;
-                    endPage = totalPages;
-                } else {
-                    startPage = currentPage;
-                    endPage = currentPage + 4;
-                }
+                startPage = currentPage;
+                endPage = currentPage + 4;
             }
-
-        // calculate start and end item indexes
             var startIndex = (currentPage - 1) * pageSize;
             var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
 
-        // create an array of pages to ng-repeat in the pager control
             var pages = _.range(startPage, endPage + 1);
-
-        // return object with all pager properties required by the view
             return {
                 totalItems: totalItems,
                 currentPage: currentPage,
@@ -55,9 +56,5 @@ angular.module('playerApp')
                 endIndex: endIndex,
                 pages: pages
             };
-        }
-
-        service.GetPager = GetPager;
-
-        return service;
+        };
     }]);
