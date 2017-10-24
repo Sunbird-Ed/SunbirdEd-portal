46151c9a40af0d35f7368868b0b69ced09fdf105
Harishkumar Gangula <harishg@ilimi.in>
2017-10-24 18:44:20 +0530

    Issue #SB-405 fix: tenant info response fix

--- src/app/helpers/tenantHelper.js
+++ src/app/helpers/tenantHelper.js
@@ -1,74 +1,75 @@
-const fs = require('fs'),
-  path = require('path'),
-  async = require('async'),
-  envHelper = require('./environmentVariablesHelper.js'),
-  default_tenant = envHelper.DEFAUULT_TENANT;
+const fs = require('fs')
+const path = require('path')
+const dateFormat = require('dateformat')
+const uuidv1 = require('uuid/v1')
+const envHelper = require('./environmentVariablesHelper.js')
+const defaultTenant = envHelper.DEFAUULT_TENANT
 
 module.exports = {
-  getInfo: function(req, res) {
-    var tenantId = req.params.tenantId || envHelper.DEFAUULT_TENANT;
+  getInfo: function (req, res) {
+    var tenantId = req.params.tenantId || envHelper.DEFAUULT_TENANT
     var responseObj = {
-      logo: (req.get('X-Forwarded-Protocol') || req.protocol) + '://' + req.get('host') + '/common/images/sunbird_logo.png',
-      favicon: (req.get('X-Forwarded-Protocol') || req.protocol) + '://' + req.get('host') +'/common/images/favicon.ico',
-      poster: (req.get('X-Forwarded-Protocol') || req.protocol) + '://' + req.get('host') +'/common/images/sunbird_logo.png',
+      logo: '/common/images/sunbird_logo.png',
+      favicon: '/common/images/favicon.ico',
+      poster: '/common/images/sunbird_logo.png',
       titleName: envHelper.PORTAL_TITLE_NAME
-    };
-    //TODO: make file checking async for performance
+    }
+    // TODO: make file checking async for performance
     if (tenantId) {
       if (fs.existsSync(path.join(__dirname, '../tenant', tenantId, 'logo.png'))) {
-        responseObj.logo = (req.get('X-Forwarded-Protocol') || req.protocol) + '://' + req.get('host') + '/tenant/' + tenantId + '/logo.png';
-      }else if (default_tenant && fs.existsSync(path.join(__dirname, '../tenant', default_tenant, 'logo.png'))) {
-        responseObj.logo = (req.get('X-Forwarded-Protocol') || req.protocol) + '://' + req.get('host') + '/tenant/' + default_tenant + '/logo.png';
+        responseObj.logo = '/tenant/' + tenantId + '/logo.png'
+      } else if (defaultTenant && fs.existsSync(path.join(__dirname, '../tenant', defaultTenant, 'logo.png'))) {
+        responseObj.logo = '/tenant/' + defaultTenant + '/logo.png'
       }
       if (fs.existsSync(path.join(__dirname, '../tenant', tenantId, 'favicon.ico'))) {
-        responseObj.favicon = (req.get('X-Forwarded-Protocol') || req.protocol) + '://' + req.get('host') + '/tenant/' + tenantId + '/favicon.ico';
-      }else if (default_tenant && fs.existsSync(path.join(__dirname, '../tenant', default_tenant, 'favicon.ico'))) {
-        responseObj.favicon = (req.get('X-Forwarded-Protocol') || req.protocol) + '://' + req.get('host') + '/tenant/' + default_tenant + '/favicon.ico';
+        responseObj.favicon = '/tenant/' + tenantId + '/favicon.ico'
+      } else if (defaultTenant && fs.existsSync(path.join(__dirname, '../tenant', defaultTenant, 'favicon.ico'))) {
+        responseObj.favicon = '/tenant/' + defaultTenant + '/favicon.ico'
       }
       if (fs.existsSync(path.join(__dirname, '../tenant', tenantId, 'poster.png'))) {
-        responseObj.poster = (req.get('X-Forwarded-Protocol') || req.protocol) + '://' + req.get('host') + '/tenant/' + tenantId + '/poster.png';
-      }else if (default_tenant && fs.existsSync(path.join(__dirname, '../tenant', default_tenant, 'poster.png'))) {
-        responseObj.poster = (req.get('X-Forwarded-Protocol') || req.protocol) + '://' + req.get('host') + '/tenant/' + default_tenant + '/poster.png';
+        responseObj.poster = '/tenant/' + tenantId + '/poster.png'
+      } else if (defaultTenant && fs.existsSync(path.join(__dirname, '../tenant', defaultTenant, 'poster.png'))) {
+        responseObj.poster = '/tenant/' + defaultTenant + '/poster.png'
       }
-      module.exports.getSucessResponse(res, "api.tenant.info", responseObj);
+      module.exports.getSucessResponse(res, 'api.tenant.info', responseObj)
     } else {
-      module.exports.getSucessResponse(res, "api.tenant.info", responseObj);
+      module.exports.getSucessResponse(res, 'api.tenant.info', responseObj)
     }
   },
-  getSucessResponse: function(res, id, result) {
-    res.status(200);
+  getSucessResponse: function (res, id, result) {
+    res.status(200)
     res.send({
-      "id": id,
-      "ver": "1.0",
-      "ts": dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss:lo"),
-      "params": {
-        "resmsgid": uuidv1(),
-        "msgid": null,
-        "status": "successful",
-        "err": "",
-        "errmsg": ""
+      'id': id,
+      'ver': '1.0',
+      'ts': dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss:lo'),
+      'params': {
+        'resmsgid': uuidv1(),
+        'msgid': null,
+        'status': 'successful',
+        'err': '',
+        'errmsg': ''
       },
-      "responseCode": "OK",
-      "result": result
-    });
-    res.end();
+      'responseCode': 'OK',
+      'result': result
+    })
+    res.end()
   },
-  getErrorResponse: function(res) {
-    res.status(404);
+  getErrorResponse: function (res) {
+    res.status(404)
     res.send({
-      "id": "api.error",
-      "ver": "1.0",
-      "ts": dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss:lo"),
-      "params": {
-        "resmsgid": uuidv1(),
-        "msgid": null,
-        "status": "failed",
-        "err": "NOT_FOUND",
-        "errmsg": "Unable to find resourse requested."
+      'id': 'api.error',
+      'ver': '1.0',
+      'ts': dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss:lo'),
+      'params': {
+        'resmsgid': uuidv1(),
+        'msgid': null,
+        'status': 'failed',
+        'err': 'NOT_FOUND',
+        'errmsg': 'Unable to find resourse requested.'
       },
-      "responseCode": "NOT_FOUND",
-      "result": {}
-    });
-    res.end();
+      'responseCode': 'NOT_FOUND',
+      'result': {}
+    })
+    res.end()
   }
 }
