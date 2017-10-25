c84e4368ae36568d7913fd170c8a3f38468ba606
anuj <anujtech46>
2017-10-25 11:41:33 +0530

    Issue #SB-282 fix: On mouse hover tooltip should display the content title

--- src/app/private/scripts/controllers/workspace/allUploadedContentController.js
+++ src/app/private/scripts/controllers/workspace/allUploadedContentController.js
@@ -1,145 +1,149 @@
-'use strict';
+'use strict'
 
 angular.module('playerApp')
     .controller('AllUploadedContentController', ['contentService', 'searchService', 'config',
-        '$rootScope', '$state', 'toasterService', '$scope', 'workSpaceUtilsService', '$timeout',
-        'PaginationService',
-        function (contentService, searchService, config, $rootScope, $state,
+      '$rootScope', '$state', 'toasterService', '$scope', 'workSpaceUtilsService', '$timeout',
+      'PaginationService',
+      function (contentService, searchService, config, $rootScope, $state,
       toasterService, $scope, workSpaceUtilsService, $timeout, PaginationService) {
-            var allUploadedContent = this;
-            allUploadedContent.userId = $rootScope.userId;
-            allUploadedContent.contentStatus = ['Draft'];
-            var mimeType = config.MIME_TYPE;
-            allUploadedContent.contentMimeType = [mimeType.pdf, mimeType.youtube, mimeType.html,
-                mimeType.ePub, mimeType.h5p, mimeType.mp4, mimeType.webm
-            ];
-            allUploadedContent.sortBy = 'desc';
-            $scope.isSelected = false;
-            allUploadedContent.selectedContentItem = [];
-            allUploadedContent.message = $rootScope.errorMessages.WORKSPACE;
-            allUploadedContent.pageLimit = 9;
-            allUploadedContent.pager = {};
+        var allUploadedContent = this
+        allUploadedContent.userId = $rootScope.userId
+        allUploadedContent.contentStatus = ['Draft']
+        var mimeType = config.MIME_TYPE
+        allUploadedContent.contentMimeType = [mimeType.pdf, mimeType.youtube, mimeType.html,
+          mimeType.ePub, mimeType.h5p, mimeType.mp4, mimeType.webm
+        ]
+        allUploadedContent.sortBy = 'desc'
+        $scope.isSelected = false
+        allUploadedContent.selectedContentItem = []
+        allUploadedContent.message = $rootScope.errorMessages.WORKSPACE
+        allUploadedContent.pageLimit = 9
+        allUploadedContent.pager = {}
 
-            function showErrorMessage(isClose, message, messageType, messageText) {
-                var error = {};
-                error.showError = true;
-                error.isClose = isClose;
-                error.message = message;
-                error.messageType = messageType;
-                if (messageText) {
-                    error.messageText = messageText;
-                }
-                return error;
-            }
-
-            allUploadedContent.getAllUploadedContent = function (pageNumber) {
-                pageNumber = pageNumber || 1;
-                allUploadedContent.loader = toasterService.loader('', allUploadedContent.message
-                                        .ALL_UPLOADED.START);
-                allUploadedContent.error = {};
-                var request = {
-                    filters: {
-                        status: allUploadedContent.contentStatus,
-                        createdBy: allUploadedContent.userId,
-                        mimeType: allUploadedContent.contentMimeType,
-                        contentType: config.contributeContentType
-                    },
-                    sort_by: {
-                        lastUpdatedOn: allUploadedContent.sortBy
-                    },
-                    offset: (pageNumber - 1) * allUploadedContent.pageLimit,
-                    limit: allUploadedContent.pageLimit
-                };
-
-                searchService.search(request).then(function (res) {
-                    if (res && res.responseCode === 'OK') {
-                        allUploadedContent.loader.showLoader = false;
-                        allUploadedContent.error.showError = false;
-                        allUploadedContent.totalCount = res.result.count;
-                        allUploadedContent.pageNumber = pageNumber;
-                        allUploadedContent.allUploadedContentData = res.result.content || [];
-                        allUploadedContent.pager = PaginationService.GetPager(res.result.count,
-              pageNumber, allUploadedContent.pageLimit);
-                        if (allUploadedContent.allUploadedContentData.length === 0) {
-                            allUploadedContent.error = showErrorMessage(true,
-                $rootScope.errorMessages.WORKSPACE.ALL_UPLOADED.NO_CONTENT,
-                $rootScope.errorMessages.COMMON.NO_RESULTS);
-                        }
-                    } else {
-                        allUploadedContent.error.showError = false;
-                        allUploadedContent.loader.showLoader = false;
-                        toasterService.error(allUploadedContent.message.ALL_UPLOADED.FAILED);
-                    }
-                }).catch(function () {
-                    allUploadedContent.error.showError = false;
-                    allUploadedContent.loader.showLoader = false;
-                    toasterService.error(allUploadedContent.message.ALL_UPLOADED.FAILED);
-                });
-            };
-
-            allUploadedContent.openContentEditor = function (item) {
-                workSpaceUtilsService.openContentEditor(item, $state.current.name);
-            };
-
-            allUploadedContent.initializeUIElement = function () {
-                $('#actionDropDown').dropdown();
-            };
-
-            allUploadedContent.openRemoveContentModel = function (ContentId) {
-                allUploadedContent.removeContentId = ContentId;
-                allUploadedContent.showRemoveContentModel = true;
-                $timeout(function () {
-                    $('#removeContentModel').modal({}).modal('show');
-                }, 10);
-            };
-
-            allUploadedContent.hideRemoveContentModel = function () {
-                $('#removeContentModel').modal('hide');
-                $('#removeContentModel').modal('hide all');
-                $('#removeContentModel').modal('hide other');
-                $('#removeContentModel').modal('hide dimmer');
-                allUploadedContent.removeContentId = '';
-                allUploadedContent.showRemoveContentModel = false;
-            };
-
-            allUploadedContent.deleteContent = function (contentId) {
-                var requestData = [contentId];
-                allUploadedContent.hideRemoveContentModel();
-                allUploadedContent.loader = toasterService.loader('', allUploadedContent.message
-          .RETIRE_CONTENT.START);
-                var request = {
-                    contentIds: requestData
-                };
-                contentService.retire(request).then(function (res) {
-                    if (res && res.responseCode === 'OK') {
-                        allUploadedContent.loader.showLoader = false;
-                        allUploadedContent.selectedContentItem = [];
-                        toasterService.success(allUploadedContent.message.RETIRE_CONTENT.SUCCESS);
-                        allUploadedContent.allUploadedContentData = workSpaceUtilsService
-              .removeContentLocal(allUploadedContent.allUploadedContentData, requestData);
-                        allUploadedContent.pager = PaginationService
-              .GetPager(allUploadedContent.totalCount - requestData.length,
-                allUploadedContent.pageNumber, allUploadedContent.pageLimit);
-                        if (allUploadedContent.allUploadedContentData.length === 0) {
-                            allUploadedContent.error = showErrorMessage(true,
-                $rootScope.errorMessages.WORKSPACE.ALL_UPLOADED.NO_CONTENT,
-                $rootScope.errorMessages.COMMON.NO_RESULTS);
-                        }
-                    } else {
-                        allUploadedContent.loader.showLoader = false;
-                        toasterService.error(allUploadedContent.message.RETIRE_CONTENT.FAILED);
-                    }
-                }).catch(function () {
-                    allUploadedContent.loader.showLoader = false;
-                    toasterService.error(allUploadedContent.message.RETIRE_CONTENT.FAILED);
-                });
-            };
-
-            allUploadedContent.setPage = function (page) {
-                if (page < 1 || page > allUploadedContent.pager.totalPages) {
-                    return;
-                }
-                allUploadedContent.getAllUploadedContent(page);
-            };
+        function showErrorMessage (isClose, message, messageType, messageText) {
+          var error = {}
+          error.showError = true
+          error.isClose = isClose
+          error.message = message
+          error.messageType = messageType
+          if (messageText) {
+            error.messageText = messageText
+          }
+          return error
         }
-    ]);
+
+        allUploadedContent.getAllUploadedContent = function (pageNumber) {
+          pageNumber = pageNumber || 1
+          allUploadedContent.loader = toasterService.loader('', allUploadedContent.message
+                                        .ALL_UPLOADED.START)
+          allUploadedContent.error = {}
+          var request = {
+            filters: {
+              status: allUploadedContent.contentStatus,
+              createdBy: allUploadedContent.userId,
+              mimeType: allUploadedContent.contentMimeType,
+              contentType: config.contributeContentType
+            },
+            sort_by: {
+              lastUpdatedOn: allUploadedContent.sortBy
+            },
+            offset: (pageNumber - 1) * allUploadedContent.pageLimit,
+            limit: allUploadedContent.pageLimit
+          }
+
+          searchService.search(request).then(function (res) {
+            if (res && res.responseCode === 'OK') {
+              allUploadedContent.loader.showLoader = false
+              allUploadedContent.error.showError = false
+              allUploadedContent.totalCount = res.result.count
+              allUploadedContent.pageNumber = pageNumber
+              allUploadedContent.allUploadedContentData = res.result.content || []
+              allUploadedContent.pager = PaginationService.GetPager(res.result.count,
+              pageNumber, allUploadedContent.pageLimit)
+              if (allUploadedContent.allUploadedContentData.length === 0) {
+                allUploadedContent.error = showErrorMessage(true,
+                $rootScope.errorMessages.WORKSPACE.ALL_UPLOADED.NO_CONTENT,
+                $rootScope.errorMessages.COMMON.NO_RESULTS)
+              }
+            } else {
+              allUploadedContent.error.showError = false
+              allUploadedContent.loader.showLoader = false
+              toasterService.error(allUploadedContent.message.ALL_UPLOADED.FAILED)
+            }
+          }).catch(function () {
+            allUploadedContent.error.showError = false
+            allUploadedContent.loader.showLoader = false
+            toasterService.error(allUploadedContent.message.ALL_UPLOADED.FAILED)
+          })
+        }
+
+        allUploadedContent.openContentEditor = function (item) {
+          workSpaceUtilsService.openContentEditor(item, $state.current.name)
+        }
+
+        allUploadedContent.initializeUIElement = function () {
+          $('#actionDropDown').dropdown()
+        }
+
+        allUploadedContent.openRemoveContentModel = function (ContentId) {
+          allUploadedContent.removeContentId = ContentId
+          allUploadedContent.showRemoveContentModel = true
+          $timeout(function () {
+            $('#removeContentModel').modal({}).modal('show')
+          }, 10)
+        }
+
+        allUploadedContent.hideRemoveContentModel = function () {
+          $('#removeContentModel').modal('hide')
+          $('#removeContentModel').modal('hide all')
+          $('#removeContentModel').modal('hide other')
+          $('#removeContentModel').modal('hide dimmer')
+          allUploadedContent.removeContentId = ''
+          allUploadedContent.showRemoveContentModel = false
+        }
+
+        allUploadedContent.deleteContent = function (contentId) {
+          var requestData = [contentId]
+          allUploadedContent.hideRemoveContentModel()
+          allUploadedContent.loader = toasterService.loader('', allUploadedContent.message
+          .RETIRE_CONTENT.START)
+          var request = {
+            contentIds: requestData
+          }
+          contentService.retire(request).then(function (res) {
+            if (res && res.responseCode === 'OK') {
+              allUploadedContent.loader.showLoader = false
+              allUploadedContent.selectedContentItem = []
+              toasterService.success(allUploadedContent.message.RETIRE_CONTENT.SUCCESS)
+              allUploadedContent.allUploadedContentData = workSpaceUtilsService
+              .removeContentLocal(allUploadedContent.allUploadedContentData, requestData)
+              allUploadedContent.pager = PaginationService
+              .GetPager(allUploadedContent.totalCount - requestData.length,
+                allUploadedContent.pageNumber, allUploadedContent.pageLimit)
+              if (allUploadedContent.allUploadedContentData.length === 0) {
+                allUploadedContent.error = showErrorMessage(true,
+                $rootScope.errorMessages.WORKSPACE.ALL_UPLOADED.NO_CONTENT,
+                $rootScope.errorMessages.COMMON.NO_RESULTS)
+              }
+            } else {
+              allUploadedContent.loader.showLoader = false
+              toasterService.error(allUploadedContent.message.RETIRE_CONTENT.FAILED)
+            }
+          }).catch(function () {
+            allUploadedContent.loader.showLoader = false
+            toasterService.error(allUploadedContent.message.RETIRE_CONTENT.FAILED)
+          })
+        }
+
+        allUploadedContent.setPage = function (page) {
+          if (page < 1 || page > allUploadedContent.pager.totalPages) {
+            return
+          }
+          allUploadedContent.getAllUploadedContent(page)
+        }
+
+        allUploadedContent.initTocPopup = function () {
+          $('.cardTitleEllipse').popup({inline: true})
+        }
+      }
+    ])
