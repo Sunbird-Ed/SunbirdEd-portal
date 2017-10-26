7bd7c27b2bcda15eedc8bf1ee6785e23fdf2f22f
sunil <sunils@ilimi.in>
2017-10-26 16:35:51 +0530

    Issue #SB-356 update: announcement   changes: - added data model - Object store Interface

--- src/app/helpers/announcement/model/MetricsModel.js
+++ src/app/helpers/announcement/model/MetricsModel.js
@@ -1,10 +1,10 @@
 let Joi = require('joi')
 
 module.exports = Joi.object().keys({
-	id: Joi.string().required(),		
-	userid: Joi.string().required(),
-	announcementid: Joi.string().required(), 
-	activity: Joi.string().required(), 
-	channel: Joi.string(),
-	createddate: Joi.string().required()
+  id: Joi.string().required(),
+  userid: Joi.string().required(),
+  announcementid: Joi.string().required(),
+  activity: Joi.string().required(),
+  channel: Joi.string(),
+  createddate: Joi.string().required()
 })
