8530cebe336502637527240de5a56596c4c6d779
sunil <sunils@ilimi.in>
2017-10-26 16:35:51 +0530

    Issue #SB-356 update: announcement   changes: - added data model - Object store Interface

--- src/app/helpers/announcement/controller.js
+++ src/app/helpers/announcement/controller.js
@@ -1,20 +1,28 @@
 let Joi = require('joi')
 let HttpStatus = require('http-status-codes')
 const _ = require('lodash')
-var async = require('asyncawait/async');
-var await = require('asyncawait/await');
+let async = require('asyncawait/async')
+let await = require('asyncawait/await')
+const ObjectStore = require('./model')
 
 let create = async(function(requestObj) {
   // validate parameters
   let request = validateCreateRequest(requestObj)
   if (!request.isValid) throw { msg: request.error, httpCode: HttpStatus.BAD_REQUEST }
 
-  let userPermissions = await (checkUserPermissions({ user: requestObj.body.request.createdBy }))  
-  if (userPermissions.data.hasCreateAccess) {
+  try {
+    var userPermissions = await (checkUserPermissions({ user: requestObj.body.request.createdBy }))      
+  } catch(error) {
+    throw { msg: 'user does not exist!', httpCode: HttpStatus.BAD_REQUEST }
+  }
+
+  if (!userPermissions.data.hasCreateAccess) throw { msg: 'user does not have create access', httpCode: HttpStatus.BAD_REQUEST }
+
+  try {  
     let announcementObj = await (createAnnouncement(requestObj.body.request))
     if (announcementObj.data) return { data: announcementObj.data }
-  } else {
-    throw { msg: 'user does not have create access', httpCode: HttpStatus.BAD_REQUEST }
+  } catch(error) {
+    throw { msg: 'unable to process the request!' }
   }
 })
 
@@ -28,6 +36,7 @@ function validateCreateRequest(requestObj) {
     'target': Joi.array().items(Joi.string().required()).required(),
     'links': Joi.array().items(Joi.string().required()) // optional
   }, { abortEarly: false })
+
   if (validation.error != null) {
     let messages = []
     _.forEach(validation.error.details, function(error, index) {
@@ -48,8 +57,17 @@ function checkUserPermissions(data) {
 
 function createAnnouncement(data) {
   return new Promise(function(resolve, reject) {
-    resolve({ data: {} })
-    //reject({ msg: 'unable to create announcement' })
+    if (!data) reject({ msg: 'invalid request'})
+
+    let query = { table: ObjectStore.MODEL.ANNOUNCEMENT, values: { /*"id": "123-1-231-32-123", "sourceid": "bangalore.teachers.org", "createddate": "27-10-17", "userid": "123-123-12313-123"*/ } }
+    ObjectStore.createObject(query)
+    .then(function(data) {
+      resolve({ data: data })
+    })
+    .catch(function(error) {
+      reject({ msg: 'unable to create announcement' })
+    })
+    
   })
 }
 
--- src/app/helpers/announcement/model/AnnouncementType.js
+++
@@ -1,9 +0,0 @@
-let Joi = require('joi')
-
-module.exports = Joi.object().keys({
-	id: Joi.string().required(),		
-	sourceid: Joi.string().required(),
-	createddate: Joi.string().required(),
-	typename: Joi.string().required(), 	
-	status: Joi.string()	
-})
