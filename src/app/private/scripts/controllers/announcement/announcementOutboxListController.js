e3055227d374de365cf11eaa1599721b9fe9bb1f
Sourav <sourav_d@techjoomla.com>
2017-10-26 18:00:52 +0530

    Issue #SB-421 feat:Changes on announcement inbox/outbox list

--- src/app/private/scripts/controllers/announcement/announcementOutboxListController.js
+++ src/app/private/scripts/controllers/announcement/announcementOutboxListController.js
@@ -4,45 +4,29 @@ angular.module('playerApp')
   .controller('announcementOutboxListController', ['$rootScope', '$scope',
     'announcementService', '$timeout', '$state', '$stateParams', 'toasterService', 'adminService',
     function ($rootScope, $scope, announcementService, $timeout, $state, $stateParams, toasterService, adminService) {
-      var announcementOutboxData = this;
-      announcementOutboxData.showLoader = true;
+      var announcementOutboxData = this
+      announcementOutboxData.showLoader = true
 
       announcementOutboxData.renderAnnouncementList = function () {
-       
-       
-       
-       
-       
-       
-       
-       
-       //~ announcementService.getAnnouncementList().then(function (apiResponse) {
+        // ~ announcementService.getAnnouncementList().then(function (apiResponse) {
 
-            //~ if (apiResponse && apiResponse.responseCode === 'OK') {
-            //~ } else {
-                //~ toasterService.error(apiResponse.params.errmsg);
-                //~ announcementOutboxData.showDataDiv = false;
-            //~ }
-        //~ })
-          //~ .catch(function (err) {
-              //~ console.log(err);
-          //~ })
-          //~ .finally(function () {
-              //~ announcementOutboxData.showLoader = false;
-          //~ });
-          
-          
-       announcementOutboxData.listData = announcementService.getOutBoxAnnouncementList();
-       announcementOutboxData.listData = JSON.parse(announcementOutboxData.listData);
-       announcementOutboxData.listData = announcementOutboxData.listData.result.announcements;
-       announcementOutboxData.showLoader = false;
-       
-       
-       
-       
-       
-       
-       
+        // ~ if (apiResponse && apiResponse.responseCode === 'OK') {
+        // ~ } else {
+        // ~ toasterService.error(apiResponse.params.errmsg);
+        // ~ announcementOutboxData.showDataDiv = false;
+        // ~ }
+        // ~ })
+        // ~ .catch(function (err) {
+        // ~ console.log(err);
+        // ~ })
+        // ~ .finally(function () {
+        // ~ announcementOutboxData.showLoader = false;
+        // ~ });
+
+        announcementOutboxData.listData = announcementService.getOutBoxAnnouncementList()
+        announcementOutboxData.listData = JSON.parse(announcementOutboxData.listData)
+        announcementOutboxData.listData = announcementOutboxData.listData.result.announcements
+        announcementOutboxData.showLoader = false
       }
     }
 
