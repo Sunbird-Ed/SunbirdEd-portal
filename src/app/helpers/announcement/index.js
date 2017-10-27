97a038442f4a366922cfe7645bf5aa5649fd2f65
sunil <sunils@ilimi.in>
2017-10-27 13:11:05 +0530

    Issue #SB-356 update: announcement   changes: - added test cases - code refactor

--- src/app/helpers/announcement/index.js
+++ src/app/helpers/announcement/index.js
@@ -4,30 +4,25 @@ let HttpStatus = require('http-status-codes')
 let announcementController = require('./controller.js')
 const API_ID = 'api.plugin.announcement'
 
-router.post('/create', validateRequestStructure, function (requestObj, responseObj) {
-  announcementController.create(requestObj)
-  .then(function(data) {
+router.post('/create', (requestObj, responseObj) => {
+  announcementController.create(requestObj.body)
+  .then((data) => {
     sendSuccessResponse(responseObj, 'create', data, HttpStatus.CREATED)
   })
-  .catch(function(err) {
-    sendErrorResponse(responseObj, 'create', err.msg, err.httpCode)    
+  .catch((err) => {
+    sendErrorResponse(responseObj, 'create', err.msg, err.statusCode)
   })
 })
 
-router.get('/get/:id', function (requestObj, responseObj) {
+router.get('/get/:id', (requestObj, responseObj) => {
   announcementController.getAnnouncementById(requestObj)
-  .then(function(data) {
+  .then((data) => {
     sendSuccessResponse(responseObj, 'get.id', data)
   })
-  .catch(function(err) {
-    sendErrorResponse(responseObj, 'create', err.msg)
+  .catch((err) => {
+    sendErrorResponse(responseObj, 'get.id', err.msg)
   })
 })
-
-function validateRequestStructure (requestObj, responseObj, next) {
-  if (typeof requestObj.body !== 'object' || typeof requestObj.body.request !== 'object') return Response.error(responseObj, 'Invalid request structure', HttpStatus.BAD_REQUEST)
-  next()
-}
 
 function sendSuccessResponse (res, id, result, code) {
   res.status(code || HttpStatus.OK)
