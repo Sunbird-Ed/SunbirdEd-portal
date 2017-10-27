0eb578528216449f2e2d34a4d34f3c63fd2750fd
sunil <sunils@ilimi.in>
2017-10-27 13:11:05 +0530

    Issue #SB-356 update: announcement   changes: - added test cases - code refactor

--- src/app/helpers/announcement/model/index.js
+++ src/app/helpers/announcement/model/index.js
@@ -9,104 +9,134 @@ let _ = require('lodash')
 let async = require('asyncawait/async')
 let await = require('asyncawait/await')
 
-const TableModelMapping = {
-  'Announcement': AnnouncementModel,
-  'AnnouncementType': AnnouncementTypeModel,
-  'Attachment': AttachmentModel,
-  'Metrics': MetricsModel,
-  'UserPermissions': UserPermissionsModel
-}
+class ObjectStore {
 
-const MODEL = {
-  'ANNOUNCEMENT': 'Announcement',
-  'ANNOUNCEMENTTYPE': 'AnnouncementType',
-  'ATTACHMENT': 'Attachment',
-  'METRICS': 'Metrics',
-  'USERPERMISSIONS': 'UserPermissions'	
-}
+  constructor() {
+    this.TableModelMapping = {
+      'Announcement': AnnouncementModel,
+      'AnnouncementType': AnnouncementTypeModel,
+      'Attachment': AttachmentModel,
+      'Metrics': MetricsModel,
+      'UserPermissions': UserPermissionsModel
+    }
 
-Object.freeze(MODEL)
-
-function validateRequest(data) {
-  if (data && TableModelMapping[data.table]) return true
-  return false
-}
-
-let createObject = async(function(data) {
-  if (!validateRequest(data)) throw { msg: 'invalid request!' }
-  if (!data.values) throw { msg: 'values required!' } 
-
-  let validation = Joi.validate(data.values, TableModelMapping[data.table], { abortEarly: false })
-  if (validation.error != null) {
-    let messages = []
-    _.forEach(validation.error.details, function(error, index) {
-      messages.push({ field: error.path[0], description: error.message })
-    })
-    throw { msg: messages }
+    this.__MODEL = {
+      'ANNOUNCEMENT': 'Announcement',
+      'ANNOUNCEMENTTYPE': 'AnnouncementType',
+      'ATTACHMENT': 'Attachment',
+      'METRICS': 'Metrics',
+      'USERPERMISSIONS': 'UserPermissions'
+    }
   }
 
-  //TODO: make API request to cassandra CRUD API
-  //TODO: create document name same as tabel name
-  return { status: 'success' }
-})
-
-let findObject = async(function(data) {
-  if (!validateRequest(data)) throw { msg: 'table not found!' }
-  if (!data.query) throw { msg: 'invalid query!' }
-
-  try {
-    _.forIn(data.query, function(value, key) {
-      let subSchema = Joi.reach(TableModelMapping[data.table], key)
-      let validation = Joi.validate(value, subSchema)
-      if (validation.error) throw { msg: 'invalid query fields!' }
-    })
-  } catch (error) {
-    throw { msg: 'invalid query fields!' }
+  get MODEL() {
+    return this.__MODEL
   }
 
-  //TODO: make API request to cassandra CRUD API
-  return { status: 'success' }
-})
-
-let getObjectById = async(function(data) {
-  if (!validateRequest(data)) throw { msg: 'table not found!' }
-  if (!data.id) throw { msg: 'Id is required!!' }
-
-  return await (findObject({ table: data.table, query: { id: data.id } }))
-})
-
-let updateObjectById = async(function(data) {
-  if (!validateRequest(data)) throw { msg: 'table not found!' }
-  if (!data.data) throw { msg: 'invalid query!' }
-  if (!data.id) throw { msg: 'Id is required!' }
-
-  try {
-    _.forIn(data.data, function(value, key) {
-      let subSchema = Joi.reach(TableModelMapping[data.table], key)
-      let validation = Joi.validate(value, subSchema)
-      if (validation.error) throw { msg: 'invalid query fields!' }
-    })
-  } catch (error) {
-    throw { msg: 'invalid query fields!' }
+  createObject(data) {
+    return this.__createObject()(data)
   }
 
-  //TODO: make API request to cassandra CRUD API
-  return { status: 'success' }
-})
+  __createObject() {
+    return async((data) => {
+      if (!this.__validateRequest(data)) throw { msg: 'invalid request!' }
+      if (!data.values) throw { msg: 'values required!' }
 
-let deleteObjectById = async(function(data) {
-  if (!validateRequest(data)) throw { msg: 'table not found!' }
-  if (!data.id) throw { msg: 'Id is required!' }
+      let validation = Joi.validate(data.values, this.TableModelMapping[data.table], { abortEarly: false })
+      if (validation.error != null) {
+        let messages = []
+        _.forEach(validation.error.details, (error, index) => {
+          messages.push({ field: error.path[0], description: error.message })
+        })
+        throw { msg: messages }
+      }
 
-  //TODO: make API request to cassandra CRUD API
-  return { status: 'success' }
-})
+      //TODO: make API request to cassandra CRUD API
+      //TODO: create document name same as table name
+      return { status: 'success' }
+    })
+  }
 
-module.exports = {
-  createObject,
-  findObject,
-  getObjectById,
-  updateObjectById,
-  deleteObjectById,
-  MODEL
+  findObject(data) {
+    return this.__findObject()(data)
+  }
+
+  __findObject() {
+    return async((data) => {
+      if (!this.__validateRequest(data)) throw { msg: 'table not found!' }
+      if (!data.query) throw { msg: 'invalid query!' }
+
+      try {
+        _.forIn(data.query, (value, key) => {
+          let subSchema = Joi.reach(this.TableModelMapping[data.table], key)
+          let validation = Joi.validate(value, subSchema)
+          if (validation.error) throw { msg: 'invalid query fields!' }
+        })
+      } catch (error) {
+        throw { msg: 'invalid query fields!' }
+      }
+
+      //TODO: make API request to cassandra CRUD API
+      return { status: 'success' }
+    })
+  }
+
+  __validateRequest(data) {
+    if (data && this.TableModelMapping[data.table]) return true
+    return false
+  }
+
+  getObjectById(data) {
+    return this.__getObjectById()(data)
+  }
+
+  __getObjectById() {
+    return async((data) => {
+      if (!this.__validateRequest(data)) throw { msg: 'table not found!' }
+      if (!data.id) throw { msg: 'Id is required!!' }
+
+      return await (this.findObject({ table: data.table, query: { id: data.id } }))
+    })
+  }
+
+  updateObjectById(data) {
+    return this.__updateObjectById()(data)
+  }
+
+  __updateObjectById() {
+    return async((data) => {
+      if (!this.__validateRequest(data)) throw { msg: 'table not found!' }
+      if (!data.data) throw { msg: 'invalid query!' }
+      if (!data.id) throw { msg: 'Id is required!' }
+
+      try {
+        _.forIn(data.data, (value, key) => {
+          let subSchema = Joi.reach(this.TableModelMapping[data.table], key)
+          let validation = Joi.validate(value, subSchema)
+          if (validation.error) throw { msg: 'invalid query fields!' }
+        })
+      } catch (error) {
+        throw { msg: 'invalid query fields!' }
+      }
+
+      //TODO: make API request to cassandra CRUD API
+      return { status: 'success' }
+    })
+  }
+
+  deleteObjectById(data) {
+    return this.__deleteObjectById()(data)
+  }
+
+  __deleteObjectById() {
+    return async((data) => {
+      if (!this.__validateRequest(data)) throw { msg: 'table not found!' }
+      if (!data.id) throw { msg: 'Id is required!' }
+
+      //TODO: make API request to cassandra CRUD API
+      return { status: 'success' }
+    })
+  }
 }
+
+module.exports = new ObjectStore()
