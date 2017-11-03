25326b28844f0e89a6c6e07cb9bedbafc81e5649
Sourav <sourav_d@techjoomla.com>
2017-11-03 15:17:17 +0530

    Issue #SB-421 feat:removing conflicts

--- src/app/private/scripts/controllers/announcement/createAnnouncementController.js
+++ src/app/private/scripts/controllers/announcement/createAnnouncementController.js
@@ -1,166 +1,165 @@
-'use strict';
+'use strict'
 
 angular.module('playerApp')
   .controller('createAnnouncementCtrl', ['$rootScope', '$scope', '$timeout', '$state', '$stateParams', 'config', 'toasterService',
     'permissionsService', 'dashboardService',
-    function($rootScope, $scope, $timeout, $state, $stateParams, config, toasterService, permissionsService, dashboardService) {
-
+    function ($rootScope, $scope, $timeout, $state, $stateParams, config, toasterService, permissionsService, dashboardService) {
       // Initialize variables
-      var createAnn = this;
+      var createAnn = this
 
         // TODO - use api to get values
-        createAnn.org = ['Org 1', 'Org 2', 'Org 3'];
-        createAnn.announcementType = ['Type 1', 'Type 2', 'Type 3'];
-        createAnn.disableBtn = true;
-        createAnn.showUrlField = false;
-        createAnn.isLastStep = false;
-        createAnn.repeatableWebLinks = [];
-        createAnn.isMetaModified = false;
+      createAnn.org = ['Org 1', 'Org 2', 'Org 3']
+      createAnn.announcementType = ['Type 1', 'Type 2', 'Type 3']
+      createAnn.disableBtn = true
+      createAnn.showUrlField = false
+      createAnn.isLastStep = false
+      createAnn.repeatableWebLinks = []
+      createAnn.isMetaModified = false
 
         // Initialize modal
-        createAnn.initializeModal = function(){
+      createAnn.initializeModal = function () {
         	$timeout(function () {
-          		$('#announcementType').dropdown();
-          		$('#orgDropdown').dropdown();
-	        }, 100);
+          		$('#announcementType').dropdown()
+          		$('#orgDropdown').dropdown()
+	        }, 100)
 
-	        createAnn.stepsHandler(1, true, false, false, false);
-        }
+	        createAnn.stepsHandler(1, true, false, false, false)
+      }
 
-        createAnn.createAnnouncement = function (){
-          $('#createAnnouncementModal').modal({
+      createAnn.createAnnouncement = function () {
+        $('#createAnnouncementModal').modal({
           	closable: false,
           	debug: true,
           	onHide: function () {
           		// TODO - Show confirmation before closing modal
           		if (!createAnn.isLastStep) {
-	          		if (createAnn.isMetaModified && confirm("Changes that you made may not be saved.")) {
-	        			createAnn.refreshFormValues();
-	        			return true;
+	          		if (createAnn.isMetaModified && confirm('Changes that you made may not be saved.')) {
+	        			createAnn.refreshFormValues()
+	        			return true
 	          		}
 
-	          		if (!createAnn.isMetaModified){
-	          			return true;
+	          		if (!createAnn.isMetaModified) {
+	          			return true
 	          		}
-	          		return false;
+	          		return false
 	          	}
-	          	return true;
+	          	return true
           	},
-          	onApprove : function a(){
+          	onApprove: function a () {
           		// Make api call to save data
-          		createAnn.isLastStep = true;
-          		createAnn.refreshFormValues();
+          		createAnn.isLastStep = true
+          		createAnn.refreshFormValues()
 		    	$('#announcementSuccessModal').modal({
 		          	closable: false
-		        }).modal('show');
+		        }).modal('show')
 		    },
-          	selector    : {
-        		approve  : '#sendAnnouncement'
+          	selector: {
+        		approve: '#sendAnnouncement'
       		}
-          }).modal('show');
-        }
+        }).modal('show')
+      }
 
-        createAnn.addNewLink = function() {
-          var newItemNo = createAnn.repeatableWebLinks.length+1;
-          createAnn.repeatableWebLinks.push({'id':'choice'+newItemNo});
-          createAnn.showUrlField = true;
-        };
+      createAnn.addNewLink = function () {
+        var newItemNo = createAnn.repeatableWebLinks.length + 1
+        createAnn.repeatableWebLinks.push({'id': 'choice' + newItemNo})
+        createAnn.showUrlField = true
+      }
 
-        createAnn.removeLink = function(index) {
-          	createAnn.repeatableWebLinks.splice(index, 1);
-          	delete createAnn.data.link[index];
-          	createAnn.showUrlField = createAnn.repeatableWebLinks.length == '0' ? false : true;
-        };
+      createAnn.removeLink = function (index) {
+          	createAnn.repeatableWebLinks.splice(index, 1)
+          	delete createAnn.data.link[index]
+          	createAnn.showUrlField = createAnn.repeatableWebLinks.length != '0'
+      }
 
         // Function to post form data
-        createAnn.selectRecipients = function(){
-        	createAnn.stepsHandler(2, false, true, false, false);
-        }
+      createAnn.selectRecipients = function () {
+        	createAnn.stepsHandler(2, false, true, false, false)
+      }
 
         // Function to detect input box change event
-        createAnn.detectChange = function (){
-        	createAnn.enableRecepientBtn();
+      createAnn.detectChange = function () {
+        	createAnn.enableRecepientBtn()
     	}
 
     	// Function to detect dropdwon value change event
-    	createAnn.detectDropdownChange = function (){
-    		createAnn.enableRecepientBtn();
+    	createAnn.detectDropdownChange = function () {
+    		createAnn.enableRecepientBtn()
     	}
 
     	// Function to track back button change
-    	createAnn.previousStep = function(item){
-    		var step = $('#annBackBtn').attr('data-current-state');
+    	createAnn.previousStep = function (item) {
+    		var step = $('#annBackBtn').attr('data-current-state')
     		switch (step) {
-		        case "2":
-		        	createAnn.stepsHandler(1, true, false, false, false);
-		            break;
-		        case "3":
-		        	createAnn.stepsHandler(2, false, true, false, false);
-		            break;
-		        case "4":
-		        	createAnn.stepsHandler(3, false, false, true, false);
-		            break;
+		        case '2':
+		        	createAnn.stepsHandler(1, true, false, false, false)
+		            break
+		        case '3':
+		        	createAnn.stepsHandler(2, false, true, false, false)
+		            break
+		        case '4':
+		        	createAnn.stepsHandler(3, false, false, true, false)
+		            break
 		        default:
 		    }
     	}
 
-    	createAnn.stepsHandler = function(stepNumber, step1, step2, step3, step4){
-    		createAnn.showStepOne   = step1;
-    		createAnn.showStepTwo   = step2;
-    		createAnn.showStepThree = step3;
-    		createAnn.showStepFour  = step4;
-    		createAnn.stepNumber    = stepNumber;
+    	createAnn.stepsHandler = function (stepNumber, step1, step2, step3, step4) {
+    		createAnn.showStepOne = step1
+    		createAnn.showStepTwo = step2
+    		createAnn.showStepThree = step3
+    		createAnn.showStepFour = step4
+    		createAnn.stepNumber = stepNumber
     	}
 
     	// Function to preview announcement
-    	createAnn.previewAnn = function(){
-    		createAnn.stepsHandler(4, false, false, false, true);
-    		var linkArray = createAnn.data.link ? Object.keys(createAnn.data.link).map(e=>createAnn.data.link[e]) : [];
+    	createAnn.previewAnn = function () {
+    		createAnn.stepsHandler(4, false, false, false, true)
+    		var linkArray = createAnn.data.link ? Object.keys(createAnn.data.link).map(e => createAnn.data.link[e]) : []
 			// TODO - show announcement preview
-        	createAnn.previewData = {"sourceId":"some-organisation-id","type":createAnn.data.announcementType,"links":linkArray,"title":createAnn.data.title,"description":createAnn.data.description,"target":["teachers"],"attachments":[{"title":"circular.pdf","downloadURL":"https://linktoattachment","mimetype":"application/pdf"}]};
+        	createAnn.previewData = {'sourceId': 'some-organisation-id', 'type': createAnn.data.announcementType, 'links': linkArray, 'title': createAnn.data.title, 'description': createAnn.data.description, 'target': ['teachers'], 'attachments': [{'title': 'circular.pdf', 'downloadURL': 'https://linktoattachment', 'mimetype': 'application/pdf'}]}
     	}
 
     	// Function to confirm recipients
-    	createAnn.confirmRecipients = function(){
-    		createAnn.stepsHandler(3, false, false, true, false);
+    	createAnn.confirmRecipients = function () {
+    		createAnn.stepsHandler(3, false, false, true, false)
     	}
 
     	// Function to enable / disable RecepientBtn
-    	createAnn.enableRecepientBtn = function() {
-	        if (createAnn.data.title && createAnn.data.from
-	        	&& createAnn.data.announcementType &&
-	        	(createAnn.data.description || createAnn.attachment.length)){
-	        	createAnn.disableBtn = false;
+    	createAnn.enableRecepientBtn = function () {
+	        if (createAnn.data.title && createAnn.data.from &&
+	        	createAnn.data.announcementType &&
+	        	(createAnn.data.description || createAnn.attachment.length)) {
+	        	createAnn.disableBtn = false
 	        } else {
-	        	createAnn.disableBtn = true;
+	        	createAnn.disableBtn = true
 	        }
-	        createAnn.isMetaModified = true;
+	        createAnn.isMetaModified = true
 	    }
 
-	    createAnn.detectUrlChange = function(index){
-	    	createAnn.isMetaModified = true;
+	    createAnn.detectUrlChange = function (index) {
+	    	createAnn.isMetaModified = true
 	    }
 
-	    createAnn.refreshFormValues = function(){
-	    	createAnn.stepsHandler(1, true, false, false, false);
-			createAnn.disableBtn    = true;
-			$('#announcementType').dropdown('restore defaults');
-			$('#orgDropdown').dropdown('restore defaults');
-			$('#createAnnouncementModal').modal('refresh');
-			$('#announcementForm').form('reset');
-			createAnn.data = {};
-			createAnn.isMetaModified = false;
-			createAnn.repeatableWebLinks.length = 0;
-			createAnn.showUrlField = false;
-			createAnn.initializeFileUploader();
+	    createAnn.refreshFormValues = function () {
+	    	createAnn.stepsHandler(1, true, false, false, false)
+      createAnn.disableBtn = true
+      $('#announcementType').dropdown('restore defaults')
+      $('#orgDropdown').dropdown('restore defaults')
+      $('#createAnnouncementModal').modal('refresh')
+      $('#announcementForm').form('reset')
+      createAnn.data = {}
+      createAnn.isMetaModified = false
+      createAnn.repeatableWebLinks.length = 0
+      createAnn.showUrlField = false
+      createAnn.initializeFileUploader()
 	    }
 
-	    createAnn.saveAnnouncement = function(){
+	    createAnn.saveAnnouncement = function () {
 	    }
 
-		createAnn.attachment = [];
-		createAnn.index = 0;
-        createAnn.initializeFileUploader = function () {
+      createAnn.attachment = []
+      createAnn.index = 0
+      createAnn.initializeFileUploader = function () {
 	        $timeout(function () {
 	            createAnn.manualUploader = new qq.FineUploader({
 	                element: document.getElementById('fine-uploader-manual-trigger'),
@@ -184,31 +183,31 @@ angular.module('playerApp')
         				mode: 'default',
         				responseProperty: 'error'
     				},
-    				showMessage: function(message) {
-        				toasterService.error(message);
+    				showMessage: function (message) {
+        				toasterService.error(message)
     				},
 	                callbacks: {
 	                    onComplete: function (id, name, responseJSON, xhr) {
 	                    	// TODO - push attachement api success response
-	                        createAnn.attachment.push('A', 'B');
-	                        createAnn.enableRecepientBtn();
+	                        createAnn.attachment.push('A', 'B')
+	                        createAnn.enableRecepientBtn()
 	                    },
 	                    onSubmitted: function (id, name) {
 	                    },
 	                    onCancel: function () {
 	                        document.getElementById('hide-section-with-button')
-	                                                .style.display = 'block';
+	                                                .style.display = 'block'
 	                    },
-	                    onError: function(id, name, errorReason, xhrOrXdr) {
-				            toasterService.error(qq.format("Error on file number {} - {}.  Reason: {}", id, name, errorReason));
-				        },
+	                    onError: function (id, name, errorReason, xhrOrXdr) {
+				            toasterService.error(qq.format('Error on file number {} - {}.  Reason: {}', id, name, errorReason))
+				        }
 	                }
-	            });
+	            })
 
 	            window.cancelUploadFile = function () {
-	                document.getElementById('hide-section-with-button').style.display = 'block';
-	            };
-	        }, 300);
-	    };
+	                document.getElementById('hide-section-with-button').style.display = 'block'
+	            }
+	        }, 300)
+	    }
     }
   ])
