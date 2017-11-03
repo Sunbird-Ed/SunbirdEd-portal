6fc564c77c03e91e9a5d106271b8fdce1bd53625
Nilesh More <nilesh_m@tekditechnologies.com>
2017-11-03 23:32:29 +0530

    Issue #SB-422 test: test cases for announcement workflow

--- src/app/private/scripts/controllers/announcement/createAnnouncementController.js
+++ src/app/private/scripts/controllers/announcement/createAnnouncementController.js
@@ -20,42 +20,41 @@ angular.module('playerApp')
 
         // Initialize modal
       createAnn.initializeModal = function () {
-        	$timeout(function () {
-          		$('#announcementType').dropdown()
-          		$('#orgDropdown').dropdown()
-	        }, 100)
-
+          $timeout(function () {
+              $('#announcementType').dropdown()
+              $('#orgDropdown').dropdown()
+          }, 100)
       }
 
       createAnn.createAnnouncement = function () {
         $('#createAnnouncementModal').modal({
-          	closable: false,
-          	onHide: function () {
-          		// TODO - Show confirmation before closing modal
-          		if (!createAnn.isLastStep) {
-	          		if (createAnn.isMetaModified && confirm('Changes that you made may not be saved.')) {
-	        			createAnn.refreshFormValues()
-	        			return true
-	          		}
+            closable: false,
+            onHide: function () {
+              // TODO - Show confirmation before closing modal
+              if (!createAnn.isLastStep) {
+                if (createAnn.isMetaModified && confirm('Changes that you made may not be saved.')) {
+                createAnn.refreshFormValues()
+                return true
+                }
 
-	          		if (!createAnn.isMetaModified) {
-	          			return true
-	          		}
-	          		return false
-	          	}
-	          	return true
-          	},
-          	onApprove: function() {
-          		// Make api call to save data
-          		createAnn.isLastStep = true
-          		createAnn.refreshFormValues()
-		    	$('#announcementSuccessModal').modal({
-		          	closable: false
-		        }).modal('show')
-		    },
-          	selector: {
-        		approve: '#sendAnnouncement'
-      		}
+                if (!createAnn.isMetaModified) {
+                  return true
+                }
+                return false
+              }
+              return true
+            },
+            onApprove: function () {
+              // Make api call to save data
+              createAnn.isLastStep = true
+              createAnn.refreshFormValues()
+          $('#announcementSuccessModal').modal({
+                closable: false
+            }).modal('show')
+        },
+            selector: {
+            approve: '#sendAnnouncement'
+          }
         }).modal('show')
       }
 
@@ -66,122 +65,122 @@ angular.module('playerApp')
       }
 
       createAnn.removeLink = function (index) {
-          	createAnn.repeatableWebLinks.splice(index, 1)
-          	delete createAnn.data.link[index]
-          	createAnn.showUrlField = createAnn.repeatableWebLinks.length != '0'
+            createAnn.repeatableWebLinks.splice(index, 1)
+            delete createAnn.data.link[index]
+            createAnn.showUrlField = createAnn.repeatableWebLinks.length != '0'
       }
 
         // Function to detect input box change event
       createAnn.detectChange = function () {
-        	createAnn.enableRecepientBtn()
-    	}
+          createAnn.enableRecepientBtn()
+      }
 
-    	// Function to detect dropdwon value change event
-    	createAnn.detectDropdownChange = function () {
-    		createAnn.enableRecepientBtn()
-    	}
+      // Function to detect dropdwon value change event
+      createAnn.detectDropdownChange = function () {
+        createAnn.enableRecepientBtn()
+      }
 
-    	// Function to track back button change
-    	createAnn.previousStep = function () {
-    		createAnn.stepNumber--;
-    	}
+      // Function to track back button change
+      createAnn.previousStep = function () {
+        createAnn.stepNumber--
+      }
 
-    	// Function to preview announcement
-    	createAnn.previewAnn = function () {
-    		var linkArray = [];
-            angular.forEach(createAnn.data.link, function(value, key){
-                linkArray.push(value);
-            });
+      // Function to preview announcement
+      createAnn.previewAnn = function () {
+        var linkArray = []
+      angular.forEach(createAnn.data.link, function (value, key) {
+        linkArray.push(value)
+      })
 
             // TODO - show announcement preview
-            createAnn.previewData = {'sourceId': 'some-organisation-id', 'type': createAnn.data.announcementType, 'links': linkArray, 'title': createAnn.data.title, 'description': createAnn.data.description, 'target': ['teachers'], 'attachments': [{'title': 'circular.pdf', 'downloadURL': 'https://linktoattachment', 'mimetype': 'application/pdf'}]}
-		}
+      createAnn.previewData = {'sourceId': 'some-organisation-id', 'type': createAnn.data.announcementType, 'links': linkArray, 'title': createAnn.data.title, 'description': createAnn.data.description, 'target': ['teachers'], 'attachments': [{'title': 'circular.pdf', 'downloadURL': 'https://linktoattachment', 'mimetype': 'application/pdf'}]}
+    }
 
-    	// Function to confirm recipients
-    	createAnn.confirmRecipients = function () {
-    		// TODO - get select ricipients
-    	}
+      // Function to confirm recipients
+      createAnn.confirmRecipients = function () {
+        // TODO - get select ricipients
+      }
 
-    	// Function to enable / disable RecepientBtn
-    	createAnn.enableRecepientBtn = function () {
-	        if (createAnn.data.title && createAnn.data.from &&
-	        	createAnn.data.announcementType &&
-	        	(createAnn.data.description || createAnn.attachment.length)) {
-	        	createAnn.disableBtn = false
-	        } else {
-	        	createAnn.disableBtn = true
-	        }
-	        createAnn.isMetaModified = true
-	    }
+      // Function to enable / disable RecepientBtn
+      createAnn.enableRecepientBtn = function () {
+          if (createAnn.data.title && createAnn.data.from &&
+            createAnn.data.announcementType &&
+            (createAnn.data.description || createAnn.attachment.length)) {
+            createAnn.disableBtn = false
+          } else {
+            createAnn.disableBtn = true
+          }
+          createAnn.isMetaModified = true
+      }
 
-	    createAnn.refreshFormValues = function () {
+      createAnn.refreshFormValues = function () {
       createAnn.disableBtn = true
-      createAnn.stepNumber = 1;
+      createAnn.stepNumber = 1
       $('#announcementType').dropdown('restore defaults')
       $('#orgDropdown').dropdown('restore defaults')
       $('#createAnnouncementModal').modal('refresh')
-      $('#announcementForm').form('reset')
+      // $('#announcementForm').form('reset')
       createAnn.data = {}
       createAnn.isMetaModified = false
       createAnn.repeatableWebLinks.length = 0
       createAnn.showUrlField = false
       createAnn.initializeFileUploader()
-	    }
+      }
 
-	    createAnn.saveAnnouncement = function () {
-	    	// TODO - call save announcement api
-	    }
+      createAnn.saveAnnouncement = function () {
+        // TODO - call save announcement api
+      }
 
       createAnn.attachment = []
       createAnn.initializeFileUploader = function () {
-	        $timeout(function () {
-	            createAnn.manualUploader = new qq.FineUploader({
-	                element: document.getElementById('fine-uploader-manual-trigger'),
-	                template: 'qq-template-manual-trigger',
-	                request: {
-	                	// TODO - use upload api url
-	                    endpoint: 'http://www.mocky.io/v2/59ef30b72e0000001d1c5e09'
-	                },
-	                autoUpload: true,
-	                debug: true,
-	                validation: {
-	                    sizeLimit: config.AnncmntMaxFileSizeToUpload,
-	                    allowedExtensions: config.AnncmntAllowedFileExtension
-	                },
-	                messages: {
-	                     sizeError: '{file} ' +
-	                     $rootScope.messages.imsg.m0006 + ' ' +
-	                                             config.AnncmntMaxFileSizeToUpload / (1000 * 1024) + ' MB.'
-	                },
-	                failedUploadTextDisplay: {
-        				mode: 'default',
-        				responseProperty: 'error'
-    				},
-    				showMessage: function (message) {
-        				toasterService.error(message)
-    				},
-	                callbacks: {
-	                    onComplete: function (id, name, responseJSON, xhr) {
-	                    	// TODO - push attachement api success response
-	                        createAnn.attachment.push('A', 'B')
-	                        createAnn.enableRecepientBtn()
-	                    },
-	                    onSubmitted: function (id, name) {
-	                    },
-	                    onCancel: function () {
-	                        document.getElementById('hide-section-with-button')
-	                                                .style.display = 'block'
-	                    },
-	                    onError: function (id, name, errorReason, xhrOrXdr) {
-				            toasterService.error(qq.format('Error on file number {} - {}.  Reason: {}', id, name, errorReason))
-				        }
-	                }
-	            })
+          $timeout(function () {
+              createAnn.manualUploader = new qq.FineUploader({
+                  element: document.getElementById('fine-uploader-manual-trigger'),
+                  template: 'qq-template-manual-trigger',
+                  request: {
+                    // TODO - use upload api url
+                      endpoint: 'http://www.mocky.io/v2/59ef30b72e0000001d1c5e09'
+                  },
+                  autoUpload: true,
+                  debug: true,
+                  validation: {
+                      sizeLimit: config.AnncmntMaxFileSizeToUpload,
+                      allowedExtensions: config.AnncmntAllowedFileExtension
+                  },
+                  messages: {
+                       sizeError: '{file} ' +
+                       $rootScope.messages.imsg.m0006 + ' ' +
+                                               config.AnncmntMaxFileSizeToUpload / (1000 * 1024) + ' MB.'
+                  },
+                  failedUploadTextDisplay: {
+                mode: 'default',
+                responseProperty: 'error'
+            },
+            showMessage: function (message) {
+                toasterService.error(message)
+            },
+                  callbacks: {
+                      onComplete: function (id, name, responseJSON, xhr) {
+                        // TODO - push attachement api success response
+                          createAnn.attachment.push('A', 'B')
+                          createAnn.enableRecepientBtn()
+                      },
+                      onSubmitted: function (id, name) {
+                      },
+                      onCancel: function () {
+                          document.getElementById('hide-section-with-button')
+                                                  .style.display = 'block'
+                      },
+                      onError: function (id, name, errorReason, xhrOrXdr) {
+                    toasterService.error(qq.format('Error on file number {} - {}.  Reason: {}', id, name, errorReason))
+                }
+                  }
+              })
 
-	            window.cancelUploadFile = function () {
-	                document.getElementById('hide-section-with-button').style.display = 'block'
-	            }
-	        }, 300)
-	    }
+              window.cancelUploadFile = function () {
+                  document.getElementById('hide-section-with-button').style.display = 'block'
+              }
+          }, 300)
+      }
     }
   ])
