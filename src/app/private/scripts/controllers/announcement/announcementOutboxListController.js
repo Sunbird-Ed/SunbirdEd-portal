afa2604c9ace75363e499c1d7ff34a549ebc0253
Nitesh Kesarkar <nitesh_k@techjoomla.com>
2017-11-10 11:52:06 +0530

    Issue #SB-672 chore:comitting the code

--- src/app/private/scripts/controllers/announcement/announcementOutboxListController.js
+++ src/app/private/scripts/controllers/announcement/announcementOutboxListController.js
@@ -16,11 +16,14 @@ angular.module('playerApp').controller('announcementOutboxListController', ['$ro
                     }
                 } else {
                     toasterService.error(apiResponse.params.errmsg)
+
                 }
             }).catch(function(err) {
                 toasterService.error(err.data.params.errmsg)
             }).finally(function() {
                 announcementOutboxData.showLoader = false
+                announcementOutboxData.showDataDiv = true
+                announcementOutboxData.listData =  [{ "announcementId": "f6000280-c3a7-11e7-af49-912448d08f48", "sourceId": "National Council For Teacher Education", "createdBy": "Creator1", "createdOn": "2017-10-24", "readBy": ["1234-12341-12313-132123", "1234-12341-12313-324234"], "type": "Circular", "links": ["https://www.google.co.in/?gfe_rd=cr&dcr=0&ei=D8r2WbjkOsKL8Qe4pJeACA", "https://diksha.gov.in/#documents"], "status":"active", "title": "Exam dates announced for CBSE and state board exams", "description": "Description goes here for the announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams. Description goes here for the announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams. Description goes here for the announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams. announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams", "target": ["teachers"], "attachments": [{ "title": "Circular A1.pdf", "downloadURL": "https://linktoattachment.com/documents/Circular A1.pdf", "mimetype": "application/pdf","filesize": "120 Kb" }] }, { "announcementId": "2", "sourceId": "National Council For Teacher Education", "createdBy": "Creator1", "createdOn": "2017-10-24", "readBy": ["1234-12341-12313-132123", "1234-12341-12313-324234"], "type": "Circular", "links": ["https://www.google.co.in/?gfe_rd=cr&dcr=0&ei=D8r2WbjkOsKL8Qe4pJeACA", "https://diksha.gov.in/#documents"], "status":"cancelled", "title": "Exam dates announced for CBSE and state board exams", "description": "Description goes here for the announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams. Description goes here for the announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams. Description goes here for the announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams. announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams", "target": ["teachers"], "attachments": [{ "title": "Circular A1.pdf", "downloadURL": "https://linktoattachment.com/documents/Circular A1.pdf", "mimetype": "application/pdf","filesize": "120 Kb" }] }]
             });
         }
 
@@ -41,17 +44,16 @@ angular.module('playerApp').controller('announcementOutboxListController', ['$ro
         announcementOutboxData.doAction = function(announcement) {
             // Get the actionBtnId
             var actionBtnId = 'actionBtn' + announcement.announcementId
+            announcementOutboxData.actionBtnId = actionBtnId
             var actionText = $('#' + actionBtnId).text()
             // Perform the appropriate action
             if (actionText == 'Delete') {
                 announcementOutboxData.deleteAnnouncementId = announcement.announcementId
-                announcementOutboxData.actionBtnId = actionBtnId
                 announcementOutboxData.showModal('announcementDeleteModal')
             } else if (actionText == 'Resend') {
                 // TODO integrate the create announcement flow for edit and resend
                 announcementOutboxData.resendAnnouncement();
                 announcementOutboxData.resendAnnouncementObj = announcement
-                announcementOutboxData.actionBtnId = actionBtnId
                 announcementOutboxData.showModal('announcementResendModal')
             }
         }
--- src/app/private/scripts/controllers/announcement/createAnnouncementController.js
+++ src/app/private/scripts/controllers/announcement/createAnnouncementController.js
@@ -1,182 +1,212 @@
 'use strict'
+angular.module('playerApp').controller('createAnnouncementCtrl', ['$rootScope', '$scope', '$timeout', '$state', '$stateParams', 'config', 'toasterService', 'permissionsService', 'dashboardService', 'announcementService',
 
-angular.module('playerApp')
-  .controller('createAnnouncementCtrl', ['$rootScope', '$scope', '$timeout', '$state', '$stateParams', 'config', 'toasterService',
-    'permissionsService', 'dashboardService',
-    function ($rootScope, $scope, $timeout, $state, $stateParams, config, toasterService, permissionsService, dashboardService) {
-      // Initialize variables
-      var createAnn = this
-
-      // TODO - use api to get values
-      createAnn.org = ['Org 1', 'Org 2', 'Org 3']
-      createAnn.announcementType = ['Type 1', 'Type 2', 'Type 3']
-      createAnn.disableBtn = true
-      createAnn.showUrlField = false
-      createAnn.isLastStep = false
-      createAnn.repeatableWebLinks = []
-      createAnn.isMetaModified = false
-      createAnn.stepNumber = 1
-      createAnn.data = {}
-
-      // Initialize modal
-      createAnn.initializeModal = function () {
-        $timeout(function () {
-          $('#announcementType').dropdown()
-          $('#orgDropdown').dropdown()
-        }, 100)
-      }
-
-      createAnn.createAnnouncement = function () {
-        $('#createAnnouncementModal').modal({
-          closable: false,
-          onHide: function () {
-            // TODO - Show confirmation before closing modal
-            if (!createAnn.isLastStep) {
-              if (createAnn.isMetaModified && confirm('Changes that you made may not be saved.')) {
-                createAnn.refreshFormValues()
-                return true
-              }
-
-              if (!createAnn.isMetaModified) {
-                return true
-              }
-              return false
-            }
-            return true
-          },
-          onApprove: function () {
-            // Make api call to save data
-            createAnn.isLastStep = true
-            createAnn.refreshFormValues()
-            $('#announcementSuccessModal').modal({
-              closable: false
-            }).modal('show')
-          },
-          selector: {
-            approve: '#sendAnnouncement'
-          }
-        }).modal('show')
-      }
-
-      createAnn.addNewLink = function () {
-        var newItemNo = createAnn.repeatableWebLinks.length + 1
-        createAnn.repeatableWebLinks.push({ 'id': 'choice' + newItemNo })
-        createAnn.showUrlField = true
-      }
-
-      createAnn.removeLink = function (index) {
-        createAnn.repeatableWebLinks.splice(index, 1)
-        delete createAnn.data.link[index]
-        createAnn.showUrlField = createAnn.repeatableWebLinks.length != '0'
-      }
-
-      // Function to detect input box change event
-      createAnn.detectChange = function () {
-        createAnn.enableRecepientBtn()
-      }
-
-      // Function to detect dropdwon value change event
-      createAnn.detectDropdownChange = function () {
-        createAnn.enableRecepientBtn()
-      }
-
-      // Function to track back button change
-      createAnn.previousStep = function () {
-        createAnn.stepNumber--
-      }
-
-      // Function to preview announcement
-      createAnn.previewAnn = function () {
-        var linkArray = []
-        angular.forEach(createAnn.data.link, function (value, key) {
-          linkArray.push(value)
-        })
-
-        // TODO - show announcement preview
-        createAnn.previewData = { 'sourceId': 'some-organisation-id', 'type': createAnn.data.announcementType, 'links': linkArray, 'title': createAnn.data.title, 'description': createAnn.data.description, 'target': ['teachers'], 'attachments': [{ 'title': 'circular.pdf', 'downloadURL': 'https://linktoattachment', 'mimetype': 'application/pdf' }] }
-      }
-
-      // Function to confirm recipients
-      createAnn.confirmRecipients = function () {
-        // TODO - get select ricipients
-      }
-
-      // Function to enable / disable RecepientBtn
-      createAnn.enableRecepientBtn = function () {
-        if (createAnn.data.title && createAnn.data.from &&
-          createAnn.data.announcementType &&
-          (createAnn.data.description || createAnn.attachment.length)) {
-          createAnn.disableBtn = false
-        } else {
-          createAnn.disableBtn = true
-        }
-        createAnn.isMetaModified = true
-      }
-
-      createAnn.refreshFormValues = function () {
+    function($rootScope, $scope, $timeout, $state, $stateParams, config, toasterService, permissionsService, dashboardService) {
+        // Initialize variables
+        var createAnn = this
+        // TODO - use api to get values
+        createAnn.org = ['Org 1', 'Org 2', 'Org 3']
+        createAnn.announcementType = ['Type 1', 'Type 2', 'Type 3']
         createAnn.disableBtn = true
-        createAnn.stepNumber = 1
-        $('#announcementType').dropdown('restore defaults')
-        $('#orgDropdown').dropdown('restore defaults')
-        $('#createAnnouncementModal').modal('refresh')
-        // $('#announcementForm').form('reset')
-        createAnn.data = {}
-        createAnn.isMetaModified = false
-        createAnn.repeatableWebLinks.length = 0
         createAnn.showUrlField = false
-        createAnn.initializeFileUploader()
-      }
-
-      createAnn.saveAnnouncement = function () {
-        // TODO - call save announcement api
-      }
-
-      createAnn.attachment = []
-      createAnn.initializeFileUploader = function () {
-        $timeout(function () {
-          createAnn.manualUploader = new qq.FineUploader({
-            element: document.getElementById('fine-uploader-manual-trigger'),
-            template: 'qq-template-manual-trigger',
-            request: {
-              // TODO - use upload api url
-              endpoint: 'http://www.mocky.io/v2/59ef30b72e0000001d1c5e09'
-            },
-            autoUpload: true,
-            debug: true,
-            validation: {
-              sizeLimit: config.AnncmntMaxFileSizeToUpload,
-              allowedExtensions: config.AnncmntAllowedFileExtension
-            },
-            messages: {
-              sizeError: '{file} ' + $rootScope.messages.imsg.m0006 + ' ' + config.AnncmntMaxFileSizeToUpload / (1000 * 1024) + ' MB.'
-            },
-            failedUploadTextDisplay: {
-              mode: 'default',
-              responseProperty: 'error'
-            },
-            showMessage: function (message) {
-              toasterService.error(message)
-            },
-            callbacks: {
-              onComplete: function (id, name, responseJSON, xhr) {
-                // TODO - push attachement api success response
-                createAnn.attachment.push('A', 'B')
-                createAnn.enableRecepientBtn()
-              },
-              onSubmitted: function (id, name) {},
-              onCancel: function () {
-                document.getElementById('hide-section-with-button').style.display = 'block'
-              },
-              onError: function (id, name, errorReason, xhrOrXdr) {
-                toasterService.error(qq.format('Error on file number {} - {}.  Reason: {}', id, name, errorReason))
-              }
+        createAnn.isLastStep = false
+        createAnn.repeatableWebLinks = []
+        createAnn.isMetaModified = false
+        createAnn.stepNumber = 1
+        createAnn.data = {}
+        // Initialize modal
+        createAnn.initializeModal = function() {
+            $timeout(function() {
+                $('#announcementType').dropdown()
+                $('#orgDropdown').dropdown()
+            }, 100)
+        }
+        createAnn.createAnnouncement = function() {
+            $('#createAnnouncementModal').modal({
+                closable: false,
+                onHide: function() {
+                    // TODO - Show confirmation before closing modal
+                    if (!createAnn.isLastStep) {
+                        if (createAnn.isMetaModified && confirm('Changes that you made may not be saved.')) {
+                            createAnn.refreshFormValues()
+                            return true
+                        }
+                        if (!createAnn.isMetaModified) {
+                            return true
+                        }
+                        return false
+                    }
+                    return true
+                },
+                onApprove: function() {
+                    // Make api call to save data
+                    createAnn.isLastStep = true
+                    createAnn.refreshFormValues()
+                    $('#announcementSuccessModal').modal({
+                        closable: false
+                    }).modal('show')
+                },
+                selector: {
+                    approve: '#sendAnnouncement'
+                }
+            }).modal('show')
+        }
+        createAnn.addNewLink = function() {
+            var newItemNo = createAnn.repeatableWebLinks.length + 1
+            createAnn.repeatableWebLinks.push({
+                'id': 'choice' + newItemNo
+            })
+            createAnn.showUrlField = true
+        }
+        createAnn.removeLink = function(index) {
+            createAnn.repeatableWebLinks.splice(index, 1)
+            delete createAnn.data.link[index]
+            createAnn.showUrlField = createAnn.repeatableWebLinks.length != '0'
+        }
+        // Function to detect input box change event
+        createAnn.detectChange = function() {
+            createAnn.enableRecepientBtn()
+        }
+        // Function to detect dropdwon value change event
+        createAnn.detectDropdownChange = function() {
+            createAnn.enableRecepientBtn()
+        }
+        // Function to track back button change
+        createAnn.previousStep = function() {
+            createAnn.stepNumber--
+        }
+        // Function to preview announcement
+        createAnn.previewAnn = function() {
+            var linkArray = []
+            angular.forEach(createAnn.data.link, function(value, key) {
+                linkArray.push(value)
+            })
+            // TODO - show announcement preview
+            createAnn.previewData = {
+                'sourceId': 'some-organisation-id',
+                'type': createAnn.data.announcementType,
+                'links': linkArray,
+                'title': createAnn.data.title,
+                'description': createAnn.data.description,
+                'target': ['teachers'],
+                'attachments': [{
+                    'title': 'circular.pdf',
+                    'downloadURL': 'https://linktoattachment',
+                    'mimetype': 'application/pdf'
+                }]
             }
-          })
+        }
+        // Function to confirm recipients
+        createAnn.confirmRecipients = function() {
+            // TODO - get select ricipients
+        }
+        // Function to enable / disable RecepientBtn
+        createAnn.enableRecepientBtn = function() {
+            if (createAnn.data.title && createAnn.data.from && createAnn.data.announcementType && (createAnn.data.description || createAnn.attachment.length)) {
+                createAnn.disableBtn = false
+            } else {
+                createAnn.disableBtn = true
+            }
+            createAnn.isMetaModified = true
+        }
+        createAnn.refreshFormValues = function() {
+            createAnn.disableBtn = true
+            createAnn.stepNumber = 1
+            $('#announcementType').dropdown('restore defaults')
+            $('#orgDropdown').dropdown('restore defaults')
+            $('#createAnnouncementModal').modal('refresh')
+            // $('#announcementForm').form('reset')
+            createAnn.data = {}
+            createAnn.isMetaModified = false
+            createAnn.repeatableWebLinks.length = 0
+            createAnn.showUrlField = false
+            createAnn.initializeFileUploader()
+        }
+        createAnn.saveAnnouncement = function() {
+            // TODO - call save announcement api
+        }
+        createAnn.attachment = []
+        createAnn.initializeFileUploader = function() {
+            $timeout(function() {
+                createAnn.manualUploader = new qq.FineUploader({
+                    element: document.getElementById('fine-uploader-manual-trigger'),
+                    template: 'qq-template-manual-trigger',
+                    request: {
+                        // TODO - use upload api url
+                        endpoint: 'http://www.mocky.io/v2/59ef30b72e0000001d1c5e09'
+                    },
+                    autoUpload: true,
+                    debug: true,
+                    validation: {
+                        sizeLimit: config.AnncmntMaxFileSizeToUpload,
+                        allowedExtensions: config.AnncmntAllowedFileExtension
+                    },
+                    messages: {
+                        sizeError: '{file} ' + $rootScope.messages.imsg.m0006 + ' ' + config.AnncmntMaxFileSizeToUpload / (1000 * 1024) + ' MB.'
+                    },
+                    failedUploadTextDisplay: {
+                        mode: 'default',
+                        responseProperty: 'error'
+                    },
+                    showMessage: function(message) {
+                        toasterService.error(message)
+                    },
+                    callbacks: {
+                        onComplete: function(id, name, responseJSON, xhr) {
+                            // TODO - push attachement api success response
+                            createAnn.attachment.push('A', 'B')
+                            createAnn.enableRecepientBtn()
+                        },
+                        onSubmitted: function(id, name) {},
+                        onCancel: function() {
+                            document.getElementById('hide-section-with-button').style.display = 'block'
+                        },
+                        onError: function(id, name, errorReason, xhrOrXdr) {
+                            toasterService.error(qq.format('Error on file number {} - {}.  Reason: {}', id, name, errorReason))
+                        }
+                    }
+                })
+                window.cancelUploadFile = function() {
+                    document.getElementById('hide-section-with-button').style.display = 'block'
+                }
+            }, 300)
+        }
+        createAnn.getAnnouncementDetailsFromId = function(announcementId) {
+            alert(announcementId)
+            /*announcementService.getAnnouncementDetailsFromId(announcementId).then(function(apiResponse) {
+                    if (apiResponse && apiResponse.responseCode === 'OK') {
+                        announcementOutboxData.listData = apiResponse.result.announcements
 
-          window.cancelUploadFile = function () {
-            document.getElementById('hide-section-with-button').style.display = 'block'
-          }
-        }, 300)
-      }
+                    } else {
+                        toasterService.error(apiResponse.params.errmsg)
+                    }
+                }).catch(function(err) {
+                    toasterService.error(err.data.params.errmsg)
+                }).finally(function() {
+
+                }); */
+            createAnn.data.resend = {
+                "announcementId": "1",
+                "sourceId": "National Council For Teacher Education",
+                "createdBy": "Creator1",
+                "createdOn": "2017-10-24",
+                "readBy": ["1234-12341-12313-132123", "1234-12341-12313-324234"],
+                "type": "Circular",
+                "links": ["https://www.google.co.in/?gfe_rd=cr&dcr=0&ei=D8r2WbjkOsKL8Qe4pJeACA", "https://diksha.gov.in/#documents"],
+                "status": "active",
+                "title": "Exam dates announced for CBSE and state board exams",
+                "description": "Description goes here for the announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams. Description goes here for the announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams. Description goes here for the announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams. announcement exam date announced for CBSC and state board exams announcement exam date announced for CBSC and state board exams",
+                "target": ["teachers"],
+                "attachments": [{
+                    "title": "Circular A1.pdf",
+                    "downloadURL": "https://linktoattachment.com/documents/Circular A1.pdf",
+                    "mimetype": "application/pdf",
+                    "filesize": "120 Kb"
+                }]
+            }
+            console.log(createAnn.data.resend);
+            alert('successful')
+            //$('#createAnnouncementModal').modal('show')
+        }
     }
-  ])
+])
