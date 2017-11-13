9d8fedaf70fe4f2780b2ea231389758049bccbe2
Sourav <sourav_d@techjoomla.com>
2017-11-13 15:25:30 +0530

    Issue #SB-737 feat:Received API implementation

--- src/app/private/scripts/filters/dateFilter.js
+++ src/app/private/scripts/filters/dateFilter.js
@@ -11,4 +11,9 @@ angular.module('playerApp')
             var local = moment(date).local().format('YYYY-MM-DD HH:mm:ss');
             return moment(local).fromNow();
         };
+    })
+    .filter('dateFilterWithDMY', function () {
+        return function (date) {
+            return moment(date).format('Do MMMM YYYY');
+        };
     });
