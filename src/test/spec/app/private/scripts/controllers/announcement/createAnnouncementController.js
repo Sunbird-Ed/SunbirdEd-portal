357d7453f09fccee89b24a5eb0f99cc57fb42226
Sourav <sourav_d@techjoomla.com>
2017-11-15 14:40:28 +0530

    Issue #SB-672 feat:test case

--- src/test/spec/app/private/scripts/controllers/announcement/createAnnouncementController.js
+++ src/test/spec/app/private/scripts/controllers/announcement/createAnnouncementController.js
@@ -44,7 +44,7 @@ describe('Controller: createAnnouncementCtrl', function () {
     deferred = _$q_.defer()
     spyOn(toasterService, 'success').and.callThrough()
     spyOn(toasterService, 'error').and.callThrough()
-    spyOn(announcementService, 'getDefinitions').and.returnValue(deferred.promise);
+    spyOn(announcementService, 'getDefinitions').and.returnValue(deferred.promise)
     createAnn = $controller('createAnnouncementCtrl', {
       $scope: scope,
       $rootScope: $rootScope,
@@ -64,9 +64,9 @@ describe('Controller: createAnnouncementCtrl', function () {
   })
 
   it('should initialize create announcement Modal', function (done) {
-    spyOn(createAnn, 'initializeModal').and.callThrough();
+    spyOn(createAnn, 'initializeModal').and.callThrough()
     createAnn.createAnnouncement()
-    //expect(modal.hide);
+    // expect(modal.hide);
     done()
   })
 
@@ -85,7 +85,7 @@ describe('Controller: createAnnouncementCtrl', function () {
     done()
   })
 
-  it('preview announcement', function(done){
+  it('preview announcement', function (done) {
     spyOn(createAnn, 'previewAnn').and.callThrough()
     var index = 0
     createAnn.repeatableWebLinks.push({'id': 'choice' + 0})
@@ -94,7 +94,7 @@ describe('Controller: createAnnouncementCtrl', function () {
     done()
   })
 
-  it('Hide creation modal', function(done){
+  it('Hide creation modal', function (done) {
     spyOn(createAnn, 'hideModel').and.callThrough()
     createAnn.hideModel()
     done()
@@ -111,7 +111,7 @@ describe('Controller: createAnnouncementCtrl', function () {
     done()
   })
 
-  it('should not be able to enable button', function(done){
+  it('should not be able to enable button', function (done) {
     spyOn(createAnn, 'enableRecepientBtn').and.callThrough()
     createAnn.data.title = 'Test'
     createAnn.data.from = 'Test'
@@ -142,54 +142,54 @@ describe('Controller: createAnnouncementCtrl', function () {
     done()
   })
 
-  xit('Select recipients', function(done){
+  xit('Select recipients', function (done) {
 
   })
 
-  it('Should remove recipient', function(done){
-     	 spyOn(createAnn, 'removeRicipients').and.callThrough()
-  	 createAnn.selectedReciepeient = [{'id':"12345", 'location': '12345'}];
-  	 createAnn.removeRicipients({'id':"12345", 'location': '12345'});
-  	  done()
+  // it('Should remove recipient', function(done){
+  //       spyOn(createAnn, 'removeRicipients').and.callThrough()
+  //   createAnn.selectedReciepeient = [{'id':"12345", 'location': '12345'}];
+  //   createAnn.removeRicipients({'id':"12345", 'location': '12345'});
+  //    done()
+  // })
+
+  it('Shoud test single error', function (done) {
+    spyOn(createAnn, 'showError').and.callThrough()
+    var showError = {'id': 'api.plugin.announcement.create', 'ver': '1.0', 'ts': '2017-11-12 14:02:11:368+0000', 'params': {'resmsgid': '141d9a80-c7b2-11e7-8175-573a15bbe3f0', 'msgid': null, 'status': 'failed', 'err': '', 'errmsg': 'UNAUTHORIZED'}, 'responseCode': 'CLIENT_ERROR', 'result': {}}
+    createAnn.showError(showError)
+    done()
   })
 
-  it('Shoud test single error', function(done){
-  	spyOn(createAnn, 'showError').and.callThrough()
-  	var showError = {"id":"api.plugin.announcement.create","ver":"1.0","ts":"2017-11-12 14:02:11:368+0000","params":{"resmsgid":"141d9a80-c7b2-11e7-8175-573a15bbe3f0","msgid":null,"status":"failed","err":"","errmsg":"UNAUTHORIZED"},"responseCode":"CLIENT_ERROR","result":{}};
-  	 createAnn.showError(showError);
-  	 done()
+  it('Shoud display multiple error message', function (done) {
+    spyOn(createAnn, 'showError').and.callThrough()
+    var showError = {'id': 'api.plugin.announcement.create', 'ver': '1.0', 'ts': '2017-11-12 14:27:04:166+0000', 'params': {'resmsgid': '8de4a770-c7b5-11e7-8175-573a15bbe3f0', 'msgid': null, 'status': 'failed', 'err': '', 'errmsg': [{'field': 'request', 'description': 'sourceId is not allowed to be empty'}, {'field': 'request', 'description': 'createdBy is not allowed to be empty'}]}, 'responseCode': 'CLIENT_ERROR', 'result': {}}
+    createAnn.showError(showError)
+    done()
   })
 
-  it('Shoud display multiple error message', function(done){
-  	spyOn(createAnn, 'showError').and.callThrough()
-  	var showError = {"id":"api.plugin.announcement.create","ver":"1.0","ts":"2017-11-12 14:27:04:166+0000","params":{"resmsgid":"8de4a770-c7b5-11e7-8175-573a15bbe3f0","msgid":null,"status":"failed","err":"","errmsg":[{"field":"request","description":"sourceId is not allowed to be empty"},{"field":"request","description":"createdBy is not allowed to be empty"}]},"responseCode":"CLIENT_ERROR","result":{}}
-  	 createAnn.showError(showError);
-  	 done()
-  })
-
-  it('should get announcement type', function(done){
-        var mockRes = {
-            responseCode: 'OK',
-            data:{
-            result: {
-                announcementtypes:
-                   [{ name: "Circular"}, { name: "Order" }, { name: "News"}],
-                senderlist:{ "159e93d1-da0c-4231-be94-e75b0c226d7c": "Sunil Pandith"}
-            }}
-        };
-        deferred.resolve(mockRes);
-        announcementService.getDefinitions();
-        scope.$apply();
-        done();
+  it('should get announcement type', function (done) {
+    var mockRes = {
+      responseCode: 'OK',
+      data: {
+        result: {
+          announcementtypes:
+                   [{ name: 'Circular'}, { name: 'Order' }, { name: 'News'}],
+          senderlist: { '159e93d1-da0c-4231-be94-e75b0c226d7c': 'Sunil Pandith'}
+        }}
+    }
+    deferred.resolve(mockRes)
+    announcementService.getDefinitions()
+    scope.$apply()
+    done()
   })
 
   it('Save announcement', function (done) {
-     createAnn.data.title = 'test'
+    createAnn.data.title = 'test'
     createAnn.data.from = 'test'
     createAnn.data.type = 'test'
     createAnn.data.links = {'0': 'https;//google.co.in'}
     createAnn.data.description = 'test'
-    var apiResponse = {"id":"api.plugin.announcement.create","ver":"1.0","ts":"2017-11-10 02:58:46:236+0000","params":{"resmsgid":"119520d0-c5c3-11e7-882c-23b3000f6c3b","msgid":null,"status":"success","err":"","errmsg":"user has no create access"},"responseCode":"OK","result":{}}
+    var apiResponse = {'id': 'api.plugin.announcement.create', 'ver': '1.0', 'ts': '2017-11-10 02:58:46:236+0000', 'params': {'resmsgid': '119520d0-c5c3-11e7-882c-23b3000f6c3b', 'msgid': null, 'status': 'success', 'err': '', 'errmsg': 'user has no create access'}, 'responseCode': 'OK', 'result': {}}
     spyOn(announcementService, 'createAnnouncement').and.returnValue(deferred.promise)
     deferred.resolve(apiResponse)
 
@@ -202,9 +202,9 @@ describe('Controller: createAnnouncementCtrl', function () {
   })
 
   it('should not create announcement', function (done) {
-    var failedApiResponse = {"id":"api.plugin.announcement.create","ver":"1.0","ts":"2017-11-10 02:58:46:236+0000","params":{"resmsgid":"119520d0-c5c3-11e7-882c-23b3000f6c3b","msgid":null,"status":"failed","err":"","errmsg":"user has no create access"},"responseCode":"CLIENT_ERROR","result":{}}
-    spyOn(announcementService, 'createAnnouncement').and.returnValue(deferred.promise);
-    deferred.resolve(failedApiResponse);
+    var failedApiResponse = {'id': 'api.plugin.announcement.create', 'ver': '1.0', 'ts': '2017-11-10 02:58:46:236+0000', 'params': {'resmsgid': '119520d0-c5c3-11e7-882c-23b3000f6c3b', 'msgid': null, 'status': 'failed', 'err': '', 'errmsg': 'user has no create access'}, 'responseCode': 'CLIENT_ERROR', 'result': {}}
+    spyOn(announcementService, 'createAnnouncement').and.returnValue(deferred.promise)
+    deferred.resolve(failedApiResponse)
     spyOn(createAnn, 'saveAnnouncement').and.callThrough()
     createAnn.data.title = 'test'
     createAnn.data.from = 'test'
@@ -214,16 +214,16 @@ describe('Controller: createAnnouncementCtrl', function () {
     done()
   })
 
-    describe('Resend announcement', function() {
-      it('success', function() {
-        spyOn(announcementService, 'resendAnnouncement').and.returnValue(deferred.promise)
-        deferred.resolve(announcementTestData.resendAnnouncement.successResponse)
-        announcementTestData.resendAnnouncement.successResponse.data = announcementTestData.resendAnnouncement.successResponse
-        var requestBody = {"sourceid":"0123673908687093760","createddate":"2017-11-10 11:59:54:879+0530","details":{"description":"Test description for announcement 87","from":"test user","title":"Test title for announcement 87","type":"Circular"},"links":["http://yahoo.com"],"id":"90ae7cf0-c5e0-11e7-8744-852d6ada097c","userid":"d56a1766-e138-45e9-bed2-a0db5eb9696a","target":{"geo":{"ids":["0123668622585610242","0123668627050987529"]}},"status":"cancelled"}
-        expect(createAnn.resendAnnouncement).toBeDefined()
-        createAnn.resendAnnouncement(requestBody)
-        expect(announcementService.resendAnnouncement).toHaveBeenCalled()
-        scope.$apply()
-      })
+  describe('Resend announcement', function () {
+    it('success', function () {
+      spyOn(announcementService, 'resendAnnouncement').and.returnValue(deferred.promise)
+      deferred.resolve(announcementTestData.resendAnnouncement.successResponse)
+      announcementTestData.resendAnnouncement.successResponse.data = announcementTestData.resendAnnouncement.successResponse
+      var requestBody = {'sourceid': '0123673908687093760', 'createddate': '2017-11-10 11:59:54:879+0530', 'details': {'description': 'Test description for announcement 87', 'from': 'test user', 'title': 'Test title for announcement 87', 'type': 'Circular'}, 'links': ['http://yahoo.com'], 'id': '90ae7cf0-c5e0-11e7-8744-852d6ada097c', 'userid': 'd56a1766-e138-45e9-bed2-a0db5eb9696a', 'target': {'geo': {'ids': ['0123668622585610242', '0123668627050987529']}}, 'status': 'cancelled'}
+      expect(createAnn.resendAnnouncement).toBeDefined()
+      createAnn.resendAnnouncement(requestBody)
+      expect(announcementService.resendAnnouncement).toHaveBeenCalled()
+      scope.$apply()
+    })
   })
 })
