1449ab92640c0e888f6e3d5dc0ee02527b30f218
Nilesh More <nilesh_m@tekditechnologies.com>
2018-01-03 14:28:41 +0530

    Issue #SB-1165 fix: review changes updated

--- src/app/private/scripts/factories/dashboard/queryService.js
+++ src/app/private/scripts/factories/dashboard/queryService.js
@@ -5,56 +5,24 @@
 'use strict'
 
 angular.module('playerApp')
-  .factory('QueryService', ['config', '$q',
-    'courseConsumptionDataSource', 'courseProgressDataSource',
-    'orgCreationDataSource', 'orgConsumptionDataSource', 'downloadReportDataSource',
-    function (config, $q, courseConsumptionDataSource,
-      courseProgressDataSource, orgCreationDataSource, orgConsumptionDataSource, downloadReportDataSource) {
-      // Datasets - api urls
-      var datasets = {
-        org: {
-          'creation': config.URL.DASHBOARD.ORG_CREATION,
-          'consumption': config.URL.DASHBOARD.ORG_CONSUMPTION
-        },
-        course: {
-          'progress': config.URL.DASHBOARD.COURSE_PROGRESS,
-          'consumption': config.URL.DASHBOARD.COURSE_CONSUMPTION
+  .factory('QueryService', ['courseConsumptionDataSource', 'courseProgressDataSource',
+    'orgCreationDataSource', 'orgConsumptionDataSource',
+    function (courseConsumptionDataSource, courseProgressDataSource, orgCreationDataSource, orgConsumptionDataSource) {
+      return {
+        getInstance: function (params) {
+          switch (params.eid) {
+          case 'courseConsumptionData':
+            return courseConsumptionDataSource
+          case 'courseProgressData':
+            return courseProgressDataSource
+          case 'orgCreationData':
+            return orgCreationDataSource
+          case 'orgConsumptionData':
+            return orgConsumptionDataSource
+          default:
+            return courseConsumptionDataSource
+          }
         }
       }
-
-      /**
-       * @method QueryService
-       * @desc function to regiester data source.
-       * @memberOf Factory.dashboardQueryFactory
-       * @param {object} data data source
-       */
-      function QueryService (data) {
-        this.clientName = data.key
-      }
-
-      /**
-       * @method query
-       * @desc function to call required data source.
-       * @memberOf Factory.dashboardQueryFactory
-       * @param {object} [varname] [description]
-       */
-      QueryService.prototype.query = function (params) {
-        switch (params.eid) {
-        case 'courseConsumptionDataSource':
-          return courseConsumptionDataSource.getData(params.request, datasets.course[params.dataset])
-        case 'courseProgressDataSource':
-          return courseProgressDataSource.getData(params.request, datasets.course[params.dataset])
-        case 'orgCreationDataSource':
-          return orgCreationDataSource.getData(params.request, datasets.org[params.dataset])
-        case 'orgConsumptionDataSource':
-          return orgConsumptionDataSource.getData(params.request, datasets.org[params.dataset])
-        case 'downloadOrgReport':
-          return downloadReportDataSource.download(params.request, datasets.org[params.dataset])
-        default:
-          return downloadReportDataSource.download(params.request, datasets.org[params.dataset])
-        }
-      }
-
-      return QueryService
     }
   ])
