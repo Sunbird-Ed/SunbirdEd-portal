4bf3f809e62ae8dbb86a797dad42e5df3e9dfdc1
Nilesh More <nilesh_m@tekditechnologies.com>
2018-01-04 23:38:53 +0530

    Issue #SB-1165 fix: review changes fixed

--- src/app/private/scripts/factories/dashboard/queryService.js
+++ src/app/private/scripts/factories/dashboard/queryService.js
@@ -5,26 +5,42 @@
 'use strict'
 
 angular.module('playerApp')
-  .factory('QueryService', ['courseConsumptionDataSource', 'courseProgressDataSource',
+  .factory('QueryService', [ 'courseConsumptionDataSource', 'courseProgressDataSource',
     'orgCreationDataSource', 'orgConsumptionDataSource', 'downloadReportDataSource',
     function (courseConsumptionDataSource, courseProgressDataSource,
       orgCreationDataSource, orgConsumptionDataSource, downloadReportDataSource) {
+      /**
+       * @method SetProvider
+       * @desc create datasource instance
+       * @param {object}  params
+       */
+      function SetProvider (params) {
+        switch (params.eid) {
+        case 'courseConsumption':
+          this.clientName = courseConsumptionDataSource
+          break
+        case 'courseProgress':
+          this.clientName = courseProgressDataSource
+          break
+        case 'orgCreation':
+          this.clientName = orgCreationDataSource
+          break
+        case 'orgConsumption':
+          this.clientName = orgConsumptionDataSource
+          break
+        case 'downloadReport':
+          this.clientName = downloadReportDataSource
+          break
+        default:
+          this.clientName = courseConsumptionDataSource
+        }
+
+        return this.clientName
+      }
+
       return {
-        GetInstance: function (params) {
-          switch (params.eid) {
-          case 'courseConsumption':
-            return courseConsumptionDataSource
-          case 'courseProgress':
-            return courseProgressDataSource
-          case 'orgCreation':
-            return orgCreationDataSource
-          case 'orgConsumption':
-            return orgConsumptionDataSource
-          case 'downloadReport':
-            return downloadReportDataSource
-          default:
-            return courseConsumptionDataSource
-          }
+        CreateNewInstance: function (params) {
+          return new SetProvider(params)
         }
       }
     }
