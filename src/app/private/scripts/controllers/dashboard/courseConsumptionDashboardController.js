45df74d6325eab5a8b1d45f28aeba519a1b1be67
Nilesh More <nilesh_m@tekditechnologies.com>
2018-01-11 10:34:32 +0530

    Issue #SB-1276 refactor: course consumption dashboard and test cases refactoring

--- src/app/private/scripts/controllers/dashboard/courseConsumptionDashboardController.js
+++ src/app/private/scripts/controllers/dashboard/courseConsumptionDashboardController.js
@@ -7,55 +7,37 @@ angular.module('playerApp')
       // Initialize variables
       var courseDashboard = this
       courseDashboard.chartHeight = 110
-      courseDashboard.myCoursesList = []
-      courseDashboard.courseIdentifier = ''
-      courseDashboard.courseName = ''
-      courseDashboard.filterTimePeriod = '7d'
-      courseDashboard.filterQueryTextMsg = '7 days'
-
-      // Dataset - consumption
-      courseDashboard.dataset = 'consumption'
-
+      spinner(true)
       var getInstanceObj = new QueryService.CreateNewInstance({ eid: 'courseConsumption' })
       var chart = new Visualizer({ type: 'line' })
-
-      // Variables to show loader/errorMsg/warningMsg
-      spinner(true)
-      courseDashboard.showError = false
-      courseDashboard.showLabelFlag = false
-      courseDashboard.errorMsg = ''
-      courseDashboard.showWarningMsg = false
 
       /**
        * @Function to load dashboard
        * @params apis request body
        * @return void
        */
-      courseDashboard.getCourseDashboardData = function () {
-        courseDashboard.filterTimePeriod = courseDashboard.filterTimePeriod ? courseDashboard.filterTimePeriod : '7d'
+      courseDashboard.getCourseDashboardData = function (timePeriod) {
+        spinner(true)
+        courseDashboard.timePeriod = timePeriod || '7d'
         getInstanceObj.getData({
           identifier: courseDashboard.courseIdentifier,
-          timePeriod: courseDashboard.filterTimePeriod
+          timePeriod: courseDashboard.timePeriod
         }).then(function (data) {
           courseDashboard.data = chart.render(data)
           courseDashboard.consumptionNumericData = data.numericData
           spinner(false)
           courseDashboard.showError = false
         }).catch(function (apiResponse) {
-          toasterService.error(apiResponse.params.errmsg)
+          courseDashboard.showErrors()
         })
       }
 
-      courseDashboard.onAfterFilterChange = function (item) {
+      courseDashboard.onAfterFilterChange = function (timeperiod) {
         // Check old filter value. If old value and new filter value are same
-        if (courseDashboard.filterTimePeriod === angular.element(item).data('timeperiod')) {
+        if (courseDashboard.timePeriod === timeperiod) {
           return false
         }
-        spinner(true)
-        courseDashboard.filterTimePeriod = angular.element(item).data('timeperiod')
-        courseDashboard.filterQueryTextMsg = angular.element(item).data('timeperiod-text')
-        courseDashboard.isMultipleCourses = false
-        courseDashboard.getCourseDashboardData()
+        courseDashboard.getCourseDashboardData(timeperiod)
       }
 
       courseDashboard.loadData = function () {
@@ -70,31 +52,25 @@ angular.module('playerApp')
           }
         }
 
-        // Make api call to get list of my courses
+        courseDashboard.myCoursesList = []
         searchService.search(request).then(function (apiResponse) {
           if (apiResponse && apiResponse.responseCode === 'OK') {
             if (apiResponse.result.content && apiResponse.result.content.length > 0) {
               courseDashboard.myCoursesList = apiResponse.result.content
-
-              console.log(' courseDashboard.myCoursesList ', courseDashboard.myCoursesList)
               courseDashboard.buildMyCoursesDropdown()
             } else {
               spinner(false)
-              courseDashboard.showWarningMsg = true
             }
           } else {
-            // Show error div
-            courseDashboard.showErrors(apiResponse)
+            courseDashboard.showErrors()
           }
         }).catch(function (apiResponse) {
-          // Show error div
-          courseDashboard.showErrors(apiResponse)
+          courseDashboard.showErrors()
         })
       }
 
       courseDashboard.buildMyCoursesDropdown = function () {
         if (courseDashboard.myCoursesList.length === 1) {
-          courseDashboard.showLabelFlag = true
           var firstChild = _.first(_.values(courseDashboard.myCoursesList), 1)
           courseDashboard.courseIdentifier = firstChild.identifier
           courseDashboard.courseName = firstChild.name
@@ -114,28 +90,22 @@ angular.module('playerApp')
         courseDashboard.showLoader = data
       }
 
-      courseDashboard.showErrors = function (apiResponse) {
+      courseDashboard.showErrors = function () {
         courseDashboard.showError = true
         spinner(false)
-        courseDashboard.errorMsg = apiResponse.params.errmsg
-        toasterService.error(apiResponse.params.errmsg)
       }
 
-      courseDashboard.onAfterCourseChange = function (courseId, courseName) {
+      courseDashboard.onAfterCourseChange = function (courseId) {
         if (courseDashboard.courseIdentifier === courseId) {
           return false
         }
-        spinner(true)
         courseDashboard.courseIdentifier = courseId
-        courseDashboard.courseName = courseName
         courseDashboard.isMultipleCourses = false
-        courseDashboard.getCourseDashboardData()
+        courseDashboard.getCourseDashboardData(courseDashboard.timePeriod)
       }
 
       courseDashboard.initDropdwon = function () {
-        $('#myCoursesListFilter').dropdown({
-          onChange: function () {}
-        })
+        $('#myCoursesListFilter').dropdown()
       }
     }
   ])
