1668446d5efde166a5e854ead1b91ab2dee3290a
Rajath <vbrajath@gmail.com>
2018-01-30 14:20:36 +0530

    Issue #SB-1426 fix:added collectionEditor params

--- src/app/private/scripts/controllers/collectionEditorController.js
+++ src/app/private/scripts/controllers/collectionEditorController.js
@@ -8,7 +8,6 @@ angular.module('playerApp')
       var collectionEditor = this
       collectionEditor.contentId = $stateParams.contentId
       collectionEditor.frameworkId = $stateParams.frameworkId
-      collectionEditor.state = $stateParams.state
       collectionEditor.openCollectionEditor = function (data) {
         $('#collectionEditor').iziModal({
           title: '',
@@ -40,7 +39,8 @@ angular.module('playerApp')
           },
           etags: { app: [], partner: [], dims: org.sunbird.portal.dims },
           channel: org.sunbird.portal.channel,
-          frameworkId: collectionEditor.frameworkId
+          frameworkId: collectionEditor.frameworkId,
+          env: data.type
         }
 
         window.config = {
@@ -74,7 +74,8 @@ angular.module('playerApp')
               type: 'plugin'
             }],
             showEndPage: false
-          }
+          },
+          editorType: data.type
         }
 
         window.config.editorConfig.publishMode = false
@@ -146,7 +147,6 @@ angular.module('playerApp')
         var editorConfig = []
         switch (type) {
         case 'Course':
-          window.config.editorType = 'Course'
           editorConfig.push({
             type: 'Course',
             label: 'Course',
@@ -197,7 +197,6 @@ angular.module('playerApp')
           })
           return editorConfig
         case 'Collection':
-          window.config.editorType = 'Collection'
           editorConfig.push({
             type: 'Collection',
             label: 'Collection',
@@ -234,7 +233,6 @@ angular.module('playerApp')
           })
           return editorConfig
         case 'LessonPlan':
-          window.config.editorType = 'LessonPlan'
           editorConfig.push({
             type: 'LessonPlan',
             label: 'LessonPlan',
@@ -285,7 +283,6 @@ angular.module('playerApp')
           })
           return editorConfig
         default:
-          window.config.editorType = 'TextBook'
           editorConfig.push({
             type: 'TextBook',
             label: 'Textbook',
