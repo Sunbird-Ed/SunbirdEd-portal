7817470408753932d2514a9b42779959db1d52bb
revathi <revathipp@gmail.com>
2018-02-01 10:57:20 +0530

    Issue #SB-1436 feat:Merged conflicts

--- src/app/private/scripts/components/courseQuestions.js
+++ src/app/private/scripts/components/courseQuestions.js
@@ -2,77 +2,152 @@
 
 angular.module('playerApp').component('courseQuestions', {
   templateUrl: 'views/course/courseQuestions.html',
-  controller: ['$scope', '$rootScope', '$timeout', 'courseQuestionsAdapter', function ($scope, $rootScope,
-    $timeout, courseQuestionsAdapter) {
+  controller: ['$scope', '$rootScope', '$timeout', 'courseQuestionsAdapter', '$stateParams', function($scope, $rootScope,
+    $timeout, courseQuestionsAdapter, $stateParams) {
+    console.log('username', $rootScope.userName)
+    $scope.userName = $rootScope.userName
     $scope.successMessage = true
     $scope.date = new Date()
-    $scope.loadQuestions = function () {
+    $scope.contextId = $stateParams.courseId || $stateParams.contentId
+    $scope.loadQuestions = function() {
       $scope.loading = true
       $scope.widget = ''
-      courseQuestionsAdapter.getQuestions().then(function (data) {
+      courseQuestionsAdapter.getQuestions($scope.contextId).then(function(data) {
         $scope.loading = false
         $scope.widget = 'list-thread'
         $scope.thread = null
         $scope.threads = data.result.threads
-      }, function (err) {
+      }, function(err) {
         $scope.loading = false
         console.log('err', err)
       })
     }
     $scope.loadQuestions()
-    $scope.changeWidget = function (widgetName) {
+    $scope.changeWidget = function(widgetName) {
       $scope.widget = widgetName
     }
 
-    $scope.gotoThread = function (id) {
+    $scope.gotoThread = function(id) {
+      $scope.threadId = id
       $scope.changeWidget('reply-thread')
       $scope.loadThread(id)
     }
 
-    $scope.loadThread = function (threadId) {
+    $scope.loadReplyActions = function(replies) {
+      $scope.replyActions = {};
+      _.forEach(replies, function(reply) {
+        $scope.replyActions[reply.id] = {};
+        var actions = _.filter(reply.actions_summary, function(action) {
+          if (action.acted === true && action.can_undo == true) {
+            $scope.replyActions[reply.id][action.id] = 'acted'
+          } else {
+            $scope.replyActions[reply.id][action.id] = 'can_act'
+          }
+        })
+
+      })
+
+    }
+
+    $scope.showAction = function(replyId, actionTypeId) {
+      var action = $scope.replyActions[replyId][actionTypeId]
+      return action;
+    }
+
+    $scope.actions = function(replyId, actionTypeId) {
+      courseQuestionsAdapter.actions(replyId, actionTypeId).then(function(result) {
+        $scope.loadThread($scope.threadId)
+      }, function(err) {
+        console.log('error while voting', err)
+      })
+    }
+    $scope.markAsCorrect = function(replyId) {
+      courseQuestionsAdapter.markAsCorrect(replyId).then(function(result) {
+        $scope.loadThread($scope.threadId)
+      }, function(err) {
+        console.log('error while voting', err)
+      })
+    }
+    $scope.undoActions = function(replyId, actionId) {
+      courseQuestionsAdapter.undoActions(replyId, actionId).then(function(result) {
+        $scope.loadThread($scope.threadId)
+      }, function(err) {
+        console.log('error while voting', err)
+      })
+    }
+
+    $scope.loadThread = function(threadId) {
       $scope.loading = true
       $scope.widget = ''
-      courseQuestionsAdapter.getQuestionById(threadId).then(function (data) {
+      courseQuestionsAdapter.getQuestionById(threadId).then(function(data) {
         $scope.loading = false
         $scope.widget = 'reply-thread'
         console.log('data', data)
+        $scope.loadReplyActions(data.result.thread.replies)
         $scope.thread = data.result.thread
-      }, function (err) {
+      }, function(err) {
         $scope.loading = false
         console.log('err', err)
       })
     }
 
+    $scope.markCorrect = function (userId){
+      $scope.selectedAnswer = false
+      if (userId == '5001'){
+          $scope.selectedAnswer = true
+      }
+    }
+
+    $scope.unMarkCorrect = function (){
+      $scope.selectedAnswer = false
+    }
+
     $scope.formSubmit = function (isValid) {
       var obj = {
+        'contextId': $scope.contextId,
         'title': $scope.threadTitle,
         'description': $scope.threadDesc
       }
       $scope.loading = true
-      courseQuestionsAdapter.composeThread(obj).then(function (result) {
+      courseQuestionsAdapter.composeThread(obj).then(function(result) {
+        console.log(obj)
         $scope.loading = false
         $scope.loadQuestions()
-      }, function (err) {
+      }, function(err) {
         console.log('eeee', err)
       })
     }
 
-    $scope.submitAnswer = function () {
+    $scope.submitAnswer = function() {
       $scope.reply = {
+        'contextId': $scope.contextId,
         'description': $scope.replyAnswer
       }
       $scope.loading = true
-      courseQuestionsAdapter.replyThread($scope.thread.id, $scope.reply).then(function (result) {
+      courseQuestionsAdapter.replyThread($scope.thread.id, $scope.reply).then(function(result) {
         $scope.loading = false
         $scope.successMessage = false
-    			$timeout(function () {
-    				$scope.successMessage = true
-    			}, 3000)
+        $timeout(function() {
+          $scope.successMessage = true
+        }, 3000)
         $scope.loadQuestions()
-      }, function (err) {
+      }, function(err) {
         $scope.loading = false
         console.log('eeee', err)
       })
     }
+
+    $scope.flagAnswer = function (replyId){
+      console.log('replyId',replyId)
+      /*var obj = {
+        'actionType': replyId
+      }*/
+      courseQuestionsAdapter.updateFlag(replyId).then(function (data) {
+         console.log('result of updateFlag: ',data)
+      }, function (err) {
+        console.log('Error in flagAnswer', err)
+      })      
+    }
+
   }]
 })
--- src/app/private/scripts/services/adapters/courseQuestionsAdapter.js
+++ src/app/private/scripts/services/adapters/courseQuestionsAdapter.js
@@ -1,41 +1,118 @@
 'use strict'
 
 angular.module('playerApp').service('courseQuestionsAdapter', ['$rootScope', '$http',
-  'httpAdapter', '$q',
-  function ($rootScope, $http, httpAdapter, $q) {
-    this.getQuestions = function () {
+<<<<<<< HEAD
+  'httpAdapter', '$q', 'toasterService',
+  function ($rootScope, $http, httpAdapter, $q, toasterService) {
+    this.getQuestions = function (contextId) {
+
       var data = ''
-      return handleHttpRequest('/discussions/v1/list', data, 'GET',
-        $rootScope.messages.fmsg.m0070)
+      return handleHttpRequest('/discussions/v1/list/'+ contextId, data, 'GET',
+        'Error while loading questions, please try again later')
     }
-    this.getQuestionById = function (threadId) {
+    this.getQuestionById = function (threadId, postedBy) {
       var data = ''
       return handleHttpRequest('/discussions/v1/thread/' + threadId, data, 'GET',
-        $rootScope.messages.fmsg.m0070)
+        'Error while loading questions, please try again later')
     }
     this.composeThread = function (obj) {
       var data = obj
       console.log(data)
       return handleHttpRequest('/discussions/v1/thread', data, 'POST',
-        $rootScope.messages.fmsg.m0070)
+        'Error while posting the question, please try again later')
     }
+=======
+'httpAdapter', '$q', 'toasterService',
+function($rootScope, $http, httpAdapter, $q, toasterService) {
+  this.getQuestions = function(contextId) {
+    var data = ''
+    return handleHttpRequest('/discussions/v1/list/' + contextId, data, 'GET',
+      'Error while loading questions, please try again later')
+  }
+  this.getQuestionById = function(threadId) {
+    var data = ''
+    return handleHttpRequest('/discussions/v1/thread/' + threadId, data, 'GET',
+      'Error while loading questions, please try again later')
+  }
+  this.composeThread = function(obj) {
+    var data = obj
+    console.log(data)
+    return handleHttpRequest('/discussions/v1/thread', data, 'POST',
+      'Error while posting the question, please try again later')
+  }
+>>>>>>> 25d7a08b26451db5cfdb9553c32c97bf21d021dc
 
-    this.replyThread = function (threadId, obj) {
-      var data = obj
-      console.log(data)
-      return handleHttpRequest('/discussions/v1/thread/reply/' + threadId, data, 'POST',
-        $rootScope.messages.fmsg.m0070)
+  this.replyThread = function(threadId, obj) {
+    var data = obj
+    console.log(data)
+    return handleHttpRequest('/discussions/v1/thread/reply/' + threadId, data, 'POST',
+      'Error while posting your reply, please try again later')
+  }
+
+  this.actions = function(replyId, actionTypeId) {
+    var data = {
+      'actionTypeId': actionTypeId
+    }
+    return handleHttpRequest('/discussions/v1/thread/actions/' + replyId, data, 'POST',
+      'Error while up voting, please try again')
+  }
+
+  this.undoActions = function(replyId, actionTypeId) {
+    var data = {
+      'actionTypeId': actionTypeId,
+      'undo': 'true'
+    }
+<<<<<<< HEAD
+
+    this.updateFlag = function (replyId, obj) {
+      var data = ''
+      return handleHttpRequest('/discussions/v1/thread/flag/' + replyId, data, 'POST',
+        'Error in flag')
     }
 
     function handleHttpRequest (url, data, type, errMsg) {
       var deferred = $q.defer()
       var response = httpAdapter.httpCall(url, data, type)
       response.then(function (res) {
-        deferred.resolve(res)
+        if (res && res.responseCode === 'OK') {
+          deferred.resolve(res)
+        } else {
+          toasterService.error(errMsg)
+          deferred.reject(res)
+        }
       }, function (err) {
+        toasterService.error(errMsg)
         deferred.reject(err)
       })
       return deferred.promise
-    }
+=======
+    return handleHttpRequest('/discussions/v1/thread/actions/' + replyId, data, 'POST',
+      'Error while down voting, please try again')
   }
-])
+  this.markAsCorrect = function(replyId,isUndo) {
+    var data = {
+      'id': replyId
+>>>>>>> 25d7a08b26451db5cfdb9553c32c97bf21d021dc
+    }
+    return handleHttpRequest('/discussions/v1/thread/replies/marksolution', data, 'POST',
+      'Error while marking answer, please try again')
+  }
+
+
+function handleHttpRequest(url, data, type, errMsg) {
+  var deferred = $q.defer()
+  var response = httpAdapter.httpCall(url, data, type)
+  response.then(function(res) {
+    if (res && res.responseCode === 'OK') {
+      deferred.resolve(res)
+    } else {
+      toasterService.error(errMsg)
+      deferred.reject(res)
+    }
+  }, function(err) {
+    toasterService.error(errMsg)
+    deferred.reject(err)
+  })
+  return deferred.promise
+}
+}])
