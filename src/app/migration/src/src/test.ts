1226a4ea78d3d21a88c29ec4b10c940394fdc668
Nilesh More <nilesh_m@tekditechnologies.com>
2018-02-28 18:21:10 +0530

    Issue #SB-1485 fix: removed conflicts

--- src/app/migration/src/src/test.ts
+++ src/app/migration/src/src/test.ts
@@ -15,7 +15,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-// const context = require.context('./', true, /\.spec\.ts$/);
-const context = require.context('./', true, /user.service\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
