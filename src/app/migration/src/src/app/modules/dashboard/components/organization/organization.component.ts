c7145fd795fd730fc19215d11465cb38d2fe9a3b
Nilesh More <nilesh_m@tekditechnologies.com>
2018-03-01 14:05:51 +0530

    Issue #SB-1485 fix: label changes

--- src/app/migration/src/src/app/modules/dashboard/components/organization/organization.component.ts
+++ src/app/migration/src/src/app/modules/dashboard/components/organization/organization.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 // SB service(S)
 import { RendererService, OrganisationService, DownloadService } from './../../services';
