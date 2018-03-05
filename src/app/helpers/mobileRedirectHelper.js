b11f92bd2ee0e8d276babac57780ee03c67c344a
Anoop HM <anoophm44@gmail.com>
2018-03-05 21:59:24 +0530

    issue #SB-1913 feat: Mobile app redirection page(/get page)

--- src/app/helpers/mobileRedirectHelper.js
+++ src/app/helpers/mobileRedirectHelper.js
@@ -1,4 +1,5 @@
 const envHelper = require('./environmentVariablesHelper.js')
+
 module.exports = function (app) {
   app.all('/get/gg', function (req, res) {
     res.redirect(envHelper.MOBILE_REDIRECT_URL)
