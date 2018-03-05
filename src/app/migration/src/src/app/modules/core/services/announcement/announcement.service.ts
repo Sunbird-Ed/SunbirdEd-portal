d2a1a04517431746a6007fbbdee3cedb22661e1a
ArchanaKumari01 <Archana.Kumari@stackroute.in>
2018-03-05 09:01:24 +0530

    Issue #SB-1541 feat:home module and course service

--- src/app/migration/src/src/app/modules/core/services/announcement/announcement.service.ts
+++ src/app/migration/src/src/app/modules/core/services/announcement/announcement.service.ts
@@ -1,14 +1,24 @@
-import { ConfigService } from '@sunbird/shared';
 import { DataService } from './../data/data.service';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+  import { Injectable } from '@angular/core';
+  import { HttpClient } from '@angular/common/http';
+ import 'rxjs/add/operator/map';
+ import 'rxjs/add/observable/throw';
+ import { Observable } from 'rxjs/Observable';
+ // import { AnnouncementSericeParam } from './../../../announcement/interfaces/announcement.serivce';
+ import { ConfigService} from '@sunbird/shared';
+ interface AnnouncementSericeParam {
+  pageNumber?: number;
+     limit?: number;
+     announcementId?: string;
+     data?: object;
+ }
 /**
  * Service to provides CRUD methods to make announcement api request by extending DataService.
  *
  */
 @Injectable()
 export class AnnouncementService extends DataService {
+
   /**
    * base Url for announcement api
    */
@@ -31,4 +41,24 @@ export class AnnouncementService extends DataService {
     this.config = config;
     this.baseUrl = this.config.urlConFig.URLS.ANNOUNCEMENT_PREFIX;
   }
+
+  /**
+ * Method to make api call to get inbox data.
+ * It calls the post method from data service class
+ *
+ * @param {AnnouncementSericeParam} requestParam Request object needed for inbox API call
+ */
+public getInboxList(requestParam: AnnouncementSericeParam) {
+  const data = {
+      'request': {
+        'limit': requestParam.limit,
+        'offset': (requestParam.pageNumber - 1) * requestParam.limit
+      }
+  };
+  const option = {
+   url: this.config.urlConFig.URLS.ANNOUNCEMENT.INBOX_LIST,
+    data: data
+     };
+ return  this.post(option);
+}
 }
