d736d11afc2e709a9b4ebdb584908797f9b1c3a6
Sourav Dey <sourav_d@techjoomla.com>
2018-03-06 14:44:32 +0530

    Issue #SB-1774 feat:Announcement details component

--- src/app/migration/src/src/app/modules/announcement/announcement.module.ts
+++ src/app/migration/src/src/app/modules/announcement/announcement.module.ts
@@ -5,10 +5,8 @@ import { AnnouncementRoutingModule } from './announcement-routing.module';
 import { SuiModule } from 'ng2-semantic-ui';
 import { SharedModule, ResourceService, PaginationService, ToasterService, RouterNavigationService} from '@sunbird/shared';
 import { AnnouncementService} from '@sunbird/core';
-import { OutboxComponent, DeleteComponent, DetailsComponent} from './components/index';
-import { GeoExplorerComponent } from './components/geo-explorer/geo-explorer.component';
+import { OutboxComponent, DeleteComponent, DetailsComponent, GeoExplorerComponent, DetailsPopupComponent} from './components';
 import { GeoExplorerService } from './services';
-import { DetailsPopupComponent } from './components/details-popup/details-popup.component';
 
 @NgModule({
   imports: [
