a34577213a163633de9159c72251514aafef060a
unknown <vaishnavi@latitudefintech.com>
2018-03-21 11:02:03 +0530

    Issue #SB-2170 feat: Removing proxy.config.js file

--- src/app/migration/src/proxy.config.js
+++ src/app/migration/src/proxy.config.js
@@ -9,7 +9,7 @@ const PROXY_CONFIG = [
     target: 'http://localhost:3000',
     secure: false,
     logLevel: 'debug',
-    pathRewrite: {'/userId': '/781c21fc-5054-4ee0-9a02-fbb1006a4fdd'}
+    pathRewrite: {'/userId': '/159e93d1-da0c-4231-be94-e75b0c226d7c'}
   }
 ]
 module.exports = PROXY_CONFIG
