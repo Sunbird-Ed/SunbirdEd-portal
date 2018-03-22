37fd9e0ff16afe6d9193ba7157638a4badd65ea9
ArchanaKumari01 <Archana.Kumari@stackroute.in>
2018-03-22 11:27:52 +0530

    Issue #SB-1780 feat:refactor component and spec file

--- src/app/migration/src/src/app/modules/shared/components/content-card/content-card.component.ts
+++ src/app/migration/src/src/app/modules/shared/components/content-card/content-card.component.ts
@@ -6,19 +6,20 @@ import {IContents} from '../../interfaces/content';
 * This display a content card
 */
 @Component({
- selector: 'app-content-card',
- templateUrl: './content-card.component.html',
- styleUrls: ['./content-card.component.css']
+selector: 'app-content-card',
+templateUrl: './content-card.component.html',
+styleUrls: ['./content-card.component.css']
 })
 export class ContentCardComponent {
- /**
- * content is used to render IContents value on the view
- */
- @Input() content: IContents;
- @Output('ondelete')
- ondelete = new EventEmitter<any>();
+/**
+* content is used to render IContents value on the view
+*/
+@Input() content: IContents;
+@Output('actionClick')
+actionClick = new EventEmitter<any>();
 
- public onDelete(contentId) {
-   this.ondelete.emit(contentId);
- }
+public onAction(contentId, actionType) {
+  console.log(contentId);
+  this.actionClick.emit({'type': actionType, 'contentId': contentId});
+}
 }
