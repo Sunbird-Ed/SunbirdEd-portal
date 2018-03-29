294473b240b536e8e74e4fc68331bca889ae797a
Rajath <vbrajath@gmail.com>
2018-03-29 14:46:04 +0530

    Issue #SB-2352 feat:Content Creation tooling issues fixed

--- src/app/private/scripts/controllers/workspace/DataDrivenFormController.js
+++ src/app/private/scripts/controllers/workspace/DataDrivenFormController.js
@@ -120,7 +120,7 @@ angular.module('playerApp')
             dependedValues = _.map(associations, function (i) { return _.pick(i, 'name','category') })
             groupdFields = _.chain(dependedValues)
             .groupBy('category')
-            .map((name, category) => ({ name, category }))
+            .map( (name, category) => ({ name, category }))
             .value()
             dataDrivenForm.updateDropDownList(id, dependedValues)
             if (groupdFields.length) {
@@ -130,6 +130,7 @@ angular.module('playerApp')
           } else {
             dataDrivenForm.updateDropDownList(id, [])
           }
+          
           })
         }
       }
