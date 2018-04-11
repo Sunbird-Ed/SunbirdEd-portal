cdf3a918d1993a6981a90f3279ca5068ddcb58c0
Harishkumar Gangula <harishg@ilimi.in>
2018-04-11 08:44:47 +0530

    Issue #SB-2536 fix:  uniq array of orgs and roles

--- src/app/helpers/permissionsHelper.js
+++ src/app/helpers/permissionsHelper.js
@@ -131,6 +131,9 @@ let PERMISSIONS_HELPER = {
                 }
               })
             }
+            reqObj.session.orgs = _.uniq(reqObj.session.orgs)
+            reqObj.session.roles = _.uniq(reqObj.session.roles)
+
             if (body.result.response.rootOrg && body.result.response.rootOrg.id) {
               reqObj.session.rootOrgId = body.result.response.rootOrg.id
               reqObj.session.rootOrghashTagId = body.result.response.rootOrg.hashTagId
