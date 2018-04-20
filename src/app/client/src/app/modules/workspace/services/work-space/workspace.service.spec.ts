6b489be44f571ab12bd45dea2cb722c54427b7da
amantarento <amankumar.sharma@tarento.com>
2018-04-20 17:13:35 +0530

    integration with content-editior and respective change in content card

--- src/app/client/src/app/modules/workspace/services/work-space/workspace.service.spec.ts
+++ src/app/client/src/app/modules/workspace/services/work-space/workspace.service.spec.ts
@@ -16,29 +16,37 @@ const testData = mockData.mockRes;
 describe('WorkSpaceService', () => {
   class RouterStub {
     navigate = jasmine.createSpy('navigate');
-  };
+  }
   const fakeActivatedRoute = {
     'params': Observable.from([{ pageNumber: '1' }]),
   };
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, HttpClientModule, CoreModule, SharedModule],
-      providers: [WorkSpaceService ,{ provide: Router, useClass: RouterStub },
-      { provide: ActivatedRoute, useValue: fakeActivatedRoute }]
+      providers: [WorkSpaceService,
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useValue: fakeActivatedRoute }]
     });
   });
-  it('should  launch  content  editor when mime type is content-collection', inject([WorkSpaceService, Router], (workSpaceService, route) => {
-    workSpaceService.openContentEditor(testData.sucessData.result.content[0], 'draft');
-    expect(route.navigate).toHaveBeenCalledWith(['/workspace/content/edit/contentEditor/', 'do_1124858179748904961134', 'draft', 'NCF']);
+  it('should  launch  content  editor when mime type is content-collection',
+    inject([WorkSpaceService, Router], (workSpaceService, route) => {
+      workSpaceService.openContentEditor(testData.sucessData.result.content[0], 'draft');
+      expect(route.navigate).toHaveBeenCalledWith(['/workspace/content/edit/contentEditor/', 'do_1124858179748904961134', 'draft', 'NCF']);
   }));
-  it('should  launch  collection  editor when mime type is ecml-archive', inject([WorkSpaceService, Router], (workSpaceService, route) => {
+
+  it('should  launch  collection  editor when mime type is ecml-archive',
+    inject([WorkSpaceService, Router], (workSpaceService, route) => {
     workSpaceService.openContentEditor(testData.sucessData.result.content[1], 'review');
-    expect(route.navigate).toHaveBeenCalledWith(['/workspace/content/edit/collection', 'do_1124858179748904961134', 'Resource', 'review', 'NCF']);
+    expect(route.navigate).toHaveBeenCalledWith(['/workspace/content/edit/collection',
+    'do_1124858179748904961134', 'Resource', 'review', 'NCF']);
   }));
-  it('should  launch  generic  editor when mime type is not matching ', inject([WorkSpaceService, Router], (workSpaceService, route) => {
-    workSpaceService.openContentEditor(testData.sucessData.result.content[2], 'review');
-    expect(route.navigate).toHaveBeenCalledWith(['/workspace/content/edit/generic/', 'do_1124858179748904961134', 'review']);
+
+  it('should  launch  generic  editor when mime type is not matching ',
+    inject([WorkSpaceService, Router], (workSpaceService, route) => {
+      workSpaceService.openContentEditor(testData.sucessData.result.content[2], 'review');
+      expect(route.navigate).toHaveBeenCalledWith(['/workspace/content/edit/generic/', 'do_1124858179748904961134', 'review']);
   }));
+
   it('should call delete api and get success response', inject([WorkSpaceService],
     (workSpaceService) => {
       spyOn(workSpaceService, 'deleteContent').and.callFake(() => Observable.of(testData.deleteSuccess));
@@ -49,6 +57,7 @@ describe('WorkSpaceService', () => {
       workSpaceService.deleteContent(DeleteParam);
       expect(workSpaceService).toBeTruthy();
   }));
+
   it('should call delete api and  get error response', inject([WorkSpaceService],
     (workSpaceService) => {
       spyOn(workSpaceService, 'deleteContent').and.callFake(() => Observable.throw(testData.deleteError));
