7e955a3789993da05cea1e7c1cc8f17976a74cdd
sunil <sunils@ilimi.in>
2018-04-24 15:13:39 +0530

    Issue #SB-2145 refactor: Telemetry service
    changes: unit test cases

--- src/app/client/src/app/modules/core/services/telemetry/telemetry.service.spec.ts
+++ src/app/client/src/app/modules/core/services/telemetry/telemetry.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { TelemetryService } from './telemetry.service';
+import { TelemetryService, TELEMETRY_PROVIDER } from './telemetry.service';
 import * as mockData from './telemetry.service.spec.data';
 
 describe('TelemetryService', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [],
-            providers: [TelemetryService]
+            providers: [TelemetryService, { provide: TELEMETRY_PROVIDER, useValue: EkTelemetry}]
         });
     });
 
