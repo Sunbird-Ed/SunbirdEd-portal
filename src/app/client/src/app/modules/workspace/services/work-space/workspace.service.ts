1dfbc134200b6b20a722329ec54b58afa6870be0
amantarento <amankumar.sharma@tarento.com>
2018-04-25 16:17:39 +0530

    Limited publish component and share link component

--- src/app/client/src/app/modules/workspace/services/work-space/workspace.service.ts
+++ src/app/client/src/app/modules/workspace/services/work-space/workspace.service.ts
@@ -6,7 +6,6 @@ import 'rxjs/add/observable/throw';
 import { ConfigService, ServerResponse } from '@sunbird/shared';
 import { ContentService } from '@sunbird/core';
 import { IDeleteParam } from '../../interfaces/delteparam';
-import { DOCUMENT } from '@angular/platform-browser';
 @Injectable()
 export class WorkSpaceService {
   /**
@@ -26,10 +25,9 @@ export class WorkSpaceService {
   * @param {ConfigService} config ConfigService reference
   * @param {HttpClient} http HttpClient reference
   */
-  constructor(config: ConfigService, content: ContentService , @Inject(DOCUMENT) private document: any) {
+  constructor(config: ConfigService, content: ContentService) {
     this.content = content;
     this.config = config;
-    this.baseUrl = document.location.origin + '/';
   }
   deleteContent(requestParam: IDeleteParam): Observable<ServerResponse> {
     const option = {
@@ -41,29 +39,5 @@ export class WorkSpaceService {
       }
     };
     return this.content.delete(option);
-  }
-  /**
-  * getBase64Url
-  * generate the base url to play unlisted content for public users.
-  * {object}  identifier - content or course identifier
-  * returns {string} type - content or course type.
-  */
-  getBase64Url(type, identifier) {
-    return btoa(type + '/' + identifier);
-  }
-  /**
-  * getUnlistedShareUrl
-  * generate the url to play unlisted content for other users.
-  * {object}  cData - content data
-  * returns {string} url to share.
-  */
-  getUnlistedShareUrl(content) {
-    if (content.contentType === 'Course') {
-      return this.baseUrl + 'unlisted' + '/' + this.getBase64Url('course', content.identifier);
-    } else if (content.mimeType === 'application/vnd.ekstep.content-collection') {
-      return this.baseUrl + 'unlisted' + '/' + this.getBase64Url('collection', content.identifier);
-    } else {
-      return this.baseUrl + 'unlisted' + '/' + this.getBase64Url('content', content.identifier);
-    }
   }
 }
