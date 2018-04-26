8c9d0eed9c5fb936a9f9a3d238704577c5a42da6
Kartheek Palla <palla.kartheekreddy@gmail.com>
2018-04-26 17:03:25 +0530

    Issue #SB-2933 fix: sunbird common header plugin & editors version updated

--- src/app/private/scripts/controllers/contentEditorController.js
+++ src/app/private/scripts/controllers/contentEditorController.js
@@ -39,7 +39,7 @@ angular.module('playerApp')
           plugins: [
             {
               id: 'org.ekstep.sunbirdcommonheader',
-              ver: '1.3',
+              ver: '1.4',
               type: 'plugin'
             },
             {
