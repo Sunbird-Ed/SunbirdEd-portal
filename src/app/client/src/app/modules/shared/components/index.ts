cbd006ac28fb4dd89dcfbbd13755815ff2b2ecf2
sunil <sunils@ilimi.in>
2018-04-27 12:45:33 +0530

    Issue #SB-2936 feat: collection-tree
    changes:
    1. component created: collection-tree to show collections
    2. component created: fancy-tree
    3. added typings
    4. removed typings from collection-editor component
    5. main header component HTML changes

--- src/app/client/src/app/modules/shared/components/index.ts
+++ src/app/client/src/app/modules/shared/components/index.ts
@@ -3,5 +3,6 @@ export * from './app-loader/app-loader.component';
 export * from './content-card/content-card.component';
 export * from './no-result/no-result.component';
 export * from './page-section/page-section.component';
-
+export * from './collection-tree/collection-tree.component';
+export * from './fancy-tree/fancy-tree.component';
 
