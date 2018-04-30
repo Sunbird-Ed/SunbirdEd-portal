b5f54f90290e8d67454bb52faf2bb75cf6f139ee
unknown <vaishnavi@latitudefintech.com>
2018-04-30 17:19:05 +0530

    Issue #SB-1536 feat: Resolved conflicts

--- src/app/client/src/app/modules/public/components/signup/signup.component.ts
+++ src/app/client/src/app/modules/public/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResourceService } from '@sunbird/shared';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(public resourceService: ResourceService) { }
 
   ngOnInit() {
+  }
+  redirect() {
+    console.log('here');
   }
 
 }
