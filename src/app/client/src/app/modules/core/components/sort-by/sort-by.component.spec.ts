0129124c9ac3f4811dfc31259865f3522e86e3e0
ArchanaKumari01 <Archana.Kumari@stackroute.in>
2018-05-02 11:28:14 +0530

    Issue #SB-2121 feat:sort by component

--- src/app/client/src/app/modules/core/components/sort-by/sort-by.component.spec.ts
+++ src/app/client/src/app/modules/core/components/sort-by/sort-by.component.spec.ts
@@ -1,14 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SortByComponent } from './sort-by.component';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SharedModule, ResourceService, ConfigService } from '@sunbird/shared';
+import { Observable } from 'rxjs/Observable';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 
 describe('SortByComponent', () => {
   let component: SortByComponent;
   let fixture: ComponentFixture<SortByComponent>;
-
+  class RouterStub {
+    navigate = jasmine.createSpy('navigate');
+  }
+  const fakeActivatedRoute = {
+    'queryParams': Observable.from([{ sortType: 'asc', sort_by: 'createdOn' }])
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SortByComponent ]
+      declarations: [ SortByComponent ],
+      imports: [HttpClientTestingModule, SharedModule],
+      providers: [ ResourceService, ConfigService,
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useValue: fakeActivatedRoute }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -18,8 +33,23 @@ describe('SortByComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should call applySorting method with learn url ', inject([ConfigService, Router],
+    (configService, route) => {
+      component.url = 'learn';
+      const sortByOption = 'createdOn';
+      const queryParams = { sortType: 'asc', sort_by: 'createdOn'};
+     component.applySorting(sortByOption);
+     fixture.detectChanges();
+     expect(route.navigate).toHaveBeenCalledWith(['learn'], {queryParams: queryParams});
+  }));
+  it('should call applySorting method with search/Courses url ', inject([ConfigService, Router],
+    (configService, route) => {
+      component.url = 'search/Courses/1';
+      const sortByOption = 'createdOn';
+      const queryParams = { sortType: 'asc', sort_by: 'createdOn'};
+     component.applySorting(sortByOption);
+     fixture.detectChanges();
+     expect(route.navigate).toHaveBeenCalledWith(['search/Courses/1'], {queryParams: queryParams});
+  }));
 });
+
