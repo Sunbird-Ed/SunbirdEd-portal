2605933ab14556d63d2f863ab4b8be1c805ca43c
Rajath <vbrajath@gmail.com>
2018-05-18 18:25:55 +0530

    Issue #SB-2157 feat:course consumption integration done

--- src/app/client/src/app/modules/learn/components/course-consumption/course-consumption-header/course-consumption-header.component.ts
+++ src/app/client/src/app/modules/learn/components/course-consumption/course-consumption-header/course-consumption-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { CourseConsumptionService } from './../../../services';
+import { CourseConsumptionService, CourseProgressService } from './../../../services';
 import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import * as _ from 'lodash';
@@ -14,20 +14,37 @@ export class CourseConsumptionHeaderComponent implements OnInit {
   @Input() courseHierarchy: any;
   @Input() enrolledCourse: boolean;
   permission = ['COURSE_MENTOR'];
+  lastPlayedContentId: string;
+  showResumeCourse = true;
   constructor(private activatedRoute: ActivatedRoute, private courseConsumptionService: CourseConsumptionService,
-    public resourceService: ResourceService, private router: Router, public permissionService: PermissionService) {
+    public resourceService: ResourceService, private router: Router, public permissionService: PermissionService,
+    private courseProgressService: CourseProgressService) {
 
     }
 
   ngOnInit() {
+    this.courseProgressService.courseProgressData.subscribe((courseProgressData) => {
+      this.courseHierarchy.progress = courseProgressData.progress;
+      this.lastPlayedContentId = courseProgressData.lastPlayedContentId;
+      this.showResumeCourse = false;
+    });
   }
   showDashboard() {
     this.router.navigate(['dashboard'], {relativeTo: this.activatedRoute.firstChild});
   }
   resumeCourse() {
+    this.navigateToContent(this.lastPlayedContentId);
     console.log('resume course');
   }
   flagCourse() {
     this.router.navigate(['flag'], {relativeTo: this.activatedRoute.firstChild});
+  }
+
+    private navigateToContent( id: string ): void {
+    const navigationExtras: NavigationExtras = {
+      queryParams: { 'contentId': id },
+      relativeTo: this.activatedRoute
+    };
+    this.router.navigate([], navigationExtras);
   }
 }
