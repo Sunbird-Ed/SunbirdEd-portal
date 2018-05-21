988df3024937f53dc6036dcd874544dc73522c1f
amantarento <amankumar.sharma@tarento.com>
2018-05-21 14:50:51 +0530

    import input and add fuction

--- src/app/client/src/app/modules/telemetry/directives/telemetry-interact.directive.ts
+++ src/app/client/src/app/modules/telemetry/directives/telemetry-interact.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appTelemetryInteract]'
 })
 export class TelemetryInteractDirective {
-
-  constructor() { }
-
+  @Input('appTelemetryInteract') appTelemetryInteract: any;
+  constructor(private elRef: ElementRef) { }
+  telemetryInteract() {
+    console.log('Telemetry service are called ');
+  }
 }
+
+
