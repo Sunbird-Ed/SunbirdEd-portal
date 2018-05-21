644b374402d4baece1c08ecf9f0ee15fdbd0e6a1
amantarento <amankumar.sharma@tarento.com>
2018-05-21 17:41:22 +0530

    aded telemetry directive

--- src/app/client/src/app/modules/telemetry/directives/telemetry-interact.directive.ts
+++ src/app/client/src/app/modules/telemetry/directives/telemetry-interact.directive.ts
@@ -1,13 +1,38 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { IInteractEventInput } from '../interfaces/telemetry';
+import { TelemetryService } from './../services';
 
+/**
+ * TelemetryInteract Directive
+ */
 @Directive({
   selector: '[appTelemetryInteract]'
 })
-export class TelemetryInteractDirective {
-  @Input('appTelemetryInteract') appTelemetryInteract: any;
-  constructor(private elRef: ElementRef) { }
-  telemetryInteract() {
-    console.log('Telemetry service are called ');
+export class TelemetryInteractDirective implements OnInit {
+  /**
+   * Interact event input
+  */
+  @Input('appTelemetryInteract') appTelemetryInteract: IInteractEventInput;
+  /**
+   * reference of permissionService service.
+  */
+  public telemetryService: TelemetryService;
+  /**
+  * Constructor to create injected service(s) object
+  Default method of Draft Component class
+  * @param {TelemetryService} telemetryService Reference of TelemetryService
+  */
+  constructor(private elRef: ElementRef, telemetryService: TelemetryService) {
+    this.telemetryService = telemetryService;
+  }
+  ngOnInit() {
+    if (this.appTelemetryInteract) {
+      this.interact();
+    }
+  }
+  interact() {
+    console.log('call TelemetryInteract method ');
+    this.telemetryService.interact(this.appTelemetryInteract);
   }
 }
 
