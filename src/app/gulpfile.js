cf4466dda46cc57c4f458c0473f76b41e825ea5f
Harishkumar Gangula <harishg@ilimi.in>
2018-05-24 12:37:27 +0530

    Issue #SB-3762 fix: build changes

--- src/app/gulpfile.js
+++ src/app/gulpfile.js
@@ -78,7 +78,7 @@ gulp.task('prepare:app:dist', () => {
     'themes/**/*',
     'package.json',
     'package-lock.json',
-    'content-plugins/**/*'
+    'content-plugins/**/*',
     'server.js'], { 'base': '.' })
         .pipe(gulp.dest('./app_dist'))
 })
