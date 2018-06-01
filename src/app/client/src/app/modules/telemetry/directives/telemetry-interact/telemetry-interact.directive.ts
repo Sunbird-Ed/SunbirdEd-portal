b188fc9204b54a7e5ae106c22e07f18062f3b494
Anoop HM <anoophm44@gmail.com>
2018-06-01 13:02:18 +0530

    Issue #SB 3659 fix:  InteractEvent

--- src/app/client/src/app/modules/telemetry/directives/telemetry-interact/telemetry-interact.directive.ts
+++ src/app/client/src/app/modules/telemetry/directives/telemetry-interact/telemetry-interact.directive.ts
@@ -1,6 +1,7 @@
-import { Directive, Input, OnInit, OnChanges } from '@angular/core';
-import { IInteractEventInput } from '../../interfaces';
+import { Directive, Input, OnInit, OnChanges, HostListener } from '@angular/core';
+import { IInteractEventInput, IInteractEventObject , IInteractEventEdata } from '../../interfaces';
 import { TelemetryService } from '../../services';
+import { ActivatedRoute, Router } from '@angular/router';
 
 /**
  * TelemetryInteract Directive
@@ -8,26 +9,42 @@ import { TelemetryService } from '../../services';
 @Directive({
   selector: '[appTelemetryInteract]'
 })
-export class TelemetryInteractDirective implements OnChanges {
+export class TelemetryInteractDirective {
   /**
    * Interact event input
   */
-  @Input('appTelemetryInteract') appTelemetryInteract: IInteractEventInput;
-  /**
-   * reference of permissionService service.
-  */
+  appTelemetryInteractData: IInteractEventInput;
+
   public telemetryService: TelemetryService;
+
+  @Input() telemetryInteractObject: IInteractEventObject;
+  @Input() telemetryInteractEdata: IInteractEventEdata;
+
+  @HostListener('click', ['$event'])
+  private onClick(e) {
+
+      console.log(this.telemetryInteractObject, this.telemetryInteractEdata);
+
+      if (this.telemetryInteractObject && this.telemetryInteractEdata) {
+        this.appTelemetryInteractData = {
+          context: {
+            env: this.activatedRoute.snapshot.data.telemetry.env
+          },
+          object: this.telemetryInteractObject,
+          edata: this.telemetryInteractEdata
+        };
+        console.log('intract event data', this.appTelemetryInteractData);
+        this.telemetryService.interact(this.appTelemetryInteractData);
+      }
+  }
   /**
-  * Constructor to create injected service(s) object
-  Default method of Draft Component class
+  * Constructor to create injected service(s) object Default method of Draft Component class
   * @param {TelemetryService} telemetryService Reference of TelemetryService
   */
-  constructor(telemetryService: TelemetryService) {
+  constructor( telemetryService: TelemetryService, public activatedRoute: ActivatedRoute,
+    public router: Router) {
     this.telemetryService = telemetryService;
   }
-  ngOnChanges() {
-    if (this.appTelemetryInteract) {
-      this.telemetryService.interact(this.appTelemetryInteract);
-    }
-  }
+
 }
+
