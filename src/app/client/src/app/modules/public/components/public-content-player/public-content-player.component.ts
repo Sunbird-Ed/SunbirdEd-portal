4693356323671cfcb4d3207716da33fcdabbcdd6
Sourav <ekta_y@tekditechnologies.com>
2018-06-02 11:27:27 +0530

    Issue #3237 fix: Anonomyous consumption

--- src/app/client/src/app/modules/public/components/public-content-player/public-content-player.component.ts
+++ src/app/client/src/app/modules/public/components/public-content-player/public-content-player.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ContentService, UserService } from '@sunbird/core';
 import * as _ from 'lodash';
+import { Observable } from 'rxjs/Observable';
 import {
   ConfigService, IUserData, ResourceService, ToasterService,
   WindowScrollService, NavigationHelperService, PlayerConfig, ContentData
@@ -38,22 +39,39 @@ export class PublicContentPlayerComponent implements OnInit {
   /**
    * contain contentData
    */
+  selectedLanguage: string;
+  queryParams: any;
   contentData: ContentData;
   constructor(public activatedRoute: ActivatedRoute, public userService: UserService,
     public resourceService: ResourceService, public toasterService: ToasterService,
     public windowScrollService: WindowScrollService, public playerService: PublicPlayerService,
     public navigationHelperService: NavigationHelperService
-    ) {
+  ) {
   }
   /**
    *
    * @memberof ContentPlayerComponent
    */
   ngOnInit() {
-    this.activatedRoute.params.subscribe((params) => {
-      this.contentId = params.contentId;
-      this.getContent();
-    });
+    Observable
+      .combineLatest(
+      this.activatedRoute.params,
+      this.activatedRoute.queryParams,
+      (params: any, queryParams: any) => {
+        return {
+          params: params,
+          queryParams: queryParams
+        };
+      })
+      .subscribe(bothParams => {
+        this.contentId = bothParams.params.contentId;
+        this.getContent();
+        this.queryParams = { ...bothParams.queryParams };
+        if (this.queryParams['language'] && this.queryParams['language'] !== this.selectedLanguage) {
+          this.selectedLanguage = this.queryParams['language'];
+          this.resourceService.getResource(this.selectedLanguage);
+        }
+      });
   }
   /**
    * used to fetch content details and player config. On success launches player.
