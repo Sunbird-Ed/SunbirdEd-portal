5528a1bf52c0c0ede3cbe609dab9cca2b45b7aa5
revathi <revathipp@gmail.com>
2018-06-03 10:07:23 +0530

    Issue #SB-3732 fix: removed local changes

--- src/app/client/src/app/modules/workspace/components/create-editors/generic-editor/generic-editor.component.ts
+++ src/app/client/src/app/modules/workspace/components/create-editors/generic-editor/generic-editor.component.ts
@@ -163,10 +163,6 @@ export class GenericEditorComponent implements OnInit, AfterViewInit, OnDestroy 
         id: 'org.ekstep.metadata',
         ver: '1.1',
         type: 'plugin'
-      }, {
-        id: 'org.ekstep.uploadcontent',
-        ver: '1.1',
-        type: 'plugin'
       }],
       previewConfig: {
         'repos': ['/content-plugins/renderer'],
