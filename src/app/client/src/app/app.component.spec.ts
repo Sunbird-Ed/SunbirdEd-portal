196e821b100315d36b7bbb8b9c2256eb50267adc
Anoop HM <anoophm44@gmail.com>
2018-06-04 21:42:20 +0530

    Issue #SB 4170 feat:  Public page impression events

--- src/app/client/src/app/app.component.spec.ts
+++ src/app/client/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { OrgManagementService } from '@sunbird/public';
 
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ConfigService, ToasterService, ResourceService, SharedModule } from '@sunbird/shared';
@@ -24,7 +25,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-      providers: [ToasterService, TenantService,
+      providers: [ToasterService, TenantService, OrgManagementService,
         UserService, ConfigService, LearnerService,
         PermissionService, ResourceService, CoursesService,
         TelemetryService, { provide: TELEMETRY_PROVIDER, useValue: EkTelemetry }, ConceptPickerService, SearchService, ContentService],
