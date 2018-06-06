732b8673100124e1985a92961749bf91f9c0e646
Harishkumar Gangula <harishg@ilimi.in>
2018-06-06 15:49:15 +0530

    Issue #SB-4209 feat: cdn changes in view files

--- src/app/client/src/app/modules/community/community.module.ts
+++ src/app/client/src/app/modules/community/community.module.ts
@@ -1,13 +1,14 @@
 import { CommunityListComponent } from './components';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '@sunbird/shared';
 import { CommunityRoutingModule } from './community-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
     CommunityRoutingModule,
+    SharedModule
   ],
   declarations: [CommunityListComponent]
 })
--- src/app/client/src/app/modules/discussion/discussion.module.ts
+++ src/app/client/src/app/modules/discussion/discussion.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DiscussionRoutingModule } from './discussion-routing.module';
 import { DiscussionsComponent } from './components/discussions/discussions.component';
+import { SharedModule } from '@sunbird/shared';
 
 @NgModule({
   imports: [
     CommonModule,
-    DiscussionRoutingModule
+    DiscussionRoutingModule,
+    SharedModule
   ],
   declarations: [DiscussionsComponent],
   exports: [DiscussionsComponent]
