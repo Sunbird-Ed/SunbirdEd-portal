852c5a6c75b91a05531f0e06c9bb145992bb6295
ArchanaKumari01 <Archana.Kumari@stackroute.in>
2018-06-12 14:28:01 +0530

    Issue #SB-4514 feat: home,shared,core,profile module test case

--- src/app/client/src/app/modules/telemetry/services/telemetry/telemetry.service.ts
+++ src/app/client/src/app/modules/telemetry/services/telemetry/telemetry.service.ts
@@ -5,7 +5,7 @@ import {
   IStartEventInput, IImpressionEventInput,
   IInteractEventInput, IShareEventInput, IErrorEventInput, IEndEventInput, ILogEventInput, ITelemetryContext
 } from './../../interfaces/telemetry';
-// export const TELEMETRY_PROVIDER = new InjectionToken('telemetryProvider');
+ export const TELEMETRY_PROVIDER = new InjectionToken('telemetryProvider');
 /**
 * Service for telemetry v3 event methods
 */
