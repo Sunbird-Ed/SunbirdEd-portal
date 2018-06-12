50bb7f7961ca23388bf8539bf3065629df021566
ArchanaKumari01 <Archana.Kumari@stackroute.in>
2018-06-12 15:11:12 +0530

    Issue #SB-4514 feat: home,shared,core,profile module test case

--- src/app/client/src/app/modules/workspace/components/limited-published/limited-published.component.spec.ts
+++ src/app/client/src/app/modules/workspace/components/limited-published/limited-published.component.spec.ts
@@ -163,7 +163,7 @@ describe('LimitedPublishedComponent', () => {
       };
       component.contentClick(params);
       expect(route.navigate).toHaveBeenCalledWith(['/workspace/content/edit/collection',
-        'do_2124341006465925121871', 'TextBook', 'limited/publish', 'NCF']);
+        'do_2124341006465925121871', 'TextBook', 'limited-publish', 'NCF']);
       fixture.detectChanges();
     }));
   it('should throw error', inject([SearchService], (searchService) => {
