76bf1c9d21e8baba93cd1aeafb629dfa820aff11
Harishkumar Gangula <harishg@ilimi.in>
2018-06-18 17:06:42 +0530

    Issue #SB-4209 feat: loading index.ejs on node start to local

--- src/app/server.js
+++ src/app/server.js
@@ -428,7 +428,9 @@ if (!process.env.sunbird_environment || !process.env.sunbird_instance) {
 }
 
 portal.server = app.listen(port, function () {
-  request(envHelper.PORTAL_CDN_URL + 'index_'+packageObj.version+'.'+packageObj.buildNumber+'.ejs?version=' ).pipe(fs.createWriteStream(path.join(__dirname, 'dist', 'index.ejs')));
+  if(envHelper.PORTAL_CDN_URL){
+    request(envHelper.PORTAL_CDN_URL + 'index.ejs?version='packageObj.version+'.'+packageObj.buildNumber).pipe(fs.createWriteStream(path.join(__dirname, 'dist', 'index.ejs')));
+  }
   console.log('app running on port ' + port)
 })
 
