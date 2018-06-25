aa0430ca17781e8616d8464a15d1c16373f4a912
ARUSHI-PC\Prajwal <prajwal.hj@compassitesinc.com>
2018-06-25 17:09:54 +0530

    Issue #SB-1332 fix: Home Page : Backward arrow on To Do list is not placed correctly

--- src/app/client/src/app/modules/home/component/profile-card/profile-card.component.spec.ts
+++ src/app/client/src/app/modules/home/component/profile-card/profile-card.component.spec.ts
@@ -29,13 +29,13 @@ describe('ProfileCardComponent', () => {
   it('for success test data', () => {
     component.profile = testData.successData;
     fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector('div span .completness').innerText).toEqual('69');
+    expect(fixture.nativeElement.querySelector('div span.profile-update-subheading span').innerText).toEqual('69');
     expect(fixture.nativeElement.querySelector('div.padding-top-5').innerText).toEqual('Add dob');
   });
   it('for no test data', () => {
     component.profile = testData.parsedData;
     fixture.detectChanges();
-    expect(fixture.nativeElement.querySelector('div span .completness').innerText).toEqual('');
+    expect(fixture.nativeElement.querySelector('div span.profile-update-subheading span').innerText).toEqual('');
     expect(fixture.nativeElement.querySelector('div.padding-top-5')).toEqual(null);
   });
 });
