89e196713c47b0aaa6606e360e81c0cf6fb2efd4
shakkeel1330 <shakkeelahammed@gmail.com>
2018-07-11 17:24:18 +0530

    Issue #SB-5269: Explore(page section) - Implementation design

---
+++ src/app/client/src/app/modules/public/module/explore/components/explore/explore.component.ts
@@ -0,0 +1,133 @@
+import {combineLatest as observableCombineLatest,  Observable } from 'rxjs';
+import { PageApiService, PlayerService, ISort } from '@sunbird/core';
+import { Component, OnInit } from '@angular/core';
+import { ResourceService, ServerResponse, ToasterService, INoResultMessage, ConfigService, UtilService} from '@sunbird/shared';
+import { ICaraouselData, IAction } from '@sunbird/shared';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as _ from 'lodash';
+import { IInteractEventObject, IInteractEventEdata, IImpressionEventInput } from '@sunbird/telemetry';
+
+@Component({
+  selector: 'app-explore',
+  templateUrl: './explore.component.html',
+  styleUrls: ['./explore.component.css']
+})
+export class ExploreComponent implements OnInit {
+/**
+ * To show toaster(error, success etc) after any API calls
+ */
+private toasterService: ToasterService;
+/**
+ * To call resource service which helps to use language constant
+ */
+public resourceService: ResourceService;
+/**
+* To call get resource data.
+*/
+private pageSectionService: PageApiService;
+/**
+ * This variable hepls to show and hide page loader.
+ * It is kept true by default as at first when we comes
+ * to a page the loader should be displayed before showing
+ * any data
+ */
+showLoader = true;
+  /**
+  * To show / hide no result message when no result found
+ */
+noResult = false;
+ /**
+ * no result  message
+*/
+noResultMessage: INoResultMessage;
+/**
+* Contains result object returned from getPageData API.
+*/
+caraouselData: Array<ICaraouselData> = [];
+public config: ConfigService;
+public filterType: string;
+public filters: any;
+public queryParams: any;
+private router: Router;
+public redirectUrl: string;
+sortingOptions: Array<ISort>;
+contents: any;
+/**
+ * The "constructor"
+ *
+ * @param {PageApiService} pageSectionService Reference of pageSectionService.
+ * @param {ToasterService} iziToast Reference of toasterService.
+ */
+constructor(pageSectionService: PageApiService, toasterService: ToasterService, private playerService: PlayerService,
+  resourceService: ResourceService, config: ConfigService, private activatedRoute: ActivatedRoute, router: Router,
+public utilService: UtilService) {
+  this.pageSectionService = pageSectionService;
+  this.toasterService = toasterService;
+  this.resourceService = resourceService;
+  this.config = config;
+  this.router = router;
+  this.router.onSameUrlNavigation = 'reload';
+  this.sortingOptions = this.config.dropDownConfig.FILTER.RESOURCES.sortingOptions;
+}
+
+  populatePageData() {
+    this.showLoader = true;
+    this.noResult = false;
+    const option = {
+      source: 'web',
+      name: 'Resource',
+      filters: {},
+      sort_by: {}
+    };
+    this.pageSectionService.getPageData(option).subscribe(
+      (apiResponse) => {
+        if (apiResponse && apiResponse.sections ) {
+          let noResultCounter = 0;
+          this.showLoader = false;
+          this.caraouselData = apiResponse.sections;
+          _.forEach(this.caraouselData, (value, index) => {
+              if (this.caraouselData[index].contents && this.caraouselData[index].contents.length > 0) {
+                const constantData = this.config.appConfig.Library.constantData;
+                const metaData = this.config.appConfig.Library.metaData;
+                const dynamicFields = this.config.appConfig.Library.dynamicFields;
+                this.caraouselData[index].contents = this.utilService.getDataForCard(this.caraouselData[index].contents,
+                  constantData, dynamicFields, metaData);
+              }
+          });
+          if (this.caraouselData.length > 0) {
+            _.forIn(this.caraouselData, (value, key) => {
+              if (this.caraouselData[key].contents === null) {
+                noResultCounter++;
+              } else if (this.caraouselData[key].contents === undefined) {
+                noResultCounter++;
+              }
+            });
+          }
+          if (noResultCounter === this.caraouselData.length) {
+            this.noResult = true;
+            this.noResultMessage = {
+              'message': this.resourceService.messages.stmsg.m0007,
+              'messageText': this.resourceService.messages.stmsg.m0006
+            };
+          }
+        }
+      },
+      err => {
+        this.noResult = true;
+        this.noResultMessage = {
+          'message': this.resourceService.messages.stmsg.m0007,
+          'messageText': this.resourceService.messages.stmsg.m0006
+        };
+        this.showLoader = false;
+        this.toasterService.error(this.resourceService.messages.fmsg.m0004);
+      }
+    );
+  }
+
+  ngOnInit() {
+    this.filterType = this.config.appConfig.library.filterType;
+    this.redirectUrl = this.config.appConfig.library.inPageredirectUrl;
+    this.populatePageData();
+  }
+
+}
