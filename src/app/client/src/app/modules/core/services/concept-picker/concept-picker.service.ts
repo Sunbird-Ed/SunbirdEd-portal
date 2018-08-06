aca7f444041e3053260c208eae679a7f608eb08d
amantarento <amankumar.sharma@tarento.com>
2018-08-06 15:53:08 +0530

    Issue:SB-6071 Content Creation : Selected concepts are displayed correctly for reviewer user

--- src/app/client/src/app/modules/core/services/concept-picker/concept-picker.service.ts
+++ src/app/client/src/app/modules/core/services/concept-picker/concept-picker.service.ts
@@ -53,7 +53,8 @@ export class ConceptPickerService {
     };
     this.searchService.compositeSearch(searchParams).subscribe(
       (apiResponse: ServerResponse) => {
-        if (apiResponse.result && _.isArray(apiResponse.result.concepts)) {
+        if (apiResponse.result && _.isArray(apiResponse.result.concepts) &&
+        apiResponse.result.concepts.length > 0) {
           _.forEach(apiResponse.result.concepts, (value) => {
             this._concepts.push(value);
           });
@@ -64,10 +65,12 @@ export class ConceptPickerService {
           } else {
             this.loadDomains();
           }
+        } else {
+          this._conceptData$.next({ err: 'no result' , data: null });
         }
       },
       err => {
-        this.toasterService.error(this.resourceService.messages.fmsg.m0015);
+        this._conceptData$.next({ err: 'no result' , data: null });
       }
     );
   }
