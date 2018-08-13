7457cedc1b085e16c02078b8a2d95cba6b351dc3
rjshrjndrn <rjshrjndrn@gmail.com>
2018-08-13 16:10:49 +0530

    Issue #SB-6252 feat: reverting index.ejs renaming

--- vcs-config.sh
+++ vcs-config.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Renaming index.ejs to index.<release-version>.<commit_hash>.ejs
-echo 'Renaming index.ejs to index.<release-version>.<commit_hash>.ejs'
-release=$(awk '/version/{print $2}' app_dist/package.json | cut -d'"' -f 2)
-mv app_dist/dist/index.ejs app_dist/dist/index.${release}.${commit_hash}.ejs
-
 # Appending commit hash in package.json
 echo 'adding commit hash to package.json'
 sed -i "/version/a\  \"commitHash\": \"${commit_hash}\"," app_dist/package.json
 
 # Creating assets tar
 echo 'Compressing assets directory'
-tar -cvf player-dist_${commit_hash}.tar.gz app_dist
+tar -cvf player-dist.tar.gz app_dist
