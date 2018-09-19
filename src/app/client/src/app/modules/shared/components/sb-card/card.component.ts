86744a6cf9f22b987fd3a211d3ecb0e363ceb2ea
ttplsudhir <sudhir_s@tekditechnologies.com>
2018-09-19 18:43:37 +0530

    Issue #SB-6018 feat: UX improvements to the card design

---
+++ src/app/client/src/app/modules/shared/components/sb-card/card.component.ts
@@ -0,0 +1,21 @@
+import { ResourceService } from '../../services/index';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { ICard } from '../../interfaces';
+import { IImpressionEventInput, IInteractEventObject } from '@sunbird/telemetry';
+@Component({
+  selector: 'sb-app-card',
+  templateUrl: './sb-card.component.html',
+  styleUrls: ['./sb-card.component.css']
+})
+export class SbCardComponent {
+  /**
+* content is used to render IContents value on the view
+*/
+  @Input() data: ICard;
+  @Input() customClass: string;
+  @Output() clickEvent = new EventEmitter<any>();
+
+  public onAction(data, action) {
+    this.clickEvent.emit({ 'action': action, 'data': data });
+  }
+}
---
+++ src/app/client/src/app/modules/shared/components/sb-card/sb-card.component.spec.ts
@@ -0,0 +1,55 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ResourceService, ConfigService } from '../../services';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { Response } from './sb-card.component.spec.data';
+import { SbCardComponent } from './sb-card.component';
+import { CdnprefixPipe } from '../../pipes/cdnprefix.pipe';
+describe('SbCardComponent', () => {
+  let component: SbCardComponent;
+  let fixture: ComponentFixture<SbCardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [ SbCardComponent, CdnprefixPipe ],
+      providers: [ResourceService, ConfigService],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SbCardComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should show TEST INPUT for all data', () => {
+    const cdnprefixPipe = new CdnprefixPipe();
+    component.data = Response.cardData;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('div .sb-card-component-description').innerText).toContain('Untitled Collection');
+    expect(fixture.nativeElement.querySelector('div .text-cencapitalize').innerText).toContain('B1 Test');
+    expect(fixture.nativeElement.querySelector('div .right').innerText).toEqual('TextBook');
+  });
+  it('should emit the event on click of trash icon', () => {
+    component.data = Response.cardData;
+    fixture.detectChanges();
+    const trashIconElm = fixture.debugElement.nativeElement.querySelector('i');
+    spyOn(component, 'onAction').and.callThrough();
+    spyOn(component.clickEvent, 'emit');
+    trashIconElm.dispatchEvent(new Event('click'));
+    expect(component.onAction).toHaveBeenCalledWith(Response.cardData, Response.cardData.action.right);
+    expect(component.clickEvent.emit).toHaveBeenCalledWith(Response.emitData);
+  });
+  it('should show badgeClassImage while passing badgesData', () => {
+    const cdnprefixPipe = new CdnprefixPipe();
+    component.data = Response.librarySearchData;
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('div .sb-card-component-description').innerText).toContain('');
+    expect(fixture.nativeElement.querySelector('div .text-cencapitalize').innerText).toContain('Official Textbook');
+    const badgesElm = fixture.nativeElement.querySelector('div .avatar');
+    expect(badgesElm.src).toContain(Response.librarySearchData.ribbon.left.image);
+  });
+});
