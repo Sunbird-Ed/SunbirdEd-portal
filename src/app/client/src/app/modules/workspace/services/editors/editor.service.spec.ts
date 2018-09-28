8a32673cfcbaf6c036c353690edb6ff619750cd2
Sourav Dey <sourav_d@techjoomla.com>
2018-09-28 17:11:42 +0530

    Issue #7209 fix: Portal provide configurable to allow attribution to organisation in editor

--- src/app/client/src/app/modules/workspace/services/editors/editor.service.spec.ts
+++ src/app/client/src/app/modules/workspace/services/editors/editor.service.spec.ts
@@ -5,12 +5,17 @@ import { ConfigService } from '@sunbird/shared';
 import { ContentService } from '@sunbird/core';
 import { mockRes } from './editor.service.spec.data';
 import { WorkSpaceService } from './../work-space/workspace.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '@sunbird/shared';
+import { CoreModule } from '@sunbird/core';
+import { CacheService } from 'ng2-cache-service';
 
 describe('EditorService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [EditorService, ConfigService, ContentService, WorkSpaceService]
+      imports: [HttpClientTestingModule, RouterTestingModule,
+        CoreModule.forRoot(), SharedModule.forRoot()],
+      providers: [EditorService, ConfigService, ContentService, WorkSpaceService, CacheService]
     });
   });
 
