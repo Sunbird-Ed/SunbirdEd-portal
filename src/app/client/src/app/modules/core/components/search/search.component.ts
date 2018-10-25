9f37d5c043423335458ca3a6b238a0e527ee64cd
raghu <http://localhost:9000>
2018-10-25 17:21:08 +0530

    Issue #SB-7950 feat: User and org search disable coding done

--- src/app/client/src/app/modules/core/components/search/search.component.ts
+++ src/app/client/src/app/modules/core/components/search/search.component.ts
@@ -118,52 +118,57 @@ export class SearchComponent implements OnInit {
   setFilters() {
     this.search = this.config.dropDownConfig.FILTER.SEARCH.search;
     this.searchUrl = this.config.dropDownConfig.FILTER.SEARCH.searchUrl;
-    this.activatedRoute.queryParams.subscribe(queryParams => {
-      this.queryParam = { ...queryParams };
-      this.key = this.queryParam['key'];
-    });
-    this.route.events.pipe(
-      filter(e => e instanceof NavigationEnd)).subscribe((params: any) => {
-        this.userService.userData$.subscribe(userdata => {
-          if (userdata && !userdata.err) {
-            this.userProfile = userdata.userProfile;
-            const currUrl = this.route.url.split('?');
-            this.value = currUrl[0].split('/', 3);
-            const searchEnabledStates = this.config.dropDownConfig.FILTER.SEARCH.searchEnabled;
-            if (this.searchUrl[this.value[1]] && searchEnabledStates.includes(this.value[1])) {
-              if (this.searchUrl[this.value[1]] === 'Users') {
-                if (!this.userProfile.rootOrgAdmin) {
-                  this.selectedOption = 'All';
-                } else {
-                  if (this.searchDropdownValues.indexOf('Users') === -1) {
-                    this.searchDropdownValues.push('Users');
-                  }
-                  this.selectedOption = this.searchUrl[this.value[1]];
-                  this.showSuiSelectDropdown = false;
-                  this.cdr.detectChanges();
-                  this.showSuiSelectDropdown = true;
-                }
-              } else {
-                this.selectedOption = this.searchUrl[this.value[1]];
-              }
-              this.showInput = true;
-            } else if (this.value[1] === 'search' && searchEnabledStates.includes(this.value[1])) {
-              this.selectedOption = this.value[2];
-              this.showInput = true;
-            } else {
-              this.selectedOption = 'All';
-              this.showInput = false;
-            }
-          }
-        });
-      });
+    const currUrl = this.route.url.split('?');
+    this.value = currUrl[0].split('/', 3);
+    const searchEnabledStates = this.config.dropDownConfig.FILTER.SEARCH.searchEnabled;
+    if (this.searchUrl[this.value[1]] && searchEnabledStates.includes(this.value[1])) {
+      this.setDropdownSelectedOption(this.searchUrl[this.value[1]]);
+    } else if (this.value[1] === 'search' && searchEnabledStates.includes(this.value[1])) {
+      this.setDropdownSelectedOption(this.value[2]);
+    } else {
+      this.selectedOption = 'All';
+      this.showInput = false;
+    }
   }
+
+  setDropdownSelectedOption (value) {
+    if ( value === 'Users' ) {
+      if ( !this.userProfile.rootOrgAdmin ) {
+        this.selectedOption = 'All';
+      } else {
+        this.selectedOption = value;
+        this.showSuiSelectDropdown = false;
+        this.cdr.detectChanges();
+        this.showSuiSelectDropdown = true;
+      }
+    } else {
+      this.selectedOption = value;
+    }
+    this.showInput = true;
+  }
+
   /**
    * gets the current url,
    * and queryParams
    */
   ngOnInit() {
-    this.setFilters();
+    this.activatedRoute.queryParams.subscribe(queryParams => {
+      this.queryParam = { ...queryParams };
+      this.key = this.queryParam['key'];
+    });
+    this.userService.userData$.subscribe(userdata => {
+      if (userdata && !userdata.err) {
+        this.userProfile = userdata.userProfile;
+        if (this.userProfile.rootOrgAdmin) {
+            this.searchDropdownValues.push('Users');
+        }
+      }
+      this.setFilters();
+      this.route.events.pipe(
+        filter(e => e instanceof NavigationEnd)).subscribe((params: any) => {
+          this.setFilters();
+        });
+    });
     this.showSuiSelectDropdown = true;
     this.userSearchRoles = this.config.rolesConfig.ROLES.userSearch;
   }
