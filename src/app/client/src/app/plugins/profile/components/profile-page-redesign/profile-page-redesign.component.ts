4b3938e8456496c58df60457f3ef61c1f41a8112
prajwal88 <prajwal.hj@compassitesinc.com>
2018-10-29 18:43:39 +0530

    Issue #8139 feat: create the profile page components

---
+++ src/app/client/src/app/plugins/profile/components/profile-page-redesign/profile-page-redesign.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-page-redesign',
+  templateUrl: './profile-page-redesign.component.html',
+  styleUrls: ['./profile-page-redesign.component.css']
+})
+export class ProfilePageRedesignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
