f2c4413cee06e0f19890651412c5d6993b14abce
Archana Kumari <archanak@ilimi.in>
2018-11-14 13:17:07 +0530

    Issue #SB-8766 feat:cleaning up the code and updating css

--- src/app/client/src/app/plugins/profile/profile.module.ts
+++ src/app/client/src/app/plugins/profile/profile.module.ts
@@ -6,18 +6,13 @@ import { SharedModule } from '@sunbird/shared';
 import { SlickModule } from 'ngx-slick';
 import { ProfileRoutingModule } from './profile-routing.module';
 import {
-  ProfilePageComponent, ProfileHeaderComponent, EditExperienceComponent, UserAddressComponent,
-  UserSummaryComponent, ProfileVisibilityComponent, UserAdditionalInfoComponent, UserExperienceViewComponent,
-  UserSkillsComponent, EditUserAddressComponent, UserEducationComponent, EditUserEducationComponent, EditUserSkillsComponent,
-  EditUserAdditionalInfoComponent, ProfilePageRedesignComponent
+  ProfilePageComponent, ProfileBadgeComponent
 } from './components';
 import { SuiModule } from 'ng2-semantic-ui';
 import { CoreModule } from '@sunbird/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { OrgManagementModule } from '@sunbird/org-management';
 import { WebExtensionModule } from '@project-sunbird/web-extensions';
-import { ProfileBadgeComponent } from './components/profile-badge/profile-badge.component';
-import { MenuItemComponent } from './components/menu-item/menu-item.component';
 import { NgInviewModule } from 'angular-inport';
 import { TelemetryModule } from '@sunbird/telemetry';
 import { AvatarModule } from 'ngx-avatar';
@@ -38,17 +33,8 @@ import { AvatarModule } from 'ngx-avatar';
     AvatarModule,
     SharedFeatureModule
   ],
-  declarations: [ProfilePageComponent, ProfileHeaderComponent, UserSummaryComponent, ProfileVisibilityComponent,
-    UserExperienceViewComponent, EditExperienceComponent, UserAddressComponent, UserEducationComponent, UserSkillsComponent,
-    UserAdditionalInfoComponent,
-    EditUserAddressComponent,
-    EditUserEducationComponent,
-    EditUserSkillsComponent,
-    EditUserAdditionalInfoComponent,
-    ProfileBadgeComponent,
-    ProfilePageRedesignComponent,
-    MenuItemComponent],
+  declarations: [ProfilePageComponent, ProfileBadgeComponent],
   providers: [ProfileService],
-  entryComponents: [ProfileBadgeComponent, MenuItemComponent]
+  entryComponents: [ProfileBadgeComponent]
 })
 export class ProfileModule { }
