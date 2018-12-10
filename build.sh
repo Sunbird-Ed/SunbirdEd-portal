5d0a070af53bd8d9bf30b1cb976b92c5f7ee50db
Harishkumar Gangula <harishg@ilimi.in>
2018-12-10 14:34:21 +0530

    Issue #000 fix: updated build and jenkinsfile

--- build.sh
+++ build.sh
@@ -1,15 +1,10 @@
-#!/bin/sh
-# Build script
-# set -o errexit
-e () {
-    echo $( echo ${1} | jq ".${2}" | sed 's/\"//g')
-}
-m=$(./src/app/metadata.sh)
-commit_hash=$1
+#!/bin/bash
+set -euo pipefail
 
-org=$(e "${m}" "org")
-name=$(e "${m}" "name")
-version=$(e "${m}" "version")
+commit_hash=$1
+org=sunbird
+name=player
+version=$2
 
 docker build -f ./Dockerfile.Build --build-arg commit_hash=${commit_hash} -t ${org}/${name}:${version}-build . 
 docker run --name=${name}-${version}-build ${org}/${name}:${version}-build
@@ -17,4 +12,5 @@ containerid=$(docker ps -aqf "name=${name}-${version}-build")
 rm -rf ./dist
 docker cp $containerid:/opt/player/app/player-dist.tar.gz .
 docker rm ${containerid}
-docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${version}-bronze .
+docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${version}_${commit_hash} .
+echo ${org}/${name}:${version}_${commit_hash} > metadata.json
