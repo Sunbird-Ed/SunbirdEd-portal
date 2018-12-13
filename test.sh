536bc1f6ef9cd7a68e9ef28db36dd6696af81de0
Rajesh Rajendran <rjshrjndrn@users.noreply.github.com>
2018-12-13 17:31:00 +0530

    Installing git for npm install

--- test.sh
+++ test.sh
@@ -5,7 +5,9 @@ apt-get update
 apt-get -y install libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 \
 xvfb gtk2-engines-pixbuf \
 xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable \
-google-chrome-stable
+google-chrome-stable \
+git
+
 cd src/app/client/
 npm install @angular/cli
 npm install
