ed97c4896ff221373e32bc097428f18e8a23d10f
Rajesh Rajendran <rjshrjndrn@users.noreply.github.com>
2018-12-13 17:22:02 +0530

    Creating test script

--- test.sh
+++ test.sh
@@ -2,15 +2,14 @@
 echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 apt-get update
-apt-get -y install libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4
-apt-get -y install xvfb gtk2-engines-pixbuf
-apt-get -y install xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable
-apt-get -y install google-chrome-stable
+apt-get -y install libxpm4 libxrender1 libgtk2.0-0 libnss3 libgconf-2-4 \
+xvfb gtk2-engines-pixbuf \
+xfonts-cyrillic xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable \
+google-chrome-stable
 
 npm install @angular/cli
 cd src/app/client/
 npm install
 ng lint
 ng build --prod
-#export CHROME_BIN=/usr/bin/chromium-browser
 npm run test-coverage
