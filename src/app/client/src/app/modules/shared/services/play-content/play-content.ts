466db651022806fa3a94f172a6ce74799f282de0
bhabaranjan19966 <bhabaranjanpanigrahi@gmail.com>
2018-12-13 10:13:39 +0530

    changes in course-player component

--- src/app/client/src/app/modules/shared/services/play-content/play-content.ts
+++ src/app/client/src/app/modules/shared/services/play-content/play-content.ts
@@ -4,7 +4,4 @@ import { Subject } from 'rxjs';
 export class PlayContent {
         public subject = new Subject<{ id: string, title: string }>();
         public CourseProgressListner = new Subject<any>();
-        public EnrolledCourseDetailsRendered: boolean = false ;
-
-
 }
