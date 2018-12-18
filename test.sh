a5d58168e43b29824311199acc0dc037d32e3e8c
rjshrjndrn <rjshrjndrn@gmail.com>
2018-12-18 14:09:58 +0530

    Issue #000 feat: Archiving build.log

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 cd src/app/client/
-npm install @angular/cli > /tmp/build.log
-npm install > /tmp/build.log
-ng lint > /tmp/build.log
-ng build --prod > /tmp/build.log
-npm run test-coverage | tee /tmp/test.log
+mkdir -p /tmp/logs
+npm install @angular/cli > /tmp/logs/build.log
+npm install > /tmp/logs/build.log
+npm install ng > /tmp/logs/build.log
+ng lint > /tmp/logs/build.log
+ng build --prod > /tmp/logs/build.log
+npm run test-coverage | tee /tmp/logs/test_cases.log
