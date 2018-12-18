b378a613600f0da802a897b5a33405175f5fd64b
rjshrjndrn <rjshrjndrn@gmail.com>
2018-12-18 13:59:36 +0530

    Issue #000 feat: Archiving artifacts

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd src/app/client/
-npm install @angular/cli
-npm install
-ng lint
-ng build --prod
-npm run test-coverage | tee player.log
+npm install @angular/cli > /tmp/build.log
+npm install > /tmp/build.log
+ng lint > /tmp/build.log
+ng build --prod > /tmp/build.log
+npm run test-coverage | tee /tmp/test.log
