979e3dc3122f021b5717e06f97cc50dcc69a5c8a
rjshrjndrn <rjshrjndrn@gmail.com>
2018-12-18 14:57:21 +0530

    Issue #000 feat: debugging ng

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-cd src/app/client/
+whoami
 mkdir -p /tmp/logs
+cd src/app/client/
 npm install @angular/cli > /tmp/logs/build.log
-npm install > /tmp/logs/build.log
-npm install ng > /tmp/logs/build.log
-ng lint > /tmp/logs/build.log
-ng build --prod > /tmp/logs/build.log
+ng
+npm install >> /tmp/logs/build.log
+ng lint >> /tmp/logs/build.log
+ng build --prod >> /tmp/logs/build.log
 npm run test-coverage | tee /tmp/logs/test_cases.log
