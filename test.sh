e8d40cc9c175a9a1381782d3bbd699b66269035a
rjshrjndrn <rjshrjndrn@gmail.com>
2018-12-18 15:36:57 +0530

    Issue #000 feat: creating function for build

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-whoami
 mkdir -p /tmp/logs
 cd src/app/client/
-npm install -g @angular/cli > /tmp/logs/build.log
-npm install >> /tmp/logs/build.log
-ng lint >> /tmp/logs/build.log
-ng build --prod >> /tmp/logs/build.log
+
+function build {
+npm install -g @angular/cli
+npm install
+ng lint
+ng build --prod
+}
+
+build &> /tmp/logs/build.sh
 npm run test-coverage | tee /tmp/logs/test_cases.log
