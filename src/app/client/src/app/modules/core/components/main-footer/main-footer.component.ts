4f3a08f1d7d3a850b59ebc443e56b3e71d377917
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2018-12-25 00:49:49 +0530

    Issue #000 fix: Footer UI

--- src/app/client/src/app/modules/core/components/main-footer/main-footer.component.ts
+++ src/app/client/src/app/modules/core/components/main-footer/main-footer.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './main-footer.component.html',
-  styleUrls: ['./main-footer.component.scss']
+  templateUrl: './main-footer.component.html'
 })
 export class MainFooterComponent implements OnInit {
+
+  date = new Date();
+  showFooter = true;
 
   constructor() { }
 
