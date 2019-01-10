2ee9bd7a498e8440c7f152b57679fdcb57a42ac9
Anoop HM <anoophm44@gmail.com>
2019-01-10 21:15:29 +0530

    Issue # SB-0000 fix: profile framework popup code refactor (#1307)
    * Issue # SB-0000 fix: tenant logo issue
    
    * Issue # SB-0000 fix: profile framework popup code refactor
    
    * Issue # SB-0000 fix: profile framework popup code refactor
    
    * Issue # SB-0000 fix: profile framework popup code refactor
    
    * Issue # SB-0000 fix: profile framework popup code refactor

--- src/app/client/src/app/modules/shared-feature/components/profile-framework-popup/profile-framework-popup.component.spec.ts
+++ src/app/client/src/app/modules/shared-feature/components/profile-framework-popup/profile-framework-popup.component.spec.ts
@@ -1,98 +1,126 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ProfileFrameworkPopupComponent } from './profile-framework-popup.component';
-import { SuiModule } from 'ng2-semantic-ui';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { FrameworkService, FormService, LearnerService, CoreModule, UserService, OrgDetailsService } from '@sunbird/core';
-import { ConfigService, ResourceService, Framework, ToasterService, SharedModule } from '@sunbird/shared';
-import { throwError as observableThrowError, of as observableOf, Observable } from 'rxjs';
+import {
+  FrameworkService, FormService, ChannelService, CoreModule, UserService, OrgDetailsService, PublicDataService } from '@sunbird/core';
+import { ConfigService, ResourceService, ToasterService, SharedModule } from '@sunbird/shared';
+import { throwError, of } from 'rxjs';
 import { Response } from './profile-framework-popup.component.spec.data';
+import { CacheService } from 'ng2-cache-service';
 
 describe('ProfileFrameworkPopupComponent', () => {
   let component: ProfileFrameworkPopupComponent;
   let fixture: ComponentFixture<ProfileFrameworkPopupComponent>;
+  let channelService, formService, cacheService, userService, publicDataService, orgDetailsService, toasterService, router;
+  let mockFormFields, mockCustodianOrg, mockFrameworkCategories, mockHashTagId, mockFrameworkId, mockCustOrgFrameWorks;
+  let makeChannelReadSuc, makeFrameworkReadSuc, makeFormReadSuc, makeCustOrgSuc, makeCustOrgFrameWorkSuc;
   const resourceBundle = {
     'messages': {
-      'emsg': {
-        'm0005': 'Fetching data failed, please try again later...',
-        'm0012': 'Something went wrong while saving your preferences. Please go to your profile to save your preferences'
-      }
+      'emsg': {'m0005': ''}
     }
   };
   class RouterStub {
     navigate = jasmine.createSpy('navigate');
   }
-  const fakeActivatedRoute = {
-    'params': observableOf({ }),
-    'queryParams': observableOf({ })
-  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, SharedModule.forRoot(), CoreModule.forRoot()],
       declarations: [ProfileFrameworkPopupComponent],
-      providers: [ConfigService, LearnerService, UserService, FrameworkService, FormService,
-        { provide: ResourceService, useValue: resourceBundle },
-        { provide: Router, useClass: RouterStub },
-        { provide: ActivatedRoute, useValue: fakeActivatedRoute }],
+      providers: [CacheService, { provide: ResourceService, useValue: resourceBundle },
+        { provide: Router, useClass: RouterStub }],
         schemas: [NO_ERRORS_SCHEMA]
-    })
-      .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileFrameworkPopupComponent);
     component = fixture.componentInstance;
+    channelService = TestBed.get(ChannelService);
+    formService = TestBed.get(FormService);
+    cacheService = TestBed.get(CacheService);
+    userService = TestBed.get(UserService);
+    publicDataService = TestBed.get(PublicDataService);
+    orgDetailsService = TestBed.get(OrgDetailsService);
+    toasterService = TestBed.get(ToasterService);
+    router = TestBed.get(Router);
+    makeChannelReadSuc = true, makeFrameworkReadSuc = true, makeFormReadSuc  = true, makeCustOrgSuc = true, makeCustOrgFrameWorkSuc = true;
+    mockFormFields = [], mockCustodianOrg = '', mockFrameworkCategories = [], mockHashTagId = '', mockFrameworkId = '',
+    mockCustOrgFrameWorks = [];
+    spyOn(publicDataService, 'get').and.callFake((options) => {
+      if (options.url === 'channel/v1/read/' + mockHashTagId && makeChannelReadSuc) {
+        return of({result: {channel: {defaultFramework: mockFrameworkId}}});
+      } else if (options.url === 'framework/v1/read/' + mockFrameworkId && makeFrameworkReadSuc) {
+        return of({result: {framework: {code: mockFrameworkId, categories: mockFrameworkCategories}}});
+      }
+      return throwError({});
+    });
+    spyOn(publicDataService, 'post').and.callFake((options) => {
+      if (makeFormReadSuc) {
+        return of({result: {form: {data: {fields: mockFormFields}}}});
+      }
+      return throwError({});
+    });
+    spyOn(orgDetailsService, 'getCustodianOrg').and.callFake((options) => {
+      if (makeCustOrgSuc) {
+        return of({result: {response: {value: mockCustodianOrg}}});
+      }
+      return throwError({});
+    });
+    spyOn(channelService, 'getFrameWork').and.callFake((options) => {
+      if (makeCustOrgFrameWorkSuc) {
+        return of({result: {channel: {frameworks: mockCustOrgFrameWorks}}});
+      }
+      return throwError({});
+    });
+    spyOn(toasterService, 'warning').and.callFake(() => {});
   });
-  it('Error message to be displayed when framework api returns error', () => {
-    const userService = TestBed.get(UserService);
-    const frameworkService = TestBed.get(FrameworkService);
-    const formService = TestBed.get(FormService);
-    const toasterService = TestBed.get(ToasterService);
-    const resourceService = TestBed.get(ResourceService);
-    const orgDetailsService = TestBed.get(OrgDetailsService);
-    const route = TestBed.get(Router);
-    resourceService.messages = resourceBundle.messages;
-    userService._userData$.next({ err: null, userProfile: Response.userData });
-    frameworkService._frameworkData$.next({ err: 'client error', framework: null, frameworkdata: null });
-    spyOn(orgDetailsService, 'getCustodianOrg').and.callFake(() => observableOf(Response.custodianOrg));
-    spyOn(formService, 'getFormConfig').and.callFake(() => observableOf(Response.formData.result.form.data.fields));
+  it('should throw warning and navigate to resource if not in edit mode and fetching custodian org details fails', () => {
+    makeCustOrgSuc = false;
     component.ngOnInit();
-    expect(component.isCustodianOrg).toBeFalsy();
+    expect(toasterService.warning).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(['/resources']);
   });
-
-  it('Error message to be displayed when form config service throws error', () => {
-    const route = TestBed.get(Router);
-    const userService = TestBed.get(UserService); const frameworkService = TestBed.get(FrameworkService);
-    const formService = TestBed.get(FormService);
-    const toasterService = TestBed.get(ToasterService);
-    const resourceService = TestBed.get(ResourceService);
-    const orgDetailsService = TestBed.get(OrgDetailsService);
-    resourceService.messages = resourceBundle.messages;
-    userService._userData$.next({ err: null, userProfile: Response.userData });
-    frameworkService._frameworkData$.next({ err: 'client error', framework: null, frameworkdata: null });
-    spyOn(orgDetailsService, 'getCustodianOrg').and.callFake(() => observableOf(Response.custodianOrg));
-    spyOn(formService, 'getFormConfig').and.callFake(() => observableThrowError({}));
+  it('should throw warning and should not navigate to resource if in edit mode and fetching custodian org details fails', () => {
+    makeCustOrgSuc = false;
+    component.formInput = {board: ['NCRT']};
     component.ngOnInit();
-    expect(route.navigate).toHaveBeenCalledWith(['/resources']);
+    expect(toasterService.warning).toHaveBeenCalled();
+    expect(router.navigate).not.toHaveBeenCalledWith(['/resources']);
   });
-
-  it('Form to successfully set input framework data sent', () => {
-    const userService = TestBed.get(UserService);
-    const frameworkService = TestBed.get(FrameworkService);
-    const formService = TestBed.get(FormService);
-    const orgDetailsService = TestBed.get(OrgDetailsService);
-    component.formInput = { board: ['NCERT'], medium: ['English'], gradeLevel: ['KG'] };
-    userService._userData$.next({ err: null, userProfile: Response.userData });
-    frameworkService._frameworkData$.next({ frameworkdata: Response.frameworkData });
-    spyOn(orgDetailsService, 'getCustodianOrg').and.callFake(() => observableOf(Response.custodianOrg));
-    spyOn(formService, 'getFormConfig').and.callFake(() => observableOf(Response.formData.result.form.data.fields));
+  it('should fetch default framework, then form and finally init dropDowns if user not belong to custodian org', () => {
+    userService._userProfile = { rootOrg: { rootOrgId: '321'} };
+    userService._hashTagId = '321';
+    mockHashTagId = '321';
+    mockCustodianOrg = '123';
+    mockFrameworkId = 'NCF';
+    mockFrameworkCategories = Response.categories1;
+    mockFormFields = Response.formFields1;
     component.ngOnInit();
-    expect(component.board).toBeDefined();
-    expect(component.medium).toBeDefined();
-    expect(component.gradeLevel).toBeDefined();
-    expect(component.subject).toBeDefined();
-    expect(component.showButton).toBeFalsy();
+    expect(component.formFieldOptions.length).toBeGreaterThan(1);
+    expect(component.formFieldOptions[0].range.length).toBeGreaterThan(1);
+    expect(component.formFieldOptions[1].range.length).toEqual(0);
+    expect(component.formFieldOptions[2].range.length).toEqual(0);
+    expect(component.formFieldOptions[3].range.length).toEqual(0);
+    expect(toasterService.warning).not.toHaveBeenCalled();
+  });
+  it('should fetch default framework, then form and finally init dropDowns if user not belong to custodian org', () => {
+    userService._userProfile = { rootOrg: { rootOrgId: '123'} }; // userProfile.rootOrg.rootOrgId
+    userService._hashTagId = '321';
+    mockHashTagId = '321';
+    mockCustodianOrg = '123';
+    mockFrameworkId = 'NCF';
+    mockFrameworkCategories = Response.categories1;
+    mockFormFields = Response.formFields1;
+    mockCustOrgFrameWorks = Response.custOrgFrameworks1;
+    component.ngOnInit();
+    expect(component.formFieldOptions.length).toBeGreaterThan(1);
+    expect(component.formFieldOptions[0].range.length).toBeGreaterThan(1);
+    expect(component.formFieldOptions[1].range).toBeUndefined();
+    expect(component.formFieldOptions[2].range).toBeUndefined();
+    expect(component.formFieldOptions[3].range).toBeUndefined();
+    expect(toasterService.warning).not.toHaveBeenCalled();
   });
 });
 
