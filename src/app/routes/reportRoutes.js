ffaa9be193051f1eb2ec1912fbef448ccedc592c
Sourav Dey <sourav_d@techjoomla.com>
2019-01-17 17:06:01 +0530

    Issue #SB-000 feat: Creating blob to stream

--- src/app/routes/reportRoutes.js
+++ src/app/routes/reportRoutes.js
@@ -1,8 +1,6 @@
 const proxyUtils = require('../proxy/proxyUtils.js')
 const reportHelper = require('../helpers/reportHelper.js')
 
-
-
 module.exports = function (app) {
     app.get('/reports/:slug/*',
         reportHelper.isValidSlug(),
