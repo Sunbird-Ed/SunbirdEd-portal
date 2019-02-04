92599e0ef87d80f2808273a5d7858653313cfa03
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-02-04 23:04:58 +0530

    Issue #SB-10380 task: workspace modals

--- src/app/client/src/app/modules/workspace/components/published/published.component.ts
+++ src/app/client/src/app/modules/workspace/components/published/published.component.ts
@@ -230,7 +230,8 @@ export class PublishedComponent extends WorkSpace implements OnInit {
   public deleteConfirmModal(contentIds) {
     const config = new TemplateModalConfig<{ data: string }, string, string>(this.modalTemplate);
     config.isClosable = true;
-    config.size = 'mini';
+    config.size = 'small';
+    config.transitionDuration = 0;
     this.modalService
       .open(config)
       .onApprove(result => {
