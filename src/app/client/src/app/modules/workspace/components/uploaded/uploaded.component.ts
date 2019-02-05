ac81ff4cb9d39755f047b973bc3352f19b7c5796
raghuv45 <raghavendrav444@gmail.com>
2019-02-05 07:22:59 +0530

    Issue #SB-1256 fix: Draft: Content on the second page is not displayed (#1389)
    * Issue #SB-1256 fix: Draft: Content on the second page is not displayed  issue
    
    * Issue #SB-1256 fix : test cases inprogress
    
    * Issue #SB-1256 fix: delete all contents fix from drafts and other pages

--- src/app/client/src/app/modules/workspace/components/uploaded/uploaded.component.ts
+++ src/app/client/src/app/modules/workspace/components/uploaded/uploaded.component.ts
@@ -254,6 +254,9 @@ export class UploadedComponent extends WorkSpace implements OnInit {
           (data: ServerResponse) => {
             this.showLoader = false;
             this.uploaded = this.removeContent(this.uploaded, contentIds);
+            if (this.uploaded.length === 0) {
+              this.fetchUploaded(this.config.appConfig.WORKSPACE.PAGE_LIMIT, this.pageNumber);
+            }
             this.toasterService.success(this.resourceService.messages.smsg.m0006);
           },
           (err: ServerResponse) => {
