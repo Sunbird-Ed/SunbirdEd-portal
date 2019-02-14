b75d39b7b289260a76e1dfba42d8cc5bbcacd878
raghu <http://localhost:9000>
2019-02-14 16:48:07 +0530

    Issue #SB-10557 fix: pending tranlations

--- src/app/client/src/app/modules/shared/components/app-loader/app-loader.component.ts
+++ src/app/client/src/app/modules/shared/components/app-loader/app-loader.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import {ILoaderMessage} from '../../interfaces';
 import { ResourceService } from '../../services/index';
+import * as _ from 'lodash';
 
 /**
  * loader component
@@ -20,8 +21,8 @@ export class AppLoaderComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.headerMessage = this.resourceService.messages.fmsg.m0087;
-    this.loaderMessage = this.resourceService.messages.fmsg.m0088;
+    this.headerMessage = _.get(this.resourceService.messages.fmsg, 'm0087');
+    this.loaderMessage = _.get(this.resourceService.messages.fmsg, 'm0088');
     if (this.data) {
       this.headerMessage = this.data.headerMessage || this.headerMessage;
       this.loaderMessage = this.data.loaderMessage || this.loaderMessage;
