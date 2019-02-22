74fa2758058f4a2e706a8dadd906ccdd7877b782
yravinderkumar33 <yravinderkumar33@gmail.com>
2019-02-22 18:40:27 +0530

    SB-10574 feat : Instrumentation for the SunBird Dashboards page

--- src/app/client/src/app/modules/dashboard/components/usage-reports/usage-reports.component.ts
+++ src/app/client/src/app/modules/dashboard/components/usage-reports/usage-reports.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { IInteractEventEdata, IInteractEventObject, TelemetryInteractDirective } from '@sunbird/telemetry';
+import { IImpressionEventInput } from './../../../telemetry/interfaces/telemetry';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UsageService } from './../../services';
 import * as _ from 'lodash';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -6,7 +8,6 @@ import { UserService } from '@sunbird/core';
 import { ToasterService, ResourceService, INoResultMessage } from '@sunbird/shared';
 import { UUID } from 'angular2-uuid';
 import { ActivatedRoute, Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-usage-reports',
@@ -24,13 +25,16 @@ export class UsageReportsComponent implements OnInit {
   noResult: boolean;
   noResultMessage: INoResultMessage;
   private activatedRoute: ActivatedRoute;
-
-
+  telemetryImpression: IImpressionEventInput;
+  telemetryInteractEdata: IInteractEventEdata;
+  telemetryInteractDownloadEdata: IInteractEventEdata;
+  @ViewChild(TelemetryInteractDirective) telemetryInteractDirective;
   constructor(private usageService: UsageService, private sanitizer: DomSanitizer,
     public userService: UserService, private toasterService: ToasterService,
-    public resourceService: ResourceService ,   activatedRoute: ActivatedRoute
-    ) {
-      this.activatedRoute =  activatedRoute;  }
+    public resourceService: ResourceService, activatedRoute: ActivatedRoute, private router: Router
+  ) {
+    this.activatedRoute = activatedRoute;
+  }
 
   ngOnInit() {
     const reportsLocation = (<HTMLInputElement>document.getElementById('reportsLocation')).value;
@@ -48,7 +52,61 @@ export class UsageReportsComponent implements OnInit {
       };
       this.noResult = true;
     });
+    this.setTelemetryImpression();
   }
+
+  setTelemetryInteractObject(val) {
+    return {
+      id: val,
+      type: 'view',
+      ver: '1.0'
+    };
+  }
+
+  reportType(reportType) {
+    this.telemetryInteractDirective.telemetryInteractObject = this.setTelemetryInteractObject(this.currentReport.id);
+    this.telemetryInteractDirective.telemetryInteractEdata = {
+      id: `report_${reportType}`,
+      type: 'click',
+      pageid: this.activatedRoute.snapshot.data.telemetry.pageid
+    };
+    this.telemetryInteractDirective.onClick();
+  }
+
+  setTelemetryImpression() {
+    this.telemetryInteractEdata = {
+      id: 'report-view',
+      type: 'click',
+      pageid: this.activatedRoute.snapshot.data.telemetry.pageid
+    };
+
+    this.telemetryInteractDownloadEdata = {
+      id: 'report-download',
+      type: 'click',
+      pageid: this.activatedRoute.snapshot.data.telemetry.pageid
+    };
+
+    this.telemetryImpression = {
+      context: {
+        env: this.activatedRoute.snapshot.data.telemetry.env,
+        cdata: [{
+          type: 'role',
+          id: _.get(this.userService, 'userProfile.userRoles').toString()
+        }]
+      },
+      object: {
+        id: this.userService.userid,
+        type: 'user',
+        ver: '1.0'
+      },
+      edata: {
+        type: this.activatedRoute.snapshot.data.telemetry.type,
+        pageid: this.activatedRoute.snapshot.data.telemetry.pageid,
+        uri: this.router.url,
+      }
+    };
+  }
+
   renderReport(report: any) {
     this.currentReport = report;
     this.isTableDataLoaded = false;
