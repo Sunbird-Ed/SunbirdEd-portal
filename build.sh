c1f797c2edd10c7a5ca613ba9eefed8df184d08c
G33tha <geetha.p@optit.co>
2019-02-25 13:07:46 +0530

    Issue #000 feat: updated jenkinsfile and build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,16 @@
-#!/bin/sh
-# Build script
-# set -o errexit
-e () {
-    echo $( echo ${1} | jq ".${2}" | sed 's/\"//g')
-}
-m=$(./src/app/metadata.sh)
-commit_hash=$1
+#!/bin/bash
+set -euo pipefail
 
-org=$(e "${m}" "org")
-name=$(e "${m}" "name")
-version=$(e "${m}" "version")
+build_tag=$1
+name=player
+node=$2
+org=$3
 
-docker build -f ./Dockerfile.Build --build-arg commit_hash=${commit_hash} -t ${org}/${name}:${version}-build . 
-docker run --name=${name}-${version}-build ${org}/${name}:${version}-build
-containerid=$(docker ps -aqf "name=${name}-${version}-build")
+docker build -f ./Dockerfile.Build --build-arg commit_hash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag}-build . 
+docker run --name=${name}-${build_tag}-build ${org}/${name}:${build_tag}-build
+containerid=$(docker ps -aqf "name=${name}-${build_tag}-build")
 rm -rf ./dist
 docker cp $containerid:/opt/player/app/player-dist.tar.gz .
 docker rm ${containerid}
-docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${version}-bronze .
+docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
+echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
