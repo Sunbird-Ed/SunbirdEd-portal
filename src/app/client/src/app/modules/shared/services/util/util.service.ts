e4185a26ce21a2c0b453d4c3daa9130b5217ee27
raghu <http://localhost:9000>
2019-02-25 11:57:43 +0530

    Issue #SB-10712 fix: tranlation issue in explore page

--- src/app/client/src/app/modules/shared/services/util/util.service.ts
+++ src/app/client/src/app/modules/shared/services/util/util.service.ts
@@ -119,6 +119,9 @@ export class UtilService {
     }
   }
   translateLabel(formFieldCategory, selectedLanguage) {
+    if (!formFieldCategory.translations) {
+      return formFieldCategory;
+    }
     const translation = JSON.parse(formFieldCategory.translations);
     if (translation && !translation.en) {
       translation.en = formFieldCategory.label;
