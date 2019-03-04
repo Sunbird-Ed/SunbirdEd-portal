d5d6e1ee3123b695cc1550099a9b351328294214
amantarento <amankumar.sharma@tarento.com>
2019-03-04 17:06:08 +0530

    Issue: fix the test case

--- src/app/client/src/app/modules/learn/components/batch/update-course-batch/update-course-batch.component.spec.ts
+++ src/app/client/src/app/modules/learn/components/batch/update-course-batch/update-course-batch.component.spec.ts
@@ -88,7 +88,7 @@ describe('UpdateCourseBatchComponent', () => {
     spyOn(courseConsumptionService, 'getCourseHierarchy').and.
     returnValue(observableOf({createdBy: 'b2479136-8608-41c0-b3b1-283f38c338ed'}));
     fixture.detectChanges();
-    expect(component.participantList.length).toBe(3);
+    expect(component.participantList.length).toBe(9);
     expect(component.mentorList.length).toBe(1);
     expect(component.courseCreator).toBeDefined();
     expect(component.batchUpdateForm).toBeDefined();
@@ -137,7 +137,7 @@ describe('UpdateCourseBatchComponent', () => {
     spyOn(courseConsumptionService, 'getCourseHierarchy').and.
     returnValue(observableOf({createdBy: 'b2479136-8608-41c0-b3b1-283f38c338ed'}));
     fixture.detectChanges();
-    expect(component.participantList.length).toBe(3);
+    expect(component.participantList.length).toBe(0);
     expect(component.mentorList.length).toBe(1);
     expect(component.mentorList[0].id).toBe('b2479136-8608-41c0-b3b1-283f38c338ed');
     expect(component.courseCreator).toBeDefined();
