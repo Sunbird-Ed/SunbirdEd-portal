3e65806f704712982c09a277c648d305d5bc6a67
ravinder kumar <yravinderkumar33@gmail.com>
2019-03-12 15:09:10 +0530

    Issue #SB-10889 fix:  When the REPORT_VIEWER role is removed by admin , the user is still displayed with dashboard option.

--- src/app/client/src/app/modules/core/services/user/user.service.ts
+++ src/app/client/src/app/modules/core/services/user/user.service.ts
@@ -187,10 +187,10 @@ export class UserService {
     hashTagIds.push(this._channel);
     let organisationIds = [];
     profileData.rootOrgAdmin = false;
-    let userRoles = profileData.roles;
+    let userRoles = [];
     if (profileData.organisations) {
       _.forEach(profileData.organisations, (org) => {
-        if (org.roles && _.isArray(org.roles)) {
+        if (org.organisationId === profileData.rootOrgId && _.isArray(org.roles)) {
           userRoles = _.union(userRoles, org.roles);
           if (org.organisationId === profileData.rootOrgId &&
             (_.indexOf(org.roles, 'ORG_ADMIN') > -1 ||
