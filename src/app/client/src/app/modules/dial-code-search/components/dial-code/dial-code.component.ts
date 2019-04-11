36c03e6147b0a4f75666c922816f385f98233bba
Sourav Dey <sourav_d@techjoomla.com>
2019-04-11 12:53:54 +0530

    Issue #SB-10073 feat: Bring in performance enhancements to the SunbirdEd /get pages

--- src/app/client/src/app/modules/dial-code-search/components/dial-code/dial-code.component.ts
+++ src/app/client/src/app/modules/dial-code-search/components/dial-code/dial-code.component.ts
@@ -1,11 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ResourceService, ServerResponse, ToasterService, ConfigService, UtilService } from '@sunbird/shared';
 import { Router, ActivatedRoute } from '@angular/router';
-import { SearchService, SearchParam } from '@sunbird/core';
+import { SearchService, SearchParam, PlayerService } from '@sunbird/core';
 import * as _ from 'lodash-es';
 import { IInteractEventObject, IInteractEventEdata, IImpressionEventInput } from '@sunbird/telemetry';
-import { takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { takeUntil, map, catchError, mergeMap } from 'rxjs/operators';
+import { Subject, forkJoin, of } from 'rxjs';
+import * as TreeModel from 'tree-model';
+
 @Component({
   selector: 'app-dial-code',
   templateUrl: './dial-code.component.html',
@@ -76,10 +79,12 @@ export class DialCodeComponent implements OnInit, OnDestroy {
   */
   public unsubscribe$ = new Subject<void>();
 
+  linkedContents: Array<any>;
+
 
   constructor(resourceService: ResourceService, router: Router, activatedRoute: ActivatedRoute,
     searchService: SearchService, toasterService: ToasterService, public configService: ConfigService,
-    public utilService: UtilService) {
+    public utilService: UtilService, public playerService: PlayerService, public http: HttpClient) {
     this.resourceService = resourceService;
     this.router = router;
     this.activatedRoute = activatedRoute;
@@ -125,22 +130,61 @@ export class DialCodeComponent implements OnInit, OnDestroy {
       }
     };
     this.searchService.contentSearch(requestParams, false).pipe(
-    takeUntil(this.unsubscribe$))
-    .subscribe(
-      (apiResponse: ServerResponse) => {
+      mergeMap(apiResponse => {
         if (apiResponse.result.content && apiResponse.result.content.length > 0) {
-          const constantData = this.configService.appConfig.GetPage.constantData;
-          const metaData = this.configService.appConfig.GetPage.metaData;
-          const dynamicFields = this.configService.appConfig.GetPage.dynamicFields;
-          this.searchResults = this.utilService.getDataForCard(apiResponse.result.content, constantData, dynamicFields, metaData);
+          const linkedCollectionsIds = [];
+          this.linkedContents = [];
+          _.forEach(apiResponse.result.content, (data) => {
+            if (data.mimeType === 'application/vnd.ekstep.content-collection') {
+              linkedCollectionsIds.push(data.identifier);
+            } else {
+              this.linkedContents.push(data);
+            }
+          });
+
+          if (linkedCollectionsIds.length) {
+            return this.getAllPlayableContent(linkedCollectionsIds);
+          } else {
+            return of([]);
+          }
         }
+      }))
+      .subscribe(data => {
+        const constantData = this.configService.appConfig.GetPage.constantData;
+        const metaData = this.configService.appConfig.GetPage.metaData;
+        const dynamicFields = this.configService.appConfig.GetPage.dynamicFields;
+        this.searchResults = this.utilService.getDataForCard(this.linkedContents, constantData, dynamicFields, metaData);
         this.showLoader = false;
-      },
-      err => {
+      }, error => {
         this.showLoader = false;
         this.toasterService.error(this.resourceService.messages.fmsg.m0049);
-      }
-    );
+      });
+  }
+
+  public getAllPlayableContent(collectionIds) {
+    const apiArray = [];
+    _.forEach(collectionIds, (data) => {
+      apiArray.push(this.getCollectionHierarchy(data));
+    });
+
+    return forkJoin(apiArray).pipe(map((results) => {
+      const model = new TreeModel();
+      _.forEach(results, (eachCollection) => {
+        if (typeof eachCollection === 'object') {
+          const treeModel = model.parse(eachCollection);
+          treeModel.walk((node) => {
+            if (_.get(node, 'model.mimeType') && node.model.mimeType !== 'application/vnd.ekstep.content-collection') {
+              this.linkedContents.push(node.model);
+            }
+            return true;
+          });
+        }
+      });
+    }));
+  }
+
+  public getCollectionHierarchy(collectionId) {
+    return this.playerService.getCollectionHierarchy(collectionId).pipe(map((res) => res.result.content), catchError(e => of(undefined)));
   }
 
   public navigateToSearch() {
@@ -151,9 +195,9 @@ export class DialCodeComponent implements OnInit, OnDestroy {
 
   public getEvent(event) {
     if (event.data.metaData.mimeType === this.configService.appConfig.PLAYER_CONFIG.MIME_TYPE.collection) {
-      this.router.navigate(['play/collection', event.data.metaData.identifier], { queryParams: { dialCode: this.searchKeyword} });
+      this.router.navigate(['play/collection', event.data.metaData.identifier], { queryParams: { dialCode: this.searchKeyword } });
     } else {
-      this.router.navigate(['play/content', event.data.metaData.identifier], { queryParams:  { dialCode: this.searchKeyword} });
+      this.router.navigate(['play/content', event.data.metaData.identifier], { queryParams: { dialCode: this.searchKeyword } });
     }
   }
   inview(event) {
