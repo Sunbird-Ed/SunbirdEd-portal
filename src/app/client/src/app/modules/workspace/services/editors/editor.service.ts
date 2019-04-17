a8f318a3fa7be792345c33f25858ee6db73b6df2
rajeevsathish <rajeev.sathish@tarento.com>
2019-04-17 20:15:58 +0530

    SB-0000 fix to Tagging Content with multiple subjects,mediums and grades

--- src/app/client/src/app/modules/workspace/services/editors/editor.service.ts
+++ src/app/client/src/app/modules/workspace/services/editors/editor.service.ts
@@ -44,6 +44,16 @@ export class EditorService {
      * @param req OBJECT
      */
     create(req): Observable<ServerResponse> {
+      if (!_.isArray(_.get(req, 'content.subject'))) {
+        const subject = [];
+        subject.push(req.content.subject);
+        req.content.subject = subject;
+      }
+      if (!_.isArray(_.get(req, 'content.medium'))) {
+        const medium = [];
+        medium.push(req.content.medium);
+        req.content.medium = medium;
+      }
         const option = {
             url: this.configService.urlConFig.URLS.CONTENT.CREATE,
             data: {
