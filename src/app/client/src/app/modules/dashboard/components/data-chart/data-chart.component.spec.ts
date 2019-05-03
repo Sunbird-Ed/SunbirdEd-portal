c6447cf318349c018a58a4ffa7ba8ec25a184f6e
ravinder kumar <yravinderkumar33@gmail.com>
2019-05-03 16:00:30 +0530

    Issue #SB-11709 fix : test case fix in data-charts

--- src/app/client/src/app/modules/dashboard/components/data-chart/data-chart.component.spec.ts
+++ src/app/client/src/app/modules/dashboard/components/data-chart/data-chart.component.spec.ts
@@ -1,6 +1,9 @@
+import { ChartsModule } from 'ng2-charts';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DataChartComponent } from './data-chart.component';
+import { SuiModule } from 'ng2-semantic-ui';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('DataChartComponent', () => {
   let component: DataChartComponent;
@@ -8,9 +11,10 @@ describe('DataChartComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DataChartComponent ]
+      declarations: [DataChartComponent],
+      imports: [ChartsModule, SuiModule , ReactiveFormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
