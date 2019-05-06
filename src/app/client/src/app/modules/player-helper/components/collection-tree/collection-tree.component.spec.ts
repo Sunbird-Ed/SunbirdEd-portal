f237cb085a8e44aed77b8d68735188fa05676786
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-05-06 18:25:46 +0530

    Issue #SB-11930 task: TOC UI

--- src/app/client/src/app/modules/player-helper/components/collection-tree/collection-tree.component.spec.ts
+++ src/app/client/src/app/modules/player-helper/components/collection-tree/collection-tree.component.spec.ts
@@ -44,19 +44,19 @@ describe('CollectionTreeComponent', () => {
     };
 
     component.options = {
-      folderIcon: 'sb-icon-folder',
-      fileIcon: 'sb-icon-content',
+      folderIcon: 'icon folder sb-fancyTree-icon',
+      fileIcon: 'sb-icon-content sb-fancyTree-icon',
       customFileIcon: {
-        'video': 'sb-icon-video',
-        'pdf': 'sb-icon-doc',
-        'youtube': 'sb-icon-video',
-        'H5P': 'sb-icon-content',
-        'audio': 'sb-icon-mp3',
-        'ECML': 'sb-icon-content',
-        'HTML': 'sb-icon-content',
-        'collection': 'sb-icon-collection',
-        'epub': 'sb-icon-doc',
-        'doc': 'sb-icon-doc'
+        'video': 'icon play circle sb-fancyTree-icon',
+        'pdf': 'sb-icon-doc sb-fancyTree-icon',
+        'youtube': 'icon play circle sb-fancyTree-icon',
+        'H5P': 'sb-icon-content sb-fancyTree-icon',
+        'audio': 'sb-icon-mp3 sb-fancyTree-icon',
+        'ECML': 'sb-icon-content sb-fancyTree-icon',
+        'HTML': 'sb-icon-content sb-fancyTree-icon',
+        'collection': 'icon folder sb-fancyTree-icon',
+        'epub': 'sb-icon-doc sb-fancyTree-icon',
+        'doc': 'sb-icon-doc sb-fancyTree-icon'
       }
     };
     fixture.detectChanges();
