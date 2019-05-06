a4e520b7ae3d36cb137040c4f05cd6a05cba1a80
Lakhan Mandloi <lakhan_m@tekditechnologies.com>
2019-05-06 19:17:18 +0530

    Issue #SB-11930 task: TOC UI

--- src/app/client/src/app/modules/resource/modules/player/components/collection-player/collection-player.component.spec.ts
+++ src/app/client/src/app/modules/resource/modules/player/components/collection-player/collection-player.component.spec.ts
@@ -75,7 +75,7 @@ describe('CollectionPlayerComponent', () => {
         'pdf': 'sb-icon-doc sb-fancyTree-icon',
         'youtube': 'icon play circle sb-fancyTree-icon',
         'H5P': 'sb-icon-content sb-fancyTree-icon',
-        'audio': 'sb-icon-mp3',
+        'audio': 'sb-icon-mp3 sb-fancyTree-icon',
         'ECML': 'sb-icon-content sb-fancyTree-icon',
         'HTML': 'sb-icon-content sb-fancyTree-icon',
         'collection': 'icon folder sb-fancyTree-icon',
