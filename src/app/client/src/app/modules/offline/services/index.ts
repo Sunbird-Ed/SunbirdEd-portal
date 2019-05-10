7b36f9d7656aad58265bd6d6f238387eadcd2311
Diptesh <diptesh1993@gmail.com>
2019-05-10 14:22:45 +0530

    Issue #SB-6391 feat : Import content funtionality

--- src/app/client/src/app/modules/offline/services/index.ts
+++ src/app/client/src/app/modules/offline/services/index.ts
@@ -1,1 +1,2 @@
-export * from './offline-file-uploader.service';
+export * from './offline-file-uploader-service/offline-file-uploader.service';
+export * from './connection-service/connection.service';
