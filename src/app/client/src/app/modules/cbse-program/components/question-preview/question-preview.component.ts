4756eb7b1f00454225bab3212b438e18bdbf8235
Kartheek Palla <kartheekp@ilimi.in>
2019-05-14 15:50:57 +0530

    Issue #SB- 11796 fix: CBSE program changes

--- src/app/client/src/app/modules/cbse-program/components/question-preview/question-preview.component.ts
+++ src/app/client/src/app/modules/cbse-program/components/question-preview/question-preview.component.ts
@@ -4,7 +4,7 @@ import { config } from 'rxjs';
 @Component({
   selector: 'app-question-preview',
   templateUrl: './question-preview.component.html',
-  styleUrls: ['./question-preview.component.css']
+  styleUrls: ['./question-preview.component.scss']
 })
 export class QuestionPreviewComponent implements OnInit {
 
