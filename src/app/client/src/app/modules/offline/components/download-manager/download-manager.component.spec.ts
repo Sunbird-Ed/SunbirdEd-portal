8703559951f4d94b9a4478872d3094300ced122b
Sourav Dey <sourav_d@techjoomla.com>
2019-05-22 19:30:17 +0530

    Issue #SB-6391 feat: Download manager

--- src/app/client/src/app/modules/offline/components/download-manager/download-manager.component.spec.ts
+++ src/app/client/src/app/modules/offline/components/download-manager/download-manager.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DownloadManagerComponent } from './download-manager.component';
 import { DownloadManagerService } from '../../services';
-import { SuiModalModule } from 'ng2-semantic-ui';
+import { SuiModalModule, SuiProgressModule, SuiAccordionModule } from 'ng2-semantic-ui';
 import { SharedModule } from '@sunbird/shared';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
@@ -12,7 +12,7 @@ describe('ContentImportComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ SuiModalModule, SharedModule.forRoot(), HttpClientTestingModule ],
+      imports: [ SuiModalModule, SharedModule.forRoot(), SuiProgressModule, SuiAccordionModule, HttpClientTestingModule ],
       declarations: [DownloadManagerComponent],
       providers: [ DownloadManagerService ]
     })
