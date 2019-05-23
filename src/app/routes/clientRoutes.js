9221ecbe6c54d0d12b2c31d5a778abee04058749
Anoop HM <anoophm44@gmail.com>
2019-05-23 21:11:01 +0530

    Issue #SB-0000 fix: invalid telemetry event for course page and removing home routes

--- src/app/routes/clientRoutes.js
+++ src/app/routes/clientRoutes.js
@@ -85,9 +85,7 @@ module.exports = (app, keycloak) => {
 
   app.all('/app', (req, res) => res.redirect(envHelper.ANDROID_APP_URL))
 
-  app.all([
-    // '/home', '/home/*',
-    '/announcement', '/announcement/*', '/search', '/search/*',
+  app.all(['/announcement', '/announcement/*', '/search', '/search/*',
     '/orgType', '/orgType/*', '/dashBoard', '/dashBoard/*',
     '/workspace', '/workspace/*', '/profile', '/profile/*', '/learn', '/learn/*', '/resources',
     '/resources/*', '/myActivity', '/myActivity/*'], keycloak.protect(), indexPage(true))
