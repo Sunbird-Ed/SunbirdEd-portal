620fd0ea081f2736ef48206fa0f877cc66d1fc88
Kartheek Palla <kartheekp@ilimi.in>
2019-05-23 15:28:07 +0530

    Issue #SB- 11796 fix: CBSE program changes

--- src/app/client/src/app/modules/cbse-program/class/McqForm.ts
+++ src/app/client/src/app/modules/cbse-program/class/McqForm.ts
@@ -6,7 +6,7 @@ export class McqForm {
 
   constructor(public question: string, public options: Array<McqOptions>,
     public templateId: string, public answer: string, public learningOutcome?,
-    public difficultyLevel?, public bloomsLevel?, public maxScore?) {
+    public bloomsLevel?, public maxScore?) {
     if (!options || !options.length) {
       this.options = [
         new McqOptions('', 0),
