9b00fd49e3568feeb0d41248ceaf21a544b8133f
Sourav Dey <sourav_d@techjoomla.com>
2019-05-24 19:13:17 +0530

    Issue #SB-10819 feat: Offline portal export, library search and fixes

--- src/app/client/src/app/modules/offline/components/download-manager/download-manager.component.ts
+++ src/app/client/src/app/modules/offline/components/download-manager/download-manager.component.ts
@@ -18,18 +18,33 @@ export class DownloadManagerComponent implements OnInit {
 
   isOpen = false;
 
+  count = 0;
+  localCount: 0;
+
   constructor(public downloadManagerService: DownloadManagerService,
     public resourceService: ResourceService, public toasterService: ToasterService,
     public connectionService: ConnectionService) { }
 
   ngOnInit() {
+    this.singleDownloadListCall();
     this.downloadManagerService.downloadEvent.subscribe((data) => {
-      this.isOpen = true;
-      this.getDownloadList();
+      this.singleDownloadListCall();
     });
   }
 
-  getDownloadList() {
+  singleDownloadListCall() {
+    const subscription = this.downloadManagerService.getDownloadList().subscribe(
+      (apiResponse: any) => {
+        this.downloadResponse = apiResponse.result.response.downloads;
+        this.localCount = apiResponse.result.response.downloads.inprogress.length + apiResponse.result.response.downloads.submitted.length;
+        if (this.localCount > 0) {
+          this.multipleDownloadListCall();
+        }
+      });
+  }
+
+  private multipleDownloadListCall() {
+    this.isOpen = true;
     const result = timer(1, 2000).pipe(
       switchMap(() => this.downloadManagerService.getDownloadList())
     );
@@ -37,14 +52,12 @@ export class DownloadManagerComponent implements OnInit {
     const subscription = result.subscribe(
       (apiResponse: any) => {
         this.downloadResponse = apiResponse.result.response.downloads;
-        if (_.isEmpty(apiResponse.result.response.downloads.inprogress) && _.isEmpty(apiResponse.result.response.downloads.submitted)) {
-          this.toasterService.success(this.resourceService.messages.smsg.m0051);
-          this.isOpen = false;
-          subscription.unsubscribe();
+        this.count = apiResponse.result.response.downloads.inprogress.length + apiResponse.result.response.downloads.submitted.length;
 
+        if (this.localCount > this.count) {
+          subscription.unsubscribe();
+          this.singleDownloadListCall();
         }
-      }, err => {
-        this.toasterService.error(this.resourceService.messages.fmsg.m0092);
       });
   }
 
