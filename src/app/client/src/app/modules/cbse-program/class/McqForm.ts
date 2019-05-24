2e65e453376b5f35dccfd3faa5cbd3540c93618a
Anoop HM <anoophm44@gmail.com>
2019-05-24 19:24:18 +0530

    Issue #SB-0000 fix: ui changes

--- src/app/client/src/app/modules/cbse-program/class/McqForm.ts
+++ src/app/client/src/app/modules/cbse-program/class/McqForm.ts
@@ -1,3 +1,5 @@
+import * as _ from 'lodash-es';
+
 export class McqOptions {
   constructor(public body: string, length: number) {
   }
@@ -8,6 +10,7 @@ export class McqForm {
     public templateId: string, public answer: string, public learningOutcome?,
     public bloomsLevel?, public maxScore?) {
     if (!options || !options.length) {
+      _.times(4, index => console.log(index));
       this.options = [
         new McqOptions('', 0),
         new McqOptions('', 0),
