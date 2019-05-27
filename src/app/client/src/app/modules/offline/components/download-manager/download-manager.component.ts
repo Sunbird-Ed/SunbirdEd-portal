2e04e24ac41bec0e8d59f84c279dbaa029e2fd5e
Sourav Dey <sourav_d@techjoomla.com>
2019-05-27 16:45:04 +0530

    Issue #SB-10819 feat: Offline portal download list fix

--- src/app/client/src/app/modules/offline/components/download-manager/download-manager.component.ts
+++ src/app/client/src/app/modules/offline/components/download-manager/download-manager.component.ts
@@ -1,11 +1,11 @@
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ResourceService, ToasterService } from '@sunbird/shared';
-import { timer } from 'rxjs';
+import { timer, combineLatest } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import * as _ from 'lodash-es';
 import { DownloadManagerService } from './../../services';
-import { ConnectionService } from './../../services';
+import { ConnectionService } from './../../services/connection-service/connection.service';
 
 @Component({
   selector: 'app-download-manager',
@@ -15,35 +15,51 @@ import { ConnectionService } from './../../services';
 export class DownloadManagerComponent implements OnInit {
 
   downloadResponse: any;
-
+  isConnected = navigator.onLine;
   isOpen = false;
-
   count = 0;
   localCount: 0;
+  isMultipleDownloadListCalled = false;
 
   constructor(public downloadManagerService: DownloadManagerService,
     public resourceService: ResourceService, public toasterService: ToasterService,
     public connectionService: ConnectionService) { }
 
   ngOnInit() {
-    this.singleDownloadListCall();
+    // Call download list initailly
+    this.getDownloadList();
+
+    // Subscribe connection service to check online/offline and call download list
+    this.connectionService.monitor().subscribe(isConnected => {
+      this.isConnected = isConnected;
+      this.getDownloadList();
+    });
+
+    // Call download list when clicked on add to library
     this.downloadManagerService.downloadEvent.subscribe((data) => {
-      this.singleDownloadListCall();
+      this.getDownloadList();
     });
   }
 
-  singleDownloadListCall() {
+  getDownloadList() {
     const subscription = this.downloadManagerService.getDownloadList().subscribe(
       (apiResponse: any) => {
         this.downloadResponse = apiResponse.result.response.downloads;
         this.localCount = apiResponse.result.response.downloads.inprogress.length + apiResponse.result.response.downloads.submitted.length;
-        if (this.localCount > 0) {
-          this.multipleDownloadListCall();
+
+        if (this.isMultipleDownloadListCalled && this.localCount === 0) {
+          this.toasterService.success(this.resourceService.messages.smsg.m0051);
+          this.isMultipleDownloadListCalled = false;
+        }
+
+        if (this.localCount > 0 && this.isConnected) {
+          this.getDownloadListUsingTimer();
         }
       });
   }
 
-  private multipleDownloadListCall() {
+  private getDownloadListUsingTimer() {
+    this.isMultipleDownloadListCalled = true;
     this.isOpen = true;
     const result = timer(1, 2000).pipe(
       switchMap(() => this.downloadManagerService.getDownloadList())
@@ -56,7 +72,7 @@ export class DownloadManagerComponent implements OnInit {
 
         if (this.localCount > this.count) {
           subscription.unsubscribe();
-          this.singleDownloadListCall();
+          this.getDownloadList();
         }
       });
   }
