470c6da4c93d10ea6c8dcf782f3178e687895a9f
Sourav Dey <sourav_d@techjoomla.com>
2019-06-07 16:16:42 +0530

    Issue #10822 feat: Adding telemetry

--- src/app/client/src/app/modules/core/directives/stickyheader.directive.ts
+++ src/app/client/src/app/modules/core/directives/stickyheader.directive.ts
@@ -45,6 +45,9 @@ export class StickyHeaderDirective implements AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit() {
+    if (this.isOffline) {
+      return false;
+    }
     this.zone.runOutsideAngular(() => {
     const scroll$ = fromEvent(window, 'scroll').pipe(
       throttleTime(10),
