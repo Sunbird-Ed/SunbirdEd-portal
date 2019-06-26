f7ace2a89682891bb657d4f159f42e64bc1c25b6
swetha-aggidevara <swetha@ilimi.in>
2019-06-26 16:37:26 +0530

    #SB-13363 feat: Added test cases for browse component, content-import-header, network status, watch-video and connection -service

--- src/app/client/src/app/modules/offline/components/watch-video/watch-video.component.spec.ts
+++ src/app/client/src/app/modules/offline/components/watch-video/watch-video.component.spec.ts
@@ -1,18 +1,40 @@
-import { ResourceService } from '@sunbird/shared';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CacheService } from 'ng2-cache-service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ResourceService, ConfigService, BrowserCacheTtlService } from '@sunbird/shared';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
- import { WatchVideoComponent } from './watch-video.component';
+import { WatchVideoComponent } from './watch-video.component';
 import { SuiModalModule } from 'ng2-semantic-ui';
+import { TelemetryModule } from '@sunbird/telemetry';
+import { Router, ActivatedRoute } from '@angular/router';
 
- xdescribe('WatchVideoComponent', () => {
+
+ describe('WatchVideoComponent', () => {
   let component: WatchVideoComponent;
   let fixture: ComponentFixture<WatchVideoComponent>;
-
+  let element: HTMLElement;
+  class RouterStub {
+    navigate = jasmine.createSpy('navigate');
+    url = jasmine.createSpy('url');
+  }
+  const resourceBundle = {
+    'frmelmnts' : {
+      'btn': {
+        'close': 'close'
+      }
+    }
+  };
+  class FakeActivatedRoute {
+  }
    beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [WatchVideoComponent],
-      imports: [SuiModalModule],
-      providers: [ResourceService]
+      imports: [SuiModalModule, TelemetryModule.forRoot(), RouterTestingModule],
+      providers: [{ provide: ResourceService, useValue: resourceBundle },
+        ConfigService, HttpClient, HttpHandler, CacheService, BrowserCacheTtlService,
+        { provide: Router, useClass: RouterStub }, { provide: ActivatedRoute, useClass: FakeActivatedRoute }],
+
     })
       .compileComponents();
   }));
@@ -21,9 +43,13 @@ import { SuiModalModule } from 'ng2-semantic-ui';
     fixture = TestBed.createComponent(WatchVideoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    spyOn(component, 'setInteractData');
+    component.setInteractData();
+    element = fixture.nativeElement;
   });
 
-   it('should create', () => {
+   it('should be able to close modal', () => {
     expect(component).toBeTruthy();
+    expect(component.setInteractData).toHaveBeenCalledTimes(1);
   });
 });
