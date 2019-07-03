c2d32ebfd45ccb0cba356e05c82182da1d74d623
ravinder kumar <yravinderkumar33@gmail.com>
2019-07-03 11:13:38 +0530

    Issue #SB-13051 fix : charts implementation

--- src/app/client/src/app/modules/dashboard/components/data-chart/data-chart.component.ts
+++ src/app/client/src/app/modules/dashboard/components/data-chart/data-chart.component.ts
@@ -1,199 +1,138 @@
-import { ResourceService } from '@sunbird/shared';
+import { ResourceService, ToasterService } from '@sunbird/shared';
 import { BaseChartDirective } from 'ng2-charts';
-import { Component, OnInit, Input, ViewChild, AfterViewInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ChangeDetectorRef } from '@angular/core';
 import * as _ from 'lodash-es';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { distinctUntilChanged, map, debounceTime } from 'rxjs/operators';
 import * as moment from 'moment';
-import { FormGroup, FormControl } from '@angular/forms';
-import { combineLatest, Subscription } from 'rxjs';
-import { startWith, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-data-chart',
   templateUrl: './data-chart.component.html',
   styleUrls: ['./data-chart.component.scss']
 })
-export class DataChartComponent implements OnInit, AfterViewInit, OnDestroy {
+export class DataChartComponent implements OnInit {
 
-  @Input() chartData: any;
-  chart;
-  chartFilters: FormGroup;
-  @ViewChild(BaseChartDirective) chartInfo: BaseChartDirective;
-  startDate;
-  endDate;
-  labelString;
-  showTimeLine = false;
-  selectedTime;
-  timeLineRange;
-  showFilters;
-  showLabelsFilter = false;
-  timeLineRangeoptions;
-  chartFiltersSubscription: Subscription;
-  filters;
-  subscription: Subscription;
-  showGraphStatistics = false;
-  avgStatistics;
-  constructor(public resourceService: ResourceService, private cdr: ChangeDetectorRef) { }
+  @Input() chartInfo: any;
+  // contains the chart configuration
+  chartConfig;
+  chartData;
+
+  chartType;
+  chartColors;
+  legend;
+  chartOptions;
+
+  datasets;
+  chartLabels = [];
+  filters: Array<{}>;
+  filtersFormGroup: FormGroup;
+  showFilters: Boolean = false;
+  filtersSubscription: Subscription;
+  noResultsFound: Boolean;
+
+  pickerMinDate: Date;
+  pickerMaxDate: Date;
+  selectedStartDate;
+  selectedEndDate;
+
+  @ViewChild(BaseChartDirective) chartDirective: BaseChartDirective;
+  constructor(public resourceService: ResourceService, private cdr: ChangeDetectorRef, private fb: FormBuilder, private toasterService: ToasterService) { }
 
   ngOnInit() {
-    this.chart = _.cloneDeep(this.chartData);
-    this.chartFilters = new FormGroup({
-      timeLineRange: new FormControl(''),
-      labels: new FormControl(''),
-      dataSet: new FormControl(''),
-      timeLine: new FormControl(''),
-    });
-    this.showFilters = _.get(this.chart, 'filters.display');
-    this.showGraphStatistics = _.get(this.chart, 'filters.showGraphStatistics');
-    this.filters = _.get(this.chart, 'filters.types');
-    this.labelString = _.get(this.chartData, 'options.scales.xAxes[0].scaleLabel.labelString') || 'Labels';
-    if (_.get(this.chartData, 'chartType') === 'horizontalBar') {
-      this.labelString = _.get(this.chartData, 'options.scales.yAxes[0].scaleLabel.labelString') || 'Labels';
-    }
-    const dateIndex = _.findIndex(this.filters, (filter) => /date/i.test(filter.name));
-    if (dateIndex !== -1) {
-      this.showTimeLine = true;
-      this.timeLineRangeoptions = this.filters[dateIndex].options.timeLineRange;
-      this.startDate = moment(this.chartData.labels[0], 'DD-MM-YYYY').toDate();
-      this.endDate = moment(this.chartData.labels[this.chartData.labels.length - 1], 'DD-MM-YYYY').toDate();
-    } else {
-      this.showLabelsFilter = true;
+    this.chartConfig = _.get(this.chartInfo, 'chartConfig');
+    this.chartData = _.get(this.chartInfo, 'chartData');
+    this.prepareChart();
+    if (this.filters) {
+      this.buildFiltersForm();
     }
   }
 
-  ngAfterViewInit() {
-    this.applyFilters();
+  selectStartDate(time) {
+    // let res = _.filter(this.chartData, data => {
+    //   if (moment(moment(data['Date'], 'DD-MM-YYYY').toDate()).isBefore(time)) {
+    //     return true; ``
+    //   }
+    //   return false;
+    // })
+    // this.getDataSetValue(res);
   }
 
-  applyFilters() {
-    this.subscription = combineLatest(this.onLabelsChange(), this.onTimeLineChange(), this.onDataSetChange(), this.onTimeLineRangeChange())
-      .subscribe(value => {
-        if (this.chartInfo) {
-          this.calculateUsage();
-          this.chartInfo.update();
-        }
-      });
+  selectEndDate(time) {
+    // this.selectedEndDate = moment(time, 'YYYY-MM-DD').format('DD-MM-YYYY');
+
+    // if (!this.selectedStartDate) {
+
+    // } else {
+    // }
   }
 
-  calculateUsage = () => {
-    const stats = [];
-    _.forEach(this.chartInfo.datasets, dataset => {
-      if (!dataset.hidden) {
-        const total = _.sumBy(dataset.data, value => Number(value));
-        const avgTotal = {};
-        avgTotal['label'] = dataset.label;
-        avgTotal['sum'] = total.toFixed(2);
-        stats.push(avgTotal);
+  buildFiltersForm() {
+    this.filtersFormGroup = this.fb.group({});
+    _.forEach(this.filters, filter => {
+      if (/date/i.test(_.get(filter, 'reference'))) {
+        this.pickerMinDate = moment(this.chartLabels[0], 'DD-MM-YYYY').toDate();
+        this.pickerMaxDate = moment(this.chartLabels[this.chartLabels.length - 1], 'DD-MM-YYYY').toDate();
       }
-    });
-    if (stats.length > 1) {
-      const summation = _.sumBy(stats, stat => Number(stat.sum));
-      stats.push({ label: 'Total', sum: summation.toFixed(2) });
-    }
-    this.avgStatistics = stats;
-    this.cdr.detectChanges();
-  }
-
-  onTimeLineChange = () => {
-    return this.chartFilters.get('timeLine').valueChanges
+      this.filtersFormGroup.addControl(_.get(filter, 'reference'), this.fb.control(''));
+      filter.options = _.uniq(_.map(this.chartData, filter.reference));
+    })
+    this.showFilters = true;
+    this.filtersSubscription = this.filtersFormGroup.valueChanges
       .pipe(
-        startWith(null),
-        tap((time) => {
-          if (!_.isNull(time)) {
-            this.selectedTime = time;
-            if (!this.timeLineRange) {
-              this.chartFilters.get('timeLineRange').setValue(this.timeLineRangeoptions[0].value);
-            }
-            const newDate = moment(time, 'YYYY-MM-DD').format('DD-MM-YYYY');
-            const endIndex = _.findIndex(this.chartData.labels, (date) => date === newDate) + 1;
-            const startIndex = (endIndex - this.timeLineRange < 0) ? 0 : endIndex - this.timeLineRange;
-            const labels = _.slice(this.chartData.labels, startIndex, endIndex);
-            const dataSets = [];
-            _.forEach(this.chartData.datasets, (dataset, i) => {
-              dataSets.push({
-                label: _.get(dataset, 'label'),
-                data: _.slice(dataset.data, startIndex, endIndex)
-              });
-            });
-            this.chart.labels = labels;
-            _.forEach(dataSets, (dataset, i) => {
-              this.chartInfo.datasets[i].data = dataSets[i].data;
-            });
-          }
-        })
-      );
-  }
-
-  onLabelsChange = () => {
-    return this.chartFilters.get('labels').valueChanges.pipe(
-      startWith(null),
-      tap(labels => {
-        if (!_.isNull(labels)) {
-          const indices = _.map(labels, label => {
-            return _.indexOf(this.chartData.labels, label);
-          });
-          const dataSets = [];
-          _.forEach(this.chartData.datasets, (dataset, i) => {
-            dataSets.push({
-              label: _.get(dataset, 'label'),
-              data: _.map(indices, index => dataset.data[index])
-            });
-          });
-          this.chart.labels = labels;
-          _.forEach(dataSets, (dataset, i) => {
-            this.chartInfo.datasets[i].data = dataSets[i].data;
-          });
+        map(filters => {
+          return _.omitBy(filters, _.isEmpty);
+        }),
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe((filters) => {
+        let res: Array<{}> = _.filter(this.chartData, data => {
+          return _.every(filters, (value, key) => {
+            return _.includes(value, data[key]);
+          })
+        });
+        this.noResultsFound = (res.length > 0) ? false : true;
+        if (this.noResultsFound) {
+          this.toasterService.error('No results found with applied filters');
         }
+        this.getDataSetValue(res);
+
+      }, (err) => {
+        console.log(err)
       })
-    );
   }
 
-  onDataSetChange = () => {
-    return this.chartFilters.get('dataSet').valueChanges
-      .pipe(
-        startWith(null),
-        tap(datasetsToShow => {
-          if (!_.isNull(datasetsToShow)) {
-            _.forEach(this.chartInfo.datasets, dataset => {
-              if (_.includes(datasetsToShow, dataset)) {
-                dataset.hidden = false;
-              } else {
-                dataset.hidden = true;
-              }
-            });
-          }
-        })
-      );
+  prepareChart() {
+    this.chartOptions = _.get(this.chartConfig, 'options') || { responsive: true };
+    this.chartColors = _.get(this.chartConfig, 'colors') || ['#024F9D'];
+    this.chartType = _.get(this.chartConfig, 'chartType') || 'line';
+    this.legend = (_.get(this.chartConfig, 'legend') === false) ? false : true;
+    this.filters = _.get(this.chartConfig, 'filters');
+    this.getDataSetValue();
   }
 
-  onTimeLineRangeChange = () => {
-    return this.chartFilters.get('timeLineRange').valueChanges
-      .pipe(
-        startWith(null),
-        tap(range => {
-          if (!_.isNull(range)) {
-            this.timeLineRange = range;
-            if (this.selectedTime) {
-              this.chartFilters.get('timeLine').setValue(this.selectedTime);
-            } else {
-              this.chartFilters.get('timeLine').setValue(this.endDate);
-            }
-          }
-        })
-      );
+  getDataSetValue(chartData = this.chartData) {
+    let groupedDataBasedOnLabels = _.groupBy(chartData, _.get(this.chartConfig, 'labelsExpr'));
+    this.chartLabels = _.uniq(_.map(chartData, _.get(this.chartConfig, 'labelsExpr')));
+    this.datasets = [];
+    _.forEach(this.chartConfig.datasets, dataset => {
+      this.datasets.push({
+        label: dataset.label,
+        data: _.get(dataset, 'data') || this.getData(groupedDataBasedOnLabels, dataset["dataExpr"]),
+        hidden: _.get(dataset, 'hidden') || false
+      });
+    });
+  }
+
+  getData(groupedDataBasedOnLabels, dataExpr) {
+    let data = _.mapValues(groupedDataBasedOnLabels, value => {
+      return _.sumBy(value, (o) => +o[dataExpr])
+    })
+    return _.values(data);
   }
 
   resetFilter() {
-    this.selectedTime = null;
-    this.timeLineRange = null;
-    this.chart.labels = this.chartData.labels;
-    _.forEach(this.chartData.datasets, (dataset, i) => {
-      this.chartInfo.datasets[i].hidden = false;
-      this.chartInfo.datasets[i].label = dataset.label;
-      this.chartInfo.datasets[i].data = dataset.data;
-    });
-    this.chartFilters.reset();
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.filtersFormGroup.reset();
   }
 }
