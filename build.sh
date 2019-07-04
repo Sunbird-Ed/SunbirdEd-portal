19675dbd0ba8398ee15c7c27d6501f7d38c0182a
keshavprasadms <msknext@gmail.com>
2019-07-04 18:41:27 +0530

    fix: temporarily exporting nvm paths

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 build_tag=$1
 name=player
 node=$2
