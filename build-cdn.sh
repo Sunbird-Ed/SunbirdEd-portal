78cd9821e0150acb0607326ea84f540cb91f6a34
keshavprasadms <msknext@gmail.com>
2019-07-08 17:59:59 +0530

    Fix: adding nvm use

--- build-cdn.sh
+++ build-cdn.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Build script
 # set -o errexit
-set -x
+set -euo pipefail
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm use 8
 #chown -R jenkins /var/lib/jenkins
 #su jenkins
 cd src/app/client
