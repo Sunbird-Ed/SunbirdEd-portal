22c4e6477d4465b31679c1e3a8ab848171d45002
Anoop HM <anoophm44@gmail.com>
2019-07-10 21:32:08 +0530

    issue 0000 fix: build issue debug

--- build-cdn.sh
+++ build-cdn.sh
@@ -7,6 +7,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+nvm install 8.11
 nvm use 8.11
 #chown -R jenkins /var/lib/jenkins
 #su jenkins
