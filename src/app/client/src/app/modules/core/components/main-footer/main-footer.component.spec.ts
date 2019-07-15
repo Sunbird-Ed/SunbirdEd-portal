d8ada8a8f3d17f784638b93667bd5e83f36c7fbb
ravinder kumar <yravinderkumar33@gmail.com>
2019-07-15 19:51:15 +0530

    Issue #SB-13084  fix : test case improvements

--- src/app/client/src/app/modules/core/components/main-footer/main-footer.component.spec.ts
+++ src/app/client/src/app/modules/core/components/main-footer/main-footer.component.spec.ts
@@ -58,8 +58,8 @@ describe('MainFooterComponent', () => {
         fixture.detectChanges();
         const spy = spyOn(component, 'redirect');
         component.redirectToDikshaApp();
-        expect(spy).toHaveBeenCalledWith('https://play.google.com/store/apps/details?id=in.gov.diksha.app&utm_source=\
-        diksha-sunbird&utm_medium=paytm&utm_campaign=dial&utm_term=EJ23P');
+        expect(spy).toHaveBeenCalledWith('https://play.google.com/store/apps/details?id=in.gov.diksha.app&utm_source='+
+        'diksha-sunbird&utm_medium=paytm&utm_campaign=dial&utm_term=EJ23P');
     });
 
     it('should redirect to diksha app with UTM params if dialcode is not avaiable', () => {
@@ -68,8 +68,8 @@ describe('MainFooterComponent', () => {
         fixture.detectChanges();
         const spy = spyOn(component, 'redirect');
         component.redirectToDikshaApp();
-        expect(spy).toHaveBeenCalledWith('https://play.google.com/store/apps/details?id=in.gov.diksha.app&utm_source=\
-        diksha-sunbird&utm_medium=get&utm_campaign=redirection');
+        expect(spy).toHaveBeenCalledWith('https://play.google.com/store/apps/details?id=in.gov.diksha.app&utm_source='+
+        'diksha-sunbird&utm_medium=get&utm_campaign=redirection');
     });
 
     it('should redirect to diksha app without UTM params if not avaiable', () => {
