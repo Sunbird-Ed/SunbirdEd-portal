9f1cba35b545ca5a5de8ba90017fa0aeb5939292
ravinder kumar <yravinderkumar33@gmail.com>
2019-07-15 16:25:16 +0530

    Issue #SB-13084  fix : test case improvements

--- src/app/client/src/app/modules/learn/components/batch/unenroll-batch/unenroll-batch.component.spec.ts
+++ src/app/client/src/app/modules/learn/components/batch/unenroll-batch/unenroll-batch.component.spec.ts
@@ -1,3 +1,4 @@
+import { CourseBatchService } from '../../../services';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { DashboardModule } from '../../../../dashboard/dashboard.module';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -6,21 +7,42 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { UnEnrollBatchComponent } from './unenroll-batch.component';
 import { SuiModule } from 'ng2-semantic-ui';
-import {SharedModule} from '@sunbird/shared';
-import {CoreModule} from '@sunbird/core';
+import { SharedModule } from '@sunbird/shared';
+import { CoreModule } from '@sunbird/core';
 import { TelemetryModule } from '@sunbird/telemetry';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import {fakeBatchDetails} from './unenroll-batch.component.spec.data';
 describe('UnEnrollBatchComponent', () => {
   let component: UnEnrollBatchComponent;
   let fixture: ComponentFixture<UnEnrollBatchComponent>;
-
+  const fakeActivatedRoute = {
+    'params': of({ 'batchId': '01271493706664345631' }),
+    snapshot: {
+      params: [
+        {
+          pageNumber: '1',
+        }
+      ],
+      data: {
+        telemetry: {
+          env: 'workspace', pageid: 'workspace-course-batch', subtype: 'scroll', type: 'list',
+          object: { type: 'batch', ver: '1.0' }
+        }
+      }
+    }
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [],
       schemas: [NO_ERRORS_SCHEMA],
       imports: [SharedModule.forRoot(), TelemetryModule.forRoot(), CoreModule, SuiModule, LearnModule, RouterTestingModule,
         DashboardModule, HttpClientTestingModule],
+      providers: [{
+        provide: ActivatedRoute, useValue: fakeActivatedRoute
+      }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -31,4 +53,25 @@ describe('UnEnrollBatchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch batch details with batch Id', () => {
+    let courseBatchService = TestBed.get(CourseBatchService);
+    let spy = spyOn(courseBatchService,'getEnrollToBatchDetails').and.returnValue(of(fakeBatchDetails));
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith('01271493706664345631');
+    expect(component.batchDetails).toEqual(fakeBatchDetails);
+    // expect(component.fetchParticipantsDetails).toHaveBeenCalled();
+  })
+
+  it('should redirect whenever batchType is open', () => {
+
+  })
+
+  it('should redirect when error occurs while fetching batch details', () => {
+
+  })
+
+  it('')
+
+
 });
