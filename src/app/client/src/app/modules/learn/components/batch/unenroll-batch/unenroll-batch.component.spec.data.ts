858012e47b6606fb57024549b7bc48adbb1c21d0
ravinder kumar <yravinderkumar33@gmail.com>
2019-07-17 17:15:15 +0530

    Issue #SB-13084  fix : test case improvements

---
+++ src/app/client/src/app/modules/learn/components/batch/unenroll-batch/unenroll-batch.component.spec.data.ts
@@ -0,0 +1,33 @@
+export let fakeOpenBatchDetails = {
+  'identifier': '01278646366204723214',
+  'createdFor': [
+    '0125046882232565760'
+  ],
+  'courseAdditionalInfo': {
+    'courseName': 'கிருபா கோர்ஸ் 4',
+    'leafNodesCount': '10',
+    'description': 'பய கிருபா',
+    'courseLogoUrl': 'ntp-content-staging/content/do_21278645271447142411200/artifact/images-2_1560846345072.thumb.jpg',
+    'tocUrl': 'ntp-content-staging/content/do_21278645271447142411200/artifact/do_21278645271447142411200_toc.json',
+    'status': 'Live'
+  },
+  'countIncrementDate': '2019-06-18 08:32:24:438+0000',
+  'description': 'கிருபா ',
+  'updatedDate': '2019-06-18 08:35:43:770+0000',
+  'completedCount': 0,
+  'countIncrementStatus': true,
+  'createdDate': '2019-06-18 08:32:24:300+0000',
+  'reportUpdatedOn': '2019-07-17T03:08:26Z',
+  'createdBy': 'd7c1a905-2268-4fdd-976d-28bdabfb7b0e',
+  'courseCreator': 'd7c1a905-2268-4fdd-976d-28bdabfb7b0e',
+  'hashTagId': '',
+  'participantCount': 11,
+  'mentors': [],
+  'countDecrementStatus': false,
+  'name': 'ஓபன் பேட்ச் ',
+  'id': '01278646366204723214',
+  'enrollmentType': 'open',
+  'courseId': 'do_21278645271447142411200',
+  'startDate': '2019-06-18',
+  'status': 1
+};
