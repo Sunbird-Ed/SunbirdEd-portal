b6467332ba6b0c69c043a85815e9b5d93371683a
Sourav Dey <sourav_d@techjoomla.com>
2019-07-30 15:31:24 +0530

    Issue #SB-14029 feat: Watch video changes

--- src/app/client/src/app/modules/offline/components/watch-video/watch-video.component.ts
+++ src/app/client/src/app/modules/offline/components/watch-video/watch-video.component.ts
@@ -1,8 +1,7 @@
-import { Router } from '@angular/router';
 import { ResourceService } from '@sunbird/shared';
-import { Component, OnInit, AfterViewInit, Output, ViewChild, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
 import { IInteractEventEdata, IInteractEventObject } from '@sunbird/telemetry';
-
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-watch-video',
@@ -13,33 +12,142 @@ export class WatchVideoComponent implements OnInit {
 
   @ViewChild('modal') modal;
   @Output() closeVideoModal = new EventEmitter<any>();
-  WatchModalCloseIntractEdata: IInteractEventEdata;
-  DownloadPdfInteractEdata: IInteractEventEdata;
-  CloaseVideoModal: IInteractEventEdata;
-  public telemetryInteractObject: IInteractEventObject;
-  constructor(public resourceService: ResourceService, public router: Router, ) { }
+  slideConfig: object;
+  slideData: object;
+  activeVideoObject: object;
+  watchModalCloseIntractEdata: IInteractEventEdata;
+  downloadPdfInteractEdata: IInteractEventEdata;
+  videoModalClose: IInteractEventEdata;
+  telemetryInteractObject: IInteractEventObject;
+  constructor(public resourceService: ResourceService) { }
 
   ngOnInit() {
-    this.setInteractData();
+    this.slideConfig = {
+      dots: false,
+      infinite: false,
+      speed: 300,
+      rtl: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 2800,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 2200,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 2000,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 1600,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 750,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 660,
+          settings: {
+            slidesToShow: 1.75,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 530,
+          settings: {
+            slidesToShow: 1.25,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 450,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+
+    this.slideData = [
+      {
+        id: 1,
+        name: this.resourceService.frmelmnts.instn.t0094,
+        thumbnail: 'assets/images/play-icon.svg',
+        url: 'assets/videos/1.mp4'
+      },
+      {
+        id: 2,
+        name: this.resourceService.frmelmnts.instn.t0095,
+        thumbnail: 'assets/images/play-icon.svg',
+        url: 'assets/videos/2.mp4'
+      },
+      {
+        id: 3,
+        name: this.resourceService.frmelmnts.instn.t0096,
+        thumbnail: 'assets/images/play-icon.svg',
+        url: 'assets/videos/3.mp4'
+      },
+      {
+        id: 4,
+        name: this.resourceService.frmelmnts.instn.t0097,
+        thumbnail: 'assets/images/play-icon.svg',
+        url: 'assets/videos/4.mp4'
+      }
+    ];
+
+    this.activeVideoObject = {
+      id: 1,
+      name: this.resourceService.frmelmnts.instn.t0094,
+      thumbnail: 'assets/images/play-icon.svg',
+      url: 'assets/videos/1.mp4'
+    };
   }
+
+  changeVideoAttributes(data: any) {
+    this.activeVideoObject = data;
+    $('#video').attr('src', data.url);
+  }
+
   closeModal() {
     this.closeVideoModal.emit('success');
   }
+
   modalClose() {
     this.modal.deny();
-  }
-
-
-  setInteractData() {
-    this.WatchModalCloseIntractEdata = {
-      id: 'watch-video-close-button',
-      type: 'click',
-      pageid: 'library'
-    };
-    this.DownloadPdfInteractEdata = {
-      id: 'download-pdf-button',
-      type: 'click',
-      pageid : 'library'
-    };
   }
 }
