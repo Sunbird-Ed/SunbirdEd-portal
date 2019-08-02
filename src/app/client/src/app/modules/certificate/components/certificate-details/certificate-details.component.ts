64cdace05657f7280eb206108f15b4e93bdf014a
sudip mukherjee <sudip.mukherjee@tarento.com>
2019-08-02 13:48:40 +0530

    Issue #SB-13854 feat: Validate certificate code post QR code scan implementation

--- src/app/client/src/app/modules/certificate/components/certificate-details/certificate-details.component.ts
+++ src/app/client/src/app/modules/certificate/components/certificate-details/certificate-details.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { CertificateService } from '@sunbird/core';
+import { ServerResponse, ResourceService } from '@sunbird/shared';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import * as _ from 'lodash-es';
+import * as moment from 'moment';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-certificate-details',
@@ -7,12 +13,69 @@ import { Component, OnInit } from '@angular/core';
 export class CertificateDetailsComponent implements OnInit {
   showSuccessModal: boolean;
   loader: boolean;
-  error: boolean;
   viewCertificate: boolean;
-  viewCertificateHtml: boolean;
+  error: boolean;
+  enableVerifyButton = false;
+  certificateCode: any;
+  wrongCertificateCode = false;
+  instance: string;
+  watchVideoLink: SafeResourceUrl;
 
-  constructor() { }
+  @ViewChild('codeInputField') codeInputField: ElementRef;
+
+  constructor(
+    public activatedRoute: ActivatedRoute,
+    public certificateService: CertificateService,
+    public resourceService: ResourceService,
+    public router: Router,
+    private sanitizer: DomSanitizer
+  ) { }
+
   ngOnInit() {
+    this.instance = _.upperCase(this.resourceService.instance);
+    this.watchVideoLink = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/2Xgff-gsf0A?rel=0&autoplay=1');
+  }
+
+  verify(event: any) {
+    this.loader = true;
+    const request = {
+      request: {
+        'certId': this.activatedRoute.snapshot.params.uuid,
+        'accessCode': _.trim(this.certificateCode),
+        'verifySignature': true,
+      }
+    };
+    this.certificateService.validateCertificate(request).subscribe(
+      (data: ServerResponse) => {
+        this.loader = false;
+       /**  manipulate response data here
+            watch video link needs to be secured by bypassSecurityTrustResourceUrl()
+        */
+      },
+      (err) => {
+        this.wrongCertificateCode = true;
+        this.loader = false;
+        this.codeInputField.nativeElement.value = '';
+        this.codeInputField.nativeElement.focus();
+        this.enableVerifyButton = false;
+      }
+    );
+  }
+  getCodeLength(event: any) {
+    this.wrongCertificateCode = false;
+    if (event.target.value.length === 6) {
+      this.enableVerifyButton = true;
+    } else {
+      this.enableVerifyButton = false;
+    }
+  }
+
+  navigateToWatchVideoModal() {
+    this.showSuccessModal = true;
+  }
+
+  navigateToCoursesPage() {
+    this.router.navigate(['/explore-course']);
   }
 
 }
