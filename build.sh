8d6a34337aed3709d5435fc319b7295a812dd0e6
keshavprasadms <msknext@gmail.com>
2019-08-06 15:02:53 +0530

    adding sc

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ org=$3
 commit_hash=$(git rev-parse --short HEAD)
 docker build --build-arg commit_hash=$(git rev-parse --short HEAD) --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\",\"commit_hash\" : \"${commit_hash}\", \"node_name\" : \"$node\"} > metadata.json
+
