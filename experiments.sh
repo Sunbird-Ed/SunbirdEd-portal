2a49fdf338deb5c400c459d96d570d82639ba75f
G33tha <geetha.p@optit.co>
2019-08-06 18:39:51 +0530

    Update experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Build script
-# set -o errexit
 set -euo pipefail
 
 export NVM_DIR="$HOME/.nvm"
@@ -9,8 +7,6 @@ export NVM_DIR="$HOME/.nvm"
 
 nvm install 8.11
 nvm use 8.11
-#chown -R jenkins /var/lib/jenkins
-#su jenkins
 cd src/app/client
 version=$(jq '.version' package.json | sed 's/\"//g')
 cdnUrl=$1
@@ -19,4 +15,3 @@ npm install
 npm run build-cdn -- --deployUrl $cdnUrl
 export sunbird_portal_cdn_url=$cdnUrl
 npm run inject-cdn-fallback
-#chown -R jenkins ./
