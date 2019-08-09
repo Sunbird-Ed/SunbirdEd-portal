d9f99c813ea4e7ea6a6aae0ecc57871aaa52c012
Sourav Dey <sourav_d@techjoomla.com>
2019-08-09 10:13:10 +0530

    Issue #SB-12220 feat: Changing video names and url for offline desktop app (#2447)
    * Issue #000 feat: Commenting help tab in offline desktop app
    
    * Issue #13760 fix: Removing organisation id from page assemblr api request for offline desktop app
    
    * Issue #SB-14029 feat: Watch video changes
    
    * Issue #SB-14029 feat: Watch video changes
    
    * Issue #SB-14029 feat: Watch video changes
    
    * Issue #SB-14029 feat: Watch video changes
    
    * Issue #SB-14029 feat: Watch video changes
    
    * Issue #SB-14029 feat: Watch video changes
    
    * Issue #SB-14029 feat: Watch video changes
    
    * Issue #SB-14029 feat: Watch video changes
    
    * Issue #SB-12220 feat: Changing video names and url for offline desktop app
    
    * Issue #SB-12220 feat: Changing video names and url for offline desktop app

--- src/app/client/src/app/modules/offline/components/watch-video/watch-video.component.ts
+++ src/app/client/src/app/modules/offline/components/watch-video/watch-video.component.ts
@@ -2,6 +2,7 @@ import { ResourceService, ConfigService } from '@sunbird/shared';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { IInteractEventEdata } from '@sunbird/telemetry';
 import * as $ from 'jquery';
+import * as _ from 'lodash-es';
 
 @Component({
   selector: 'app-watch-video',
@@ -14,46 +15,54 @@ export class WatchVideoComponent implements OnInit {
   slideConfig: object;
   slideData: object;
   activeVideoObject;
+  instance: string;
   selectVideoInteractEdata: IInteractEventEdata;
-  constructor(public resourceService: ResourceService, public configService: ConfigService) { }
+  constructor(public resourceService: ResourceService, public configService: ConfigService) {
+    this.instance = (<HTMLInputElement>document.getElementById('instance'))
+        ? (<HTMLInputElement>document.getElementById('instance')).value : 'sunbird';
+  }
 
   ngOnInit() {
     this.slideConfig = this.configService.offlineConfig.watchVideo;
     this.slideData = [
       {
-        id: 'download-content-video',
-        name: this.resourceService.frmelmnts.instn.t0094,
+        id: 'add-content-online',
+        name: this.interpolateInstance(this.resourceService.frmelmnts.instn.t0094),
         thumbnail: 'assets/images/play-icon.svg',
-        url: 'assets/videos/download-content.mp4'
+        url: 'assets/videos/How_do_I_add_content_to_the_desktop_app_when_I_am_connected_to_the_Internet.mp4'
       },
       {
-        id: 'copy-content-video',
-        name: this.resourceService.frmelmnts.instn.t0095,
+        id: 'add-content-offline',
+        name: this.interpolateInstance(this.resourceService.frmelmnts.instn.t0095),
         thumbnail: 'assets/images/play-icon.svg',
-        url: 'assets/videos/copy-content.mp4'
+        url: 'assets/videos/How_do_I_add_content_to_the_desktop_app_when_I_am_offline_or_using_a_pen_drive.mp4'
       },
       {
-        id: 'browse-content-video',
-        name: this.resourceService.frmelmnts.instn.t0096,
+        id: 'find-content-offline',
+        name: this.interpolateInstance(this.resourceService.frmelmnts.instn.t0096),
         thumbnail: 'assets/images/play-icon.svg',
-        url: 'assets/videos/browse-content.mp4'
+        url: 'assets/videos/How_and_where_can_I_find_content_in_My_Library.mp4'
       },
       {
-        id: 'play-content-video',
-        name: this.resourceService.frmelmnts.instn.t0097,
+        id: 'copy-content',
+        name: this.interpolateInstance(this.resourceService.frmelmnts.instn.t0097),
         thumbnail: 'assets/images/play-icon.svg',
-        url: 'assets/videos/play-content.mp4'
+        url: 'assets/videos/How_do_I_copy_content_to_my_pen_drive.mp4'
       }
     ];
 
     this.activeVideoObject = {
-      id: 'download-content-video',
-      name: this.resourceService.frmelmnts.instn.t0094,
+      id: 'add-content-online',
+      name: this.interpolateInstance(this.resourceService.frmelmnts.instn.t0094),
       thumbnail: 'assets/images/play-icon.svg',
-      url: 'assets/videos/download-content.mp4'
+      url: 'assets/videos/How_do_I_add_content_to_the_desktop_app_when_I_am_connected_to_the_Internet.mp4'
     };
 
     this.setInteractData();
+  }
+
+  interpolateInstance(message) {
+    return message.replace('{instance}', _.upperCase(this.instance));
   }
 
   changeVideoAttributes(data: any) {
