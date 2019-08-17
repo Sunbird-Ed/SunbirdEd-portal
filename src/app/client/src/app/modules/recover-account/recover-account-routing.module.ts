80855824b4f7e78d69b87ed945110cd12d5fa258
Anoop HM <anoophm44@gmail.com>
2019-08-18 00:43:05 +0530

    Issue #SB-13637 feat: removed reset password component

--- src/app/client/src/app/modules/recover-account/recover-account-routing.module.ts
+++ src/app/client/src/app/modules/recover-account/recover-account-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IdentifyAccountComponent, SelectAccountIdentifierComponent, VerifyAccountIdentifierComponent,
-  ResetPasswordComponent, RecoverAccountComponent } from './components';
+  RecoverAccountComponent } from './components';
 const telemetryEnv = 'AccountRecovery';
 
 const routes: Routes = [
