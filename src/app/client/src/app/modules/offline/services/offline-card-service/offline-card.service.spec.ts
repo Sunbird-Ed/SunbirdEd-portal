3e17ccf27a1e47bee2681d5dd2fc8f2e78fc3fdc
Sourav Dey <sourav_d@techjoomla.com>
2019-08-22 15:39:36 +0530

    Issue #SB-13558 feat: Adding test case for offline card service

--- src/app/client/src/app/modules/offline/services/offline-card-service/offline-card.service.spec.ts
+++ src/app/client/src/app/modules/offline/services/offline-card-service/offline-card.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
 import { OfflineCardService } from './offline-card.service';
+import { Response } from './offline-card.service.spec.data';
 
 describe('OfflineCardService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
     const service: OfflineCardService = TestBed.get(OfflineCardService);
+    const response = service.checkYoutubeContent(Response.cardData);
+    expect(response).toBe(true);
+    expect(service).toBeTruthy();
+  });
+  it('should be created', () => {
+    const service: OfflineCardService = TestBed.get(OfflineCardService);
+    const response = service.checkYoutubeContent(Response.cardDataWithoutYoutubeContent);
+    expect(response).toBe(false);
     expect(service).toBeTruthy();
   });
 });
