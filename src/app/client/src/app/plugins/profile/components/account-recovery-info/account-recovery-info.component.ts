30cb83ccb283330b74fa54fb734c54f693bc62d9
sudip mukherjee <sudip.mukherjee@tarento.com>
2019-08-27 11:33:41 +0530

    Issue #SC-1288 feat: Skeleton for account recovery identifier in profile page

---
+++ src/app/client/src/app/plugins/profile/components/account-recovery-info/account-recovery-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-recovery-info',
+  templateUrl: './account-recovery-info.component.html',
+  styleUrls: ['./account-recovery-info.component.scss']
+})
+export class AccountRecoveryInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
