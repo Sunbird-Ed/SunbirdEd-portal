4dd6751fb52230c0ac53cb349b629986b834cedc
venkanagouda-k <shashi.kallanagoudar@gmail.com>
2019-09-15 01:14:49 +0530

    Phase3 changes - report and dashboard

--- src/app/client/src/app/modules/cbse-program/components/dashboard/dashboard.component.ts
+++ src/app/client/src/app/modules/cbse-program/components/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { PublicDataService, UserService, CollectionHierarchyAPI, ActionService } from '@sunbird/core';
 import { ConfigService, ServerResponse, ContentData, ToasterService } from '@sunbird/shared';
 import { map } from 'rxjs/operators';
 import * as $ from 'jquery';
 import 'datatables.net';
 import * as _ from 'lodash-es';
-// import { saveAs } from 'file-saver';
+import { ExportToCsv } from 'export-to-csv';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -21,12 +22,12 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   public hierarchyObj = {};
   textBookChapters;
   reports;
-  table
   selectedReport;
   statusLabel
   headersTooltip: Array<any> = [];
   showLoader: boolean = false;
-  category = 'vsa';
+  selectedCategory;
+  tableData;
   questionTypeName = {
     vsa: 'Very Short Answer',
     sa: 'Short Answer',
@@ -34,7 +35,9 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     mcq: 'Multiple Choice Question',
     curiosityquestion: 'Curiosity Question'
   };
-  showCategory;
+  @ViewChild('topicSection') tableHeader: ElementRef;
+  datatable: any;
+
 
 
   constructor(public publicDataService: PublicDataService, private configService: ConfigService,
@@ -45,25 +48,29 @@ export class DashboardComponent implements OnInit, AfterViewInit {
 
     this.reports = [{ name: 'Question Bank Status' }, { name: 'Textbook Status' }];
     this.selectedReport = this.reports[0].name;
-    this.getCollectionHierarchy(this.selectedAttributes.textbook);
 
+    //should not change the order of below array
     this.questionType = ['vsa', 'sa', 'la', 'mcq', 'curiosityquestion'];
+    //default selected category
+    this.selectedCategory = 'vsa';
+    this.headersTooltip = [{ tip: "No. of resource (no. of published questions)" }];
+    this.statusLabel = [{ name: 'Up For Review', tip: "No. of questions pending for review" }, { name: "Rejected", tip: 'No. of questions rejected by reviewer' }, { name: 'Accepted', tip: 'No. of questions approved by reviewer' }, { name: 'Published', tip: 'No. of questions published by publisher' }];
+    this.getCollectionHierarchy(this.selectedAttributes.textbook);
+  }
 
-    this.showCategory = this.questionTypeName['vsa'];
-
-    // this.dtOptions = {
-    //   dom: 'Bfrtip',
-    //   buttons: [
-    //     'csv'
-    //   ]
-    // }
+  ngAfterViewInit() {
+      this.tableHeader.nativeElement
   }
 
   changeQuestionCategory(type) {
-    this.showCategory = this.questionTypeName[type];
-    this.category = type;
-    // var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
-    // saveAs(blob, "hello world.txt");
+    this.showLoader = true;
+    //refresh datatable values and re-initializing it, setTimeOut is given to show loader on change of data
+    setTimeout(() => {
+      this.showLoader = false;
+      this.initializeDataTable(this.selectedReport);
+    }, 500);
+    this.selectedCategory = type;
+    this.generateTableData(this.selectedReport);
   }
 
   public getCollectionHierarchy(identifier: string) {
@@ -108,7 +115,6 @@ export class DashboardComponent implements OnInit, AfterViewInit {
       });
       this.textBookMeta = textBookMetaData;
       this.dashboardApi(this.textBookMeta);
-      console.log("textbookMeta no. of topics ---> ", this.textBookMeta);
     }, error => {
 
       this.toasterService.error(_.get(error, 'error.params.errmsg') || 'Fetching TextBook details failed');
@@ -135,7 +141,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     return this.hierarchyObj;
   }
 
-  public searchQuestionsByType(questionType?: string, createdBy?: string, status?: any) {
+  public searchQuestionsByType() {
     const req = {
       url: `${this.configService.urlConFig.URLS.COMPOSITE.SEARCH}`,
       data: {
@@ -161,15 +167,12 @@ export class DashboardComponent implements OnInit, AfterViewInit {
         }
       }
     };
-    if (createdBy) {
-      req.data.request.filters['createdBy'] = createdBy;
-    }
-    if (status) {
-      req.data.request.filters['status'] = status;
-      req.data.request.filters['organisation'] = this.selectedAttributes.selectedSchoolForReview;
-    }
+    
     return this.publicDataService.post(req).pipe(
-      map(res => _.get(res, 'result.aggregations[0].values')));
+      map(res => { return _.get(res, 'result.aggregations[0].values') },
+        err => {
+          this.toasterService.error(_.get(err, 'error.params.errmsg') || 'Fetching aggregations of TextBook failed');
+        }));
   }
 
   public searchResources(qtype) {
@@ -179,7 +182,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
         'request': {
           'filters': {
             'objectType': 'content',
-            'contentType': qtype === 'curiosity' ? 'CuriosityQuestionSet' : 'PracticeQuestionSet',
+            'contentType': qtype === 'curiosityquestion' ? 'CuriosityQuestionSet' : 'PracticeQuestionSet',
             'mimeType': 'application/vnd.ekstep.ecml-archive',
             'board': this.selectedAttributes.board,
             'framework': this.selectedAttributes.framework,
@@ -187,7 +190,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
             'subject': this.selectedAttributes.subject,
             'medium': this.selectedAttributes.medium,
             'status': ['Live'],
-            'questionCategories': (qtype === 'curiosity') ? 'CuriosityQuestion' : qtype.toUpperCase()
+            'questionCategories': (qtype === 'curiosityquestion') ? 'CuriosityQuestion' : qtype.toUpperCase()
           },
           'sort_by': { 'createdOn': 'desc' },
           'fields': ['identifier', 'status', 'createdOn', 'topic', 'name', 'questions'],
@@ -197,6 +200,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
     };
     return this.publicDataService.post(request).pipe(
       map(res => {
+        // return _.get(res, 'result');
         const content = _.get(res, 'result.content');
         const publishCount = [];
         _.forIn(_.groupBy(content, 'topic'), (value, key) => {
@@ -205,26 +209,32 @@ export class DashboardComponent implements OnInit, AfterViewInit {
         });
         return publishCount;
       }, err => {
-        console.log(err);
+        this.toasterService.error(_.get(err, 'error.params.errmsg') || 'Fetching Resource details failed');
       }));
   }
 
   dashboardApi(textBookMetaData) {
 
-    this.searchQuestionsByType().subscribe((res) => {
-      let aggregationData = res;
+    let apiRequest;
+    apiRequest = [this.searchQuestionsByType(), ...this.questionType.map(type => this.searchResources(type))]
+    if (!apiRequest) {
+      console.log("something error")
+    }
+    forkJoin(apiRequest).subscribe(data => {
+      console.log("seqP---->", data)
+
       this.textBookChapters = _.map(textBookMetaData, topicData => {
         const results = { name: topicData.name, topic: topicData.topic, identifier: topicData.identifier };
-        _.forEach(aggregationData, (Tobj) => {
+        _.forEach(data[0], (Tobj) => {
           if (Tobj.name === topicData.topic.toLowerCase()) {
             _.forEach(Tobj.aggregations[0].values, (Cobj) => {
-              let modi = _.map(Cobj.aggregations[0].values, (a) => {
+              let modify = _.map(Cobj.aggregations[0].values, (a) => {
                 let temp = {}
                 temp[a.name] = a.count;
                 return temp;
               })
 
-              results[Cobj.name] = modi.reduce((result, item) => {
+              results[Cobj.name] = modify.reduce((result, item) => {
                 var key = Object.keys(item)[0];
                 result[key] = item[key];
                 return result;
@@ -234,81 +244,124 @@ export class DashboardComponent implements OnInit, AfterViewInit {
         })
         return results;
       });
-      //Below code to make api call to get published question and to include in the variable 'textBookChapters'
-      _.forEach(this.questionType, (type) => {
-        this.searchResources(type).subscribe((res) => {
-          var publishedData = res;
-          _.forEach(this.textBookChapters, (chap) => {
-            _.forEach(publishedData, (data) => {
-              if (chap.topic.toLowerCase() === data.name) {
-                Object.assign(chap[type], { 'published': data.count })
-              }
-            })
 
-          })
-        })
-      })
+     //Below code to make api call to get published question and to include in the variable 'textBookChapters'
+      _.forEach(this.textBookChapters, (chap) => {
+        _.forEach(this.questionType, (type, index) => {
+          let filter_by_category = _.filter(data[index + 1], { name: chap.topic.toLowerCase() })
+          if (filter_by_category.length > 0) Object.assign(chap[type], { 'published': filter_by_category[0].count })
+        });
+      });
+
+      this.generateTableData(this.selectedReport);
       this.initializeDataTable(this.selectedReport);
-    })
+    });
   }
 
   refreshReport() {
     this.getCollectionHierarchy(this.selectedAttributes.textbook)
   }
 
+  downloadReport() {
+
+    const options = {
+      filename: `${this.selectedReport}`,
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalSeparator: '.',
+      showLabels: true,
+      showTitle: true,
+      title: this.selectedAttributes.textbookName,
+      useTextFile: false,
+      useBom: true,
+      useKeysAsHeaders: true,
+      // headers: ['Column 1', 'Column 2', etc...] <-- Won't work with useKeysAsHeaders present!
+    };
+    const csvExporter = new ExportToCsv(options);
+ csvExporter.generateCsv(this.tableData);
+  }
+
+  generateTableData(report) {
+    let Tdata
+    if (report === this.reports[0].name) {
+      
+        Tdata = _.map(this.textBookChapters, (item) => {
+        let result = {};
+        result['Topic Name'] = item.name + "(" + item.topic + ")";
+        result[this.statusLabel[0].name] = (item[this.selectedCategory] && item[this.selectedCategory].review) ? item[this.selectedCategory].review : 0;
+        result[this.statusLabel[1].name] = (item[this.selectedCategory] && item[this.selectedCategory].reject) ? item[this.selectedCategory].reject : 0;
+        result[this.statusLabel[2].name] = (item[this.selectedCategory] && item[this.selectedCategory].live) ? item[this.selectedCategory].live : 0;
+        result[this.statusLabel[3].name] = (item[this.selectedCategory] && item[this.selectedCategory].published) ? item[this.selectedCategory].published : 0;
+        return result;
+      });
+      this.tableData = Tdata;
+      
+
+    } else if (report === this.reports[1].name) {
+      Tdata = _.map(this.textBookChapters, (item) => {
+        let result = {};
+        result['Topic Name'] = item.name + "(" + item.topic + ")";
+        result[this.questionTypeName[this.questionType[0]]] = (item[this.questionType[0]] && item[this.questionType[0]].published) ? item[this.questionType[0]].published : 0;
+        result[this.questionTypeName[this.questionType[1]]] = (item[this.questionType[1]] && item[this.questionType[1]].published) ? item[this.questionType[1]].published : 0;
+        result[this.questionTypeName[this.questionType[2]]] = (item[this.questionType[2]] && item[this.questionType[2]].published) ? item[this.questionType[2]].published : 0;
+        result[this.questionTypeName[this.questionType[3]]] = (item[this.questionType[3]] && item[this.questionType[3]].published) ? item[this.questionType[3]].published : 0;
+        result[this.questionTypeName[this.questionType[4]]] = (item[this.questionType[4]] && item[this.questionType[4]].published) ? item[this.questionType[4]].published : 0;
+        return result;
+      });
+     
+     this.tableData = Tdata;
+    }
+  }
+
+  onReportChange(report) {
+    if (report !== this.selectedReport) {
+      this.generateTableData(report);
+      this.initializeDataTable(report);
+    }
+  }
+
   initializeDataTable(report) {
     console.log("initialized")
     if (report === this.reports[0].name) {
-      $('#questionBank').DataTable();
       setTimeout(() => {
-        $('#questionBank').DataTable(
+       this.datatable =  $('#questionBank').DataTable(
           {
-            dom: 'Bfrtip',
-            buttons: [
-              'csv'
-            ]
+            paging: false,
+            searching: false,
+            info: false,
+            destroy: true,
+            lengthChange: false,
+            dom: 'itp',
           }
         );
+
         this.showLoader = false;
-      }, 5000);
+      }, 0);
     } else if (report === this.reports[1].name) {
       setTimeout(() => {
         $('#TextbookStatus').DataTable(
           {
-            dom: 'Bfrtip',
-            buttons: [
-              'csv'
-            ]
+            paging: false,
+            searching: false,
+            info: false,
+            destroy: true,
+            lengthChange: false,
+            dom: 'itp',
           }
         );
         this.showLoader = false;
-      }, 5000);
+      }, 0);
     }
-    this.manageHeaders(report);
+
   }
 
-  manageHeaders(report) {
-    if (report === this.reports[1].name) {
-      this.headersTooltip = [{ tip: "No. of resource (no. of published questions)" }]
 
-    } else if (report === this.reports[0].name) {
-      this.statusLabel = [{ name: 'Up For Review', tip: "No. of questions pending for review" }, { name: "Rejected", tip: 'No. of questions rejected by reviewer' }, { name: 'Accepted', tip: 'No. of questions approved by reviewer' }, { name: 'Published', tip: 'No. of questions published by publisher' }];
-    }
-  }
 
-  ngAfterViewInit() {
-  }
 
-  sample(){
-    console.log("dataTable initialized=====>")
-    $('#questionBank').DataTable(
-      {
-        dom: 'Bfrtip',
-        buttons: [
-          'csv'
-        ]
-      }
-    );
-  }
-  
+
+
+
+
+
+
 }
