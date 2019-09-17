e5ff441f1e92556e11ec078790d9270d176d19b6
Devesh <devesh.mehta@mantralabsglobal.com>
2019-09-17 16:49:40 +0530

    Issue #SB-13773 fix:fixed failing test cases

--- src/app/client/src/app/modules/shared-feature/components/sso-merge-confirmation/sso-merge-confirmation.component.spec.ts
+++ src/app/client/src/app/modules/shared-feature/components/sso-merge-confirmation/sso-merge-confirmation.component.spec.ts
@@ -1,16 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {SuiModule} from 'ng2-semantic-ui';
 
-import { SsoMergeConfirmationComponent } from './sso-merge-confirmation.component';
+import {SsoMergeConfirmationComponent} from './sso-merge-confirmation.component';
+import {ActivatedRoute, Router} from '@angular/router';
+import {of as observableOf} from 'rxjs';
+import {ResourceService} from '@sunbird/shared';
 
 describe('SsoMergeConfirmationComponent', () => {
   let component: SsoMergeConfirmationComponent;
   let fixture: ComponentFixture<SsoMergeConfirmationComponent>;
+  const resourceBundle = {
+    languageSelected$: observableOf({})
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SsoMergeConfirmationComponent ]
+      imports: [SuiModule],
+      declarations: [SsoMergeConfirmationComponent],
+      providers: [
+        {provide: ResourceService, useValue: resourceBundle},
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
