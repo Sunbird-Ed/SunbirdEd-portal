92b7a2554ff539672aa4b3bfa46635be4f0edc6e
ravinder kumar <yravinderkumar33@gmail.com>
2019-09-18 17:32:42 +0530

    Issue #SB-14719 feat : L0 Version of MHRD, PMO, CM Dashboards

--- src/app/client/src/app/modules/dashboard/components/data-chart/data-chart.component.ts
+++ src/app/client/src/app/modules/dashboard/components/data-chart/data-chart.component.ts
@@ -1,3 +1,4 @@
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { ResourceService, ToasterService } from '@sunbird/shared';
 import { BaseChartDirective } from 'ng2-charts';
@@ -50,6 +51,7 @@ export class DataChartComponent implements OnInit, OnDestroy {
   selectedFilters: {};
   dateFilterReferenceName;
   telemetryCdata: Array<{}>;
+  iframeDetails: any;
 
   @ViewChild('datePickerForFilters') datepicker: ElementRef;
 
@@ -64,7 +66,7 @@ export class DataChartComponent implements OnInit, OnDestroy {
 
   @ViewChild(BaseChartDirective) chartDirective: BaseChartDirective;
   constructor(public resourceService: ResourceService, private fb: FormBuilder,
-    private toasterService: ToasterService, private activatedRoute: ActivatedRoute) {
+    private toasterService: ToasterService, private activatedRoute: ActivatedRoute, private sanitizer: DomSanitizer) {
     this.alwaysShowCalendars = true;
   }
 
@@ -79,7 +81,7 @@ export class DataChartComponent implements OnInit, OnDestroy {
     }
   }
 
-  buildFiltersForm() {
+  private buildFiltersForm() {
     this.filtersFormGroup = this.fb.group({});
     if (_.get(this.chartConfig, 'labelsExpr')) {
       _.forEach(this.filters, filter => {
@@ -93,7 +95,7 @@ export class DataChartComponent implements OnInit, OnDestroy {
         filter.options = _.uniq(_.map(this.chartData, data => data[filter.reference].toLowerCase()));
       });
       if (this.filters.length > 0) {
-       this.showFilters = true;
+        this.showFilters = true;
       }
       this.filtersSubscription = this.filtersFormGroup.valueChanges
         .pipe(
@@ -123,16 +125,35 @@ export class DataChartComponent implements OnInit, OnDestroy {
     }
   }
 
-  prepareChart() {
-    this.chartOptions = _.get(this.chartConfig, 'options') || { responsive: true };
-    this.chartColors = _.get(this.chartConfig, 'colors') || ['#024F9D'];
-    this.chartType = _.get(this.chartConfig, 'chartType') || 'line';
-    this.legend = (_.get(this.chartConfig, 'legend') === false) ? false : true;
-    this.filters = _.get(this.chartConfig, 'filters') || [];
-    this.getDataSetValue();
+  private checkForExternalChart() {
+    const iframeConfig = _.get(this.chartConfig, 'iframeConfig');
+    if (iframeConfig) {
+      if (_.get(iframeConfig, 'sourceUrl')) {
+        return true;
+      }
+    }
+    return false;
   }
 
-  getDataSetValue(chartData = this.chartData) {
+  getIframeURL() {
+    const url = `${_.get(this.iframeDetails, 'sourceUrl')}?qs=${Math.round(Math.random() * 10000000)}`; // to prevent browser caching
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  private prepareChart() {
+    if (!this.checkForExternalChart()) {
+      this.chartOptions = _.get(this.chartConfig, 'options') || { responsive: true };
+      this.chartColors = _.get(this.chartConfig, 'colors') || ['#024F9D'];
+      this.chartType = _.get(this.chartConfig, 'chartType') || 'line';
+      this.legend = (_.get(this.chartConfig, 'legend') === false) ? false : true;
+      this.getDataSetValue();
+    } else {
+      this.iframeDetails = _.get(this.chartConfig, 'iframeConfig');
+    }
+    this.filters = _.get(this.chartConfig, 'filters') || [];
+  }
+
+  private getDataSetValue(chartData = this.chartData) {
     let labels = [];
     let groupedDataBasedOnLabels;
     if (_.get(this.chartConfig, 'labelsExpr')) {
@@ -198,7 +219,7 @@ export class DataChartComponent implements OnInit, OnDestroy {
     this.unsubscribe.complete();
   }
 
-  setTelemetryCdata() {
+  private setTelemetryCdata() {
     this.telemetryCdata = [
       {
         id: 'dashboard:filter',
@@ -217,4 +238,5 @@ export class DataChartComponent implements OnInit, OnDestroy {
       pageid: this.activatedRoute.snapshot.data.telemetry.pageid
     };
   }
+
 }
