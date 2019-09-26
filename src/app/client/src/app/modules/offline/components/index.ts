86959d9d7fb8b83cb64af0960f41cc8716083dec
Anoop HM <anoophm44@gmail.com>
2019-09-26 11:32:34 +0530

    Issue #SB-14823 feat: import content to be handled in electron app modal (#2631)
    * Issue #SB-14823 feat: import content to be handled in electron app modal
    
    * Issue #SB-14823 feat: import content to be handled in electron app modal for standalone mode and in browser for client server mode.
    
    * Issue #SB-14823 feat: import content to be handled in electron app modal for standalone mode and in browser for client server mode.
    
    * Issue #SB-14823 feat: testcase fix
    
    * Issue #SB-14823 feat: url change
    
    * Issue #SB-14823 feat: removed unused http client

--- src/app/client/src/app/modules/offline/components/index.ts
+++ src/app/client/src/app/modules/offline/components/index.ts
@@ -1,4 +1,3 @@
-export * from './content-import/content-import.component';
 export * from './content-import-header/content-import-header.component';
 export * from './browse/browse.component';
 export * from './network-status/network-status.component';
