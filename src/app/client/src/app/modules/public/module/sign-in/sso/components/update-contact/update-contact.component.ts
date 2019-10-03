a8833cdced9cb38b0baeedb6a797ac42d5609403
Devesh <devesh.mehta@mantralabsglobal.com>
2019-10-03 14:58:01 +0530

    Issue #SB-15045 fix:state user should not be allowed to do migration

--- src/app/client/src/app/modules/public/module/sign-in/sso/components/update-contact/update-contact.component.ts
+++ src/app/client/src/app/modules/public/module/sign-in/sso/components/update-contact/update-contact.component.ts
@@ -25,6 +25,7 @@ export class UpdateContactComponent implements OnInit, AfterViewInit {
   public userDetails: any = {};
   public showError = false;
   public custodianOrgDetails;
+  public isValidIdentifier;
   public validationPattern = {
     phone: /^[6-9]\d{9}$/,
     email: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-z]{2,4}$/
@@ -56,14 +57,20 @@ export class UpdateContactComponent implements OnInit, AfterViewInit {
         }
       };
     });
+    this.showOtpComp = false;
+    this.showMergeConfirmation = true;
   }
   handleFormChangeEvent() {
     this.contactDetailsForm.valueChanges.pipe(delay(1)).subscribe((data, data2) => {
       if (_.get(this.contactDetailsForm, 'controls.value.status') === 'VALID'
         && this.validationPattern[this.contactForm.type].test(this.contactForm.value)) {
-          this.checkUserExist();
+        this.disableSubmitBtn = false;
+         this.isValidIdentifier = true;
       } else {
         this.disableSubmitBtn = true;
+        this.isValidIdentifier = false;
+        this.userExist = false;
+        this.userBlocked = false;
       }
     });
   }
@@ -79,15 +86,19 @@ export class UpdateContactComponent implements OnInit, AfterViewInit {
           this.disableSubmitBtn = false;
           this.userExist = false;
           this.userBlocked =  false;
+          this.generateOtp();
         } else {
           this.userExist = true;
+          this.disableSubmitBtn = true;
         }
       }, err => {
         this.userDetails = {};
         if (_.get(err, 'error.params.status') && err.error.params.status === 'USER_ACCOUNT_BLOCKED') {
           this.userBlocked =  true;
+          this.disableSubmitBtn = true;
           return;
         }
+        this.generateOtp();
         this.disableSubmitBtn = false;
     });
   }
@@ -100,7 +111,12 @@ export class UpdateContactComponent implements OnInit, AfterViewInit {
       return custodianOrgDetails;
     }));
   }
-  public handleSubmitEvent() {
+
+  public onFormUpdate() {
+    this.checkUserExist();
+  }
+
+  private generateOtp() {
     const request = {
       request: {
         'key': this.contactForm.value,
