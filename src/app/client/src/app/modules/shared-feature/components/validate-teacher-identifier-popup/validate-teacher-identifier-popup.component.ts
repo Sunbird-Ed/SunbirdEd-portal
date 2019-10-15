2e70d5598a17b67d31ca0af7ef41f5ad635e9137
sudip mukherjee <sudip.mukherjee@tarento.com>
2019-10-15 10:46:01 +0530

    Issue #SC-1349 feat: Added mock api endpoints and modifying UI according to design

--- src/app/client/src/app/modules/shared-feature/components/validate-teacher-identifier-popup/validate-teacher-identifier-popup.component.ts
+++ src/app/client/src/app/modules/shared-feature/components/validate-teacher-identifier-popup/validate-teacher-identifier-popup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output,  EventEmitter  } from '@angular/core';
+import { UserService } from '@sunbird/core';
 
 @Component({
   selector: 'app-validate-teacher-identifier-popup',
@@ -6,13 +7,90 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./validate-teacher-identifier-popup.component.scss']
 })
 export class ValidateTeacherIdentifierPopupComponent implements OnInit {
+  @Output() close = new EventEmitter<any>();
   @ViewChild('createValidateModal') createValidateModal;
+  @ViewChild('extIdInputField') extIdInputField: ElementRef;
+  externalId: string;
+  processValidation = false;
+  enableSubmitButton: boolean;
+  count = 0;
+  showError: boolean;
+  extIdVerified: boolean;
+  extIdFailed: boolean;
+  navivateToResult =  false;
 
-  constructor() { }
+  constructor(public userService: UserService) { }
+  ngOnInit() {
+  }
+
+  verifyExtId() {
+    const request = {
+
+    };
+    this.userService.validateExtId(request).subscribe(
+      (response) => {
+        this.extIdVerified = true;
+
+      }, (error) => {
+        this.count += 1;
+        this.extIdInputField.nativeElement.value = '';
+        this.extIdInputField.nativeElement.focus();
+        this.enableSubmitButton = false;
+        if (this.count === 2) {
+          this.navivateToResult = true;
+          this.failExtIdAndUpdateDb();
+        } else {
+          this.showError = true;
+        }
+      }
+    );
+  }
+
+  rejectExtId() {
+    const request = {
+
+    };
+    this.userService.rejectExtId(request).subscribe(
+      (response) => {
+        this.closeModal();
+
+
+      }, (error) => {
+        this.closeModal();
+      }
+    );
+  }
+
+  failExtIdAndUpdateDb() {
+    const request = {
+
+    };
+    this.userService.failExtId(request).subscribe(
+      (response) => {
+
+      }, (error) => {
+        this.extIdFailed = true;
+
+      }
+    );
+  }
+
+  getIdLength(event: any) {
+    if (event.target.value.length > 0) {
+      this.showError = false;
+      this.enableSubmitButton = true;
+    } else {
+      this.enableSubmitButton = false;
+    }
+  }
+
   closeModal() {
     this.createValidateModal.deny();
+    this.close.emit();
   }
-  ngOnInit() {
+
+  navigateToValidateId() {
+    this.processValidation = true;
   }
 
 }
