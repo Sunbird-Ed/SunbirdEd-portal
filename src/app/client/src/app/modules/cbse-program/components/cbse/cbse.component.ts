67898716b1f20ea4a053487a44da1418335bd456
Rajeev Sathish <rajeev.sathish@tarento.com>
2019-10-24 16:29:22 +0530

    Issue #SB-15048 Ecert changes

--- src/app/client/src/app/modules/cbse-program/components/cbse/cbse.component.ts
+++ src/app/client/src/app/modules/cbse-program/components/cbse/cbse.component.ts
@@ -54,7 +54,7 @@ export class CbseComponent implements OnInit, OnDestroy {
     {value: 'Best Student Certificate'}
   ];
   public showCertDashboard = false;
-  public slug = (<HTMLInputElement>document.getElementById('defaultTenant')).value
+  public slug ;
   public selectedOption;
   public showModal: boolean = false;
   constructor(public frameworkService: FrameworkService, public toasterService: ToasterService) { }
@@ -82,6 +82,7 @@ export class CbseComponent implements OnInit, OnDestroy {
     this.formFieldOptions = _.get(this.programDetails, 'config.onBoardForm.fields');
     this.fetchFrameWorkDetails();
     this.selectedAttributes.lastOpenedUnit = 0;
+    this.slug = _.get(this.userProfile, 'rootOrg.slug') || (<HTMLInputElement>document.getElementById('defaultTenant')).value
 
   }
 
@@ -120,13 +121,13 @@ export class CbseComponent implements OnInit, OnDestroy {
 
   handleRoleChange(component?:string) {
     this.role = Object.assign({}, {currentRole : this.selectedAttributes.currentRole});
+    this.showDashboard = (component === 'Dashboard');
     if(component === 'dashboard'){
       this.showCertDashboard = true;
       this.selectedOption = "";
     } else{
       this.showCertDashboard = false;
     }
-    //this.showDashboard = (component === 'Dashboard');
   }
   public fetchFrameWorkDetails() {
     this.frameworkService.initialize(this.selectedAttributes.framework);
