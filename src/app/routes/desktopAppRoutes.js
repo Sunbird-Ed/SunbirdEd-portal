b2366def7c65da7694f980770ab73cdae69ea0b5
Sourav Dey <sourav_d@techjoomla.com>
2019-10-24 13:05:04 +0530

    Issue #SB-12681 feat: Adding new API to check app update

--- src/app/routes/desktopAppRoutes.js
+++ src/app/routes/desktopAppRoutes.js
@@ -1,5 +1,6 @@
-const desktopAppHelper = require('../helpers/desktopAppHelper.js')
-const bodyParser = require('body-parser')
+const desktopAppHelper = require('../helpers/desktopAppHelper.js');
+const bodyParser = require('body-parser');
+const envHelper = require('./../helpers/environmentVariablesHelper.js');
 
 module.exports = function (app) {
     app.post('/v1/desktop/update', bodyParser.urlencoded({ extended: true }),
@@ -7,6 +8,6 @@ module.exports = function (app) {
         desktopAppHelper.getAppUpdate())
 
     app.get('/desktop/latest/:artifact', function (req, res) {
-        res.redirect(`/latest/${req.params.artifact}`);
+        res.redirect(`${envHelper.DESKTOP_APP_STORAGE_URL}/latest/${req.params.artifact}`);
     });
 }
