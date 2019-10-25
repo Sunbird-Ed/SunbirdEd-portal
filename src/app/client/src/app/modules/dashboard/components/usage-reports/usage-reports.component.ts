7dfd25a64985472dcfa3e0e4015ce07180d7746d
ravinder kumar <yravinderkumar33@gmail.com>
2019-10-25 16:00:24 +0530

    Issue #SB-14804 feat : L1 Version of MHRD, PMO, CM Dashboards - ECG of Learning

--- src/app/client/src/app/modules/dashboard/components/usage-reports/usage-reports.component.ts
+++ src/app/client/src/app/modules/dashboard/components/usage-reports/usage-reports.component.ts
@@ -84,7 +84,7 @@ export class UsageReportsComponent implements OnInit, AfterViewInit {
           const data = _.get(response, 'result');
           this.showLoader = false;
           if (_.get(report, 'charts')) {
-            this.createChartData(_.get(report, 'charts'), data);
+            this.createChartData(_.get(report, 'charts'), data, url);
           }
           if (_.get(report, 'table')) { this.renderTable(_.get(report, 'table'), data); } else {
             this.renderTable({}, data);
@@ -95,11 +95,12 @@ export class UsageReportsComponent implements OnInit, AfterViewInit {
       }, err => { console.log(err); });
   }
 
-  createChartData(charts, data) {
+  createChartData(charts, data, downloadUrl) {
     this.chartData = [];
     _.forEach(charts, chart => {
       const chartObj: any = {};
       chartObj.chartConfig = chart;
+      chartObj.downloadUrl = downloadUrl;
       chartObj.chartData = _.get(data, 'data');
       chartObj.lastUpdatedOn = _.get(data, 'metadata.lastUpdatedOn');
       this.chartData.push(chartObj);
