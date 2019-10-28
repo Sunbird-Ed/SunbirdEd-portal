89753749839d8c1016263bc24cfda1c2f2ae79fa
Anoop HM <anoophm44@gmail.com>
2019-10-28 23:36:19 +0530

    Issue #SB-0000 feat: created desktop app project for better code maintains

--- src/app/client/src/app/modules/offline/services/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './offline-file-uploader-service/offline-file-uploader.service';
-export * from './connection-service/connection.service';
-export * from './download-manager/download-manager.service';
-export * from './electron-dialog/electron-dialog.service';
