fe6250bcbf1b848e4b7c37224870d0099560df6b
Rajeev Sathish <rajeev.sathish@tarento.com>
2019-11-05 10:33:25 +0530

    Issue #SB-15048 Ecert Changes

--- src/app/client/src/app/modules/cbse-program/components/cbse/cbse.component.ts
+++ src/app/client/src/app/modules/cbse-program/components/cbse/cbse.component.ts
@@ -74,8 +74,8 @@ export class CbseComponent implements OnInit, OnDestroy {
       board: _.get(this.programDetails, 'config.scope.board[0]'),
       medium: _.get(this.programDetails, 'config.scope.medium[0]'),
       bloomsLevel: _.get(this.programDetails, 'config.scope.bloomsLevel'),
-      //programId: _.get(this.programDetails, 'programId'),
-      programId: '31ab2990-7892-11e9-8a02-93c5c62c03f1',
+      programId: _.get(this.programDetails, 'programId'),
+      //programId: '31ab2990-7892-11e9-8a02-93c5c62c03f1',
       program: _.get(this.programDetails, 'name'),
       onBoardSchool: _.get(this.programDetails, 'userDetails.onBoardingData.school')
     };
