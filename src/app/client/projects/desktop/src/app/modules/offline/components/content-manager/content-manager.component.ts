59a33473b576186bd154b5b07ac3c93f0854799f
Sourav Dey <sourav_d@techjoomla.com>
2019-11-10 13:54:00 +0530

    Issue #SB-15420 feat: UI changes to support content import

--- src/app/client/projects/desktop/src/app/modules/offline/components/content-manager/content-manager.component.ts
+++ src/app/client/projects/desktop/src/app/modules/offline/components/content-manager/content-manager.component.ts
@@ -1,21 +1,20 @@
-
 import { Component, OnInit } from '@angular/core';
 import { ResourceService, ToasterService, ConfigService } from '@sunbird/shared';
-import { timer } from 'rxjs';
-import { switchMap, delay, map } from 'rxjs/operators';
+import { timer, Subject, combineLatest } from 'rxjs';
+import { switchMap, map, filter } from 'rxjs/operators';
 import * as _ from 'lodash-es';
 import { ContentManagerService, ElectronDialogService } from '../../services';
 import { ConnectionService } from '../../services';
 import { Router, ActivatedRoute } from '@angular/router';
 import { IInteractEventEdata } from '@sunbird/telemetry';
-
+​
 @Component({
   selector: 'app-content-manager',
   templateUrl: './content-manager.component.html',
   styleUrls: ['./content-manager.component.scss']
 })
 export class ContentManagerComponent implements OnInit {
-
+​
   contentResponse: any;
   isConnected: boolean = navigator.onLine;
   isOpen = false;
@@ -35,72 +34,22 @@ export class ContentManagerComponent implements OnInit {
   denyCancelInteractData: IInteractEventEdata;
   localStatusArr = ['inProgress', 'inQueue', 'resume', 'resuming'];
   cancelId: string;
-
+  apiCallTimer = timer(1000, 3000).pipe(filter(data => !data || (this.callContentList && this.isConnected)));
+  apiCallSubject = new Subject();
   constructor(public contentManagerService: ContentManagerService,
     public resourceService: ResourceService, public toasterService: ToasterService,
     public connectionService: ConnectionService, public electronDialogService: ElectronDialogService,
     public configService: ConfigService,
     public activatedRoute: ActivatedRoute,
-    public router: Router) { }
-
-  ngOnInit() {
-    this.setTelemetryInteractData();
-    // Call download list initially
-    this.getContentList();
-
-    // Subscribe connection service to check online/offline and call download list
-    this.connectionService.monitor().subscribe(isConnected => {
-      this.isConnected = isConnected;
-      this.getContentList();
-    });
-
-    // Call content list when clicked on add to library
-    this.contentManagerService.downloadEvent.subscribe((data) => {
-      this.isOpen = true;
-      this.getContentList();
-    });
-
-    // Call content list while uploading content
-    this.electronDialogService.uploadEvent.subscribe((data) => {
-      this.isOpen = true;
-      this.getContentList();
-    });
-  }
-
-  getContentList() {
-    this.contentManagerService.getContentList()
-      .pipe(
-        map((resp: any) => {
-          this.callContentListTimer = false;
-          // Changing content status if local content status is changed
-          _.forEach(_.get(resp, 'result.response.contents'), (value) => {
-            const data = this.contentStatusObject[value.id];
-            if (data) { value.status = data.currentStatus; }
-            if (_.includes(this.localStatusArr, value.status)) {
-              this.callContentListTimer = true;
-            }
-          });
-          return _.get(resp, 'result.response.contents');
-        }))
-      .subscribe(
-        (apiResponse: any) => {
-          this.contentResponse = apiResponse;
-          if (this.callContentListTimer && this.isConnected) {
-            this.getContentListUsingTimer();
-          }
-        });
-  }
-
-  private getContentListUsingTimer() {
-    const result = timer(1, 3000).pipe(
-      switchMap(() => this.contentManagerService.getContentList())
-    );
-
-    this.subscription = result
-      .pipe(
+    public router: Router) {
+      this.getList();
+    }
+​
+  getList() {
+    combineLatest(this.apiCallTimer, this.apiCallSubject, (data1, data2) => true)
+      .pipe(switchMap(() => this.contentManagerService.getContentList()),
         map((resp: any) => {
           this.callContentList = false;
-          // Changing content status if local content status is changed
           _.forEach(_.get(resp, 'result.response.contents'), (value) => {
             const data = this.contentStatusObject[value.id];
             if (data) { value.status = data.currentStatus; }
@@ -109,17 +58,34 @@ export class ContentManagerComponent implements OnInit {
             }
           });
           return _.get(resp, 'result.response.contents');
-        }))
-      .subscribe(
-        (apiResponse: any) => {
-          this.contentResponse = apiResponse;
-          if (this.callContentList === false || !this.isConnected) {
-            this.subscription.unsubscribe();
-            this.getContentList();
-          }
-        });
+        })).subscribe((apiResponse: any) => {
+            this.contentResponse = apiResponse;
+          });
   }
-
+  ngOnInit() {
+    this.setTelemetryInteractData();
+    // Call download list initially
+    this.apiCallSubject.next();
+    // Subscribe connection service to check online/offline and call download list
+    this.connectionService.monitor().subscribe(isConnected => {
+      this.isConnected = isConnected;
+      this.apiCallSubject.next();
+    });
+​
+    // Call content list when clicked on add to library
+    this.contentManagerService.downloadEvent.subscribe((data) => {
+      this.isOpen = true;
+      this.apiCallSubject.next();
+    });
+​
+    // Call content list while uploading content
+    this.electronDialogService.uploadEvent.subscribe((data) => {
+      this.isOpen = true;
+      this.apiCallSubject.next();
+    });
+  }
+​
+​
   updateLocalStatus(contentData, currentStatus) {
     this.contentStatusObject[contentData.id] = {
       currentStatus: currentStatus,
@@ -128,57 +94,54 @@ export class ContentManagerComponent implements OnInit {
     const data = _.find(this.contentResponse, { id: contentData.id });
     data.status = currentStatus;
   }
-
+​
   cancelImportContent(contentId) {
-    this.subscription.unsubscribe();
     this.contentManagerService.cancelImportContent(contentId).subscribe(
       (apiResponse: any) => {
-        this.getContentList();
+        this.apiCallSubject.next();
         this.deleteLocalContentStatus(contentId);
       },
       (err) => {
         this.toasterService.info(this.resourceService.messages.fmsg.m0097);
-        this.getContentList();
+        this.apiCallSubject.next();
         this.deleteLocalContentStatus(contentId);
       });
   }
-
+​
   pauseImportContent(contentId) {
-    this.subscription.unsubscribe();
     this.contentManagerService.pauseImportContent(contentId).subscribe(
       (apiResponse: any) => {
-        this.getContentList();
+        this.apiCallSubject.next();
         this.deleteLocalContentStatus(contentId);
       },
       (err) => {
         this.toasterService.info(this.resourceService.messages.fmsg.m0097);
-        this.getContentList();
+        this.apiCallSubject.next();
         this.deleteLocalContentStatus(contentId);
       });
   }
-
+​
   resumeImportContent(contentId) {
-    this.subscription.unsubscribe();
     this.contentManagerService.resumeImportContent(contentId).subscribe(
       (apiResponse: any) => {
-        this.getContentList();
+        this.apiCallSubject.next();
         this.deleteLocalContentStatus(contentId);
       },
       (err) => {
         this.toasterService.info(this.resourceService.messages.fmsg.m0097);
-        this.getContentList();
+        this.apiCallSubject.next();
         this.deleteLocalContentStatus(contentId);
       });
   }
-
+​
   deleteLocalContentStatus(contentId) {
     delete this.contentStatusObject[contentId];
   }
-
+​
   showProgressValue(progressSize, totalSize) {
     return (progressSize / totalSize) * 100;
   }
-
+​
   openContent(contentId, mimeType) {
     if (mimeType === this.configService.appConfig.PLAYER_CONFIG.MIME_TYPE.collection) {
       this.router.navigate(['play/collection', contentId]);
@@ -186,7 +149,7 @@ export class ContentManagerComponent implements OnInit {
       this.router.navigate(['play/content', contentId]);
     }
   }
-
+​
   getTelemetryInteractData() {
     return {
       id: this.isOpen ? 'content-manager-close' : 'content-manager-open',
@@ -194,7 +157,7 @@ export class ContentManagerComponent implements OnInit {
       pageid: 'content-manager'
     };
   }
-
+​
   setTelemetryInteractData() {
     this.pauseInteractData = {
       id: 'pause',
