66e47c9e015bfaafada8ca7f739fecdb7216f3bd
sri-pusuluri <srini@zool.in>
2019-11-14 11:10:07 +0530

    reverted 2.6.0 files

--- src/app/client/projects/desktop/src/app/modules/offline/offline-routing.module.ts
+++ src/app/client/projects/desktop/src/app/modules/offline/offline-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BrowseComponent, OfflineHelpCenterComponent } from './components';
-import { LibraryComponent } from './components';
 import {ViewAllComponent} from '@sunbird/shared-feature';
 import { ExploreContentComponent } from './../../../../../../src/app/modules/public/module/explore';
 
@@ -55,8 +54,7 @@ const routes: Routes = [
     path: 'browse/get', loadChildren: './../../../../../../src/app/modules/dial-code-search/dial-code-search.module#DialCodeSearchModule'
   },
   {
-    //path: '', loadChildren: './../../../../../../src/app/modules/public/module/explore/explore.module#ExploreModule'
-    path: '', component: LibraryComponent
+    path: '', loadChildren: './../../../../../../src/app/modules/public/module/explore/explore.module#ExploreModule'
   },
   {
     path: ':slug/explore', loadChildren: './../../../../../../src/app/modules/public/module/explore/explore.module#ExploreModule'
@@ -72,3 +70,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class OfflineRoutingModule { }
+
