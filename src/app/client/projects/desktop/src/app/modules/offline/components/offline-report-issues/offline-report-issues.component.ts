2bc2b2e15daeac9e6a9c49e88496fb4249fe7098
Arti <arti@zool.in>
2019-11-14 16:30:58 +0530

    Issue #SB-15624 fix:Desktop App UI - Help page - Report Other Issue component updated

--- src/app/client/projects/desktop/src/app/modules/offline/components/offline-report-issues/offline-report-issues.component.ts
+++ src/app/client/projects/desktop/src/app/modules/offline/components/offline-report-issues/offline-report-issues.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfflineReportIssuesComponent implements OnInit {
   issueReportText = false;
+  showNormalModal = false;
   constructor() { }
 
   ngOnInit() {
