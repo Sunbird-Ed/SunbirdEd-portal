7852ed27b6c07b10413a9bcac2e9a4eeeea02c85
swetha-aggidevara <swetha@ilimi.in>
2019-11-21 22:22:41 +0530

    Issue #SB-15738 feat: added onboarding location

--- src/app/client/src/app/modules/telemetry/directives/telemetry-interact/telemetry-interact.directive.ts
+++ src/app/client/src/app/modules/telemetry/directives/telemetry-interact/telemetry-interact.directive.ts
@@ -31,7 +31,7 @@ export class TelemetryInteractDirective {
     if (this.telemetryInteractEdata) {
       this.appTelemetryInteractData = {
        context: {
-          env: this.telemetryInteractContext.env || _.get(this.activatedRoute, 'snapshot.root.firstChild.data.telemetry.env') ||
+          env: _.get(this.telemetryInteractContext, 'env') || _.get(this.activatedRoute, 'snapshot.root.firstChild.data.telemetry.env') ||
           _.get(this.activatedRoute, 'snapshot.data.telemetry.env') ||
           _.get(this.activatedRoute.snapshot.firstChild, 'children[0].data.telemetry.env') ,
           cdata: this.telemetryInteractCdata || [],
