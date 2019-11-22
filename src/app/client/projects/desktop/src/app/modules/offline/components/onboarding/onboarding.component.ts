e2d6a1231c3d15137fa9742b68e869a731a75f21
Sourav Dey <sourav_d@techjoomla.com>
2019-11-22 22:31:40 +0530

    Issue #SB-15822 feat: User preference data saving implementation

--- src/app/client/projects/desktop/src/app/modules/offline/components/onboarding/onboarding.component.ts
+++ src/app/client/projects/desktop/src/app/modules/offline/components/onboarding/onboarding.component.ts
@@ -1,8 +1,7 @@
 import { IImpressionEventInput, IInteractEventEdata } from '@sunbird/telemetry';
 import { ToasterService } from '@sunbird/shared';
-import { Component, OnInit, Output, OnDestroy, Input, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
 import { ResourceService } from '@sunbird/shared';
 import * as _ from 'lodash-es';
 import { OnboardingService } from './../../services';
@@ -12,17 +11,13 @@ import { OnboardingService } from './../../services';
   templateUrl: './onboarding.component.html',
   styleUrls: ['./onboarding.component.scss']
 })
-export class OnboardingComponent implements OnInit {
-
+export class OnboardingComponent {
   slide = 'location';
   telemetryImpressionData: IImpressionEventInput;
   telemetryInteractEdata: IInteractEventEdata;
   constructor(private router: Router, public toasterService: ToasterService,
     public activatedRoute: ActivatedRoute, public resourceService: ResourceService,
     public onboardingService: OnboardingService) {
-  }
-
-  ngOnInit() {
   }
 
   handleLocationSaveEvent() {
@@ -34,7 +29,7 @@ export class OnboardingComponent implements OnInit {
   }
 
   setTelemetryData() {
-   return {
+    return {
       context: { env: 'onboarding' },
       edata: {
         type: 'view',
--- src/app/client/projects/desktop/src/app/modules/offline/services/onboarding/onboarding.service.ts
+++ src/app/client/projects/desktop/src/app/modules/offline/services/onboarding/onboarding.service.ts
@@ -1,12 +1,13 @@
-import { map, catchError } from 'rxjs/operators';
 import { PublicDataService } from '@sunbird/core';
 import { ConfigService, ServerResponse } from '@sunbird/shared';
 import { Injectable, EventEmitter } from '@angular/core';
-import { throwError as observableThrowError, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import * as _ from 'lodash-es';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class OnboardingService {
   onboardCompletion = new EventEmitter();
   userData;
@@ -23,12 +24,14 @@ export class OnboardingService {
     };
     return this.publicDataService.post(options);
   }
+
   getUser() {
     const options = {
       url: this.configService.urlConFig.URLS.OFFLINE.READ_USER
     };
     return this.publicDataService.get(options);
   }
+
   saveLocation(request) {
     const options = {
       url: this.configService.urlConFig.URLS.OFFLINE.LOCATION_SAVE,
@@ -36,6 +39,7 @@ export class OnboardingService {
     };
     return this.publicDataService.post(options);
   }
+
   saveUserPreference(request) {
     const options = {
       url: this.configService.urlConFig.URLS.OFFLINE.CREATE_USER,
