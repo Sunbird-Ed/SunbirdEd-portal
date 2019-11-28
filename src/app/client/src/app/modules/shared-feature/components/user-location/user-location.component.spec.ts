a6ad7a2ac2872a6ba59eb3637ac958acf9e9ed06
Devesh <mailme.devmehta@gmail.com>
2019-11-28 16:10:55 +0530

    Issue #SB-15726 fix:added test cases for new code

--- src/app/client/src/app/modules/shared-feature/components/user-location/user-location.component.spec.ts
+++ src/app/client/src/app/modules/shared-feature/components/user-location/user-location.component.spec.ts
@@ -3,13 +3,19 @@ import {SuiModule} from 'ng2-semantic-ui';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {TelemetryModule} from '@sunbird/telemetry';
 import {UserLocationComponent} from './user-location.component';
-import {ResourceService, ToasterService, ConfigService, BrowserCacheTtlService, NavigationHelperService} from '@sunbird/shared';
+import {
+  ResourceService,
+  ToasterService,
+  ConfigService,
+  BrowserCacheTtlService,
+  NavigationHelperService
+} from '@sunbird/shared';
 import {ProfileService} from '@sunbird/profile';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {CacheService} from 'ng2-cache-service';
 import {DeviceDetectorService} from 'ngx-device-detector';
 import {RouterTestingModule} from '@angular/router/testing';
-
+import {userLocationMockData} from './user-location.component.spec.data';
 
 
 describe('UserLocationComponent', () => {
@@ -37,4 +43,31 @@ describe('UserLocationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not set state as state and district not found', () => {
+    component.allDistricts = null;
+    component.allStates = null;
+    component.setStateDistrict({state: {}, district: {}});
+    expect(component.selectedDistrict).toEqual(undefined);
+    expect(component.selectedState).toEqual(undefined);
+  });
+
+  it('should set state and not set district as district list not found', () => {
+    component.allStates = userLocationMockData.stateList;
+    component.setStateDistrict({state: userLocationMockData.stateList[0], district: {}});
+    expect(component.selectedDistrict).toEqual(undefined);
+    expect(component.selectedState).toEqual(userLocationMockData.stateList[0]);
+  });
+
+  it('should set state and district', () => {
+    component.allStates = userLocationMockData.stateList;
+    component.allDistricts = userLocationMockData.districtList;
+    component.setStateDistrict({
+      state: userLocationMockData.stateList[0],
+      district: userLocationMockData.districtList[0]
+    });
+    expect(component.selectedDistrict).toEqual(userLocationMockData.districtList[0]);
+    expect(component.selectedState).toEqual(userLocationMockData.stateList[0]);
+  });
+
 });
