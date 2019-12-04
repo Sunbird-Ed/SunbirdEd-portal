8b6eeb19eb2d37985d9e4635bf87288fc4a7471f
santosh <santosh.mattikoppa@ilimi.in>
2019-12-04 10:06:00 +0530

    Issue #SB-16016 bug : added test cases for raise support ticket

--- src/app/client/projects/desktop/src/app/modules/offline/components/offline-report-issues/offline-report-issues.component.spec.ts
+++ src/app/client/projects/desktop/src/app/modules/offline/components/offline-report-issues/offline-report-issues.component.spec.ts
@@ -9,10 +9,12 @@ import { OfflineReportIssuesService } from '../../services';
 import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { By } from '@angular/platform-browser';
+import { ofline_report_issues_spect_data } from './offline-report-issues.component.spec.data';
 describe('OfflineReportIssuesComponent', () => {
   let component: OfflineReportIssuesComponent;
   let fixture: ComponentFixture<OfflineReportIssuesComponent>;
   let resourceServiceStub;
+  let apiData;
   class RouterStub {
     navigate = jasmine.createSpy('navigate');
   }
@@ -35,7 +37,17 @@ describe('OfflineReportIssuesComponent', () => {
           issueReportedSuccessfulySubNote: 'Note: Your report will be automatically sent to {instance} while online.',
           errorWhileGeneratingTicket: 'Unable to raise ticket. please try again after some times.'
         }
+      },
+      messages:
+      {
+        emsg: {
+          m0023: 'Unable to raise a ticket because you are not connected to the internet.'
+        }
       }
+    },
+    apiData ={
+      email: 'santoh@gmail.com',
+      description: 'fdsfsdf'
     };
     TestBed.configureTestingModule({
       declarations: [OfflineReportIssuesComponent],
@@ -124,4 +136,39 @@ describe('OfflineReportIssuesComponent', () => {
     expect(component.raiseSupportTicketInteractEdata).toBeDefined();
     expect(component.toasterService.error(resourceServiceStub.frmelmnts.lbl.errorWhileGeneratingTicket));
   });
+  fit('should throw network error an when unable raise ticket ', () => {
+    const offlineReportIssuesService = TestBed.get(OfflineReportIssuesService);
+    spyOn(offlineReportIssuesService, 'reportOtherIssue').and.returnValue(of('false'));
+    spyOn(component.toasterService, 'error').and.returnValue(throwError(resourceServiceStub.frmelmnts.lbl.errorWhileGeneratingTicket));
+    spyOn(component, 'setTelemetryData').and.callThrough();
+    component.setTelemetryData();
+    expect(component.setTelemetryData).toHaveBeenCalled();
+    expect(component.raiseSupportTicketInteractEdata).toBeDefined();
+    component.offlineReportIssuesService.reportOtherIssue(apiData).subscribe(res => {
+
+    }, error => {
+      expect(component.isDisplayLoader).toBeDefined();
+      expect(error['error']['params']['err']).toEqual(ofline_report_issues_spect_data.noNetworkError);
+      expect(component.toasterService.error).toHaveBeenCalledWith(resourceServiceStub.messages.emsg.m0023);
+    });
+
+  });
+  fit('should throw fresh desk error an when unable raise ticket ', () => {
+    const offlineReportIssuesService = TestBed.get(OfflineReportIssuesService);
+    spyOn(offlineReportIssuesService, 'reportOtherIssue').and.returnValue(of('false'));
+    spyOn(component, 'setTelemetryData').and.callThrough();
+    spyOn(component.toasterService, 'error').and.returnValue(throwError(resourceServiceStub.messages.emsg.m0023));
+    component.setTelemetryData();
+    expect(component.setTelemetryData).toHaveBeenCalled();
+    expect(component.raiseSupportTicketInteractEdata).toBeDefined();
+    component.offlineReportIssuesService.reportOtherIssue(apiData).subscribe(res => {
+
+    }, error => {
+      expect(component.isDisplayLoader).toBeDefined();
+      expect(error['error']['params']['err']).toEqual(ofline_report_issues_spect_data.freshDeskError);
+      expect(component.toasterService.error).toHaveBeenCalledWith(resourceServiceStub.frmelmnts.lbl.errorWhileGeneratingTicket);
+    });
+
+  });
+
 });
