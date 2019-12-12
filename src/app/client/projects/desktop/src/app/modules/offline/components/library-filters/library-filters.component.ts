bbc4225be12424f69fdb4b60a41d834f8db612ec
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-12-12 11:27:08 +0530

    Issue #SB-15780 fix: Lint issue fixed

--- src/app/client/projects/desktop/src/app/modules/offline/components/library-filters/library-filters.component.ts
+++ src/app/client/projects/desktop/src/app/modules/offline/components/library-filters/library-filters.component.ts
@@ -11,244 +11,244 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 interface IFilters {
-    board: string[];
-    medium?: string[];
-    gradeLevel?: string[];
+  board: string[];
+  medium?: string[];
+  gradeLevel?: string[];
 }
 
 interface IFilterChange {
-    filters: IFilters;
-    channelId: string;
+  filters: IFilters;
+  channelId: string;
 }
 
 @Component({
-    selector: 'app-library-filters',
-    templateUrl: './library-filters.component.html',
-    styleUrls: ['./library-filters.component.scss']
+  selector: 'app-library-filters',
+  templateUrl: './library-filters.component.html',
+  styleUrls: ['./library-filters.component.scss']
 })
 export class LibraryFiltersComponent implements OnInit, OnDestroy {
 
-    selectedBoard: any;
-    selectedMediumIndex: number[] = [];
-    selectedClassIndex: number[] = [];
+  selectedBoard: any;
+  selectedMediumIndex: number[] = [];
+  selectedClassIndex: number[] = [];
 
-    boards: any[] = [];
-    mediums: any[] = [];
-    classes: any[] = [];
+  boards: any[] = [];
+  mediums: any[] = [];
+  classes: any[] = [];
 
-    mediumLayout: LibraryFiltersLayout = LibraryFiltersLayout.SQUARE;
-    classLayout: LibraryFiltersLayout = LibraryFiltersLayout.ROUND;
+  mediumLayout: LibraryFiltersLayout = LibraryFiltersLayout.SQUARE;
+  classLayout: LibraryFiltersLayout = LibraryFiltersLayout.ROUND;
 
-    frameworkCategories: any;
-    userDetails: any;
-    hashTagId: string;
-    public unsubscribe$ = new Subject<void>();
+  frameworkCategories: any;
+  userDetails: any;
+  hashTagId: string;
+  public unsubscribe$ = new Subject<void>();
 
-    @Input() selectedFilters;
-    @Output() filterChange: EventEmitter<IFilterChange> = new EventEmitter();
+  @Input() selectedFilters;
+  @Output() filterChange: EventEmitter<IFilterChange> = new EventEmitter();
 
-    constructor(
-        public resourceService: ResourceService,
-        public frameworkService: FrameworkService,
-        private orgDetailsService: OrgDetailsService,
-        private channelService: ChannelService,
-        private onboardingService: OnboardingService,
-        public router: Router
-    ) { }
+  constructor(
+    public resourceService: ResourceService,
+    public frameworkService: FrameworkService,
+    private orgDetailsService: OrgDetailsService,
+    private channelService: ChannelService,
+    private onboardingService: OnboardingService,
+    public router: Router
+  ) { }
 
-    ngOnInit() {
-        this.orgDetailsService.getCustodianOrg()
+  ngOnInit() {
+    this.orgDetailsService.getCustodianOrg()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(data => {
+        this.hashTagId = _.get(data, 'result.response.value');
+        this.userDetails = this.onboardingService.userData;
+        this.setBoard();
+      });
+  }
+
+  setBoard() {
+    this.channelService.getFrameWork(this.hashTagId)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(orgDetails => {
+        this.boards = _.get(orgDetails, 'result.channel.frameworks');
+
+        if (this.boards) {
+          if (_.get(this.selectedFilters, 'board[0]')) {
+            this.selectedBoard = this.boards.find((board) => board.name === this.selectedFilters.board[0]);
+          } else {
+            this.selectedBoard = this.boards.find((board) => board.name === this.userDetails.framework.board);
+          }
+        }
+
+        if (this.selectedBoard) {
+          this.frameworkService.getFrameworkCategories(_.get(this.selectedBoard, 'identifier'))
             .pipe(takeUntil(this.unsubscribe$))
-            .subscribe(data => {
-                this.hashTagId = _.get(data, 'result.response.value');
-                this.userDetails = this.onboardingService.userData;
-                this.setBoard();
-            });
-    }
-
-    setBoard() {
-        this.channelService.getFrameWork(this.hashTagId)
-            .pipe(takeUntil(this.unsubscribe$))
-            .subscribe(orgDetails => {
-                this.boards = _.get(orgDetails, 'result.channel.frameworks');
-
-                if (this.boards) {
-                    if (_.get(this.selectedFilters, 'board[0]')) {
-                        this.selectedBoard = this.boards.find((board) => board.name === this.selectedFilters.board[0]);
-                    } else {
-                        this.selectedBoard = this.boards.find((board) => board.name === this.userDetails.framework.board);
-                    }
+            .subscribe((res) => {
+              if (res && _.get(res, 'result.framework.categories')) {
+                this.frameworkCategories = _.get(res, 'result.framework.categories');
+                if (_.get(this.selectedFilters, 'board[0]')) {
+                  this.setFilters(false);
+                } else {
+                  this.setFilters(true);
                 }
-
-                if (this.selectedBoard) {
-                    this.frameworkService.getFrameworkCategories(_.get(this.selectedBoard, 'identifier'))
-                        .pipe(takeUntil(this.unsubscribe$))
-                        .subscribe((res) => {
-                            if (res && _.get(res, 'result.framework.categories')) {
-                                this.frameworkCategories = _.get(res, 'result.framework.categories');
-                                if (_.get(this.selectedFilters, 'board[0]')) {
-                                    this.setFilters(false);
-                                } else {
-                                    this.setFilters(true);
-                                }
-                            }
-                        });
-                }
+              }
             });
-    }
-
-    setFilters(showDefault?) {
-        this.resetFilters();
-        const framework = this.userDetails.framework;
-
-        this.frameworkCategories.forEach(element => {
-            switch (element.code) {
-                case 'medium':
-                    this.mediums = element.terms.map(medium => medium.name);
-
-                    let mediumIndex;
-                    if (_.get(this.selectedFilters, 'medium[0]')) {
-                        mediumIndex = this.mediums.findIndex((medium) => medium === this.selectedFilters.medium[0]);
-                    } else if (showDefault) {
-                        mediumIndex = this.mediums.findIndex(medium => framework.medium.includes(medium));
-                    }
-
-                    if (_.isNumber(mediumIndex)) {
-                        this.selectedMediumIndex.push(mediumIndex);
-                    }
-                    break;
-                case 'gradeLevel':
-                    this.classes = element.terms.map(gradeLevel => gradeLevel.name);
-                    let classIndex;
-
-                    if (_.get(this.selectedFilters, 'gradeLevel[0]')) {
-                        classIndex = this.classes.findIndex((classElement) =>
-                            classElement === this.selectedFilters.gradeLevel[0]);
-                    } else if (showDefault) {
-                        classIndex = this.classes.findIndex(value => framework.gradeLevel.includes(value));
-                    }
-
-                    if (_.isNumber(classIndex)) {
-                        this.selectedClassIndex.push(classIndex);
-                    }
-                    break;
-            }
-        });
-
-        this.triggerFilterChangeEvent();
-    }
-
-    onBoardChange(option) {
-        this.resetFilters();
-        this.frameworkService.getFrameworkCategories(_.get(option, 'identifier'))
-            .pipe(takeUntil(this.unsubscribe$))
-            .subscribe((data) => {
-                if (data && _.get(data, 'result.framework.categories')) {
-                    this.frameworkCategories = _.get(data, 'result.framework.categories');
-                    this.setFilters(false);
-                }
-            });
-    }
-
-    resetFilters() {
-        this.mediums = [];
-        this.classes = [];
-        this.selectedClassIndex = [];
-        this.selectedMediumIndex = [];
-
-    }
-
-    applyFilters(event, type) {
-        this.getSelectedFilters();
-
-        if (type === 'medium') {
-            this.selectedMediumIndex = [event.data.index];
-            this.selectedClassIndex = [];
-        } else if (type === 'class') {
-            this.selectedClassIndex = [event.data.index];
         }
+      });
+  }
 
-        this.triggerFilterChangeEvent();
-    }
+  setFilters(showDefault?) {
+    this.resetFilters();
+    const framework = this.userDetails.framework;
 
-    getSelectedFilters() {
-        const filters: any = {};
-        filters.board = [this.selectedBoard.name];
+    this.frameworkCategories.forEach(element => {
+      switch (element.code) {
+        case 'medium':
+          this.mediums = element.terms.map(medium => medium.name);
 
-        if (this.selectedMediumIndex.length) {
-            filters.appliedFilters = true;
-            filters.medium = [this.mediums[this.selectedMediumIndex[0]]];
+          let mediumIndex;
+          if (_.get(this.selectedFilters, 'medium[0]')) {
+            mediumIndex = this.mediums.findIndex((medium) => medium === this.selectedFilters.medium[0]);
+          } else if (showDefault) {
+            mediumIndex = this.mediums.findIndex(medium => framework.medium.includes(medium));
+          }
+
+          if (_.isNumber(mediumIndex)) {
+            this.selectedMediumIndex.push(mediumIndex);
+          }
+          break;
+        case 'gradeLevel':
+          this.classes = element.terms.map(gradeLevel => gradeLevel.name);
+          let classIndex;
+
+          if (_.get(this.selectedFilters, 'gradeLevel[0]')) {
+            classIndex = this.classes.findIndex((classElement) =>
+              classElement === this.selectedFilters.gradeLevel[0]);
+          } else if (showDefault) {
+            classIndex = this.classes.findIndex(value => framework.gradeLevel.includes(value));
+          }
+
+          if (_.isNumber(classIndex)) {
+            this.selectedClassIndex.push(classIndex);
+          }
+          break;
+      }
+    });
+
+    this.triggerFilterChangeEvent();
+  }
+
+  onBoardChange(option) {
+    this.resetFilters();
+    this.frameworkService.getFrameworkCategories(_.get(option, 'identifier'))
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((data) => {
+        if (data && _.get(data, 'result.framework.categories')) {
+          this.frameworkCategories = _.get(data, 'result.framework.categories');
+          this.setFilters(false);
         }
+      });
+  }
 
-        if (this.selectedClassIndex.length) {
-            filters.appliedFilters = true;
-            filters.gradeLevel = [this.classes[this.selectedClassIndex[0]]];
-        }
+  resetFilters() {
+    this.mediums = [];
+    this.classes = [];
+    this.selectedClassIndex = [];
+    this.selectedMediumIndex = [];
 
-        return filters;
+  }
+
+  applyFilters(event, type) {
+    this.getSelectedFilters();
+
+    if (type === 'medium') {
+      this.selectedMediumIndex = [event.data.index];
+      this.selectedClassIndex = [];
+    } else if (type === 'class') {
+      this.selectedClassIndex = [event.data.index];
     }
 
-    triggerFilterChangeEvent() {
-        const data: IFilterChange = {
-            filters: this.getSelectedFilters(),
-            channelId: this.hashTagId
-        };
-        this.filterChange.emit(data);
+    this.triggerFilterChangeEvent();
+  }
+
+  getSelectedFilters() {
+    const filters: any = {};
+    filters.board = [this.selectedBoard.name];
+
+    if (this.selectedMediumIndex.length) {
+      filters.appliedFilters = true;
+      filters.medium = [this.mediums[this.selectedMediumIndex[0]]];
     }
 
-
-    getBoardInteractEdata(selectedBoard) {
-        const selectBoardInteractEdata: IInteractEventEdata = {
-            id: 'board-select-button',
-            type: 'click',
-            pageid: this.router.url.split('/')[1] || 'library'
-        };
-
-        if (selectedBoard) {
-            selectBoardInteractEdata['extra'] = {
-                board: selectedBoard.name
-            };
-        }
-
-        return selectBoardInteractEdata;
+    if (this.selectedClassIndex.length) {
+      filters.appliedFilters = true;
+      filters.gradeLevel = [this.classes[this.selectedClassIndex[0]]];
     }
 
+    return filters;
+  }
 
-    getMediumInteractEdata() {
-        const selectMediumInteractEdata: IInteractEventEdata = {
-            id: 'medium-select-button',
-            type: 'click',
-            pageid: this.router.url.split('/')[1] || 'library'
-        };
+  triggerFilterChangeEvent() {
+    const data: IFilterChange = {
+      filters: this.getSelectedFilters(),
+      channelId: this.hashTagId
+    };
+    this.filterChange.emit(data);
+  }
 
-        if (this.selectedMediumIndex.length) {
-            selectMediumInteractEdata['extra'] = {
-                medium: [this.mediums[this.selectedMediumIndex[0]]]
-            };
-        }
 
-        return selectMediumInteractEdata;
+  getBoardInteractEdata(selectedBoard) {
+    const selectBoardInteractEdata: IInteractEventEdata = {
+      id: 'board-select-button',
+      type: 'click',
+      pageid: this.router.url.split('/')[1] || 'library'
+    };
+
+    if (selectedBoard) {
+      selectBoardInteractEdata['extra'] = {
+        board: selectedBoard.name
+      };
     }
 
-    getClassInteractEdata() {
-        const selectClassInteractEdata: IInteractEventEdata = {
-            id: 'grade-level-select-button',
-            type: 'click',
-            pageid: this.router.url.split('/')[1] || 'library'
-        };
+    return selectBoardInteractEdata;
+  }
 
-        if (this.selectedClassIndex.length) {
-            selectClassInteractEdata['extra'] = {
-                gradeLevel: [this.classes[this.selectedClassIndex[0]]]
-            };
-        }
 
-        return selectClassInteractEdata;
+  getMediumInteractEdata() {
+    const selectMediumInteractEdata: IInteractEventEdata = {
+      id: 'medium-select-button',
+      type: 'click',
+      pageid: this.router.url.split('/')[1] || 'library'
+    };
+
+    if (this.selectedMediumIndex.length) {
+      selectMediumInteractEdata['extra'] = {
+        medium: [this.mediums[this.selectedMediumIndex[0]]]
+      };
     }
 
-    ngOnDestroy() {
-        this.unsubscribe$.next();
-        this.unsubscribe$.complete();
+    return selectMediumInteractEdata;
+  }
+
+  getClassInteractEdata() {
+    const selectClassInteractEdata: IInteractEventEdata = {
+      id: 'grade-level-select-button',
+      type: 'click',
+      pageid: this.router.url.split('/')[1] || 'library'
+    };
+
+    if (this.selectedClassIndex.length) {
+      selectClassInteractEdata['extra'] = {
+        gradeLevel: [this.classes[this.selectedClassIndex[0]]]
+      };
     }
+
+    return selectClassInteractEdata;
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
 
