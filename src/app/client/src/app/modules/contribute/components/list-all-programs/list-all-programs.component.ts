21171620b33613f999b49c43db0011eac9a9ca4a
ravinder kumar <yravinderkumar33@gmail.com>
2019-12-19 11:15:13 +0530

    Issue #SB-16428 feat : Contribution programs portal readiness to scale upto 30 - 50 instances (merge with SunbirdEd)

--- src/app/client/src/app/modules/contribute/components/list-all-programs/list-all-programs.component.ts
+++ src/app/client/src/app/modules/contribute/components/list-all-programs/list-all-programs.component.ts
@@ -1,6 +1,7 @@
 import { ResourceService } from '@sunbird/shared';
-import {  ProgramsService } from '@sunbird/core';
+import { ProgramsService } from '@sunbird/core';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-all-programs',
@@ -10,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class ListAllProgramsComponent implements OnInit {
 
   public programsList$;
-  constructor(private programsService: ProgramsService, public resourceService: ResourceService) { }
+  constructor(private programsService: ProgramsService, public resourceService: ResourceService, private router: Router) { }
 
   ngOnInit() {
     this.programsList$ = this.programsService.programsList$;
--- src/app/client/src/app/modules/contribute/contribute-routing.module.ts
+++ src/app/client/src/app/modules/contribute/contribute-routing.module.ts
@@ -3,11 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{
-  path: '', component: ListAllProgramsComponent, children: [
-    {
-      path: 'programs', component: ListAllProgramsComponent
-    }
-  ]
+  path: '', component: ListAllProgramsComponent
 }];
 
 @NgModule({
