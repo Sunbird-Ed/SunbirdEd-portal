7fe701e847832dbd4869375bd4c439ab858d9480
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2019-12-27 11:13:40 +0530

    Issue #SB-16698: Generate theme object based on questionList changes done

--- src/app/client/src/app/modules/cbse-program/services/cbse-program/data.ts
+++ src/app/client/src/app/modules/cbse-program/services/cbse-program/data.ts
@@ -85,155 +85,162 @@ export let themeObject = {
       {
         'id': 'org.ekstep.questionunit.renderer.audioicon',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/assets/audio-icon.png',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/assets/audio-icon.png',
         'type': 'image'
       },
       {
         'id': 'org.ekstep.questionunit.renderer.downarrow',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/assets/down_arrow.png',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/assets/down_arrow.png',
         'type': 'image'
       },
       {
         'id': 'org.ekstep.questionunit_js',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/components/js/components.js',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/components/js/components.js',
         'type': 'js'
       },
       {
         'id': 'org.ekstep.questionunit_css',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/components/css/components.css',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/components/css/components.css',
         'type': 'css'
       },
       {
         'id': 'org.ekstep.questionunit',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/plugin.js',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/plugin.js',
         'type': 'plugin'
       },
       {
         'id': 'org.ekstep.questionunit_manifest',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/manifest.json',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/manifest.json',
         'type': 'json'
       },
       {
           'id': 'org.sunbird.questionunit.quml_manifest',
           'plugin': 'org.sunbird.questionunit.quml',
-          'ver': '1.0',
-          'src': '/content-plugins/org.sunbird.questionunit.quml-1.0/manifest.json',
+          'ver': '1.1',
+          'src': '/content-plugins/org.sunbird.questionunit.quml-1.1/manifest.json',
           'type': 'json'
       },
       {
           'id': 'org.sunbird.questionunit.quml',
           'plugin': 'org.sunbird.questionunit.quml',
-          'ver': '1.0',
-          'src': '/content-plugins/org.sunbird.questionunit.quml-1.0/renderer/plugin.js',
+          'ver': '1.1',
+          'src': '/content-plugins/org.sunbird.questionunit.quml-1.1/renderer/plugin.js',
           'type': 'plugin'
       },
       {
+        'id': 'org.sunbird.questionunit.quml',
+        'plugin': 'org.sunbird.questionunit.quml',
+        'ver': '1.1',
+        'src': '/content-plugins/org.sunbird.questionunit.quml-1.1/renderer/utils/quml_feedback_popup.js',
+        'type': 'js'
+    },
+      {
           'id': 'org.sunbird.questionunit.quml_css',
           'plugin': 'org.sunbird.questionunit.quml',
-          'ver': '1.0',
-          'src': '/content-plugins/org.sunbird.questionunit.quml-1.0/renderer/styles/style.css',
+          'ver': '1.1',
+          'src': '/content-plugins/org.sunbird.questionunit.quml-1.1/renderer/styles/style.css',
           'type': 'css'
       },
       {
         'id': '4b2b66a2-9c5b-42d5-891d-ec0c3d1d516c',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/katex.min.js',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/katex.min.js',
         'type': 'js'
       },
       {
         'id': '7e02c56a-c048-444d-bb2a-d4b854723adc',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/katex.min.css',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/katex.min.css',
         'type': 'css'
       },
       {
         'id': '04ad980a-88ac-4c55-a597-266e42240670',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_main-bold.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_main-bold.ttf',
         'type': 'js'
       },
       {
         'id': '75ab3bcd-2c52-47b0-8b46-b635256ce06a',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_main-bolditalic.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_main-bolditalic.ttf',
         'type': 'js'
       },
       {
         'id': '098cc69e-8658-483e-8597-8c392a1b3545',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_main-italic.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_main-italic.ttf',
         'type': 'js'
       },
       {
         'id': '102d1165-9544-480b-9ec1-1cb83937e650',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_main-regular.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_main-regular.ttf',
         'type': 'js'
       },
       {
         'id': '3bc5dce9-51ca-4a59-a0d9-d87d47c44670',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_math-bolditalic.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_math-bolditalic.ttf',
         'type': 'js'
       },
       {
         'id': 'a95bb693-d688-4e7a-a9bc-08e0cb6d1d62',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_math-italic.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_math-italic.ttf',
         'type': 'js'
       },
       {
         'id': '15757105-9023-4773-ba2a-8e619a0dc3a6',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_math-regular.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_math-regular.ttf',
         'type': 'js'
       },
       {
         'id': '5b1a9e58-8b7e-4295-a90d-593dc831262a',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_size1-regular.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_size1-regular.ttf',
         'type': 'js'
       },
       {
         'id': 'bc6ee547-9db0-480e-b298-b5509d43207a',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_size2-regular.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_size2-regular.ttf',
         'type': 'js'
       },
       {
         'id': '35b0245e-d7d3-4ac0-9cac-6c2c328c8948',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_size3-regular.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_size3-regular.ttf',
         'type': 'js'
       },
       {
         'id': '80a19118-48eb-4859-9b0c-576a90a01148',
         'plugin': 'org.ekstep.questionunit',
-        'ver': '1.0',
-        'src': '/content-plugins/org.ekstep.questionunit-1.0/renderer/libs/katex/fonts/katex_size4-regular.ttf',
+        'ver': '1.1',
+        'src': '/content-plugins/org.ekstep.questionunit-1.1/renderer/libs/katex/fonts/katex_size4-regular.ttf',
         'type': 'js'
       }
     ]
@@ -248,7 +255,7 @@ export let themeObject = {
       },
       {
         'id': 'org.ekstep.questionunit',
-        'ver': '1.0',
+        'ver': '1.1',
         'type': 'plugin',
         'depends': ''
       },
@@ -266,7 +273,7 @@ export let themeObject = {
       },
       {
         'id': 'org.sunbird.questionunit.quml',
-        'ver': '1.0',
+        'ver': '1.1',
         'type': 'plugin',
         'depends': 'org.ekstep.questionunit'
       }
@@ -310,7 +317,7 @@ export let questionObject = {
   'id': '',
   'type': 'quml',
   'pluginId': 'org.sunbird.questionunit.quml',
-  'pluginVer': '1.0',
+  'pluginVer': '1.1',
   'templateId': 'qumltemplate',
   'data': {
     '__cdata': {}
