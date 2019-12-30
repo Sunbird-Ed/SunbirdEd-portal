d4a47eb8c40f91687ee3fd493338d9ee16116a3c
santosh <santosh.mattikoppa@ilimi.in>
2019-12-30 18:32:56 +0530

    Issue #SB-16517 feat: content metedata component api integration and test cases

---
+++ src/app/client/projects/desktop/src/app/modules/content-consumption/components/content-metadata/content-metadata.component.interface.ts
@@ -0,0 +1,14 @@
+export interface ContentDetailsInterface {
+    description: string;
+    board: string;
+    medium: string;
+    gradeLevel: Array<string>;
+    subject: string;
+    license: string;
+    copyright: string;
+    creator: string;
+    author: string;
+    createdOn: string;
+    keywords: Array<string>;
+    resourceType: string;
+}
