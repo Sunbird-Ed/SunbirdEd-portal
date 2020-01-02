5cee1ffb7c5e0175eb331706960dacee67628d90
santosh <santosh.mattikoppa@ilimi.in>
2020-01-02 15:23:55 +0530

    Issue #SB-16517 feat: content metedata component api integration and removed custom element schema

--- src/app/client/projects/desktop/src/app/modules/content-consumption/content-consumption-routing.module.ts
+++ src/app/client/projects/desktop/src/app/modules/content-consumption/content-consumption-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-
+import { ResourcePageComponent, TocPageComponent } from './components';
+const routes: Routes = [
+  {
+    path: 'content/:contentId', component: ResourcePageComponent, data: {
+      telemetry: {
+        env: 'player-page', pageid: 'play-content', type: 'view', subtype: 'paginate'
+      },
+    },
+  },
+  {
+    path: 'collection/:collectionId', component: TocPageComponent, data: {
+      telemetry: {
+        env: 'player-page', pageid: 'play-collection', type: 'view', subtype: 'paginate'
+      },
+    }
+  }
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
