71c416798b19608a3f217c1b4d451f8fcc7833dd
santosh <santosh.mattikoppa@ilimi.in>
2020-01-02 17:56:48 +0530

    Issue #SB-16517 feat: api integration for resource component for content

--- src/app/client/projects/desktop/src/app/modules/content-consumption/components/resource-page/resource-page.component.spec.ts
+++ src/app/client/projects/desktop/src/app/modules/content-consumption/components/resource-page/resource-page.component.spec.ts
@@ -1,16 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResourcePageComponent } from './resource-page.component';
-
-xdescribe('ResourcePageComponent', () => {
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PublicPlayerService } from '@sunbird/public';
+import { SharedModule } from '@sunbird/shared';
+import { of } from 'rxjs';
+import {resourceData} from './resource-page.component.spec.data';
+import { TelemetryModule } from '@sunbird/telemetry';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+describe('ResourcePageComponent', () => {
   let component: ResourcePageComponent;
   let fixture: ComponentFixture<ResourcePageComponent>;
-
+  class RouterStub {
+    navigate = jasmine.createSpy('navigate');
+  }
+  const ActivatedRouteStub = {
+    'params': of({ 'contentId': resourceData.contentId }),
+    snapshot: {
+      params: {
+        'contentId': resourceData.contentId
+      },
+      data: {
+        telemetry: {
+          env: 'player-page', pageid: 'play-content', type: 'view', subtype: 'paginate'
+        }
+      }
+    }
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResourcePageComponent ]
+      declarations: [ResourcePageComponent],
+      imports: [HttpClientTestingModule,  TelemetryModule.forRoot(), SharedModule.forRoot()],
+      providers: [
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useValue: ActivatedRouteStub },
+        PublicPlayerService,
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +50,28 @@ xdescribe('ResourcePageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should get content id from activated route', () => {
+    fixture.detectChanges();
+    expect(component.contentId).toBe(resourceData.contentId);
+  });
+  it('should call getContent method ', () => {
+    const publicService = TestBed.get(PublicPlayerService);
+    spyOn(component, 'getContent');
+    publicService.getContent(resourceData.contentId, resourceData.PublicPlayer.contentApiQueryParams).subscribe(data => {
+      expect(data).toBe(resourceData.content);
+      expect(component.contentDetails).toHaveBeenCalledWith(resourceData.content.result.content);
+      spyOn(component, 'setTelemetryData').and.callThrough();
+      component.setTelemetryData();
+      expect(component.setTelemetryData).toHaveBeenCalled();
+    });
+  });
+  it('should get content config details ', () => {
+    const publicService = TestBed.get(PublicPlayerService);
+    spyOn(publicService, 'getConfig').and.returnValue(resourceData.configDetails);
+    component.contentId = resourceData.contentId;
+    component.contentDetails = resourceData.content.result.content;
+    component.getContentConfigDetails();
+    expect(component.playerConfig).toEqual(resourceData.configDetails);
   });
 });
--- src/app/client/projects/desktop/src/app/modules/content-consumption/components/resource-page/resource-page.component.ts
+++ src/app/client/projects/desktop/src/app/modules/content-consumption/components/resource-page/resource-page.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PublicPlayerService } from '@sunbird/public';
+import {
+  ConfigService, NavigationHelperService, PlayerConfig
+} from '@sunbird/shared';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import * as _ from 'lodash-es';
+import { IImpressionEventInput } from '@sunbird/telemetry';
 
 @Component({
   selector: 'app-resource-page',
   templateUrl: './resource-page.component.html',
   styleUrls: ['./resource-page.component.scss']
 })
-export class ResourcePageComponent implements OnInit {
-
-  constructor() { }
+export class ResourcePageComponent implements OnInit, OnDestroy {
+  public unsubscribe$ = new Subject<void>();
+  contentId: any;
+  contentDetails: any;
+  playerConfig: PlayerConfig;
+  public telemetryImpression: IImpressionEventInput;
+  constructor(public activatedRoute: ActivatedRoute,
+    public playerService: PublicPlayerService,
+    public configService: ConfigService,
+    public router: Router,
+    public navigationHelperService: NavigationHelperService
+  ) { }
 
   ngOnInit() {
+    this.getContentIdFromRoute();
+  }
+  getContentIdFromRoute() {
+    this.activatedRoute.params.pipe(
+      takeUntil(this.unsubscribe$))
+      .subscribe(params => {
+        if (params.contentId) {
+          this.contentId = params.contentId;
+          this.getContent();
+        }
+      });
+  }
+  getContent() {
+    const params = { params: this.configService.appConfig.PublicPlayer.contentApiQueryParams };
+    this.playerService.getContent(this.contentId, params)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(response => {
+        this.contentDetails = _.get(response, 'result.content');
+        this.getContentConfigDetails();
+        this.setTelemetryData();
+      });
+  }
+  getContentConfigDetails() {
+    const contentDetails = {
+      contentId: this.contentId,
+      contentData: this.contentDetails
+    };
+    this.playerConfig = this.playerService.getConfig(contentDetails);
+  }
+  setTelemetryData() {
+    this.telemetryImpression = {
+      context: {
+        env: this.activatedRoute.snapshot.data.telemetry.env
+      },
+      edata: {
+        type: this.activatedRoute.snapshot.data.telemetry.type,
+        pageid: this.activatedRoute.snapshot.data.telemetry.pageid,
+        uri: this.router.url,
+        subtype: this.activatedRoute.snapshot.data.telemetry.subtype,
+        duration: this.navigationHelperService.getPageLoadTime()
+      }
+    };
   }
 
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
