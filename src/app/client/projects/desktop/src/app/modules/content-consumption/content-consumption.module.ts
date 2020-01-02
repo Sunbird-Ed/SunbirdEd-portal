eda58582cde65d539eb0f25c3ad70cda8bb5f782
santosh <santosh.mattikoppa@ilimi.in>
2020-01-02 14:46:55 +0530

    issue #SB-16517 feat: content player integration and module file

--- src/app/client/projects/desktop/src/app/modules/content-consumption/content-consumption.module.ts
+++ src/app/client/projects/desktop/src/app/modules/content-consumption/content-consumption.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContentConsumptionRoutingModule } from './content-consumption-routing.module';
@@ -6,6 +6,9 @@ import { ContentPlayerComponent, ContentMetadataComponent, ResourcePageComponent
   ContentHeaderComponent
  } from './components';
 import { SuiAccordionModule } from 'ng2-semantic-ui';
+import { OrderModule } from 'ngx-order-pipe';
+import { SharedModule } from '@sunbird/shared';
+import { TelemetryModule } from '@sunbird/telemetry';
 
 
 @NgModule({
@@ -18,9 +21,13 @@ import { SuiAccordionModule } from 'ng2-semantic-ui';
   ],
   imports: [
     CommonModule,
+    OrderModule,
+    SharedModule,
+    TelemetryModule,
     ContentConsumptionRoutingModule,
     SuiAccordionModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [
     ContentPlayerComponent, ContentMetadataComponent
   ]
