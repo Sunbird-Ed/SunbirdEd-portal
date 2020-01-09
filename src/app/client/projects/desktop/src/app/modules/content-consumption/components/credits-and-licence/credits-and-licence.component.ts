a41fba6e4130d96756aea64c6ba929a6422aebaf
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-01-09 19:03:30 +0530

    Issue #SB-17149 fix: Fixed continuos loading on view more page

--- src/app/client/projects/desktop/src/app/modules/content-consumption/components/credits-and-licence/credits-and-licence.component.ts
+++ src/app/client/projects/desktop/src/app/modules/content-consumption/components/credits-and-licence/credits-and-licence.component.ts
@@ -1,20 +1,26 @@
-import { Component, Input, ElementRef, ViewChild } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 import { ResourceService } from '@sunbird/shared';
+import * as _ from 'lodash-es';
 
 @Component({
   selector: 'app-credits-and-licence',
   templateUrl: './credits-and-licence.component.html',
   styleUrls: ['./credits-and-licence.component.scss']
 })
-export class CreditsAndLicenceComponent {
+export class CreditsAndLicenceComponent implements OnInit, AfterViewInit {
 
   @Input() contentData;
   @ViewChild('licence') licenceElement: ElementRef;
 
+  instance: string;
+
   constructor(public resourceService: ResourceService) { }
 
-  ngAfterViewInit() {
+  ngOnInit() {
+    this.instance = _.upperCase(this.resourceService.instance);
+  }
 
+  ngAfterViewInit() {
     if (this.contentData && this.contentData.license) {
       const urlRegex = /(https?:\/\/[^\s]+)/g;
       this.licenceElement.nativeElement.innerHTML = this.contentData.license.replace(urlRegex, '<a href="">$1</a>');
