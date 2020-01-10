b0f8c3a266049dd137d671d6b0c6423bafdf0648
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2020-01-10 20:10:47 +0530

    Issue #15452 fix: Default question creation workflow for VSA, SA, LA and MCQ

--- src/app/client/src/app/modules/cbse-program/components/chapter-list/chapter-list.component.ts
+++ src/app/client/src/app/modules/cbse-program/components/chapter-list/chapter-list.component.ts
@@ -164,7 +164,12 @@ export class ChapterListComponent implements OnInit, OnChanges, OnDestroy {
       practiceQuestionSetComponentInput: {
         sessionContext: this.sessionContext,
         templateDetails: this.templateDetails,
-        role: this.role
+        unitIdentifier: this.unitIdentifier,
+        role: this.role,
+        selectedSharedContext: this.selectedSharedContext,
+        contentIdentifier: this.contentId,
+        action: action,
+        programContext: _.get(this.chapterListComponentInput, 'programContext')
       }
     };
   }
