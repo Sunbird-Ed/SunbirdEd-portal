ae2d3cd3a241a4204da565028931e43aeaf90edf
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-01-13 18:15:26 +0530

    Issue #SB-17162 fix: Test cases fixed

--- src/app/client/src/app/modules/shared-feature/components/user-location/user-location.component.spec.ts
+++ src/app/client/src/app/modules/shared-feature/components/user-location/user-location.component.spec.ts
@@ -1,8 +1,8 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {SuiModule} from 'ng2-semantic-ui';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {TelemetryModule} from '@sunbird/telemetry';
-import {UserLocationComponent} from './user-location.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SuiModule } from 'ng2-semantic-ui';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TelemetryModule } from '@sunbird/telemetry';
+import { UserLocationComponent } from './user-location.component';
 import {
   ResourceService,
   ToasterService,
@@ -10,12 +10,12 @@ import {
   BrowserCacheTtlService,
   NavigationHelperService
 } from '@sunbird/shared';
-import {ProfileService} from '@sunbird/profile';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {CacheService} from 'ng2-cache-service';
-import {DeviceDetectorService} from 'ngx-device-detector';
-import {RouterTestingModule} from '@angular/router/testing';
-import {userLocationMockData} from './user-location.component.spec.data';
+import { ProfileService } from '@sunbird/profile';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CacheService } from 'ng2-cache-service';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { RouterTestingModule } from '@angular/router/testing';
+import { userLocationMockData } from './user-location.component.spec.data';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { SharedModule } from '@sunbird/shared';
 
@@ -27,7 +27,13 @@ describe('UserLocationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [SuiModule, FormsModule, ReactiveFormsModule, HttpClientTestingModule, TelemetryModule.forRoot(), RouterTestingModule,
+      imports: [
+        SuiModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        TelemetryModule.forRoot(),
+        RouterTestingModule,
         SharedModule.forRoot()],
       declarations: [UserLocationComponent],
       providers: [ResourceService, ToasterService, ProfileService, ConfigService, CacheService, BrowserCacheTtlService,
@@ -52,7 +58,7 @@ describe('UserLocationComponent', () => {
     spyOn(component, 'setState');
     spyOn(component, 'setDistrict');
     spyOn(component, 'onStateChange');
-    component.setStateDistrict({state: {}, district: {}});
+    component.setStateDistrict({ state: {}, district: {} });
     expect(component.setState).toHaveBeenCalled();
     expect(component.setDistrict).toHaveBeenCalled();
     expect(component.onStateChange).toHaveBeenCalled();
@@ -75,7 +81,7 @@ describe('UserLocationComponent', () => {
     spyOn(component, 'setState');
     spyOn(component, 'setDistrict');
     spyOn(component, 'onStateChange');
-    component.setStateDistrict({state: null, district: null});
+    component.setStateDistrict({ state: null, district: null });
     expect(component.setState).toHaveBeenCalledTimes(0);
     expect(component.setDistrict).toHaveBeenCalledTimes(0);
     expect(component.onStateChange).toHaveBeenCalled();
@@ -85,7 +91,7 @@ describe('UserLocationComponent', () => {
     spyOn(component, 'setState');
     spyOn(component, 'setDistrict');
     spyOn(component, 'onStateChange');
-    component.setStateDistrict({state: undefined, district: undefined});
+    component.setStateDistrict({ state: undefined, district: undefined });
     expect(component.setState).toHaveBeenCalledTimes(0);
     expect(component.setDistrict).toHaveBeenCalledTimes(0);
     expect(component.onStateChange).toHaveBeenCalled();
