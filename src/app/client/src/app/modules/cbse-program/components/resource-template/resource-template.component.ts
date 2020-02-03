bc865efbfb2b07a9a4ed725ea1c87c46c964e295
venkanagouda-k <shashi.kallanagoudar@gmail.com>
2020-02-03 17:17:18 +0530

    Issue #SB-17542 feat: telemetry events are corrected by adding proper roll-up object After removing code duplicate

--- src/app/client/src/app/modules/cbse-program/components/resource-template/resource-template.component.ts
+++ src/app/client/src/app/modules/cbse-program/components/resource-template/resource-template.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild, OnDestroy, Output, Input, EventEmitter } from '@angular/core';
 import * as _ from 'lodash-es';
 import { ISessionContext, IChapterListComponentInput, IResourceTemplateComponentInput } from '../../interfaces';
+import { ProgramTelemetryService } from '../../../program/services';
+import { ConfigService } from '@sunbird/shared';
+import { UserService } from '@sunbird/core';
 
 @Component({
   selector: 'app-resource-template',
@@ -13,14 +16,32 @@ export class ResourceTemplateComponent implements OnInit, OnDestroy {
   @Input() resourceTemplateComponentInput: IResourceTemplateComponentInput = {};
   @Output() templateSelection = new EventEmitter<any>();
   showButton = false;
+  public telemetryInteractCdata: any;
+  public telemetryInteractPdata: any;
+  public telemetryInteractObject: any;
   public templateList;
   public templateSelected;
-  constructor() { }
+  public programContext;
+  public sessionContext;
+  public unitIdentifier;
+  constructor( public programTelemetryService: ProgramTelemetryService, public userService: UserService,
+    public configService: ConfigService ) { }
 
 
   ngOnInit() {
     this.templateList = _.get(this.resourceTemplateComponentInput, 'templateList');
+    this.programContext = _.get(this.resourceTemplateComponentInput, 'programContext');
+    this.sessionContext = _.get(this.resourceTemplateComponentInput, 'sessionContext');
+    this.unitIdentifier  = _.get(this.resourceTemplateComponentInput, 'unitIdentifier');
+
+    // tslint:disable-next-line:max-line-length
+    this.telemetryInteractCdata = this.programTelemetryService.getTelemetryInteractCdata(this.programContext.userDetails.programId, 'Program');
+    // tslint:disable-next-line:max-line-length
+    this.telemetryInteractPdata = this.programTelemetryService.getTelemetryInteractPdata(this.userService.appId, this.configService.appConfig.TELEMETRY.PID + '.programs');
+     // tslint:disable-next-line:max-line-length
+     this.telemetryInteractObject = this.programTelemetryService.getTelemetryInteractObject(this.unitIdentifier, 'Content', '1.0', {l1: this.sessionContext.collection});
   }
+
 
   handleSubmit() {
     const templateDetails = _.find(this.templateList, (template) => {
