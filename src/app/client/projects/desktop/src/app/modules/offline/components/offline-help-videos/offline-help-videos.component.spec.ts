3b2b8a283efa966599b65f8199a5b7041fe4c709
santosh <santosh.mattikoppa@ilimi.in>
2020-02-10 16:25:49 +0530

    Issue #SB-17793 feat: change in order of help center videos test cases

--- src/app/client/projects/desktop/src/app/modules/offline/components/offline-help-videos/offline-help-videos.component.spec.ts
+++ src/app/client/projects/desktop/src/app/modules/offline/components/offline-help-videos/offline-help-videos.component.spec.ts
@@ -28,7 +28,7 @@ describe('OfflineHelpVideosComponent', () => {
         instn: {
           't0094': 'How do I add content to the {instance} desktop app when I am connected to the Internet?',
           't0095': 'How do I add content to the {instance} desktop app when I am offline or using a pen drive?',
-          't0096': 'My Library: How and where can I find content in My Library?',
+          't0096': 'My Downloads: How do i play downloaded content when i am online or offline?',
           't0097': 'How do I copy content to my pen drive?'
         }
       }
@@ -60,7 +60,7 @@ describe('OfflineHelpVideosComponent', () => {
     spyOn(component, 'interpolateInstance');
     component.ngOnInit();
     expect(component.slideData).toBeDefined();
-    expect(component.slideData[0]['id']).toEqual('add-content-online');
+    expect(component.slideData[0]['id']).toEqual('add-content-offline');
     expect(component.setVideoHeight).toHaveBeenCalled();
     expect(component.interpolateInstance).toHaveBeenCalled();
   });
@@ -74,7 +74,7 @@ describe('OfflineHelpVideosComponent', () => {
       url: 'assets/videos/How_do_I_add_content_to_the_desktop_app_when_I_am_connected_to_the_Internet.mp4'};
     component.changeVideoAttributes(data);
     fixture.detectChanges();
-    const value = fixture.debugElement.query(By.css('.help-video__player__title')).nativeElement.innerText;
+    const value = fixture.debugElement.query(By.css('.content-video__player__title')).nativeElement.innerText;
     expect(component.activeVideoObject).toBeDefined();
     expect(component.activeVideoObject.id).toEqual('add-content-online');
     expect(value).toContain(data.name);
