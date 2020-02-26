18805547ac690c833b1758cea27721192fe3142a
Devesh <mailme.devmehta@gmail.com>
2020-02-26 14:32:08 +0530

    Issue #SB-0000 fix:google sign not working for mobile

--- src/app/helpers/ssoHelper.js
+++ src/app/helpers/ssoHelper.js
@@ -172,18 +172,20 @@ const createSession = async (loginId, client_id, req, res) => {
     scope = 'offline_access';
   }
   grant = await keycloakClient.grantManager.obtainDirectly(loginId, undefined, undefined, scope);
-  keycloakClient.storeGrant(grant, req, res)
-  req.kauth.grant = grant
-  keycloakClient.authenticated(req, function (error, data) {
-    if (error) {
-      logger.info({msg: 'SsoHelper:createSession error creating session', additionalInfo: error});
-      throw new Error('ERROR_CREATING_SSO_SESSION')
-    } else {
-      return {
-        access_token: grant.access_token.token,
-        refresh_token: grant.refresh_token.token
+  keycloakClient.storeGrant(grant, req, res);
+  req.kauth.grant = grant;
+  return new Promise((resolve, reject) => {
+    keycloakClient.authenticated(req, function (error) {
+      if (error) {
+        logger.info({msg: 'SsoHelper:createSession error creating session', additionalInfo: error});
+        reject('ERROR_CREATING_SSO_SESSION')
+      } else {
+        resolve({
+          access_token: grant.access_token.token,
+          refresh_token: grant.refresh_token.token
+        })
       }
-    }
+    });
   });
 }
 const updateContact = (req, userDetails) => { // will be called from player docker to learner docker
