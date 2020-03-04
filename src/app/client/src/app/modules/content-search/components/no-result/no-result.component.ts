651e46753380ca5dcd6319adf6f1e2ba3e91fab9
sudip mukherjee <sudip.mukherjee@tarento.com>
2020-03-04 16:05:23 +0530

    Issue #0000 feat: Empty result component implementation

--- src/app/client/src/app/modules/content-search/components/no-result/no-result.component.ts
+++ src/app/client/src/app/modules/content-search/components/no-result/no-result.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ResourceService } from '@sunbird/shared';
+import * as _ from 'lodash-es';
 
 @Component({
-  selector: 'app-no-result',
+  selector: 'app-no-result-found',
   templateUrl: './no-result.component.html',
   styleUrls: ['./no-result.component.scss']
 })
 export class NoResultComponent implements OnInit {
 
-  constructor() { }
+  @Input() title: string;
+  @Input() subTitle: string;
+  @Input() buttonText: string;
+  @Input() redirectUrl: string;
+  instance: string;
+
+  constructor( public router: Router, public resourceService: ResourceService  ) { }
 
   ngOnInit() {
+    this.instance = _.upperCase(this.resourceService.instance);
+  }
+
+  exploreMoreContent(url) {
+    this.router.navigate([url]);
   }
 
 }
--- src/app/client/src/app/modules/content-search/content-search.module.ts
+++ src/app/client/src/app/modules/content-search/content-search.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoResultComponent, SearchProminentFilterComponent, SearchFilterComponent } from './components';
+import { SharedModule } from '@sunbird/shared';
 
 @NgModule({
   declarations: [NoResultComponent, SearchProminentFilterComponent, SearchFilterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [NoResultComponent, SearchProminentFilterComponent, SearchFilterComponent]
 })
