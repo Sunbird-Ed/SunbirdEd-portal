842cfc305dde09d9a3415d6c13fabbcf44fcae39
anoophm <anoophm44@gmail.com>
2020-03-04 15:50:38 +0530

    issue SB-0000 fix: added sb-library-cards-grid to explore page

--- src/app/client/src/app/modules/content-search/content-search.module.ts
+++ src/app/client/src/app/modules/content-search/content-search.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NoResultComponent } from './components';
+import { NoResultComponent, SearchProminentFilterComponent, SearchFilterComponent } from './components';
 
 @NgModule({
-  declarations: [NoResultComponent],
+  declarations: [NoResultComponent, SearchProminentFilterComponent, SearchFilterComponent],
   imports: [
     CommonModule
   ],
-  exports: [NoResultComponent]
+  exports: [NoResultComponent, SearchProminentFilterComponent, SearchFilterComponent]
 })
 export class ContentSearchModule { }
