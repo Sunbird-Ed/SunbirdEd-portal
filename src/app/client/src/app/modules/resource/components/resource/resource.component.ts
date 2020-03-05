aa8c11bd70a43088bf87a2a3c4c86c56d307c107
sudip mukherjee <sudip.mukherjee@tarento.com>
2020-03-05 19:41:06 +0530

    Issue #SB-18089 feat: Added filters and common sonsumption card in library page

--- src/app/client/src/app/modules/resource/components/resource/resource.component.ts
+++ src/app/client/src/app/modules/resource/components/resource/resource.component.ts
@@ -1,5 +1,5 @@
 import { combineLatest, Subject } from 'rxjs';
-import { PageApiService, PlayerService, UserService, ISort } from '@sunbird/core';
+import { PlayerService, UserService, ISort, SearchService, FrameworkService } from '@sunbird/core';
 import { Component, OnInit, OnDestroy, EventEmitter, ChangeDetectorRef, AfterViewInit, HostListener } from '@angular/core';
 import {
   ResourceService, ToasterService, INoResultMessage, ConfigService, UtilService, ICaraouselData,
@@ -17,8 +17,8 @@ export class ResourceComponent implements OnInit, OnDestroy, AfterViewInit {
 
   public showLoader = true;
   public baseUrl: string;
-  public noResultMessage: INoResultMessage;
-  public carouselMasterData: Array<ICaraouselData> = [];
+  public noResultMessage;
+  public apiContentList: Array<ICaraouselData> = [];
   public filterType: string;
   public hashTagId: string;
   public sortingOptions: Array<ISort>;
@@ -33,20 +33,22 @@ export class ResourceComponent implements OnInit, OnDestroy, AfterViewInit {
   public initFilters = false;
   public loaderMessage;
   public redirectUrl;
-  public pageSections: Array<ICaraouselData> = [];
+  public pageSections: Array<any> = [];
+  selectedFilters = {};
+  frameworkId;
 
   @HostListener('window:scroll', []) onScroll(): void {
     if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight * 2 / 3)
-    && this.pageSections.length < this.carouselMasterData.length) {
-        this.pageSections.push(this.carouselMasterData[this.pageSections.length]);
+    && this.pageSections.length < this.apiContentList.length) {
+        this.pageSections.push(this.apiContentList[this.pageSections.length]);
     }
   }
-  constructor(private pageApiService: PageApiService, private toasterService: ToasterService, private cdr: ChangeDetectorRef,
+  constructor(private searchService: SearchService, private toasterService: ToasterService, private cdr: ChangeDetectorRef,
     public resourceService: ResourceService, private configService: ConfigService, private activatedRoute: ActivatedRoute,
     public router: Router, private utilService: UtilService,
     private playerService: PlayerService, private cacheService: CacheService,
     private browserCacheTtlService: BrowserCacheTtlService, private userService: UserService,
-    public navigationhelperService: NavigationHelperService) {
+    public navigationhelperService: NavigationHelperService, public frameworkService: FrameworkService) {
     window.scroll({
       top: 0,
       left: 0,
@@ -59,6 +61,12 @@ export class ResourceComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   ngOnInit() {
+    this.frameworkService.channelData$.pipe(takeUntil(this.unsubscribe$)).subscribe((channelData) => {
+      if (!channelData.err) {
+        this.frameworkId = _.get(channelData, 'channelData.defaultFramework');
+        console.log('this.frameworkId', this.frameworkId);
+      }
+    });
     this.userService.userData$.subscribe(userData => {
       if (userData && !userData.err) {
           this.frameworkData = _.get(userData.userProfile, 'framework');
@@ -66,7 +74,7 @@ export class ResourceComponent implements OnInit, OnDestroy, AfterViewInit {
     });
     this.initFilters = true;
     this.hashTagId = this.userService.hashTagId;
-    this.dataDrivenFilterEvent.pipe(first())
+    this.dataDrivenFilterEvent
     .subscribe((filters: any) => {
       this.dataDrivenFilters = filters;
       this.fetchContentOnParamChange();
@@ -74,13 +82,8 @@ export class ResourceComponent implements OnInit, OnDestroy, AfterViewInit {
     });
   }
   public getFilters(filters) {
-    const defaultFilters = _.reduce(filters, (collector: any, element) => {
-        if (element.code === 'board') {
-          collector.board = _.get(_.orderBy(element.range, ['index'], ['asc']), '[0].name') || '';
-        }
-        return collector;
-      }, {});
-    this.dataDrivenFilterEvent.emit(defaultFilters);
+    const selectedFilters = _.pick(filters.filters, ['board', 'medium', 'gradeLevel']);
+    this.dataDrivenFilterEvent.emit(selectedFilters);
   }
   private fetchContentOnParamChange() {
     combineLatest(this.activatedRoute.params, this.activatedRoute.queryParams)
@@ -94,59 +97,80 @@ export class ResourceComponent implements OnInit, OnDestroy, AfterViewInit {
       takeUntil(this.unsubscribe$))
     .subscribe((result) => {
       this.queryParams = { ...result[0], ...result[1] };
-      this.carouselMasterData = [];
+      this.apiContentList = [];
       this.pageSections = [];
       this.fetchPageData();
     });
   }
+private prepareSearchRequest() {
+  let filters = _.pickBy(this.dataDrivenFilters, (value: Array<string> | string) => value && value.length);
+  filters = _.omit(filters, ['key', 'sort_by', 'sortType', 'appliedFilters']);
+  filters['contentType'] = filters.contentType || ['TextBook']; // TODO: if req. // ['Collection', 'TextBook', 'LessonPlan',
+  const option = {
+    limit: 100 || this.configService.appConfig.SEARCH.PAGE_LIMIT,
+    filters: filters || {},
+    params : this.configService.appConfig.Library.contentApiQueryParams
+  };
+
+  if (this.frameworkId) {
+    option.params.framework = this.frameworkId;
+  }
+
+  return option;
+}
+
   private fetchPageData() {
-    const filters = _.pickBy(this.queryParams, (value: Array<string> | string, key) => {
-      if (_.includes(['sort_by', 'sortType', 'appliedFilters'], key)) {
-        return false;
+    const option = this.prepareSearchRequest();
+    this.searchService.contentSearch(option).pipe(
+      map((response) => {
+        const filteredContents = _.omit(_.groupBy(_.get(response, 'result.content'), 'subject'), ['undefined']);
+      for (const [key, value] of Object.entries(filteredContents)) {
+        const isMultipleSubjects = key.split(',').length > 1;
+        if (isMultipleSubjects) {
+            const subjects = key.split(',');
+            subjects.forEach((subject) => {
+                if (filteredContents[subject]) {
+                    filteredContents[subject] = _.uniqBy(filteredContents[subject].concat(value), 'identifier');
+                } else {
+                    filteredContents[subject] = value;
+                }
+            });
+            delete filteredContents[key];
+        }
       }
-      return value.length;
-    });
-    const softConstraintData = {
-      filters: {channel: this.userService.hashTagId,
-      board: [this.dataDrivenFilters.board]},
-      softConstraints: _.get(this.activatedRoute.snapshot, 'data.softConstraints'),
-      mode: 'soft'
-    };
-    const manipulatedData = this.utilService.manipulateSoftConstraint( _.get(this.queryParams, 'appliedFilters'),
-    softConstraintData, this.frameworkData );
-    const option: any = {
-      source: 'web',
-      name: 'Resource',
-      filters: _.get(this.queryParams, 'appliedFilters') ?  filters : _.get(manipulatedData, 'filters'),
-      mode: _.get(manipulatedData, 'mode'),
-      exists: [],
-      params : this.configService.appConfig.Library.contentApiQueryParams
-    };
-    if (_.get(manipulatedData, 'filters')) {
-      option.softConstraints = _.get(manipulatedData, 'softConstraints');
-    }
-    if (this.queryParams.sort_by) {
-      option.sort_by = {[this.queryParams.sort_by]: this.queryParams.sortType  };
-    }
-    this.pageApiService.getPageData(option)
+      const sections = [];
+      for (const section in filteredContents) {
+        if (section) {
+            sections.push({
+                name: section,
+                contents: filteredContents[section]
+            });
+        }
+      }
+      return _.map(sections, (section) => {
+        _.forEach(section.contents, contents => {
+          contents.cardImg = contents.appIcon || 'assets/images/book.png';
+        });
+        return section;
+      });
+    }))
       .subscribe(data => {
         this.showLoader = false;
-        this.carouselMasterData = this.prepareCarouselData(_.get(data, 'sections'));
-        if (!this.carouselMasterData.length) {
+        this.apiContentList = data;
+        if (!this.apiContentList.length) {
           return; // no page section
         }
-        if (this.carouselMasterData.length >= 2) {
-          this.pageSections = [this.carouselMasterData[0], this.carouselMasterData[1]];
-        } else if (this.carouselMasterData.length >= 1) {
-          this.pageSections = [this.carouselMasterData[0]];
+        if (this.apiContentList.length >= 2) {
+          this.pageSections = [this.apiContentList[0], this.apiContentList[1]];
+        } else if (this.apiContentList.length >= 1) {
+          this.pageSections = [this.apiContentList[0]];
         }
-        this.cdr.detectChanges();
       }, err => {
         this.showLoader = false;
-        this.carouselMasterData = [];
+        this.apiContentList = [];
         this.pageSections = [];
         this.toasterService.error(this.resourceService.messages.fmsg.m0004);
-    });
+      });
   }
   private prepareCarouselData(sections = []) {
     const { constantData, metaData, dynamicFields, slickSize } = this.configService.appConfig.Library;
@@ -221,9 +245,15 @@ export class ResourceComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   private setNoResultMessage() {
-      this.noResultMessage = {
-        'message': 'messages.stmsg.m0007',
-        'messageText': 'messages.stmsg.m0006'
-      };
+    this.noResultMessage = {
+      'title': this.resourceService.frmelmnts.lbl.noBookfoundTitle,
+      'subTitle': this.resourceService.frmelmnts.lbl.noBookfoundSubTitle,
+      'buttonText': this.resourceService.frmelmnts.lbl.noBookfoundButtonText,
+      'showExploreContentButton': true
+    };
+  }
+
+  private navigateToExploreContent() {
+    this.router.navigate(['search/Library', 1], {queryParams: this.dataDrivenFilters});
   }
 }
