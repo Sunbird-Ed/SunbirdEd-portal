5f75bc6286a3bf3ea9e39726a7f4da78925c1f31
sudip mukherjee <sudip.mukherjee@tarento.com>
2020-03-06 11:11:16 +0530

    Issue #SB-18089 feat: Fixed test case failure

--- src/app/client/src/app/modules/resource/components/resource/resource.component.spec.data.ts
+++ src/app/client/src/app/modules/resource/components/resource/resource.component.spec.data.ts
@@ -1,5 +1,267 @@
 export const Response = {
-    successData: {
+  searchResult: {
+    'id': 'api.content.search',
+    'ver': '1.0',
+    'ts': '2020-03-05T05:11:37.162Z',
+    'params': {
+      'resmsgid': 'ca1842a0-5e9f-11ea-9c40-61a751c55653',
+      'msgid': '97f24610-32e5-2aff-1dd7-30a586680455',
+      'status': 'successful',
+      'err': null,
+      'errmsg': null
+    },
+    'responseCode': 'OK',
+    'result': {
+      'count': 2,
+      'content': [
+        {
+          'ownershipType': [
+            'createdBy'
+          ],
+          'copyright': 'SAP',
+          'keywords': [
+            'hgh',
+            'testing lessons',
+            'key'
+          ],
+          'subject': 'English',
+          'channel': '012530141516660736208',
+          'downloadUrl': 'https://ntpstagingr_fi1/assam-text-book_1576731343811_do_212916581536096256131_1.0_spine.ecar',
+          'organisation': [
+            'SAP'
+          ],
+          'language': [
+            'English'
+          ],
+          'mimeType': 'application/vnd.ekstep.content-collection',
+          'variants': {
+            'online': {
+              'ecarUrl': 'https://_files/do_212916581536096256131/assam-text-book_1576731344224_do_212916581536096256131_1.0_online.ecar',
+              'size': 11806
+            },
+            'spine': {
+              'ecarUrl': 'https://ntpsiles/do_212916581536096256131/assam-text-book_1576731343811_do_212916581536096256131_1.0_spine.ecar',
+              'size': 1021747
+            }
+          },
+          'leafNodes': [
+            'do_212848119817945088185',
+            'do_2123178641611407361570',
+            'do_2125388336945070081193',
+            'do_2128458481719705601144',
+            'do_2125392680404008961221',
+            'do_2125392907628953601223'
+          ],
+          'objectType': 'Content',
+          'gradeLevel': [
+            'Class 5',
+            'Class 6',
+            'Class 7'
+          ],
+          'appIcon': 'https://ntpstagingall.blob.core.windows.act/api2_1530687542792.thumb.png',
+          'children': [
+            'do_212848119817945088185',
+            'do_2128458481719705601144',
+            'do_2123178641611407361570',
+            'do_2125393116783165441232'
+          ],
+          'appId': 'staging.diksha.portal',
+          'contentEncoding': 'gzip',
+          'lockKey': 'c98ba06f-cd7c-4507-b286-68d5aa960b60',
+          'mimeTypesCount': '',
+          'totalCompressedSize': 11089779,
+          'contentType': 'TextBook',
+          'identifier': 'do_212916581536096256131',
+          'audience': [
+            'Learner'
+          ],
+          'visibility': 'Default',
+          'toc_url': 'htt536096256131/artifact/do_212916581536096256131_toc.json',
+          'contentTypesCount': '{\'TextBookUnit\':2,\'Resource\':8,\'Collection\':2}',
+          'consumerId': '56ff6913-abcc-4a88-b247-c976e47cbfb4',
+          'childNodes': [
+            'do_212848119817945088185',
+            'do_2125393116783165441232',
+            'do_2123178641611407361570',
+            'do_21291658181889228811065',
+            'do_2125388336945070081193',
+            'do_21291658181888409611064',
+            'do_2128458481719705601144',
+            'do_2125393105532600321231',
+            'do_2125392680404008961221',
+            'do_2125392907628953601223'
+          ],
+          'mediaType': 'content',
+          'osId': 'org.ekstep.quiz.app',
+          'ageGroup': [
+            '<5',
+            '>10'
+          ],
+          'graph_id': 'domain',
+          'nodeType': 'DATA_NODE',
+          'lastPublishedBy': '93ef4d2c-c8ad-487e-833c-1f7ffb4ac346',
+          'version': 2,
+          'license': 'CC BY 4.0',
+          'prevState': 'Review',
+          'qrCodeProcessId': '1f3621d4-983a-4324-a30c-af512135c0d6',
+          'size': 1021747,
+          'lastPublishedOn': '2019-12-19T04:55:43.684+0000',
+          'IL_FUNC_OBJECT_TYPE': 'Content',
+          'domain': [
+            'Biology'
+          ],
+          'name': 'Assam text book',
+          'status': 'Live',
+          'code': 'org.sunbird.6gMOb5',
+          'description': 'Enter description for TextBook',
+          'medium': 'English',
+          'idealScreenSize': 'normal',
+          'posterImage': 'https://198721383/artifact/api2_1530687542792.png',
+          'createdOn': '2019-12-19T04:52:24.549+0000',
+          'reservedDialcodes': '{\'F5M5M7\':2,\'V1E7I2\':1,\'K8D3T7\':0}',
+          'contentDisposition': 'inline',
+          'lastUpdatedOn': '2019-12-19T04:55:43.369+0000',
+          'SYS_INTERNAL_LAST_UPDATED_ON': '2019-12-19T04:55:44.316+0000',
+          'dialcodeRequired': 'No',
+          'creator': 'azam m',
+          'createdFor': [
+            '012530141516660736208'
+          ],
+          'lastStatusChangedOn': '2019-12-19T04:55:44.308+0000',
+          'IL_SYS_NODE_TYPE': 'DATA_NODE',
+          'os': [
+            'All'
+          ],
+          'pkgVersion': 1,
+          'versionKey': '1576731343369',
+          'idealScreenDensity': 'hdpi',
+          'framework': 'NCF',
+          'depth': 0,
+          'dialcodes': [
+            'K8D3T7'
+          ],
+          's3Key': 'ecar_files/do_212916581536096256131/assam-text-book_1576731343811_do_212916581536096256131_1.0_spine.ecar',
+          'lastSubmittedOn': '2019-12-19T04:54:20.014+0000',
+          'createdBy': '5936c4ee-7e44-4a1b-9211-1c17fc8601e7',
+          'compatibilityLevel': 1,
+          'leafNodesCount': 6,
+          'IL_UNIQUE_ID': 'do_212916581536096256131',
+          'board': 'State (Assam)',
+          'resourceType': 'Book',
+          'node_id': 521312,
+          'orgDetails': {
+            'email': null,
+            'orgName': 'SAP'
+          }
+        },
+        {
+          'ownershipType': [
+            'createdBy'
+          ],
+          'copyright': 'Chhattisgarh',
+          'subject': 'English',
+          'downloadUrl': 'https://ntpstagido_21285725285052416011433/test-k12-book_1569489359437_do_21285725285052416011433_1.0_spine.ecar',
+          'channel': '01258043108936908899',
+          'organisation': [
+            'Chhattisgarh'
+          ],
+          'language': [
+            'English'
+          ],
+          'variants': {
+            'online': {
+              'ecarUrl': 'https://ntpsta/do_21285725285052416011433/test-k12-book_1569489359599_do_21285725285052416011433_1.0_online.ecar',
+              'size': 4629
+            },
+            'spine': {
+              'ecarUrl': 'https://ntpstagingal725285052416011433/test-k12-book_1569489359437_do_21285725285052416011433_1.0_spine.ecar',
+              'size': 59795
+            }
+          },
+          'mimeType': 'application/vnd.ekstep.content-collection',
+          'leafNodes': [
+            'do_21285718979780608011405'
+          ],
+          'objectType': 'Content',
+          'gradeLevel': [
+            'Class 5',
+            'Class 6',
+            'Class 7',
+            'Class 8'
+          ],
+          'appIcon': 'https://ntpsta1285725285052416011433/artifact/1_1565067478206.thumb.png',
+          'appId': 'staging.diksha.portal',
+          'contentEncoding': 'gzip',
+          'lockKey': '93e020b8-de57-45ad-9863-737ad82e51d2',
+          'totalCompressedSize': 50594,
+          'mimeTypesCount': '{\'application/vnd.ekstep.content-collection\':1,\'application/vnd.ekstep.ecml-archive\':1}',
+          'contentType': 'TextBook',
+          'identifier': 'do_21285725285052416011433',
+          'audience': [
+            'Learner'
+          ],
+          'toc_url': 'https://ntpst_21285725285052416011433/artifact/do_21285725285052416011433_toc.json',
+          'visibility': 'Default',
+          'contentTypesCount': '{\'TextBookUnit\':1,\'Resource\':1}',
+          'author': 'Umesh',
+          'childNodes': [
+            'do_21285725355311104011435',
+            'do_21285718979780608011405'
+          ],
+          'consumerId': 'a9cb3a83-a164-4bf0-aa49-b834cebf1c07',
+          'mediaType': 'content',
+          'osId': 'org.ekstep.quiz.app',
+          'lastPublishedBy': '6c2f53fb-ae8d-49c5-b0c0-125c4d390634',
+          'graph_id': 'domain',
+          'nodeType': 'DATA_NODE',
+          'version': 2,
+          'license': 'CC BY 4.0',
+          'prevState': 'Review',
+          'lastPublishedOn': '2019-09-26T09:15:59.387+0000',
+          'size': 59795,
+          'name': 'Test k12 book',
+          'status': 'Live',
+          'code': 'org.sunbird.IYLpFa',
+          'description': 'Enter description for TextBook',
+          'medium': 'English',
+          'posterImage': 'https://nnt/do_21282103277991526412038/artifact/1_1565067478206.png',
+          'idealScreenSize': 'normal',
+          'createdOn': '2019-09-26T09:07:53.354+0000',
+          'copyrightYear': 2019,
+          'contentDisposition': 'inline',
+          'lastUpdatedOn': '2019-09-26T09:15:59.202+0000',
+          'SYS_INTERNAL_LAST_UPDATED_ON': '2019-09-26T09:15:59.676+0000',
+          'dialcodeRequired': 'No',
+          'createdFor': [
+            '01258043108936908899'
+          ],
+          'lastStatusChangedOn': '2019-09-26T09:15:59.669+0000',
+          'creator': 'Bcreator',
+          'os': [
+            'All'
+          ],
+          'pkgVersion': 1,
+          'versionKey': '1569489359202',
+          'idealScreenDensity': 'hdpi',
+          's3Key': 'ecar_files/do_21285725285052416011433/test-k12-book_1569489359437_do_21285725285052416011433_1.0_spine.ecar',
+          'depth': 0,
+          'framework': 'as_k-12',
+          'lastSubmittedOn': '2019-09-26T09:15:26.046+0000',
+          'createdBy': 'b15441a6-7095-45c1-829d-ec167879ab52',
+          'leafNodesCount': 1,
+          'compatibilityLevel': 1,
+          'board': 'State (Assam)',
+          'resourceType': 'Book',
+          'node_id': 497769,
+          'orgDetails': {
+            'email': null,
+            'orgName': 'Chhattisgarh'
+          }
+        }
+      ]
+    }
+  },
+  successData: {
        ' id': 'api.page.assemble',
        ' params': {
             'err': null,
--- src/app/client/src/app/modules/resource/components/resource/resource.component.spec.ts
+++ src/app/client/src/app/modules/resource/components/resource/resource.component.spec.ts
@@ -2,7 +2,7 @@ import { ResourceComponent } from './resource.component';
 import { BehaviorSubject, throwError, of } from 'rxjs';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ResourceService, ToasterService, SharedModule } from '@sunbird/shared';
-import { PageApiService, CoreModule, UserService} from '@sunbird/core';
+import { SearchService, CoreModule, UserService} from '@sunbird/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SuiModule } from 'ng2-semantic-ui';
 import * as _ from 'lodash-es';
@@ -14,8 +14,8 @@ import { TelemetryModule } from '@sunbird/telemetry';
 describe('ResourceComponent', () => {
   let component: ResourceComponent;
   let fixture: ComponentFixture<ResourceComponent>;
-  let toasterService, userService, pageApiService, resourceService;
-  const mockPageSection: Array<any> = Response.successData.result.response.sections;
+  let toasterService, userService, resourceService, searchService;
+  const mockPageSection: any = Response.successData;
   let sendPageApi = true;
   class RouterStub {
     navigate = jasmine.createSpy('navigate');
@@ -51,18 +51,18 @@ describe('ResourceComponent', () => {
     toasterService = TestBed.get(ToasterService);
     userService = TestBed.get(UserService);
     resourceService = TestBed.get(ResourceService);
-    pageApiService = TestBed.get(PageApiService);
+    searchService = TestBed.get(SearchService);
     sendPageApi = true;
-    spyOn(pageApiService, 'getPageData').and.callFake((options) => {
+    spyOn(searchService, 'contentSearch').and.callFake((options) => {
       if (sendPageApi) {
-        return of({sections: mockPageSection});
+        return of(mockPageSection);
       }
       return throwError({});
     });
   });
   it('should emit filter data when getFilters is called with data', () => {
     spyOn(component.dataDrivenFilterEvent, 'emit');
-    component.getFilters([{ code: 'board', range: [{index: 0, name: 'NCRT'}, {index: 1, name: 'CBSC'}]}]);
+    component.getFilters({ filters: { board: 'NCRT'}});
     expect(component.dataDrivenFilterEvent.emit).toHaveBeenCalledWith({ board: 'NCRT'});
   });
   it('should emit filter data when getFilters is called with no data', () => {
@@ -81,7 +81,7 @@ describe('ResourceComponent', () => {
     component.getFilters([{ code: 'board', range: [{index: 0, name: 'NCRT'}, {index: 1, name: 'CBSC'}]}]);
     expect(component.dataDrivenFilters).toEqual({ board: 'NCRT'});
     expect(component.showLoader).toBeFalsy();
-    expect(component.carouselMasterData.length).toEqual(1);
+    expect(component.apiContentList.length).toEqual(1);
   });
   xit('should fetch content after getting hashTagId and filter data and throw error if page api fails', () => {
     sendPageApi = false;
@@ -90,7 +90,7 @@ describe('ResourceComponent', () => {
     component.getFilters([{ code: 'board', range: [{index: 0, name: 'NCRT'}, {index: 1, name: 'CBSC'}]}]);
     expect(component.dataDrivenFilters).toEqual({ board: 'NCRT'});
     expect(component.showLoader).toBeFalsy();
-    expect(component.carouselMasterData.length).toEqual(0);
+    expect(component.apiContentList.length).toEqual(0);
     expect(toasterService.error).toHaveBeenCalled();
   });
   xit('should unsubscribe from all observable subscriptions', () => {
