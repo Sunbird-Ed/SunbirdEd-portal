250e0b0b7a8722ec2fc40a52a656b43a535c1c4f
Sourav Dey <sourav_d@techjoomla.com>
2020-03-09 12:59:43 +0530

    Issue #SB-12993 feat: Adding new API to upload desktop crash logs to azure blob

--- src/app/helpers/desktopCrashReporter.js
+++ src/app/helpers/desktopCrashReporter.js
@@ -5,92 +5,113 @@ const dateFormat = require('dateformat')
 const uuidv1 = require('uuid/v1')
 const blobService = azure.createBlobService(envHelper.desktop_azure_account_name, envHelper.desktop_azure_account_key);
 const logger = require('sb_logger_util_v2');
-const getStream = require('into-stream');
+const multiparty = require('multiparty');
 const containerName = envHelper.desktop_azure_crash_container_name;
 
 function storeCrashLogsToAzure() {
-    return function (req, res) {
-        if (!_.get(req, 'file')) {
-            const response = {
-                responseCode: "BAD_REQUEST",
+  return function (req, res) {
+    try {
+      let form = new multiparty.Form();
+      form.on('part', function (part) {
+        if (part.filename) {
+          var size = part.byteCount - part.byteOffset;
+          var name = getBlobName(_.get(req, 'query.deviceId'), part.filename);
+          blobService.createBlockBlobFromStream(containerName, name, part, size, (error) => {
+            if (error && error.statusCode === 403) {
+              const response = {
+                responseCode: "FORBIDDEN",
                 params: {
-                    err: "BAD_REQUEST",
-                    status: "Bad Request",
-                    errmsg: "File missing"
+                  err: "FORBIDDEN",
+                  status: "failed",
+                  errmsg: "Unable to authorize to azure blob"
                 },
                 result: req.file
-            }
-            logger.error({ msg: 'File missing in desktop crash reporter' });
-            return res.status(400).send(apiResponse(response));
-        }
-        const
-            blobName = getBlobName(_.get(req, 'file.originalname'), _.get(req, 'body.deviceId')),
-            stream = getStream(_.get(req, 'file.buffer')),
-            streamLength = _.get(req, 'file.buffer').length;
-
-        blobService.createBlockBlobFromStream(containerName, blobName, stream, streamLength, error => {
-            if (error && error.statusCode === 403) {
-                const response = {
-                    responseCode: "FORBIDDEN",
-                    params: {
-                        err: "FORBIDDEN",
-                        status: "failed",
-                        errmsg: "Unable to authorize to azure blob"
-                    },
-                    result: req.file
-                }
-                logger.error({ msg: 'Unable to authorize to azure blob for uploading desktop crash logs', error: error });
-                return res.status(403).send(apiResponse(response));
+              }
+              logger.error({
+                msg: 'Unable to authorize to azure blob for uploading desktop crash logs',
+                error: error
+              });
+              return res.status(403).send(apiResponse(response));
             } else if (error) {
-                const response = {
-                    responseCode: "SERVER_ERROR",
-                    params: {
-                        err: "SERVER_ERROR",
-                        status: "failed",
-                        errmsg: "Failed to upload to blob"
-                    },
-                    result: {}
-                }
-                logger.error({ msg: 'Failed to upload desktop crash logs to blob', error: error });
-                return res.status(500).send(apiResponse(response));
+              const response = {
+                responseCode: "SERVER_ERROR",
+                params: {
+                  err: "SERVER_ERROR",
+                  status: "failed",
+                  errmsg: "Failed to upload to blob"
+                },
+                result: {}
+              }
+              logger.error({
+                msg: 'Failed to upload desktop crash logs to blob',
+                error: error
+              });
+              return res.status(500).send(apiResponse(response));
             } else {
-                const response = {
-                    responseCode: "OK",
-                    params: {
-                        err: null,
-                        status: "success",
-                        errmsg: null
-                    },
-                    result: {
-                        'message': 'Successfully uploaded to blob'
-                    }
+              const response = {
+                responseCode: "OK",
+                params: {
+                  err: null,
+                  status: "success",
+                  errmsg: null
+                },
+                result: {
+                  'message': 'Successfully uploaded to blob'
                 }
-                return res.status(200).send(apiResponse(response));
+              }
+              return res.status(200).send(apiResponse(response));
             }
-        });
+          });
+        }
+      });
+      form.parse(req);
+    } catch (error) {
+      const response = {
+        responseCode: "SERVER_ERROR",
+        params: {
+          err: "SERVER_ERROR",
+          status: "failed",
+          errmsg: "Failed to upload to blob"
+        },
+        result: {}
+      }
+      logger.error({
+        msg: 'Failed to upload desktop crash logs to blob',
+        error: error
+      });
+      return res.status(500).send(apiResponse(response));
     }
+  }
 }
 
-const getBlobName = (originalName, deviceId) => {
-    const identifier = Math.random().toString().replace(/0\./, ''); // remove "0." from start of string
-    return `${deviceId}_${identifier}.${originalName}`;
+const getBlobName = (deviceId, extension) => {
+  const identifier = Math.random().toString().replace(/0\./, ''); // remove "0." from start of string
+  return `${deviceId}_${identifier}.${extension}`;
 };
 
-const apiResponse = ({ responseCode, result, params: { err, errmsg, status } }) => {
-    return {
-        'id': 'api.desktop.upload.crash.log',
-        'ver': '1.0',
-        'ts': dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss:lo'),
-        'params': {
-            'resmsgid': uuidv1(),
-            'msgid': null,
-            'status': status,
-            'err': err,
-            'errmsg': errmsg
-        },
-        'responseCode': responseCode,
-        'result': result
-    }
+const apiResponse = ({
+  responseCode,
+  result,
+  params: {
+    err,
+    errmsg,
+    status
+  }
+}) => {
+  return {
+    'id': 'api.desktop.upload.crash.log',
+    'ver': '1.0',
+    'ts': dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss:lo'),
+    'params': {
+      'resmsgid': uuidv1(),
+      'msgid': null,
+      'status': status,
+      'err': err,
+      'errmsg': errmsg
+    },
+    'responseCode': responseCode,
+    'result': result
+  }
 }
 
 module.exports = {
--- src/app/routes/desktopAppRoutes.js
+++ src/app/routes/desktopAppRoutes.js
@@ -1,13 +1,7 @@
 const desktopAppHelper = require('../helpers/desktopAppHelper.js');
 const crashReporter = require('../helpers/desktopCrashReporter.js');
-
 const bodyParser = require('body-parser');
 const logger = require('sb_logger_util_v2');
-
-const multer = require('multer'),
-  storage = multer.memoryStorage(),
-  uploadStrategy = multer({ storage: storage }).single('upload_file_minidump');
-
 module.exports = function (app) {
     app.post('/v1/desktop/update', bodyParser.urlencoded({ extended: true }),
     bodyParser.json(),
@@ -15,5 +9,5 @@ module.exports = function (app) {
         logger.info({msg: '/v1/desktop/update called'});
     })
 
-    app.post('/v1/desktop/upload-crash-logs', uploadStrategy, crashReporter.storeCrashLogsToAzure());
+    app.post('/v1/desktop/upload-crash-logs', crashReporter.storeCrashLogsToAzure());
 }
