69ceb73a6ca1ee947158abf3c97ef2cfd51084af
Sourav Dey <sourav_d@techjoomla.com>
2020-03-09 13:02:26 +0530

    Issue #SB-12993 feat: Adding new API to upload desktop crash logs to azure blob

--- src/app/routes/desktopAppRoutes.js
+++ src/app/routes/desktopAppRoutes.js
@@ -4,10 +4,10 @@ const bodyParser = require('body-parser');
 const logger = require('sb_logger_util_v2');
 module.exports = function (app) {
     app.post('/v1/desktop/update', bodyParser.urlencoded({ extended: true }),
-    bodyParser.json(),
-    desktopAppHelper.getAppUpdate(), () => {
-        logger.info({msg: '/v1/desktop/update called'});
-    })
+      bodyParser.json(),
+      desktopAppHelper.getAppUpdate(), () => {
+          logger.info({msg: '/v1/desktop/update called'});
+      })
 
     app.post('/v1/desktop/upload-crash-logs', crashReporter.storeCrashLogsToAzure());
 }
