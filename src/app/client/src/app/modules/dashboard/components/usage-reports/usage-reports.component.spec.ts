8ffbb3d20bec641bacdee43449421cd4e0f7690b
anoophm <anoophm44@gmail.com>
2020-03-20 15:36:59 +0530

    Issue #SB-18430 fix: testcase fix for dashboard, home and manage modules

--- src/app/client/src/app/modules/dashboard/components/usage-reports/usage-reports.component.spec.ts
+++ src/app/client/src/app/modules/dashboard/components/usage-reports/usage-reports.component.spec.ts
@@ -12,6 +12,7 @@ import { Router } from '@angular/router';
 import { UsageReportsComponent } from './usage-reports.component';
 import { TelemetryModule } from '@sunbird/telemetry';
 import { DataChartComponent } from '../data-chart/data-chart.component';
+import { CoreModule } from '@sunbird/core';
 
 describe('UsageReportsComponent', () => {
   let component: UsageReportsComponent;
@@ -22,7 +23,7 @@ describe('UsageReportsComponent', () => {
   const routerStub = { url: '/dashBoard/organization' };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, SharedModule.forRoot(), TelemetryModule.forRoot()],
+      imports: [HttpClientTestingModule, SharedModule.forRoot(), CoreModule, TelemetryModule.forRoot()],
       schemas: [NO_ERRORS_SCHEMA],
       declarations: [UsageReportsComponent, DataChartComponent],
       providers: [ ToasterService, UserService, NavigationHelperService,
