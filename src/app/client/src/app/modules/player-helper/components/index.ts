97418c4ba172d366085ced01b48b2d0af6d0ae00
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-04-06 17:31:44 +0530

    Issue #SB-10681 changes for the player to use the common-consumption

--- src/app/client/src/app/modules/player-helper/components/index.ts
+++ src/app/client/src/app/modules/player-helper/components/index.ts
@@ -7,7 +7,6 @@ export * from './fancy-tree/fancy-tree.component';
 export * from './content-player-metadata/content-player-metadata.component';
 export * from './content-rating/content-rating.component';
 export * from './comming-soon/comming-soon.component';
-export * from './content-download/content-download.component';
 export * from './contentplayer-page/contentplayer-page.component';
 export * from './credits-and-licence/credits-and-licence.component';
 export * from './content-player/content-player.component';
