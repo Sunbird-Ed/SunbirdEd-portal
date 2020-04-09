1119413bf449be41863dec7168214294d3612c0d
Rajesh <rajesh.kumaravel@tarento.com>
2020-04-09 12:01:07 +0530

    Issue #SB-18793 fix: Sort pipe handled for white space.

--- src/app/client/src/app/modules/shared/pipes/sortBy/sortBy.pipe.ts
+++ src/app/client/src/app/modules/shared/pipes/sortBy/sortBy.pipe.ts
@@ -23,12 +23,16 @@ export class SortByPipe implements PipeTransform {
   transform(data: any[], sortField: string, sortOrder: string): any[] {
     if (!data || !data.length || sortOrder === '' || !sortOrder) { return data; }
     if (!sortField || sortField === '') {
+      data = data.map(e => e.trim());
       if (sortOrder === 'asc') {
         return data.sort();
       } else {
         return data.sort().reverse();
       }
     }
+    data.forEach((obj) => {
+      obj[sortField] = obj[sortField].trim();
+    });
     return orderBy(data, [sortField], [sortOrder]);
   }
 }
