b84fab583c46a324597ba0c8ea28e9ce9971fa9f
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-04-09 14:22:54 +0530

    Issue #SB-18739 filter issue fix git commit -m Issue

--- src/app/client/src/app/modules/core/services/tenant/tenant.service.ts
+++ src/app/client/src/app/modules/core/services/tenant/tenant.service.ts
@@ -5,6 +5,9 @@ import { DataService } from '../data/data.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { skipWhile } from 'rxjs/operators';
+import { CacheService } from 'ng2-cache-service';
+import * as _ from 'lodash-es';
+
 
 /**
  * This service returns the organization details.
@@ -18,6 +21,9 @@ export class TenantService extends DataService {
    * BehaviorSubject containing tenant data.
    */
   private _tenantData$ = new BehaviorSubject<ITenantInfo>(undefined);
+  public slugForProminentFilter = (<HTMLInputElement>document.getElementById('slugForProminentFilter')) ?
+  (<HTMLInputElement>document.getElementById('slugForProminentFilter')).value : null;
+
   /**
    * Read only observable containing tenant data.
    */
@@ -45,7 +51,7 @@ export class TenantService extends DataService {
    * @param {HttpClient} http Reference of HttpClient.
    * @param {ConfigService} config Reference of ConfigService.
    */
-  constructor(http: HttpClient, config: ConfigService) {
+  constructor(http: HttpClient, config: ConfigService, private cacheService: CacheService) {
     super(http);
     this.config = config;
     this.baseUrl = this.config.urlConFig.URLS.TENANT_PREFIX;
@@ -56,7 +62,10 @@ export class TenantService extends DataService {
    * @param slug Organization details passed from main-header component.
    */
   public getTenantInfo(slug?: string) {
-
+    const orgDetailsFromSlug = this.cacheService.get('orgDetailsFromSlug')
+    if (_.get(orgDetailsFromSlug, 'slug') === this.slugForProminentFilter) {
+      slug = _.get(orgDetailsFromSlug, 'slug')
+    }
     const url = `${this.config.urlConFig.URLS.TENANT.INFO + '/'}` + (slug ? slug : '');
     this.get({ url }).subscribe(
       (apiResponse: ServerResponse) => {
