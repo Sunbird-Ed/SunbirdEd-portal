b5a417e511ffa0306b0b3e669c0e3c3e4d6c8f4c
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-04-09 23:50:41 +0530

    Issue #SB-18829 User is redirected to library page post clicking on login form course link

--- src/app/client/src/app/modules/core/components/main-menu/main-menu.component.ts
+++ src/app/client/src/app/modules/core/components/main-menu/main-menu.component.ts
@@ -64,7 +64,7 @@ export class MainMenuComponent implements OnInit {
 
   signInIntractEdata: IInteractEventEdata;
   showContributeTab: boolean;
-  hrefPath: string = '/resources';
+  hrefPath = '/resources';
   /*
   * constructor
   */
@@ -79,22 +79,22 @@ export class MainMenuComponent implements OnInit {
     this.workSpaceRole = this.config.rolesConfig.headerDropdownRoles.workSpaceRole;
     this.updateHrefPath(this.router.url);
     router.events.pipe(
-      filter(event => event instanceof NavigationEnd)  
+      filter(event => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
-      this.updateHrefPath(event.url)
+      this.updateHrefPath(event.url);
     });
   }
   updateHrefPath(url) {
-     if(url.indexOf('explore-course') >= 0) {
-      this.hrefPath = url.replace('explore-course','learn');
-    } else if(url.indexOf('explore') >= 0) {
-      this.hrefPath = url.replace('explore','resources');
+     if (url.indexOf('explore-course') >= 0) {
+      this.hrefPath = url.replace('explore-course', 'learn');
+    } else if (url.indexOf('explore') >= 0) {
+      this.hrefPath = url.replace('explore', 'resources');
     } else {
       this.hrefPath = '/resources';
     }
   }
   ngOnInit() {
-    
+
     try {
       this.helpLinkVisibility = (<HTMLInputElement>document.getElementById('helpLinkVisibility')).value;
     } catch (error) {
