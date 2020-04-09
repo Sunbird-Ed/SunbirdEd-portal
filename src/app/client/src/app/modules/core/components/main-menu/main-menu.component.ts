5681ef3affef56e817d333cc93170a37ac93fdf9
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-04-10 00:40:28 +0530

    Issue #SB-18829 User is redirected to library page post clicking on login form course link

--- src/app/client/src/app/modules/core/components/main-menu/main-menu.component.ts
+++ src/app/client/src/app/modules/core/components/main-menu/main-menu.component.ts
@@ -64,7 +64,7 @@ export class MainMenuComponent implements OnInit {
 
   signInIntractEdata: IInteractEventEdata;
   showContributeTab: boolean;
-  hrefPath = '/resources';
+
   /*
   * constructor
   */
