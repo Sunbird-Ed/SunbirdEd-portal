5cb7981498b1eaabbdfeb865139384e77b9b2d22
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-04-14 19:25:07 +0530

    Issue #SB-10681 changes for the player to use the common-consumption

--- src/app/client/src/app/modules/player-helper/components/content-chapterlist/content-chapterlist.component.spec.ts
+++ src/app/client/src/app/modules/player-helper/components/content-chapterlist/content-chapterlist.component.spec.ts
@@ -1,26 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContentPlayerPageComponent } from './content-chapterlist.component.';
+import { ContentChapterlistComponent } from './content-chapterlist.component';
 import {
-  ConfigService, NavigationHelperService, PlayerConfig, ContentData, ToasterService, ResourceService,
-  UtilService, SharedModule
+   ToasterService, SharedModule
 } from '@sunbird/shared';
-import { Subject } from 'rxjs';
-import { takeUntil, filter, map } from 'rxjs/operators';
 import * as _ from 'lodash-es';
-import { IImpressionEventInput, TelemetryService } from '@sunbird/telemetry';
-import { of, throwError } from 'rxjs';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterModule } from '@angular/router';
 import { TelemetryModule } from '@sunbird/telemetry';
-import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('ContentPlayerComponent', () => {
-  let component: ContentPlayerPageComponent;
-  let fixture: ComponentFixture<ContentPlayerPageComponent>;
+  let component: ContentChapterlistComponent;
+  let fixture: ComponentFixture<ContentChapterlistComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ContentPlayerPageComponent],
+      declarations: [ContentChapterlistComponent],
       imports: [HttpClientTestingModule, TelemetryModule.forRoot(), RouterModule.forRoot([]), SharedModule.forRoot()],
       providers: [
        ToasterService
@@ -31,14 +26,12 @@ describe('ContentPlayerComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ContentPlayerPageComponent);
+    fixture = TestBed.createComponent(ContentChapterlistComponent);
     component = fixture.componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  // it('should define player config details', () => {
-  //   expect(component.playerConfig).toBeTruthy();
-  // });
+  
 });
