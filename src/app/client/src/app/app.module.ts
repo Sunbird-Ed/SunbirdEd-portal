a99f682dc4d511645c515bba4e532a96a8c409be
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-04-17 00:28:53 +0530

    Issue #SB-0000 fix:chat-bot integration in portal

--- src/app/client/src/app/app.module.ts
+++ src/app/client/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { CacheStorageAbstract } from 'ng2-cache-service/dist/src/services/storag
 import { CacheSessionStorage } from 'ng2-cache-service/dist/src/services/storage/session-storage/cache-session-storage.service';
 import { DeviceDetectorModule } from 'ngx-device-detector';
 import { PluginModules } from './framework.config';
+import {ChatLibModule, ChatLibService} from 'chat-lib';
 @NgModule({
   declarations: [
     AppComponent
@@ -33,13 +34,14 @@ import { PluginModules } from './framework.config';
     TelemetryModule.forRoot(),
     DeviceDetectorModule.forRoot(),
     SharedFeatureModule,
+    ChatLibModule,
     ...PluginModules,
     AppRoutingModule // don't add any module below this because it contains wildcard route
   ],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent],
   providers: [
-    CacheService,
+    CacheService, ChatLibService,
     { provide: CacheStorageAbstract, useClass: CacheSessionStorage },
     { provide: HTTP_INTERCEPTORS, useClass: SessionExpiryInterceptor, multi: true }
   ]
