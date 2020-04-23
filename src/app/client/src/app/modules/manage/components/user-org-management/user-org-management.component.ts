1d903f5a41ee1f0e09e2292e56f2bdeb00851f60
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-04-23 21:01:52 +0530

    Isssue #SB-19076 ShadowDB fix

--- src/app/client/src/app/modules/manage/components/user-org-management/user-org-management.component.ts
+++ src/app/client/src/app/modules/manage/components/user-org-management/user-org-management.component.ts
@@ -164,16 +164,16 @@ export class UserOrgManagementComponent implements OnInit, AfterViewInit {
       data => {
         const result = _.get(data, 'result');
         this.uploadedDetails = {
-          'total_uploaded' : 0,
+          'total_uploaded': 0,
           'accounts_validated': result['accounts_validated'] ? result['accounts_validated'] : 0,
           'accounts_rejected': result['accounts_rejected'] ? result['accounts_rejected'] : 0,
           'accounts_failed': result['accounts_failed'] ? result['accounts_failed'] : 0,
           'duplicate_account': result['duplicate_account'] ? result['duplicate_account'] : 0,
           'accounts_unclaimed': result['accounts_unclaimed'] ? result['accounts_unclaimed'] : 0
         };
-        this.uploadedDetails['total_uploaded'] = this.uploadedDetails['accounts_validated'] + 
-        this.uploadedDetails['accounts_rejected'] + this.uploadedDetails['accounts_failed']
-        + this.uploadedDetails['duplicate_account'] + this.uploadedDetails['accounts_unclaimed'];
+        this.uploadedDetails['total_uploaded'] = this.uploadedDetails['accounts_validated'] +
+        this.uploadedDetails['accounts_rejected'] + this.uploadedDetails['accounts_failed'] +
+        this.uploadedDetails['duplicate_account'] + this.uploadedDetails['accounts_unclaimed'];
       },
       error => {
         console.log(error);
