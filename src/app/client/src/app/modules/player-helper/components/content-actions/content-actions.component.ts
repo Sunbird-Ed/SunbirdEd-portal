f1a68a0e1a0348c76863856767675d1f98fc757f
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-04-27 14:24:52 +0530

    Issur #SB-19138 CCFT : player UI changes

--- src/app/client/src/app/modules/player-helper/components/content-actions/content-actions.component.ts
+++ src/app/client/src/app/modules/player-helper/components/content-actions/content-actions.component.ts
@@ -45,7 +45,7 @@ export class ContentActionsComponent implements OnInit, OnChanges {
     this.contentPrintable();
   }
   ngOnChanges(changes: SimpleChanges) {
-    console.log(changes.contentData);
+    // console.log(changes.contentData);
     this.contentPrintable();
   }
     onActionButtonClick(event, content) {
@@ -96,7 +96,7 @@ export class ContentActionsComponent implements OnInit, OnChanges {
     }
   contentPrintable() {
     // selectedContent?.model?.itemSetPreviewUrl
-    console.log('------>', this.contentData);
+   // console.log('------>', this.contentData);
     _.forEach(this.actionButtons, data => {
       if (data.name === 'print') {
         if (this.contentData.itemSetPreviewUrl) {
@@ -105,7 +105,7 @@ export class ContentActionsComponent implements OnInit, OnChanges {
           data.disable = true;
         }
       }
-    })
+    });
   }
 
   }
