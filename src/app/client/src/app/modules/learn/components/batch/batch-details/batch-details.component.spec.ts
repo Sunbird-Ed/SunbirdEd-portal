154114342006e213bff588232c92745bd1e4035e
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-05-06 12:44:40 +0530

    Issue #SB-19204 Issue in editing the course batch enrolment date and course duration

--- src/app/client/src/app/modules/learn/components/batch/batch-details/batch-details.component.spec.ts
+++ src/app/client/src/app/modules/learn/components/batch/batch-details/batch-details.component.spec.ts
@@ -15,7 +15,7 @@ class RouterStub {
 }
 const fakeActivatedRoute = {
   'params': observableOf({ courseId: 'do_1125083286221291521153' }),
-  'queryParams': observableOf({ enrollmentType: 'open'})
+  'queryParams': observableOf({})
 };
 const resourceServiceMockData = {
   messages : {
@@ -50,6 +50,7 @@ describe('BatchDetailsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BatchDetailsComponent);
     component = fixture.componentInstance;
+
   });
   it('should fetch only open batch of course if course is not enrolled and user is not mentor', () => {
     const courseBatchService = TestBed.get(CourseBatchService);
@@ -121,12 +122,12 @@ describe('BatchDetailsComponent', () => {
     expect(component.courseBatchService.getAllBatchDetails).toHaveBeenCalledWith(searchParams);
   });
   it('should navigate to update batch route', () => {
-      const courseBatchService = TestBed.get(CourseBatchService);
-      const route = TestBed.get(Router);
-      const queryParams = observableOf({enrollmentType: 'open'});
-      spyOn(courseBatchService, 'setUpdateBatchDetails');
-      component.batchUpdate({identifier: '123', queryParams:queryParams});
-      expect(route.navigate).toHaveBeenCalled();
+    const courseBatchService = TestBed.get(CourseBatchService);
+    const route = TestBed.get(Router);
+    spyOn(courseBatchService, 'setUpdateBatchDetails');
+    component.batchUpdate({ identifier: '123', enrollmentType: 'open' });
+    expect(route.navigate).toHaveBeenCalledWith(['update/batch', '123'],
+      { queryParams: { enrollmentType: 'open' }, relativeTo: component.activatedRoute });
   });
   it('should navigate to enroll route', () => {
     const courseBatchService = TestBed.get(CourseBatchService);
