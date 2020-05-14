b49957d5d41b8574f16193bf56d37e17da569272
Sourav <sourav_d@techjoomla.com>
2020-05-14 17:44:09 +0530

    Issue #SH-58 feat: Changing search request in workspace for new coursetype

--- src/app/client/src/app/modules/workspace/components/batch-page-section/batch-page-section.component.ts
+++ src/app/client/src/app/modules/workspace/components/batch-page-section/batch-page-section.component.ts
@@ -202,7 +202,6 @@ export class BatchPageSectionComponent extends WorkSpace implements OnInit, OnDe
         'identifier': _.uniq(courseIds),
         'status': ['Live'],
         'contentType': ['Course'],
-        'courseType': ['CurriculumCourse', 'TrainingCourse']
       }
     };
 
