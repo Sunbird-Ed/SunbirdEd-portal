cc1b24098bf425f7996860284b68421de0259626
swetha-aggidevara <swetha@ilimi.in>
2020-05-14 12:42:43 +0530

    Issue #SH-35 feat: added components for group

--- src/app/client/src/app/modules/groups/components/my-groups/my-groups.component.ts
+++ src/app/client/src/app/modules/groups/components/my-groups/my-groups.component.ts
@@ -1,22 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {CsModule} from '@project-sunbird/client-services';
 @Component({
   selector: 'app-my-groups',
   templateUrl: './my-groups.component.html',
   styleUrls: ['./my-groups.component.scss']
 })
 export class MyGroupsComponent implements OnInit {
-  groupService;
-  constructor() {
-    this.groupService = CsModule.instance.groupService;
-  }
+  constructor() {}
 
-  ngOnInit() {
-    this.createGroup();
-  }
-
-  async createGroup() {
-   const groupData =  await this.groupService.create('Test Group', 'ICSE', 'English', 'Class 8', 'English').toPromise();
-   console.log('groupDatattata', groupData);
-  }
+  ngOnInit() {}
 }
