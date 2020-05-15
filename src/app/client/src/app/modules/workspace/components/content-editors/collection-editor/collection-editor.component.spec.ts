f0637b19db427233a64a23615eef9e8befdbfc2a
sudip-mukherjee <38308961+sudip-mukherjee@users.noreply.github.com>
2020-05-15 09:19:08 +0530

    Issue #SH-71 feat: Passing board with the context object for courses (#4079)
    * Issue #SH-3 fix: Fetching default couse framework for traing page and workspace creation
    
    * Issue #SH-71 feat: Passing board with the context object for courses
    
    * ISsue #SH-71 fix: Added test cases

--- src/app/client/src/app/modules/workspace/components/content-editors/collection-editor/collection-editor.component.spec.ts
+++ src/app/client/src/app/modules/workspace/components/content-editors/collection-editor/collection-editor.component.spec.ts
@@ -28,7 +28,13 @@ class RouterStub {
 class NavigationHelperServiceStub {
   public navigateToWorkSpace() {}
 }
-const mockUserService = { userProfile: { userId: '68777b59-b28b-4aee-88d6-50d46e4c35090'} };
+const mockUserService = { userProfile: {
+  userId: '68777b59-b28b-4aee-88d6-50d46e4c35090',
+  organisationIds: [],
+  framework: {
+    board: ['CBSE']
+  }
+}};
 describe('CollectionEditorComponent', () => {
   let component: CollectionEditorComponent;
   let fixture: ComponentFixture<CollectionEditorComponent>;
@@ -107,5 +113,13 @@ describe('CollectionEditorComponent', () => {
     spyOn<any>(component, 'updateModeAndStatus').and.stub();
     component['setWindowConfig']();
     expect(window.config.nodeDisplayCriteria).toEqual(windowConfigData);
+  });
+
+  it('should pass user selected board in the context for courses', () => {
+    const userService = TestBed.get(UserService);
+    component['userProfile'] = userService.userProfile;
+    component['routeParams'] = {type: 'course'};
+    component['setWindowContext']();
+    expect(window.context.board).toEqual(['CBSE']);
   });
 });
