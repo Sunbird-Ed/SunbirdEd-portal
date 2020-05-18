ce9b6bd25ec82db6a03cf9174d1760f33b33ec5d
Sourav Dey <sourav_d@techjoomla.com>
2020-05-18 17:52:01 +0530

    Issue #SH-67 feat: Adding create user and choose user componenet (#4103)
    * Issue #SH-67 feat: Adding usercreate and choose user componenet
    
    * Issue #SH-67 feat: Adding usercreate and choose user componenet
    
    * Issue #SH-67 feat: Adding usercreate and choose user componenet

---
+++ src/app/client/src/app/plugins/profile/components/choose-user/choose-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-choose-user',
+  templateUrl: './choose-user.component.html',
+  styleUrls: ['./choose-user.component.scss']
+})
+export class ChooseUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
