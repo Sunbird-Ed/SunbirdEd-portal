b447901ec207d1c7d6796660a19f148690564941
Nikunj Beladiya <nikunj.beladiya@tarento.com>
2020-05-18 12:34:24 +0530

    Issue #SH-35 fix: Added Functional welcome popup

--- src/app/client/src/app/modules/public/module/group/components/explore-group/explore-group.component.ts
+++ src/app/client/src/app/modules/public/module/group/components/explore-group/explore-group.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExploreGroupComponent implements OnInit {
   hideLoginContainer;
+  showWelcomePopup = true;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  redirectTologin() {
+    window.location.href = '/groups';
   }
 
 }
