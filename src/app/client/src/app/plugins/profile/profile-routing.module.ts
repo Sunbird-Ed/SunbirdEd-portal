7a8c51aabf5d6b6609bcfe7eaad0b77adf2433fe
Sourav <sourav_d@techjoomla.com>
2020-05-18 16:38:15 +0530

    Issue #SH-67 feat: Adding usercreate and choose user componenet

--- src/app/client/src/app/plugins/profile/profile-routing.module.ts
+++ src/app/client/src/app/plugins/profile/profile-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProfilePageComponent } from './components';
+import { ProfilePageComponent, CreateUserComponent, ChooseUserComponent } from './components';
 const telemetryEnv = 'profile';
 const objectType = 'profile';
 const routes: Routes = [
   {
     path: '', component: ProfilePageComponent,
+    data: {
+      telemetry: {
+        env: telemetryEnv, type: 'view', mode: 'create', subtype: 'paginate', object: { type: objectType, ver: '1.0' }
+      }, breadcrumbs: [{ label: 'Home', url: '/home' }, { label: 'Profile', url: '' }]
+    }
+  },
+  {
+    path: 'create-user', component: CreateUserComponent,
+    data: {
+      telemetry: {
+        env: telemetryEnv, type: 'view', mode: 'create', subtype: 'paginate', object: { type: objectType, ver: '1.0' }
+      }, breadcrumbs: [{ label: 'Home', url: '/home' }, { label: 'Profile', url: '' }]
+    }
+  },
+  {
+    path: 'choose-user', component: ChooseUserComponent,
     data: {
       telemetry: {
         env: telemetryEnv, type: 'view', mode: 'create', subtype: 'paginate', object: { type: objectType, ver: '1.0' }
