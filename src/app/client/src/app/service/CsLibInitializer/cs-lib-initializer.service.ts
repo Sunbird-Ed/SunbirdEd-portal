be9338175df4f1c198c977e8208eba6ba02f7092
anoophm <anoophm44@gmail.com>
2020-05-28 18:46:18 +0530

    Issue 18983 fix: removed clientService reference from app and coreModule. Lazy load when groupModule loads

---
+++ src/app/client/src/app/service/CsLibInitializer/cs-lib-initializer.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { CsModule } from '@project-sunbird/client-services';
+import { UserService } from '@sunbird/core';
+import { first } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CsLibInitializerService {
+  fingerprintInfo = '';
+
+  constructor(public userService: UserService) { }
+
+  private _initializeCs() {
+    this.fingerprintInfo = (<HTMLInputElement>document.getElementById('deviceId')).value;
+    if (!CsModule.instance.isInitialised) {
+       // Singleton initialised or not
+        CsModule.instance.init({
+          core: {
+              httpAdapter: 'HttpClientBrowserAdapter',
+              global: {
+                  channelId: this.userService.hashTagId, // required
+                  producerId: this.userService.appId, // required
+                  deviceId: this.fingerprintInfo // required
+              },
+              api: {
+                  host: document.location.origin, // default host
+                  authentication: {
+                      // userToken: string; // optional
+                      // bearerToken: string; // optional
+                  }
+              }
+          },
+          services: {
+              groupServiceConfig: {
+                apiPath: 'learner/v1/group',
+              }
+          }
+      });
+    }
+  }
+  initializeCs() {
+    this.userService.userData$.pipe(first())
+    .subscribe(this._initializeCs);
+  }
+}
