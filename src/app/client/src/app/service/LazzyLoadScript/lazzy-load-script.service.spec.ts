eeede09ee1d9cdab16fba272a8ae77eee26f3251
anoophm <anoophm44@gmail.com>
2020-06-02 14:11:52 +0530

    Issue 18983 fix: testcase for lazzyLoadScriptService

--- src/app/client/src/app/service/LazzyLoadScript/lazzy-load-script.service.spec.ts
+++ src/app/client/src/app/service/LazzyLoadScript/lazzy-load-script.service.spec.ts
@@ -5,8 +5,16 @@ import { LazzyLoadScriptService } from './lazzy-load-script.service';
 describe('LazzyLoadScriptService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
+  it('should fetch script and return observable that completes on script load', () => {
     const service: LazzyLoadScriptService = TestBed.get(LazzyLoadScriptService);
+    const mockScript = {type: '', src: '', onload: () => {}};
+    spyOn(service['document'], 'createElement').and.returnValue(mockScript);
+    spyOn(service['document']['body'], 'appendChild').and.returnValue({});
+    const observable = service.loadScript('test.js');
+    mockScript.onload();
     expect(service).toBeTruthy();
+    observable.subscribe(() => {
+      expect(service['_loadedLibraries']['test.js']).toBeDefined();
+    });
   });
 });
