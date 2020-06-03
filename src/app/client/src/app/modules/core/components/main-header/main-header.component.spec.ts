75d4bdef7c7222c8d7d869ef2e18f1639a4e707f
Sourav <sourav_d@techjoomla.com>
2020-06-03 18:05:35 +0530

    Issue #SH-243 feat: Test cases

--- src/app/client/src/app/modules/core/components/main-header/main-header.component.spec.ts
+++ src/app/client/src/app/modules/core/components/main-header/main-header.component.spec.ts
@@ -1,4 +1,4 @@
-import {of as observableOf, throwError as observableThrowError, of, observable} from 'rxjs';
+import {of as observableOf, throwError as observableThrowError} from 'rxjs';
 import { RouterTestingModule } from '@angular/router/testing';
 import { mockUserData } from './../../services/user/user.mock.spec.data';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
