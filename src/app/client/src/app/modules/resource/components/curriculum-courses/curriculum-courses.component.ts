18cbda87571ef9643d22aa5f14968d076b35bc4d
Vivek kasture <vivek_kasture@techjoomla.com>
2020-06-03 22:56:26 +0530

    Issue #SH-311 fix: Added course logo for image path (#4250)
    * Issue #SH-131 merge: Merge with release-3.0.0
    
    * Issue #SH-131 feat: Added course details page for non-logged in users
    
    * Issue #SH-131 feat: Reverted files
    
    * Issue #SH-131 feat: Reverted changes
    
    * Issue #SH-131 feat: Reverted component
    
    * Issue #SH-131 feat: Reverted component
    
    * Issue #SH-131 feat: Reverted components
    
    * Issue #SH-131 feat: Removed duplicate entry
    
    * Issue #SH-131 feat: Removed groups link from menu
    
    * Issue #SH-131 feat: Removed route for groups
    
    * Issue #SH-131 feat: Public course player UI revamp
    
    * Issue #SH-131 feat: Removed unused lifecycle hook
    
    * Issue #131 feat: Added assessment events for content-player
    
    * Issue #SH-131 feat: Added test cases
    
    * Issue #SH-131 feat: Updated events
    
    * Issue #SH-131 feat: Added assessment-player
    
    * Issue #SH-131 fix: Fixed test cases
    
    * Issue #SH-131 feat: Updated course progress workflow
    
    * Issue #SH-131 feat: Updated test cases
    
    * Issue #SH-131 feat: Updated test case
    
    * Issue #SH-131 fix: Fixed test cases
    
    * Issue #SH-131 fix: Review comments resolved
    
    * Issue #SH-131 fix: Test cases fixed
    
    * Issue #SH-131 feat: Updated test cases
    
    * Issue #SH-131 fix: Test cases fixed
    
    * Issue #SH-131 feat: Unsubscribed events
    
    * Issue #SH-131 feat: Test cases issue fixed
    
    * Issue #SH-131 fix: Fixed  test cases
    
    * Issue #SH-131 feat: Test cases updated
    
    * Issue #SH-131 fix: Fixed test cases
    
    * Issue #SH-131 feat: Updated Assessment player comeponent
    
    * Issue #SH-131 feat: Updated button status
    
    * Issue #SH-131 feat: Updated progress calculation
    
    * Issue #SH-131 feat: Courseplayer component updated
    
    * Issue #SH-131 fix: Fixed progress calculation
    
    * Issue #SH-131 feat: Added test cases
    
    * Issue #SH-131 feat: Added test cases
    
    * Issue #SH-131 feat: Added test cases
    
    * Issue #SH-131 fix: Fixed test cases
    
    * Issue #SH-131 fix: Test cases fixed
    
    * Issue #SH-131 fix: Fixed test cases
    
    * Issue #SH-131 fix: Fixed test cases
    
    * Issue #SH-131 fix: Fixed test cases
    
    * Issue #SH-131 fix: Fixed test cases
    
    * Issue #SH-131 fix: Fixed test cases
    
    * Issue #SH-277 fix: Hide credit link when there is no required data available to show
    
    * Issue #SH-286 fix: Showing Course details for course creator
    
    * Issue #SH-277 fix: Resolved review comments
    
    * Issue #SH-319 fix: Added course logo url as fallback logo
    
    * Issue #SH-311 fix: Added courseLogo url
    
    * Issue #SH-311 fix: Removed console logs
    
    * Issue #SH-311 feat: Updated package version

--- src/app/client/src/app/modules/resource/components/curriculum-courses/curriculum-courses.component.ts
+++ src/app/client/src/app/modules/resource/components/curriculum-courses/curriculum-courses.component.ts
@@ -30,6 +30,7 @@ export class CurriculumCoursesComponent implements OnInit, OnDestroy {
   public courseList: any = [];
   public title: string;
   public mergedCourseList: any = [];
+  fallbackImg = './../../../../../assets/images/book.png';
 
   public telemetryImpression: IImpressionEventInput;
   constructor(private searchService: SearchService, private toasterService: ToasterService,
