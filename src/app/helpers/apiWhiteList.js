0ae82989fe775842a5292f84765cf33417f0705a
Rajesh <rajesh.kumaravel@tarento.com>
2020-06-11 12:02:14 +0530

    Issue #SB-19488 fix: Added content routes for whitelisting.

--- src/app/helpers/apiWhiteList.js
+++ src/app/helpers/apiWhiteList.js
@@ -5,31 +5,27 @@
  * @version 1.0
  */
 
-const _ = require('lodash');
-const logger = require('sb_logger_util_v2');
-const uuidv1 = require('uuid/v1');
-const dateFormat = require('dateformat');
-const ROUTE = require('./apiLists');
+const _           = require('lodash');
+const logger      = require('sb_logger_util_v2');
+const uuidv1      = require('uuid/v1');
+const dateFormat  = require('dateformat');
+const ROUTE       = require('./whitelistApis');
 
 /**
- * @param  {String} routeType - Route category [LEARNER, CONTENT]
  * @description - Function to check whether incoming API is whitelisted (or) not
- * 1. `routeType` is required to identify the category of API belongs to
- * 2. `routeType` will be set to `DEFAULT` if not passed
- * 3. If incoming API is present in API_LISTS and is enabled then request is passed further
- * 4. If API is not enabled then request is terminated and 401 UNAUTHORIZED response is sent
+ * 1. If incoming API is present in API_LISTS (whitelistApis); then request is passed further
+ * 2. If API is does not exists then request is terminated and 401 UNAUTHORIZED response is sent
  * @since release-3.1.0
  */
-const isAPIWhiteListed = (routeType) => {
-  if (!routeType) routeType = 'DEFAULT';
+const isAPIWhiteListed = () => {
   return function (req, res, next) {
-    if (_.get(ROUTE, urlMatch(routeType, req.originalUrl))) {
+    const REQ_URL = req.originalUrl;
+    if (_.get(ROUTE, REQ_URL)) {
       next();
     } else {
-      //TODO  Err message needs to be finalized
       logger.error({
-        msg: 'API WHITELIST :: Type [ ' + routeType + ' ]' + '. API [ ' + req.originalUrl + ' ]',
-        url: req.originalUrl
+        msg: 'API WHITELIST :: API [ ' + REQ_URL + ' ]',
+        url: REQ_URL
       });
       res.status(401);
       res.send({
@@ -49,16 +45,6 @@ const isAPIWhiteListed = (routeType) => {
       res.end()
     }
   }
-};
-
-/**
- * @param  {String} routeType - Route category [LEARNER, CONTENT]
- * @param  {String} url       - Request original URL
- * @description - Function to construct string using params
- * @returns  {String} - String to pass as argument in _.get
- */
-const urlMatch = (routeType, url) => {
-  return routeType.toUpperCase() + '[' + url + ']';
 };
 
 module.exports = {
