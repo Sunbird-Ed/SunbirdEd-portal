6de3732a052bb699499201b930a142e464a06ce9
anoophm <anoophm44@gmail.com>
2020-06-12 20:20:48 +0530

    Issue #TG-64 fix: added keep alive and timeout for keycloak api's

--- src/app/routes/refreshTokenRoutes.js
+++ src/app/routes/refreshTokenRoutes.js
@@ -35,11 +35,9 @@ const setConnectionTimeout = (time) => {
 }
 module.exports = (app) => {
 
-  app.post('/auth/v1/refresh/token', // setConnectionTimeout(120000), 
-  bodyParser.urlencoded({ extended: false }), bodyParser.json({ limit: '10mb' }),
+  app.post('/auth/v1/refresh/token', setConnectionTimeout(120000), bodyParser.urlencoded({ extended: false }), bodyParser.json({ limit: '10mb' }),
     async (req, res) => {
       logger.info({msg: '>>>> /auth/v1/refresh/token called'});
-      console.log("Refresh token api called2");
       try {
        let refreshToken = req.body.refresh_token;
 
@@ -58,8 +56,8 @@ module.exports = (app) => {
           throw { error: 'INVALID_CLIENT', message: "client not supported", statusCode: 400 }
         }
         let options = {
-          // forever: true,
-          // timeout: 120000,
+          forever: true,
+          timeout: 120000,
           method: 'POST',
           url: `${envHelper.PORTAL_AUTH_SERVER_URL}/realms/${envHelper.PORTAL_REALM}/protocol/openid-connect/token`,
           form: {
@@ -84,8 +82,6 @@ module.exports = (app) => {
         })
       } catch(error) {
         logError(req, error, "Refresh Token failed");
-        logger.error({mes: "Refresh token api failed"});
-        console.log("Refresh token api failed2")
         res.status(error.statusCode || 500).json({
           'id': 'api.refresh.token',
           'ver': '1.0',
@@ -118,8 +114,8 @@ const handleError = (error) => {
 const verifyAuthToken = async (req) => {
   let options = {
     method: 'GET',
-    // forever: true,
-    // timeout: 120000,
+    forever: true,
+    timeout: 120000,
     url: envHelper.PORTAL_ECHO_API_URL + 'test',
     'rejectUnauthorized': false,
     headers: {
