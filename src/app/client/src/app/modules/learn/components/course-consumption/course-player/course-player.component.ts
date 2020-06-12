34f16499f259588d569d54de9c498de31f62aa21
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-06-12 14:20:16 +0530

    Issue #SH-407 fix: Course flatten

--- src/app/client/src/app/modules/learn/components/course-consumption/course-player/course-player.component.ts
+++ src/app/client/src/app/modules/learn/components/course-consumption/course-player/course-player.component.ts
@@ -394,6 +394,9 @@ export class CoursePlayerComponent implements OnInit, OnDestroy {
     this.telemetryService.interact(interactData);
   }
 
+  getAllBatchDetails(event) {
+    this.courseConsumptionService.getAllOpenBatches(event);
+  }
 
   ngOnDestroy() {
     this.unsubscribe.next();
