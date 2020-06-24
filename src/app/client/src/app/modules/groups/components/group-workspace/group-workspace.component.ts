030070d6492ba16c6a075e1ca28322af0d4601bd
vinod <vinodkumar@zool.in>
2020-06-24 18:19:14 +0530

    Issue #SH-130 group creation ui changes

--- src/app/client/src/app/modules/groups/components/group-workspace/group-workspace.component.ts
+++ src/app/client/src/app/modules/groups/components/group-workspace/group-workspace.component.ts
@@ -7,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupWorkspaceComponent {
   showModal = false;
-  modalName: string;
   addActivity() {
     this.showModal = true;
-    this.modalName = 'addActivity';
   }
   closeModal() {
     this.showModal = false;
