4f7db3c8c8f95e20919f82876fbb8b1a72885f73
vinod <vinodkumar@zool.in>
2020-06-25 16:45:57 +0530

    Issue #SH-512 UI html - Portal>>Group creation and management workflows

--- src/app/client/src/app/modules/groups/components/group-workspace/group-workspace.component.ts
+++ src/app/client/src/app/modules/groups/components/group-workspace/group-workspace.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { ResourceService, NavigationHelperService } from '@sunbird/shared';
 
 @Component({
   selector: 'app-group-workspace',
@@ -7,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupWorkspaceComponent {
   showModal = false;
+  showActivityList = false;
+  HideAddActivity = true;
+  showFilters: boolean = false;
+ 
+  constructor(private renderer: Renderer2, public resourceService: ResourceService,
+    private navigationHelperService: NavigationHelperService) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (e.target['tabIndex'] === -1) {
+        this.closeModal();
+      }
+     });
+  }
   addActivity() {
     this.showModal = true;
   }
   closeModal() {
     this.showModal = false;
+  }
+  ActivitiesList() {
+    this.showActivityList = true;
+    this.closeModal();
+    this.HideAddActivity = false;
+  }
+  filterList() {
+    this.showFilters = !this.showFilters;
   }
 }
