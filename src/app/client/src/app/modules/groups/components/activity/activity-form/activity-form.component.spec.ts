e62652866cb576a33d66e67bd005001b72e27985
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-06-28 21:08:40 +0530

    Issue #SH-151 feat: Added test cases

--- src/app/client/src/app/modules/groups/components/activity/activity-form/activity-form.component.spec.ts
+++ src/app/client/src/app/modules/groups/components/activity/activity-form/activity-form.component.spec.ts
@@ -1,16 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ActivityFormComponent } from './activity-form.component';
+import { ResourceService } from '@sunbird/shared';
 
 describe('ActivityFormComponent', () => {
   let component: ActivityFormComponent;
   let fixture: ComponentFixture<ActivityFormComponent>;
 
+  const resourceBundle = {
+    'messages': {
+      'fmsg': {
+        'm0085': 'There is some technical error',
+      }
+    },
+    'frmelmnts': {
+      'lbl': {}
+    }
+  };
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ActivityFormComponent ]
+      declarations: [ActivityFormComponent],
+      providers: [
+        { provide: ResourceService, useValue: resourceBundle }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
