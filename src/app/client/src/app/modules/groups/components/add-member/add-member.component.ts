52b3cb9e54ca1c50aa12253a19f69a1fee811036
Srinivasan <srini@zool.in>
2020-06-30 10:34:20 +0530

    Issue:#SH-515 Group creation and management workflows UI fixes

--- src/app/client/src/app/modules/groups/components/add-member/add-member.component.ts
+++ src/app/client/src/app/modules/groups/components/add-member/add-member.component.ts
@@ -1,21 +1,15 @@
+import { ResourceService } from'@sunbird/shared';
 import { Component, OnInit } from '@angular/core';
-import { ResourceService } from 'src/app/modules/shared';
-
 @Component({
   selector: 'app-add-member',
   templateUrl: './add-member.component.html',
   styleUrls: ['./add-member.component.scss']
 })
 export class AddMemberComponent {
-
   constructor(public resourceService: ResourceService) {
-    
   }
-  
   public  membersList = [
     {identifier: '1', initial: 'A', title: 'Abc', isAdmin: false,  isMenu: false, indexOfMember: 1}
   ];
   config={size:'small', isBold:true, isSelectable:false, view:"horizontal"}
-
 }
-
