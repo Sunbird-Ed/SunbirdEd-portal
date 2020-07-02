c15ee8bbebbbc55fdb5fb446bc2db50b91bc0e91
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-07-02 11:36:10 +0530

    Issue #SH-414 feat: Updated activity form text changes

--- src/app/client/src/app/modules/groups/components/activity/activity-form/activity-form.component.ts
+++ src/app/client/src/app/modules/groups/components/activity/activity-form/activity-form.component.ts
@@ -11,8 +11,15 @@ export class ActivityFormComponent implements OnInit {
   @Output() nextClick = new EventEmitter<{ activityType: string }>();
 
   activityTypes = [
-    { name: 'course', title: this.resourceService.frmelmnts.lbl.courses },
-    { name: 'textbooks', title: `${this.resourceService.frmelmnts.lbl.textbooks}&nbsp;(${this.resourceService.frmelmnts.lbl.comingSoon})` },
+    {
+      name: 'course',
+      title: this.resourceService.frmelmnts.lbl.courses,
+      disabled: false
+    }, {
+      name: 'textbooks',
+      title: `${this.resourceService.frmelmnts.lbl.textbooks} (${this.resourceService.frmelmnts.lbl.comingSoon})`,
+      disabled: true
+    },
   ];
 
   selectedActivity: any;
