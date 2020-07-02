96abb7f9e6b81b7f042da6d813e5e9ecba10e702
deveshMantra <48819442+deveshMantra@users.noreply.github.com>
2020-07-02 19:21:52 +0530

    Issue #SH-237 fix:state adming should download declared details (#4489)
    * Issue #SH-237 fix:state adming should download declared details
    
    * Issue #SH-237 fix:state adming should download declared details
    
    * Issue #SH-237 fix:state adming should download declared details

--- src/app/client/src/app/modules/manage/components/user-org-management/user-org-management.component.ts
+++ src/app/client/src/app/modules/manage/components/user-org-management/user-org-management.component.ts
@@ -60,12 +60,14 @@ export class UserOrgManagementComponent implements OnInit, AfterViewInit {
   public geoTableHeader;
   public geoTabledata = [];
   public userTableId = 'ValidatedUserDetailsTable';
+  public userDeclaredDetailsUrl;
   public userTableHeader;
   public userTabledata = [];
   public activatedRoute: ActivatedRoute;
   public resourceService: ResourceService;
   public telemetryImpression: IImpressionEventInput;
   public geoViewInteractEdata: IInteractEventEdata;
+  public userDeclaredDetailsEdata: IInteractEventEdata;
   public geoDownloadInteractEdata: IInteractEventEdata;
   public userViewInteractEdata: IInteractEventEdata;
   public userDownloadInteractEdata: IInteractEventEdata;
@@ -102,6 +104,7 @@ export class UserOrgManagementComponent implements OnInit, AfterViewInit {
     this.userService.userData$.pipe(first()).subscribe(async (user) => {
       if (user && user.userProfile) {
         this.userProfile = user.userProfile;
+        this.fetchDeclaredUserDetails();
         this.slug = await _.get(this.userService, 'userProfile.rootOrg.slug');
         if (user.userProfile && user.userProfile['rootOrg'] && !user.userProfile['rootOrg']['isSSOEnabled']) {
           this.getUserJSON();
@@ -112,6 +115,24 @@ export class UserOrgManagementComponent implements OnInit, AfterViewInit {
         this.getUserDetail();
       }
     });
+  }
+
+  downloadFile(path) {
+    window.open(path, '_blank');
+  }
+
+  fetchDeclaredUserDetails() {
+    let channelName = _.get(this.userProfile, 'rootOrg.channel');
+    if (channelName) {
+      channelName = channelName.toLowerCase() + '.csv';
+      this.manageService.getData('declared_user_detail', channelName).subscribe(response => {
+          const url = (_.get(response, 'result.signedUrl'));
+          if (url) {
+            this.userDeclaredDetailsUrl = url;
+          }
+        }
+      );
+    }
   }
 
   ngAfterViewInit() {
@@ -130,6 +151,11 @@ export class UserOrgManagementComponent implements OnInit, AfterViewInit {
       this.geoViewInteractEdata = {
         id: 'geo-details',
         type: 'view',
+        pageid: this.activatedRoute.snapshot.data.telemetry.pageid
+      };
+      this.userDeclaredDetailsEdata = {
+        id: 'user-declared-details',
+        type: 'click',
         pageid: this.activatedRoute.snapshot.data.telemetry.pageid
       };
       this.geoDownloadInteractEdata = {
