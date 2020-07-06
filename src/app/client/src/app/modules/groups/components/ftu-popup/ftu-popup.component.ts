f2532edefdc2e2a19f7e5dd15e1f1a5afd6eaba4
srinivasan pusuluri <srini@zool.in>
2020-07-06 10:52:34 +0530

    Issue:#SH-515 Group Creation UI fixes and carouse updates for Instance ID (#4505)
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue #SH-000 feat: added routes and components for groups
    
    * Issue:SH-130 Groups creation UI upadtes
    
    * Issue:SH-130 Groups creation UI upadtes
    
    * Issue #SH-130 fix: resolved merge conflicts
    
    * Issue #SH-130 fix: resolved merge conflicts
    
    * Issue #SH-130 fix: resolved merge conflicts
    
    * Issue #SH-130 fix: fixed review changes
    
    * Issue #SH-130 fix: fixed review changes
    
    * Issue:SH-515 - Group user mgmt UI creation and modifications
    
    * Issue:SH-515 - Group user mgmt UI creation and modifications
    
    * Issue:SH-515 - Group user mgmt UI creation and modifications
    
    * Issue #SH-130 fix: groups integartion
    
    * mend
    
    Issue #SH-35 fix: groups integartion
    
    * mend
    
    Issue #SH-35 fix: groups integartion
    
    * mend
    
    Issue #SH-35 fix: groups integartion
    
    * mend
    
    Issue #SH-35 fix: user can create groups
    
    * Issue:SH-515 - Group Creation UI updates & Fixes
    
    * mend
    
    Issue #SH-35 fix: user can create groups
    
    * Issue #SH-35 fix: user can create groups
    
    * Issue #SH-35 fix: user can create groups
    
    * Issue #SH-35 fix: user can create groups
    
    * Issue #SH-35 fix: user can create groups
    
    * Issue #SH-35 fix: user can create groups
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:#SH-515 Group creation and management workflows UI fixes
    
    * Issue:SH-515 Group Creation UI fixes for mobile web view & othrs
    
    * Issue:#SH-515 Group Creation UI FTU for instace ID
    
    * Issue:#SH-515 Group Creation UI FTU for instace ID
    
    * Issue:#SH-515 Group Creation UI FTU for instace ID
    
    * Issue:#SH-515 Group Creation UI FTU for instace ID
    
    * Issue:#SH-515 Group UI fixes
    
    * Issue:#SH-515 Group Creation UI fixes and carouse updates for Instance ID
    
    Co-authored-by: swetha-aggidevara <swetha@ilimi.in>

--- src/app/client/src/app/modules/groups/components/ftu-popup/ftu-popup.component.ts
+++ src/app/client/src/app/modules/groups/components/ftu-popup/ftu-popup.component.ts
@@ -1,32 +1,38 @@
 import { ResourceService } from '@sunbird/shared';
-import { Component, OnInit, Output, EventEmitter, Input, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-ftu-popup',
   templateUrl: './ftu-popup.component.html',
   styleUrls: ['./ftu-popup.component.scss']
 })
-export class FtuPopupComponent implements OnInit {
+export class FtuPopupComponent implements OnInit, AfterViewInit {
   @Input() showWelcomePopup;
   @Output() close = new EventEmitter();
-  slideConfig1 = {
-    "lazyLoad": 'progressive',
-    "slidesToShow": 1,
-    "infinite": false,
-    "rtl": false,
-    "dots": true,
-    "adaptiveHeight": true,
-    "fade": true,
-    "cssEase": 'linear',
-    "autoplay": true,
-    "autoplaySpeed": 2000
-  };
 
   @Input()showMemberPopup;
   constructor(public resourceService: ResourceService, private elementRef: ElementRef) { }
+  slideConfig1 = {};
+  instance;
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.instance = this.resourceService.instance;
+    this.slideConfig1 = {
+      // "lazyLoad": 'progressive',
+      "slidesToShow": 1,
+      "infinite": false,
+      "rtl": false,
+      "dots": true,
+      "adaptiveHeight": true
+      //"fade": true,
+      //"cssEase": 'linear',
+      // "autoplay": true,
+      // "autoplaySpeed": 2000
+    };
+  }
+  ngAfterViewInit(){
+ 
+  }
 
   closeModal() {
     this.showWelcomePopup = false;
