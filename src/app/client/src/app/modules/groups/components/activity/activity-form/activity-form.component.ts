5945d9cfa9139fc91a6a36e52e44930e83137608
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-07-08 22:34:51 +0530

    Issue #SH-415 feat: Added form API for activities form

--- src/app/client/src/app/modules/groups/components/activity/activity-form/activity-form.component.ts
+++ src/app/client/src/app/modules/groups/components/activity/activity-form/activity-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { ResourceService } from '@sunbird/shared';
+import { FormService } from '@sunbird/core';
 
 @Component({
   selector: 'app-activity-form',
@@ -7,30 +8,33 @@ import { ResourceService } from '@sunbird/shared';
   styleUrls: ['./activity-form.component.scss']
 })
 export class ActivityFormComponent implements OnInit {
-
   @Output() nextClick = new EventEmitter<{ activityType: string }>();
 
-  activityTypes = [
-    {
-      name: 'course',
-      title: this.resourceService.frmelmnts.lbl.courses,
-      disabled: false
-    }, {
-      name: 'textbooks',
-      title: `${this.resourceService.frmelmnts.lbl.textbooks} (${this.resourceService.frmelmnts.lbl.comingSoon})`,
-      disabled: true
-    },
-  ];
-
+  activityTypes;
   selectedActivity: any;
-  constructor(public resourceService: ResourceService) { }
+  constructor(public resourceService: ResourceService, private formService: FormService) { }
 
   ngOnInit() {
-    this.chooseActivity(this.activityTypes[0]);
+    this.getFormDetails();
+  }
+
+  private getFormDetails() {
+    const formServiceInputParams = {
+      formType: 'group',
+      contentType: 'activities',
+      formAction: 'list'
+    };
+    this.formService.getFormConfig(formServiceInputParams).subscribe(fields => {
+      fields.forEach(item => { item.title = this.resourceService.frmelmnts.lbl[item.title]; });
+      this.activityTypes = [...fields];
+      this.chooseActivity(this.activityTypes[0]);
+    }, error => {
+      console.error('Error while getting activity form data', error);
+    });
+
   }
 
   chooseActivity(value: any) {
-    // TODO: Handle telemetry here
     this.selectedActivity = value;
   }
 
