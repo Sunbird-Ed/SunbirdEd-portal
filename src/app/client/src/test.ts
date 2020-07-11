6cc353337ade97666f1dfffc2a8cee9aff5815cb
swetha-aggidevara <swetha@ilimi.in>
2020-07-11 17:39:55 +0530

    Issue #SH-36, SH-418, Sh-38, SH-37, SH-417 feat: added telemetry events

--- src/app/client/src/test.ts
+++ src/app/client/src/test.ts
@@ -45,8 +45,12 @@ if (!CsModule.instance.isInitialised) {
      },
      services: {
          groupServiceConfig: {
-           apiPath: 'learner/v1/group',
-         }
+           apiPath: '/learner/group/v1',
+           dataApiPath: '',
+         },
+         userServiceConfig: {
+          apiPath: '/learner/user/v1',
+        }
      }
  });
 }
