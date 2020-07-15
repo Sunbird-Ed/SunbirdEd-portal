eb498b70429c1a5baeb31cb23a70ccfa0951d9f4
swetha-aggidevara <swetha@ilimi.in>
2020-07-15 12:12:13 +0530

    Issue #SH-414: fix added telemtry

--- src/app/client/src/app/modules/groups/components/activity/activity-search/activity-search.component.ts
+++ src/app/client/src/app/modules/groups/components/activity/activity-search/activity-search.component.ts
@@ -216,8 +216,12 @@ export class ActivitySearchComponent implements OnInit {
   }
 
   addActivity(event) {
-    this.groupsService.addTelemetry('activity-course-card', this.activatedRoute.snapshot, [{id: _.get(event, 'data.identifier'), type: 'Course'}]);
+    this.addTelemetry('activity-course-card', event);
     this.router.navigate(['/learn/course', _.get(event, 'data.identifier')], { queryParams: { groupId: _.get(this.groupData, 'id') } });
+  }
+
+  addTelemetry(id, event) {
+    this.groupsService.addTelemetry(id, this.activatedRoute.snapshot, [{id: _.get(event, 'data.identifier'), type: 'Course'}]);
   }
 
   private setNoResultMessage() {
