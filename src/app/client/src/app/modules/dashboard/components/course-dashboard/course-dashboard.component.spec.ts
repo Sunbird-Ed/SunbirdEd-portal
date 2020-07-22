27ec9bf0697d54fc5e81e6d6fe3d98bdd94f5f80
swetha-aggidevara <swetha@ilimi.in>
2020-07-22 16:10:09 +0530

    Issue #SH-636 feat: added re-issue certificate components

--- src/app/client/src/app/modules/dashboard/components/course-dashboard/course-dashboard.component.spec.ts
+++ src/app/client/src/app/modules/dashboard/components/course-dashboard/course-dashboard.component.spec.ts
@@ -1,15 +1,27 @@
+import { mockUserData } from './../course-progress/course-progress.component.spec.data';
+import { TelemetryService } from '@sunbird/telemetry';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SharedModule } from '@sunbird/shared';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseDashboardComponent } from './course-dashboard.component';
 import { configureTestSuite } from '@sunbird/test-util';
+import { APP_BASE_HREF } from '@angular/common';
+import { of, throwError } from 'rxjs';
+import { CourseProgressService } from '../../services';
 
 describe('CourseDashboardComponent', () => {
   let component: CourseDashboardComponent;
   let fixture: ComponentFixture<CourseDashboardComponent>;
+  let courseProgressService: CourseProgressService;
   configureTestSuite();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CourseDashboardComponent ]
+      declarations: [ CourseDashboardComponent ],
+      imports: [SharedModule.forRoot(), HttpClientTestingModule],
+      providers: [CourseProgressService, TelemetryService,
+        {provide: APP_BASE_HREF, value: '/'},
+      ],
     })
     .compileComponents();
   }));
@@ -17,11 +29,49 @@ describe('CourseDashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CourseDashboardComponent);
     component = fixture.componentInstance;
+    courseProgressService = TestBed.get(CourseProgressService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should call getBatchList', () => {
+    spyOn(component, 'getBatchList');
     expect(component).toBeTruthy();
     component.ngOnInit();
+    expect(component.getBatchList).toHaveBeenCalled();
   });
+
+  it('should create', () => {
+    const searchParamsCreator = {
+      courseId: '123',
+      status: ['0', '1', '2']
+    };
+    spyOn(component['courseProgressService'], 'getBatches').and.returnValue(of (mockUserData.getBatchRes));
+    component.getBatchList();
+    courseProgressService.getBatches(searchParamsCreator).subscribe(data => {
+      expect(data).toEqual(mockUserData.getBatchRes);
+    });
+    expect(component['courseProgressService'].getBatches).toHaveBeenCalledWith(searchParamsCreator);
+  });
+
+  it('should create', () => {
+    const searchParamsCreator = {
+      courseId: '123',
+      status: ['0', '1', '2']
+    };
+    spyOn(component['courseProgressService'], 'getBatches').and.returnValue(throwError ([]));
+    component.getBatchList();
+    courseProgressService.getBatches(searchParamsCreator).subscribe(data => {
+    }, err => {
+      expect(err).toEqual([]);
+      expect(component.batchList).toEqual([]);
+    });
+    expect(component['courseProgressService'].getBatches).toHaveBeenCalledWith(searchParamsCreator);
+  });
+
+  it('should assign enrollment and completed count', () => {
+    component.batchList = [{id: 1, completedCount: 1, participantCount: 5}];
+    component.getEnrollmentAndCompletedCount();
+  });
+
+
 });
--- src/app/client/src/app/modules/dashboard/components/course-dashboard/course-dashboard.component.ts
+++ src/app/client/src/app/modules/dashboard/components/course-dashboard/course-dashboard.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-    /* istanbul ignore else */
+import { IDashboard } from '../../interfaces';
+import * as _ from 'lodash-es';
+import { CourseProgressService } from '../../services';
 @Component({
   selector: 'app-course-dashboard',
   templateUrl: './course-dashboard.component.html',
   styleUrls: ['./course-dashboard.component.scss']
 })
 export class CourseDashboardComponent implements OnInit {
+  batchList = [];
+  dashBoardData: IDashboard;
 
-  constructor() { }
+  constructor(private courseProgressService: CourseProgressService) { }
 
   ngOnInit() {
+  this.getBatchList();
   }
 
+  getBatchList() {
+  }
+
+  getEnrollmentAndCompletedCount() {
+  }
 }
