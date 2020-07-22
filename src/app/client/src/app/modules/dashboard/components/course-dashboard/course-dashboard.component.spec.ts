89c129f365f85ed3c687223e25806ca249a213ba
swetha-aggidevara <swetha@ilimi.in>
2020-07-22 12:14:32 +0530

    Issue #SH-636 feat: added re-issue certificate components

--- src/app/client/src/app/modules/dashboard/components/course-dashboard/course-dashboard.component.spec.ts
+++ src/app/client/src/app/modules/dashboard/components/course-dashboard/course-dashboard.component.spec.ts
@@ -1,11 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourseDashboardComponent } from './course-dashboard.component';
+import { configureTestSuite } from '@sunbird/test-util';
 
 describe('CourseDashboardComponent', () => {
   let component: CourseDashboardComponent;
   let fixture: ComponentFixture<CourseDashboardComponent>;
-
+  configureTestSuite();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CourseDashboardComponent ]
--- src/app/client/src/app/modules/dashboard/components/re-issue-certificate/re-issue-certificate.component.spec.ts
+++ src/app/client/src/app/modules/dashboard/components/re-issue-certificate/re-issue-certificate.component.spec.ts
@@ -1,11 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReIssueCertificateComponent } from './re-issue-certificate.component';
+import { configureTestSuite } from '@sunbird/test-util';
 
 describe('ReIssueCertificateComponent', () => {
   let component: ReIssueCertificateComponent;
   let fixture: ComponentFixture<ReIssueCertificateComponent>;
-
+  configureTestSuite();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ReIssueCertificateComponent ]
