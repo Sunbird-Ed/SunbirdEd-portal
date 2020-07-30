fdcc4c07069384eec90794aacb7679c2add0a95a
sharath <sharaths.kashyap@gmail.com>
2020-07-30 11:20:32 +0530

    Issue #SB-19808 Feat: Unit Portal Migration

--- src/app/client/src/app/modules/workspace/components/workspace/workspace.component.ts
+++ src/app/client/src/app/modules/workspace/components/workspace/workspace.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-import {LayoutService} from '@sunbird/shared';
+import { LayoutService } from '@sunbird/shared';
 
 @Component({
   selector: 'app-workspace',
