845c9b91bd4fd98902a71398bbf51a9d975d8ab0
Srinivasan <srini@zool.in>
2020-08-06 15:39:52 +0530

    Issue #SB-19948 - UI - MHRD Layout and styling changes for portal & CC demo

--- src/app/client/src/app/modules/core/components/main-footer/main-footer.component.ts
+++ src/app/client/src/app/modules/core/components/main-footer/main-footer.component.ts
@@ -60,7 +60,8 @@ export class MainFooterComponent implements OnInit, AfterViewInit {
   redoLayout() {
       if (this.layoutConfiguration != null) {
         this.FIRST_PANEL_LAYOUT = this.layoutService.redoLayoutCSS(0, this.layoutConfiguration, COLUMN_TYPE.threeToNine);
-        this.SECOND_PANEL_LAYOUT = this.layoutService.redoLayoutCSS(1, this.layoutConfiguration, COLUMN_TYPE.threeToNine) + ' sbt-page-content-area';
+        this.SECOND_PANEL_LAYOUT = this.layoutService.redoLayoutCSS(1, this.layoutConfiguration, COLUMN_TYPE.threeToNine);
+        // + ' sbt-page-content-area'
       } else {
         this.FIRST_PANEL_LAYOUT = this.layoutService.redoLayoutCSS(0, null, COLUMN_TYPE.fullLayout);
         this.SECOND_PANEL_LAYOUT = this.layoutService.redoLayoutCSS(1, null, COLUMN_TYPE.fullLayout);
