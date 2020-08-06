31f727de0fb2dd5c2037d19014741c2c210ac983
anoophm <anoophm44@gmail.com>
2020-08-06 12:43:57 +0530

    Merge branch 'release-3.2.0' of https://github.com/Sunbird-Ed/SunbirdEd-portal into logging-winston
    # Conflicts:
    #	src/app/routes/certRegRoutes.js

--- src/app/server.js
+++ src/app/server.js
@@ -1,5 +1,5 @@
 'use strict'
-const { logger, enableLogger } = require('@project-sunbird/logger');
+const { enableLogger } = require('@project-sunbird/logger');
 const envHelper = require('./helpers/environmentVariablesHelper.js');
 const path = require('path');
 enableLogger({
@@ -10,6 +10,7 @@ enableLogger({
     adopter: 'winston'
   }
 });
+const { logger } = require('@project-sunbird/logger');
 const express = require('express');
 const gracefulShutdown = require('http-graceful-shutdown');
 const proxy = require('express-http-proxy')
