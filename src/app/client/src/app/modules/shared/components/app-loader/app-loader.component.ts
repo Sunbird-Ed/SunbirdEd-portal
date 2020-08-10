34fe9a8f6a22a010f9f7cbde67cc4fe911ab09ec
Devesh <mailme.devmehta@gmail.com>
2020-08-10 13:34:11 +0530

    Issue #SB-19808 fix:added new layout for app loader

--- src/app/client/src/app/modules/shared/components/app-loader/app-loader.component.ts
+++ src/app/client/src/app/modules/shared/components/app-loader/app-loader.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import {ILoaderMessage} from '../../interfaces';
-import { ResourceService } from '../../services/index';
+import {LayoutService, ResourceService} from '../../services/index';
 import * as _ from 'lodash-es';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 /**
  * loader component
@@ -15,17 +17,29 @@ export class AppLoaderComponent implements OnInit {
   @Input() data: ILoaderMessage;
   headerMessage: string;
   loaderMessage: string;
+  layoutConfiguration: any;
+  private unsubscribe$ = new Subject<void>();
 
-  constructor(public resourceService: ResourceService) {
+  constructor(public resourceService: ResourceService, public layoutService: LayoutService) {
     this.resourceService = resourceService;
   }
 
   ngOnInit() {
+    this.initLayout();
     this.headerMessage = _.get(this.resourceService.messages.fmsg, 'm0087');
     this.loaderMessage = _.get(this.resourceService.messages.fmsg, 'm0088');
     if (this.data) {
       this.headerMessage = this.data.headerMessage || this.headerMessage;
       this.loaderMessage = this.data.loaderMessage || this.loaderMessage;
     }
+  }
+
+  initLayout() {
+    this.layoutConfiguration = this.layoutService.initlayoutConfig();
+    this.layoutService.switchableLayout().pipe(takeUntil(this.unsubscribe$)).subscribe(layoutConfig => {
+      if (layoutConfig != null) {
+        this.layoutConfiguration = layoutConfig.layout;
+      }
+    });
   }
 }
