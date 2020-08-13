4938920b5fff8fb4f070b68196a24b911c1a109b
Devesh <mailme.devmehta@gmail.com>
2020-08-13 15:11:52 +0530

    Issue #SB-19808 fix:added joy theme popup

--- src/app/client/src/app/modules/shared-feature/components/joy-theme-popup/joy-theme-popup.component.ts
+++ src/app/client/src/app/modules/shared-feature/components/joy-theme-popup/joy-theme-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-joy-theme-popup',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoyThemePopupComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
