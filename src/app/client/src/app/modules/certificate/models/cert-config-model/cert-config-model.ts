cc9b1410c426dae4feac0a99448ed785e02b8a19
sudip mukherjee <sudip.mukherjee@tarento.com>
2020-08-15 20:09:56 +0530

    Issue #SH-638 fix: Added edit cert flow

--- src/app/client/src/app/modules/certificate/models/cert-config-model/cert-config-model.ts
+++ src/app/client/src/app/modules/certificate/models/cert-config-model/cert-config-model.ts
@@ -1,32 +1,42 @@
 import * as _ from 'lodash-es';
-export enum Modes {
-    PROCESS_DROPDOWNS = 'processDropdowns',
-    PROCESS_CRITERIA = 'processCriteria'
-}
-export class CertConfigModel {
-    public _issueTo: any;
-    public _certTypes: any;
-    public _criteria: any;
 
-    constructor(data) {
-        if (_.get(data, 'mode') === Modes.PROCESS_DROPDOWNS) {
-            this.processDropDownValues(_.get(data, 'values'), _.get(data, 'rootOrgId'));
-        } else if (_.get(data, 'mode') === Modes.PROCESS_CRITERIA) {
-            this.processCriteria(_.get(data, 'values'));
-        }
+export class CertConfigModel {
+    private dropDownFields = {
+        COMPLETION_CERTIFICATE: 'Completion certificate',
+        MY_STATE_TEACHER : 'My state teacher',
+        ALL: 'All'
+    };
+    constructor() {
     }
 
     processDropDownValues(rawValues, rootOrgId) {
         const criteria = {};
-        criteria['user'] = _.get(rawValues, 'issueTo.type') === 'All' ? {} : {  rootOrgId: rootOrgId };
+        criteria['user'] = _.get(rawValues, 'issueTo.type') === this.dropDownFields.ALL ? {} : { rootOrgId: rootOrgId };
         criteria['enrollment'] = _.get(rawValues, 'certificateType.value');
-        this._criteria = criteria;
+        return criteria;
     }
 
     processCriteria(criteria) {
-        console.log(criteria);
         const dropDowns = {};
-        this._issueTo = [];
-        this._certTypes = [];
+        dropDowns['issueTo'] = _.get(criteria, 'user.rootOrgId') ?
+        [{ type: this.dropDownFields.MY_STATE_TEACHER }] : [{ type: this.dropDownFields.ALL }];
+        dropDowns['certTypes'] = _.get(criteria, 'enrollment.status') === 2 ? [{ type: this.dropDownFields.COMPLETION_CERTIFICATE }] : [{}];
+        return dropDowns;
+    }
+
+    getDropDownValues(certTypeData) {
+        const processedDropdownValues = {};
+        const certTypes = certTypeData.filter(val => {
+            return val.code === 'certTypes';
+        });
+        processedDropdownValues['certTypes'] = _.get(certTypes[0], 'range');
+
+        const issueTo = certTypeData.filter(data => {
+            return data.code === 'issueTo';
+        });
+        processedDropdownValues['issueTo'] = _.get(issueTo[0], 'range');
+
+        return processedDropdownValues;
+
     }
 }
