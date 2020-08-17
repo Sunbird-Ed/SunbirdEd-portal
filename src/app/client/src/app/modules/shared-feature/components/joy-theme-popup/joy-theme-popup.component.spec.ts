1b20753a6e74c4ea4ae2f4695e728ef846767cd2
Devesh <mailme.devmehta@gmail.com>
2020-08-17 16:53:35 +0530

    Issue #SB-19808 fix:fixed joy theme popup

--- src/app/client/src/app/modules/shared-feature/components/joy-theme-popup/joy-theme-popup.component.spec.ts
+++ src/app/client/src/app/modules/shared-feature/components/joy-theme-popup/joy-theme-popup.component.spec.ts
@@ -2,17 +2,26 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {SuiModule} from 'ng2-semantic-ui';
 
 import {JoyThemePopupComponent} from './joy-theme-popup.component';
+import {ResourceService, ConfigService, BrowserCacheTtlService, LayoutService, InterpolatePipe} from '@sunbird/shared';
 import {configureTestSuite} from '@sunbird/test-util';
 
 describe('JoyThemePopupComponent', () => {
   let component: JoyThemePopupComponent;
   let fixture: ComponentFixture<JoyThemePopupComponent>;
   configureTestSuite();
+  const resourceBundle = {
+    frmelmnts: {
+      lbl: {
+        textbook: 'textbook'
+      },
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [SuiModule],
-      declarations: [JoyThemePopupComponent]
+      declarations: [JoyThemePopupComponent, InterpolatePipe],
+      providers: [LayoutService, {provide: ResourceService, useValue: resourceBundle}, ConfigService]
     })
       .compileComponents();
   }));
@@ -35,5 +44,33 @@ describe('JoyThemePopupComponent', () => {
     expect(component.isShown).toBe(false);
     expect(component.closeJoyThemePopup.emit).toHaveBeenCalled();
     expect(localStorage.setItem).toHaveBeenCalledWith('joyThemePopup', 'true');
+  });
+
+  it('should not switch to old layout as already in old ui', () => {
+    component.isShown = true;
+    const layoutService = TestBed.get(LayoutService);
+    spyOn(layoutService, 'getLayoutConfig').and.returnValue(null);
+    spyOn(layoutService, 'initiateSwitchLayout');
+    spyOn(localStorage, 'setItem');
+    spyOn(component.closeJoyThemePopup, 'emit');
+    component.switchToOldLayout();
+    expect(component.isShown).toBe(false);
+    expect(component.closeJoyThemePopup.emit).toHaveBeenCalled();
+    expect(localStorage.setItem).toHaveBeenCalledWith('joyThemePopup', 'true');
+    expect(layoutService.initiateSwitchLayout).not.toHaveBeenCalled();
+  });
+
+  it('should switch to old layout as already', () => {
+    component.isShown = true;
+    const layoutService = TestBed.get(LayoutService);
+    spyOn(layoutService, 'getLayoutConfig').and.returnValue({layout: 'layout'});
+    spyOn(layoutService, 'initiateSwitchLayout');
+    spyOn(localStorage, 'setItem');
+    spyOn(component.closeJoyThemePopup, 'emit');
+    component.switchToOldLayout();
+    expect(component.isShown).toBe(false);
+    expect(component.closeJoyThemePopup.emit).toHaveBeenCalled();
+    expect(localStorage.setItem).toHaveBeenCalledWith('joyThemePopup', 'true');
+    expect(layoutService.initiateSwitchLayout).toHaveBeenCalled();
   });
 });
