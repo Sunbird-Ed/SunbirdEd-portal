37207a7e13a3b7b4f4033b26eb0770ae3552a6e3
Rajesh <rajesh.kumaravel@tarento.com>
2020-08-18 18:03:18 +0530

    Issue #SB-19777 fix: Landing page guard test case added.

--- src/app/client/src/app/modules/public/services/landingpage-guard/landingpage.guard.spec.ts
+++ src/app/client/src/app/modules/public/services/landingpage-guard/landingpage.guard.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, async, inject } from '@angular/core/testing';
-import { configureTestSuite } from '@sunbird/test-util';
 import { LandingpageGuard } from './landingpage.guard';
+import { configureTestSuite } from '@sunbird/test-util';
 
 describe('LandingpageGuard', () => {
   configureTestSuite();
@@ -11,6 +11,15 @@ describe('LandingpageGuard', () => {
   });
 
   it('should ...', inject([LandingpageGuard], (guard: LandingpageGuard) => {
+    const res = guard.canActivate(null, null);
     expect(guard).toBeTruthy();
+    expect(res).toEqual(true);
+  }));
+
+  it('should replace window location', inject([LandingpageGuard], (guard: LandingpageGuard) => {
+    document.body.innerHTML = document.body.innerHTML +
+      '<input id="defaultTenantIndexStatus" value="true" type="hidden" />';
+    const res = guard.canActivate(null, null);
+    expect(res).toEqual(false);
   }));
 });
