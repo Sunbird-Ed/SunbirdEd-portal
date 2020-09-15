fdf1df3d40a4c91d646ef53d634ba5ac7d21f0cf
Sourav <sourav_d@techjoomla.com>
2020-09-15 11:35:39 +0530

    Issue #SB-20292 feat: Create help module

--- src/app/client/src/app/modules/public/module/help/components/faq/faq.component.ts
+++ src/app/client/src/app/modules/public/module/help/components/faq/faq.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CacheService } from 'ng2-cache-service';
+import { UtilService } from '@sunbird/shared';
+import * as _ from 'lodash-es';
 
 @Component({
   selector: 'app-faq',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent implements OnInit {
+  faqList: any;
+  faqBaseUrl: string;
+  selectedLanguage: string;
+  showLoader = true;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private http: HttpClient, private _cacheService: CacheService, private utilService: UtilService) {
+    this.faqBaseUrl = 'https://ntpstagingall.blob.core.windows.net/public/faq/resources/res';
   }
 
+  ngOnInit() {
+    this.selectedLanguage = this._cacheService.get('portalLanguage') || 'en';
+    this.getFaqJson();
+    this.utilService.languageChange.subscribe((langData) => {
+      this.showLoader = true;
+      this.selectedLanguage = _.get(langData, 'value') || 'en';
+      this.getFaqJson();
+    });
+  }
+
+  private getFaqJson() {
+    this.faqList = undefined;
+    this.http.get(`${this.faqBaseUrl}/faq-${this.selectedLanguage}.json`).subscribe(data => {
+      this.faqList = data;
+      this.showLoader = false;
+    }, err => {
+      this.showLoader = false;
+    });
+  }
 }
