67008518c5876f5f0a549694bab83431cd25234d
Sourav <sourav_d@techjoomla.com>
2020-09-18 14:53:50 +0530

    Issue #SB-20657 fix: Changing help url to faq url

--- src/app/client/src/app/modules/public/module/help/components/faq/faq.component.ts
+++ src/app/client/src/app/modules/public/module/help/components/faq/faq.component.ts
@@ -43,7 +43,7 @@ export class FaqComponent implements OnInit {
       this.tenantFooter = data;
     });
     this.defaultFooterConfig = {
-      helpCenterLink: `/help/troubleshooting/textbook-errors/index.html`,
+      helpCenterLink: `/help/faqs/faq/index.html`,
       helpDeskEmail: `support@${_.lowerCase(this.instance)}-ncte.freshdesk.com`
     };
     this.selectedLanguage = this._cacheService.get('portalLanguage') || 'en';
