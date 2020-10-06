070dc9841b915f4e471f53163eef1710c11c1376
nani3592 <venkatkadiveti248@gmail.com>
2020-10-06 20:19:27 +0530

    Issue #SH-918 fix: Dashboard reports

---
+++ src/app/client/src/app/modules/shared/components/on-demand-reports/on-demand-reports-mock.data.spec.ts
@@ -0,0 +1,573 @@
+export const OnDemandReports = {
+  responseData: {
+    result: {
+      jobs: [
+        {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'progress-exhaust',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing request',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com', 'https://www.google.com',
+          ],
+          'expires_at': '2020-08-20'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing success',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com',
+          ],
+          'expires_at': '2020-09-20'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing failed',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-09-22'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Expired',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-08-20'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing success',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-09-30'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing success',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-07-20'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing success',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-10-20'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing success',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-09-10'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing success',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-09-20'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing success',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-09-20'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing success',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-09-20'
+        }, {
+          'request_id': 'AE3DDC23B3F189ED2A57B567D6434BE7',
+          'tag': 'test-tag:in.ekstep',
+          'job_id': 'assessment-dashboard-metrics',
+          'requested_by': 'client-1',
+          'requested_channel': 'in.ekstep',
+          'status': 'Processing success',
+          'last_updated': 1599728944037,
+          'request_data': {
+            'batchFilters': [
+              'TPD',
+              'NCFCOPY'
+            ],
+            'contentFilters': {
+              'request': {
+                'filters': {
+                  'identifier': [
+                    'do_11305960936384921612216',
+                    'do_1130934466492252161819'
+                  ],
+                  'prevState': 'Draft'
+                },
+                'sort_by': {
+                  'createdOn': 'desc'
+                },
+                'limit': 10000,
+                'fields': [
+                  'framework',
+                  'identifier',
+                  'name',
+                  'channel',
+                  'prevState'
+                ]
+              }
+            },
+            'reportPath': 'course-progress-v2/'
+          },
+          'attempts': 0,
+          'job_stats': {
+            'dt_job_submitted': 1599728944037,
+            'dt_job_completed': null,
+            'execution_time': null
+          },
+          'download_urls': [
+            'https://www.google.com'
+          ],
+          'expires_at': '2020-09-20'
+        },
+      ]
+    }
+  }
+}
