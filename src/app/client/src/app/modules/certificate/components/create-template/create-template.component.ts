b945d430918b74990c12520dbcd2fd88c7fbe7b9
ArtiSingh23 <arti@zool.in>
2020-10-13 17:03:58 +0530

    Issue#SH-1312 UI - support for create new certificate template

--- src/app/client/src/app/modules/certificate/components/create-template/create-template.component.ts
+++ src/app/client/src/app/modules/certificate/components/create-template/create-template.component.ts
@@ -12,47 +12,47 @@ export class CreateTemplateComponent implements OnInit {
 
   public unsubscribe$ = new Subject<void>();
   userPreference: FormGroup;
-  selectStateOption: any = [];
-  selectLanguageOption: any = [];
-  selectState:any;
-  selectLanguage:any;
-  showSelectImageModal;
-  showUploadUserModal;
+  // selectStateOption: any = [];
+  // selectLanguageOption: any = [];
+  // selectState:any;
+  // selectLanguage:any;
+  // showSelectImageModal;
+  // showUploadUserModal;
   constructor() { }
 
   ngOnInit() {
-    this.selectStateOption = [
-      {
-        name: 'Karnataka',
-        value: '0'
-      },
-      {
-        name: 'Maharashtra',
-        value: '1'
-       },
-       {
-        name: 'Tamil Nadu',
-        value: '2'
-      },
-      {
-        name: 'Andhra Pradesh',
-        value: '3'
-       },
-    ];
-    this.selectLanguageOption = [
-      {
-        name: 'All',
-        value: '0'
-      },
-      {
-        name: 'hindi',
-        value: '1'
-       },
-       {
-        name: 'English',
-        value: '2'
-      }
-    ];
+    // this.selectStateOption = [
+    //   {
+    //     name: 'Karnataka',
+    //     value: '0'
+    //   },
+    //   {
+    //     name: 'Maharashtra',
+    //     value: '1'
+    //    },
+    //    {
+    //     name: 'Tamil Nadu',
+    //     value: '2'
+    //   },
+    //   {
+    //     name: 'Andhra Pradesh',
+    //     value: '3'
+    //    },
+    // ];
+    // this.selectLanguageOption = [
+    //   {
+    //     name: 'All',
+    //     value: '0'
+    //   },
+    //   {
+    //     name: 'hindi',
+    //     value: '1'
+    //    },
+    //    {
+    //     name: 'English',
+    //     value: '2'
+    //   }
+    // ];
   }
 
   ngOnDestroy() {
