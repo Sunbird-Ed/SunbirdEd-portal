74523b3329d399dc53b12500704bf4e855d8a07f
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-10-13 16:32:17 +0530

    Issue SH-1323 Course with audience type as Teacher

--- src/app/client/src/app/modules/public/module/course/components/public-course/public-course.component.ts
+++ src/app/client/src/app/modules/public/module/course/components/public-course/public-course.component.ts
@@ -251,15 +251,15 @@ export class PublicCourseComponent implements OnInit, OnDestroy, AfterViewInit {
       return value.length;
     });
     filters = _.omit(filters, ['utm_source']);
-    if (localStorage.getItem('userType')) {
-      const userType = localStorage.getItem('userType');
-      const userTypeMapping = this.configService.appConfig.userTypeMapping;
-      _.map(userTypeMapping, (value, key) => {
-        if (userType === key) {
-          filters['audience'] = value;
-        }
-      });
-    }
+    // if (localStorage.getItem('userType')) {
+    //   const userType = localStorage.getItem('userType');
+    //   const userTypeMapping = this.configService.appConfig.userTypeMapping;
+    //   _.map(userTypeMapping, (value, key) => {
+    //     if (userType === key) {
+    //       filters['audience'] = value;
+    //     }
+    //   });
+    // }
     // filters.board = _.get(this.queryParams, 'board') || this.dataDrivenFilters.board;
     const option = {
       source: 'web',
