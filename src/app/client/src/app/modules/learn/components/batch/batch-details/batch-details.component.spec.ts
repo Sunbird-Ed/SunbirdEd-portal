c7f4851805a5482e2eb1d19f7c771be49eb80fcb
Rajeev Sathish <rajeev.sathish@tarento.com>
2020-10-27 13:36:00 +0530

    Issue #SB-20748 fixing a issue

--- src/app/client/src/app/modules/learn/components/batch/batch-details/batch-details.component.spec.ts
+++ src/app/client/src/app/modules/learn/components/batch/batch-details/batch-details.component.spec.ts
@@ -343,7 +343,7 @@ describe('BatchDetailsComponent', () => {
       enrollmentEndDate: null,
       enrollmentType: 'open',
       name: 'SHS cert course 1 - 0825',
-      startDate: '2020-10-25',
+      startDate: '2031-10-25',
       status: 1
     };
     const message = (resourceServiceMockData.messages.emsg.m009).replace('{startDate}', batch.startDate);
