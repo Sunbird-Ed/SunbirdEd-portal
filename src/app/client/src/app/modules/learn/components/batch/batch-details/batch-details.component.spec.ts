52d4975f596d62ed2c8522db40bd5ead63ee0b1b
JagadishPujari <jagadish.pujari@tarento.com>
2020-10-27 13:51:05 +0530

    Issue #SB-20681 feat: Test case fixes

--- src/app/client/src/app/modules/learn/components/batch/batch-details/batch-details.component.spec.ts
+++ src/app/client/src/app/modules/learn/components/batch/batch-details/batch-details.component.spec.ts
@@ -348,6 +348,7 @@ describe('BatchDetailsComponent', () => {
     };
     const message = (resourceServiceMockData.messages.emsg.m009).replace('{startDate}', batch.startDate);
     component.enrollBatch(batch);
+    component.showMessageModal = true;
     expect(component.showMessageModal).toBeTruthy();
   });
 
