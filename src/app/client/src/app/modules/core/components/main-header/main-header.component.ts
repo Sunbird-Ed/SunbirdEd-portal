11ae1eedad88f3b47904e8de25a7d5fac6af80be
vinod <vinodkumar@zool.in>
2020-11-13 17:50:20 +0530

    Issue #SB-21462 App notifications UI

--- src/app/client/src/app/modules/core/components/main-header/main-header.component.ts
+++ src/app/client/src/app/modules/core/components/main-header/main-header.component.ts
@@ -167,6 +167,50 @@ export class MainHeaderComponent implements OnInit, OnDestroy {
         title: '2 Some title which is lengthy so it can overflow 2',
         deepLink: 'http://staging.ntp.net.in/'
       }
+    },
+    {
+      id: 'id_00001',
+      isRead: false,
+      displayTime: 789789,
+      actionData: {
+        appIcon: 'assets/common-consumption/images/book.png',
+        title: '1. Some title which is lengthy so that we can cehck overflow',
+        richText: '1. Sample description about the notification paragraph',
+        deepLink: 'http://staging.ntp.net.in/'
+      }
+    },
+    {
+      id: 'id_00002',
+      isRead: true,
+      displayTime: 789789,
+      actionData: {
+        appIcon: 'assets/common-consumption/images/book.png',
+        title: '2. Some title which is lengthy so that we can cehck overflow',
+        richText: '2. Sample description about the notification paragraph',
+        deepLink: 'http://staging.ntp.net.in/'
+      }
+    },
+    {
+      id: 'id_00003',
+      isRead: true,
+      displayTime: 789789,
+      actionData: {
+        appIcon: '',
+        title: '3. Some title which is lengthy so that we can cehck overflow',
+        richText: '',
+        deepLink: 'http://staging.ntp.net.in/'
+      }
+    },
+    {
+      id: 'id_00004',
+      isRead: false,
+      displayTime: 789789,
+      actionData: {
+        appIcon: '',
+        title: '4. Some title which is lengthy so that we can cehck overflow',
+        richText: '4. Sample description about the notification paragraph',
+        deepLink: 'http://staging.ntp.net.in/'
+      }
     }
   ];
 
