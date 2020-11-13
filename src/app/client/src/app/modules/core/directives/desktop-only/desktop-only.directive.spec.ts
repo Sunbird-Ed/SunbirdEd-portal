402a521194b4bd326c6e9b62113f308d41dcae0d
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-11-13 17:13:07 +0530

    Issue #SB-21192 fix: Fixed test cases

--- src/app/client/src/app/modules/core/directives/desktop-only/desktop-only.directive.spec.ts
+++ src/app/client/src/app/modules/core/directives/desktop-only/desktop-only.directive.spec.ts
@@ -1,7 +1,7 @@
 import { DesktopOnlyDirective } from './desktop-only.directive';
 import { TemplateRef, ViewContainerRef } from '@angular/core';
 
-fdescribe('DesktopOnlyDirective', () => {
+describe('DesktopOnlyDirective', () => {
   it('should create an instance', () => {
     const directive = new DesktopOnlyDirective(TemplateRef as any, ViewContainerRef as any);
     expect(directive).toBeTruthy();
