91a28bb2e301f6eaf4f776a0756aec7f87db69ce
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-11-18 11:26:14 +0530

    Issue #SB-21192 feat: Added test cases

--- src/app/client/src/app/modules/public/module/offline/services/connection-service/connection.service.spec.ts
+++ src/app/client/src/app/modules/public/module/offline/services/connection-service/connection.service.spec.ts
@@ -1,14 +1,20 @@
- import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
- import { ConnectionService } from './connection.service';
- import {of as observableOf } from 'rxjs';
+import { ConnectionService } from './connection.service';
+import { of as observableOf } from 'rxjs';
+import { ToasterService, ResourceService } from '@sunbird/shared';
 
- describe('ConnectionService', () => {
+describe('ConnectionService', () => {
+  const resourceMockData = {
+    messages: {
+      fmsg: { m0097: 'Something went wrong' }
+    }
+  };
   beforeEach(() => TestBed.configureTestingModule({
-
+    providers: [ToasterService, { provide: ResourceService, useValue: resourceMockData }]
   }));
 
-   it('to make the connection status true', () => {
+  it('to make the connection status true', () => {
     const service: ConnectionService = TestBed.get(ConnectionService);
     expect(service).toBeTruthy();
     const mockConnectionStatus = true;
