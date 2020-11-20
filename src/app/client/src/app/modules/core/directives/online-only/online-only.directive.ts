6725d77a4cbf9406f6d52800391f4ec5423dcae3
Vivek kasture <vivek_kasture@techjoomla.com>
2020-11-20 13:18:21 +0530

    Issue #SB-21192 feat: Added online-only and desktop-only directives (#5462)
    * Issue #SB-21192 feat: Added online-only and desktop-only directives
    
    * Issue #SB-21192 fix: Fixed test cases
    
    * Issue #SB-21192 fix: Fixed environment
    
    * Issue #SB-21192 feat: Added test cases
    
    * Issue #SB-21192 feat: Updated test cases
    
    * Issue #SB-21192 fix: Fixed test cases
    
    * Issue #SB-21192 fix: Test cases fixed
    
    * Issue #SB-21192 fix: Fixed test cases
    
    * Issue #SB-21192 fix: Updated test cases
    
    * Issue #SB-21192 fix: Updated test cases
    
    * Issue #SB-21192 feat: Resolved review comments
    
    * Issue #SB-21192 feat: Added angular.json

--- src/app/client/src/app/modules/core/directives/online-only/online-only.directive.ts
+++ src/app/client/src/app/modules/core/directives/online-only/online-only.directive.ts
@@ -1,44 +1,61 @@
-import { Directive,  ElementRef, HostListener, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { ConnectionService, ResourceService, ToasterService } from '@sunbird/shared';
+import * as _ from 'lodash-es';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { ConnectionService } from '@sunbird/shared';
 
 @Directive({
   selector: '[appOnlineOnly]'
 })
 export class OnlineOnlyDirective implements OnInit, OnDestroy {
+  @Input() showWarningMessage = false;
+  @Input() appOnlineOnly = true;
 
   public unsubscribe$ = new Subject<void>();
   private isConnected = true;
-  constructor(private el: ElementRef, private connectionService: ConnectionService, public renderer: Renderer2) {
-    console.log('construct called');
-  }
+  constructor(
+    private el: ElementRef,
+    private connectionService: ConnectionService,
+    public renderer: Renderer2,
+    private toastService: ToasterService,
+    private resourceService: ResourceService
+  ) { }
 
   @HostListener('click', ['$event']) onClick($event) {
-    if (!this.isConnected) {
+    if (!this.isConnected && this.appOnlineOnly) {
       $event.preventDefault();
       $event.stopPropagation();
-      this.showAlertMessage();
+      if (this.showWarningMessage) {
+        this.showAlertMessage();
+      }
     }
   }
 
   ngOnInit() {
     this.connectionService.monitor()
-    .pipe(takeUntil(this.unsubscribe$)).subscribe(isConnected => {
-      this.isConnected = isConnected;
-      if (!isConnected) {
-        this.disableElement();
-      }
-    });
+      .pipe(takeUntil(this.unsubscribe$)).subscribe(isConnected => {
+        this.isConnected = isConnected;
+        if (!isConnected && this.appOnlineOnly) {
+          this.disableElement();
+        } else {
+          this.enableElement();
+        }
+      });
   }
 
+  private enableElement() {
+    this.renderer.removeAttribute(this.el.nativeElement, 'disabled');
+    this.renderer.removeClass(this.el.nativeElement, 'sb-btn-disabled');
+    this.renderer.removeClass(this.el.nativeElement, 'disabled');
+  }
   private disableElement() {
-    this.renderer.setAttribute(this.el.nativeElement, 'data-name', 'testname');
+    this.renderer.setAttribute(this.el.nativeElement, 'disabled', 'true');
+    this.renderer.addClass(this.el.nativeElement, 'sb-btn-disabled');
     this.renderer.addClass(this.el.nativeElement, 'disabled');
   }
 
   private showAlertMessage() {
-    alert('You are offline');
+    this.toastService.error(_.get(this.resourceService, 'frmelmnts.lbl.offline'));
     return false;
   }
 
@@ -46,5 +63,4 @@ export class OnlineOnlyDirective implements OnInit, OnDestroy {
     this.unsubscribe$.next();
     this.unsubscribe$.complete();
   }
-
 }
