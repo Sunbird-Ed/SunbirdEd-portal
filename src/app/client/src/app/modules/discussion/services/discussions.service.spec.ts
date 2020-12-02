22f781bd11578d3ed815089f6d146bcc24b7c451
nani3592 <venkatkadiveti248@gmail.com>
2020-12-02 19:38:42 +0530

    Issue #SH-21326 fix: adding discussion forum apis

--- src/app/client/src/app/modules/discussion/services/discussions.service.spec.ts
+++ src/app/client/src/app/modules/discussion/services/discussions.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ConfigService } from '@sunbird/shared';
 import { DiscussionsService } from './discussions.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('DiscussionsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [ HttpClientModule],
+    providers: [ConfigService]
+  }));
 
   it('should be created', () => {
     const service: DiscussionsService = TestBed.get(DiscussionsService);
