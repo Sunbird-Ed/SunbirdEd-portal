c3c8c9668b3c277050462027381780663a96dd98
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-12-09 22:08:53 +0530

    Issue #SB-21192 feat: Updated test cases

--- src/app/client/src/app/modules/core/components/main-footer/main-footer.component.spec.ts
+++ src/app/client/src/app/modules/core/components/main-footer/main-footer.component.spec.ts
@@ -157,4 +157,17 @@ describe('MainFooterComponent', () => {
     expect(component.unsubscribe$.next).toHaveBeenCalled();
   });
 
+    it('should open system browser for desktop app', () => {
+        component.isDesktopApp = true;
+        spyOn(window, 'open');
+        component.redirect('http://google.com');
+        expect(window.open).toHaveBeenCalled();
+    });
+
+    it('should open link', () => {
+        component.isDesktopApp = false;
+        component.redirect('http://google.com');
+        expect(window.location.href).toBeDefined();
+    });
+
 });
