a331a445c48ee5c878476f446b457dcf7d0bb34a
harishkumar gangula <harish@sanketika.in>
2020-12-09 11:39:40 +0530

    Issue #SB-21202 fix: updated the metadata script

--- src/desktop/scripts/metadata.js
+++ src/desktop/scripts/metadata.js
@@ -59,9 +59,9 @@ const getChannels = async () => {
 const getOrgs = async () => {
     const envs = await fse.readJSON(path.join(__dirname, '..', 'env.json'))
     const instance = await getInstance()
-    let { data } = await instance.post("/api/org/v1/search", {"request":{"filters":{"isRootOrg":true,"slug": envs.CHANNEL}}})
+    let { data } = await instance.post("/api/org/v1/search", { "request": { "filters": { "isRootOrg": true, "slug": envs.CHANNEL } } })
     // inject the Channel to env.json
-     
+
     envs.CHANNEL = data.result.response.content[0].slug;
     console.log(`Root org is ${envs.CHANNEL}`)
     rootOrgHashTagId = data.result.response.content[0].hashTagId;
@@ -161,8 +161,10 @@ const getForms = async () => {
     for (const { type, subtype, action } of forms) {
         let response = await instance.post(`/api/data/v1/form/read`, { "request": { "type": type, "action": action, "subtype": subtype } })
             .catch(err => console.log(`error while getting form ${type} ${subtype} ${action}`, err.response.status, err.response.data))
-        response = await instance.post(`/api/data/v1/form/read`, { "request": { "type": type, "action": action, "subType": subtype } })
-            .catch(err => console.log(`error while getting form ${type} ${subtype} ${action}`, err.response.status, err.response.data))    
+        if (!response) {
+            response = await instance.post(`/api/data/v1/form/read`, { "request": { "type": type, "action": action, "subType": subtype } })
+                .catch(err => console.log(`error while getting form ${type} ${subtype} ${action}`, err.response.status, err.response.data))
+        }
         if (response && response.data) {
             const formFile = path.join(baseDirPath, 'forms', `${type}_${subtype}_${action}.json`)
             await fse.createFile(formFile)
