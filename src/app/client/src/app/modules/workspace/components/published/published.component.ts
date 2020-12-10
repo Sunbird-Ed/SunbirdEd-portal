09a8feeca331300f466118738d2a20ea0a884355
“Prashant <prashant_s@techjoomla.com>
2020-12-10 12:33:05 +0530

    Issue #DP-1414 Fix:Adopted textbooks should not be editable

--- src/app/client/src/app/modules/workspace/components/published/published.component.ts
+++ src/app/client/src/app/modules/workspace/components/published/published.component.ts
@@ -310,7 +310,7 @@ export class PublishedComponent extends WorkSpace implements OnInit, AfterViewIn
   */
   contentClick(param, content) {
     this.contentMimeType = content.metaData.mimeType;
-    if (typeof param.data.originData !== "undefined") {
+     if(param.data && param.data.originData) {
       const originData = JSON.parse(param.data.originData)
       if (originData.copyType === 'shallow') {
         const errMsg =  (this.resourceService.messages.emsg.m1414).replace('{instance}', originData.organisation[0]);
