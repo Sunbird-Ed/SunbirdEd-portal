1d7a2749710db63496db99884103b9c85a1ed52e
nani3592 <venkatkadiveti248@gmail.com>
2020-12-16 16:32:31 +0530

    Issue sh-21326 fix: discussion forum service

--- src/app/helpers/environmentVariablesHelper.js
+++ src/app/helpers/environmentVariablesHelper.js
@@ -11,7 +11,7 @@ let envVariables = {
   DEFAULT_CHANNEL: env.sunbird_default_channel,
 
   // discussion forum 
-  discussions_middleware: env.discussions_middleware,
+  discussions_middleware: env.discussions_middleware || 'http://discussionsmw-service:3002',
 
   // Application Start-up - Hosts and PORT Configuration
   PORTAL_PORT: env.sunbird_port || 3000,
