43527dc5d3bdfa84e208444c99a0b0fe40d9f28b
Sachin <sachin.kumar@goodworklabs.com>
2020-12-21 16:16:22 +0530

    accessible theme check condition removed

--- src/app/client/src/app/app.component.ts
+++ src/app/client/src/app/app.component.ts
@@ -120,15 +120,9 @@ export class AppComponent implements OnInit, OnDestroy {
     this.instance = (<HTMLInputElement>document.getElementById('instance'))
       ? (<HTMLInputElement>document.getElementById('instance')).value : 'sunbird';
     const layoutType = localStorage.getItem('layoutType') || '';
-    const layoutTypeAccessablethm = localStorage.getItem('accessable-theme') || '';
     if (layoutType === '' || layoutType === 'joy') {
       this.layoutConfiguration = this.configService.appConfig.layoutConfiguration;
       document.documentElement.setAttribute('layout', 'joy');
-      if (layoutTypeAccessablethm === 'accessible') {
-        document.documentElement.setAttribute('accessable-theme', 'accessible');
-      } else {
-        document.documentElement.setAttribute('accessable-theme', '');
-      }
     } else {
       document.documentElement.setAttribute('layout', 'old');
     }
