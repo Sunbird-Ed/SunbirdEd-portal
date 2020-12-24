aa737e3c29621766d8fb576e7b4bab48efcdf72c
Vivek kasture <vivek_kasture@techjoomla.com>
2020-12-24 17:04:43 +0530

    Issue #SB-22192 fix: Translated media type label (#5705)

--- src/app/client/src/app/modules/core/services/search/search.service.ts
+++ src/app/client/src/app/modules/core/services/search/search.service.ts
@@ -496,6 +496,11 @@ export class SearchService {
           facet['label'] = this.resourceService.frmelmnts.lbl.mediaType;
           facet['mimeTypeList'] = this.mimeTypeList;
           break;
+        case 'mediaType':
+            facet['index'] = '8';
+            facet['label'] = this.resourceService.frmelmnts.lbl.mediaType;
+            facet['mimeTypeList'] = this.mimeTypeList;
+            break;
         case 'audience':
             facet['index'] = '9';
             facet['label'] =  this.resourceService.frmelmnts.lbl.userType;
