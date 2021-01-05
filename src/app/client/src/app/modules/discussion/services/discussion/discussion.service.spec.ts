4f19874ee34522a1f70912cef150809c15a2e66d
sudip mukherjee <sudip.mukherjee@tarento.com>
2021-01-05 11:38:14 +0530

    Issue #SB-21948 fix: Fixed TC failure issue for DiscussionService

--- src/app/client/src/app/modules/discussion/services/discussion/discussion.service.spec.ts
+++ src/app/client/src/app/modules/discussion/services/discussion/discussion.service.spec.ts
@@ -4,20 +4,54 @@ import { configureTestSuite } from '@sunbird/test-util';
 import { ConfigService } from '@sunbird/shared';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CacheService } from 'ng2-cache-service';
-
-
-
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CsModule } from '@project-sunbird/client-services';
 
 
 describe('DiscussionService', () => {
   configureTestSuite();
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [ HttpClientTestingModule ],
-    providers: [ ConfigService, CacheService],
+    imports: [HttpClientTestingModule, RouterTestingModule],
+    providers: [
+      ConfigService,
+      CacheService,
+      DiscussionService,
+      { provide: APP_BASE_HREF, useValue: '/' }
+    ],
   }));
 
-  it('>>>', () => {
-    const service = TestBed.get(DiscussionService);
+  it('should be created', inject([DiscussionService], (service: DiscussionService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('It should register user to nodebb', () => {
+    const discussionService = TestBed.get(DiscussionService);
+    const data = {
+      username: 'SOME_USER_NAME',
+      identifier: 'SOME_USER_IDENTIFIER',
+    };
+    spyOn(discussionService.discussionCsService, 'createUser').and.callThrough();
+    discussionService.registerUser(data);
+    expect(discussionService['discussionCsService'].createUser).toHaveBeenCalledWith(data);
+  });
+
+  it('It should fetch userDetails fom nodebb', () => {
+    const discussionService = TestBed.get(DiscussionService);
+    const uid = 16;
+    spyOn(discussionService.discussionCsService, 'getUserDetails').and.callThrough();
+    discussionService.getUserDetails(uid);
+    expect(discussionService['discussionCsService'].getUserDetails).toHaveBeenCalledWith(uid);
+  });
+
+  it('It should fetch the forum ids attached to a course', () => {
+    const discussionService = TestBed.get(DiscussionService);
+    const request = {
+      identifier: ['SOME_IDENTIFIER'],
+      type: 'Course'
+    };
+    spyOn(discussionService.discussionCsService, 'getForumIds').and.callThrough();
+    discussionService.getForumIds(request);
+    expect(discussionService['discussionCsService'].getForumIds).toHaveBeenCalledWith(request);
   });
 });
