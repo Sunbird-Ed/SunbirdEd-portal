b8931dc14fa539877ae852611a966e27196ad3f8
Vivek Kasture <vivek_kasture@techjoomla.com>
2021-01-07 15:07:49 +0530

    Issue #SB-21199 fix: Fixed test cases

--- src/app/client/src/app/modules/core/services/app-update/app-update.service.spec.ts
+++ src/app/client/src/app/modules/core/services/app-update/app-update.service.spec.ts
@@ -1,16 +1,15 @@
-import { of as observableOf, throwError } from 'rxjs';
-import { serverRes } from './app-update.service.spec.data';
-import { TestBed } from '@angular/core/testing';
-import { AppUpdateService } from './app-update.service';
+import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 import { ConfigService } from '@sunbird/shared';
-import { PublicDataService } from '@sunbird/core';
-
+import { of as observableOf, throwError } from 'rxjs';
+import { AppUpdateService } from './app-update.service';
+import { serverRes } from './app-update.service.spec.data';
 
 describe('AppUpdateService', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [HttpClientTestingModule],
-    providers: [ConfigService, PublicDataService]
+    providers: [ConfigService, HttpClient]
   }));
 
   it('should be created', () => {
@@ -20,42 +19,38 @@ describe('AppUpdateService', () => {
 
   it('app-update should send the response', () => {
     const service: AppUpdateService = TestBed.get(AppUpdateService);
-    const publicDataService = TestBed.get(PublicDataService);
-    spyOn(publicDataService, 'get').and.returnValue(observableOf(serverRes.app_update));
+    spyOn(service, 'get').and.returnValue(observableOf(serverRes.app_update));
     service.checkForAppUpdate().subscribe(response => {
       expect(response).toBe(serverRes.app_update);
     });
-    expect(publicDataService.get).toHaveBeenCalled();
+    expect(service.get).toHaveBeenCalled();
   });
 
   it('app-update should throw error', () => {
     const service: AppUpdateService = TestBed.get(AppUpdateService);
-    const publicDataService = TestBed.get(PublicDataService);
-    spyOn(publicDataService, 'get').and.returnValue(throwError(serverRes.error));
+    spyOn(service, 'get').and.returnValue(throwError(serverRes.error));
     service.checkForAppUpdate().subscribe(data => {}, err => {
       expect(err).toBe(serverRes.error);
     });
-    expect(publicDataService.get).toHaveBeenCalled();
+    expect(service.get).toHaveBeenCalled();
   });
 
   it('should get app info data', () => {
     const service: AppUpdateService = TestBed.get(AppUpdateService);
-    const publicDataService = TestBed.get(PublicDataService);
-    spyOn(publicDataService, 'get').and.returnValue(observableOf(serverRes.appInfoSuccess));
+    spyOn(service, 'get').and.returnValue(observableOf(serverRes.appInfoSuccess));
     service.getAppInfo().subscribe(data => {
       expect(data).toEqual(serverRes.appInfoSuccess);
     });
-    expect(publicDataService.get).toHaveBeenCalledWith({url: 'app/v1/info'});
+    expect(service.get).toHaveBeenCalledWith({url: 'app/v1/info'});
   });
 
   it('should get app info failure case', () => {
     const service: AppUpdateService = TestBed.get(AppUpdateService);
-    const publicDataService = TestBed.get(PublicDataService);
-    spyOn(publicDataService, 'get').and.returnValue(throwError(serverRes.appInfoFailureCase));
+    spyOn(service, 'get').and.returnValue(throwError(serverRes.appInfoFailureCase));
     service.getAppInfo().subscribe(data => {}, err => {
       expect(err).toEqual(serverRes.appInfoFailureCase);
     });
-    expect(publicDataService.get).toHaveBeenCalledWith({url: 'app/v1/info'});
+    expect(service.get).toHaveBeenCalledWith({url: 'app/v1/info'});
   });
 
 
