59de86fa0f3332312dcb0e4f9ecfaae61a5126c8
Vivek Kasture <vivek_kasture@techjoomla.com>
2021-01-08 19:52:26 +0530

    Issue #SB-21199 fix: Fixed test cases

--- src/app/client/src/app/modules/core/interceptor/session-expiry.interceptor.spec.ts
+++ src/app/client/src/app/modules/core/interceptor/session-expiry.interceptor.spec.ts
@@ -6,6 +6,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { TestBed } from '@angular/core/testing';
 import { CoreModule } from '@sunbird/core';
 import { configureTestSuite } from '@sunbird/test-util';
+import { UtilService } from '../../shared/services/util/util.service';
 
 describe('Session Expiry Interceptor', () => {
 
@@ -17,6 +18,9 @@ describe('Session Expiry Interceptor', () => {
         const http = TestBed.get(HttpClient);
         return http.get(mockUrl);
     };
+    const mockUtilService = {
+        isDesktopApp: false
+    };
     configureTestSuite();
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -25,7 +29,7 @@ describe('Session Expiry Interceptor', () => {
                 provide: HTTP_INTERCEPTORS,
                 useClass: SessionExpiryInterceptor,
                 multi: true,
-            }]
+            }, { provide: UtilService, useValue: mockUtilService }]
         });
         sessionExpiryInterceptor = TestBed.get(SessionExpiryInterceptor);
         httpMock = TestBed.get(HttpTestingController);
