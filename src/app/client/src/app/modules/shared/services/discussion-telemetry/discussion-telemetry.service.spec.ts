8e92a048cabfa9f6e79c870cefea56c8ae438efe
sudip mukherjee <sudip.mukherjee@tarento.com>
2021-01-19 21:57:06 +0530

    Issue #SB-22566 fix: TC failure

--- src/app/client/src/app/modules/shared/services/discussion-telemetry/discussion-telemetry.service.spec.ts
+++ src/app/client/src/app/modules/shared/services/discussion-telemetry/discussion-telemetry.service.spec.ts
@@ -1,11 +1,9 @@
-import { TelemetryModule, TelemetryService } from '@sunbird/telemetry';
-
+import { TelemetryService } from '@sunbird/telemetry';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SharedModule } from './../../shared.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NavigationHelperService } from './../navigation-helper/navigation-helper.service';
-import { TestBed, inject } from '@angular/core/testing';
-
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
 import { DiscussionTelemetryService } from './discussion-telemetry.service';
 
 const event = {
@@ -38,17 +36,19 @@ describe('DiscussionTelemetryService', () => {
   });
 
   it ('should call impression()',  inject([DiscussionTelemetryService, TelemetryService, NavigationHelperService ],
-    (service: DiscussionTelemetryService, telemetryService: TelemetryService, navigationHelperService: NavigationHelperService) => {
+    fakeAsync((service: DiscussionTelemetryService,
+      telemetryService: TelemetryService, navigationHelperService: NavigationHelperService) => {
     spyOn(telemetryService, 'impression');
     spyOn(navigationHelperService, 'getPageLoadTime').and.returnValue(2.0);
     service.logTelemetryEvent({eid: 'IMPRESSION', context: {
       cdata: [{id: '1', type: 'Category'}], object: {id: '1', type: 'Category', ver: '1', rollup: {}}},
-      edata: {id: 'category-card', type: 'view', pageid: 'discussion'}})
+      edata: {id: 'category-card', type: 'view', pageid: 'discussion'}});
     event.edata['duration'] = 2.0;
-    event.edata.type = 'view'
+    event.edata.type = 'view';
+    tick(100);
     expect(telemetryService.impression).toHaveBeenCalledWith(event);
     expect(navigationHelperService.getPageLoadTime).toHaveBeenCalled();
-  }));
+  })));
 
   it ('should call interact()', inject([DiscussionTelemetryService, TelemetryService ],
     (service: DiscussionTelemetryService, telemetryService: TelemetryService) => {
@@ -57,7 +57,7 @@ describe('DiscussionTelemetryService', () => {
       delete event.edata['duration'];
       service.logTelemetryEvent({eid: 'INTERACT', context: {
         cdata: [{id: '1', type: 'Category'}], object: {id: '1', type: 'Category', ver: '1', rollup: {}}},
-        edata: {id: 'category-card', type: 'CLICK', pageid: 'discussion'}})
+        edata: {id: 'category-card', type: 'CLICK', pageid: 'discussion'}});
       expect(telemetryService.interact).toHaveBeenCalledWith(event);
   }));
 
