56d02fa507d052dd74eb0b6556a2fd7e3b9d6025
Vivek kasture <vivek_kasture@techjoomla.com>
2021-01-28 13:58:37 +0530

    Issue #SB-0000 merge: Merged release-3.5.0 to release-3.6.0 (#5918)
    * Issue #SB-21192 feat: Added online-only and desktop-only directives
    
    * Issue #SB-21192 fix: Fixed test cases
    
    * Issue #SB-21192 fix: Fixed environment
    
    * Issue #SB-21192 feat: Added test cases
    
    * Issue #SB-21192 feat: Updated test cases
    
    * Issue #SB-21192 fix: Fixed test cases
    
    * Issue #SB-21192 fix: Test cases fixed
    
    * Issue #SB-21192 fix: Fixed test cases
    
    * Issue #SB-21192 fix: Updated test cases
    
    * Issue #SB-21192 fix: Updated test cases
    
    * Issue #SB-21192 feat: Resolved review comments
    
    * Issue #SB-21192 feat: Added angular.json
    
    * Issue #SB-21192 fix: Content player fix
    
    * Issue #SB-21192 fix: Added disabled class
    
    * Issue #SB-21192 fix: Added loadcontent popup
    
    * Issue #SB-21192 fix: Device register API added
    
    * Issue #SB-21192 fix: Load FAQ on language change
    
    * Issue #SB-21192 feat: Updated onlineOnly directive
    
    * Issue #SB-21192 fix: Updated test cases
    
    * Issue #SB-21192 fix: Updated test cases
    
    * Issue #SB-21192 fix: Updated test cases
    
    * Issue #SB-21192 fix: Fixed test cases
    
    * Issue #SB-21192 fix: Fixed test cases
    
    * Issue #SB-21192 feat: Removed managed location as online only feature
    
    * Issue #SB-21199 fix: removed desktop only menu from form config
    
    * Issue #SB-21199 feat: open new login window and generating user session
    
    * Issue #SB-21199 fix: generaliseresourcebundles form loading issue fixed
    
    * Issue #SB-21199 fix: unit test case fixed
    
    * Issue #SB-21199 fix: shuffled test case
    
    * Issue #SB-0000 fix: Updated action routes
    
    * Issue #SB-0000 fix: Updated action routes
    
    * Issue #SB-0000 fix: Test case circle ci updated.
    
    * Issue #SB-0000 fix: Reverted circle ci config.
    
    * Issue #SB-0000 fix: Updated circle ci command
    
    * Update config.yml
    
    * Issue #SB-0000 fix: Updated roles for action routes.
    
    * Issue #SB-0000 fix: Updated roles for action routes.
    
    * Update config.yml
    
    * Update config.yml
    
    * Update reviewsubmissions-contentplayer.component.spec.ts
    
    * Issue #SB-22354 fix: Updated v3 read and unlisted API roles
    
    * Issue #SB-22354 fix: Updated framewrok and comment read API
    
    * Issue #SB-22354 fix: Update action content v3 read
    
    * Issue #SB-0000 fix: Updated release-3.5.0
    
    * Issue #SB-0000 fix: Updated release-3.5.0
    
    * Issue #SB-0000 Fix: Pulled release-3.5.0
    
    * Issue #SB-0000 fix: Updated device registry Implementation
    
    * Issue #SB-0000 fix: Register from the device sdk
    
    * Issue #SB-0000 fix: Updated deviceSDK
    
    * Issue #SB-0000 fix: Resolved conflicts
    
    * Issue #SB-0000 merge: Merged release-3.5.0 to release-3.6.0
    
    * Issue #SB-0000 merge: Merged release-3.5.0 to release-3.6.0
    
    Co-authored-by: Nikunj Beladiya <nikunj.beladiya@tarento.com>
    Co-authored-by: Rajesh <rajesh.kumaravel@tarento.com>
    Co-authored-by: Rajeev Sathish <rajeev.sathish@tarento.com>

--- src/desktop/modules/routes.ts
+++ src/desktop/modules/routes.ts
@@ -121,7 +121,8 @@ export class Router {
       if (locationData && _.isObject(locationData.state) || !_.isObject(locationData.city)) {
         const deviceProfile = new Device(manifest);
         deviceProfile.updateDeviceProfile(req.body.request);
-        next();
+        containerAPI.getDeviceSdkInstance().register();
+        res.status(200).send(Response.success('analytics.device-register', { status: 'success' }, req));
       } else {
         logger.error(
           `ReqId = "${req.headers[
@@ -132,11 +133,7 @@ export class Router {
         res.status(status);
         return res.send(Response.error('analytics.device-register', status));
       }
-    },proxy(proxyUrl, {
-      proxyReqPathResolver(req) {
-        return `/device/register/:id`;
-      },
-    }));
+    });
       
     authRoutes(app, proxyUrl);
     contentRoutes(app, proxyUrl, this.contentDownloadManager)
