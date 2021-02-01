e97a6e83c5e583475cd306396e670d9c0438360a
Vivek Kasture <vivek_kasture@techjoomla.com>
2021-02-01 14:59:04 +0530

    Issue #SB-22793 feat: Updated message

--- src/desktop/main.ts
+++ src/desktop/main.ts
@@ -175,8 +175,8 @@ expressApp.use("/dialog/telemetry/export", async (req, res) => {
   if (destFolder && destFolder[0]) {
     fse.access(destFolder[0], fs.constants.F_OK | fs.constants.W_OK, (err) => {
       if (err) {
-        logger.error("Telemetry dest folder not selected", err);
-        res.status(500).send({ message: "Telemetry dest folder not selected", responseCode: "OPERATION_NOT_PERMITTED" });
+        logger.error("Folder does not have write permission", err);
+        res.status(500).send({ message: "Folder does not have write permission", responseCode: "OPERATION_NOT_PERMITTED" });
       } else {
         res.send({
           message: "SUCCESS",
