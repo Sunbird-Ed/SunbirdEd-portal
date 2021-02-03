b41cf9e2af6e8e892a265744b8052c320ec77f04
Vivek kasture <vivek_kasture@techjoomla.com>
2021-02-03 13:50:55 +0530

    Issue #SB-0000 feat: Implementation of Propagation of trace id in API calls (#5945)
    * Issue #SB-21632 feat: Updated Electron to latest version
    
    * Issue #SB-21632 fix: App crash fix
    
    * Issue #SB-0000 feat: Implementation of Propagation of trace id in API calls
    
    * Issue #SB-0000 feat: Added test cases
    
    * Issue #SB-0000 feat: Added test cases

--- src/app/client/src/app/modules/core/services/data/data.service.spec.ts
+++ src/app/client/src/app/modules/core/services/data/data.service.spec.ts
@@ -10,9 +10,50 @@ describe('DataService', () => {
       imports: [HttpClientModule],
       providers: [DataService, HttpClient]
     });
+
+    let store = {};
+    const mockLocalStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      removeItem: (key: string) => {
+        delete store[key];
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+    spyOn(localStorage, 'getItem').and.callFake(mockLocalStorage.getItem);
+    spyOn(localStorage, 'setItem').and.callFake(mockLocalStorage.setItem);
+    spyOn(localStorage, 'removeItem').and.callFake(mockLocalStorage.removeItem);
+    spyOn(localStorage, 'clear').and.callFake(mockLocalStorage.clear);
   });
 
   it('should be created', inject([DataService], (service: DataService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should return required headers', inject([DataService], (service: DataService) => {
+    spyOn(document, 'getElementById').and.callFake((id) => {
+      if (id === 'deviceId') {
+        return { value: 'fake-device-id' };
+      }
+      if (id === 'appId') {
+        return { value: 'fake-appId' };
+      }
+      return { value: 'mock Id' };
+    });
+    service.appVersion = 'fake-appversion';
+    service.rootOrgId = 'fake-rootOrgId';
+    service.channelId = 'fake-channelId';
+    service['userId'] = 'fake_userId';
+    service['sessionId'] = 'fake-sessionId';
+    localStorage.setItem('traceId', 'fake-traceId');
+    const request = service['getHeader']();
+    expect(request).toBeDefined();
+    expect(request['X-Request-ID']).toEqual('fake-traceId');
   }));
 });
