b624f50868d1677eaa30a8a85f4a8367a52cd02d
Rajeev Sathish <rajeev.sathish@tarento.com>
2021-03-05 01:14:34 +0530

    Issue #SB-0000 cleaning up unused code

--- src/app/client/src/app/modules/search/components/index.ts
+++ src/app/client/src/app/modules/search/components/index.ts
@@ -6,5 +6,3 @@ export * from './user-filter/user-filter.component';
 export * from './user-delete/user-delete.component';
 export * from './user-edit/user-edit.component';
 export * from './user-profile/user-profile.component';
-export * from './org-search/org-search.component';
-export * from './org-filter/org-filter.component';
