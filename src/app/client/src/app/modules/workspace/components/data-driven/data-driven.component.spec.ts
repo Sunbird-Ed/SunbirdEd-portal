f31f6d43708474844d4016d88214daeca86b5ba5
Kartheek Palla <palla.kartheekreddy@gmail.com>
2021-03-29 18:38:53 +0530

    Issue #SB-23174 fix: Tag objects with org and target frameworks

--- src/app/client/src/app/modules/workspace/components/data-driven/data-driven.component.spec.ts
+++ src/app/client/src/app/modules/workspace/components/data-driven/data-driven.component.spec.ts
@@ -406,6 +406,45 @@ describe('DataDrivenComponent', () => {
     spyOn(workSpaceService, 'getCategoryDefinition').and.returnValue(observableOf(mockFrameworkData.successCategory));
     spyOn(componentParent, 'getFrameworkDataByType').and.returnValue(observableOf(mockFrameworkData.frameworkDataByType));
     componentParent.selectFramework();
+    componentParent.orgFWType = _.get(mockFrameworkData.successCategory, 'result.objectCategoryDefinition.objectMetadata.schema.properties.framework.default');
+    componentParent.targetFWType = _.get(mockFrameworkData.successCategory, 'result.objectCategoryDefinition.objectMetadata.schema.properties.targetFWIds.default');
+    expect(componentParent.createContent).toHaveBeenCalledWith(undefined);
+  });
+
+  it('#selectFramework() should fetch category and framwork data when API success', () => {
+    spyOn(componentParent, 'createContent').and.stub();
+    const workSpaceService = TestBed.get(WorkSpaceService);
+    const mockdata = {
+        'id': 'api.object.category.definition.read',
+        'ver': '3.0',
+        'ts': '2021-02-24T08:06:08ZZ',
+        'params': {
+          'resmsgid': '4e4a7b07-0e9d-4d33-8287-b6d38a3c2765',
+          'msgid': null,
+          'err': null,
+          'status': 'successful',
+          'errmsg': null
+        },
+        'responseCode': 'OK',
+        'result': {
+          'objectCategoryDefinition': {
+            'identifier': 'obj-cat:course_collection_all',
+            'objectMetadata': {
+              'config': {
+                'frameworkMetadata': {'orgFWType': 'K-12', 'targetFWType': 'K-12'},
+              },
+              'schema': {}
+            },
+            'languageCode': [],
+            'name': 'Course',
+          }
+        }
+    };
+    spyOn(workSpaceService, 'getCategoryDefinition').and.returnValue(observableOf(mockdata));
+    spyOn(componentParent, 'getFrameworkDataByType').and.returnValue(observableOf(mockFrameworkData.frameworkDataByType));
+    componentParent.selectFramework();
+    componentParent.orgFWType = _.get(mockdata, 'result.objectCategoryDefinition.objectMetadata.config.frameworkMetadata.orgFWType');
+    componentParent.targetFWType = _.get(mockdata, 'result.objectCategoryDefinition.objectMetadata.config.frameworkMetadata.targetFWType');
     expect(componentParent.createContent).toHaveBeenCalledWith(undefined);
   });
 
@@ -419,7 +458,7 @@ describe('DataDrivenComponent', () => {
     expect(toasterService.error).toHaveBeenCalledWith('Unknown framework category Course. Please check the configuration.');
   });
 
-  it('#selectFramework() should throw error if category defination API failed', () => {
+  xit('#selectFramework() should throw error if category defination API failed', () => {
     const resourceService = TestBed.get(ResourceService);
     const toasterService = TestBed.get(ToasterService);
     spyOn(toasterService, 'error').and.callThrough();
