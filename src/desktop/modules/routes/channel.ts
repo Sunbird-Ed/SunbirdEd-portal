d0f9653899eb809705d8c381fc7e2403bcc853c3
Vivek kasture <vivek_kasture@techjoomla.com>
2021-04-22 10:02:30 +0530

    Issue #SB-21974 feat: Added standardized logs (#6390)
    * Issue #SB-0000 fix: Enabled accessibility feature
    
    * Issue #SB-0000 fix: Reverted changes
    
    * Issue #SB-0000 fix: Reverted changes
    
    * Issue #SB-21974 feat: Implementation of standard log format
    
    * Issue #SB-21974 feat: Implemetation of standard logs
    
    * Issue #SB-21974 feat: Added standardized logs
    
    * Issue #SB-21974 feat: Updated common-consumption package version
    
    * Issue #SB-21974 feat: Added standardized logs
    
    * Issue #SB-21974 feat: Added standardized logs
    
    * Issue #SB-21974 feat: Implementation of standard log
    
    * Issue #SB-21974 feat: Moved standardLogger to OpenRAP

--- src/desktop/modules/routes/channel.ts
+++ src/desktop/modules/routes/channel.ts
@@ -1,10 +1,11 @@
 import { Channel } from "./../controllers/channel";
 import { manifest } from "./../manifest";
 const proxy = require('express-http-proxy');
-import { logger } from "@project-sunbird/logger";
+import { containerAPI } from '@project-sunbird/OpenRAP/api';
 
 export default (app, proxyURL) => {
     const channel = new Channel(manifest);
+    const standardLog = containerAPI.getStandardLoggerInstance();
     app.get(
         "/api/channel/v1/read/:id",
         proxy(proxyURL, {
@@ -12,7 +13,7 @@ export default (app, proxyURL) => {
                 return `/api/channel/v1/read/${req.params.id}`;
             },
             proxyErrorHandler: function (err, res, next) {
-                logger.warn(`While getting channel data from online`, err);
+                standardLog.warn({ id: 'CHANNEL_FETCH_FAILED', message: 'Error while getting channel data from online', error: err });
                 next();
             },
             userResDecorator: function (proxyRes, proxyResData) {
@@ -21,17 +22,14 @@ export default (app, proxyURL) => {
                         const channelResponse = JSON.parse(proxyResData.toString('utf8'));
                         channel.upsert(channelResponse);
                     } catch (error) {
-                        logger.error(`Unable to parse or do DB update of channel data after fetching from online`, error)
+                        standardLog.error({ id: 'CHANNEL_PARSE_FAILED', message: 'Unable to parse or do DB update of channel data after fetching from online', error });
                     }
                     resolve(proxyResData);
                 });
             }
         }),
         (req, res, next) => {
-            logger.debug(
-                `Received API call to get channel data for channel with Id: ${req.params.id}`,
-            );
-            logger.debug(`ReqId = "${req.headers["X-msgid"]}": Check proxy`);
+            standardLog.debug({ id: 'CHANNEL_API_REQUEST', message: `Received API call to get channel data for channel with Id: ${req.params.id}`, mid: req.headers["X-msgid"] });
                 return channel.get(req, res);
         },
 
