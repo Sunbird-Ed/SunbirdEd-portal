516841976e048e12411c7bff104d13badf7045a8
kiranTunerlabs <kiran@tunerlabs.com>
2021-05-17 09:51:33 +0530

    SB-24484: Feat:ML guard implementation

--- src/app/client/src/app/modules/observation/observation.module.ts
+++ src/app/client/src/app/modules/observation/observation.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ObservationRoutingModule } from './observation-routing.module';
 import { ObservationListingComponent } from './components/observation-listing/observation-listing.component';
 import { ObservationDetailsComponent } from './components/observation-details/observation-details.component';
+import { MlGuard } from './guards';
 
 @NgModule({
   declarations: [ObservationListingComponent, ObservationDetailsComponent],
   imports: [
     CommonModule,
     ObservationRoutingModule
-  ]
+  ],
+  providers:[MlGuard]
 })
 export class ObservationModule { }
