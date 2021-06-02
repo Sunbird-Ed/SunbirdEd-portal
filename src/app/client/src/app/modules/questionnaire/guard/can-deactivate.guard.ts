db1b0dbf669ceb05c63eac7df479bd938f9aabcc
pradeep kumar <pradeep.k@pacewisdom.com>
2021-06-02 12:41:35 +0530

    SB-24484 : fix : language translation added in guard

--- src/app/client/src/app/modules/questionnaire/guard/can-deactivate.guard.ts
+++ src/app/client/src/app/modules/questionnaire/guard/can-deactivate.guard.ts
@@ -1,17 +1,17 @@
 import { Injectable } from "@angular/core";
 import { CanDeactivate } from "@angular/router";
 import { ComponentCanDeactivate } from "./component-can-deactivate";
+import { ResourceService } from '@sunbird/shared';
 
 @Injectable()
 export class CanDeactivateGuard
   implements CanDeactivate<ComponentCanDeactivate>
 {
+  constructor(public resourceService: ResourceService){}
   canDeactivate(component: ComponentCanDeactivate): boolean {
     if (!component.canDeactivate()) {
       if (
-        confirm(
-          "You have unsaved changes! If you leave, your changes will be lost."
-        )
+        confirm(this.resourceService.frmelmnts.alert.confirmmsg)
       ) {
         return true;
       } else {
