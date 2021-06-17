b599e265bf4d0457bb8592702effbf8f4553fa04
Vivek Kasture <vivek_kasture@techjoomla.com>
2021-06-17 12:25:39 +0530

    Issue #SB-25204 fix: Moved notify networkchange method to app component

--- src/app/client/src/app/modules/shared/services/connection-service/connection.service.spec.ts
+++ src/app/client/src/app/modules/shared/services/connection-service/connection.service.spec.ts
@@ -1,8 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ConnectionService } from './connection.service';
 import { of as observableOf, of } from 'rxjs';
-import { ToasterService, ResourceService, UtilService } from '@sunbird/shared';
+import { UtilService } from '@sunbird/shared';
 import { Router } from '@angular/router';
 
 describe('ConnectionService', () => {
@@ -10,16 +9,8 @@ describe('ConnectionService', () => {
     navigate = jasmine.createSpy('navigate');
     url = 'explore-course/course/do_213129030425993216112'
   }
-  const resourceMockData = {
-    messages: {
-      fmsg: { m0097: 'Something went wrong' },
-      stmsg: { desktop: { onlineStatus: 'You are online' } },
-      emsg: { desktop: { onlineStatus: 'You are offline' } }
-    }
-  };
   beforeEach(() => TestBed.configureTestingModule({
-    providers: [ToasterService, { provide: ResourceService, useValue: resourceMockData },
-      { provide: Router, useClass: RouterStub }, UtilService]
+    providers: [{ provide: Router, useClass: RouterStub }, UtilService]
   }));
 
   it('to make the connection status true', () => {
@@ -50,26 +41,4 @@ describe('ConnectionService', () => {
       expect(data).toBe(true);
     });
   });
-
-  it('should call notifyNetworkChange', () => {
-    const service: ConnectionService = TestBed.get(ConnectionService);
-    const toasterService = TestBed.get(ToasterService);
-    const utilService = TestBed.get(UtilService);
-    utilService._isDesktopApp = true;
-    spyOn(toasterService, 'info');
-    service['connectionMonitor'] = of(true);
-    service.notifyNetworkChange();
-    expect(toasterService.info).toHaveBeenCalledWith('You are online');
-  });
-
-  it('should navigate to my download page if network is not available', () => {
-    const service: ConnectionService = TestBed.get(ConnectionService);
-    const router = TestBed.get(Router);
-    const toasterService = TestBed.get(ToasterService);
-    spyOn(toasterService, 'info');
-    service['connectionMonitor'] = of(false);
-    service.notifyNetworkChange();
-    expect(router.navigate).toHaveBeenCalledWith(['mydownloads'], {queryParams: { selectedTab: 'mydownloads' }});
-  });
-
 });
