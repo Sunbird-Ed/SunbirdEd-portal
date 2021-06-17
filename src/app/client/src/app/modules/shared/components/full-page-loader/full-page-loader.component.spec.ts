d971dfd679520d5c34b6eb8e8ee9a97b9688086a
kiranTunerlabs <kiran@tunerlabs.com>
2021-06-17 18:44:36 +0530

    SB-24052: fix: test master

--- src/app/client/src/app/modules/shared/components/full-page-loader/full-page-loader.component.spec.ts
+++ src/app/client/src/app/modules/shared/components/full-page-loader/full-page-loader.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FullPageLoaderComponent } from './full-page-loader.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 fdescribe('FullPageLoaderComponent', () => {
   let component: FullPageLoaderComponent;
@@ -8,7 +9,10 @@ fdescribe('FullPageLoaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FullPageLoaderComponent ]
+      declarations: [ FullPageLoaderComponent ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
     })
     .compileComponents();
   }));
