8874b2f6d3ce5e3941edb7d50f141b1321784c76
pradeep kumar <pradeep.k@pacewisdom.com>
2021-06-25 12:52:48 +0530

    SB-24852 : feat : portal report for observation

---
+++ src/app/client/src/app/modules/report/components/entity-list/entity-list.component.ts
@@ -0,0 +1,55 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { ResourceService } from "@sunbird/shared";
+
+@Component({
+  selector: "app-entity-list",
+  templateUrl: "./entity-list.component.html",
+  styleUrls: ["./entity-list.component.scss"],
+})
+export class EntityListComponent implements OnInit {
+  @ViewChild("modal") modal;
+  @Output() closeEvent = new EventEmitter<any>();
+  @Input() solution:any;
+  showLoaderBox: boolean = false;
+  public throttle = 50;
+  public scrollDistance = 2;
+  selectedListCount=0;
+  showSuccessModal;
+  selectedEntity:any;
+  constructor(public resourceService: ResourceService) {}
+
+  ngOnInit() {}
+
+  public closeModal() {
+    this.modal.approve();
+    this.closeEvent.emit({value:null});
+  }
+  onEntityChange(entity) {
+    this.selectedEntity={};
+    this.solution.entities.forEach(item=>{
+      if(item._id !== entity._id){ 
+         item.selected = false;
+      }else{
+         item.selected = true;
+         this.selectedEntity=entity;
+      } 
+    })
+    this.selectedListCount=1;
+  }
+
+  submit() {
+    let data={
+      solutionDetail:this.solution,
+      selectedEntity:this.selectedEntity
+    }
+    this.closeEvent.emit({value:data});
+    this.modal.approve();
+  }
+}
---
+++ src/app/client/src/app/modules/report/components/index.ts
@@ -0,0 +1,3 @@
+export * from './solution-listing/solution-listing.component';
+export * from './entity-list/entity-list.component';
+export * from './report-view/report-view.component'
