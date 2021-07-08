94e0b523f1abfa95251fe84eec830212f36270f5
pradeep kumar <pradeep.k@pacewisdom.com>
2021-07-08 11:25:49 +0530

    SB-24852 : test :guard,alert modl test case

--- src/app/client/src/app/modules/shared/components/alert-modal/alert-modal.component.spec.ts
+++ src/app/client/src/app/modules/shared/components/alert-modal/alert-modal.component.spec.ts
@@ -1,16 +1,50 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { AlertModalComponent } from "./alert-modal.component";
+import {
+  SuiModal,
+  ComponentModalConfig,
+  ModalSize,
+  SuiModalModule,
+} from "ng2-semantic-ui-v9";
+import { Location } from "@angular/common";
+import { ResourceService, SharedModule } from "@sunbird/shared";
+import { FormsModule } from "@angular/forms";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {metaData} from './alert-modal.component.spec.data';
+import { Directive } from "@angular/core";
 
-import { AlertModalComponent } from './alert-modal.component';
+interface IAlertModalContext {
+  data: any;
+}
 
-xdescribe('AlertModalComponent', () => {
+describe("AlertModalComponent", () => {
   let component: AlertModalComponent;
   let fixture: ComponentFixture<AlertModalComponent>;
+  let alertModal;
+  
+  const modalContext:IAlertModalContext = {
+    data: metaData,
+  };
+  const fakeModalService = {
+    approve: () => {},
+    deny: () => {},
+    context: modalContext,
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlertModalComponent ]
-    })
-    .compileComponents();
+      imports: [
+        SharedModule.forRoot(),
+        SuiModalModule,
+        FormsModule,
+        HttpClientTestingModule,
+      ],
+      declarations: [],
+      providers: [
+        ResourceService,
+        { provide: SuiModal, useValue: fakeModalService },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +53,30 @@ xdescribe('AlertModalComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should call getMethod type accept", () => {
+    spyOn(component, "getMethod").and.callThrough();
+    component.getMethod(metaData.footer.buttons[1]);
+    expect(component.getMethod).toHaveBeenCalled();
+  });
+
+  it("should call getMethod type cancel", () => {
+    spyOn(component, "getMethod").and.callThrough();
+    component.getMethod(metaData.footer.buttons[0]);
+    expect(component.getMethod).toHaveBeenCalled();
+  });
+
+  it("should call navigatePrevious type cancel", () => {
+    spyOn(component, "navigatePrevious").and.callThrough();
+    component.navigatePrevious(metaData);
+    expect(component.navigatePrevious).toHaveBeenCalled();
+  });
 });
+
+
+
+
+
