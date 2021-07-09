ee2a77c14b8efe676e11c77d9bffaaac9832f9be
Balakrishna <balakrishna@mantralabsglobal.com>
2021-07-09 11:46:29 +0530

    Issue #SB-24116 fix: navigation back issue in DF

--- src/app/client/src/app/modules/shared/services/navigation-helper/navigation-helper.service.ts
+++ src/app/client/src/app/modules/shared/services/navigation-helper/navigation-helper.service.ts
@@ -66,15 +66,15 @@ export class NavigationHelperService {
         const url = urlAfterRedirects.url.split('?')[0];
         let history: UrlHistory;
         if (_.isEmpty(queryParams)) {
-        history = {url};
+          history = {url};
         } else {
-        history = {url, queryParams};
+          history = {url, queryParams};
         }
         const previousUrl = this._history.pop();
         if (previousUrl === undefined || (previousUrl && previousUrl.url === history.url )) {
-            this._history.push(history);
+          this._history.push(history);
         } else {
-            this._history.push(previousUrl, history);
+          this._history.push(previousUrl, history);
         }
       }
     });
