ceba405f8ef5f540922d088e94f35a4285b48f43
Gourav More <gourav_m@tekditechnologies.com>
2021-07-09 17:57:28 +0530

    Issue #SB-24596 fix: Banner based discovery of content

--- src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
+++ src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
@@ -427,7 +427,7 @@ export class ExplorePageComponent implements OnInit, OnDestroy, AfterViewInit {
                                     const facetKeys = _.map(currentPageData.sections, (section) => { return section.facetKey });
                                     const facets = this.utilService.processCourseFacetData(_.get(response, 'result'), facetKeys);
                                     forEach(currentPageData.sections, facet => {
-                                        if (_.get(facets, facet.facetKey) && _.get(facets, facet.facetKey).length > 0) {
+                                        if (_.get(facets, facet.facetKey)) {
                                             let _facetArray = [];
                                             forEach(facets[facet.facetKey], _facet => {
                                                 _facetArray.push({
@@ -440,10 +440,11 @@ export class ExplorePageComponent implements OnInit, OnDestroy, AfterViewInit {
                                                 name: facet.facetKey,
                                                 data: _.sortBy(_facetArray, ['name']),
                                                 section: facet,
+                                                index: facet.index
                                             });
                                         }
                                     });
-
+                                    this.facetSections = _.sortBy(this.facetSections, ['index']);
                                     if (facetKeys.indexOf('search') > -1) {
                                         const section = currentPageData.sections.find(sec => sec.facetKey === 'search');
                                         this.contentSection = {
@@ -912,6 +913,10 @@ export class ExplorePageComponent implements OnInit, OnDestroy, AfterViewInit {
 
     getSectionTitle (title) {
         return get(this.resourceService, 'frmelmnts.lbl.browseBy') + ' ' + get(this.resourceService, title);
+    }
+
+    getBannerTitle (title) {
+        return get(this.resourceService, title);
     }
 
     getSelectedTab () {
