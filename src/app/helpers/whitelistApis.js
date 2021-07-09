728d24d37939d5ea6cee0d72cfe628fe4352931b
kiranTunerlabs <kiran@tunerlabs.com>
2021-07-09 10:43:46 +0530

    API bloacking issue fix

--- src/app/helpers/whitelistApis.js
+++ src/app/helpers/whitelistApis.js
@@ -1632,51 +1632,51 @@ const API_LIST = {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ORG_ADMIN, ROLE.CONTENT_CREATOR, ROLE.COURSE_MENTOR]
     },
-    '/kendra/api/v1/users/entityTypesByLocationAndRole/:stateId': {
+    '/kendra/v1/users/entityTypesByLocationAndRole/:stateId': {
       checksNeeded: [],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/kendra/api/v1/solutions/targetedSolutions': {
+    '/kendra/v1/solutions/targetedSolutions': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/assessment/api/v1/observations/entities': {
+    '/assessment/v1/observations/entities': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/assessment/api/v1/observationSubmissions/list/:id': {
+    '/assessment/v1/observationSubmissions/list/:id': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/kendra/api/v1/users/targetedEntity/:id': {
+    '/kendra/v1/users/targetedEntity/:id': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/assessment/api/v2/observations/searchEntities': {
+    '/assessment/v2/observations/searchEntities': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/assessment/api/v1/observationSubmissions/create/:id': {
+    '/assessment/v1/observationSubmissions/create/:id': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/assessment/api/v2/observations/assessment/:id': {
+    '/assessment/v2/observations/assessment/:id': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/kendra/api/v1/cloud-services/files/preSignedUrls': {
+    '/kendra/v1/cloud-services/files/preSignedUrls': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/assessment/api/v1/observationSubmissions/list/:id': {
+    '/assessment/v1/observationSubmissions/list/:id': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/assessment/api/v1/observations/updateEntities/:id': {
+    '/assessment/v1/observations/updateEntities/:id': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/assessment/api/v1/observationSubmissions/update/:id': {
+    '/assessment/v1/observationSubmissions/update/:id': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
@@ -1700,15 +1700,15 @@ const API_LIST = {
         ROLE.BOOK_CREATOR
       ]
     },
-    '/dhiti/api/v1/reports/fetch': {
+    '/dhiti/v1/reports/fetch': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/assessment/api/v1/observationSubmissions/solutionList': {
+    '/assessment/v1/observationSubmissions/solutionList': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
-    '/dhiti/api/v1/observations/listAllEvidences': {
+    '/dhiti/v1/observations/listAllEvidences': {
       checksNeeded: ['ROLE_CHECK'],
       ROLE_CHECK: [ROLE.ALL, ROLE.PUBLIC]
     },
@@ -1821,14 +1821,14 @@ const API_LIST = {
     '/dataset/v1/request/list/:tag',
     '/dataset/v1/request/read/:tag',
     '/report/request/read/:tag',
-    '/kendra/api/v1/users/entityTypesByLocationAndRole/:stateId',
-    '/assessment/api/v1/observationSubmissions/list/:id',
-    '/kendra/api/v1/users/targetedEntity/:id',
-    '/assessment/api/v1/observationSubmissions/create/:id',
-    '/assessment/api/v2/observations/assessment/:id',
-    '/assessment/api/v1/observationSubmissions/list/:id',
-    '/assessment/api/v1/observations/updateEntities/:id',
-    '/assessment/api/v1/observationSubmissions/update/:id',
+    '/kendra/v1/users/entityTypesByLocationAndRole/:stateId',
+    '/assessment/v1/observationSubmissions/list/:id',
+    '/kendra/v1/users/targetedEntity/:id',
+    '/assessment/v1/observationSubmissions/create/:id',
+    '/assessment/v2/observations/assessment/:id',
+    '/assessment/v1/observationSubmissions/list/:id',
+    '/assessment/v1/observations/updateEntities/:id',
+    '/assessment/v1/observationSubmissions/update/:id',
     '/action/collection/v1/import/:id',
     '/action/collection/v1/export/:id'
   ]
