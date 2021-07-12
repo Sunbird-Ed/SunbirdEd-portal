52383f8c977807c87089ce0a94f091c8311abcfa
rakeshSgr <rakesh.k@pacewisdom.com>
2021-07-12 18:04:59 +0530

    Issue #SB-25459 fix: removed spaces

--- src/app/client/src/app/modules/dashboard/components/report/report.component.ts
+++ src/app/client/src/app/modules/dashboard/components/report/report.component.ts
@@ -158,7 +158,6 @@ export class ReportComponent implements OnInit {
                 let chart = (charts && this.reportService.prepareChartData(charts, data, updatedDataSource,
                   _.get(reportConfig, 'reportLevelDataSourceId'))) || [];
                 chart = chart.filter(function(chartData){ if(chartData ){ return (chartData['chartData'] != null || chartData['chartData'] != undefined) } });
-                
                 result['charts'] =  chart;
                 result['tables'] = (tables && this.reportService.prepareTableData(tables, data, _.get(reportConfig, 'downloadUrl'),
                   this.hash)) || [];
