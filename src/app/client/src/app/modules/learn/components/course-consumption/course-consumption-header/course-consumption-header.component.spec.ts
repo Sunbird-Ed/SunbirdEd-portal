6db65db6728a13e29eb11d8caa306a707e0d7fc1
sudip mukherjee <sudip.mukherjee@tarento.com>
2021-07-13 17:33:38 +0530

    Issue #SB-25570 fix: TC failure

--- src/app/client/src/app/modules/learn/components/course-consumption/course-consumption-header/course-consumption-header.component.spec.ts
+++ src/app/client/src/app/modules/learn/components/course-consumption/course-consumption-header/course-consumption-header.component.spec.ts
@@ -492,14 +492,14 @@ describe('CourseConsumptionHeaderComponent', () => {
   it('should navigate to discussion Forum', () => {
     const routerData = {
       forumIds: [6],
-      userName: 'cctn1350'
+      userId: 1
     };
     spyOn(component['router'], 'navigate');
     component.assignForumData(routerData);
     expect(component['router'].navigate).toHaveBeenCalledWith(['/discussion-forum'], {
       queryParams: {
         categories: JSON.stringify({ result: routerData.forumIds }),
-        userName: routerData.userName
+        userId: routerData.userId
       }
     });
   });
