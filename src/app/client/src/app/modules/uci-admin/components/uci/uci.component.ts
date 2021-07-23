4f2c3c1103b403d4670b7bb089a2850ea1aecb6b
Manoj Singh Rawat <manojrawat1502@gmail.com>
2021-07-23 21:05:54 +0530

    UCI: base URL input add and route file add

--- src/app/client/src/app/modules/uci-admin/components/uci/uci.component.ts
+++ src/app/client/src/app/modules/uci-admin/components/uci/uci.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 import { UserService } from '../../../core/services/user/user.service';
+import {ConfigService} from '@sunbird/shared';
 
 @Component({
   selector: 'app-uci',
@@ -8,15 +9,18 @@ import { UserService } from '../../../core/services/user/user.service';
   styleUrls: ['./uci.component.scss']
 })
 export class UciComponent implements OnInit {
-
   public userProfile: any;
-  constructor(public userService: UserService) { }
+  public url;
+
+  constructor(public userService: UserService,
+              private config: ConfigService) { }
 
   ngOnInit(): void {
+    // this.url = this.config.urlConFig.URLS.PUBLIC_PREFIX;
+    this.url = 'https://uci-server2.ngrok.samagra.io';
     this.userService.userData$.pipe(first()).subscribe(async (user) => {
       if (user && user.userProfile) {
         this.userProfile = user.userProfile;
-        console.log(this.userProfile)
       }
     });
   }
