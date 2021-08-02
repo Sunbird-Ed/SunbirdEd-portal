3585c119a153bbde5c1c82c09cd0353ea010d009
pradeep kumar <pradeep.k@pacewisdom.com>
2021-08-02 12:04:53 +0530

    Issue #SB-25858 : fix : credit and license added

--- src/app/client/src/app/modules/observation/components/observation-details/observation-details.component.ts
+++ src/app/client/src/app/modules/observation/components/observation-details/observation-details.component.ts
@@ -41,7 +41,27 @@ export class ObservationDetailsComponent implements OnInit {
   public noResultMessageForEntity: INoResultMessage = {
     'messageText': 'frmelmnts.msg.noEntityFound'
   };
-
+  courseHierarchy={
+    author:'CBSE',
+    creator:'CBSE',
+    copyright: "CBSE",
+    copyrightYear: 1998,
+    contentType: "Course",
+    organisation: [
+      "CBSE"
+    ],
+    orgDetails: {
+      email: null,
+      orgName: "CBSE"
+    },
+    licenseDetails: {
+      name: "CC BY 4.0",
+      url: "https://creativecommons.org/licenses/by/4.0/legalcode",
+      description: "For details see below:"
+    },
+    createdOn: Date.now(),
+    lastUpdatedOn:Date.now()
+  };
   constructor(
     private observationService: ObservationService,
     config: ConfigService,
