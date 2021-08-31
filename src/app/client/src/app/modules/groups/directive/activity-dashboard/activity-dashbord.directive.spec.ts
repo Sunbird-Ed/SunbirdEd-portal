7e0ae58bbcca9ca03c7f4f2e92de801e76e7e60d
Kamaleeswary <kamaleeanbu@gmail.com>
2021-08-31 15:51:28 +0530

    Issue #SB-26396 fix: added tc for service file

--- src/app/client/src/app/modules/groups/directive/activity-dashboard/activity-dashbord.directive.spec.ts
+++ src/app/client/src/app/modules/groups/directive/activity-dashboard/activity-dashbord.directive.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { ActivityDashboardService } from '@sunbird/shared';
+import { ActivityDashboardService } from '../../../shared/services';
 import { ActivityDashboardDirective } from './activity-dashbord.directive';
 
 describe('ActivityDashboardDirective', () => {
