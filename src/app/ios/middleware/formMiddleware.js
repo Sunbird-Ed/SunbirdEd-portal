2728a7312aeacf0b60142f8df74ac5ef13300b13
Ravinder Kumar <yravinderkumar33@gmail.com>
2021-09-20 18:44:33 +0530

    Issue #SB-0000 fix: IOS form type redirection logic

--- src/app/ios/middleware/formMiddleware.js
+++ src/app/ios/middleware/formMiddleware.js
@@ -8,15 +8,15 @@ const mapping = {
 const IOSFormMiddleware = (req, res, next) => {
     const platform = req.get('X-Platform-Id');
     if (platform && typeof(platform) == 'string' && platform.toLowerCase() === 'ios') {
-        const { body, query: { platform } } = req;
-        if (body && platform.toLowerCase() === 'ios') {
+        const { body } = req;
+        if (body) {
             const { type, subType, component } = body.request || {};
             const key = `${type}_${subType}_${component}`;
             const hasMapping = key in mapping;
             if (hasMapping) {
                 req.body.request = {
                     ...req.body.request,
-                    ...mapping[formType]
+                    ...mapping[key]
                 }
             }
         }
