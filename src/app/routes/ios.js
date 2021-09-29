0fd87e863c2bdfc2b954ccaa5b450ea8beb43876
Raghavendra Shivhare <shivhareraghavendra@gmail.com>
2021-09-29 15:58:03 +0530

    Issue #SB-25729 feat: apple login route refactoring 3rd itteration

--- src/app/routes/ios.js
+++ src/app/routes/ios.js
@@ -1,10 +1,7 @@
 const bodyParser = require("body-parser");
-const { IOSFormMiddleware } = require("../helpers/ios/middleware");
 const { appleSininHandler } = require("../helpers/ios/appleSignInHandler");
 
 
 module.exports = function (app) {
-    app.use('/api/data/v1/form/read', bodyParser.json(), IOSFormMiddleware)
-
     app.post('/apple/auth/ios', bodyParser.json(), appleSininHandler);
 }
