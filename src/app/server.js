529fa8a3f28be13a768c70af968a1d0d84b5fd18
Ravinder Kumar <yravinderkumar33@gmail.com>
2021-09-29 13:13:24 +0530

    Issue #SB-0000 feat: form Service version update & removal of unused middlware for iOS Platform

--- src/app/server.js
+++ src/app/server.js
@@ -234,8 +234,6 @@ require('./routes/refreshTokenRoutes.js')(app, keycloak) // refresh token routes
 
 require('./routes/accountMergeRoute.js')(app, keycloak) // refresh token routes
 
-require('./routes/ios.js')(app);
-
 require('./routes/clientRoutes.js')(app, keycloak) // client app routes
 
 require('./routes/reportRoutes.js')(app, keycloak) // report routes
