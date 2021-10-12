2bc850b5fa1f5be957d8c8e7184f7f5d588bcb15
paritshivani <shivani_p@techjjomla.com>
2021-10-12 08:50:29 +0530

    integratin

--- src/app/client/src/environments/urlConfig.ts
+++ src/app/client/src/environments/urlConfig.ts
@@ -1,62 +1,85 @@
+
 //  export const urlConfig = {
 //     // endpoint configs...!
 //     // URLs...!
-//     eventDetailApi :  "assets/api/event-detail.json",
+//     // eventDetailApi :  "assets/api/event-detail.json",
 //     eventListApi : "assets/api/eventlist.json",
 //     eventCreateApi: "https://jsonplaceholder.typicode.com/posts",
-//     eventFormConfigApi : "assets/api/event-create.json",
-//     enrollListApi : "assets/api/enroll-list.json",
+//     // eventFormConfigApi : "assets/api/event-create.json",
+//     // eventFilterConfigApi: "assets/api/event-filter.json",
+//     enrollListApi : "assets/api/enrolled-events.json",
+//     // myEvents: "assets/api/myeventlist.json",
+//     myEvents:"v2/user/courses/list",
 //     enrollApi: "v1/event/enroll",
+//     usersApi: "assets/api/users.json",
 //     // URLs...!
-//      detail :  "assets/api/event-detail.json",
-//     //detail :  "https://igot-sunbird.idc.tarento.com/api/event/v4/read/",
+//     // detail :  "/event/v4/read/do_11334351316861747212",
+//     // detail :  "/event/v4/read/",
+//     detail : "assets/api/event-detail.json",
 //     list : "assets/api/eventlist.json",
-//     create: "https://igot-sunbird.idc.tarento.com/api/event/v4/create",
-//     update: "https://igot-sunbird.idc.tarento.com/api/event/v4/update",
+//     create: "/api/event/v4/create",
+//     update: "/event/v4/update",
 //     formConfig : "assets/api/event-create.json",
-//     enrolllist : "assets/api/enroll-list.json",
+//     enrolllist : "assets/api/enrolled-events.json",
+//     // enrolllist : "assets/api/enroll-list.json",
 //     enroll: "https://igot-sunbird.idc.tarento.com/v1/event/enroll",
 //     retire : "https://igot-sunbird.idc.tarento.com/private/event/v4/retire",
-//     publish: "https://igot-sunbird.idc.tarento.com/event/v4/publish",
+//     publish: "/event/v4/publish",
 //     calenderevent:"assets/api/eventlist_new.json",
-//     eventFilterConfigApi: "assets/api/event-filter.json"
+//     ImageSearchApi :  "assets/api/search-images.json",
+//     EditModeApi :  "assets/api/editmode.json",
+//     createImage:"content/v3/create",
+//     BBBGetUrl:"event/v4/join",
+//     search:"/api/event/v1/search",
+//     eventFormConfigApi : "https://staging-sunbird.nsdl.co.in/api/data/v1/form/read",
+//     eventFilterConfigApi : "https://staging-sunbird.nsdl.co.in/api/data/v1/form/read"
 // };
 
- export const urlConfig = {
+
+
+
+
+
+export const urlConfig = {
     // endpoint configs...!
-    // URLs...!
-    // eventDetailApi :  "assets/api/event-detail.json",
+    // Live API's
+    create: "/event/v4/create",
+    update: "/event/v4/update",
+    detail :  "/event/v4/read/",
+    publish:"/event/v4/publish",
+    enrollApi: "/v1/event/enroll",
+    unenrollApi: "/v1/event/unenroll",
+    BBBGetUrlModerator: "/event/v4/join/moderator",
+    BBBGetUrlAttendee:"/event/v4/join/attendee",
+    createImage: "content/v3/create",
+    batchlist: "/v1/course/batch/search",
+    createBatch: "/v1/course/batch/create",
+    enrollUserEventList:"/v2/user/courses/list",
+    eventFormConfigApi : "https://staging-sunbird.nsdl.co.in/api/data/v1/form/read",
+    eventFilterConfigApi : "https://staging-sunbird.nsdl.co.in/api/data/v1/form/read",
+    myEvents:"/v2/user/courses/list",
+    search:"/api/event/v3/search",
+
     eventListApi : "assets/api/eventlist.json",
     eventCreateApi: "https://jsonplaceholder.typicode.com/posts",
-    // eventFormConfigApi : "assets/api/event-create.json",
-    // eventFilterConfigApi: "assets/api/event-filter.json",
     enrollListApi : "assets/api/enrolled-events.json",
-    // myEvents: "assets/api/myeventlist.json",
-    myEvents:"v2/user/courses/list",
-    enrollApi: "v1/event/enroll",
+    
     usersApi: "assets/api/users.json",
     // URLs...!
-    // detail :  "/event/v4/read/do_11334351316861747212",
-    // detail :  "/event/v4/read/",
-    // detail :  "https://igot-sunbird.idc.tarento.com/api/event/v4/read/",
-    detail : "assets/api/event-detail.json",
-    list : "assets/api/eventlist.json",
-    create: "/api/event/v4/create",
-    update: "/event/v4/update",
+//    list : "assets/api/eventlist.json",
+    
     formConfig : "assets/api/event-create.json",
     enrolllist : "assets/api/enrolled-events.json",
     // enrolllist : "assets/api/enroll-list.json",
     enroll: "https://igot-sunbird.idc.tarento.com/v1/event/enroll",
     retire : "https://igot-sunbird.idc.tarento.com/private/event/v4/retire",
     // publish: "https://igot-sunbird.idc.tarento.com/event/v4/publish",
-    publish: "/event/v4/publish",
+    
     calenderevent:"assets/api/eventlist_new.json",
     ImageSearchApi :  "assets/api/search-images.json",
     EditModeApi :  "assets/api/editmode.json",
-    createImage:"content/v3/create",
+    
     // BBBGetUrl:"assets/api/BBBCreatEvent.json",
-    BBBGetUrl:"event/v4/join",
-    search:"/v1/search",
-    eventFormConfigApi : "https://staging-sunbird.nsdl.co.in/api/data/v1/form/read",
-    eventFilterConfigApi : "https://staging-sunbird.nsdl.co.in/api/data/v1/form/read"
+    // BBBGetUrl:"event/v4/join",
+    
 };
