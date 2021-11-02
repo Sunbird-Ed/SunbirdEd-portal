0088038ca0ebaa4220b7a6c39005d6062470bc90
paritshivani <shivani_p@techjjomla.com>
2021-11-02 18:28:17 +0530

    issues

--- src/app/routes/clientRoutes.js
+++ src/app/routes/clientRoutes.js
@@ -101,7 +101,7 @@ module.exports = (app, keycloak) => {
   '/orgType', '/orgType/*', '/dashBoard', '/dashBoard/*',
   '/workspace', '/workspace/*', '/profile', '/profile/*', '/learn', '/learn/*', '/resources', '/discussion-forum/*',
   '/resources/*', '/myActivity', '/myActivity/*', '/org/*', '/manage', '/contribute','/contribute/*','/groups','/groups/*', '/my-groups','/my-groups/*','/certs/configure/*',
-   '/observation', '/observation/*','/solution','/solution/*','/questionnaire','/questionnaire/*', '/uci-admin', '/uci-admin/*'], 
+   '/observation', '/observation/*','/solution','/solution/*','/questionnaire','/questionnaire/*', '/uci-admin', '/uci-admin/*','/explore-events/*'], 
   session({
     secret: envHelper.PORTAL_SESSION_SECRET_KEY,
     resave: false,
@@ -116,7 +116,7 @@ module.exports = (app, keycloak) => {
   app.all(['/', '/get', '/:slug/get', '/:slug/get/dial/:dialCode',  '/get/dial/:dialCode', '/explore',
     '/explore/*', '/:slug/explore', '/:slug/explore/*', '/play/*', '/:slug/play/*',  '/explore-course', '/explore-course/*',
     '/:slug/explore-course', '/:slug/explore-course/*', '/:slug/signup', '/signup', '/:slug/sign-in/*',
-    '/sign-in/*', '/download/*', '/accountMerge/*','/:slug/accountMerge/*', '/:slug/download/*', '/certs/*', '/:slug/certs/*', '/recover/*', '/:slug/recover/*', '/explore-groups',
+    '/sign-in/*', '/download/*', '/accountMerge/*','/:slug/accountMerge/*', '/:slug/download/*', '/certs/*', '/:slug/certs/*', '/recover/*', '/:slug/recover/*', '/explore-groups','/explore-events/*',
     '/guest-profile'], 
     session({
       secret: envHelper.PORTAL_SESSION_SECRET_KEY,
@@ -278,7 +278,7 @@ const loadTenantFromLocal = (req, res) => {
   }
 }
 const redirectTologgedInPage = (req, res) => {
-	let redirectRoutes = { '/explore': '/resources', '/explore/1': '/search/Library/1', '/explore-course': '/learn', '/explore-course/1': '/search/Courses/1', '/guest-profile': '/profile', '/explore-groups': '/my-groups' };
+	let redirectRoutes = { '/explore': '/resources', '/explore/1': '/search/Library/1', '/explore-course': '/learn', '/explore-course/1': '/search/Courses/1', '/guest-profile': '/profile', '/explore-groups': '/my-groups' ,'/explore-events':'/explore-events'};
 	if (req.params.slug) {
 		redirectRoutes = {
 			[`/${req.params.slug}/explore`]: '/resources',
