eb1f93e77ef237e81da41d93015173df03234883
paritshivani <shivani_p@techjjomla.com>
2021-11-03 17:46:39 +0530

    issues

--- src/app/client/src/app/modules/public/module/events/events.module.ts
+++ src/app/client/src/app/modules/public/module/events/events.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-  EventViewTypeComponent, EventDetailComponent,EventReportComponent
-} from './components';
+  EventViewTypeComponent, EventDetailComponent,EventReportComponent} from './components';
 import { EventRoutingModule } from './events.routing.module';
 import { TelemetryModule } from '@sunbird/telemetry';
 import { CoreModule } from '@sunbird/core';
