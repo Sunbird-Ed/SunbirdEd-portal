d5f98aa3415889ad5bd51367a4556fce70b4e51f
Vivek kasture <vivek_kasture@techjoomla.com>
2022-01-04 13:53:48 +0530

    Issue #SB-27892 fix: Fixed multi instances of player (#7639)
    * Issue #SB-25116 fix: Updated username for the guest user
    
    * Issue #SB-25116 fix: Updated username for the guest user
    
    * Issue #SB-25116 fix: Updated username for the guest user
    
    * Issue #SB-24955 fix: Updated EPUB player
    
    * Issue #SB-23649 fix: Updated pdf player
    
    * Issue #SB-23649 fix: Updated pdf player
    
    * Issue #SB-28049 fix: Updated QUML player
    
    * Issue #SB-27892 fix: Fixed multi instances of player
    
    * Issue #0000 fix: Updated players
    
    * Issue #0000 fix: Updated players

--- src/app/client/src/app/modules/learn/components/course-consumption/assessment-player/assessment-player.component.ts
+++ src/app/client/src/app/modules/learn/components/course-consumption/assessment-player/assessment-player.component.ts
@@ -338,7 +338,7 @@ export class AssessmentPlayerComponent implements OnInit, OnDestroy, ComponentCa
       return this.toasterService.error(_.get(this.resourceService, 'frmelmnts.lbl.selfAssessMaxAttempt'));
     } else if (_.get(event, 'event.isLastAttempt') && !this._routerStateContentStatus) {
       this.showLastAttemptsModal = true;
-    } else {
+    } else if (_.get(this.navigationObj, 'event.data') && this.navigationObj?.event?.data?.identifier !== this.activeContent?.identifier) {
       this.onTocCardClick();
     }
   }
@@ -346,13 +346,10 @@ export class AssessmentPlayerComponent implements OnInit, OnDestroy, ComponentCa
   onTocCardClick() {
     this.showPlayer = false;
     this.previousContent = _.cloneDeep(this.activeContent);
-    /* istanbul ignore else */
-    if (_.get(this.navigationObj, 'event.data')) {
-      this.activeContent = this.navigationObj.event.data;
-      this.initPlayer(_.get(this.activeContent, 'identifier'));
-      this.highlightContent();
-      this.logTelemetry(this.navigationObj.id, this.navigationObj.event.data);
-    }
+    this.activeContent = this.navigationObj.event.data;
+    this.initPlayer(_.get(this.activeContent, 'identifier'));
+    this.highlightContent();
+    this.logTelemetry(this.navigationObj.id, this.navigationObj.event.data);
   }
 
   private getContentState() {
