9150c1bda52e21f19a4bb9c39a2f968960b60a65
Rajeev Sathish <rajeev.sathish@tarento.com>
2022-01-28 12:15:22 +0530

    Issue #SB-28134 Migration of Popup into material ui

--- src/app/client/src/app/modules/shared-feature/components/cert-preview-popup/cert-preview-popup.component.spec.ts
+++ src/app/client/src/app/modules/shared-feature/components/cert-preview-popup/cert-preview-popup.component.spec.ts
@@ -1,10 +1,11 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CertPreviewPopupComponent } from './cert-preview-popup.component';
 import { SuiModule } from 'ng2-semantic-ui-v9';
-import { ResourceService } from '@sunbird/shared';
+import { ResourceService, SharedModule } from '@sunbird/shared';
 import { By } from '@angular/platform-browser';
 
-describe('CertPreviewPopupComponent', () => {
+fdescribe('CertPreviewPopupComponent', () => {
   let component: CertPreviewPopupComponent;
   let fixture: ComponentFixture<CertPreviewPopupComponent>;
 
@@ -13,7 +14,7 @@ describe('CertPreviewPopupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [SuiModule],
+      imports: [SuiModule, SharedModule.forRoot(), BrowserAnimationsModule],
       declarations: [ CertPreviewPopupComponent ],
       providers: [
         {provide: ResourceService, useValue: resourceBundle}
