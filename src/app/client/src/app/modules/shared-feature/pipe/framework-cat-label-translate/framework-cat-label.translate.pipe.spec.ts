0471eb6177d474e284c6ee8268873d864d13d8a8
Ravinder Kumar <yravinderkumar33@gmail.com>
2022-02-09 14:51:46 +0530

    Issue #SB-28608 fix: test case fix

---
+++ src/app/client/src/app/modules/shared-feature/pipe/framework-cat-label-translate/framework-cat-label.translate.pipe.spec.ts
@@ -0,0 +1,44 @@
+import { } from 'jasmine';
+import { ResourceService, SharedModule } from '@sunbird/shared';
+import { FrameworkCatLabelTranslatePipe } from './framework-cat-label-translate.pipe';
+import { TestBed, async } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+beforeEach(async(() => {
+    TestBed.configureTestingModule({
+        declarations: [FrameworkCatLabelTranslatePipe],
+        imports: [SharedModule.forRoot(), HttpClientTestingModule],
+        providers: [
+            {
+                provide: ResourceService, useValue: {
+                    frmelmnts: {
+                        lbl: {
+                            boards: 'boards'
+                        }
+                    }
+                }
+            }
+        ]
+    })
+        .compileComponents();
+}));
+
+describe('FrameworkCatLabelTranslatePipe', () => {
+    describe('#transform', () => {
+        it('Should tranform category label', () => {
+            const resourceService = TestBed.get(ResourceService);
+            const pipe = new FrameworkCatLabelTranslatePipe(resourceService);
+            const transformedResult = pipe.transform('board');
+            expect(transformedResult).toBeDefined();
+            expect(transformedResult).toBe('boards');
+        });
+
+        it('Should return the label if translation is not available', () => {
+            const resourceService = TestBed.get(ResourceService);
+            const pipe = new FrameworkCatLabelTranslatePipe(resourceService);
+            const transformedResult = pipe.transform('random');
+            expect(transformedResult).toBeDefined();
+            expect(transformedResult).toBe('random');
+        });
+    });
+});
