b500c2b2fa2e2c50df734ccefd0d854b639e4305
Rajeev Sathish <rajeev.sathish@tarento.com>
2022-02-10 13:57:42 +0530

    Revert "Issue #SB-0000 node update for poortal and desktop build failure"

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 STARTTIME=$(date +%s)
-NODE_VERSION=14.19.0
 echo "Starting portal build from build.sh"
 set -euo pipefail	
 export NVM_DIR="$HOME/.nvm"
@@ -21,7 +20,7 @@ then
 fi
 
 commit_hash=$(git rev-parse --short HEAD)
-nvm install $NODE_VERSION # same is used in client and server
+nvm install 12.16.1 # same is used in client and server
 
 cd src/app
 mkdir -p app_dist/ # this folder should be created prior server and client build
@@ -46,7 +45,7 @@ build_client_cdn(){
 # function to run client build
 build_client(){
     echo "Building client in background"
-    nvm use $NODE_VERSION
+    nvm use 12.16.1
     cd client
     echo "starting client yarn install"
     yarn install --no-progress --production=true
@@ -69,7 +68,7 @@ build_server(){
     echo "copying requied files to app_dist"
     cp -R libs helpers proxy resourcebundles package.json framework.config.js sunbird-plugins routes constants controllers server.js ./../../Dockerfile app_dist
     cd app_dist
-    nvm use $NODE_VERSION
+    nvm use 12.16.1
     echo "starting server yarn install"
     yarn install --no-progress --production=true
     echo "completed server yarn install"
