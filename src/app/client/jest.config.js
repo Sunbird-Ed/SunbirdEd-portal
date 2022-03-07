08467b5a3888592c4777d849939d3e062176c7e0
Rajesh <rajesh.kumaravel@tarento.com>
2022-03-07 09:52:59 +0530

    Issue #SB-0000 fix: Jest ignore test pattern updated.

--- src/app/client/jest.config.js
+++ src/app/client/jest.config.js
@@ -1,15 +1,44 @@
+const { defaults } = require('jest-config');
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
 module.exports = {
   preset: "jest-preset-angular",
+  "verbose": true,
+  "testEnvironment": "jsdom",
   roots: [
     "<rootDir>"
   ],
   setupFilesAfterEnv: [
     "<rootDir>/setup-jest.ts"
   ],
-  moduleNameMapper: {
-    '@sunbirdshared': "<rootDir>/app/modules/shared/"
-  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' }),
+  // "modulePaths": [
+  //   "<rootDir>",
+  // ],
+  // preset is optional, you don't need it in case you use babel preset typescript
+  preset: 'ts-jest',
+  // note this prefix option
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
   transformIgnorePatterns: [
-    "<rootDir>/app/modules/shared/"
+    "<rootDir>/node_modules/"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    ".data.spec.ts",
+    ".spec.data.ts",
+    "sb-form-location-selection.delegate.spec.ts"
   ]
+  // transform: {
+  //   "lodash-es": "<rootDir>/node_modules/lodash-es"
+  // }
 }
+
+function setModuleNameMapper () {
+  console.log('_______________MODULE_MAPPER________________')
+  let _moduleMaps = pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' });
+  console.log(_moduleMaps); // TODO: log!
+  console.log('_______________MODULE_MAPPER________________')
+  // return _moduleMaps;
+}
+setModuleNameMapper()
