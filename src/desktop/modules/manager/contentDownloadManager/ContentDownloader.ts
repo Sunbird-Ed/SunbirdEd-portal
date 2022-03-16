6122718a546e94ff44c6e05d5660e506701da9e4
Nik720 <nikunj.beladiya@tarento.com>
2022-03-16 16:29:00 +0530

    Issue #SB-29081 fix: trigger build

--- src/desktop/modules/manager/contentDownloadManager/ContentDownloader.ts
+++ src/desktop/modules/manager/contentDownloadManager/ContentDownloader.ts
@@ -259,7 +259,7 @@ export class ContentDownloader implements ITaskExecuter {
     metaData.baseDir = `content/${contentDetails.identifier}`;
     if(metaData.mimeType === "application/vnd.sunbird.question") {
       metaData.isAvailableLocally = true;
-      metaData.basePath = `content/${contentDetails.identifier}`;
+      metaData.basePath = `content/${contentDetails.identifier}`; 
     }
     metaData.desktopAppMetadata = {
       "addedUsing": ContentDownloader.taskType,
