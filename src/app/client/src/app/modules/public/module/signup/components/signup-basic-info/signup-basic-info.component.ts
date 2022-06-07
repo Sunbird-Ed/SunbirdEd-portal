4323e185eec5b0db75199c2905765cc5a7bed4cb
Nik720 <nikunj.beladiya@tarento.com>
2022-06-07 16:01:46 +0530

    Issue #SB-30088 fix: added basic sign up form details

--- src/app/client/src/app/modules/public/module/signup/components/signup-basic-info/signup-basic-info.component.ts
+++ src/app/client/src/app/modules/public/module/signup/components/signup-basic-info/signup-basic-info.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit,Input, Output } from '@angular/core';
+import { ResourceService, UtilService, ConfigService } from '@sunbird/shared';
+import { Observable } from 'rxjs';
+import { TelemetryService } from '@sunbird/telemetry';
+import { IStartEventInput, IImpressionEventInput, IInteractEventEdata } from '@sunbird/telemetry';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
+import * as _ from 'lodash-es';
 
 @Component({
   selector: 'app-signup-basic-info',
@@ -7,15 +14,78 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class SignupBasicInfoComponent implements OnInit {
 
-  @Output() triggerNext = new EventEmitter();
+  @Input() startingForm: FormGroup;
+  @Output() subformInitialized: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
+  @Output() triggerNext: EventEmitter<boolean> = new EventEmitter<boolean>();
+  public personalInfoForm: FormGroup;
+  @Input() isIOSDevice;
+  @Input() telemetryImpression;
+  birthYearOptions: Array<string> = [];
+  filteredYOB: Observable<number[]>;
+  yearOfBirth: string;
+  isMinor: boolean;
   
-  constructor() { }
+  constructor(
+    public resourceService: ResourceService, public telemetryService: TelemetryService,
+    public utilService: UtilService, public configService: ConfigService, private _fb: FormBuilder) { }
+  
 
   ngOnInit(): void {
+    if (this.startingForm) {
+      this.personalInfoForm = this.startingForm;
+    } else {
+      this.personalInfoForm = this._fb.group({
+        name: ['', Validators.required],
+        yearOfBirth: ['', Validators.required]
+      })
+    }
+    this.subformInitialized.emit(this.personalInfoForm);
+    this.initiateYearSelecter();
+    // @ts-ignore
+    this.filteredYOB = this.personalInfoForm.controls.yearOfBirth.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value)),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.birthYearOptions.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   next() {
-    this.triggerNext.emit();
+    if(this.personalInfoForm.valid) {
+      let userDetails;
+      if(localStorage.getItem('guestUserDetails')) {
+        userDetails = JSON.parse(localStorage.getItem('guestUserDetails'));
+      } else {
+        userDetails = {name: this.personalInfoForm.controls.name.value}
+      }
+      userDetails.name = this.personalInfoForm.controls.name.value;
+      localStorage.setItem('guestUserDetails', JSON.stringify(userDetails))
+      this.triggerNext.emit();
+    } else {
+      console.log("Invalid form");
+    }
+  }
+
+  initiateYearSelecter() {
+    const endYear = new Date().getFullYear();
+    const startYear = endYear - this.configService.constants.SIGN_UP.MAX_YEARS;
+    for (let year = endYear; year > startYear; year--) {
+      this.birthYearOptions.push(year.toString());
+    }
+  }
+
+  changeBirthYear(selectedBirthYear) {
+    let _selectedYOB = parseInt(_.get(selectedBirthYear, 'option.value'));
+    if (this.isIOSDevice) {
+      _selectedYOB = parseInt(selectedBirthYear.target.value);
+    }
+    const currentYear = new Date().getFullYear();
+    this.yearOfBirth = `${_selectedYOB}`;
+    const userAge = currentYear - _selectedYOB;
+    this.isMinor = userAge < this.configService.constants.SIGN_UP.MINIMUN_AGE;
   }
 
 }
