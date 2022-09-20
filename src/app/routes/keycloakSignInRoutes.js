ac9153078d8cb12e6081489bb13e961172460dec
Pavithra <pavithra.prakash@tarento.com>
2022-09-20 14:21:45 +0530

    Issue #ED-76 fix: "Optimized code"

--- src/app/routes/keycloakSignInRoutes.js
+++ src/app/routes/keycloakSignInRoutes.js
@@ -1,78 +1,111 @@
 const bodyParser = require('body-parser');
 const envHelper = require('../helpers/environmentVariablesHelper.js');
 const request = require('request-promise');
-var qs = require('querystring');
-var DOMParser = require('xmldom').DOMParser;
+const qs = require('querystring');
+const DOMParser = require('xmldom').DOMParser;
 const parser = new DOMParser();
 const { logger } = require('@project-sunbird/logger');
+const _ = require('lodash');
+
+let formData = {
+  username: "",
+  password: ""
+}
+let redirect_uri = "";
+let client_id = "";
 
 module.exports = (app) => {
   app.post('/keycloak/login', bodyParser.json(), async (req, res) => {
-    let config = req.body.loginConfig;
-    let formData = {
-      username: req.body.emailId,
-      password: req.body.password
+    if (req && req.body && req.body.loginConfig) {
+      let config = req.body.loginConfig;
+      client_id = req.body.client_id;
+      formData.username = req.body.emailId;
+      formData.password = req.body.password;
+      let queryArr = [];
+      (config.params).forEach(item => {
+        if (item['key'] == 'redirect_uri') {
+          redirect_uri = item['value'];
+        }
+        queryArr.push((Object.values(item)))
+      });
+      let reqOption = getRequestOptions(config.host+config.path, getHeaders(), qs.stringify(Object.fromEntries(queryArr)));
+      handleRequest(reqOption, "reqLogin", res);
+    } else {
+      logger.info({msg: 'Keycloak Signin route: Cannot read a Property of undefined'})
+      res.send('Cannot read property of undefined');
     }
-    let redirect_uri = "";
-    let queryArr = [];
-    (config.params).forEach(item => {
-      if (item['key'] == 'redirect_uri') {
-        redirect_uri = item['value'];
-      }
-      queryArr.push((Object.values(item)))
-    });
-    let reqOption = getRequestOptions(config.host+config.path, getHeaders(), qs.stringify(Object.fromEntries(queryArr)));
-    request(reqOption, ((err, response, data) => {
-      if (!err && response.statusCode == 200) {
-        logger.info({msg: 'keyclaok Signin route: initialized ' + reqOption.method + reqOption.url})
-        var htmlObject = parser.parseFromString(data, 'text/html');
-        let ele = htmlObject.getElementById("kc-form-login");
-        let loginUrl = ele.getAttribute('action');
-        let loginOption = getRequestOptions(loginUrl, getHeaders(response.headers['set-cookie'].toString(), response.request.href+'?'+response.request.body), formData)
-        request(loginOption, ((e, resp, dataObj) => {
-          if (e) {
-            logger.error({msg: 'keyclaok Signin route : login failed ' + resp.statusCode + dataObj + 'error:'+ JSON.stringify(e)})
-            res.error(e + dataObj);
-          } else if (resp.statusCode == 302 && dataObj == "") {
-            logger.info({msg: 'keyclaok Signin route : successfully login with a keycloak user'});
-            let code = resp.headers.location.split('=');
-            let reqTokenOption = getRequestOptions(`${envHelper.PORTAL_AUTH_SERVER_URL}/realms/${envHelper.PORTAL_REALM}/protocol/openid-connect/token`, 
-              getHeaders(), qs.stringify({redirect_uri: redirect_uri, code: code[code.length - 1], grant_type: 'authorization_code',client_id: req.body.client_id}))
-            request(reqTokenOption, ((err, response, data) => {
-              if (!err && response.statusCode == 200) {
-                logger.info({msg: 'keyclaok Signin route : generated access token and refresh token - login sucessful'});
-                res.status(response.statusCode).send(data)
-              } else {
-                logger.error({msg: 'keyclaok Signin route : error while getting access token ' + resp.statusCode + ' error: ' + JSON.parse(data)})
-                res.send(JSON.parse(data));
-              }
-            }));
-          } else {
-            if (resp.statusCode == 200) {
-              let errMsg = extractErrorMsg(dataObj, "error-summary");
-              logger.error({msg: 'keyclaok Signin route : validation error ' + resp.statusCode + ' error: ' + errMsg.trim()});
-              res.status(resp.statusCode).send({error_msg: errMsg.trim()});
-            } else {
-              let errMsg = extractErrorMsg(dataObj, "kc-error-message");
-              logger.error({msg: 'keyclaok Signin route : error on Keyclaok login ' + resp.statusCode + ' error: ' + errMsg.trim()});
-              res.status(resp.statusCode).send({error_msg: errMsg.trim()});
-            }
-          }
-        }))
-      } else {
-        let errMsg = extractErrorMsg(data, "kc-error-message");
-        logger.error({ msg: 'keyclaok Signin route : initialize keyclaok error: '+ response.statusCode, data, error: errMsg.trim() });
-        res.status(response.statusCode).send({error_msg: errMsg.trim()});
-      }
-    }))
   });
+}
 
-  function extractErrorMsg(data, id) {
-    let errObj = parser.parseFromString(data, 'text/html');
-    let err_res = errObj.getElementById(id).textContent;
-    err_res.replace(/\\\n/g, '');
-    return err_res;
+function handleRequest(option, type, res) {
+  request(option, ((err, response, data) => {
+    if(!err && (_.get(response, 'statusCode') == 200)) {
+      handleSuccessResponse(option, response, data, type, res);
+    } else {
+      handleResponse(err, response, data, type, res);
+    }
+  }))
+}
+
+function handleSuccessResponse(option, response, data, type, res) {
+  if(type == "reqLogin") {
+    logger.info({msg: 'keyclaok Signin route: initialized ' + option.method + option.url})
+    let htmlObject = parser.parseFromString(data, 'text/html');
+    let ele = htmlObject.getElementById("kc-form-login");
+    let loginUrl = ele.getAttribute('action');
+    let header = _.get(response, 'headers');
+    let request = _.get(response, 'request');
+    let loginOption = getRequestOptions(loginUrl, getHeaders(header['set-cookie'].toString(), request.href+'?'+request.body), formData)
+    handleRequest(loginOption, "reqData", res);
+  } else if(type == "reqData") {
+    let errMsg = extractErrorMsg(data, "error-summary");
+    logger.error({msg: 'keyclaok Signin route : validation error ' + _.get(response, 'statusCode') + ' error: ' + errMsg.trim()});
+    res.status(_.get(response, 'statusCode')).send({error_msg: errMsg.trim()});
+  } else {
+    logger.info({msg: 'keyclaok Signin route : generated access token and refresh token - login sucessful'});
+    res.status(_.get(response, 'statusCode')).send(data)
   }
+}
+
+function handleResponse(err, response, data, type, res) {
+  if(type == "reqLogin") {
+    let errMsg = extractErrorMsg(data, "kc-error-message");
+    logger.error({ msg: 'keyclaok Signin route : initialize keyclaok error: '+ response.statusCode, error: errMsg.trim() });
+    res.status(_.get(response, 'statusCode')).send({error_msg: errMsg.trim()});
+  } else if(type == "reqData") {
+    if (err) {
+      logger.error({msg: 'keyclaok Signin route : login failed ' + response.statusCode + 'error:'+ JSON.stringify(err)})
+      res.error(err + data);
+    } else if ((_.get(response, 'statusCode') == 302) && data == "") {
+      logger.info({msg: 'keyclaok Signin route : successfully login with a keycloak user'});
+      let code = _.get(response, 'headers').location.split('=');
+      let reqTokenOption = getRequestOptions(`${envHelper.PORTAL_AUTH_SERVER_URL}/realms/${envHelper.PORTAL_REALM}/protocol/openid-connect/token`, 
+      getHeaders(), qs.stringify({redirect_uri: redirect_uri, code: code[code.length - 1], grant_type: 'authorization_code',client_id: client_id}))
+      handleRequest(reqTokenOption, 'reqToken', res);
+    } else {
+      let errMsg = extractErrorMsg(data, "kc-error-message");
+      logger.error({msg: 'keyclaok Signin route : error on Keyclaok login ' + _.get(response, 'statusCode') + ' error: ' + errMsg.trim()});
+      res.status(_.get(response, 'statusCode')).send({error_msg: errMsg.trim()});
+    }
+  } else {
+    logger.error({msg: 'keyclaok Signin route : error while getting access token ' + _.get(response, 'statusCode') + 'error'})
+    if(data) {
+      try {
+        let resData = JSON.parse(data);
+        res.send(resData);
+      }
+      catch(err) {
+        res.send(data);
+      }
+    }
+  }
+}
+
+function extractErrorMsg(data, id) {
+  let errObj = parser.parseFromString(data, 'text/html');
+  let err_res = errObj.getElementById(id).textContent;
+  err_res.replace(/\\\n/g, '');
+  return err_res;
 }
 
 function getHeaders(cookie, referer) {
