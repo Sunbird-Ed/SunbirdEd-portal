e42b93f653864d143c95033f411641aaaf07065a
Rajesh Kumaravel <rajesh.kumaravel@tarento.com>
2022-11-16 17:11:06 +0530

    Issue #ED-393 feat: Externalie CSP package implemented in portal backend

--- src/app/helpers/cloudStorage/index.js
+++ src/app/helpers/cloudStorage/index.js
@@ -2,17 +2,16 @@
 /**
  * @file        - Entry file referencing Storage Service
  * @description - Entry file referencing Storage Service
- * @exports     - `AzureStorageService` and `AWSStorageService`
+ * @exports     - `AzureStorageService`, `AWSStorageService` and `GoogleStorageService`
+ * @author      - RAJESH KUMARAVEL
  * @since       - 5.0.1
+ * @updated     - 5.1.0
  * @version     - 1.0.0
  */
 
- const AzureStorageService = require('./AzureStorageService');
- const AWSStorageService   = require('./AWSStorageService');
- const GCPStorageService   = require('./GCPStorageService');
- const envHelper           = require('../../helpers/environmentVariablesHelper');
- 
- const cloudProvider       = envHelper.sunbird_cloud_storage_provider;
+const cloudService  = require('client-cloud-services');
+const envHelper     = require('../../helpers/environmentVariablesHelper');
+const cloudProvider = envHelper.sunbird_cloud_storage_provider;
 
 /**
  * Based on Environment Cloud Provider value
@@ -21,13 +20,41 @@
 
 switch (cloudProvider) {
   case 'azure':
-    exports.CLOUD_CLIENT = new AzureStorageService();
+    let azureConfig = {
+      identity: envHelper.sunbird_azure_account_name,
+      credential: envHelper.sunbird_azure_account_key,
+      reportsContainer: envHelper.sunbird_azure_report_container_name,
+      labelsContainer: envHelper.sunbird_azure_resourceBundle_container_name
+    };
+    let azureClient = cloudService.init('azure');
+    const azureStorage = new azureClient(azureConfig);
+    exports.CLOUD_CLIENT = azureStorage;
     break;
   case 'aws':
-    exports.CLOUD_CLIENT = new AWSStorageService();
+    let awsConfig = {
+      identity: envHelper.sunbird_aws_access_key,
+      credential: envHelper.sunbird_aws_secret_key,
+      region: envHelper.sunbird_aws_region,
+      containerName: envHelper.sunbird_aws_bucket_name,
+      reportsContainer: envHelper.sunbird_aws_reports,
+      labelsContainer: envHelper.sunbird_aws_labels
+    };
+    let awsClient = cloudService.init('aws');
+    const awsStorage = new awsClient(awsConfig);
+    exports.CLOUD_CLIENT = awsStorage;
     break;
   case 'gcloud':
-    exports.CLOUD_CLIENT = new GCPStorageService();
+    let gcpConfig = {
+      identity: envHelper.sunbird_gcloud_client_email,
+      credential: envHelper.sunbird_gcloud_private_key,
+      projectId: envHelper.sunbird_gcloud_projectId,
+      containerName: envHelper.sunbird_gcloud_bucket_name,
+      reportsContainer: envHelper.sunbird_gcloud_reports,
+      labelsContainer: envHelper.sunbird_gcloud_labels
+    };
+    let gcpClient = cloudService.init('gcloud');
+    const gcpStorage = new gcpClient(gcpConfig);
+    exports.CLOUD_CLIENT = gcpStorage;
     break;
   default:
     break;
