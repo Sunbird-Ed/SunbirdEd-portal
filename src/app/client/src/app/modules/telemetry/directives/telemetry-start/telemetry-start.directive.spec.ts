5ee0909325d4676b7198f169972a4ef91c9422bf
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2022-11-17 13:06:27 +0530

    Issue #SB-0000 fix: Added testcases for uci component

--- src/app/client/src/app/modules/telemetry/directives/telemetry-start/telemetry-start.directive.spec.ts
+++ src/app/client/src/app/modules/telemetry/directives/telemetry-start/telemetry-start.directive.spec.ts
@@ -1,10 +1,10 @@
-import {of as observableOf } from 'rxjs';
+import { of as observableOf } from 'rxjs';
 import { TelemetryStartDirective } from './telemetry-start.directive';
 import { TelemetryService } from '../../services';
-import {eventData} from './telemetry-start.directive.spec.data';
+import { eventData } from './telemetry-start.directive.spec.data';
 
 describe('TelemetryStartDirective', () => {
-    let directive:TelemetryStartDirective;
+    let directive: TelemetryStartDirective;
     const telemetryService: Partial<TelemetryService> = {
         start: jest.fn()
     };
@@ -12,12 +12,12 @@ describe('TelemetryStartDirective', () => {
     beforeEach(() => {
         directive = new TelemetryStartDirective(telemetryService as TelemetryService);
     });
-  it('should take input and  generate the telemetry start event', () => {
- jest.spyOn(telemetryService, 'start').mockImplementation(() => observableOf(eventData.inputData));
-    directive.appTelemetryStart = eventData.inputData;
-    directive.ngOnChanges();
-    expect(directive.appTelemetryStart).toBeDefined();
-    expect(directive.appTelemetryStart).toBe(eventData.inputData);
-    expect(telemetryService.start).toHaveBeenCalled();
-  });
+    it('should take input and  generate the telemetry start event', () => {
+        jest.spyOn(telemetryService, 'start').mockImplementation(() => observableOf(eventData.inputData));
+        directive.appTelemetryStart = eventData.inputData;
+        directive.ngOnChanges();
+        expect(directive.appTelemetryStart).toBeDefined();
+        expect(directive.appTelemetryStart).toBe(eventData.inputData);
+        expect(telemetryService.start).toHaveBeenCalled();
+    });
 });
