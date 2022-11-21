75af4c3341009828b8eb7e54abca60b3d57de0e2
Rajesh Kumaravel <rajesh.kumaravel@tarento.com>
2022-11-21 17:19:42 +0530

    Issue #SB-0000 fix: Updated sb-notification package to angular 9

--- src/app/client/src/app/modules/notification/notification.module.ts
+++ src/app/client/src/app/modules/notification/notification.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SuiModule } from 'ng2-semantic-ui-v9';
 import { CommonConsumptionModule } from '@project-sunbird/common-consumption-v9';
 import { InAppNotificationComponent } from './components/in-app-notification/in-app-notification.component';
-import { SbNotificationModule } from 'sb-notification';
+import { SbNotificationModule } from '@project-sunbird/sb-notification';
 import { NotificationServiceImpl } from './services/notification/notification-service-impl';
 import { SharedModule } from '@sunbird/shared';
 @NgModule({
