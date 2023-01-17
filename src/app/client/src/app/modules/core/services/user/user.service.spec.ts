dcf3b0f4a8744bc0ab838959c3386083002f39a4
Rajesh Kumaravel <rajesh.kumaravel@tarento.com>
2023-01-17 17:01:36 +0530

    Issue #ED-976 fix: Test cases

---
+++ src/app/client/src/app/modules/core/services/user/user.service.spec.ts
@@ -0,0 +1,242 @@
+import { of, throwError } from 'rxjs';
+import { ConfigService } from '../../../shared/services/config/config.service';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from './user.service';
+import { ContentService, DataService, LearnerService, PublicDataService } from '../../../core';
+import { CacheService } from 'ng2-cache-service';
+import { Inject } from '@angular/core';
+import { mockUserData } from './user.mock.spec.data';
+
+describe('UserService', () => {
+  let userService: UserService;
+  const mockConfigService: Partial<ConfigService> = {
+    urlConFig: {
+      params: {
+        userReadParam: {
+          fields: 'organisations,roles,locations,declarations,externalIds'
+        }
+      },
+      URLS: {
+        OTP: {
+          GENERATE: 'otp/v1/generate',
+          VERIFY: 'otp/v1/verify',
+        },
+        USER: {
+          GET_PROFILE: 'user/v5/read/',
+          USER_MIGRATE: 'user/v1/migrate',
+          END_SESSION: 'endSession',
+          GET_USER_FEED: 'user/v1/feed/'
+        },
+        OFFLINE: {
+          READ_USER: 'desktop/user/v1/read',
+          CREATE_USER: 'desktop/user/v1/create',
+          UPDATE_USER: 'desktop/user/v1/update',
+          GET_USER_BY_KEY: 'desktop/user/v1/read/by/key',
+          USER_EXISTS_GET_USER_BY_KEY: 'desktop/user/v1/exists/by/key'
+        }
+      }
+    }
+  };
+  const mockLearnerService: Partial<LearnerService> = {
+    post: jest.fn().mockImplementation(() => { }),
+    get: jest.fn().mockImplementation(() => { }),
+    getWithHeaders: jest.fn().mockImplementation(() => {
+      return of(mockUserData.success)
+    })
+  };
+  const mockCacheService: Partial<CacheService> = {
+    set: jest.fn()
+  };
+  const mockHttpClient: Partial<HttpClient> = {
+    get: jest.fn()
+  };
+  const mockContentService: Partial<ContentService> = {};
+  const mockPublicDataService: Partial<PublicDataService> = {
+    post: jest.fn().mockImplementation(() => { }),
+    get: jest.fn().mockImplementation(() => { })
+  };
+  const mockDataService: Partial<DataService> = {};
+  const mockbaseHref = 'sunbird.org';
+  beforeAll(() => {
+    userService = new UserService(
+      mockConfigService as ConfigService,
+      mockLearnerService as LearnerService,
+      mockCacheService as CacheService,
+      mockHttpClient as HttpClient,
+      mockContentService as ContentService,
+      mockPublicDataService as PublicDataService,
+      mockbaseHref as typeof window.location.href as any,
+      mockDataService as DataService
+    );
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it('should create a instance of UserService', () => {
+    expect(userService).toBeTruthy();
+  });
+
+  it('should fetch user profile details', () => {
+    jest.spyOn(mockLearnerService, 'getWithHeaders').mockImplementation(() => {
+      return of(mockUserData.success)
+    });
+    userService.initialize(true);
+    expect(userService._userProfile).toBeDefined();
+  });
+
+  it('should emit user profile data on success', () => {
+    jest.spyOn(mockLearnerService, 'getWithHeaders').mockImplementation(() => {
+      return of(mockUserData.success)
+    });
+    userService.initialize(true);
+    userService.userData$.subscribe(userData => {
+      expect(userData.userProfile).toBeDefined();
+    });
+  });
+
+  it('should emit error on api failure', () => {
+    jest.spyOn(mockLearnerService, 'getWithHeaders').mockImplementation(() => {
+      return of(mockUserData.error)
+    });
+    userService.initialize(true);
+    userService.userData$.subscribe(userData => {
+      expect(userData.err).toBeDefined();
+    });
+  });
+
+  it('should return userProfile when userProfile get method is called', () => {
+    jest.spyOn(mockLearnerService, 'getWithHeaders').mockImplementation(() => {
+      return of(mockUserData.success)
+    });
+    userService.initialize(true);
+    const userProfile = userService.userProfile;
+    expect(userProfile).toBeDefined();
+  });
+
+  it('should set rootOrgAdmin to true', () => {
+    jest.spyOn(mockLearnerService, 'getWithHeaders').mockImplementation(() => {
+      return of(mockUserData.rootOrgSuccess)
+    });
+    userService.initialize(true);
+    expect(userService._userProfile.rootOrgAdmin).toBeTruthy();
+  });
+
+  it('should set rootOrgAdmin to false', () => {
+    jest.spyOn(mockLearnerService, 'getWithHeaders').mockImplementation(() => {
+      return of(mockUserData.success)
+    });
+    userService.initialize(true);
+    expect(userService._userProfile.rootOrgAdmin).toBeFalsy();
+  });
+
+  it('should call registerUser method', () => {
+    jest.spyOn(mockLearnerService, 'post').mockImplementation(() => {
+      return of(mockUserData.registerSuccess)
+    });
+    const reqData = { 'request': { 'firstName': 'test', 'managedBy': '5488df8f-2090-4735-a767-ad0588bf7659', 'locationIds': [] } };
+    jest.spyOn(userService.createManagedUser, 'emit').mockReturnValue('0008ccab-2103-46c9-adba-6cdf84d37f06' as any);
+    userService.registerUser(reqData).subscribe(apiResponse => {
+      expect(apiResponse.responseCode).toBe('OK');
+      expect(apiResponse.result.response).toBe('SUCCESS');
+      expect(userService.createManagedUser.emit).toHaveBeenCalledWith('0008ccab-2103-46c9-adba-6cdf84d37f06');
+    });
+  });
+
+  it('should migrate custodian user', () => {
+    const params = {
+      'request': {
+        'userId': '12345',
+        'action': 'accept',
+        'userExtId': 'bt240',
+        'channel': 'TN',
+        'feedId': '32145669877'
+      }
+    };
+    jest.spyOn(mockLearnerService, 'post').mockImplementation(() => {
+      return of(mockUserData.migrateSuccessResponse)
+    });
+    userService.userMigrate(params);
+    const options = { url: 'user/v1/migrate', data: params };
+    expect(mockLearnerService.post).toHaveBeenCalledWith(options);
+  });
+
+  it('should call getAnonymousUserPreference', () => {
+    jest.spyOn(mockPublicDataService, 'post').mockImplementation(() => {
+      return of({ result: { board: 'CBSE' } }) as any
+    }) as any;
+    jest.spyOn(mockPublicDataService, 'get').mockImplementation(() => {
+      return of() as any
+    }) as any;
+    userService.getAnonymousUserPreference();
+    expect(mockPublicDataService.get).toHaveBeenCalled();
+
+  });
+
+  it('should call updateAnonymousUserDetails', () => {
+    jest.spyOn(mockPublicDataService, 'post').mockImplementation(() => {
+      return of({ result: { board: 'CBSE' } }) as any
+    }) as any;
+    userService.updateAnonymousUserDetails({});
+    expect(mockPublicDataService.post).toHaveBeenCalled();
+  });
+
+  it('should call createAnonymousUser', () => {
+    jest.spyOn(mockPublicDataService, 'post').mockImplementation(() => {
+      return of({ result: { board: 'CBSE' } }) as any
+    }) as any;
+    userService.createAnonymousUser({});
+    expect(mockPublicDataService.post).toHaveBeenCalled();
+  });
+
+  it('should call createGuestUser', () => {
+    userService.isDesktopApp = true;
+    jest.spyOn(userService, 'createAnonymousUser').mockReturnValue(of({} as any) as any);
+    userService.createGuestUser({});
+    expect(userService.createAnonymousUser).toHaveBeenCalled();
+  });
+
+  it('should call createGuestUser for portal', () => {
+    userService.isDesktopApp = false;
+    jest.spyOn(userService, 'getGuestUser').mockReturnValue(of({} as any) as any);
+    const resp = userService.createGuestUser({});
+    expect(userService.getGuestUser).toHaveBeenCalled();
+  });
+
+  it('should call updateGuestUser for desktop', () => {
+    userService.isDesktopApp = true;
+    jest.spyOn(userService, 'updateAnonymousUserDetails').mockReturnValue(of({} as any) as any);
+    userService.updateGuestUser({ _id: 'abcd', name: 'Guest' });
+    expect(userService.updateAnonymousUserDetails).toHaveBeenCalled();
+  });
+
+  it('should call updateGuestUser for portal', () => {
+    userService.isDesktopApp = false;
+    jest.spyOn(userService, 'getGuestUser').mockReturnValue(of({} as any) as any);
+    userService.updateGuestUser({ _id: 'abcd', name: 'Guest' });
+    expect(userService.getGuestUser).toHaveBeenCalled();
+  });
+
+  it('should call endsession', () => {
+    userService.endSession();
+    expect(mockHttpClient.get).toBeCalled();
+  });
+
+  it('should call getUserByKey', () => {
+    userService.getUserByKey('sunbird');
+    expect(mockLearnerService.get).toBeCalled();
+  });
+
+  it('should call getIsUserExistsUserByKey', () => {
+    userService.getIsUserExistsUserByKey('sunbird');
+    expect(mockLearnerService.get).toBeCalled();
+  });
+
+  it('should call getFeedData', () => {
+    userService.getFeedData();
+    expect(mockLearnerService.get).toBeCalled();
+  });
+
+});
---
+++ src/app/client/src/app/modules/shared-feature/components/content-player/content-player.component.spec.ts
@@ -0,0 +1,196 @@
+import { ActivatedRoute } from '@angular/router';
+import { EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService, PlayerService, CopyContentService, PermissionService } from '@sunbird/core';
+import * as _ from 'lodash-es';
+import {
+  ConfigService, ResourceService, ToasterService, WindowScrollService, NavigationHelperService,
+  ContentUtilsServiceService, LayoutService
+} from '@sunbird/shared';
+import { TelemetryService } from '@sunbird/telemetry';
+import { PopupControlService } from '../../../../service/popup-control.service';
+import { of } from 'rxjs';
+import { PublicPlayerService } from '@sunbird/public';
+import { ContentPlayerComponent } from './content-player.component';
+
+describe('ContentPlayerComponent', () => {
+  let component: ContentPlayerComponent;
+  const mockActivatedRoute: Partial<ActivatedRoute> = {
+    queryParams: of({}),
+    params: of({})
+  };
+  const mockNavigationHelperService: Partial<NavigationHelperService> = {
+    contentFullScreenEvent: new EventEmitter<any>(),
+    getPageLoadTime: jest.fn().mockReturnValue('1ms')
+  };
+  const mockUserService: Partial<UserService> = {
+    loggedIn: true,
+    slug: jest.fn().mockReturnValue("tn") as any,
+    userData$: of({
+      userProfile: {
+        userId: 'sample-uid',
+        rootOrgId: 'sample-root-id',
+        rootOrg: {},
+        hashTagIds: ['id']
+      } as any
+    }) as any,
+    setIsCustodianUser: jest.fn(),
+    userid: 'sample-uid',
+    appId: 'sample-id',
+    getServerTimeDiff: '',
+  };
+  const mockResourceService: Partial<ResourceService> = {
+    messages: {
+      imsg: { m0027: 'Something went wrong' },
+      stmsg: { m0009: 'error' }
+    },
+    frmelmnts: {
+      btn: {
+        tryagain: 'tryagain',
+        close: 'close'
+      },
+      lbl: {
+        description: 'description'
+      }
+    }
+  };
+  const mockRouter: Partial<Router> = {
+    events: of({ id: 1, url: 'sample-url' }) as any,
+    navigate: jest.fn()
+  };
+  const resourceServiceMockData = {
+    messages: {
+      imsg: { m0027: 'Something went wrong' },
+      stmsg: { m0009: 'error' }
+    },
+    frmelmnts: {
+      btn: {
+        tryagain: 'tryagain',
+        close: 'close'
+      },
+      lbl: {
+        description: 'description'
+      }
+    }
+  };
+  const serverRes = {
+    id: 'api.content.read',
+    ver: '1.0',
+    ts: '2018-05-03T10:51:12.648Z',
+    params: 'params',
+    responseCode: 'OK',
+    result: {
+      content: {
+        mimeType: 'application/vnd.ekstep.ecml-archive',
+        body: 'body',
+        identifier: 'domain_66675',
+        versionKey: '1497028761823',
+        status: 'Live',
+        me_averageRating: '4',
+        description: 'ffgg',
+        name: 'ffgh',
+        concepts: ['AI', 'ML']
+      }
+    }
+  };
+  const mockToasterService: Partial<ToasterService> = {};
+  const mockWindowScrollService: Partial<WindowScrollService> = {
+    smoothScroll: jest.fn()
+  };
+  const mockPlayerService: Partial<PlayerService> = {
+    getContent: jest.fn()
+  };
+  const mockPublicPlayerService: Partial<PublicPlayerService> = {};
+  const mockCopyContentService: Partial<CopyContentService> = {
+    copyContent: jest.fn()
+  };
+  const mockPermissionService: Partial<PermissionService> = {};
+  const mockContentUtilsServiceService: Partial<ContentUtilsServiceService> = {
+    getPublicShareUrl: jest.fn()
+  };
+  const mockPopupControlService: Partial<PopupControlService> = {};
+  const mockConfigService: Partial<ConfigService> = {
+    appConfig: {
+      ContentPlayer: {
+        contentApiQueryParams: {
+          orgdetails: 'orgName,email'
+        }
+      }
+    }
+  };
+  const mockLayoutService: Partial<LayoutService> = {};
+  const mockTelemetryService: Partial<TelemetryService> = {};
+
+  beforeAll(() => {
+    component = new ContentPlayerComponent(
+      mockActivatedRoute as ActivatedRoute,
+      mockNavigationHelperService as NavigationHelperService,
+      mockUserService as UserService,
+      mockResourceService as ResourceService,
+      mockRouter as Router,
+      mockToasterService as ToasterService,
+      mockWindowScrollService as WindowScrollService,
+      mockPlayerService as PlayerService,
+      mockPublicPlayerService as PublicPlayerService,
+      mockCopyContentService as CopyContentService,
+      mockPermissionService as PermissionService,
+      mockContentUtilsServiceService as ContentUtilsServiceService,
+      mockPopupControlService as PopupControlService,
+      mockConfigService as ConfigService,
+      mockLayoutService as LayoutService,
+      mockTelemetryService as TelemetryService
+    )
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it('should create a instance of contentPlayerComponent', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should config content player if content status is "Live"', (() => {
+    mockResourceService.messages = resourceServiceMockData.messages;
+    mockResourceService.frmelmnts = resourceServiceMockData.frmelmnts;
+    jest.spyOn(mockPlayerService, 'getContent').mockReturnValue(of(serverRes) as any);
+    mockUserService._userProfile = { 'organisations': ['01229679766115942443'] } as any;
+    mockLayoutService.initlayoutConfig = jest.fn(() => { });
+    mockLayoutService.switchableLayout = jest.fn(() => of([{ data: '' }]));
+    component.ngOnInit();
+    expect(component.showLoader).toBeFalsy();
+  }));
+
+  it('it should call copyContent', () => {
+    jest.spyOn(mockCopyContentService, 'copyContent').mockReturnValue(of(serverRes as any) as any);
+    component.copyContent({} as any);
+    expect(mockCopyContentService.copyContent).toBeCalled();
+  });
+
+  it('it should call onShareLink', () => {
+    jest.spyOn(mockContentUtilsServiceService, 'getPublicShareUrl').mockReturnValue((of({} as any) as any)) as any;
+    component.contentData = {} as any;
+    component.contentData['mimeType'] = 'mp4';
+    component.contentId = 'do_1131446242806251521827';
+    component.onShareLink();
+    expect(component.shareLink).toBeDefined();
+  });
+
+  it('it should call ngAfterViewInit', () => {
+    jest.spyOn(mockNavigationHelperService, 'getPageLoadTime').mockReturnValue(1) as any;
+    component.ngAfterViewInit();
+    expect(mockNavigationHelperService.getPageLoadTime).toBeCalled();
+    expect(component.pageLoadDuration).toEqual(1);
+  });
+
+  it('should invoked ngOnDestroy', () => {
+    component.unsubscribe$ = {
+      next: jest.fn(),
+      complete: jest.fn()
+    } as any;
+    component.ngOnDestroy();
+    expect(component.unsubscribe$.next).toHaveBeenCalled();
+    expect(component.unsubscribe$.complete).toHaveBeenCalled();
+  });
+});
