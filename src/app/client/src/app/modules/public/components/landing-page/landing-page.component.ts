971dd2238815882586668018c1e5d3389061b3be
sohail amjad <sohail.amjad@tarento.com>
2023-01-27 16:49:45 +0530

    show/hide calender and Article from landing page based on the config

--- src/app/client/src/app/modules/public/components/landing-page/landing-page.component.ts
+++ src/app/client/src/app/modules/public/components/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LayoutService } from '@sunbird/shared';
+import { LayoutService, ServerResponse } from '@sunbird/shared';
 
 @Component({
   selector: 'app-landing-page',
@@ -9,11 +9,46 @@ import { LayoutService } from '@sunbird/shared';
 export class LandingPageComponent implements OnInit {
 
   layoutConfiguration;
+  UserArticle:{}
+  userCalender:{}
+  showUserArticle:boolean = true;
+  showUserCalender:boolean = true;
+  showAnnoucements: boolean = true;
 
   constructor(public layoutService: LayoutService) { }
 
   ngOnInit() {
     this.layoutConfiguration = this.layoutService.initlayoutConfig();
+    this.getUserContentconfig()
+  }
+
+  getUserContentconfig(){
+    const formInputParams = {
+      formType: 'user',
+      subType: 'content',
+      formAction: 'display',
+      component:'portal',
+    };
+
+    this.layoutService.getFormData(formInputParams).subscribe(
+      (data:ServerResponse)=>{
+        if(data.result.form.data){
+          this.UserArticle= data.result.form.data.fields[0]
+          this.showUserArticle = this.UserArticle['showFeatureArticle']
+          if(!this.showUserArticle){
+              this.showAnnoucements = false
+          }
+          this.userCalender = data.result.form.data.fields[1]
+          this.showUserCalender = this.userCalender['showCalender']
+        }   
+      },
+      (err:ServerResponse)=>{
+        this.showUserArticle = true
+        this.showUserCalender = true
+        this.showAnnoucements = true
+
+      }
+    )
   }
 
 }
