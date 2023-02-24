c66fa0a2863a3caf963ff80b6c69bf88db914ab5
Rajeev Satish <rajeevsatish@TI-MAC-104.local>
2023-02-24 10:40:51 +0530

    Issue #ED-1347 fix:updating editor and player version

--- src/app/client/src/app/modules/workspace/workspace.module.ts
+++ src/app/client/src/app/modules/workspace/workspace.module.ts
@@ -26,7 +26,7 @@ import { ReviewCommentsComponent } from './components/review-comments/review-com
 import { OrderModule } from 'ngx-order-pipe';
 import { PlayerHelperModule } from '@sunbird/player-helper';
 import { ContentSearchModule } from '@sunbird/content-search';
-import { CollectionEditorLibraryModule } from '@project-sunbird/sunbird-collection-editor-v9';
+import { CollectionEditorLibraryModule } from '@project-sunbird/sunbird-collection-editor';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
