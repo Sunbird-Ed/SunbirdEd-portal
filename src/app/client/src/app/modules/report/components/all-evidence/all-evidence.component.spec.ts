f779753ac2e3ea66ec39df9c0ae5c9f6ef10c69a
5Amogh <amoghavarsh@navadhiti.com>
2023-03-16 23:05:19 +0530

    Unit test added for the new functionality

---
+++ src/app/client/src/app/modules/report/components/all-evidence/all-evidence.component.spec.ts
@@ -0,0 +1,37 @@
+import { ResourceService, ConfigService } from '@sunbird/shared';
+import { LocationStrategy } from '@angular/common';
+import { AllEvidenceComponent } from './all-evidence.component';
+import { DhitiService } from '@sunbird/core';
+import { MatTab } from '@angular/material/tabs';
+
+describe('AllEvidenceComponent', () => {
+  let component: AllEvidenceComponent;
+  const resourceService: Partial<ResourceService> = {};
+  const configService:Partial<ConfigService> = {}
+  const dhitiService:Partial<DhitiService> = {}
+  const locationStratergy:Partial<LocationStrategy> = {
+      onPopState:jest.fn as any
+  }
+
+  beforeAll(() => {
+    component = new AllEvidenceComponent(
+      resourceService as ResourceService,
+      configService as ConfigService,
+      dhitiService as DhitiService,
+      locationStratergy as LocationStrategy
+    )
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call tabChanged', () => {
+    component.tabChanged({tab:{} as MatTab,index:1});
+    expect(component.activeIndex).toBe(1)
+  });
+})
