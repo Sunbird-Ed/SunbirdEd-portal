ad9ad23d830fbacdf694571ac20feab0172095d4
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2023-05-03 12:12:43 +0530

    Issue #ED-1817: jest config changes

--- src/app/client/jest.config.js
+++ src/app/client/jest.config.js
@@ -20,13 +20,12 @@ globalThis.ngJest = {
 /** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
   preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
   moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>/src' }),
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   transform: { '^.+.(ts|mjs|js|html)$': 'jest-preset-angular' },
-
-     transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@project-sunbird)'],
-     moduleFileExtensions: [
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@project-sunbird)'],
+  moduleFileExtensions: [
           'js',
           'jsx',
           'json',
@@ -36,80 +35,18 @@ module.exports = {
           'mjs',
           'd.ts'
         ],
-          moduleDirectories: ['node_modules', 'src']
-,//   testPathIgnorePatterns: [
-//     '/node_modules/',
-//     '.data.spec.ts',
-//     '.spec.data.ts'
-//   ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '.data.spec.ts',
+    '.spec.data.ts'
+  ],
+  collectCoverage: true,
+  coverageReporters: ['text', 'html'],
+  coverageDirectory: '<rootDir>/coverage/',
+  testMatch: [
+      '**/?(*.)(spec).ts'
+    ],
 };
-
-
-
-// /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-// module.exports = {
-  
-//   preset: 'ts-jest',
-//   verbose: true,
-//   testEnvironment: 'jsdom',
-//   moduleNameMapper: pathsToModuleNameMapper(paths, { prefix: '<rootDir>/src' }),
-//   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-//   // globals: {
-//   //   'ts-jest': {
-//   //     tsconfig: '<rootDir>/tsconfig.spec.json',
-//   //     stringifyContentPathRegex: '\\.(html|svg)$',
-//   //   },
-//   // },
-//   globalSetup: 'jest-preset-angular/global-setup',
-
-//   transform: { '^.+.(ts|mjs|js|html)$': 'jest-preset-angular' },
-//   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|@project-sunbird|@sunbird)'],
-//   snapshotSerializers: [
-//     'jest-preset-angular/build/serializers/no-ng-attributes',
-//     'jest-preset-angular/build/serializers/ng-snapshot',
-//     'jest-preset-angular/build/serializers/html-comment',
-//   ],
-//   collectCoverage: true,
-//   coverageReporters: ['text', 'html'],
-//   coverageDirectory: '<rootDir>/coverage/',
-//   testMatch: [
-//     '**/?(*.)(spec).ts'
-//   ],
-//   moduleFileExtensions: [
-//     'js',
-//     'jsx',
-//     'json',
-//     'ts',
-//     'tsx',
-//     'html',
-//     'mjs',
-//     'd.ts'
-//   ],
-
-//   testPathIgnorePatterns: [
-//     '/node_modules/',
-//     '.data.spec.ts',
-//     '.spec.data.ts'
-//   ],
-//   moduleDirectories: ['node_modules', 'src']
-//   // transform: {
-//   //   'lodash-es': '<rootDir>/node_modules/lodash-es'
-  
-// };
-
-// /**
-//  * @description
-//  * Function to log all module mapped under src folder
-//  * - Use mode - DEBUG ONLY
-//  */
-// function setModuleNameMapper() {
-//   console.log('_______________MODULE_MAPPER________________')
-//   let _moduleMaps = pathsToModuleNameMapper(paths, { prefix: '<rootDir>/src' });
-//   console.log(_moduleMaps); // TODO: log!
-//   console.log('_______________MODULE_MAPPER________________')
-// }
-// // }
-// {
-//   forceExit: true
-// }
-// setModuleNameMapper()
+{
+  forceExit: true
+}
