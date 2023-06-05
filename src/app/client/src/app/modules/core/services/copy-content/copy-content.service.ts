ccc3eb2740809d090489324881cb796bf55a98ae
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2023-06-05 16:15:45 +0530

    ED-2117 fix: refactor copy question set

--- src/app/client/src/app/modules/core/services/copy-content/copy-content.service.ts
+++ src/app/client/src/app/modules/core/services/copy-content/copy-content.service.ts
@@ -122,8 +122,8 @@ export class CopyContentService {
    * @description  request will be formed based on the mimetype
    */
   formatData(contentData: ContentData) {
+    const defaultReqKey = "content";
     const userData = this.userService.userProfile;
-    let req;
     if (contentData.description === undefined) {
       contentData.description = '';
     }
@@ -131,51 +131,56 @@ export class CopyContentService {
     if (!_.isEmpty(userData.lastName)) {
       creator = userData.firstName + ' ' + userData.lastName;
     }
-    if (_.get(contentData,'mimeType') === 'application/vnd.sunbird.questionset') {
-      req = {
-        request: {
-          questionset: {
-            name: 'Copy of ' + contentData.name,
-            createdFor: userData.organisationIds,
-            createdBy: userData.userId,
-            mimeType: contentData.mimeType,
-            
-          }
+    let commonReq = {
+      request: {
+        content: {
+          name: 'Copy of ' + contentData.name,
+          createdFor: userData.organisationIds,
+          createdBy: userData.userId,
+          mimeType: contentData.mimeType,
         }
-      };
-      return of(req);
-     }
+      }
+    }
+    if (_.get(contentData, 'mimeType') === 'application/vnd.sunbird.questionset') {
+      return this.dynamicReqKeyHandler(commonReq, defaultReqKey, 'questionset');
+    }
     else {
-      req = {
-        request: {
-          content: {
-            name: 'Copy of ' + contentData.name,
-            description: contentData.description,
-            code: contentData.code + '.copy',
-            creator: creator,
-            createdFor: userData.organisationIds,
-            createdBy: userData.userId,
-            organisation: _.uniq(this.userService.orgNames),
-            framework: '',
-            mimeType: contentData.mimeType,
-            contentType: contentData.contentType
-          }
-        }
-      };
-    if (_.lowerCase(contentData.contentType) === 'course') {
-      req.request.content.framework = contentData.framework;
-      return of(req);
-    } else {
-      return this.frameworkService.frameworkData$.pipe(
-        switchMap((frameworkData: any) => {
-          if (!frameworkData.err) {
-            req.request.content.framework = _.get(frameworkData, 'frameworkdata.defaultFramework.code');
-          }
-          return of(req);
-        })
-      );
+      let req: any = { ...commonReq,};
+      req.request.content.description = contentData.description;
+      req.request.content.code = contentData.code + '.copy';
+      req.request.content.creator = creator;
+      req.request.content.organisation = _.uniq(this.userService.orgNames);
+      req.request.content.framework = '';
+      req.request.content.contentType = contentData.contentType;
+      if (_.lowerCase(contentData.contentType) === 'course') {
+        req.request.content.framework = contentData.framework;
+        return of(req);
+      } else {
+        return this.frameworkService.frameworkData$.pipe(
+          switchMap((frameworkData: any) => {
+            if (!frameworkData.err) {
+              req.request.content.framework = _.get(frameworkData, 'frameworkdata.defaultFramework.code');
+            }
+            return of(req);
+          })
+        );
+      }
     }
   }
+  /**
+  * This method will construct and return the request body based on user define key ex: questionset,content etc
+  * @param {reqParms} ContentData request data 
+  * @param {oldkey} string default key ex: content
+  * @param {newkey} string new key to change the request metadata key
+  */
+  dynamicReqKeyHandler(reqParms, oldKey, newKey) {
+    Object.defineProperty(
+      reqParms.request,
+      newKey,
+      Object.getOwnPropertyDescriptor(reqParms.request, oldKey)
+    );
+    delete reqParms.request[oldKey];
+    return of(reqParms)
   }
   /**
    * This method redirect to the editor page depending on mimetype
