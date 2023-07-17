7b1582e9d5b896fd84d3a1201080c38a1006e691
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2023-07-17 14:36:02 +0530

    Isssue #ED-0000 fix: code changes

--- build.sh
+++ build.sh
@@ -79,7 +79,7 @@ build_server(){
     echo "var1=$var1, var2=$var2"
     echo $PWD
 
-    sed -i '' "s/[a-zA-z]*-cloud-services/$var1/g" index.js
+    sed -i'' "s/[a-zA-z]*-cloud-services/$var1/g" index.js
 
     cd ../..
     nvm use $NODE_VERSION
