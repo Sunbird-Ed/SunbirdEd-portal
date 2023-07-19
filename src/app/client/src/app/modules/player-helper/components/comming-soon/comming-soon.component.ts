61fbd7302a39b01087b62899c61a29ad482d5adb
Vinod Kumar <vinodkumar@zool.in>
2023-07-19 10:05:57 +0530

    Issue #ED-2596 removing legacy code css/dependency from portal

--- src/app/client/src/app/modules/player-helper/components/comming-soon/comming-soon.component.ts
+++ src/app/client/src/app/modules/player-helper/components/comming-soon/comming-soon.component.ts
@@ -6,7 +6,8 @@ import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-comming-soon',
-  templateUrl: './comming-soon.component.html'
+  templateUrl: './comming-soon.component.html',
+  styleUrls: ['./comming-soon.component.scss']
 })
 
 export class CommingSoonComponent implements OnInit, OnDestroy {
