bd11bdd5c011292622426ff0af5c23e2a8de6c9b
shoaib-mohmad <shoaibmohammad@Netopss-MacBook-Pro.local>
2023-08-17 17:38:51 +0530

    Integrated API for public landing page courses.

--- src/app/client/src/app/modules/public/components/landing-page/landing-page.component.ts
+++ src/app/client/src/app/modules/public/components/landing-page/landing-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { LayoutService } from '@sunbird/shared';
 import * as publicService from '../../services';
+import { SearchService } from './../../../core/services/search/search.service';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,33 +9,115 @@ import * as publicService from '../../services';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
-  configContent:any = {}
-  layoutConfiguration;
-  CAROUSEL_BREAKPOINT = 768;
+  configContent: any = {}
+  // CAROUSEL_BREAKPOINT = 768;
+  CAROUSEL_BREAKPOINT = 1400;
   carouselDisplayMode = 'multiple';
-  courses:any = {};
-  slideConfig = { slidesToShow: 3, slidesToScroll: 3 };
+  courses: any = {};
 
-  constructor(public layoutService: LayoutService, private landingPageContentService:publicService.LandingPageContentService) { }
+  layoutConfiguration;
+
+  constructor(public layoutService: LayoutService, private landingPageContentService: publicService.LandingPageContentService, public search: SearchService) { }
 
   ngOnInit() {
+    // alert()
     this.layoutConfiguration = this.layoutService.initlayoutConfig();
     this.landingPageContentService.getPageContent().subscribe(res => {
       this.configContent = res;
     })
+    // this.search.compositeSearch({}).subscribe(res => {
+    //   this.courses = res;
+    //   console.log('Courses', this.courses);
+    // })
+    let requestData = {
+      "request": {
+        "filters": {
+          "channel": "0138193046778920963",
+          "primaryCategory": [
+            "Collection",
+            "Resource",
+            "Content Playlist",
+            "Course",
+            "Course Assessment",
+            "Digital Textbook",
+            "eTextbook",
+            "Explanation Content",
+            "Learning Resource",
+            "Lesson Plan Unit",
+            "Practice Question Set",
+            "Teacher Resource",
+            "Textbook Unit",
+            "LessonPlan",
+            "FocusSpot",
+            "Learning Outcome Definition",
+            "Curiosity Questions",
+            "MarkingSchemeRubric",
+            "ExplanationResource",
+            "ExperientialResource",
+            "Practice Resource",
+            "TVLesson",
+            "Course Unit"
+          ],
+          "visibility": [
+            "Default",
+            "Parent"
+          ]
+        },
+        "limit": 100,
+        "sort_by": {
+          "lastPublishedOn": "desc"
+        },
+        "fields": [
+          "name",
+          "appIcon",
+          "mimeType",
+          "gradeLevel",
+          "identifier",
+          "medium",
+          "pkgVersion",
+          "board",
+          "subject",
+          "resourceType",
+          "primaryCategory",
+          "contentType",
+          "channel",
+          "organisation",
+          "trackable"
+        ],
+        "softConstraints": {
+          "badgeAssertions": 98,
+          "channel": 100
+        },
+        "mode": "soft",
+        "facets": [
+          "se_boards",
+          "se_gradeLevels",
+          "se_subjects",
+          "se_mediums",
+          "primaryCategory"
+        ],
+        "offset": 0
+      }
+    };
+    this.landingPageContentService.getCourses(requestData).subscribe(res => {
+      this.courses = res["result"]["content"];
+      console.log('Courses', this.courses);
+    })
   }
 
+  slideConfig = { slidesToShow: 3, slidesToScroll: 3 };
+
   slickInit(e: any) {
-    console.log('slick initialized');
+    // console.log('slick initialized');
   }
   breakpoint(e: any) {
-    console.log('breakpoint');
+    // console.log('breakpoint');
   }
   afterChange(e: any) {
-    console.log('afterChange');
+    // console.log('afterChange');
   }
   beforeChange(e: any) {
-    console.log('beforeChange');
+    // console.log('beforeChange');
   }
 
 }
