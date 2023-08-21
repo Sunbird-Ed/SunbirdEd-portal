e78a4f1d170c50fc7f71ae560dfc6119c2c907de
DeviGopinath <devigopinath111@gmail.com>
2023-08-21 17:11:46 +0530

    browse by cards added

--- src/app/client/src/app/modules/public/services/index.ts
+++ src/app/client/src/app/modules/public/services/index.ts
@@ -2,3 +2,4 @@ export * from './public-player/public-player.service';
 export * from './landingpage-guard/landingpage.guard';
 export * from './pending-changes-guard/pendingchanges.guard';
 export * from './landing-page-content/landing-page-content.service'
+export * from './learn-page-content/learn-page-content.service';
