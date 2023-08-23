500f518a1e91efcfd0fa3b6eb8138fb1b20ba6ea
DeviGopinath <devigopinath111@gmail.com>
2023-08-23 22:03:24 +0530

    Browse by competency API integration done

--- src/app/client/src/app/modules/public/services/learn-page-content/learn-page-content.service.ts
+++ src/app/client/src/app/modules/public/services/learn-page-content/learn-page-content.service.ts
@@ -1,15 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+// const envHelper = require('../../../../../../../helpers/environmentVariablesHelper.js');
+
 @Injectable({
   providedIn: 'root'
 })
 export class LearnPageContentService {
+
+  // competencyApiUrl = envHelper.CONTENT_URL;
+  competencyApiUrl = 'https://compass-dev.tarento.com/api/content/v1';
 
   constructor(private http: HttpClient) { }
 
   getPageContent(){
     console.log("hi");
     return this.http.get('/learnLandingPageContent');
+  }
+
+  getBrowseByCompetencyData(data: any){
+    return this.http.post(`${this.competencyApiUrl}/search?orgdetails=orgName,email&licenseDetails=name,description,url`, data);
   }
 }
