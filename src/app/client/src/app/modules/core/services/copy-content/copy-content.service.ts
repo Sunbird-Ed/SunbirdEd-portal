49e06d445d71497927c904e51ea052531daaff15
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2023-09-20 15:20:26 +0530

    Issue #ED-2881: reintroduced navigation

--- src/app/client/src/app/modules/core/services/copy-content/copy-content.service.ts
+++ src/app/client/src/app/modules/core/services/copy-content/copy-content.service.ts
@@ -208,10 +208,10 @@ export class CopyContentService {
       url = `/workspace/content/edit/content/${copiedIdentifier}/draft/${contentData.framework}/Draft`;
     } else if (_.get(contentData,'mimeType') === 'application/vnd.sunbird.questionset') {
       url = `/workspace/edit/QuestionSet/${copiedIdentifier}/allcontent/Draft`;
-      setTimeout(() => {
-        window.open(this.hostUrl + url, "_self");
-      }, 1000);
-      return;
+      // setTimeout(() => {
+      //   window.open(this.hostUrl + url, "_self");
+      // }, 1000);
+      // return;
     }
     else {
       url = `/workspace/content/edit/generic/${copiedIdentifier}/uploaded/${contentData.framework}/Draft`;
