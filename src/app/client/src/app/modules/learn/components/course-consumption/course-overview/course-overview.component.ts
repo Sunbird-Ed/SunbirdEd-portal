ce20a25e4fb9e2ae451f6270d75aed0b6946bfdf
santhosh-tarento <120555456+santhosh-tarento@users.noreply.github.com>
2023-09-22 14:20:28 -0400

    competencies (#68)

--- src/app/client/src/app/modules/learn/components/course-consumption/course-overview/course-overview.component.ts
+++ src/app/client/src/app/modules/learn/components/course-consumption/course-overview/course-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FrameworkService } from '@sunbird/core';
 
 @Component({
   selector: 'app-course-overview',
@@ -9,39 +10,27 @@ export class CourseOverviewComponent implements OnInit {
 
   @Input() courseDetails: any;
   @Input() configContent: any;
+  associatedTerms = [];
 
-  levelsInfo = {
-    header:{
-      content: 'Content Type',
-      type: 'Type',
-      level: 'Your level'
-    },
-    data: [
-      {
-        content: 'Consultation and Consensus Building',
-        type: 'Behavioural',
-        level: 3
-      },
-      {
-        content: 'Seeking Information',
-        type: 'Domain',
-        level: 2
-      },
-      {
-        content: 'Result Orientation',
-        type: 'Functional',
-        level: 3
-      },
-      {
-        content: 'Decision Making',
-        type: 'Functional',
-        level: 4
-      }
-    ]
-  }
-  constructor() { }
+  // levelsInfo = {
+  //   header:{
+  //     content: 'Content Type',
+  //   },
+  //   data: []
+  // }
+  categoryTermsId: any;
+  associatedCompetencies:[]
+  constructor(private framework: FrameworkService) { }
 
   ngOnInit(): void {
+    this.categoryTermsId = this.courseDetails.se_subjectIds || this.courseDetails.targetTaxonomyCategory4Ids;
+    if(this.categoryTermsId){
+      this.framework.getSelectedFrameworkCategories(this.courseDetails.targetFWIds[0]).subscribe((res:any) => {
+        if(res.result.framework.categories.length>3) {
+         this.associatedCompetencies = res.result.framework.categories[3].terms.filter((term:any) => this.categoryTermsId.includes(term.identifier))
+        }
+      }) 
+    }
   }
 
 }
