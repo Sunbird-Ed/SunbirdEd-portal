420bdeb2286885a4ec9aaa40c2dafddbf837d3f6
Rajeev Satish <rajeevsatish@TI-MAC-104.local>
2023-10-02 10:19:11 +0530

    Issue #ED-0000 for testcases

--- src/app/client/src/app/modules/shared-feature/components/tnc-popup/terms-conditions-popup.component.spec.ts
+++ src/app/client/src/app/modules/shared-feature/components/tnc-popup/terms-conditions-popup.component.spec.ts
@@ -5,43 +5,72 @@
 *
 */
 
-import { Component,OnInit,Input,EventEmitter,Output,OnDestroy } from '@angular/core';
-import { UserService,TenantService } from '@sunbird/core';
-import { Subscription,Subject } from 'rxjs';
-import { ResourceService,ToasterService } from '@sunbird/shared';
-import { IUserProfile,ILoaderMessage } from '@sunbird/shared';
+import { Component, OnInit, Input, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { UserService, TenantService } from '@sunbird/core';
+import { Subscription, Subject, throwError } from 'rxjs';
+import { ResourceService, ToasterService } from '@sunbird/shared';
+import { IUserProfile, ILoaderMessage } from '@sunbird/shared';
 import { DomSanitizer } from '@angular/platform-browser';
 import { _ } from 'lodash-es';
+import { of } from 'rxjs';
 import { PopupControlService } from '../../../../service/popup-control.service';
+import { mockUserData } from '../../../core/services/user/user.mock.spec.data';
+import { tNcMockResponse } from './terms-conditions-popup.component.spec.data';
 import { MatDialog } from '@angular/material/dialog';
 import { TermsAndConditionsPopupComponent } from './terms-conditions-popup.component';
 
 describe('TermsAndConditionsPopupComponent', () => {
     let component: TermsAndConditionsPopupComponent;
 
-    const userService :Partial<UserService> ={};
-	const resourceService :Partial<ResourceService> ={
+    const userService: Partial<UserService> = {
+        loggedIn: true,
+    slug: jest.fn().mockReturnValue('tn') as any,
+    userData$: of({userProfile: {
+      userId: 'sample-uid',
+      rootOrgId: 'sample-root-id',
+      rootOrg: {},
+      hashTagIds: ['id'],
+    } as any}) as any,
+    setIsCustodianUser: jest.fn(),
+    userid: 'sample-uid',
+    appId: 'sample-id',
+    getServerTimeDiff: '',
+    acceptTermsAndConditions:jest.fn()
+    };
+    const resourceService: Partial<ResourceService> = {
         messages: {
-            stmsg:{
+            stmsg: {
                 m0129: 'Loading the terms and conditions.'
+            },
+            emsg: {
+                m0005: 'Something went wrong, try again later'
             }
         }
     };
-	const toasterService :Partial<ToasterService> ={};
-	const tenantService :Partial<TenantService> ={};
-	const sanitizer :Partial<DomSanitizer> ={};
-	const popupControlService :Partial<PopupControlService> ={};
-	const matDialog :Partial<MatDialog> ={};
+    const toasterService: Partial<ToasterService> = {
+        success: jest.fn(),
+        error: jest.fn()
+    };
+    const tenantService: Partial<TenantService> = {
+        tenantData$: of({ favicon: 'sample-favicon', tenantData: { logo: 'test' } }) as any
+    };
+    const sanitizer: Partial<DomSanitizer> = {
+        bypassSecurityTrustResourceUrl:jest.fn()
+    };
+    const popupControlService: Partial<PopupControlService> = {
+        changePopupStatus: jest.fn()
+    };
+    const matDialog: Partial<MatDialog> = {};
 
     beforeAll(() => {
         component = new TermsAndConditionsPopupComponent(
             userService as UserService,
-			resourceService as ResourceService,
-			toasterService as ToasterService,
-			tenantService as TenantService,
-			sanitizer as DomSanitizer,
-			popupControlService as PopupControlService,
-			matDialog as MatDialog
+            resourceService as ResourceService,
+            toasterService as ToasterService,
+            tenantService as TenantService,
+            sanitizer as DomSanitizer,
+            popupControlService as PopupControlService,
+            matDialog as MatDialog
         )
     });
 
@@ -49,8 +78,56 @@ describe('TermsAndConditionsPopupComponent', () => {
         jest.clearAllMocks();
         jest.resetAllMocks();
     });
-            
+
     it('should create a instance of component', () => {
         expect(component).toBeTruthy();
+    });
+    it('should call the ngOninit method and  userProfile need to be', () => {
+        userService._userProfile = { 'organisations': ['01229679766115942443'] } as any;
+        tenantService._tenantData$=jest.fn().mockReturnValue(of(tNcMockResponse.tenantMockData) as any) as any;
+        userService._userData$ = jest.fn().mockReturnValue(throwError({error:true})as any)as any;
+        component.ngOnInit();
+        const obj ={
+            userId: 'sample-uid',
+            rootOrgId: 'sample-root-id',
+            rootOrg: {},
+            hashTagIds: [ 'id' ]
+          }
+        expect(JSON.stringify(component['userProfile'])).toEqual(JSON.stringify(obj));
+    });
+    it('should call the ngOninit method and  userProfile need to be', () => {
+        component.tncUrl='https://sunbird.org/termsandcond/demo.html'
+        userService._userProfile = { 'organisations': ['01229679766115942443'] } as any;
+        tenantService._tenantData$=jest.fn().mockReturnValue(of(tNcMockResponse.tenantMockData) as any) as any;
+        userService._userData$ = jest.fn().mockReturnValue(throwError({error:true})as any)as any;
+        component.ngOnInit();
+        expect(sanitizer.bypassSecurityTrustResourceUrl).toBeCalled();
+    });
+    it('should call acceptTermsAndConditions api', () => {
+        component.disableContinueBtn = true;
+        userService._userProfile = { 'organisations': ['01229679766115942443'] }as any;
+        jest.spyOn(userService, 'acceptTermsAndConditions').mockReturnValue(of({}) as any);
+        jest.spyOn(component, 'onClose');
+        component.onSubmitTnc('1234');
+        expect(component.onClose).toHaveBeenCalled();
+      });
+    describe("ngOnDestroy", () => {
+        it('should destroy sub', () => {
+            component['userSubscription'] = {
+                unsubscribe: jest.fn(),
+            } as any;
+            component['tenantDataSubscription'] = {
+                unsubscribe: jest.fn(),
+            } as any;
+            component.unsubscribe = {
+                next: jest.fn(),
+                complete: jest.fn()
+            } as any;
+            component.ngOnDestroy();
+            expect(component.unsubscribe.next).toHaveBeenCalled();
+            expect(component.unsubscribe.complete).toHaveBeenCalled();
+            expect(component.userSubscription.unsubscribe).toHaveBeenCalled();
+            expect(component.tenantDataSubscription.unsubscribe).toHaveBeenCalled();
+        });
     });
 });
