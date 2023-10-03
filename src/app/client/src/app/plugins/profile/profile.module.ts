220f6ecaa42a4d7e5131e812f5ea4386cbd76b57
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2023-10-03 14:57:42 +0530

    Issue #ED-2848 feat: removed otp-modal

--- src/app/client/src/app/plugins/profile/profile.module.ts
+++ src/app/client/src/app/plugins/profile/profile.module.ts
@@ -5,7 +5,7 @@ import { SharedModule } from '@sunbird/shared';
 import { ProfileRoutingModule } from './profile-routing.module';
 import {
   ProfilePageComponent, ProfileBadgeComponent, UpdateContactDetailsComponent,
-  AccountRecoveryInfoComponent, CreateUserComponent,DeleteUserComponent,DeleteAccountComponent,OtpModalComponent, ChooseUserComponent, SubmitTeacherDetailsComponent
+  AccountRecoveryInfoComponent, CreateUserComponent,DeleteUserComponent,DeleteAccountComponent, ChooseUserComponent, SubmitTeacherDetailsComponent
 } from './components';
 import { SuiSelectModule, SuiModalModule, SuiAccordionModule, SuiPopupModule, SuiDropdownModule,
   SuiProgressModule, SuiRatingModule, SuiCollapseModule } from 'ng2-semantic-ui-v9';
@@ -62,7 +62,6 @@ export const csCertificateServiceFactory = (csLibInitializerService: CsLibInitia
    CreateUserComponent,
    DeleteUserComponent,
    DeleteAccountComponent,
-   OtpModalComponent,
    ChooseUserComponent,
    SubmitTeacherDetailsComponent],
   providers: [
