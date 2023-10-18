2d961e247356deafeb05bc0e40817b536898535a
sedin-tushar <tushar@sedintechnologies.com>
2023-10-18 13:24:29 +0530

    Imporoving Code Quality for Explore Content

--- src/app/client/src/app/modules/public/module/explore/components/explore-content/explore-content.component.spec.data.ts
+++ src/app/client/src/app/modules/public/module/explore/components/explore-content/explore-content.component.spec.data.ts
@@ -1224,3 +1224,121 @@ export const Response = {
     ]
 }
 };
+export let data = {
+  "id": "api.content.search",
+  "ver": "1.0",
+  "ts": "2023-10-10T13:56:50.374Z",
+  "params": {
+      "resmsgid": "dc3a4e60-6774-11ee-a1e6-236e810e887c",
+      "msgid": "6803be90-3c86-4964-a5db-fdb8b39dd9cc",
+      "status": "successful",
+      "err": null,
+      "errmsg": null
+  },
+  "responseCode": "OK",
+  "result": {
+      "count": 1,
+      "content": [
+          {
+              "trackable": {
+                  "enabled": "No",
+                  "autoBatch": "No"
+              },
+              "identifier": "do_113777389672423424171",
+              "primaryCategory": "Course Unit",
+              "channel": "0137541424673095687",
+              "name": "Course Unit",
+              "mimeType": "application/vnd.ekstep.content-collection",
+              "contentType": "CourseUnit",
+              "pkgVersion": 1,
+              "objectType": "Content",
+              "orgDetails": {
+                  "email": null,
+                  "orgName": "Sunbird Org"
+              }
+          }
+      ],
+      "facets": [
+          {
+              "values": [   {
+                      "name": "grains",
+                      "count": 9
+                  },
+                  {
+                      "name": "horticulture",
+                      "count": 32
+                  }],
+
+              "name": "foodcrops"
+          },
+          {
+              "values": [   {
+                      "name": "feedingandnutrition",
+                      "count": 9
+                  },
+                  {
+                      "name": "geneticsandselection",
+                      "count": 32
+                  }],
+              "name": "livestockmanagement"
+          },
+          {
+              "values": [
+                  {
+                      "name": "course unit",
+                      "count": 1
+                  }
+              ],
+              "name": "primaryCategory"
+          },
+          {
+              "values": [   {
+                      "name": "crops",
+                      "count": 9
+                  },
+                  {
+                      "name": "pastures",
+                      "count": 32
+                  }],
+              "name": "commercialcrops"
+          },
+          {
+              "values": [   {
+                      "name": "beefcattle",
+                      "count": 9
+                  },
+                  {
+                      "name": "bees",
+                      "count": 32
+                  }],
+              "name": "livestockspecies"
+          },
+          {
+              "values": [   {
+                      "name": "diseases",
+                      "count": 9
+                  }
+                  ],
+              "name": "animalwelfare"
+          },
+          {
+              "values": [
+                  {
+                      "name": "content",
+                      "count": 1
+                  }
+              ],
+              "name": "mediaType"
+          },
+          {
+              "values": [
+                  {
+                      "name": "application/vnd.ekstep.content-collection",
+                      "count": 1
+                  }
+              ],
+              "name": "mimeType"
+          }
+      ]
+  }
+}
--- src/app/client/src/app/modules/public/module/explore/components/explore-content/explore-content.component.ts
+++ src/app/client/src/app/modules/public/module/explore/components/explore-content/explore-content.component.ts
@@ -12,6 +12,7 @@ import { takeUntil, map, mergeMap, first, debounceTime, tap, delay } from 'rxjs/
 import { CacheService } from '../../../../../shared/services/cache-service/cache.service';
 import { ContentManagerService } from '../../../offline/services';
 import {omit, groupBy, get, uniqBy, toLower, find, map as _map, forEach, each} from 'lodash-es';
+import { data } from './explore-content.component.spec.data'
 
 @Component({
   templateUrl: './explore-content.component.html',
@@ -234,7 +235,7 @@ export class ExploreContentComponent implements OnInit, OnDestroy, AfterViewInit
       facets: this.globalSearchFacets,
       params: this.configService.appConfig.ExplorePage.contentApiQueryParams || {}
     };
-    _.filter(Object.keys(this.queryParams),filterValue => { 
+    _.filter(Object.keys(this.queryParams),filterValue => {
       if(((_.get(this.allTabData , 'search.facets').indexOf(filterValue) !== -1)))
       {
           option.filters[filterValue] = (typeof(this.queryParams[filterValue]) === "string" ) ? this.queryParams[filterValue].split(',') : this.queryParams[filterValue];
@@ -256,124 +257,7 @@ export class ExploreContentComponent implements OnInit, OnDestroy, AfterViewInit
     if (cbseNcertExists) {
       option.filters.se_boards = ['CBSE'];
     }
-    let data = {
-            "id": "api.content.search",
-            "ver": "1.0",
-            "ts": "2023-10-10T13:56:50.374Z",
-            "params": {
-                "resmsgid": "dc3a4e60-6774-11ee-a1e6-236e810e887c",
-                "msgid": "6803be90-3c86-4964-a5db-fdb8b39dd9cc",
-                "status": "successful",
-                "err": null,
-                "errmsg": null
-            },
-            "responseCode": "OK",
-            "result": {
-                "count": 1,
-                "content": [
-                    {
-                        "trackable": {
-                            "enabled": "No",
-                            "autoBatch": "No"
-                        },
-                        "identifier": "do_113777389672423424171",
-                        "primaryCategory": "Course Unit",
-                        "channel": "0137541424673095687",
-                        "name": "Course Unit",
-                        "mimeType": "application/vnd.ekstep.content-collection",
-                        "contentType": "CourseUnit",
-                        "pkgVersion": 1,
-                        "objectType": "Content",
-                        "orgDetails": {
-                            "email": null,
-                            "orgName": "Sunbird Org"
-                        }
-                    }
-                ],
-                "facets": [
-                    {
-                        "values": [   {
-                                "name": "grains",
-                                "count": 9
-                            },
-                            {
-                                "name": "horticulture",
-                                "count": 32
-                            }],
 
-                        "name": "foodcrops"
-                    },
-                    {
-                        "values": [   {
-                                "name": "feedingandnutrition",
-                                "count": 9
-                            },
-                            {
-                                "name": "geneticsandselection",
-                                "count": 32
-                            }],
-                        "name": "livestockmanagement"
-                    },
-                    {
-                        "values": [
-                            {
-                                "name": "course unit",
-                                "count": 1
-                            }
-                        ],
-                        "name": "primaryCategory"
-                    },
-                    {
-                        "values": [   {
-                                "name": "crops",
-                                "count": 9
-                            },
-                            {
-                                "name": "pastures",
-                                "count": 32
-                            }],
-                        "name": "commercialcrops"
-                    },
-                    {
-                        "values": [   {
-                                "name": "beefcattle",
-                                "count": 9
-                            },
-                            {
-                                "name": "bees",
-                                "count": 32
-                            }],
-                        "name": "livestockspecies"
-                    },
-                    {
-                        "values": [   {
-                                "name": "diseases",
-                                "count": 9
-                            }
-                            ],
-                        "name": "animalwelfare"
-                    },
-                    {
-                        "values": [
-                            {
-                                "name": "content",
-                                "count": 1
-                            }
-                        ],
-                        "name": "mediaType"
-                    },
-                    {
-                        "values": [
-                            {
-                                "name": "application/vnd.ekstep.content-collection",
-                                "count": 1
-                            }
-                        ],
-                        "name": "mimeType"
-                    }
-                ]
-            }
-        }
     this.searchService.contentSearch(option)
       .pipe(
         mergeMap(service_data => {
