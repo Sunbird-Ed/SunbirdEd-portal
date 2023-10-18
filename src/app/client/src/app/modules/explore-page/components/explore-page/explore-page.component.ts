8a5b798e702b8415951a6d495f5b48c296376fe9
sedin-tushar <tushar@sedintechnologies.com>
2023-10-18 13:20:43 +0530

    code quality refactor

--- src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
+++ src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
@@ -16,7 +16,7 @@ import * as _ from 'lodash-es';
 import { CacheService } from '../../../shared/services/cache-service/cache.service';
 import { ProfileService } from '@sunbird/profile';
 import { SegmentationTagService } from '../../../core/services/segmentation-tag/segmentation-tag.service';
-
+import { response } from './explore-page.component.spec.data'
 @Component({
     selector: 'app-explore-page-component',
     templateUrl: './explore-page.component.html',
@@ -465,124 +465,7 @@ export class ExplorePageComponent implements OnInit, OnDestroy, AfterViewInit {
                         if (this.userService.loggedIn) {
                             option.filters['visibility'] = option.filters['channel'] = [];
                         }
-                        let response = {
-                            "id": "api.content.search",
-                            "ver": "1.0",
-                            "ts": "2023-10-10T13:56:50.374Z",
-                            "params": {
-                                "resmsgid": "dc3a4e60-6774-11ee-a1e6-236e810e887c",
-                                "msgid": "6803be90-3c86-4964-a5db-fdb8b39dd9cc",
-                                "status": "successful",
-                                "err": null,
-                                "errmsg": null
-                            },
-                            "responseCode": "OK",
-                            "result": {
-                                "count": 1,
-                                "content": [
-                                    {
-                                        "trackable": {
-                                            "enabled": "No",
-                                            "autoBatch": "No"
-                                        },
-                                        "identifier": "do_113777389672423424171",
-                                        "primaryCategory": "Course Unit",
-                                        "channel": "0137541424673095687",
-                                        "name": "Course Unit",
-                                        "mimeType": "application/vnd.ekstep.content-collection",
-                                        "contentType": "CourseUnit",
-                                        "pkgVersion": 1,
-                                        "objectType": "Content",
-                                        "orgDetails": {
-                                            "email": null,
-                                            "orgName": "Sunbird Org"
-                                        }
-                                    }
-                                ],
-                                "facets": [
-                                    {
-                                        "values": [   {
-                                                "name": "grains",
-                                                "count": 9
-                                            },
-                                            {
-                                                "name": "horticulture",
-                                                "count": 32
-                                            }],
 
-                                        "name": "foodcrops"
-                                    },
-                                    {
-                                        "values": [   {
-                                                "name": "feedingandnutrition",
-                                                "count": 9
-                                            },
-                                            {
-                                                "name": "geneticsandselection",
-                                                "count": 32
-                                            }],
-                                        "name": "livestockmanagement"
-                                    },
-                                    {
-                                        "values": [
-                                            {
-                                                "name": "course unit",
-                                                "count": 1
-                                            }
-                                        ],
-                                        "name": "primaryCategory"
-                                    },
-                                    {
-                                        "values": [   {
-                                                "name": "crops",
-                                                "count": 9
-                                            },
-                                            {
-                                                "name": "pastures",
-                                                "count": 32
-                                            }],
-                                        "name": "commercialcrops"
-                                    },
-                                    {
-                                        "values": [   {
-                                                "name": "beefcattle",
-                                                "count": 9
-                                            },
-                                            {
-                                                "name": "bees",
-                                                "count": 32
-                                            }],
-                                        "name": "livestockspecies"
-                                    },
-                                    {
-                                        "values": [   {
-                                                "name": "diseases",
-                                                "count": 9
-                                            }
-                                            ],
-                                        "name": "animalwelfare"
-                                    },
-                                    {
-                                        "values": [
-                                            {
-                                                "name": "content",
-                                                "count": 1
-                                            }
-                                        ],
-                                        "name": "mediaType"
-                                    },
-                                    {
-                                        "values": [
-                                            {
-                                                "name": "application/vnd.ekstep.content-collection",
-                                                "count": 1
-                                            }
-                                        ],
-                                        "name": "mimeType"
-                                    }
-                                ]
-                            }
-                        }
                         return this.searchService.contentSearch(option)
                             .pipe(
                                 map((service_response) => {
