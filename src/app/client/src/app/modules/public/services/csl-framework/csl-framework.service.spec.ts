ce933ea485dab50be0c349fa34bfcca735042370
sedin-tushar <tushar@sedintechnologies.com>
2023-11-21 17:10:40 +0530

    Test Cases for Newly written code in Csl Framework

--- src/app/client/src/app/modules/public/services/csl-framework/csl-framework.service.spec.ts
+++ src/app/client/src/app/modules/public/services/csl-framework/csl-framework.service.spec.ts
@@ -40,4 +40,39 @@ describe('CslFrameworkService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should set default framework and fetch from channel service', () => {
+    const setDefaultFWSpy = jest.spyOn(service, 'setDefaultFWforCsl');
+    const setFWCatConfigSpy = jest.spyOn(service, 'setFWCatConfigFromCsl');
+
+    service.setDefaultFWforCsl();
+
+    expect(setDefaultFWSpy).toHaveBeenCalled();
+    expect(setFWCatConfigSpy).toHaveBeenCalled();
+  });
+
+  it('should fetch framework categories object', () => {
+    const getFrameworkCategoriesObjectSpy = jest.spyOn(service, 'getFrameworkCategoriesObject');
+    service.getFrameworkCategoriesObject();
+    expect(getFrameworkCategoriesObjectSpy).toHaveBeenCalled();
+  });
+
+  it('should fetch framework categories', () => {
+    const getFrameworkCategoriesSpy = jest.spyOn(service, 'getFrameworkCategories');
+    service.getFrameworkCategories();
+    expect(getFrameworkCategoriesSpy).toHaveBeenCalled();
+  });
+
+  it('should resolve the Promise and log if userSelFramework is not provided', async () => {
+    const spyConsoleLog = jest.spyOn(console, 'log');
+    const userSelFramework = undefined;
+    await service.setFWCatConfigFromCsl(userSelFramework);
+    expect(spyConsoleLog).toHaveBeenCalledWith('userSelFramework not found');
+    expect(localStorage.getItem('fwCategoryObject')).toBeNull();
+  });
+
+  it('should log a message when userSelFramework is not provided', () => {
+    service.setFwCatObjConfigFromCsl(undefined);
+    expect(console.log).toHaveBeenCalledWith('userSelFramework not found');
+  });
+
 });
