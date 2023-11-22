381687c521d4df1ec331882a574b43b1792945bc
mithun30052001 <lmithun01@gmail.com>
2023-11-22 17:56:09 +0530

    ED-0000 feat:Onboarding skip data is sent as a behaviorsubject observable to the components needing it

--- src/app/client/src/app/service/popup-control.service.ts
+++ src/app/client/src/app/service/popup-control.service.ts
@@ -8,11 +8,18 @@ export class PopupControlService {
 
   private popupSource = new BehaviorSubject(true);
   public checkPopupStatus = this.popupSource.asObservable();
-
+  private onboardPopupSubject = new BehaviorSubject<any>(null);
   constructor() { }
 
   changePopupStatus(value: boolean) {
     this.popupSource.next(value);
   }
 
+  setOnboardingData(formData: any) {
+    this.onboardPopupSubject.next(formData);
+  }
+
+  getOnboardingData() {
+    return this.onboardPopupSubject.asObservable();
+  }
 }
