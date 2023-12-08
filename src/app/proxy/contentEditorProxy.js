4bbe18932966b00a6c182b79e1738e0d16420afa
Rajnish Dargan <rajnish.code30@gmail.com>
2023-12-08 16:26:03 +0530

    Issue #IQ-587 feat: Resolving review comments

--- src/app/proxy/contentEditorProxy.js
+++ src/app/proxy/contentEditorProxy.js
@@ -146,10 +146,7 @@ module.exports = function (app) {
   app.get([
     '/action/questionset/v2/read/:do_id',
     '/action/question/v2/read/:do_id',
-    '/action/questionset/v2/hierarchy/:do_id',
-    '/action/questionset/v2/read/:do_id',
-    '/action/question/v2/read/:do_id',
-    '/action/questionset/v2/hierarchy/:do_id',
+    '/action/questionset/v2/hierarchy/:do_id'
     ],
     isAPIWhitelisted.isAllowed(),
     addCorsHeaders,
