cbf3f9bed2439e5f04f06c1eda92eeda3590e0ec
sedin-tushar <tushar@sedintechnologies.com>
2023-12-12 18:02:56 +0530

    Test Case Coverages for Newly Written Code

--- src/app/client/src/app/modules/shared/pipes/TranslateJsonPipe/translate-json.pipe.spec.ts
+++ src/app/client/src/app/modules/shared/pipes/TranslateJsonPipe/translate-json.pipe.spec.ts
@@ -1,8 +1,41 @@
-import { TranslateJsonPipe } from '../TranslateJsonPipe/translate-json.pipe';
+import { TranslateJsonPipe } from './translate-json.pipe';
+import { TranslateService } from '@ngx-translate/core';
 
 describe('TranslateJsonPipe', () => {
-  it('create an instance', () => {
-    // const pipe = new TranslateJsonPipe();
-    // expect(pipe).toBeTruthy();
+  let translateJsonPipe: TranslateJsonPipe;
+  let translateService: TranslateService;
+
+  beforeEach(() => {
+    translateJsonPipe = new TranslateJsonPipe(translateService);
   });
+
+  it('should create an instance', () => {
+    expect(translateJsonPipe).toBeTruthy();
+  });
+
+  it('should return the original value if parsing fails', () => {
+    const invalidJsonString = 'invalid-json-string';
+    const result = translateJsonPipe.transform(invalidJsonString);
+    expect(result).toEqual(invalidJsonString);
+  });
+
+  it('should return the original value if parsing fails', () => {
+    const invalidJsonString = 'invalid-json-string';
+    const result = translateJsonPipe.transform(invalidJsonString);
+    expect(result).toEqual(invalidJsonString);
+  });
+
+  it('should return the original value if value is not a valid JSON string', () => {
+    const invalidJsonString = 'invalid-json-string';
+    const result = translateJsonPipe.transform(invalidJsonString);
+    expect(result).toEqual(invalidJsonString);
+  });
+
+  it('should return the original value if value is null or undefined', () => {
+    const resultNull = translateJsonPipe.transform(null);
+    const resultUndefined = translateJsonPipe.transform(undefined);
+    expect(resultNull).toEqual(null);
+    expect(resultUndefined).toEqual(undefined);
+  });
+
 });
