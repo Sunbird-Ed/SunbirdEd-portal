9f2e3b4b49767e8ed10f8d4b236d43a10b79e978
mithun30052001 <lmithun01@gmail.com>
2023-12-14 10:55:53 +0530

    ED-3285 fix: A condition for skipping observations tab is added

--- src/app/client/src/app/modules/core/components/content-type/content-type.component.ts
+++ src/app/client/src/app/modules/core/components/content-type/content-type.component.ts
@@ -6,6 +6,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { combineLatest, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { TelemetryService } from '@sunbird/telemetry';
+import { environment } from '@sunbird/environment';
 
 
 @Component({
@@ -160,7 +161,7 @@ export class ContentTypeComponent implements OnInit, OnDestroy {
   }
   makeFormChange() {
     const index = this.contentTypes.findIndex(cty => cty.contentType === 'observation');
-    if (this.userType != 'administrator') {
+    if (this.userType != 'administrator' || !(environment.sunbirdMlConfigured)) {
       this.contentTypes[index].isEnabled = false;
     } else {
       this.contentTypes[index].isEnabled = true;
