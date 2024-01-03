055cdbea6378fc4838e943fda8bf8d14c3af1bab
= <samarth.nigam@trigyn.com>
2024-01-03 17:35:47 +0530

    fixing search api on medium field change for rating count on cards

--- src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
+++ src/app/client/src/app/modules/explore-page/components/explore-page/explore-page.component.ts
@@ -409,6 +409,7 @@ export class ExplorePageComponent implements OnInit, OnDestroy, AfterViewInit {
     }
 
     private fetchContents() {
+        const fieldsToAdd = ['me_averageRating', 'me_totalRatingsCount', 'me_totalPlaySessionCount'];
         return this.fetchContents$
             .pipe(
                 skipWhile(data => data === undefined || data === null),
@@ -452,7 +453,11 @@ export class ExplorePageComponent implements OnInit, OnDestroy, AfterViewInit {
                     if (!this.isUserLoggedIn() && get(this.selectedFilters, 'channel') && get(this.selectedFilters, 'channel.length') > 0) {
                         request.channelId = this.selectedFilters['channel'];
                     }
-                    request.fields.push("me_averageRating", "me_totalPlaySessionCount" , "me_totalRatingsCount");
+                    fieldsToAdd.forEach(field => {
+                        if (!request.fields.includes(field)) {
+                            request.fields.push(field);
+                        }
+                    });
                     const option = this.searchService.getSearchRequest(request, get(filters, 'primaryCategory'));
                         const params = _.get(this.activatedRoute, 'snapshot.queryParams');
                         _.filter(Object.keys(params),filterValue => { 
@@ -468,7 +473,11 @@ export class ExplorePageComponent implements OnInit, OnDestroy, AfterViewInit {
                     if (this.userService.loggedIn) {
                         option.filters['visibility'] = option.filters['channel'] = [];
                     }
-                    option.facets.push("me_averageRating", "me_totalPlaySessionCount" , "me_totalRatingsCount");
+                    fieldsToAdd.forEach(field => {
+                        if (!option.facets.includes(field)) {
+                            option.facets.push(field);
+                        }
+                    });
                     return this.searchService.contentSearch(option)
                         .pipe(
                             map((response) => {
