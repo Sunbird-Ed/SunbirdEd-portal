ea435cba73b109b9b1d4bea953673dc028ba6eba
Rajnish Dargan <rajnish.code30@gmail.com>
2024-01-22 18:15:05 +0530

    Issue #ED-3391 fix: List API is failing with forbidden Error

--- src/app/helpers/whitelistApis.js
+++ src/app/helpers/whitelistApis.js
@@ -1705,6 +1705,13 @@ const API_LIST = {
         ROLE.CONTENT_CREATOR
       ]
     },
+    '/action/question/v2/list': {
+      description: 'Question List',
+      checksNeeded: ['ROLE_CHECK'],
+      ROLE_CHECK: [
+        ROLE.CONTENT_CREATOR
+      ]
+    },
     '/action/object/category/definition/v1/read': {
       description: 'Object category read',
       checksNeeded: ['ROLE_CHECK'],
@@ -2025,6 +2032,7 @@ const API_LIST = {
     '/action/question/v2/review/:do_id',
     '/action/question/v2/publish/:do_id',
     '/action/question/v2/update/:do_id',
+    '/action/question/v2/list',
     '/report/request/list/:tag',
     '/dataset/v1/request/list/:tag',
     '/dataset/v1/request/read/:tag',
