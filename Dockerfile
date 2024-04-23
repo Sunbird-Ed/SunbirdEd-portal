25b86d501fde2ad854e074b7e9922b3170bb752c
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2024-04-23 16:59:25 +0530

    ED-4128 feat: refactor docker script for deployment

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,65 @@
-# Dockerfile for the player setup
-FROM node:18.20.2-slim
-RUN useradd -u 1001 -md /home/sunbird sunbird
-WORKDIR /home/sunbird
-COPY --chown=sunbird . /home/sunbird/app_dist/
-USER sunbird
-WORKDIR /home/sunbird/app_dist
+# Use a base image suitable for building the client and server (e.g., Node.js)
+FROM node:18.20.2 AS builder
+
+# Install nvm to manage Node.js versions
+RUN curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Set the working directory for the client build
+WORKDIR /usr/src/app/client
+
+# Remove existing node_modules and yarn.lock to ensure clean installation
+RUN rm -rf node_modules \
+    && rm -rf yarn.lock \
+    && yarn cache clean
+
+# Copy the client code into the Docker container
+COPY src/app/client ./
+
+# Install client dependencies and build
+RUN yarn install --no-progress --production=true \
+    && npm run build
+
+
+# Set the working directory for server build
+WORKDIR /app/app_dist
+
+# Run the command to rename the file
+RUN mv dist/index.html dist/index.ejs
+
+# Set the working directory for server build
+WORKDIR /usr/src/app
+
+# Copy server-related files into the app_dist directory
+COPY src/app/libs ./app_dist/libs
+COPY src/app/helpers ./app_dist/helpers
+COPY src/app/proxy ./app_dist/proxy
+COPY src/app/resourcebundles ./app_dist/resourcebundles
+COPY src/app/package.json ./app_dist/
+COPY src/app/framework.config.js ./app_dist/
+COPY src/app/sunbird-plugins ./app_dist/sunbird-plugins
+COPY src/app/routes ./app_dist/routes
+COPY src/app/constants ./app_dist/constants
+COPY src/app/controllers ./app_dist/controllers
+COPY src/app/server.js ./app_dist/
+
+# Install server dependencies in the app_dist directory
+WORKDIR /usr/src/app/app_dist
+RUN yarn install --ignore-engines --no-progress --production=true
+
+# Use a new base image for the final stage
+FROM node:18.20.2
+
+# Set the working directory
+WORKDIR /usr/src/app
+
+# Copy the built app from the builder stage
+COPY --from=builder /usr/src/app ./
+
+# Run the build script to perform additional tasks (e.g., phraseAppPull)
+RUN node helpers/resourceBundles/build.js -task="phraseAppPull"
+
+# Expose the port used by the server
 EXPOSE 3000
+
+# Start the server in the background
 CMD ["node", "server.js", "&"]
--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ NODE_VERSION=18.20.2
 echo "Starting portal build from build.sh"
 set -euo pipefail	
 export NVM_DIR="$HOME/.nvm"
-echo $NVM_DIR
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 build_tag=$1
@@ -16,118 +15,86 @@ buildDockerImage=$4
 buildCdnAssests=$5
 echo "buildDockerImage: " $buildDockerImage
 echo "buildCdnAssests: " $buildCdnAssests
-if [ $buildCdnAssests == true ]
-then
-    cdnUrl=$6
-    echo "cdnUrl: " $cdnUrl
-fi
+### docker test start
+# if [ $buildCdnAssests == true ]
+# then
+#     cdnUrl=$6
+#     echo "cdnUrl: " $cdnUrl
+# fi
 
-commit_hash=$(git rev-parse --short HEAD)
+# commit_hash=$(git rev-parse --short HEAD)
 # nvm install $NODE_VERSION # same is used in client and server
-sudo -i
 
-# Start by installing Node 20:
+# cd src/app
+# mkdir -p app_dist/ # this folder should be created prior server and client build
+# rm -rf dist-cdn # remove cdn dist folder
 
-sudo apt-get install python3 g++ make python3-pip gcc bison
+# # function to run client build for docker image
+# build_client_docker(){
+#     echo "starting client local prod build"
+#     npm run build # Angular prod build
+#     echo "completed client local prod build"
+#     cd ..
+#     mv app_dist/dist/index.html app_dist/dist/index.ejs # rename index file
+# }
+# # function to run client build for cdn
+# build_client_cdn(){
+#     echo "starting client cdn prod build"
+#     npm run build-cdn -- --deployUrl $cdnUrl # prod command
+#     export sunbird_portal_cdn_url=$cdnUrl # required for inject-cdn-fallback task
+#     npm run inject-cdn-fallback
+#     echo "completed client cdn prod build"
+# }
+# # function to run client build
+# build_client(){
+#     echo "Building client in background"
+#     nvm use $NODE_VERSION
+#     cd client
+#     echo "starting client yarn install"
+#     yarn install --no-progress --production=true
+#     echo "completed client yarn install"
+#     if [ $buildDockerImage == true ]
+#     then
+#     build_client_docker & # run client local build in background 
+#     fi
+#     if [ $buildCdnAssests == true ]
+#     then
+#     build_client_cdn & # run client local build in background
+#     fi
+#     wait # wait for both build to complete
+#     echo "completed client post_build"
+# }
 
-curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n
-bash n 18.20.2
+# # function to run server build
+# build_server(){
+#     echo "Building server in background"
+#     echo "copying requied files to app_dist"
+#     cp -R libs helpers proxy resourcebundles package.json framework.config.js sunbird-plugins routes constants controllers server.js ./../../Dockerfile app_dist
+#     cd app_dist
+#     nvm use $NODE_VERSION
+#     echo "starting server yarn install"
+#     yarn install --ignore-engines --no-progress --production=true
+#     echo "completed server yarn install"
+#     node helpers/resourceBundles/build.js -task="phraseAppPull"
+# }
 
-# Node 18.20.2 is now at /usr/local/bin/node, but glibc 2.28 is missing:
-# node: /lib/aarch64-linux-gnu/libc.so.6: version `GLIBC_2.28' not found (required by node)
-# /usr/local/bin/node: /lib/aarch64-linux-gnu/libc.so.6: version `GLIBC_2.28' not found (required by /usr/local/bin/node)
+# build_client & # run client build in background 
+# if [ $buildDockerImage == true ]
+# then
+#    build_server & # run client build in background
+# fi
 
-# Build and install glibc 2.28:
-apt install -y gawk
-cd ~
-wget -c https://ftp.gnu.org/gnu/glibc/glibc-2.28.tar.gz
-tar -zxf glibc-2.28.tar.gz
-cd glibc-2.28
-pwd
-mkdir glibc-build
-cd glibc-build
-../configure --prefix=/opt/glibc-2.28
-make -j 4 # Use all 4 Jetson Nano cores for much faster building
-make install
-cd ..
-rm -fr glibc-2.28 glibc-2.28.tar.gz
- 
-# Patch the installed Node 18.20.2 to work with /opt/glibc-2.28 instead: 
-apt install -y patchelf
-patchelf --set-interpreter /opt/glibc-2.28/lib/ld-linux-x86-64.so.2 --set-rpath /opt/glibc-2.28/lib/:/lib/x86_64-linux-gnu/:/usr/lib/x86_64-linux-gnu/ /usr/local/bin/node
+# ## wait for both build to complete
+# wait 
 
-# Et voilà:
-node --version
+# BUILD_ENDTIME=$(date +%s)
+# echo "Client and Server Build complete Took $[$BUILD_ENDTIME - $STARTTIME] seconds to complete."
 
-cd src/app
-mkdir -p app_dist/ # this folder should be created prior server and client build
-rm -rf dist-cdn # remove cdn dist folder
-
-# function to run client build for docker image
-build_client_docker(){
-    echo "starting client local prod build"
-    npm run build # Angular prod build
-    echo "completed client local prod build"
-    cd ..
-    mv app_dist/dist/index.html app_dist/dist/index.ejs # rename index file
-}
-# function to run client build for cdn
-build_client_cdn(){
-    echo "starting client cdn prod build"
-    npm run build-cdn -- --deployUrl $cdnUrl # prod command
-    export sunbird_portal_cdn_url=$cdnUrl # required for inject-cdn-fallback task
-    npm run inject-cdn-fallback
-    echo "completed client cdn prod build"
-}
-# function to run client build
-build_client(){
-    echo "Building client in background"
-    nvm use $NODE_VERSION
-    cd client
-    echo "starting client yarn install"
-    yarn install --no-progress --production=true
-    echo "completed client yarn install"
-    if [ $buildDockerImage == true ]
-    then
-    build_client_docker & # run client local build in background 
-    fi
-    if [ $buildCdnAssests == true ]
-    then
-    build_client_cdn & # run client local build in background
-    fi
-    wait # wait for both build to complete
-    echo "completed client post_build"
-}
-
-# function to run server build
-build_server(){
-    echo "Building server in background"
-    echo "copying requied files to app_dist"
-    cp -R libs helpers proxy resourcebundles package.json framework.config.js sunbird-plugins routes constants controllers server.js ./../../Dockerfile app_dist
-    cd app_dist
-    nvm use $NODE_VERSION
-    echo "starting server yarn install"
-    yarn install --ignore-engines --no-progress --production=true
-    echo "completed server yarn install"
-    node helpers/resourceBundles/build.js -task="phraseAppPull"
-}
-
-build_client & # run client build in background 
-if [ $buildDockerImage == true ]
-then
-   build_server & # run client build in background
-fi
-
-## wait for both build to complete
-wait 
-
-BUILD_ENDTIME=$(date +%s)
-echo "Client and Server Build complete Took $[$BUILD_ENDTIME - $STARTTIME] seconds to complete."
-
-if [ $buildDockerImage == true ]
-then
-cd app_dist
-sed -i "/version/a\  \"buildHash\": \"${commit_hash}\"," package.json
+# if [ $buildDockerImage == true ]
+# then
+# cd app_dist
+# sed -i "/version/a\  \"buildHash\": \"${commit_hash}\"," package.json
+### docker test end
 echo "starting docker build"
 docker build --no-cache --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo "completed docker build"
