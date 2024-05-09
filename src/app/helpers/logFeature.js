1a36a6af4b07b3b17ea9661503a7380b1d7f2226
Abhishek Nagesh <abhishek.nagesh@tarento.com>
2024-05-09 11:09:02 +0530

    Issue #ED-4231 feat: added feature mapping in apiwhitelist file

--- src/app/helpers/logFeature.js
+++ src/app/helpers/logFeature.js
@@ -1,35 +1,34 @@
 'use strict';
 /**
- * @file - Handle telemetry for the features of Portal API(s)
+ * @file - Handle logs for the features of Portal API(s)
  * @since release-8.0.0
  * @version 1.0
  */
 
 const _ = require('lodash');
 const { pathToRegexp } = require("path-to-regexp");
-const { FEATURE_LIST, getMappingValue } = require('./featureList');
-
+const { API_LIST, getMappingValue } = require('./whitelistApis.js');
 /**
  * @description - Function to check whether
  * 1. Incoming API is having feature id (or) not
  * 2. If API does not exists then normal flow is contiued
- * 3. If incoming API is present in FEATURE_LIST ; then check URL pattern matching and add telemetry log for the feature
- * 4. Refer `FEATURE_LIST` 
+ * 3. If incoming API is present in API_LIST ; then check URL pattern matching and add telemetry log for the feature
+ * 4. Refer `API_LIST` 
  * @since release-8.0.0
  */
-const isLogFeature = () => {
+const mapFeature = () => {
   return function (req, res, next) {
     let REQ_URL = req.path;
     req.originalUrl = REQ_URL
 
-    _.forEach(FEATURE_LIST.URL_PATTERN, (url) => {
+    _.forEach(API_LIST.URL_PATTERN, (url) => {
       let regExp = pathToRegexp(url);
       if (regExp.test(REQ_URL)) {
         REQ_URL = url;
         return false;
       }
     });
-    if (_.get(FEATURE_LIST.URL, REQ_URL)) {
+    if (_.get(API_LIST.URL, REQ_URL)) {
       let mappingValue = getMappingValue(REQ_URL);
       req.feature = mappingValue
       next();
@@ -39,5 +38,5 @@ const isLogFeature = () => {
 };
 
 module.exports = {
-  isLogFeature
+  mapFeature
 };
