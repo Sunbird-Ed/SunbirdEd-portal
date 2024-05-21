82c9742e8c5b5870097e57d543708ed781243187
Rajeev Sathish <rajeev.sathish@tarento.com>
2024-05-21 11:30:08 +0530

    Issue #ED-4296 changing the end points for DialCode

--- src/app/proxy/contentEditorProxy.js
+++ src/app/proxy/contentEditorProxy.js
@@ -310,32 +310,6 @@ module.exports = function (app) {
     userResDecorator: userResDecorator
   })
 )
-app.post('/action/dialcode/v1/reserve/:do_id',
-    isAPIWhitelisted.isAllowed(),
-    proxy(contentServiceBaseUrl, {
-      preserveHostHdr: true,
-      limit: reqDataLimitOfContentUpload,
-      proxyReqOptDecorator: proxyUtils.decorateRequestHeaders(contentServiceBaseUrl),
-      proxyReqPathResolver: function (req) {
-        let originalUrl = req.originalUrl.replace('/action/', '')
-        return require('url').parse(contentServiceBaseUrl + originalUrl).path
-      },
-      userResDecorator: userResDecorator
-    })
-  )
-  app.get('/action/dialcode/v1/process/status/*',
-  isAPIWhitelisted.isAllowed(),
-  proxy(contentServiceBaseUrl, {
-    preserveHostHdr: true,
-    limit: reqDataLimitOfContentUpload,
-    proxyReqOptDecorator: proxyUtils.decorateRequestHeaders(contentServiceBaseUrl),
-    proxyReqPathResolver: function (req) {
-      let originalUrl = req.originalUrl.replace('/action/', '')
-      return require('url').parse(contentServiceBaseUrl + originalUrl).path
-    },
-    userResDecorator: userResDecorator
-  })
-)
   // asset api's  ends
 
   app.all('/action/*',
