5b3ab1b37afaffc71ac71884876e23107b7d830e
Harish Kumar Gangula <harish@sanketika.in>
2025-05-07 14:56:03 +0530

    Added github actions for the building the image

--- build.sh
+++ build.sh
@@ -9,10 +9,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 build_tag=$1
 name=player
-node=$2
-org=$3
-buildDockerImage=$4
-buildCdnAssests=$5
+buildDockerImage=$2
+buildCdnAssests=$3
 echo "buildDockerImage: " $buildDockerImage
 echo "buildCdnAssests: " $buildCdnAssests
 if [ $buildCdnAssests == true ]
@@ -94,7 +92,7 @@ then
 cd app_dist
 sed -i "/version/a\  \"buildHash\": \"${commit_hash}\"," package.json
 echo "starting docker build"
-docker build --no-cache --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
+docker build --no-cache --label commitHash=$(git rev-parse --short HEAD) -t ${name}:${build_tag} .
 echo "completed docker build"
 cd ../../..
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\",\"commit_hash\" : \"${commit_hash}\", \"node_name\" : \"$node\"} > metadata.json
