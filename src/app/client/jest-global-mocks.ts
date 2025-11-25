b58bce4ab19944b5bf67ad8c9db6d7b593949d60
JeraldJF <jeraldj@sanketika.in>
2025-11-25 10:55:38 +0530

    SB-I367: Test case changes

--- src/app/client/jest-global-mocks.ts
+++ src/app/client/jest-global-mocks.ts
@@ -1,36 +1,21 @@
-// import { jest } from '@jest/globals';
+// Polyfill for fetch API globals (Headers, Request, Response, FormData)
+// MUST be first before any other imports to ensure ky library can use them
+// Required for @digitalbazaar/http-client which uses ky
+if (typeof global.Headers === 'undefined') {
+  try {
+    const nodeFetch = require('node-fetch');
+    global.Headers = nodeFetch.Headers;
+    global.Request = nodeFetch.Request;
+    global.Response = nodeFetch.Response;
+    global.FormData = nodeFetch.FormData;
+    if (typeof global.fetch === 'undefined') {
+      global.fetch = nodeFetch.default;
+    }
+  } catch (e) {
+    console.error('Failed to load node-fetch polyfills:', e);
+  }
+}
 
-// Object.defineProperty(window, 'CSS', { value: null });
-
-// Object.defineProperty(window, 'EkTelemetry', { value: {} });
-
-// Object.defineProperty(document, 'doctype', {
-//   value: '<!DOCTYPE html>',
-// });
-
-// Object.defineProperty(window, 'getComputedStyle', {
-//   value: () => {
-//     return {
-//       display: 'none',
-//       appearance: ['-webkit-appearance'],
-//     };
-//   },
-// });
-
-// /**
-//  * ISSUE: https://github.com/angular/material2/issues/7101
-//  * Workaround for JSDOM missing transform property
-//  */
-// Object.defineProperty(document.body.style, 'transform', {
-//   value: () => {
-//     return {
-//       enumerable: true,
-//       configurable: true,
-//     };
-//   },
-// });
-
-// HTMLCanvasElement.prototype.getContext = <typeof HTMLCanvasElement.prototype.getContext>jest.fn();
 import { jest } from '@jest/globals';
 
 Object.defineProperty(window, 'CSS', { value: null });
@@ -63,10 +48,12 @@ Object.defineProperty(document.body.style, 'transform', {
 
 HTMLCanvasElement.prototype.getContext = <typeof HTMLCanvasElement.prototype.getContext>jest.fn();
 
-// Polyfill for TextEncoder and TextDecoder
-const { TextEncoder, TextDecoder } = require('util');
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
+// Polyfill for TextEncoder and TextDecoder (if not already set)
+if (typeof global.TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}
 
 // Polyfill for crypto.randomUUID if needed
 if (!global.crypto) {
